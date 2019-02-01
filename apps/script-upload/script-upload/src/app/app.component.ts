9de62f2e10e60b8fb0d15a0a32122134522f6c11
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-02-01 12:41:52 +0100

    Redirect when logout

--- apps/script-upload/script-upload/src/app/app.component.ts
+++ apps/script-upload/script-upload/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthQuery, User } from '@blockframes/auth';
-import { NgWallet } from '@blockframes/ethers';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -12,10 +12,13 @@ import { Observable } from 'rxjs';
 export class AppComponent implements OnInit {
   public user$: Observable<User>;
 
-  constructor(private auth: AuthQuery) {}
+  constructor(private auth: AuthQuery, private router: Router) {}
 
   async ngOnInit() {
     this.user$ = this.auth.select(state => state.user);
   }
 
+  public logout() {
+    this.router.navigate([''])
+  }
 }
