fcf47656fd7317ab1b3a0f15c1cfe4b7373d0628
Hugo <hhontans@cascade8.com>
2019-06-25 17:42:38 +0200

    implements controlValueAccessor on email-form-field

--- libs/ui/src/lib/form/form-field/email-form-field/email-form-field.component.ts
+++ libs/ui/src/lib/form/form-field/email-form-field/email-form-field.component.ts
@@ -1,5 +1,5 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, ChangeDetectionStrategy, Input, Optional, Self } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
 
 @Component({
   selector: 'email-form-field',
@@ -7,9 +7,55 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./email-form-field.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EmailFormFieldComponent  {
+export class EmailFormFieldComponent implements ControlValueAccessor {
 
-  @Input() form: FormGroup;
+  @Input() placeholder = 'Email';
+  @Input() hint: string;
+  @Input() required = true;
+  private _value = "";
 
-  constructor() {}
+  private onTouch: (value: string) => void = () => {};
+  private onChange: (value: string) => void = () => {};
+
+  constructor(
+    @Optional() @Self() public ngControl: NgControl,
+    ) {
+    if (this.ngControl !== null) {
+      this.ngControl.valueAccessor = this;
+    }
+  }
+
+  get value() {
+    const value = this._value;
+    return value;
+  }
+
+  set value(value: string) {
+    if (value !== undefined) {
+      this._value = value;
+      this.onChange(this._value);
+      this.onTouch(this._value);
+    }
+  }
+
+  public addEvent(event: any) {
+    event.stopPropagation();
+    this.value = event.target.value;
+    this.onChange(this.value);
+  }
+
+  public writeValue(value: string) {
+    if (value) {
+      this.value = value;
+      this.onChange(this.value);
+    }
+  }
+
+  public registerOnChange(fn: (value: string) => void) {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: (value: string) => void){
+    this.onTouch = fn;
+  }
 }
