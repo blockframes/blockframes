64cb4cdd20bbb058acc6359bb43c91abfd5be066
DK <jenna_kim1@hotmail.com>
2019-10-11 15:51:07 +0200

    removed unnecessary modules

--- libs/ui/src/lib/cropper/cropper.module.ts
+++ libs/ui/src/lib/cropper/cropper.module.ts
@@ -3,34 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { DropZoneDirective } from './drop-zone.directive';
 import { ImageCropperModule } from 'ngx-image-cropper';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Material
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
-// Angular Fire
-import { firebase } from '@env';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireStorageModule } from '@angular/fire/storage';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-
 // Components
 import { CropperComponent } from './cropper/cropper.component';
-import { CropperPageComponent } from './page/cropper-page.component';
 
 @NgModule({
-  declarations: [DropZoneDirective, CropperComponent, CropperPageComponent],
+  declarations: [DropZoneDirective, CropperComponent],
   imports: [
     CommonModule,
-    BrowserAnimationsModule,
     MatIconModule,
     MatProgressSpinnerModule,
-    ImageCropperModule,
-    AngularFireModule.initializeApp({firebase}),
-    AngularFireStorageModule,
-    AngularFirestoreModule
+    ImageCropperModule
   ],
-  exports: [DropZoneDirective, CropperComponent, CropperPageComponent]
+  exports: [DropZoneDirective, CropperComponent]
 })
 export class CropperModule {}
