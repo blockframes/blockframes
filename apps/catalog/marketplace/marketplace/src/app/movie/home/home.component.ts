067d4b9a6a270659191ca8eb81fdc624b5b3c9cf
DK <jenna_kim1@hotmail.com>
2019-10-14 15:14:41 +0200

    show in biggerboat

--- apps/catalog/marketplace/marketplace/src/app/movie/home/home.component.ts
+++ apps/catalog/marketplace/marketplace/src/app/movie/home/home.component.ts
@@ -1,9 +1,16 @@
 import { combineLatest, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 import { ChangeDetectionStrategy, Component, HostBinding, OnInit } from '@angular/core';
 import { Movie, MovieQuery } from '@blockframes/movie/movie/+state';
 
+function createOrg(org: any) {
+  return {
+    poster: 'posters',
+    ...org,
+  };
+}
 interface CarouselSection {
   title: string;
   subline: string;
@@ -17,11 +24,11 @@ interface CarouselSection {
 })
 export class MarketplaceHomeComponent implements OnInit {
   @HostBinding('attr.page-id') pageId = 'catalog-marketplace-homepage';
-
+  public path$: Observable<string>;
   /** Observable to fetch all movies from the store */
   public moviesBySections$: Observable<CarouselSection[]>;
 
-  constructor(private movieQuery: MovieQuery) {}
+  constructor(private movieQuery: MovieQuery, private db: AngularFirestore) {}
 
   ngOnInit() {
     const latest$ = this.movieQuery.selectAll({
@@ -33,6 +40,10 @@ export class MarketplaceHomeComponent implements OnInit {
     const prizes$ = this.movieQuery.selectAll({
       filterBy: movies => !!movies.festivalPrizes.prizes
     });
+
+    this.path$ = this.db.doc('orgs/org1').valueChanges().pipe(
+      map(org => createOrg(org).poster),
+    );
 
     this.moviesBySections$ = combineLatest([latest$, scoring$, prizes$]).pipe(
       map(([latest, scoring, prizes]) => {
@@ -59,5 +70,12 @@ export class MarketplaceHomeComponent implements OnInit {
 
   public alignment(index: number) {
     return index % 2 === 0 ? 'start start' : 'start end';
+  }
+  public save(path: string) {
+    this.db.doc('orgs/org1').update({ poster: path });
+  }
+
+  public remove() {
+    this.db.doc('orgs/org1').set({  });
   }
 }
