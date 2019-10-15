8a0ccfd43460d3a9b240fa08917d7f58fb4ed1ff
hugo-hontans <hugohontans@gmail.com>
2019-10-15 15:49:06 +0200

    remove FormField

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -17,7 +17,6 @@ export * from './lib/file-sanitizer';
 export * from './lib/akita-helper/sort-movie-by';
 // forms
 export * from './lib/form/forms/types';
-export * from './lib/form/forms/field.form';
 export * from './lib/form/forms/list.form';
 export * from './lib/form/forms/entity.form';
 export * from './lib/form/forms/batch.form';
