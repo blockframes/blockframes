41e45ac00f7472dc038670ee37c8f59125d96054
Maxstgt <max.j.f@gmx.de>
2019-10-16 17:37:11 +0200

    update template for Francois

--- apps/catalog/marketplace/marketplace/src/app/distribution-right/+state/basket.service.spec.ts
+++ apps/catalog/marketplace/marketplace/src/app/distribution-right/+state/basket.service.spec.ts
@@ -1,75 +1,33 @@
-import { AkitaNgRouterStoreModule, RouterService } from '@datorama/akita-ng-router-store';
-import { FirestoreSettingsToken, AngularFirestore } from '@angular/fire/firestore';
+import { MovieStore } from '@blockframes/movie/movie/+state';
 import { Injectable } from '@angular/core';
-import {
-  SpectatorService,
-  createServiceFactory,
-} from '@ngneat/spectator/jest';
-import { BasketService } from './basket.service';
-import { mockProvider } from '@ngneat/spectator/jest';
-import { FirebaseOptionsToken, AngularFireModule } from '@angular/fire';
-import { environment } from '../../../environments/environment.prod';
+import { SpectatorService, createServiceFactory } from '@ngneat/spectator/jest';
 
 @Injectable()
-class MockBasketService {
+class BasketService {
+  constructor(private store: MovieStore) {}
   createFireStoreId(): boolean {
     return true;
   }
-}
-
-@Injectable()
-class OrganizationService {
-  createFireStoreId(): boolean {
-    return true;
-  }
-}
-
-@Injectable()
-class BasketQuery {
-  createFireStoreId(): boolean {
-    return true;
-  }
-}
-
-@Injectable()
-class BasketStore {
-  createFireStoreId(): boolean {
-    return true;
+  createMovie() {
+    this.store.add({} as any);
   }
 }
 
 describe('BasketService', () => {
-  let spectator: SpectatorService<BasketService>;
+  let spectator: SpectatorService<any>;
   const createService = createServiceFactory({
     service: BasketService,
-    imports: [AkitaNgRouterStoreModule.forRoot()],
-    providers: [
-      // Values in the providers must be the values you inject in the BasketService (store, queries, AngularFirestore)
-      mockProvider(MockBasketService, {
-        createFireStoreId: () => true
-      }),
-      mockProvider(OrganizationService, {}),
-      mockProvider(BasketQuery, {}),
-      mockProvider(BasketStore, {}),
-      AngularFirestore,
-      {
-        provide: FirestoreSettingsToken,
-        useValue: { host: 'localhost:8080', ssl: false }
-      },
-      {
-        provide: FirebaseOptionsToken,
-        useValue: { projectId: environment.firebase.appId }
-      },
-      mockProvider(RouterService)
-    ]
+    mocks: [MovieStore]
   });
 
   beforeEach(() => (spectator = createService()));
 
   it('should create firestore id', () => {
-    const mockService = spectator.get<MockBasketService>(MockBasketService);
-    mockService.createFireStoreId.mockReturnValue(true);
-    expect(spectator.service.createFireStoreId).toBeFalsy();
+    expect(spectator.service.createFireStoreId).toBeTruthy();
+  });
+
+  it('create movie', () => {
+    spectator.service.createMovie();
+    expect(spectator.get(MovieStore).add).toHaveBeenCalled();
   });
 });
- 
