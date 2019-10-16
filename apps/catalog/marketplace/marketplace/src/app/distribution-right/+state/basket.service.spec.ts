f1f15ce8e7268d2f8f1c0fcc4b3fa06583bd015c
Maxstgt <max.j.f@gmx.de>
2019-10-16 09:59:25 +0200

    first try of unit test

--- apps/catalog/marketplace/marketplace/src/app/distribution-right/+state/basket.service.spec.ts
+++ apps/catalog/marketplace/marketplace/src/app/distribution-right/+state/basket.service.spec.ts
@@ -1,7 +1,10 @@
-import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
+import { AkitaNgRouterStoreModule, RouterService } from '@datorama/akita-ng-router-store';
 import { FirestoreSettingsToken, AngularFirestore } from '@angular/fire/firestore';
 import { Injectable } from '@angular/core';
-import { SpectatorService, createServiceFactory } from '@ngneat/spectator/jest';
+import {
+  SpectatorService,
+  createServiceFactory,
+} from '@ngneat/spectator/jest';
 import { BasketService } from './basket.service';
 import { mockProvider } from '@ngneat/spectator/jest';
 import { FirebaseOptionsToken, AngularFireModule } from '@angular/fire';
@@ -39,7 +42,7 @@ describe('BasketService', () => {
   let spectator: SpectatorService<BasketService>;
   const createService = createServiceFactory({
     service: BasketService,
-    imports: [AkitaNgRouterStoreModule],
+    imports: [AkitaNgRouterStoreModule.forRoot()],
     providers: [
       // Values in the providers must be the values you inject in the BasketService (store, queries, AngularFirestore)
       mockProvider(MockBasketService, {
@@ -57,6 +60,7 @@ describe('BasketService', () => {
         provide: FirebaseOptionsToken,
         useValue: { projectId: environment.firebase.appId }
       },
+      mockProvider(RouterService)
     ]
   });
 
