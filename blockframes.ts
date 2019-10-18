406be80a2c1568a315fbf909addfe1d026d91d35
Maxstgt <max.j.f@gmx.de>
2019-10-18 09:47:49 +0200

    added some scripts

--- blockframes.ts
+++ blockframes.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env ts-node
 const commander = require('commander');
 const client = require('scp2');
-import { exec, execSync, spawn } from 'child_process';
+import { exec, execSync, spawn, ChildProcess, spawnSync } from 'child_process';
 import { copyFile, existsSync, readFileSync } from 'fs';
 require('dotenv').config();
 
@@ -12,7 +12,8 @@ require('dotenv').config();
 commander
   .option('-s, script <command>', 'Launches the following script command')
   .option('-d, deploy <file>', 'Launces the function to deploy the wanted file')
-  .option('-t, token <token>', 'Pass a token to the script');
+  .option('-t, token <token>', 'Pass a token to the script')
+  .option('-c, --config <command>', 'Defines the config for the function');
 
 commander.parse(process.argv);
 
@@ -66,7 +67,7 @@ const deployDemos = () => {
   execSync(`git push origin ${tag}`);
 };
 
-const deploySecrets = (firebase_ci_token: string) => {
+const deploySecrets = (firebase_ci_token?: string) => {
   let secrets, secretsTemplate;
   let tokenArg: string;
 
@@ -102,25 +103,172 @@ const deploySecrets = (firebase_ci_token: string) => {
   }
 };
 
+const deployProduction = () => {
+  exec('set -x && set -e');
+
+  copyFile('./env/env.prod.ts', './env/env.ts', err => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+  });
+
+  const build = spawnSync('npm run build:all');
+
+  execSync('firebase use prod');
+
+  // old command was ./secrets.sh blockframes , not sure where to put blockframes
+  this.deploySecrets('blockframes');
+
+  execSync('firebase deploy --except functions');
+  // we deploy everything but functions, they tend to crash
+  let rerunLoop = true;
+  while (rerunLoop) {
+    exec('firebase deploy --only functions', err => {
+      if (err) {
+        rerunLoop = true;
+      }
+      rerunLoop = false;
+    });
+  }
+};
+
 ////////////////////////////////
 // PORT PACKAGE.JSON WITH ENV //
 ////////////////////////////////
 
-const packageEnv = (command: string) => {
+const packageEnv = (command: string, config?: string) => {
+  let configENV = 'dev';
+  if (config) {
+    configENV = config;
+  }
   switch (command) {
     case 'start':
-      const npx = spawn('/home/max/.nvm/versions/node/v10.16.1/bin/npx ng serve --hmr --disable-host-check');
+      const npx: ChildProcess = spawn('npx', ['ng', 'serve', '--hmr', '--disable-host-check']);
       npx.stdout.on('data', stdout => {
         console.log(stdout.toString());
       });
+      break;
+    case 'build:main':
+      console.log(`excuting in '${configENV}' environment`);
+      const buildMain: ChildProcess = spawn('npx', [
+        'ng',
+        'build',
+        'main',
+        '--base-href',
+        `--configuration=${configENV}`
+      ]);
+      buildMain.stderr.on('data', stderr => {
+        console.log(stderr.toString());
+      });
+      buildMain.stdout.on('data', stdou => {
+        console.log(stdou.toString());
+      });
+      break;
+    case 'build:delivery':
+      console.log(`excuting in '${configENV}' environment`);
+      const buildDelivery: ChildProcess = spawn('npx', [
+        'ng',
+        'build',
+        'delivery',
+        '--base-href',
+        'delivery',
+        `--configuration=${configENV}`
+      ]);
+      buildDelivery.stderr.on('data', stderr => {
+        console.log(stderr.toString());
+      });
+      buildDelivery.stdout.on('data', stdou => {
+        console.log(stdou.toString());
+      });
+      buildDelivery.on('message', x => {
+        console.log(x);
+      });
+      break;
+    case 'build:movie-financing':
+      console.log(`excuting in '${configENV}' environment`);
+      const buildFinancing: ChildProcess = spawn('npx', [
+        'ng',
+        'build',
+        'movie-financing',
+        '--base-href',
+        'movie-financing',
+        `--configuration=${configENV}`
+      ]);
+      buildFinancing.stderr.on('data', stderr => {
+        console.log(stderr.toString());
+      });
+      buildFinancing.stdout.on('data', stdou => {
+        console.log(stdou.toString());
+      });
+      break;
+    case 'build:catalog-marketplace':
+      console.log(`excuting in '${configENV}' environment`);
+      const buildMarketplace: ChildProcess = spawn('npx', [
+        'ng',
+        'build',
+        'catalg-marketplace',
+        '--base-href',
+        'catalg-marketplace',
+        `--configuration=${configENV}`
+      ]);
+      buildMarketplace.stderr.on('data', stderr => {
+        console.log(stderr.toString());
+      });
+      buildMarketplace.stdout.on('data', stdou => {
+        console.log(stdou.toString());
+      });
+      break;
+    case 'build:catalog-dashboard':
+      console.log(`excuting in '${configENV}' environment`);
+      const buildDashboard: ChildProcess = spawn('npx', [
+        'ng',
+        'build',
+        'catalog-dashboard',
+        '--base-href',
+        'catalog-dashboard',
+        `--configuration=${configENV}`
+      ]);
+      buildDashboard.stderr.on('data', stderr => {
+        console.log(stderr.toString());
+      });
+      buildDashboard.stdout.on('data', stdou => {
+        console.log(stdou.toString());
+      });
+      break;
+    case 'build:functions':
+      console.log(`excuting in '${configENV}' environment`);
+      const buildFunctions: ChildProcess = spawn('npx', [
+        'ng',
+        'build',
+        'backend-functions',
+        '--base-href',
+        'backend-functions',
+        `--configuration=${configENV}`
+      ]);
+      buildFunctions.stderr.on('data', stderr => {
+        console.log(stderr.toString());
+      });
+      buildFunctions.stdout.on('data', stdou => {
+        console.log(stdou.toString());
+      });
+      break;
+    case 'pree2e':
+      console.log(`excuting in '${configENV}' environment`);
+      const pree2e: ChildProcess = spawn('ng', [
+        'build',
+        'backend-ops',
+        `--configuration=${configENV}`,
+      ]);
+      pree2e.stderr.on('data', stderr => {
+        console.log(stderr.toString());
+      });
+      pree2e.stdout.on('data', stdou => {
+        console.log(stdou.toString());
+      });
+      execSync('node dist/apps/backend-ops/main.js');
+      break;
   }
-  // cross-env NODE_ENV=production ???
-  // "build:main": "npx ng build main --base-href / --configuration=\"${ENV:-dev}\"",
-  // "build:delivery": "npx ng build delivery --base-href /delivery/ --configuration=\"${ENV:-dev}\"",
-  // "build:movie-financing": "npx ng build movie-financing --base-href /movie-financing/ --configuration=\"${ENV:-dev}\"",
-  // "build:catalog-marketplace": "npx ng build catalog-marketplace --base-href /catalog-marketplace/ --configuration=\"${ENV:-dev}\"",
-  // "build:catalog-dashboard": "npx ng build catalog-dashboard --base-href /catalog-dashboard/ --configuration=\"${ENV:-dev}\"",
-  // "build:functions": "npx ng build backend-functions --configuration=\"${ENV:-dev}\"",
   // "pree2e": "ng build backend-ops --configuration=\"${ENV}\" && node dist/apps/backend-ops/main.js",
   // "e2e:main": "./node_modules/.bin/ng e2e main-e2e --configuration=\"${ENV}\" --headless",
   // "e2e:delivery": "./node_modules/.bin/ng e2e delivery-e2e --configuration=\"${ENV}\" --headless",
@@ -142,7 +290,11 @@ const firebasePredeploy = () => {};
 
 const program = () => {
   if (commander.script) {
-    packageEnv(commander.script);
+    if (commander.config === 'production' || commander.config === 'config') {
+      packageEnv(commander.script, commander.config);
+    } else {
+      packageEnv(commander.script);
+    }
   } else if (commander.deploy === 'demo') {
     deployDemos();
   } else if (commander.deploy === 'secrets') {
