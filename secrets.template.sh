02a81b397ad397f950ba29091c48741694afe22a
Max Fritz <39312833+Maxstgt@users.noreply.github.com>
2019-10-24 14:16:19 +0200

    Update secrets.template.sh

--- secrets.template.sh
+++ secrets.template.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export ETHEREUM_MNEMONIC="anchor eager purpose screen pen spider fix strategy pizza twelve zone toy";
-export SENDGRID_API_KEY="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"
+export SENDGRID_API_KEY=""
 export ALGOLIA_API_KEY=""
 export CASCADE8_ADMIN="test+admin@blockframes.io"
