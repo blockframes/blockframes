28fa7ccb418d8ab52a19163590009464b2f438e6
DK <jenna_kim1@hotmail.com>
2019-10-30 14:49:10 +0100

    added cropper in auth/identity

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule, Routes } from '@angular/router';
+import { CropperModule } from '@blockframes/ui/cropper/cropper.module';
 
 // Angular Fire
 import { AngularFireAuthModule } from '@angular/fire/auth';
@@ -44,6 +45,7 @@ export const AuthRoutes: Routes = [
     ReactiveFormsModule,
     AngularFireAuthModule,
     FlexLayoutModule,
+    CropperModule,
     // Material
     MatFormFieldModule,
     MatInputModule,
