9f6952bc74ec2d0ad5f8289132bf3a1ed9713ca7
Mussuranae <mandonnetdev@outlook.com>
2019-10-30 16:51:28 +0100

    details

--- apps/catalog/marketplace/marketplace/src/app/pages/team-page/team-page.module.ts
+++ apps/catalog/marketplace/marketplace/src/app/pages/team-page/team-page.module.ts
@@ -1,13 +1,13 @@
 // Angular
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 // Pages
 import { TeamPageComponent } from './team-page.component';
 
 // Material
 import { MatCardModule } from '@angular/material/card';
-import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
