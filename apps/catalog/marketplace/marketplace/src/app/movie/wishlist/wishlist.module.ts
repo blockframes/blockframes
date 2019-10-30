4423e880e91136cd12f41baf3ed77b22ce1111c1
hugo-hontans <hugohontans@gmail.com>
2019-10-30 14:39:52 +0100

    add poster to wishlist-repertory

--- apps/catalog/marketplace/marketplace/src/app/movie/wishlist/wishlist.module.ts
+++ apps/catalog/marketplace/marketplace/src/app/movie/wishlist/wishlist.module.ts
@@ -3,35 +3,27 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { WishlistViewComponent } from './wishlist-view/wishlist-view.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { MatTableModule, MatSortModule, MatIconModule } from '@angular/material';
 
 // Material
 import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
 
-// Custom modules
+// Components
 import { WishlistCurrentRepertoryComponent } from './wishlist-current-repertory/wishlist-current-repertory.component';
-import { MatTableModule, MatSortModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [WishlistViewComponent, WishlistCurrentRepertoryComponent],
   imports: [
     CommonModule,
     FlexLayoutModule,
-    ReactiveFormsModule,
     // Material
     MatButtonModule,
     MatDividerModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatSelectModule,
     MatTableModule,
     MatSortModule,
+    MatIconModule,
 
     RouterModule.forChild([
       {
