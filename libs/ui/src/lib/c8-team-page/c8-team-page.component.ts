6a7e9d05629709801c4c6b3f76aac199993e945a
Mussuranae <mandonnetdev@outlook.com>
2019-10-30 14:18:18 +0100

    team page, change image for congrats org page

---
+++ libs/ui/src/lib/c8-team-page/c8-team-page.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+  selector: 'c8-team-page',
+  templateUrl: './c8-team-page.component.html',
+  styleUrls: ['./c8-team-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class C8TeamPageComponent {}
