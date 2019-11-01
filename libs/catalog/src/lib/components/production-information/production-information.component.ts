3aa9fecbb8e4c33adf9d2c98ce98e81eaecfdb21
Maxstgt <max.j.f@gmx.de>
2019-11-01 16:56:30 +0100

    almoste done

--- libs/catalog/src/lib/components/production-information/production-information.component.ts
+++ libs/catalog/src/lib/components/production-information/production-information.component.ts
@@ -1,17 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'catalog-production-information',
-  template: `
-    <h3>Production information</h3>
-    <div fxLayout="row nowrap" fxLayoutAlign="start center" fxLayoutGap="30px">
-      <span><p class="bf-dark-text">Production company</p>
-        <p>{{ companies.name }}</p>
-      </span>
-      <span><p class="bf-dark-text">Sales agent</p>
-        <img [src]="companies.salesAgent"/></span>
-    </div>
-  `
+  templateUrl: './production-information.component.html'
 })
 export class CatalogProductionInformationComponent {
-  public companies = { name: 'The Jokers', salesAgent: '/assets/icon/cross.svg' };
+  @Input() productionCompanies;
+  @Input() salesAgentDealLogo
 }
