a5c134d3efc233dc3e94037843fc272f976fad9c
Maxstgt <max.j.f@gmx.de>
2019-11-01 17:42:47 +0100

    fixed typo

--- libs/catalog/src/lib/components/production-information/production-information.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-@Component({
-  selector: 'catalog-production-information',
-  templateUrl: './production-information.component.html'
-})
-export class CatalogProductionInformationComponent {
-  @Input() productionCompanies;
-  @Input() salesAgentDealLogo
-}
