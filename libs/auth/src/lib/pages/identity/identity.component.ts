b74db07c96981d0c3e1d2ac4721438651f1766bc
Maxstgt <max.j.f@gmx.de>
2019-11-04 12:01:50 +0100

    added night rocket

--- libs/auth/src/lib/pages/identity/identity.component.ts
+++ libs/auth/src/lib/pages/identity/identity.component.ts
@@ -12,13 +12,15 @@ import { PasswordControl } from '@blockframes/utils';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class IdentityComponent {
+  public userEmail = this.query.getValue().user.email;
+
   public form = new FormGroup({
     name: new FormControl(''),
     surname: new FormControl(''),
     avatar: new FormControl(''),
-    email: new FormControl({ value: this.query.getValue().user.email, disabled: true }),
+    email: new FormControl({ value: this.userEmail, disabled: true }),
     generatedPassword: new FormControl(''),
-    newPassword: new PasswordControl(),
+    newPassword: new PasswordControl()
   });
 
   constructor(
@@ -35,7 +37,15 @@ export class IdentityComponent {
     }
     try {
       const uid = this.query.userId;
-      await this.service.update(uid, this.form.value);
+      await this.service.update(uid, {
+        name: this.form.get('name').value,
+        surname: this.form.get('surname').value,
+        avatar: this.form.get('avatar').value
+      });
+      await this.service.updatePassword(
+        this.form.get('generatedPassword').value,
+        this.form.get('newPassword').value
+      );
       this.router.navigate(['auth/congratulation']);
     } catch (error) {
       this.snackBar.open(error.message, 'close', { duration: 5000 });
