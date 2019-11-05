8f6b712a7c33e6ec73daf2d530b442b09e883bca
bruce <eelios@gmail.com>
2019-11-05 17:44:08 +0100

    remove wishlist menu & debug org member abatars

--- libs/ui/src/lib/avatar-list/avatar-list.module.ts
+++ libs/ui/src/lib/avatar-list/avatar-list.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from "@angular/common";
 import { AvatarListComponent } from "./avatar-list.component";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatIconModule } from "@angular/material";
+import { CropperModule } from "../cropper/cropper.module";
 
 @NgModule({
   declarations: [AvatarListComponent],
-  imports: [CommonModule, FlexLayoutModule, MatIconModule],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    MatIconModule,
+    CropperModule,
+  ],
   exports: [AvatarListComponent],
 })
 export class AvatarListModule { }
