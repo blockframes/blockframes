aacf05f9aae534f0e10434580c00c250ee831ac5
hugo-hontans <hugohontans@gmail.com>
2019-11-05 15:49:59 +0100

    change banners, add button for promotionalElements, fixs css

--- libs/catalog/src/lib/components/promotional-elements/promotional-elements.module.ts
+++ libs/catalog/src/lib/components/promotional-elements/promotional-elements.module.ts
@@ -2,9 +2,12 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { CatalogPromotionalElementsComponent } from './promotional-elements.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button'
+
 @NgModule({
   declarations: [CatalogPromotionalElementsComponent],
-  imports: [CommonModule, FlexLayoutModule],
+  imports: [CommonModule, FlexLayoutModule, MatIconModule, MatButtonModule],
   exports: [CatalogPromotionalElementsComponent]
 })
 export class CatalogPromotionalElementsModule {}
