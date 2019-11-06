3e5a71337f7ad9fe5d445145b99b138b3215008a
hugo-hontans <hugohontans@gmail.com>
2019-11-06 14:48:44 +0100

    correct wishlistsWithMovies$ query to filter organization

--- apps/catalog/marketplace/marketplace/src/app/distribution-right/+state/basket.query.ts
+++ apps/catalog/marketplace/marketplace/src/app/distribution-right/+state/basket.query.ts
@@ -5,7 +5,7 @@ import { CatalogBasket } from './basket.model';
 import { MovieQuery } from '@blockframes/movie';
 import { OrganizationQuery, Wishlist } from '@blockframes/organization';
 import { Observable, combineLatest } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, filter } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class BasketQuery extends QueryEntity<BasketState, CatalogBasket> {
@@ -15,11 +15,12 @@ export class BasketQuery extends QueryEntity<BasketState, CatalogBasket> {
 
   /** Return an observable of a WishList array containing the movies */
   public wishlistsWithMovies$: Observable<Wishlist[]> = combineLatest([
-    this.organizationQuery.select(state => state.org.wishlist),
+    this.organizationQuery.select(state => state.org),
     this.movieQuery.selectAll()
   ]).pipe(
-    map(([wishlists, movies]) => {
-      return wishlists.map(wishlist => {
+    filter(([org, movies]) => !!org),
+    map(([org, movies]) => {
+      return org.wishlist.map(wishlist => {
         const wishlistMovies = wishlist.movieIds.map(id => movies.find(movie => movie.id === id));
         return { ...wishlist, movies: wishlistMovies };
       })
