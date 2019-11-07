cb9b600034c8f6d121a13cf388826e46800a7d80
Mussuranae <mandonnetdev@outlook.com>
2019-11-07 10:37:25 +0100

    intercom module doesn't work

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -8,6 +8,7 @@ export * from './lib/form/controls/password.control';
 export * from './lib/form/validators/validators';
 export * from './lib/form/matchers';
 export * from './lib/pipes/translate-slug.module';
+export * from './lib/intercom.module';
 
 export { OrganizationAlgoliaResult } from './lib/algolia';
 export * from './lib/apps';
