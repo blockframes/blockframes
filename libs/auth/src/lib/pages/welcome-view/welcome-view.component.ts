c5e22f495a664e898c909ed48ca857875a82b06c
Maxstgt <max.j.f@gmx.de>
2019-11-18 15:41:42 +0100

    added route tracking

--- libs/auth/src/lib/pages/welcome-view/welcome-view.component.ts
+++ libs/auth/src/lib/pages/welcome-view/welcome-view.component.ts
@@ -1,13 +1,33 @@
-import { Component, ChangeDetectionStrategy, HostBinding } from '@angular/core';
+import { filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { Router, NavigationEnd } from '@angular/router';
+import { Component, ChangeDetectionStrategy, HostBinding, OnDestroy } from '@angular/core';
+
+declare const gtag;
 
 @Component({
   selector: 'auth-welcome-view',
   templateUrl: './welcome-view.component.html',
   styleUrls: ['./welcome-view.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
-
 })
+export class WelcomeViewComponent implements OnDestroy {
 
-export class WelcomeViewComponent {
   @HostBinding('attr.page-id') pageId = 'welcome-view';
+
+  private subscription: Subscription;
+
+  constructor(private router: Router) {
+    const navEnds = this.router.events.pipe(filter(event => event instanceof NavigationEnd));
+    this.subscription = navEnds.subscribe((event: NavigationEnd) => {
+      console.log(event);
+      gtag('config', 'G-Q4BWTRSV6P', {
+        page_path: event.urlAfterRedirects
+      })
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
