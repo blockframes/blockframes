372a4694208da770da93cef66451202c2ece713d
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-11-20 14:21:24 +0100

    update routes

--- apps/catalog/dashboard/dashboard/src/app/app-routing-module.ts
+++ apps/catalog/dashboard/dashboard/src/app/app-routing-module.ts
@@ -1,91 +1,35 @@
 // Angular
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, NoPreloading } from '@angular/router';
 
 // Components
 import { LayoutComponent } from './layout/layout.component';
+import { createRoutes } from '@blockframes/utils/routes';
+import { App } from '@blockframes/utils';
 
-// Guards
-import { AuthGuard } from '@blockframes/auth';
-import { PermissionsGuard, OrganizationGuard } from '@blockframes/organization';
-import { MovieCreateComponent } from '@blockframes/movie/movie/pages/movie-create/movie-create.component';
+const routes: Routes = createRoutes({
+  appName: App.catalogDashboard,
+  layout: LayoutComponent,
+  appsRoutes: [
+  {
+    path: '',
+    redirectTo: App.catalogDashboard,
+    pathMatch: 'full'
+  },
+  {
+    path: App.catalogDashboard,
+    loadChildren: () => import('./dashboard.module').then(m => m.CatalogDashboardAppModule)
+  }]
+});
 
-export const routes: Routes = [
-  { path: '', redirectTo: 'layout', pathMatch: 'full' },
-  {
-    path: 'auth',
-    loadChildren: () => import('@blockframes/auth').then(m => m.AuthModule)
-  },
-  {
-    path: 'layout',
-    component: LayoutComponent,
-    canActivate: [AuthGuard],
-    canDeactivate: [AuthGuard],
-    children: [
-      {
-        path: '',
-        redirectTo: 'o',
-        pathMatch: 'full'
-      },
-      {
-        // The redirection route when user has no organization
-        path: 'organization',
-        loadChildren: () => import('@blockframes/organization').then(m => m.NoOrganizationModule)
-      },
-      {
-        path: 'o',
-        canActivate: [PermissionsGuard, OrganizationGuard],
-        canDeactivate: [PermissionsGuard, OrganizationGuard],
-        children: [
-          {
-            path: '',
-            redirectTo: 'home',
-            pathMatch: 'full'
-          },
-          {
-            path: 'no-movies',
-            component: MovieCreateComponent
-          },
-          {
-            path: 'organization',
-            loadChildren: () => import('@blockframes/organization').then(m => m.OrganizationModule)
-          },
-          {
-            path: 'account',
-            loadChildren: () => import('@blockframes/account').then(m => m.AccountModule)
-          },
-          {
-            path: 'home',
-            loadChildren: () => import('@blockframes/movie').then(m => m.MovieModule)
-          },
-          {
-            path: 'import',
-            loadChildren: () => import('@blockframes/movie/import').then(m => m.ImportMovieModule)
-          },
-          {
-            path: 'catalog',
-            loadChildren: () => import('./pages/dashboard-home/dashboard-home.module').then(m => m.DashboardHomeModule)
-          },
-        ]
-      },
-    ]
-  },
-  {
-    path: 'not-found',
-    loadChildren: () => import('@blockframes/ui').then(m => m.ErrorNotFoundModule)
-  },
-  {
-    path: '**',
-    redirectTo: 'not-found'
-  }
-];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
       anchorScrolling: 'enabled',
       onSameUrlNavigation: 'reload',
-      paramsInheritanceStrategy: 'always'
+      paramsInheritanceStrategy: 'always',
+      preloadingStrategy: NoPreloading
     })
   ],
   exports: [RouterModule]
