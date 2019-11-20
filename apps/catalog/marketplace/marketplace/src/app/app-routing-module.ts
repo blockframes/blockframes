8d828dd6032ceb8fb39cd3e0abedbb07d513561f
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-11-20 10:43:43 +0100

    update routes for catalog-marketplace

--- apps/catalog/marketplace/marketplace/src/app/app-routing-module.ts
+++ apps/catalog/marketplace/marketplace/src/app/app-routing-module.ts
@@ -1,163 +1,35 @@
-// Angular
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, NoPreloading } from '@angular/router';
 
-// Components
 import { LayoutComponent } from './layout/layout.component';
+import { createRoutes } from '@blockframes/utils/routes';
+import { App } from '@blockframes/utils';
 
-// Guards
-import { AuthGuard } from '@blockframes/auth';
-import { PermissionsGuard, OrganizationGuard } from '@blockframes/organization';
-import { MovieEmptyComponent } from '@blockframes/movie/movie/components/movie-empty/movie-empty.component';
-import { CatalogBasketGuard } from './guards/catalog-basket-list.guard';
-import { MovieCollectionGuard, MovieActiveGuard } from '@blockframes/movie';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'layout', pathMatch: 'full' },
-  {
-    path: 'auth',
-    loadChildren: () => import('@blockframes/auth').then(m => m.AuthModule)
-  },
-  {
-    path: 'layout',
-    component: LayoutComponent,
-    canActivate: [AuthGuard],
-    canDeactivate: [AuthGuard],
-    children: [
-      {
-        path: '',
-        redirectTo: 'o',
-        pathMatch: 'full'
-      },
-      {
-        // The redirection route when user has no organization
-        path: 'organization',
-        loadChildren: () => import('@blockframes/organization').then(m => m.NoOrganizationModule)
-      },
-      {
-        path: 'o',
-        canActivate: [PermissionsGuard, OrganizationGuard],
-        canDeactivate: [PermissionsGuard, OrganizationGuard],
-        children: [
-          {
-            path: '',
-            redirectTo: 'catalog',
-            pathMatch: 'full'
-          },
-          {
-            path: 'no-movies',
-            component: MovieEmptyComponent
-          },
-          {
-            path: 'organization',
-            loadChildren: () => import('@blockframes/organization').then(m => m.OrganizationModule)
-          },
-          {
-            path: 'account',
-            loadChildren: () => import('@blockframes/account').then(m => m.AccountModule)
-          },
-          {
-            path: 'home',
-            loadChildren: () => import('@blockframes/movie').then(m => m.MovieModule)
-          },
-          // APP SPECIFIC
-          {
-            path: 'catalog',
-            children: [
-              { path: '', redirectTo: 'home', pathMatch: 'full' },
-              {
-                path: 'about',
-                loadChildren: () =>
-                import('./pages/about-page/about.module').then(m => m.AboutModule)
-              },
-              {
-                path: 'who-are-we',
-                loadChildren: () =>
-                import('./pages/team-page/team.module').then(m => m.TeamModule)
-              },
-              {
-                path: 'contact',
-                loadChildren: () =>
-                import('./pages/contact-page/contact.module').then(m => m.ContactModule)
-              },
-              {
-                path: 'terms',
-                loadChildren: () =>
-                import('./pages/privacy-page/privacy.module').then(m => m.PrivacyModule)
-              },
-              {
-                path: 'home',
-                canActivate: [MovieCollectionGuard],
-                canDeactivate: [MovieCollectionGuard],
-                loadChildren: () =>
-                  import('./movie/home/home.module').then(m => m.MarketplaceHomeModule)
-              },
-              {
-                path: 'search',
-                canActivate: [MovieCollectionGuard],
-                canDeactivate: [MovieCollectionGuard],
-                loadChildren: () =>
-                  import('./movie/search/search.module').then(m => m.MarketplaceSearchModule)
-              },
-              {
-                path: 'wishlist',
-                canActivate: [CatalogBasketGuard],
-                canDeactivate: [CatalogBasketGuard],
-                children: [
-                  {
-                    path: '',
-                    redirectTo: 'view',
-                    pathMatch: 'full'
-                  },
-                  {
-                    path: 'view',
-                    loadChildren: () =>
-                      import('./movie/wishlist/wishlist.module').then(m => m.WishlistModule)
-                  }
-                ]
-              },
-              {
-                path: ':movieId',
-                canActivate: [MovieActiveGuard],
-                canDeactivate: [MovieActiveGuard],
-                children: [
-                  { path: '', redirectTo: 'view', pathMatch: 'full' },
-                  {
-                    path: 'view',
-                    loadChildren: () =>
-                      import('./movie/view/view.module').then(m => m.MovieViewModule)
-                  },
-                  {
-                    path: 'create',
-                    loadChildren: () =>
-                      import('./distribution-right/create/create.module').then(
-                        m => m.DistributionRightCreateModule
-                      )
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  },
-  {
-    path: 'not-found',
-    loadChildren: () => import('@blockframes/ui').then(m => m.ErrorNotFoundModule)
-  },
-  {
-    path: '**',
-    redirectTo: 'not-found'
-  }
-];
+/** Scaffold a marketplace application routing for this application */
+const routes = createRoutes({
+  appName: App.biggerBoat,
+  layout: LayoutComponent,
+  appsRoutes: [
+    {
+      path: '',
+      redirectTo: App.biggerBoat,
+      pathMatch: 'full'
+    },
+    {
+      path: App.biggerBoat,
+      loadChildren: () =>
+        import('./catalog-marketplace.module').then(m => m.CatalogMarketplaceAppModule)
+    }
+  ]
+});
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
       anchorScrolling: 'enabled',
       onSameUrlNavigation: 'reload',
-      paramsInheritanceStrategy: 'always'
+      paramsInheritanceStrategy: 'always',
+      preloadingStrategy: NoPreloading
     })
   ],
   exports: [RouterModule]
