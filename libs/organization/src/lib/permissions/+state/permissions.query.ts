78ab961d1df6cb4dfc5beb59857560ec59026fc5
ci-build <ci-build@blockframes.io>
2019-11-20 12:08:58 +0100

    Apply new role check logic

--- libs/organization/src/lib/permissions/+state/permissions.query.ts
+++ libs/organization/src/lib/permissions/+state/permissions.query.ts
@@ -4,6 +4,7 @@ import { PermissionsStore, PermissionsState } from './permissions.store';
 import { AuthQuery } from '@blockframes/auth';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { UserRole } from './permissions.firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -11,18 +12,18 @@ import { map } from 'rxjs/operators';
 export class PermissionsQuery extends Query<PermissionsState> {
 
     /** Checks if the connected user is superAdmin of his organization. */
-    public isSuperAdmin$: Observable<boolean> = this.select(state =>
-      state.superAdmins.includes(this.auth.userId)
+    public isSuperAdmin$: Observable<boolean> = this.select(permissions =>
+      permissions.roles[this.auth.userId] === UserRole.superAdmin
     );
 
     /** Checks if the connected user is admin of his organization. */
     public isAdmin$: Observable<boolean> = this.isSuperAdmin$.pipe(
-      map(isSuperAdmin => isSuperAdmin || this.getValue().admins.includes(this.auth.userId))
+      map(isSuperAdmin => isSuperAdmin || this.getValue().roles[this.auth.userId] === UserRole.admin)
     );
 
     /** Checks if the connected user is either member of his organization. */
     public isOrgMember$: Observable<boolean> = this.isAdmin$.pipe(
-      map(isAdmin => isAdmin || this.getValue().members.includes(this.auth.userId))
+      map(isAdmin => isAdmin || this.getValue().roles[this.auth.userId] === UserRole.member)
     );
 
   constructor(protected store: PermissionsStore, private auth: AuthQuery) {
@@ -31,11 +32,11 @@ export class PermissionsQuery extends Query<PermissionsState> {
 
   /** Returns the number of organization admins. */
   public get superAdminCount(): number {
-    return this.getValue().superAdmins.length;
+    return Object.values(this.getValue().roles).filter(value => value === UserRole.superAdmin).length;
   }
 
   /** Checks if the user is SuperAdmin of his organization. */
   public isUserAdmin(userId: string): boolean {
-    return this.getValue().admins.includes(userId);
+    return this.getValue().roles[userId] === UserRole.admin;
   }
 }
