f5daa015c593914e6a2f62873fc44891ac8e4232
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-11-20 10:34:12 +0100

    Add routes folder

---
+++ libs/utils/src/lib/routes/create-routes.ts
@@ -0,0 +1,78 @@
+import { Routes } from '@angular/router';
+import { AuthGuard } from '@blockframes/auth';
+import { PermissionsGuard, OrganizationGuard } from '@blockframes/organization';
+import { NotificationsGuard } from '@blockframes/notification';
+
+interface RouteOptions {
+  /** The Layout Component to render */
+  layout: any,
+  /** The routes of the apps */
+  appsRoutes: Routes,
+  /** Name of the app to put in data of the route */
+  appName: string,
+  /** The page to load by default */
+  rootPath?: string;
+}
+
+function root(LayoutComponent, children: Routes, rootPath: string, appName: string) {
+  return [
+    { path: '', redirectTo: rootPath, pathMatch: 'full' },
+    {
+      path: 'auth',
+      loadChildren: () => import('@blockframes/auth/auth.module').then(m => m.AuthModule)
+    },
+    {
+      path: 'layout',
+      component: LayoutComponent,
+      canActivate: [AuthGuard],
+      canDeactivate: [AuthGuard],
+      data: { app: appName },
+      children: [
+        {
+          path: '',
+          redirectTo: 'o',
+          pathMatch: 'full'
+        },
+        {
+          // The redirection route when user has no organization
+          path: 'organization',
+          loadChildren: () => import('@blockframes/organization').then(m => m.NoOrganizationModule)
+        },
+        {
+          path: 'o',
+          canActivate: [NotificationsGuard, PermissionsGuard, OrganizationGuard],
+          canDeactivate: [NotificationsGuard, PermissionsGuard, OrganizationGuard],
+          children
+        }
+      ]
+    },
+    {
+      path: 'not-found',
+      loadChildren: () => import('@blockframes/ui').then(m => m.ErrorNotFoundModule)
+    },
+    {
+      path: '**',
+      loadChildren: () => import('@blockframes/ui').then(m => m.ErrorNotFoundModule)
+    }
+  ]
+}
+
+
+
+export function createRoutes({ layout, appsRoutes, appName, rootPath = 'layout' }: RouteOptions) {
+  const children = [
+    ...appsRoutes,
+    {
+      path: 'organization',
+      loadChildren: () => import('@blockframes/organization').then(m => m.OrganizationModule)
+    },
+    {
+      path: 'account',
+      loadChildren: () => import('@blockframes/account').then(m => m.AccountModule)
+    },
+  ];
+  return root(layout, children, rootPath, appName);
+}
+
+
+export const appsRoute = '/layout/o';
