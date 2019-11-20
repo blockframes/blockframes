6d7c52dd4b7699b4fc433a791176acf85904c701
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-11-20 13:33:48 +0100

    small fixes

--- libs/auth/src/lib/components/accept-conditions/accept-conditions.module.ts
+++ libs/auth/src/lib/components/accept-conditions/accept-conditions.module.ts
@@ -9,10 +9,13 @@ import { AcceptConditionsDirective } from './accept-conditions.directive';
 
 // Material
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+
+const material = [ MatCheckboxModule, MatDialogModule ];
 
 @NgModule({
   declarations: [AcceptConditionsComponent, AcceptConditionsDirective],
-  imports: [RouterModule, CommonModule, FlexLayoutModule, MatCheckboxModule],
+  imports: [RouterModule, CommonModule, FlexLayoutModule, ...material],
   exports: [AcceptConditionsComponent, AcceptConditionsDirective]
 })
 export class AcceptConditionsModule {}
