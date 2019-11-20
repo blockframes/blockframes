315ac1cb6730d5dc709d856be608216e4eba9e2d
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-11-20 13:31:23 +0100

    fix selector issue for linter

--- libs/auth/src/lib/components/accept-conditions/accept-conditions.directive.ts
+++ libs/auth/src/lib/components/accept-conditions/accept-conditions.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, TemplateRef } from "@angular/core";
 
 @Directive({
-  selector: '[conditions]'
+  selector: '[authConditions]'
 })
 export class AcceptConditionsDirective {
   constructor(public tpl: TemplateRef<any>) {}
