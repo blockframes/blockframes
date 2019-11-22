3ef65edd968a7418bb622a6f23693227f5bef937
Laurent Senta <Laurent.Senta@gmail.com>
2019-11-22 14:56:05 +0100

    e2e: rename useless function name

--- apps/catalog/dashboard/dashboard-e2e/src/integration/app.spec.ts
+++ apps/catalog/dashboard/dashboard-e2e/src/integration/app.spec.ts
@@ -1,9 +1,9 @@
-import { getGreeting } from '../support/app.po';
+import { getH1 } from '../support/app.po';
 
 describe('catalog-dashboard', () => {
   beforeEach(() => cy.visit('/'));
 
   it.skip('should display welcome message', () => {
-    getGreeting().contains('Welcome to catalog-dashboard!');
+    getH1().contains('Welcome to catalog-dashboard!');
   });
 });
