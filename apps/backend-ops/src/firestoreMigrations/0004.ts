d617c3f4ce69bf87ecb69190149f316c9f50294d
ci-build <ci-build@blockframes.io>
2019-11-29 11:35:54 +0100

    Update permissions migration script

--- apps/backend-ops/src/firestoreMigrations/0004.ts
+++ apps/backend-ops/src/firestoreMigrations/0004.ts
@@ -1,23 +1,28 @@
-import * as admin from 'firebase-admin';
+import { loadAdminServices } from "../admin";
 
 /**
  * Update organisation permissions data model.
  */
 export async function updateOrganizationPermissionsModel() {
-  const permissions = await admin.firestore().collection('permissions').get();
+  const permissions = await loadAdminServices().db.collection('permissions').get();
   const newPermissionsData = permissions.docs.map(async (permissionsSnap: any): Promise<any> => {
     const permissionsData = permissionsSnap.data();
+    const updatedPermissionsData = {
+      ...permissionsData,
+      id: permissionsData.orgId,
+      roles: {}
+    }
 
-    permissionsData.superAdmins.forEach((uid:string) => permissionsData.roles[uid] = 'superAdmin');
-    permissionsData.admins.forEach((uid:string) => permissionsData.roles[uid] = 'admin');
-    permissionsData.members.forEach((uid:string) => permissionsData.roles[uid] = 'member');
+    permissionsData.superAdmins.forEach((uid:string) => updatedPermissionsData.roles[uid] = 'superAdmin');
+    permissionsData.admins.forEach((uid:string) => updatedPermissionsData.roles[uid] = 'superAdmin');
 
-    delete permissionsData.superAdmins;
-    delete permissionsData.admins;
-    delete permissionsData.members;
+    delete updatedPermissionsData.superAdmins;
+    delete updatedPermissionsData.admins;
+    delete updatedPermissionsData.orgId;
 
-    return permissionsSnap.ref.set(permissionsData);
+    return permissionsSnap.ref.set(updatedPermissionsData);
   });
+
   await Promise.all(newPermissionsData);
   console.log('Organization permissions model updated.');
 }
