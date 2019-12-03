c48f55f9e2bf649c62fcbeb4b4b3514f50fdb964
Maxstgt <max.j.f@gmx.de>
2019-12-03 10:21:01 +0100

    added basket service unit tests

--- apps/catalog/marketplace/marketplace/src/app/distribution-right/+state/basket.service.spec.ts
+++ apps/catalog/marketplace/marketplace/src/app/distribution-right/+state/basket.service.spec.ts
@@ -1,27 +1,26 @@
+import { OrganizationService } from '@blockframes/organization/+state/organization.service';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { firebase } from '@env';
-import { AngularFireFunctions, AngularFireFunctionsModule } from '@angular/fire/functions';
-import { TestBed, inject } from '@angular/core/testing';
+import { AngularFireFunctionsModule } from '@angular/fire/functions';
+import { TestBed } from '@angular/core/testing';
 import { BasketService } from './basket.service';
-import { AngularFireModule, FirebaseApp } from '@angular/fire';
+import { AngularFireModule } from '@angular/fire';
+import { createServiceFactory, SpectatorService } from '@ngneat/spectator/jest';
 
 describe('BasketService', () => {
-  beforeEach(() => {
-    let app: FirebaseApp;
-    let afFns: AngularFireFunctions;
+  let spectator: SpectatorService<BasketService>;
+  const createService = createServiceFactory({
+    service: BasketService,
+    imports: [
+      AngularFireModule.initializeApp(firebase),
+      AngularFireFunctionsModule,
+      AngularFirestoreModule
+    ],
+    mocks: [OrganizationService]
+  });
 
-    TestBed.configureTestingModule({
-      imports: [
-        AngularFireModule.initializeApp(firebase),
-        AngularFireFunctionsModule,
-        AngularFirestoreModule
-      ],
-      providers: [BasketService, AngularFireFunctions]
-    });
-    inject([FirebaseApp, AngularFireFunctions], (app_: FirebaseApp, _fn: AngularFireFunctions) => {
-      app = app_;
-      afFns = _fn;
-    })();
+  beforeEach(() => {
+    spectator = createService();
   });
 
   it('should be created', () => {
@@ -29,9 +28,48 @@ describe('BasketService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should update the wishlist', () => {
+  it('should not call OrganizationService if no Organization ID is available', () => {
+    const serviceBasket = spectator.get(BasketService);
+    const serviceOrg = spyOn(spectator.get<OrganizationService>(OrganizationService), 'update');
+    const currentTime: Date = new Date();
+    serviceBasket.updateWishlist({
+      id: '1',
+      main: { title: { original: 'test' } },
+      _type: 'movies',
+      deliveryIds: ['12'],
+      story: { synopsis: 'test movie', logline: 'test movie long' },
+      promotionalElements: {
+        images: [{ originalRef: '123', ref: '1234', url: 'http://test.com' }],
+        promotionalElements: [{ label: 'test', type: 'movie', url: 'http://test.com' }]
+      },
+      budget: { totalBudget: '1 mio' },
+      promotionalDescription: { keyAssets: ['testassets'], keywords: ['testassertion'] },
+      salesAgentDeal: {
+        medias: ['freetv'],
+        rights: { from: currentTime, to: currentTime },
+        territories: ['world']
+      },
+      salesCast: { credits: [{ firstName: 'testname' }] },
+      festivalPrizes: { prizes: [{ name: 'cannes', year: 2019 }] },
+      versionInfo: { dubbings: ['french'], subtitles: ['german'] },
+      salesInfo: {
+        theatricalRelease: false,
+        broadcasterCoproducers: ['test'],
+        certifications: ['none'],
+        color: 'yes',
+        europeanQualification: false,
+        internationalPremiere: { name: 'cannes', year: 2019 },
+        pegi: '18',
+        originCountryReleaseDate: currentTime,
+        scoring: '5'
+      }
+    });
+    expect(serviceOrg).not.toHaveBeenCalled();
+  });
+
+  it('should update the wishlist and to have been called only 1 time', () => {
     const service: BasketService = TestBed.get(BasketService);
-    const wishlistSpy = jest.spyOn(service, 'updateWishlist').mockRejectedValue(new Error());
+    const wishlistSpy = jest.spyOn(service, 'updateWishlist').mockImplementation();
     /**
      * We want to compare the date values, but since
      * the time is passing during the test, we need to have a fixed time
@@ -102,5 +140,18 @@ describe('BasketService', () => {
         scoring: '5'
       }
     });
+    expect(wishlistSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should have only one function for one task', () => {
+    const service: BasketService = TestBed.get(BasketService);
+    expect(service.updateWishlist).toBeTruthy();
+    expect(service.addBasket).toBeTruthy();
+    expect(service.updateWishlistStatus).toBeTruthy();
+    expect(service.updateWishlist).toBeTruthy();
+    expect(service.isAddedToWishlist).toBeTruthy();
+    expect(service.removeDistributionRight).toBeTruthy();
+    expect(service.removeMovieFromWishlist).toBeTruthy();
+    expect(service.rewriteBasket).toBeTruthy();
   });
 });
--- jestGlobalMocks.ts
+++ jestGlobalMocks.ts
@@ -1,4 +1,4 @@
-// ng s is complaining about global, so doing this "hackishly"
+// ng is complaining about global, so doing this "hackishly"
 declare var global;
 global['CSS'] = null;
 
@@ -36,18 +36,4 @@ Object.defineProperty(document.body.style, 'transform', {
       configurable: true
     };
   }
-});
-
-// Workaround for error: TypeError: window.matchMedia is not a function
-window.matchMedia = jest.fn().mockImplementation(query => {
-  return {
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn()
-  };
 });
