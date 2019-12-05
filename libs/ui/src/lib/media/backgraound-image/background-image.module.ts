b420b058a3be5466b1beb208786d97808a3b830b
DK <jenna_kim1@hotmail.com>
2019-12-05 16:07:28 +0100

    created BackgroundImageDirective

---
+++ libs/ui/src/lib/media/backgraound-image/background-image.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { BackgroundImageDirective } from './background-image.directive';
+
+@NgModule({
+  declarations: [BackgroundImageDirective],
+  exports: [BackgroundImageDirective]
+})
+export class BackgroundImageModule {}
