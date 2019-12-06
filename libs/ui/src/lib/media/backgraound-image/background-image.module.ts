247cede1764649fa5f7118998d0cf783ac2d5749
DK <jenna_kim1@hotmail.com>
2019-12-06 12:16:34 +0100

    Revert "created BackgroundImageDirective"
    This reverts commit 4859e52aaf2794018b4228e3543640f676e7d6b1.

--- libs/ui/src/lib/media/backgraound-image/background-image.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { BackgroundImageDirective } from './background-image.directive';
-
-@NgModule({
-  declarations: [BackgroundImageDirective],
-  exports: [BackgroundImageDirective]
-})
-export class BackgroundImageModule {}
