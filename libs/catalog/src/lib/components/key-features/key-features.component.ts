947249c540e5810e152e3267166f166c9078d6a8
ci-build <ci-build@blockframes.io>
2019-12-06 11:25:36 +0100

    Create key-features component

---
+++ libs/catalog/src/lib/components/key-features/key-features.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'catalog-key-features',
+  templateUrl: './key-features.component.html',
+  styleUrls: ['./key-features.component.scss']
+})
+export class CatalogKeyFeaturesComponent {}
