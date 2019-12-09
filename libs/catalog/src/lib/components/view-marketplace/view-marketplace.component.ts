97380de31a0022cf9d298ba3b9bdbb79a95c9eae
maxstgt <mfritz@cascade8.com>
2019-12-09 15:12:34 +0100

    implemented video component

---
+++ libs/catalog/src/lib/components/view-marketplace/view-marketplace.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'catalog-view-marketplace',
+    templateUrl: './view-marketplace.component.html',
+   styleUrls: ['./view-marketplace.component.scss']
+})
+export class CatalogViewMarketplaceComponent {}
