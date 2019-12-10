7fd02ec4ae1dcb9f5a44433a5678395d35282803
DK <jenna_kim1@hotmail.com>
2019-12-10 17:17:10 +0100

    changed name to widget directive & module

--- libs/ui/src/lib/widget/widget.directive.ts
+++ libs/ui/src/lib/widget/widget.directive.ts
@@ -1,12 +1,14 @@
 import { Directive, Input, TemplateRef, HostListener, ViewContainerRef, ElementRef } from '@angular/core';
-import { Overlay } from '@angular/cdk/overlay';
+import { Overlay, BlockScrollStrategy, ViewportRuler, NoopScrollStrategy } from '@angular/cdk/overlay';
 import { TemplatePortal } from '@angular/cdk/portal';
+import { WidgetComponent } from './widget.component';
 
 @Directive({
   selector: "[widgetTarget]"
 })
+
 export class WidgetDirective {
-  @Input() widgetTarget: TemplateRef<any>;
+  @Input() widgetTarget: WidgetComponent;
   @HostListener('click')
     open() {
       const positionStrategy = this.overlay
@@ -21,15 +23,18 @@ export class WidgetDirective {
       const overlayRef = this.overlay.create({
         hasBackdrop: true,
         backdropClass: 'cdk-overlay-transparent-backdrop',
-        positionStrategy
+        positionStrategy,
+        scrollStrategy: new NoopScrollStrategy(),
+        // maxHeight: '10px'
       });
-      const widget = new TemplatePortal(this.widgetTarget, this.viewContainerRef);
+      const widget = new TemplatePortal(this.widgetTarget.ref, this.viewContainerRef);  // Use the template ref of the widget component instead.
       overlayRef.attach(widget);
       overlayRef.backdropClick().subscribe(() => overlayRef.detach());
     }
 constructor(
   private overlay : Overlay,
   private viewContainerRef: ViewContainerRef,
-  private _elementRef: ElementRef
+  private _elementRef: ElementRef,
+  private _viewportRuler: ViewportRuler
 ){}
 }
