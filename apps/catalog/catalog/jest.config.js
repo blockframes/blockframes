31bc81199e0c4f7128237a63c7dd2c876e3b7952
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-12-11 13:40:19 +0100

    make test work

--- apps/catalog/catalog/jest.config.js
+++ apps/catalog/catalog/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'catalog',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/catalog',
+  preset: '../../../jest.config.js',
+  coverageDirectory: '../../../coverage/apps/catalog',
   snapshotSerializers: [
     'jest-preset-angular/AngularSnapshotSerializer.js',
     'jest-preset-angular/HTMLCommentSerializer.js'
