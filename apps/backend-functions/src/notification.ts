11cb98b41f5c86870d469c71d86b5e5c88cffcc0
Yohann Nikas <ioa.nikas@gmail.com>
2019-12-13 16:09:31 +0100

    Moving createNotification into backend

--- apps/backend-functions/src/notification.ts
+++ apps/backend-functions/src/notification.ts
@@ -1,5 +1,16 @@
-import { NotificationDocument } from '@blockframes/notification/types';
+import { NotificationDocument, NotificationOptions } from './data/types';
 import { db } from './internals/firebase'
+import { firestore } from 'firebase/app';
+
+/** Createa a Notification with required and generic informations. */
+export function createNotification(notification: NotificationOptions): NotificationDocument {
+  return {
+    id: db.collection('notifications').doc().id,
+    isRead: false,
+    date: firestore.Timestamp.now(),
+    ...notification
+  };
+}
 
 /** Takes one or more notifications and add them on the notifications collection */
 export function triggerNotifications(notifications: NotificationDocument[]): Promise<any> {
