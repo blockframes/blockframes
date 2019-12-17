67823d098164699ef9a089df8fe551de96b82fc9
Bruce <eelios@gmail.com>
2019-12-17 14:57:45 +0100

    Happy Linter

--- apps/catalog/marketplace/marketplace/src/app/distribution-deal/+state/cart.firestore.ts
+++ apps/catalog/marketplace/marketplace/src/app/distribution-deal/+state/cart.firestore.ts
@@ -29,9 +29,11 @@ interface ContractRaw<D> {
   paymentSchedule?: string; // @todo #1397 change this when creating invoices
 }
 
+/*
 export interface InvoiceRaw<D> {
-  // @todo #1397
+   @todo #1397
 }
+*/
 
 export interface ContractDocumentWithDates extends ContractRaw<Date> {
 }
