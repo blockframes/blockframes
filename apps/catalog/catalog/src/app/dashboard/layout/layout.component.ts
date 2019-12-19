49af9c25f4feca34f3febd5ab337b41bb688176a
DK <jenna_kim1@hotmail.com>
2019-12-19 17:58:19 +0100

    responsive

--- apps/catalog/catalog/src/app/dashboard/layout/layout.component.ts
+++ apps/catalog/catalog/src/app/dashboard/layout/layout.component.ts
@@ -1,4 +1,7 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 @Component({
   selector: 'catalog-layout',
   templateUrl: './layout.component.html',
@@ -7,5 +10,12 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 })
 
 export class LayoutComponent{
-  opened: boolean;
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
