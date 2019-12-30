38ca69832866aea9a79b91e792ab1389051d4167
Mussuranae <mandonnetdev@outlook.com>
2019-12-30 16:19:24 +0100

    Layout for movie tunnel ok

--- apps/catalog/catalog/src/app/dashboard/movie-tunnel/movie-tunnel.module.ts
+++ apps/catalog/catalog/src/app/dashboard/movie-tunnel/movie-tunnel.module.ts
@@ -2,6 +2,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { Routes } from '@angular/router';
 
 // Component
 import { MovieTunnelComponent } from './movie-tunnel.component';
@@ -12,6 +13,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [MovieTunnelComponent],
@@ -23,6 +25,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
     MatIconModule,
     MatSidenavModule,
     MatExpansionModule,
+    MatProgressSpinnerModule,
   ],
   exports: [MovieTunnelComponent]
 })
