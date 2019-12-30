738f58ad439b6a117210162e05557cace4ee2c8b
DK <jenna_kim1@hotmail.com>
2019-12-30 18:38:53 +0100

    updated after review

--- apps/catalog/catalog/src/app/dashboard/movie-tunnel/movie-tunnel.service.ts
+++ apps/catalog/catalog/src/app/dashboard/movie-tunnel/movie-tunnel.service.ts
@@ -4,9 +4,8 @@ import { RouterQuery } from '@datorama/akita-ng-router-store';
 
 @Injectable({ providedIn: 'root' })
 export class MovieTunnelService {
-  previousPage: any;
-  constructor(private routerQuery: RouterQuery, private router: Router){
-  }
+  previousPage: string;
+  constructor(private routerQuery: RouterQuery, private router: Router){}
 
   openTunnel(){
     this.previousPage = this.routerQuery.getValue().state.url;
