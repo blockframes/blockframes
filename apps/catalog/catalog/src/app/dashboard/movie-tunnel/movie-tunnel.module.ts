4295d87f5111a496af0ecf8f401211f935d7c35b
Mussuranae <mandonnetdev@outlook.com>
2019-12-30 15:16:42 +0100

    layout movie tunnel WIP

--- apps/catalog/catalog/src/app/dashboard/movie-tunnel/movie-tunnel.module.ts
+++ apps/catalog/catalog/src/app/dashboard/movie-tunnel/movie-tunnel.module.ts
@@ -1,19 +1,29 @@
 // Angular
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 // Component
 import { MovieTunnelComponent } from './movie-tunnel.component';
 import { MovieTunnelRoutingModule } from './movie-tunnel-routing.module';
 
+// Materials
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [MovieTunnelComponent],
   imports: [
     CommonModule,
-    HttpClientModule,
-    MovieTunnelRoutingModule
-  ]
+    MovieTunnelRoutingModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatExpansionModule,
+  ],
+  exports: [MovieTunnelComponent]
 })
 export class MovieTunnelModule {}
