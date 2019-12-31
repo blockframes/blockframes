091cf89c26c257a0129e38e270ed6b941e2f2093
Mussuranae <mandonnetdev@outlook.com>
2019-12-31 11:08:02 +0100

    Request changes, add sidenav width to theme

--- apps/catalog/catalog/src/app/dashboard/movie-tunnel/movie-tunnel.module.ts
+++ apps/catalog/catalog/src/app/dashboard/movie-tunnel/movie-tunnel.module.ts
@@ -2,6 +2,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
 
 // Component
 import { MovieTunnelComponent } from './movie-tunnel.component';
@@ -13,18 +14,21 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [MovieTunnelComponent],
   imports: [
     CommonModule,
     MovieTunnelRoutingModule,
+    RouterModule,
     FlexLayoutModule,
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
     MatExpansionModule,
     MatProgressSpinnerModule,
+    MatButtonModule,
   ],
   exports: [MovieTunnelComponent]
 })
