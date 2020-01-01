0db089cbe1dc9ee7505cc4124561670982ddc988
grandschtroumpf <grandschtroumpf@LAPTOP-C2HIB6DK.localdomain>
2020-01-01 11:01:00 +0100

    Guard for movie-tunnel uses movieId

--- apps/catalog/catalog/src/app/dashboard/dashboard.module.ts
+++ apps/catalog/catalog/src/app/dashboard/dashboard.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutModule } from './layout/layout.module';
 import { LayoutComponent } from './layout/layout.component';
+import { MovieCollectionGuard } from '@blockframes/movie';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    canActivate: [MovieCollectionGuard],    // todo(#1476) move to a more precise place (overview or/and deals maybe)
+    canDeactivate: [MovieCollectionGuard],  // todo(#1476) move to a more precise place (overview or/and deals maybe)
     children: [
       {
         path: 'overview'
@@ -38,10 +41,10 @@ const routes: Routes = [
       },
     ]
   },
-    {
-      path: 'movie-tunnel',
-      loadChildren: () => import('./movie-tunnel/movie-tunnel.module').then(m => m.MovieTunnelModule)
-    }
+  {
+    path: 'movie-tunnel/:movieId',
+    loadChildren: () => import('./movie-tunnel/movie-tunnel.module').then(m => m.MovieTunnelModule)
+  }
 ];
 
 @NgModule({
