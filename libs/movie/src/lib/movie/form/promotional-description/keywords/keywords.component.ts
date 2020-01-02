ea53e7ed8b22ce7a027f5d21aedb8735539db7b1
DK <jenna_kim1@hotmail.com>
2020-01-02 19:35:25 +0100

    replaced formArray to formGroup & formArrayName

--- libs/movie/src/lib/movie/form/promotional-description/keywords/keywords.component.ts
+++ libs/movie/src/lib/movie/form/promotional-description/keywords/keywords.component.ts
@@ -1,5 +1,7 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { ControlContainer } from '@angular/forms';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { ControlContainer, FormArray, FormControl } from '@angular/forms';
+import { startWith } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: '[formArray] movie-form-keywords, [formArrayName] movie-form-keywords',
@@ -7,15 +9,23 @@ import { ControlContainer } from '@angular/forms';
   styleUrls: ['./keywords.component.scss'],
   // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class KeywordsComponent {
+export class KeywordsComponent implements OnInit {
+  keyword = new FormControl();
+  values$: Observable<string>; //  = this.keywords.valueChanges.pipe(startWith(this.keywords.value));
 
-  constructor(private controlContainer: ControlContainer) {
-    console.log('controlContianer', this.controlContainer)
+  constructor(private controlContainer: ControlContainer) {}
+
+  ngOnInit() {
+    this.values$ = this.keywords.valueChanges.pipe(startWith(this.keywords.value));
   }
 
-  // get keywords() {
-  //   // console.log('controlContianer', this.controlContainer)
-  //   return this.controlContainer.control
-  // }
+  get keywords() : FormArray {
+    return this.controlContainer.control as FormArray
+  }
 
+  add() {
+    const v = this.keyword.value;
+    this.keywords.push(new FormControl(v))
+    this.keyword.reset();
+  }
 }
