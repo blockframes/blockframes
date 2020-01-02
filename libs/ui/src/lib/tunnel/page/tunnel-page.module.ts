b808cd9fa178facae1afef5db10f2029f8cdce23
DK <jenna_kim1@hotmail.com>
2020-01-02 11:02:50 +0100

    removed tunnel page on start-tunnel

--- libs/ui/src/lib/tunnel/page/tunnel-page.module.ts
+++ libs/ui/src/lib/tunnel/page/tunnel-page.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { TunnelPageComponent } from './tunnel-page.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+const material = [
+  MatButtonModule,
+  MatIconModule
+]
 
 @NgModule({
   declarations: [ TunnelPageComponent ],
   imports: [
     CommonModule,
     FlexLayoutModule,
+    ...material
   ],
   exports: [TunnelPageComponent]
 })
