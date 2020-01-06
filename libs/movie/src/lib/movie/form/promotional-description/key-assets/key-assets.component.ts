c6cbef9b00f9142dbc34faf94e34975e6bc53185
DK <jenna_kim1@hotmail.com>
2020-01-06 11:16:19 +0100

    update on behalf of francois

--- libs/movie/src/lib/movie/form/promotional-description/key-assets/key-assets.component.ts
+++ libs/movie/src/lib/movie/form/promotional-description/key-assets/key-assets.component.ts
@@ -2,6 +2,7 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { ControlContainer } from '@angular/forms';
 
 @Component({
+  //  TODO#1478 Sync Data and UI, remove movie-form-key-assets from selector
   selector: '[formGroup] movie-form-key-assets, [formGroupName] movie-form-key-assets, movie-form-key-assets',
   templateUrl: './key-assets.component.html',
   styleUrls: ['./key-assets.component.scss'],
