37eaa4e78801586b997f891d82b39da86c3b6afb
DK <jenna_kim1@hotmail.com>
2020-01-07 16:54:30 +0100

    created small components in movie/form/main

---
+++ libs/movie/src/lib/form/main/production-companies/production-companies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'movie-form-production-companies',
+  templateUrl: './production-companies.component.html',
+  styleUrls: ['./production-companies.component.scss']
+})
+export class ProductionCompaniesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
