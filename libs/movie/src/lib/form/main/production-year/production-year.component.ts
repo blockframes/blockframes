cb959a0b08f7608035e3aa902711abe061b35de1
DK <jenna_kim1@hotmail.com>
2020-01-07 16:54:30 +0100

    created small components in movie/form/main

---
+++ libs/movie/src/lib/form/main/production-year/production-year.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'movie-form-production-year',
+  templateUrl: './production-year.component.html',
+  styleUrls: ['./production-year.component.scss']
+})
+export class ProductionYearComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
