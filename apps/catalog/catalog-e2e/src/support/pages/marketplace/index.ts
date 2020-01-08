3a53225f8ab9ed06eb423637a00c329ddb4ac68e
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-08 15:10:32 +0100

    Migrate catalog e2e

---
+++ apps/catalog/catalog-e2e/src/support/pages/marketplace/index.ts
@@ -0,0 +1,25 @@
+import WelcomeViewPage from '../auth/WelcomeViewPage';
+import LoginViewPage from '../auth/LoginViewPage';
+import HomePage from './HomePage';
+import NavbarPage from './NavbarPage';
+import SearchPage from './SearchPage'
+import ViewPage from './ViewPage';
+import DistributionPage from './DistributionPage';
+import SelectionPage from './SelectionPage';
+import FeedbackPage from './FeedbackPage';
+import WishlistPage from './WishlistPage';
+import OrganizationHomePage from './OrganizationHomePage';
+
+export {
+  WelcomeViewPage,
+  LoginViewPage,
+  HomePage,
+  NavbarPage,
+  SearchPage,
+  ViewPage,
+  DistributionPage,
+  SelectionPage,
+  FeedbackPage,
+  WishlistPage,
+  OrganizationHomePage
+};
