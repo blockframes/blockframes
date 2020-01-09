0ec9699e4ee3a20dfe9a242cfcf500154e8c3bca
maxstgt <mfritz@cascade8.com>
2020-01-09 15:02:59 +0100

    added components for tunnel page

---
+++ libs/movie/src/lib/movie/form/previous-deals/previous-deals.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'movie-form-previous-deals',
+    templateUrl: './previous-deals.component.html',
+    styleUrls: ['./previous-deals.component.scss']
+})
+export class MovieFormPreviousDealsComponent {
+
+}
