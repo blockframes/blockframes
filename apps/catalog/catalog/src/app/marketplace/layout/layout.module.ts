ead8374b3e393bd953fb2033e6032f85c1f010cd
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-09 17:59:23 +0100

    Add layout on marketplace

---
+++ apps/catalog/catalog/src/app/marketplace/layout/layout.module.ts
@@ -0,0 +1,62 @@
+// Angular
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
+// Libraries
+import { ToolbarModule } from '@blockframes/ui';
+import { KeyManagerModule } from '@blockframes/ethers';
+import { EmailVerifyModule } from '@blockframes/auth';
+
+// Widgets
+import { ProfileWidgetModule, ProfileMenuModule } from '@blockframes/account';
+import { NotificationWidgetModule } from '@blockframes/notification';
+import { ThemeWidgetModule } from '@blockframes/ui/theme';
+import { WalletWidgetModule } from '@blockframes/ethers';
+import { OrganizationWidgetModule } from '@blockframes/organization';
+
+// Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatMenuModule } from '@angular/material/menu';
+
+// Components
+import { LayoutComponent } from './layout.component';
+
+@NgModule({
+  declarations: [LayoutComponent],
+  exports: [LayoutComponent],
+  imports: [
+    // Angular
+    CommonModule,
+    FlexLayoutModule,
+    RouterModule,
+
+    // Material
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatBadgeModule,
+    MatMenuModule,
+
+    // Libraries
+    ToolbarModule,
+    ProfileMenuModule,
+    KeyManagerModule,
+    EmailVerifyModule,
+
+    // Widget
+    OrganizationWidgetModule,
+    ThemeWidgetModule,
+    NotificationWidgetModule,
+    WalletWidgetModule,
+    ProfileWidgetModule,
+  ],
+})
+export class LayoutModule {}
