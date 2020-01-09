c77735ffe8d058fcfeaed79d8a4d3e6befc4daf7
DK <jenna_kim1@hotmail.com>
2020-01-09 13:32:06 +0100

    sales case WIP

--- libs/movie/src/lib/movie/form/sales-cast/sales-cast.module.ts
+++ libs/movie/src/lib/movie/form/sales-cast/sales-cast.module.ts
@@ -4,6 +4,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MovieFormSalesCastComponent } from './sales-cast.component';
+// Material
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [MovieFormSalesCastComponent],
@@ -11,6 +17,10 @@ import { MovieFormSalesCastComponent } from './sales-cast.component';
       CommonModule,
       ReactiveFormsModule,
       FlexLayoutModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatIconModule,
+      MatButtonModule
     ],
   exports: [MovieFormSalesCastComponent],
 })
