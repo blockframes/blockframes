1204ea2765086665ff36a3b6a1594db289fea191
DK <jenna_kim1@hotmail.com>
2020-01-09 17:38:52 +0100

    refactored sales-cast

--- libs/movie/src/lib/movie/form/sales-cast/sales-cast.component.ts
+++ libs/movie/src/lib/movie/form/sales-cast/sales-cast.component.ts
@@ -1,25 +1,10 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
-import { FormControl, FormArray } from '@angular/forms';
-import { ControlContainer } from '@angular/forms';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { FormArray } from '@angular/forms';
 import { default as staticModels } from '../../static-model/staticModels';
-import { MovieSalesCastForm } from './sales-cast.form';
+import { CreditForm } from './sales-cast.form';
 
-function getFirstLastName(name: string) {
-  const fullname = name.split(/\s+/);
-  const firstname = fullname[0];
-  const lastname = fullname[1];
-  return { firstname, lastname }
-}
-
-function sepereatedByComma(input){
-  let arr = [];
-  if (input.indexOf(',') > -1) {
-    arr = input.split(',')
-  }
-  return arr;
-}
 @Component({
-  selector: '[formGroup] movie-form-sales-cast, [formGroupName] movie-form-sales-cast, [form] movie-form-sales-cast, movie-form-sales-cast',
+  selector: '[form] movie-form-sales-cast, movie-form-sales-cast',
   templateUrl: './sales-cast.component.html',
   styleUrls: ['./sales-cast.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -27,26 +12,11 @@ function sepereatedByComma(input){
 
 
 export class MovieFormSalesCastComponent {
-    @Input() form: FormControl;
-    @Input() role: string;
-    name = new FormControl();
-  public staticModels: any;
+  @Input() form: FormArray;
+  roles = staticModels.CREDIT_ROLES;
 
-  // constructor(public controlContainer: ControlContainer) { }
-
-  ngOnInit() {
-    this.staticModels = staticModels;
+  add() {
+    this.form.push(new CreditForm())
   }
-
-  update(name){
-    console.log(sepereatedByComma(name))
-    const list = sepereatedByComma(name)
-    const listOfObj = list.map((i) => getFirstLastName(i))
-    console.log(listOfObj);
-  }
-
-  // get salesCast() : MovieSalesCastForm {
-  //   return this.controlContainer.control as MovieSalesCastForm;
-  // }
 
 }
--- libs/movie/src/lib/movie/form/sales-cast/sales-cast.module.ts
+++ libs/movie/src/lib/movie/form/sales-cast/sales-cast.module.ts
@@ -2,25 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { FormDisplayNameModule } from '@blockframes/ui/form/display-name/display-name.module';
 import { MovieFormSalesCastComponent } from './sales-cast.component';
+
 // Material
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [MovieFormSalesCastComponent],
   imports: [
       CommonModule,
+      FormDisplayNameModule,
       ReactiveFormsModule,
       FlexLayoutModule,
       MatFormFieldModule,
       MatInputModule,
       MatIconModule,
-      MatButtonModule
+      MatButtonModule,
+      MatSelectModule
     ],
   exports: [MovieFormSalesCastComponent],
 })
