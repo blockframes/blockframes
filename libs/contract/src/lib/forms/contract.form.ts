881ee315c0a6c26fb1bcad082f2b79d5ee34019b
maxstgt <mfritz@cascade8.com>
2020-01-09 16:25:53 +0100

    chaning branches

--- libs/contract/src/lib/forms/contract.form.ts
+++ libs/contract/src/lib/forms/contract.form.ts
@@ -1,9 +1,26 @@
+import { FormControl } from '@angular/forms';
+import { Party, createParty } from '@blockframes/utils/common-interfaces/identity';
 import { FormEntity } from '@blockframes/utils/form/forms/entity.form';
 import { Injectable } from '@angular/core';
 import { createContract } from '@blockframes/contract/+state/contract.model';
 import { Contract } from './../+state/contract.model';
 
-function createContractControls(contract: Partial<Contract>){
+function createPartyControl(party?: Partial<Party[]>) {
+    const entity = createParty(party);
+    return {
+        displayName: new FormControl(entity.displayName)
+    }
+}
+
+type ContractPartyControl = ReturnType<typeof createPartyControl>;
+
+class ContractPartiesForm extends FormEntity<ContractPartyControl>{
+    constructor(party: Partial<Party[]>) {
+        super(createPartyControl(party))
+    }
+}
+
+function createContractControls(contract: Partial<Contract>) {
     const entity = createContract(contract);
     return {
         parties: new ContractPartiesForm(entity.parties),
