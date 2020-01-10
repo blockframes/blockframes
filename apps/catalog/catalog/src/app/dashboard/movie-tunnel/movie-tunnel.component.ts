3a2391ef5e37a9a3ef9a840bde4018953a9fe73e
Mussuranae <mandonnetdev@outlook.com>
2020-01-10 11:40:51 +0100

    Dynamic step in toolbar

--- apps/catalog/catalog/src/app/dashboard/movie-tunnel/movie-tunnel.component.ts
+++ apps/catalog/catalog/src/app/dashboard/movie-tunnel/movie-tunnel.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { MovieForm } from '@blockframes/movie/movie/form/movie.form';
 import { MovieService } from '@blockframes/movie/movie/+state';
 import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { RouterQuery } from '@datorama/akita-ng-router-store';
 
 @Component({
   selector: 'catalog-layout',
@@ -14,11 +16,18 @@ import { Subscription } from 'rxjs';
 
 export class MovieTunnelComponent {
   private sub: Subscription;
+  public pages = [
+    ['main', 'synopsis', 'credits', 'budget', 'technical-info', 'keywords'],
+    ['rights', 'deals'],
+    ['images', 'files&links'],
+    ['chain', 'evaluation']
+  ];
 
   constructor(
     @Host() private form: MovieForm,
     private service: MovieService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private routerQuery: RouterQuery
   ) {}
 
   ngOnInit() {
@@ -27,10 +36,21 @@ export class MovieTunnelComponent {
         ? await this.service.getValue(movieId)  // Edit movie
         : {};                                   // Create movie
       this.form.patchValue(movie)
-    })
+    });
   }
 
   ngOnDestroy() {
     this.sub.unsubscribe();
+  }
+
+  get pageUrl() {
+    return this.routerQuery.getValue().state.url.split('/').pop();
+  }
+
+  get pageData() {
+    const panel = this.pages.find(page => page.includes(this.pageUrl));
+    const index = panel.indexOf(this.pageUrl) + 1;
+    const arrayLength = panel.length;
+    return { index: index, length: arrayLength};
   }
 }
