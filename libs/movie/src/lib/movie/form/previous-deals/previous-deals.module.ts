194d8a25ce90aeea78d0a4975f405eb438339659
maxstgt <mfritz@cascade8.com>
2020-01-10 12:07:13 +0100

    fixed module

--- libs/movie/src/lib/movie/form/previous-deals/previous-deals.module.ts
+++ libs/movie/src/lib/movie/form/previous-deals/previous-deals.module.ts
@@ -1,11 +1,17 @@
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { CommonModule } from '@angular/common';
-import { MovieFormPreviousSales } from './previous-deals.component';
+import { MovieFormPreviousDealsComponent } from './previous-deals.component';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-    declarations: [MovieFormPreviousDeals],
-    imports: [CommonModule, FlexLayoutModule],
-    exports: [MovieFormPreviousDeals]
+    declarations: [MovieFormPreviousDealsComponent],
+    imports: [CommonModule, FlexLayoutModule, ReactiveFormsModule,
+    
+    // Material
+    MatFormFieldModule
+    ],
+    exports: [MovieFormPreviousDealsComponent]
 })
 export class MoviePreviousDealsModule {}
