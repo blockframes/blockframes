2cbeb12bcd3f5bc8b3437f3229a0da67fc3d38ee
DK <jenna_kim1@hotmail.com>
2020-01-10 10:57:00 +0100

    created media-image module&component

---
+++ apps/catalog/catalog/src/app/dashboard/movie-tunnel/media-image/media-image.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MediaImageComponent } from './media-image.component';
+
+
+
+@NgModule({
+  declarations: [MediaImageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MediaImageModule { }
