6146f73cfabde6cbb08a5315def681f7a709c3cc
DK <jenna_kim1@hotmail.com>
2020-01-10 10:57:00 +0100

    created media-image module&component

---
+++ apps/catalog/catalog/src/app/dashboard/movie-tunnel/media-image/media-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'catalog-media-image',
+  templateUrl: './media-image.component.html',
+  styleUrls: ['./media-image.component.scss']
+})
+export class MediaImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
