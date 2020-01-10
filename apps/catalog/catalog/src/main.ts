32dbd06032bbeadc59bf0ac11e7119aaf6bf324d
Yohann <ioa.nikas@gmail.com>
2020-01-10 18:40:30 +0100

    Create active contract guard

--- apps/catalog/catalog/src/main.ts
+++ apps/catalog/catalog/src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { akitaDevtools, akitaConfig, enableAkitaProdMode } from '@datorama/akita';
+import { akitaConfig, akitaDevtools, enableAkitaProdMode } from '@datorama/akita';
 
 if (environment.production) {
   enableProdMode();
---
+++ libs/contract/src/lib/guards/active-contract.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { CollectionGuard, CollectionGuardConfig } from "akita-ng-fire";
+import { ActivatedRouteSnapshot } from "@angular/router";
+import { ContractService } from "../+state/contract.service";
+import { ContractState } from "../+state/contract.store";
+
+@Injectable({ providedIn: 'root' })
+@CollectionGuardConfig({ awaitSync: true })
+export class ActiveContractGuard extends CollectionGuard<ContractState> {
+
+  constructor(service: ContractService) {
+    super(service);
+  }
+
+  /**
+   * Sync and set active.
+   * Use it on a route with :contractId in it.
+   */
+  sync(next: ActivatedRouteSnapshot) {
+    return this.service.syncActive({ id: next.params.contractId });
+  }
+}
