78261ded8b380d6f4792f4802d722038aab0518e
Mussuranae <mandonnetdev@outlook.com>
2020-01-10 14:13:08 +0100

    last changes

--- apps/catalog/catalog/src/app/dashboard/movie-tunnel/movie-tunnel.component.ts
+++ apps/catalog/catalog/src/app/dashboard/movie-tunnel/movie-tunnel.component.ts
@@ -2,9 +2,31 @@ import { Component, ChangeDetectionStrategy, Host } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MovieForm } from '@blockframes/movie/movie/form/movie.form';
 import { MovieService } from '@blockframes/movie/movie/+state';
-import { Subscription, Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { RouterQuery } from '@datorama/akita-ng-router-store';
+
+function getPageData(url: string, array: string[][]) {
+  const pageUrl = url.split('/').pop();
+  const panel = array.find(page => page.includes(pageUrl));
+  const index = panel.indexOf(pageUrl) + 1;
+  const arrayLength = panel.length;
+  return { index: index, length: arrayLength};
+}
+
+interface PageData {
+  index: number;
+  length: number;
+}
+
+// This table has to be synced with the routes, it allows the steps to be updated depending of the routes
+// See movie-tunnel-routing.module.ts to synchronize the table
+const pages = [
+  ['main', 'synopsis', 'credits', 'budget', 'technical-info', 'keywords'],
+  ['rights', 'deals'],
+  ['images', 'files&links'],
+  ['chain', 'evaluation']
+];
 
 @Component({
   selector: 'catalog-layout',
@@ -16,15 +38,9 @@ import { RouterQuery } from '@datorama/akita-ng-router-store';
 
 export class MovieTunnelComponent {
   private sub: Subscription;
-  // This table has to be synced with the routes, it allows the steps to be updated depending of the routes
-  // See movie-tunnel-routing.module.ts to synchronize the table
-  public pages = [
-    ['main', 'synopsis', 'credits', 'budget', 'technical-info', 'keywords'],
-    ['rights', 'deals'],
-    ['images', 'files&links'],
-    ['chain', 'evaluation']
-  ];
-  public pageData$: Observable<PageData>;
+  public pageData$ = this.routerQuery.select('state').pipe(
+    map( ({url}) => getPageData(url, pages))
+  );;
 
   constructor(
     @Host() private form: MovieForm,
@@ -40,29 +56,11 @@ export class MovieTunnelComponent {
         : {};                                   // Create movie
       this.form.patchValue(movie)
     });
-    this.pageUrl();
   }
 
   ngOnDestroy() {
     this.sub.unsubscribe();
   }
-
-  pageUrl() {
-    this.pageData$ = this.routerQuery.select('state').pipe(
-      map( state => getPageData(state.url, this.pages))
-    );
-  }
 }
 
-function getPageData(url: string, pages: string[][]) {
-  const pageUrl = url.split('/').pop();
-  const panel = pages.find(page => page.includes(pageUrl));
-  const index = panel.indexOf(pageUrl) + 1;
-  const arrayLength = panel.length;
-  return { index: index, length: arrayLength};
-}
 
-interface PageData {
-  index: number;
-  length: number;
-}
