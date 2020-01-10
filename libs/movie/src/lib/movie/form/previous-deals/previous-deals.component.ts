99ad51227eca48fb4e5da4ae966cc5047b2ca1a4
maxstgt <mfritz@cascade8.com>
2020-01-10 12:07:13 +0100

    fixed module

--- libs/movie/src/lib/movie/form/previous-deals/previous-deals.component.ts
+++ libs/movie/src/lib/movie/form/previous-deals/previous-deals.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
     selector: 'movie-form-previous-deals',
     templateUrl: './previous-deals.component.html',
-    styleUrls: ['./previous-deals.component.scss']
+    styleUrls: ['./previous-deals.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MovieFormPreviousDealsComponent {
 
