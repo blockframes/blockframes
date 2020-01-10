40e14be4842e1e32b7fc0d102e853429e12115c7
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-10 16:11:26 +0100

    remove marketplace & dashboard from main

--- libs/utils/src/lib/routes/context-menu/main.ts
+++ libs/utils/src/lib/routes/context-menu/main.ts
@@ -3,7 +3,6 @@
 import * as common from './common/common';
 
 // app
-import * as catalogDashboard from './app/catalog-dashboard';
 import * as catalogMarketplace from './app/catalog-marketplace';
 import * as delivery from './app/delivery';
 
@@ -11,7 +10,6 @@ import { ctxMenu } from './helpers';
 
 export const CONTEXT_MENU = ctxMenu([
   ...common.CONTEXT_MENU,
-  ...catalogDashboard.baseMenu,
   ...catalogMarketplace.baseMenu,
   ...delivery.baseMenu,
 ]);
