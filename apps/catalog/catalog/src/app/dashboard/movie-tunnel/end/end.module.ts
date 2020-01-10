bba63d6f36decae2298c240cf3c7d8cc96fdc202
Mussuranae <mandonnetdev@outlook.com>
2020-01-10 16:11:36 +0100

    change name of files

--- apps/catalog/catalog/src/app/dashboard/movie-tunnel/end/end.module.ts
+++ apps/catalog/catalog/src/app/dashboard/movie-tunnel/end/end.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
-import { EndTunnelComponent } from './end-tunnel.component';
+import { EndTunnelComponent } from './end.component';
 
 // Materials
 import { MatButtonModule } from '@angular/material/button';
