c4c9082493e915dbb077d763cc5aa38bd4e9cfca
maxstgt <mfritz@cascade8.com>
2020-01-13 14:51:17 +0100

    added territory function

--- libs/movie/src/lib/distribution-deals/form/distribution-deal.form.ts
+++ libs/movie/src/lib/distribution-deals/form/distribution-deal.form.ts
@@ -5,19 +5,25 @@ import { Injectable } from '@angular/core';
 import { DistributionDeal } from '@blockframes/movie/distribution-deals/+state/distribution-deal.model';
 
 function createDistributionDealControls(deal: Partial<DistributionDeal>) {
-    const entity = createDistributionDeal(deal)
-    return {
-        exclusive: new FormControl(entity.exclusive),
-        territory: new FormControl(entity.territory),
-        territoryExcluded: new FormControl(entity.territoryExcluded)
-    }
+  const entity = createDistributionDeal(deal);
+  return {
+    exclusive: new FormControl(entity.exclusive),
+    territory: new FormControl(entity.territory),
+    territoryExcluded: new FormControl(entity.territoryExcluded)
+  };
 }
 
 type DistributionDealControls = ReturnType<typeof createDistributionDealControls>;
 
 @Injectable()
 export class DistributionDealForm extends FormEntity<DistributionDealControls> {
-    constructor() {
-        super(createDistributionDealControls({}))
-    }
+  constructor() {
+    super(createDistributionDealControls({}));
+  }
+
+  addTerritory(territory: string) {
+    const state = this.get('territory').value;
+    this.get('territory').setValue([...state, territory]);
+    console.log(this.get('territory').value)
+  }
 }
