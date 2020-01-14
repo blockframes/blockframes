b93dc0b58cc4a7e593a7e1a52869d86b6e141ede
Yohann <ioa.nikas@gmail.com>
2020-01-14 14:40:28 +0100

    remove versions event from backend in favor of frontend

--- apps/backend-functions/src/data/types.ts
+++ apps/backend-functions/src/data/types.ts
@@ -36,7 +36,6 @@ export {
 } from '@blockframes/permissions/types';
 export { PublicUser } from '@blockframes/auth/types';
 export { RequestDemoInformations } from '@blockframes/catalog/demo-request.model';
-export { ContractVersionDocument, VersionMeta } from '@blockframes/contract/+state/contract.firestore';
 
 /**
  * Types used by the firebase backend.
