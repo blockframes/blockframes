743ee8014cbd1fb4db51917e2f4f0a013775e638
DK <jenna_kim1@hotmail.com>
2020-01-14 11:32:32 +0100

    reel form array

--- libs/movie/src/lib/movie/form/promotional-elements/promo-reel-link/promo-reel-link.component.ts
+++ libs/movie/src/lib/movie/form/promotional-elements/promo-reel-link/promo-reel-link.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'movie-form-promo-reel-link',
   templateUrl: './promo-reel-link.component.html',
-  styleUrls: ['./promo-reel-link.component.scss']
+  styleUrls: ['./promo-reel-link.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PromoReelLinkComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class PromoReelLinkComponent {
+  @Input() form:FormArray;
 
 }
