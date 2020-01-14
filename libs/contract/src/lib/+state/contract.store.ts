1d39c7614e8b4228303256619a519bc8b9b2aad1
Yohann <ioa.nikas@gmail.com>
2020-01-14 09:52:20 +0100

    Rewriting contract movies query

--- libs/contract/src/lib/+state/contract.store.ts
+++ libs/contract/src/lib/+state/contract.store.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Contract } from './contract.model';
+import { ContractWithLastVersion } from './contract.model';
 import { EntityState, ActiveState, EntityStore, StoreConfig } from '@datorama/akita';
 
-export interface ContractState extends EntityState<Contract, string>, ActiveState<string> {}
+export interface ContractState extends EntityState<ContractWithLastVersion, string>, ActiveState<string> {}
 
 @Injectable({ providedIn: 'root' })
 @StoreConfig({ name: 'contracts' })
