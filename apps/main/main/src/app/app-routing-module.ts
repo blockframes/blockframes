e44281428cf8b32414179cb3fe5f388bbffd0bfd
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-14 13:46:56 +0100

    remove useless routes in main

--- apps/main/main/src/app/app-routing-module.ts
+++ apps/main/main/src/app/app-routing-module.ts
@@ -8,42 +8,6 @@ import { AppGridComponent } from './app-grid/app-grid.component';
 import { App } from '@blockframes/utils';
 import { createRoutes } from '@blockframes/utils/routes';
 
-/** Scaffold a dashboard like application routing for this application */
-// const routes = createRoutes({
-//   appName: 'main',
-//   appsRoutes: [
-//     {
-//       path: '',
-//       redirectTo: 'apps',
-//       pathMatch: 'full'
-//     },
-//     {
-//       path: 'apps',
-//       component: AppGridComponent
-//     },
-//     {
-//       path: 'catalog',
-//       loadChildren: () => import('@blockframes/apps/catalog/catalog.module').then(m => m.CatalogModule)
-//     },
-//     {
-//       path: App.mediaDelivering,
-//       data: { app: App.mediaDelivering },
-//       loadChildren: () => import('@blockframes/apps/delivery').then(m => m.DeliveryAppModule)
-//     },
-//     {
-//       path: App.biggerBoat,
-//       data: { app: App.biggerBoat },
-//       loadChildren: () =>
-//         import('@blockframes/apps/catalog/marketplace').then(m => m.CatalogMarketplaceAppModule)
-//     },
-//     {
-//       path: App.catalogDashboard,
-//       data: { app: App.catalogDashboard },
-//       loadChildren: () =>
-//         import('@blockframes/apps/catalog/dashboard').then(m => m.CatalogDashboardAppModule)
-//     }
-//   ]
-// });
 
 const routes = [{
   path: '',
