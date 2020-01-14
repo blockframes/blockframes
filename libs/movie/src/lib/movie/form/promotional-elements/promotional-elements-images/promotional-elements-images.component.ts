7cd2e0ec593b18dec466f10fd60e6dcb16db729a
DK <jenna_kim1@hotmail.com>
2020-01-14 15:09:26 +0100

    cropper or imgRef

--- libs/movie/src/lib/movie/form/promotional-elements/promotional-elements-images/promotional-elements-images.component.ts
+++ libs/movie/src/lib/movie/form/promotional-elements/promotional-elements-images/promotional-elements-images.component.ts
@@ -4,6 +4,7 @@ import { PromotionalElement } from '../../../+state/movie.firestore';
 import { PromotionalElementTypesSlug } from '@blockframes/movie/moviestatic-model/types';
 import { MoviePromotionalElementForm } from '../promotional-elements.form';
 import { FormArray } from '@angular/forms';
+import { createImgRef } from '@blockframes/utils/image-uploader';
 // import { PROMOTIONAL_ELEMENT_TYPES } from '../../../static-model/staticModels';
 
 @Component({
@@ -14,18 +15,23 @@ import { FormArray } from '@angular/forms';
 })
 export class PromotionalElementsImagesComponent {
   @Input() form: FormList<PromotionalElement>
+  displayCropper: boolean;
 
   get controls() {
     console.log(this.form.controls)
     const bannerForm = this.form.controls.filter(c => c.value.type === 'banner');
     // console.log(bannerForm)
     if (bannerForm.length) {
+      this.displayCropper = false;
       // display the value
+      console.log('display value', bannerForm)
     } else {
+      console.log('display cropper')
+      this.displayCropper = true;
       // display the cropper
       // Once you've got a value in the cropper
       // Push the value inside the formList
-      this.form.push(new MoviePromotionalElementForm({ type: 'banner' }))
+      this.form.push(new MoviePromotionalElementForm({ label: 'Banner', type: 'banner', media: createImgRef()}))
 
     }
     return this.form.controls.filter(control => control.value.type === 'banner')
