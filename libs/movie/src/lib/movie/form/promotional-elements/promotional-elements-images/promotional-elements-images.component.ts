0d1dccc617b5ff89b8e7eccf458b6cb2796d0b32
DK <jenna_kim1@hotmail.com>
2020-01-14 13:22:21 +0100

    filter WIP

--- libs/movie/src/lib/movie/form/promotional-elements/promotional-elements-images/promotional-elements-images.component.ts
+++ libs/movie/src/lib/movie/form/promotional-elements/promotional-elements-images/promotional-elements-images.component.ts
@@ -7,23 +7,30 @@ import { FormArray } from '@angular/forms';
 // import { PROMOTIONAL_ELEMENT_TYPES } from '../../../static-model/staticModels';
 
 @Component({
-  selector: '[type][form] movie-promotional-elements-images',
+  selector: '[form] movie-promotional-elements-images',
   templateUrl: './promotional-elements-images.component.html',
   styleUrls: ['./promotional-elements-images.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PromotionalElementsImagesComponent {
   @Input() form: FormList<PromotionalElement>
-  // @Input() form: FormArray;
-  @Input() type: PromotionalElementTypesSlug;
 
   get controls() {
     console.log(this.form.controls)
-    // console.log(this.form.get('promotionalElements').value[0].type)
-    // console.log(Object.keys(this.form.controls).forEach(e => {console.log(e)}))
-    // console.log(this.form.controls.forEach((e)=> console.log(e)))
-    return this.form.controls.filter(control => control.value.type === this.type)
+    const bannerForm = this.form.controls.filter(c => c.value.type === 'banner');
+    // console.log(bannerForm)
+    if (bannerForm.length) {
+      // display the value
+    } else {
+      // display the cropper
+      // Once you've got a value in the cropper
+      // Push the value inside the formList
+      this.form.push(new MoviePromotionalElementForm({ type: 'banner' }))
+
+    }
+    return this.form.controls.filter(control => control.value.type === 'banner')
   }
+
 
   add() {
     // this.form.push(new MoviePromotionalElementForm({ type: this.type ))
