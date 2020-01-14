05b2029dabe90b920bbf9dd236bd8fe48211c83e
DK <jenna_kim1@hotmail.com>
2020-01-14 11:24:49 +0100

    UI

---
+++ libs/movie/src/lib/movie/form/promotional-elements/promo-reel-link/promo-reel-link.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'movie-form-promo-reel-link',
+  templateUrl: './promo-reel-link.component.html',
+  styleUrls: ['./promo-reel-link.component.scss']
+})
+export class PromoReelLinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ libs/movie/src/lib/movie/form/promotional-elements/promotional-links/promotional-links.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'movie-form-promotional-links',
+  templateUrl: './promotional-links.component.html',
+  styleUrls: ['./promotional-links.component.scss']
+})
+export class PromotionalLinksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
