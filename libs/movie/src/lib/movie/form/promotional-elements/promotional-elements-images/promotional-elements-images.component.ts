59ce9e350e7fba7a391e848b42324081fd8640b1
DK <jenna_kim1@hotmail.com>
2020-01-14 17:04:09 +0100

    removed comments

--- libs/movie/src/lib/movie/form/promotional-elements/promotional-elements-images/promotional-elements-images.component.ts
+++ libs/movie/src/lib/movie/form/promotional-elements/promotional-elements-images/promotional-elements-images.component.ts
@@ -5,8 +5,6 @@ import { PromotionalElementTypesSlug } from '@blockframes/movie/moviestatic-mode
 import { MoviePromotionalElementForm } from '../promotional-elements.form';
 import { FormArray, FormControl } from '@angular/forms';
 import { createImgRef, ImgRef } from '@blockframes/utils/image-uploader';
-// import { PROMOTIONAL_ELEMENT_TYPES } from '../../../static-model/staticModels';
-
 @Component({
   selector: '[form] movie-promotional-elements-images',
   templateUrl: './promotional-elements-images.component.html',
@@ -18,29 +16,6 @@ export class PromotionalElementsImagesComponent {
   @Input() type: string;
   localForm = new MoviePromotionalElementForm();
   displayCropper: boolean;
-
-  // get controls() {
-  //   console.log(this.form.controls)
-  //   const bannerForm = this.form.controls.filter(c => c.value.type === 'banner');
-  //   // console.log(bannerForm)
-  //   if (bannerForm.length) {
-  //     this.displayCropper = false;
-  //     // display the value
-  //     console.log('display value', bannerForm)
-  //   } else {
-  //     console.log('display cropper', bannerForm)
-  //     this.displayCropper = true;
-  //     // display the cropper
-  //     // Once you've got a value in the cropper
-  //     // Push the value inside the formList
-
-  //     // this.form.push(new FormControl());
-  //     this.form.push(new MoviePromotionalElementForm({ label: 'Banner', type: 'banner', media: createImgRef()}));
-
-  //   }
-  //   return this.form.controls.filter(control => control.value.type === 'banner')
-  // }
-
 
   add(media: ImgRef) {
     const control = new MoviePromotionalElementForm({ label: '', type: this.type, media: media });
