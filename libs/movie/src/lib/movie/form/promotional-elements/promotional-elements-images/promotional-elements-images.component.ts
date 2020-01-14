c3ca288d25678a3052c51dbd8011274dc512045b
DK <jenna_kim1@hotmail.com>
2020-01-14 17:03:25 +0100

    connect with form

--- libs/movie/src/lib/movie/form/promotional-elements/promotional-elements-images/promotional-elements-images.component.ts
+++ libs/movie/src/lib/movie/form/promotional-elements/promotional-elements-images/promotional-elements-images.component.ts
@@ -3,8 +3,8 @@ import { FormList } from '@blockframes/utils/form/forms/list.form';
 import { PromotionalElement } from '../../../+state/movie.firestore';
 import { PromotionalElementTypesSlug } from '@blockframes/movie/moviestatic-model/types';
 import { MoviePromotionalElementForm } from '../promotional-elements.form';
-import { FormArray } from '@angular/forms';
-import { createImgRef } from '@blockframes/utils/image-uploader';
+import { FormArray, FormControl } from '@angular/forms';
+import { createImgRef, ImgRef } from '@blockframes/utils/image-uploader';
 // import { PROMOTIONAL_ELEMENT_TYPES } from '../../../static-model/staticModels';
 
 @Component({
@@ -15,30 +15,35 @@ import { createImgRef } from '@blockframes/utils/image-uploader';
 })
 export class PromotionalElementsImagesComponent {
   @Input() form: FormList<PromotionalElement>
+  @Input() type: string;
+  localForm = new MoviePromotionalElementForm();
   displayCropper: boolean;
 
-  get controls() {
-    console.log(this.form.controls)
-    const bannerForm = this.form.controls.filter(c => c.value.type === 'banner');
-    // console.log(bannerForm)
-    if (bannerForm.length) {
-      this.displayCropper = false;
-      // display the value
-      console.log('display value', bannerForm)
-    } else {
-      console.log('display cropper')
-      this.displayCropper = true;
-      // display the cropper
-      // Once you've got a value in the cropper
-      // Push the value inside the formList
-      this.form.push(new MoviePromotionalElementForm({ label: 'Banner', type: 'banner', media: createImgRef()}))
+  // get controls() {
+  //   console.log(this.form.controls)
+  //   const bannerForm = this.form.controls.filter(c => c.value.type === 'banner');
+  //   // console.log(bannerForm)
+  //   if (bannerForm.length) {
+  //     this.displayCropper = false;
+  //     // display the value
+  //     console.log('display value', bannerForm)
+  //   } else {
+  //     console.log('display cropper', bannerForm)
+  //     this.displayCropper = true;
+  //     // display the cropper
+  //     // Once you've got a value in the cropper
+  //     // Push the value inside the formList
 
-    }
-    return this.form.controls.filter(control => control.value.type === 'banner')
-  }
+  //     // this.form.push(new FormControl());
+  //     this.form.push(new MoviePromotionalElementForm({ label: 'Banner', type: 'banner', media: createImgRef()}));
+
+  //   }
+  //   return this.form.controls.filter(control => control.value.type === 'banner')
+  // }
 
 
-  add() {
-    // this.form.push(new MoviePromotionalElementForm({ type: this.type ))
+  add(media: ImgRef) {
+    const control = new MoviePromotionalElementForm({ label: '', type: this.type, media: media });
+    this.form.push(control);
   }
 }
