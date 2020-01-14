8e4f410fd7aedccbe63407e69dd8ad96100420fc
DK <jenna_kim1@hotmail.com>
2020-01-14 15:09:26 +0100

    cropper or imgRef

--- libs/movie/src/lib/movie/form/promotional-elements/promotional-elements-images/promotional-elements-images.module.ts
+++ libs/movie/src/lib/movie/form/promotional-elements/promotional-elements-images/promotional-elements-images.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { PromotionalElementsImagesComponent } from './promotional-elements-images.component';
 import { CropperModule } from '@blockframes/ui/media/cropper/cropper.module';
+import { ImageReferenceModule } from '@blockframes/ui/media/image-reference/image-reference.module';
 @NgModule({
   declarations: [PromotionalElementsImagesComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    CropperModule
+    CropperModule,
+    ImageReferenceModule
   ],
   exports: [PromotionalElementsImagesComponent]
 })
