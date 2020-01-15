f2ee52c8b95d2754f534df44ed512f0564adcd83
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-15 13:50:37 +0100

    clean language & content-type

--- libs/movie/src/lib/movie/form/main/languages/languages.module.ts
+++ libs/movie/src/lib/movie/form/main/languages/languages.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { LanguagesComponent } from './languages.component';
 
 
 
 @NgModule({
   declarations: [LanguagesComponent],
+  exports: [LanguagesComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
-export class LanguagesModule { }
+export class MovieFormLanguagesModule { }
