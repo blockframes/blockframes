81b881788e778a69b6ee9991fd4c617c066d8123
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-15 17:50:23 +0100

    update original release

--- libs/movie/src/lib/movie/form/sales-info/original-releases/original-releases.module.ts
+++ libs/movie/src/lib/movie/form/sales-info/original-releases/original-releases.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { OriginalReleaseComponent } from './original-releases.component';
 
 import { MovieFormOriginCountryModule } from '../../main/origin-country/origin-country.module';
@@ -8,6 +9,7 @@ import { MovieFormOriginCountryModule } from '../../main/origin-country/origin-c
 // Material
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -19,10 +21,12 @@ import { MatNativeDateModule } from '@angular/material/core';
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    FlexLayoutModule,
     MovieFormOriginCountryModule,
     // Material
     MatFormFieldModule,
     MatInputModule,
+    MatSelectModule,
     MatDatepickerModule,
     MatNativeDateModule,
     MatButtonModule,
