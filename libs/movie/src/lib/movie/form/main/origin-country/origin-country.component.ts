ec8d27ca0fa3cb791c5c331a7e29476b6541bdb0
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-15 17:26:40 +0100

    Add autocomplete to countries

--- libs/movie/src/lib/movie/form/main/origin-country/origin-country.component.ts
+++ libs/movie/src/lib/movie/form/main/origin-country/origin-country.component.ts
@@ -1,17 +1,32 @@
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { default as staticModel } from '../../../static-model/staticModels'
+import { startWith, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'movie-form-origin-country',
+  selector: '[form] movie-form-origin-country',
   templateUrl: './origin-country.component.html',
   styleUrls: ['./origin-country.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OriginCountryComponent {
+export class OriginCountryComponent implements OnInit {
   @Input() form: FormControl;
+  countries = staticModel.TERRITORIES;
 
-  constructor() { }
+  filteredCountries$: Observable<typeof staticModel.TERRITORIES>;
 
+  ngOnInit() {
+    this.filteredCountries$ = this.form.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => typeof value === 'string' ? value : value.name),
+        map(country => country ? this._filter(country) : this.countries.slice())
+      );
+  }
 
-
+  private _filter(country: string) {
+    const filterValue = country.toLowerCase();
+    return this.countries.filter(({ label }) => label.toLowerCase().indexOf(filterValue) === 0)
+  }
 }
