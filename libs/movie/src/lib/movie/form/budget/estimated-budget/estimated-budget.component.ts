857f19e915f3db284804117895d8573bd466dd01
Mussuranae <mandonnetdev@outlook.com>
2020-01-15 15:16:54 +0100

    change on estimated budget and range

--- libs/movie/src/lib/movie/form/budget/estimated-budget/estimated-budget.component.ts
+++ libs/movie/src/lib/movie/form/budget/estimated-budget/estimated-budget.component.ts
@@ -13,8 +13,12 @@ export class EstimatedBudgetComponent {
   @Input() form: FormControl;
 
   budgetList: NumberRange[] = [
-    { from: 0, to: 1000000 },
-    { from: 1000000, to: 2000000 },
-    { from: 2000000, to: 3000000 },
+    { from: 0, to: 1000000, label: 'Less than 1 millions' },
+    { from: 1000000, to: 5000000, label: '1 - 5 millions' },
+    { from: 5000000, to: 10000000, label: '5 - 10 millions' },
+    { from: 10000000, to: 50000000, label: '10 - 50 millions' },
+    { from: 50000000, to: 100000000, label: '50 - 100 millions' },
+    { from: 100000000, to: 300000000, label: '100 - 300 millions' },
+    { from: 300000000, to: 999999999, label: 'More than 300 millions' },
   ];
 }
