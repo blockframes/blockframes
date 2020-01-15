ab62a48cd2aa4f786b2f4e89843f2aa0caedb28f
maxstgt <mfritz@cascade8.com>
2020-01-15 11:41:35 +0100

    made changes on fracnois vehalf

--- libs/movie/src/lib/distribution-deals/form/territory/territory.component.ts
+++ libs/movie/src/lib/distribution-deals/form/territory/territory.component.ts
@@ -9,14 +9,7 @@ import { Observable } from 'rxjs';
 // Angular
 import { ENTER } from '@angular/cdk/keycodes';
 import { FormControl } from '@angular/forms';
-import {
-  Component,
-  Input,
-  ChangeDetectionStrategy,
-  OnInit,
-  ViewChild,
-  ElementRef
-} from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, ViewChild, ElementRef } from '@angular/core';
 
 // Others
 import { DistributionDealForm } from '../distribution-deal.form';
@@ -27,172 +20,130 @@ import {
 } from '@blockframes/movie/movie/static-model/staticModels';
 import { TERRITORIES_SLUG } from '@blockframes/movie/movie/static-model/types';
 
+type TerritoryType = 'territory' | 'territoryExcluded';
+
 @Component({
   selector: '[form] distribution-form-territory',
   templateUrl: './territory.component.html',
   styleUrls: ['./territory.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DistributionDealTerritoryComponent implements OnInit {
+export class DistributionDealTerritoryComponent {
   @Input() form: DistributionDealForm;
 
-  public includedTerritories: string[] = [];
+  public value = {
+    territory: [],
+    territoryExcluded: []
+  };
 
-  public excludedTerritories: string[] = [];
+  public controls = {
+    territory: new FormControl(),
+    territoryExcluded: new FormControl()
+  };
+
+  public $ = {
+    territory: this.select('territory'),
+    territoryExcluded: this.select('territoryExcluded')
+  };
 
   public staticTerritories = staticModels['TERRITORIES'];
-
-  public includedControl: FormControl = new FormControl('');
-
-  public excludedControl: FormControl = new FormControl('');
 
   // Lets the user use the command to add more territories
   public separatorKeysCodes: number[] = [ENTER];
 
-  public filteredIncludedTerritories$: Observable<string[] | SlugAndLabel[]>;
-
-  public filteredExcludedTerritories$: Observable<string[] | SlugAndLabel[]>;
-
   // For included territories
-  @ViewChild('includedInput', { static: false })
+  @ViewChild(`includedInput`, { static: false })
   includedTerritoryInput: ElementRef<HTMLInputElement>;
 
   @ViewChild('includedAuto', { static: false }) includedAuto: MatAutocomplete;
 
-  // For excluded territories
   @ViewChild('excludedInput', { static: false })
   excludedTerritoryInput: ElementRef<HTMLInputElement>;
 
   @ViewChild('excludedAuto', { static: false }) excludedAuto: MatAutocomplete;
 
-  ngOnInit() {
-    this.filteredIncludedTerritories$ = this.includedControl.valueChanges.pipe(
-      startWith(this.includedControl.value),
-      map((territory: string | null) =>
-        territory ? this.territoryFilter(territory) : this.staticTerritories.slice()
-      )
+  public select(type: TerritoryType): Observable<string[] | SlugAndLabel[]> {
+    return this.controls[type].valueChanges.pipe(
+      startWith(this.controls[type].value),
+      map(territory => {
+        return territory ? this.territoryFilter(territory) : this.staticTerritories.slice();
+      })
     );
-
-    this.filteredExcludedTerritories$ = this.excludedControl.valueChanges.pipe(
-      startWith(this.excludedControl.value),
-      map((territory: string | null) =>
-        territory ? this.territoryFilter(territory) : this.staticTerritories.slice()
-      )
-    );
-  }
-
-  private hasTerritory(value: string, type: string): boolean {
-    if (type === 'included') {
-      this.includedTerritories.includes(value);
-    } else if (type === 'excluded') {
-      return this.excludedTerritories.includes(value);
-    }
-  }
-
-  private noDuplicate(value: string, type: string): boolean {
-    if (type === 'included') {
-      return this.excludedTerritories.includes(getCodeIfExists('TERRITORIES', value));
-    } else if (type === 'excluded') {
-      return this.includedTerritories.includes(getCodeIfExists('TERRITORIES', value));
-    }
-  }
-
-  public includedAdd(event: MatChipInputEvent) {
-    if (!this.includedAuto.isOpen) {
-      const input = event.input;
-      const value = event.value.trim();
-
-      // Add the territory
-      if (
-        (value || '') &&
-        !this.hasTerritory(value, 'included') &&
-        TERRITORIES_SLUG.includes(value) &&
-        !this.noDuplicate(value, 'included')
-      ) {
-        this.includedTerritories.push(value);
-        this.form.addTerritory(value, 'included');
-      }
-
-      // Reset the input value
-      if (input) {
-        input.value = '';
-      }
-
-      this.includedControl.setValue(null);
-    }
-  }
-
-  public excludedAdd(event: MatChipInputEvent) {
-    if (!this.excludedAuto.isOpen) {
-      const input = event.input;
-      const value = event.value.trim();
-
-      // Add the territory
-      if (
-        (value || '') &&
-        !this.hasTerritory(value, 'excluded') &&
-        TERRITORIES_SLUG.includes(value) &&
-        !this.noDuplicate(value, 'excluded')
-      ) {
-        this.excludedTerritories.push(value);
-        this.form.addTerritory(value, 'excluded');
-      }
-
-      // Reset the input value
-      if (input) {
-        input.value = '';
-      }
-
-      this.excludedControl.setValue(null);
-    }
-  }
-
-  public removeIncluded(territory: string) {
-    const index = this.includedTerritories.indexOf(territory);
-
-    if (index >= 0) {
-      this.includedTerritories.splice(index, 1);
-      this.form.removeIncludedTerritory(index);
-    }
-  }
-
-  public removeExcluded(territory: string) {
-    const index = this.excludedTerritories.indexOf(territory);
-
-    if (index >= 0) {
-      this.excludedTerritories.splice(index, 1);
-      this.form.removeExcludedTerritory(index);
-    }
-  }
-
-  public includedSelected(event: MatAutocompleteSelectedEvent) {
-    if (
-      !this.includedTerritories.includes(event.option.viewValue) &&
-      TERRITORIES_SLUG.includes(event.option.value) &&
-      !this.excludedTerritories.includes(event.option.viewValue)
-    ) {
-      this.form.addTerritory(event.option.value, 'included');
-      this.includedTerritories.push(event.option.viewValue);
-    }
-    this.includedTerritoryInput.nativeElement.value = '';
-    this.includedControl.reset();
-  }
-
-  public excludedSelected(event: MatAutocompleteSelectedEvent) {
-    if (
-      !this.excludedTerritories.includes(event.option.viewValue) &&
-      TERRITORIES_SLUG.includes(event.option.value) &&
-      !this.includedTerritories.includes(event.option.viewValue)
-    ) {
-      this.form.addTerritory(event.option.value, 'excluded');
-      this.excludedTerritories.push(event.option.viewValue);
-    }
-    this.excludedTerritoryInput.nativeElement.value = '';
-    this.excludedControl.reset();
   }
 
   private territoryFilter(value: string): SlugAndLabel[] {
     const filterValue = value.toLowerCase();
     return this.staticTerritories.filter(territory => territory.slug.trim().includes(filterValue));
+  }
+
+  private isValidTerritoryAndNoDuplicate(type: TerritoryType, territory: string): boolean {
+    if (type === 'territory') {
+      return (
+        !this.value[type].includes(territory) &&
+        TERRITORIES_SLUG.includes(getCodeIfExists('TERRITORIES', territory)) &&
+        !this.value['territoryExcluded'].includes(territory)
+      );
+    }
+    return (
+      !this.value[type].includes(territory) &&
+      TERRITORIES_SLUG.includes(getCodeIfExists('TERRITORIES', territory)) &&
+      !this.value['territory'].includes(territory)
+    );
+  }
+
+  public addTerritory(event: MatChipInputEvent, type: TerritoryType) {
+    if (type === 'territory' && !this.includedAuto.isOpen) {
+      const input = event.input;
+      const value = event.value.trim();
+
+      // Add the territory
+      if ((value || '') && this.isValidTerritoryAndNoDuplicate(type, value)) {
+        this.value[type].push(value);
+        this.form.addTerritory(value, type);
+      }
+
+      // Reset the input value
+      if (input) {
+        input.value = '';
+      }
+      this.controls[type].reset();
+    } else if (type === 'territoryExcluded' && !this.excludedAuto.isOpen) {
+      const input = event.input;
+      const value = event.value.trim();
+
+      // Add the territory
+      if ((value || '') && this.isValidTerritoryAndNoDuplicate(type, value)) {
+        this.value[type].push(value);
+        this.form.addTerritory(value, type);
+      }
+
+      // Reset the input value
+      if (input) {
+        input.value = '';
+      }
+      this.controls[type].reset();
+    }
+  }
+
+  public removeTerritory(territory: string, type: TerritoryType) {
+    const index = this.value[type].indexOf(territory);
+    if (index >= 0) {
+      this.value[type].splice(index, 1);
+      this.form.removeTerritory(index, type);
+    }
+  }
+
+  public selectedTerritory(event: MatAutocompleteSelectedEvent, type: TerritoryType) {
+    if (this.isValidTerritoryAndNoDuplicate(type, event.option.viewValue)) {
+      this.form.addTerritory(event.option.value, type);
+      this.value[type].push(event.option.viewValue);
+      this.includedTerritoryInput.nativeElement.value = '';
+      this.controls[type].reset();
+    } else if (this.isValidTerritoryAndNoDuplicate(type, event.option.viewValue)) {
+      this.form.addTerritory(event.option.value, type);
+      this.value[type].push(event.option.viewValue);
+      this.excludedTerritoryInput.nativeElement.value = '';
+      this.controls[type].reset();
+    }
   }
 }
