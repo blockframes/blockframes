18d8129417663dfde3d739e9370843ecff7856a5
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-15 13:05:41 +0100

    Add languages

---
+++ libs/movie/src/lib/movie/form/main/languages/languages.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LanguagesComponent } from './languages.component';
+
+
+
+@NgModule({
+  declarations: [LanguagesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LanguagesModule { }
