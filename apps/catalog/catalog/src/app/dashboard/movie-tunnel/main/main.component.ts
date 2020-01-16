205ea0c4dad99ec166dbcd1c1310e4d883e3a5d9
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-16 22:08:54 +0100

    update main

--- apps/catalog/catalog/src/app/dashboard/movie-tunnel/main/main.component.ts
+++ apps/catalog/catalog/src/app/dashboard/movie-tunnel/main/main.component.ts
@@ -19,6 +19,10 @@ export class TunnelMainComponent implements OnInit {
     return this.form.get('salesInfo');
   }
 
+  get festivalPrizes() {
+    return this.form.get('festivalPrizes');
+  }
+
   ngOnInit() {
   }
 
