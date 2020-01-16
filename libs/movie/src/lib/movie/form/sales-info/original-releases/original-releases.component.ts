0d1069e31412d921708501d01f624e6edf7947f0
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-16 19:11:26 +0100

    Finish style

--- libs/movie/src/lib/movie/form/sales-info/original-releases/original-releases.component.ts
+++ libs/movie/src/lib/movie/form/sales-info/original-releases/original-releases.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
 import { OriginalReleaseForm } from '../sales-info.form';
 import { MovieForm } from '../../movie.form';
 import { startWith } from 'rxjs/operators';
 import { default as staticModel, ExtractCode } from '../../../static-model/staticModels';
 import { MovieMainControl } from '../../main/main.form';
 import { MovieSalesInfoControl } from '../sales-info.form'
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'movie-form-original-releases',
@@ -12,10 +13,11 @@ import { MovieSalesInfoControl } from '../sales-info.form'
   styleUrls: ['./original-releases.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OriginalReleaseComponent implements OnInit {
+export class OriginalReleaseComponent implements OnInit, OnDestroy {
 
   @Input() releasesForm: MovieSalesInfoControl['originalRelease'];
   @Input() countriesForm: MovieMainControl['originCountries'];
+  private sub: Subscription;
   media = staticModel.MEDIAS;
 
   constructor() { }
@@ -29,7 +31,7 @@ export class OriginalReleaseComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.originCountries.valueChanges.pipe(
+    this.sub = this.originCountries.valueChanges.pipe(
       startWith(this.originCountries.value),
     ).subscribe(countries => {
       countries.forEach((country: any, i: number) => {
@@ -37,6 +39,10 @@ export class OriginalReleaseComponent implements OnInit {
         this.originalRelease.setControl(i, control);
       });
     });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
   // Remove cannot be automated by valueChange or it would resul into bad UX (remove form when input is emptied)
