2e2e6a6ba578043f927a537622f7cb22cce543e8
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-16 16:25:59 +0100

    update festival prizes

--- libs/movie/src/lib/movie/form/festival-prizes/festival-prizes.module.ts
+++ libs/movie/src/lib/movie/form/festival-prizes/festival-prizes.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MovieFormFestivalPrizesComponent } from './festival-prizes.component';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -15,6 +16,7 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    FlexLayoutModule,
     // Material
     MatFormFieldModule,
     MatInputModule,
