243a45d705c2781c89d5fa35e0408da2c29bff44
hugo-hontans <hugohontans@gmail.com>
2020-01-17 13:38:46 +0100

    add bigQueryAnalyticsTable @env variable to ci

--- apps/backend-functions/src/environments/environment.ci.ts
+++ apps/backend-functions/src/environments/environment.ci.ts
@@ -4,7 +4,7 @@
 import * as functions from 'firebase-functions';
 
 import { algolia as algoliaClient } from '@env';
-export { backupBucket, relayer, firebase, appUrl, sentryDsn } from '@env';
+export { backupBucket, relayer, firebase, appUrl, sentryDsn, bigQueryAnalyticsTable } from '@env';
 
 export const sendgridAPIKey = functions.config().sendgrid.api_key;
 export const mnemonic = functions.config().relayer.mnemonic;
