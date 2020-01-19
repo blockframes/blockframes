e77e7bc27f5a9d9507eacfef43f6e75c4a57a464
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-19 18:25:10 +0100

    clean up

--- libs/movie/src/lib/distribution-deals/selection-map/selection-map.component.ts
+++ libs/movie/src/lib/distribution-deals/selection-map/selection-map.component.ts
@@ -1,16 +1,7 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, ChangeDetectionStrategy, ChangeDetectorRef, NgZone } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, ChangeDetectionStrategy, ChangeDetectorRef, NgZone, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map , geoJSON, StyleFunction, GeoJSON, Map, control, Control, Layer } from 'leaflet';
-
-const style: StyleFunction = (feature) => {
-  return  {
-    fillColor: '#ECEFF9',
-    weight: 1,
-    color: '#000000A0',
-    fillOpacity: 1
-  };
-};
-
+import { Movie } from '@blockframes/movie/movie+state';
+import { map , geoJSON, GeoJSON,  Layer } from 'leaflet';
 
 @Component({
   selector: 'distribution-deals-selection-map',
@@ -18,12 +9,14 @@ const style: StyleFunction = (feature) => {
   styleUrls: ['./selection-map.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SelectionMapComponent implements OnInit, AfterViewInit {
+export class SelectionMapComponent implements AfterViewInit {
 
-  @ViewChild('leaflet', { static: false }) leafletEl: ElementRef;
+  @Input() movie: Movie;
   private geojson: GeoJSON;
-  private selected: Layer;
+  private layer: Layer;
   public country: any;
+  
+  @ViewChild('leaflet', { static: false }) leafletEl: ElementRef;
 
   constructor(
     private http: HttpClient,
@@ -31,36 +24,42 @@ export class SelectionMapComponent implements OnInit, AfterViewInit {
     private ngZone: NgZone
   ) { }
 
-  async ngOnInit() {
-
-  }
-
   async ngAfterViewInit() {
     const world = map(this.leafletEl.nativeElement, { zoomSnap: 0.5, attributionControl: false }).setView([40, 40], 1.5);
     const countries = await this.http.get<GeoJSON.GeoJsonObject>('assets/maps/world.geo.json').toPromise();
     this.geojson = geoJSON(countries, {
-      style,
+      style: this.setStyle.bind(this),
       onEachFeature: this.addFeature.bind(this)
     });
     this.geojson.addTo(world);
+  }
+
+  /** Set the default style of this  */
+  private setStyle(feature: GeoJSON.Feature) {
+    return  {
+      fillColor: '#ECEFF9',
+      weight: 1,
+      color: '#000000A0',
+      fillOpacity: 1
+    };
   }
 
   private addFeature(feature: GeoJSON.Feature, layer: Layer): void {
     this.ngZone.runOutsideAngular(() => {
       layer.on({
         mouseover: ({ target }) => {
-          if (layer !== this.selected) {
+          if (layer !== this.layer) {
             target.setStyle({ fillColor: '#8788A3' });
           }
         },
         mouseout: () => {
-          if (layer !== this.selected) {
+          if (layer !== this.layer) {
             this.geojson.resetStyle(layer);
           }
         },
         click: ({ target }) => {
-          this.geojson.resetStyle(this.selected);
-          this.selected = layer;
+          this.geojson.resetStyle(this.layer);
+          this.layer = layer;
           this.country = feature.properties;
           this.cdr.markForCheck();
           target.setStyle({ fillColor: '#334AEC' });
