0543eafb54bf7a787067b5ccc7463d981bd514f5
DK <jenna_kim1@hotmail.com>
2020-01-21 13:25:04 +0100

    file-upload & promotional-links

--- apps/catalog/catalog/src/app/dashboard/movie-tunnel/media-file/media-file.component.ts
+++ apps/catalog/catalog/src/app/dashboard/movie-tunnel/media-file/media-file.component.ts
@@ -1,6 +1,6 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { MovieForm } from '@blockframes/movie/movie/form/movie.form';
-
+import { createImgRef } from "@blockframes/utils/image-uploader";
 @Component({
   selector: 'catalog-movie-tunnel-media-file',
   templateUrl: './media-file.component.html',
@@ -15,8 +15,8 @@ export class MediaFileComponent {
     return this.form.get('promotionalElements');
   }
 
-  importPDF(bytes: Uint8Array) {
-    console.log(bytes)
-   console.log(this.form.get('promotionalElements'))
+  importPDF(file, path: 'scenario' | 'presentation_deck') {
+    const url = createImgRef(file)
+    this.form.get('promotionalElements').get(path).get('media').patchValue(url);
   }
 }
