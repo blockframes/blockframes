7676615e5e4f311dccc3d6e3c45db3c310a8c2c4
hugo-hontans <hugohontans@gmail.com>
2020-01-21 19:09:22 +0100

    refactor movie display main

--- libs/movie/src/lib/movie/display/main/main.component.ts
+++ libs/movie/src/lib/movie/display/main/main.component.ts
@@ -1,19 +1,23 @@
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
-import { MovieMain, createMovieMain } from '../../+state';
-import { getLabelByCode } from '@blockframes/utils/static-model/staticModels';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { MovieMainForm } from '@blockframes/movie/movieform/main/main.form';
+import { MovieFestivalPrizesForm } from '@blockframes/movie/movieform/festival-prizes/festival-prizes.form';
+import { MovieSalesCastForm } from '@blockframes/movie/movieform/sales-cast/sales-cast.form';
+import { MovieSalesInfoForm } from '@blockframes/movie/movieform/sales-info/sales-info.form';
 
 @Component({
-  selector: '[main] movie-display-main',
+  selector: '[main] [festivalPrizes] [salesCast] [salesInfo] movie-display-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MovieDisplayMainComponent {
+  @Input() main: MovieMainForm;
+  @Input() festivalPrizes: MovieFestivalPrizesForm;
+  @Input() salesCast: MovieSalesCastForm;
+  @Input() salesInfo: MovieSalesInfoForm;
 
-  public data : MovieMain;
-  public getLabelByCode = getLabelByCode;
-  @Input() set main(main: Partial<MovieMain>) {
-    this.data = createMovieMain(main);
+  public get genres() {
+    return this.main.genres.controls.concat(this.main.customGenres.controls);
   }
 
   get shouldDisplayMainInformations()  {
@@ -24,5 +28,29 @@ export class MovieDisplayMainComponent {
       this.data.shortSynopsis ||
       this.data.totalRunTime ||
       this.data.status
+  }
+
+  public get totalRunTime() {
+    return this.main.get('totalRunTime');
+  }
+
+  public get status() {
+    return this.main.get('status');
+  }
+
+  public get workType() {
+    return this.main.get('workType');
+  }
+
+  public get storeType() {
+    return this.main.storeConfig.get('storeType');
+  }
+
+  public get original() {
+    return this.main.title.get('original');
+  }
+
+  public get international() {
+    return this.main.title.get('international');
   }
 }
