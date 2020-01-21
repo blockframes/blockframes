884e6ba350741f187c8fabc5c1615cd6d82aa9a9
hugo-hontans <hugohontans@gmail.com>
2020-01-21 10:52:32 +0100

    create tunnel summary component

---
+++ apps/catalog/catalog/src/app/dashboard/movie-tunnel/summary/summary.component.ts
@@ -0,0 +1,18 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { MovieForm } from '@blockframes/movie/movieform/movie.form';
+
+@Component({
+  selector: 'catalog-summary-tunnel',
+  templateUrl: './summary.component.html',
+  styleUrls: ['./summary.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+
+export class TunnelSummaryComponent {
+
+  constructor(private form: MovieForm) { }
+
+  get main() {
+    return this.form.get('main');
+  }
+}
