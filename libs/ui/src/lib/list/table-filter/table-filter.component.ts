7b3b1f987926aa8588587a02c9642e0d440240e4
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-21 15:23:59 +0100

    Create reusable table filter

---
+++ libs/ui/src/lib/list/table-filter/table-filter.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit, ChangeDetectionStrategy, ViewChild, Input, AfterViewInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+
+import { Observable } from 'rxjs';
+import { startWith } from 'rxjs/operators';
+
+@Component({
+  selector: 'blockframes-table-filter',
+  templateUrl: './table-filter.component.html',
+  styleUrls: ['./table-filter.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TableFilterComponent implements OnInit, AfterViewInit {
+
+  @Input() columns: Record<string, any>;
+  @Input() initialColumns: string[];
+  @Input() set source(data: any[]) {
+    this.dataSource = new MatTableDataSource(data);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  // Column & rows
+  displayedColumns$: Observable<string[]>;
+  dataSource: MatTableDataSource<any>;
+
+  // filters
+  columnFilter = new FormControl([]);
+
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.dataSource = new MatTableDataSource([]);
+    this.columnFilter.patchValue(this.initialColumns);
+    this.displayedColumns$ = this.columnFilter.valueChanges.pipe(
+      startWith(this.columnFilter.value)
+    );
+  }
+  
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  /** 
+   * Get the value of an item based on a path
+   * @example item = movie, key = 'budget.totalBudget'
+   */
+  getValue(item: any, key: string) {
+    const path = key.split('.');
+    for (let i=0; i < path.length; i++) {
+      item = item[path[i]];
+    };
+    return item;
+  }
+}
---
+++ libs/ui/src/lib/list/table-filter/table-filter.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { OverlayWidgetModule } from '../../overlay-widget/overlay-widget.module';
+
+import { TableFilterComponent } from './table-filter.component';
+
+// Material
+import { MatButtonModule } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+
+
+@NgModule({
+  declarations: [TableFilterComponent],
+  exports: [TableFilterComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    OverlayWidgetModule,
+    // Material
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    // Router
+  ]
+})
+export class TableFilterModule { }
