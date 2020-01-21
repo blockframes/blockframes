aa5fd819a6e9575906e4c47ba0778941c5130f5b
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-21 16:41:01 +0100

    Create list of movie

--- apps/catalog/catalog/src/app/dashboard/title/list/list.module.ts
+++ apps/catalog/catalog/src/app/dashboard/title/list/list.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { TableFilterModule } from '@blockframes/ui/list/table-filter/table-filter.module';
 
 import { TitleListComponent } from './list.component';
 
 // Material
 import { MatButtonModule } from '@angular/material';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 @NgModule({
   declarations: [TitleListComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    TableFilterModule,
+    // Material
     MatButtonModule,
+    MatTabsModule,
+    MatIconModule,
+    // Router
     RouterModule.forChild([{ path: '', component: TitleListComponent }])
   ]
 })
