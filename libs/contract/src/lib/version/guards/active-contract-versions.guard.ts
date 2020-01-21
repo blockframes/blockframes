f563650ecb916ae980044835a28e36209062608d
Yohann <ioa.nikas@gmail.com>
2020-01-21 10:29:12 +0100

    Change contract queries, and some renaming

--- libs/contract/src/lib/version/guards/active-contract-versions.guard.ts
+++ libs/contract/src/lib/version/guards/active-contract-versions.guard.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
-import { CollectionGuard, CollectionGuardConfig } from 'akita-ng-fire';
+import { CollectionGuard } from 'akita-ng-fire';
 import { ContractVersionService } from '../+state/contract-version.service';
 import { ContractVersionState } from '../+state/contract-version.store';
+import { ContractVersionQuery } from '../+state/contract-version.query';
 
 @Injectable({ providedIn: 'root' })
-@CollectionGuardConfig({ awaitSync: true })
 export class ActiveContractVersionsGuard extends CollectionGuard<ContractVersionState> {
-  constructor(service: ContractVersionService) {
+  constructor(service: ContractVersionService, private query: ContractVersionQuery) {
     super(service);
+  }
+
+  get awaitSync() {
+    return this.query.getCount() === 0;
+  }
+
+  sync() {
+    return this.service.syncCollection();
   }
 }
