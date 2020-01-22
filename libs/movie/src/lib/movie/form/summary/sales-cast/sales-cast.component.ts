18b2f93db180d169ce45415cacf0f827a178fdf4
hugo-hontans <hugohontans@gmail.com>
2020-01-22 11:54:25 +0100

    create sales-cast component

---
+++ libs/movie/src/lib/movie/form/summary/sales-cast/sales-cast.component.ts
@@ -0,0 +1,12 @@
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { MovieSalesCastForm } from '../../sales-cast/sales-cast.form';
+
+@Component({
+  selector: '[salesCast] movie-summary-sales-cast',
+  templateUrl: './sales-cast.component.html',
+  styleUrls: ['./sales-cast.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MovieSummarySalesCastComponent {
+  @Input() salesCast: MovieSalesCastForm;
+}
