0e71ac55547c5b54a913b996cb09e9976d00ad8f
hugo-hontans <hugohontans@gmail.com>
2020-01-22 12:50:40 +0100

    more precise form to country

--- libs/movie/src/lib/movie/form/summary/country/country.component.ts
+++ libs/movie/src/lib/movie/form/summary/country/country.component.ts
@@ -1,14 +1,15 @@
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 import { MovieSalesInfoForm } from '../../sales-info/sales-info.form';
-import { MovieMainForm } from '../../main/main.form';
+import { FormList } from '@blockframes/utils/form/forms/list.form';
+import { TerritoriesSlug } from '@blockframes/utils/static-model/types';
 
 @Component({
-  selector: '[main] [salesInfo] movie-summary-country',
+  selector: '[salesInfo] [originCountries] movie-summary-country',
   templateUrl: './country.component.html',
   styleUrls: ['./country.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MovieSummaryCountryComponent {
   @Input() salesInfo: MovieSalesInfoForm;
-  @Input() main: MovieMainForm;
+  @Input() originCountries: FormList<TerritoriesSlug>;
 }
