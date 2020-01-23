b2ec5028c201b10cfdd853549057f99416f60c68
hugo-hontans <hugohontans@gmail.com>
2020-01-23 17:57:55 +0100

    create new pipe to display missing and slug

--- libs/utils/src/lib/pipes/missing-slug.pipe.ts
+++ libs/utils/src/lib/pipes/missing-slug.pipe.ts
@@ -1,13 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { getLabelBySlug, Scope } from '../static-model/staticModels';
+import { getLabelByCode, Scope } from '../static-model/staticModels';
 
 @Pipe({
-  name: 'missingWithLabel'
+  name: 'missingWithSlug'
 })
 export class MissingSlugPipe implements PipeTransform {
 
   transform(control: FormControl, property: Scope,) {
-    return control.hasError('required') ? 'Mandatory' : control.value ? getLabelBySlug(property, control.value.trim().toLocaleLowerCase()) : 'Missing';
+    return control.hasError('required') ? 'Mandatory' : control.value ? getLabelByCode(property, control.value.trim().toLocaleLowerCase()) : 'Missing';
   }
 }
