06c36515ad15685e936654f2ffbf021d11b527d6
hugo-hontans <hugohontans@gmail.com>
2020-01-23 17:37:27 +0100

    create summary technical information component

--- apps/catalog/catalog/src/app/dashboard/movie-tunnel/summary/summary.module.ts
+++ apps/catalog/catalog/src/app/dashboard/movie-tunnel/summary/summary.module.ts
@@ -16,9 +16,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MovieSummaryCreditModule } from '@blockframes/movie/movieform/summary/credit/credit.module';
 import { MovieSummaryBudgetModule } from '@blockframes/movie/movieform/summary/budget/budget.module';
 import { MovieSummaryTechnicalInformationModule } from '@blockframes/movie/movieform/summary/technical-information/technical-information.module';
-import { MovieSummaryImageModule } from '@blockframes/movie/movieform/summary/image/image.module';
-import { MovieSummaryFileModule } from '@blockframes/movie/movieform/summary/file/file.module';
-import { MovieSummaryEvaluationModule } from '@blockframes/movie/movieform/summary/evaluation/evaluation.module';
 
 @NgModule({
   declarations: [TunnelSummaryComponent],
@@ -36,10 +33,6 @@ import { MovieSummaryEvaluationModule } from '@blockframes/movie/movieform/summa
     MovieSummaryCreditModule,
     MovieSummaryBudgetModule,
     MovieSummaryTechnicalInformationModule,
-    MovieSummaryImageModule,
-    MovieSummaryFileModule,
-    MovieSummaryEvaluationModule,
-    // Materials
     MatCardModule,
     MatIconModule,
     RouterModule.forChild([{ path: '', component: TunnelSummaryComponent }])
