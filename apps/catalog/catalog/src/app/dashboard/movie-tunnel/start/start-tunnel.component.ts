ea9416af000fda7b1d05c7da65184cff01c7c5bf
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-23 13:54:13 +0100

    Create entities on start tunnel

--- apps/catalog/catalog/src/app/dashboard/movie-tunnel/start/start-tunnel.component.ts
+++ apps/catalog/catalog/src/app/dashboard/movie-tunnel/start/start-tunnel.component.ts
@@ -1,6 +1,10 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { MovieService } from '@blockframes/movie';
 import { Router, ActivatedRoute } from '@angular/router';
+import { ContractService, createContractTitleDetail } from '@blockframes/contract/contract/+state';
+import { DistributionDealService } from '@blockframes/movie/distribution-deals/+state';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ContractVersionService } from '@blockframes/contract/version/+state/contract-version.service';
 
 const cardContents = [
   {
@@ -30,15 +34,44 @@ const cardContents = [
 
 export class StartTunnelComponent{
   public cardContents = cardContents;
+  public loading: boolean;
 
   constructor(
-    private service: MovieService,
+    private db: AngularFirestore,
+    private movieService: MovieService,
+    private contractService: ContractService,
+    private dealService: DistributionDealService,
+    private contractVersionService: ContractVersionService,
     private router: Router,
     private route: ActivatedRoute
   ) {}
 
   async begin() {
-    const movieId = await this.service.add({});
-    this.router.navigate([movieId], { relativeTo: this.route });
+    this.loading = true;
+    try {
+      const write = this.db.firestore.batch();
+      ///////////////
+      // Create Movie
+      const movieId = await this.movieService.add({}, { write });
+      // Create Contract
+      const contractId = await this.contractService.add({ titleIds: [movieId]}, { write });
+      // Create first Distribution Deal for this contract 
+      const dealId = await this.dealService.add({ contractId }, { write, params: { movieId } });
+      // Create Details for this movie on the first contract version
+      const details = createContractTitleDetail({ titleId: movieId, distributionDealIds: [dealId] });
+      const titles = { [movieId]: details };
+      const versionId = await this.contractVersionService.add({ id: '1', titles }, {
+        write,
+        params: { contractId }
+      });
+      console.log({ movieId, contractId, dealId, versionId })
+      ///////////////
+      write.commit();
+      this.loading = false;
+      this.router.navigate([movieId], { relativeTo: this.route });
+    } catch (err) {
+      this.loading = false;
+      console.error(err);
+    }
   }
 }
