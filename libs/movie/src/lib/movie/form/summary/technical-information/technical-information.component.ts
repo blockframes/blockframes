7dd37c1c7c58b52a89aa925d4cc95eaac5b3df86
hugo-hontans <hugohontans@gmail.com>
2020-01-24 11:40:15 +0100

    finish summary technical info component

--- libs/movie/src/lib/movie/form/summary/technical-information/technical-information.component.ts
+++ libs/movie/src/lib/movie/form/summary/technical-information/technical-information.component.ts
@@ -3,13 +3,12 @@ import { MovieSalesInfoForm } from '../../sales-info/sales-info.form';
 import { MovieVersionInfoForm } from '../../version-info/version-info.form';
 
 @Component({
-  selector: '[versionInfo] movie-summary-technical-information',
+  selector: '[salesInfo] [versionInfo] movie-summary-technical-information',
   templateUrl: './technical-information.component.html',
   styleUrls: ['./technical-information.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MovieSummaryTechnicalInformationComponent {
-  //@Input() salesInfo: MovieSalesInfoForm;
-  public salesInfo = new MovieSalesInfoForm({ color: 'b', format: '1_33', formatQuality: '4k', soundFormat: 'mono' });
+  @Input() salesInfo: MovieSalesInfoForm;
   @Input() versionInfo: MovieVersionInfoForm;
 }
