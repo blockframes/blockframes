9359da8f2d1d8f8eb1ee80226256ce2c12614c85
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-24 15:45:47 +0100

    Create and use patchAllValue

--- libs/utils/src/lib/form/forms/entity.form.ts
+++ libs/utils/src/lib/form/forms/entity.form.ts
@@ -13,4 +13,26 @@ export class FormEntity<C extends EntityControl<T>, T = any> extends FormGroup {
   get<K extends keyof C>(path: Extract<K, string>): C[K] {
     return super.get(path) as C[K];
   }
+
+  /**
+   * Use to apply patchAllValue to FormList
+   * @note this method is specific from FormList and is not part and Angular FormArray interface
+   */
+  patchAllValue(
+    value: Partial<T>,
+    options: {
+      onlySelf?: boolean;
+      emitEvent?: boolean;
+    } = {}
+  ) {
+    Object.keys(value).forEach(name => {
+      if (this.controls[name]) {
+        if (!!this.controls[name]['patchAllValue']) {
+          this.controls[name]['patchAllValue'](value[name], {onlySelf: true, emitEvent: options.emitEvent});
+        } else {
+          this.controls[name].patchValue(value[name], {onlySelf: true, emitEvent: options.emitEvent});
+        }
+      }
+    });
+  }
 }
