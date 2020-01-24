47b86360fbb3830c2ae0936e8b1765ad7799aeb7
DK <jenna_kim1@hotmail.com>
2020-01-24 14:33:49 +0100

    changed to ngx-apexcharts

--- apps/catalog/catalog/src/app/dashboard/title/sales/sales.component.ts
+++ apps/catalog/catalog/src/app/dashboard/title/sales/sales.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ViewChild } from '@angular/core';
 import { MovieAnalytics } from '@blockframes/movie/movie+state/movie.model';
 import { MovieService } from '@blockframes/movie/movie+state/movie.service';
 import { Observable } from 'rxjs';
 import { MovieQuery } from '@blockframes/movie/movie+state/movie.query';
-
+import { ChartComponent } from 'ng-apexcharts';
+import { ChartOptions } from './default-chart-options';
 @Component({
   selector: 'catalog-title-sales',
   templateUrl: './sales.component.html',
@@ -11,10 +12,117 @@ import { MovieQuery } from '@blockframes/movie/movie+state/movie.query';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TitleSalesComponent implements OnInit {
-
+  @ViewChild('chart', { static: false }) chart: ChartComponent;
+  public lineChartOptions: Partial<ChartOptions>;
+  public barChartOptions: Partial<ChartOptions>;
   public movieAnalytics: Observable<MovieAnalytics[]>;
 
-  constructor(private movieService: MovieService, private movieQuery: MovieQuery) { }
+  constructor(private movieService: MovieService, private movieQuery: MovieQuery) {    
+    this.lineChartOptions = {
+      chart: {
+        height: 150,
+        type: "line",
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: "smooth",
+        width: 2.5
+      },
+      grid: {
+        row: {
+          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5
+        }
+      },
+      xaxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep"
+        ]
+      }
+    };
+    this.barChartOptions = {
+      series: [
+        {
+          name: "Marine Sprite",
+          data: [44, 55, 41, 37, 22, 43, 21]
+        },
+        {
+          name: "Striking Calf",
+          data: [53, 32, 33, 52, 13, 43, 32]
+        },
+        {
+          name: "Tank Picture",
+          data: [12, 17, 11, 9, 15, 11, 20]
+        },
+        {
+          name: "Bucket Slope",
+          data: [9, 7, 5, 8, 6, 9, 4]
+        },
+        {
+          name: "Reborn Kid",
+          data: [25, 12, 19, 32, 25, 24, 10]
+        }
+      ],
+      chart: {
+        type: "bar",
+        height: 350,
+        stacked: true
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false
+        }
+      },
+      stroke: {
+        width: 1,
+        colors: ["#fff"]
+      },
+      title: {
+        text: "Fiction Books Sales"
+      },
+      xaxis: {
+        categories: [2008, 2009, 2010, 2011, 2012, 2013, 2014],
+        labels: {
+          formatter: function(val) {
+            return val + "K";
+          }
+        }
+      },
+      yaxis: {
+        title: {
+          text: undefined
+        }
+      },
+      tooltip: {
+        y: {
+          formatter: function(val) {
+            return val + "K";
+          }
+        }
+      },
+      fill: {
+        opacity: 1
+      },
+      legend: {
+        position: "top",
+        horizontalAlign: "left",
+        offsetX: 40
+      }
+    };
+  }
 
   ngOnInit() {
     this.movieAnalytics = this.movieService.getMovieAnalytics([this.movieQuery.getActiveId()]);
--- apps/catalog/catalog/src/app/dashboard/title/sales/sales.module.ts
+++ apps/catalog/catalog/src/app/dashboard/title/sales/sales.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { NgApexchartsModule } from "ng-apexcharts";
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { TitleSalesComponent } from './sales.component';
 
-
+// Material
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [TitleSalesComponent],
   imports: [
     CommonModule,
+    NgApexchartsModule,
+    FlexLayoutModule,
+    // Material
+    MatCardModule,
+    MatIconModule,
     RouterModule.forChild([{ path: '', component: TitleSalesComponent }])
   ]
 })
