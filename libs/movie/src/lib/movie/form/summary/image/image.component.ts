4a3cfb9d34d9f08ef5b28be6789ce7b4da9b581d
hugo-hontans <hugohontans@gmail.com>
2020-01-24 11:54:36 +0100

    create summary image component

---
+++ libs/movie/src/lib/movie/form/summary/image/image.component.ts
@@ -0,0 +1,10 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'movie-summary-image',
+  templateUrl: './image.component.html',
+  styleUrls: ['./image.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MovieSummaryImageComponent {
+}
