91dacf32be5949dbd78a6cb2d843468fcd7890a2
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-24 12:26:14 +0100

    improve routing

--- apps/catalog/catalog/src/app/dashboard/movie-tunnel/movie-tunnel.component.ts
+++ apps/catalog/catalog/src/app/dashboard/movie-tunnel/movie-tunnel.component.ts
@@ -1,33 +1,21 @@
 // Angular
 import { Component, ChangeDetectionStrategy, Host, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 
 // Akita
 import { RouterQuery } from '@datorama/akita-ng-router-store';
 
 // Blockframes
-import { MovieService } from '@blockframes/movie/movie/+state';
+import { MovieService, MovieQuery } from '@blockframes/movie/movie/+state';
 import { MovieForm } from '@blockframes/movie/movie/form/movie.form';
 
 // RxJs
-import { map } from 'rxjs/operators';
-import { Observable, Subscription } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { MatSnackBar } from '@angular/material';
 
 interface PageData {
   index: number;
   length: number;
-}
-
-function getPageData(url: string, array: string[][]): PageData {
-  const pageUrl = url.split('/').pop();
-  const panel = array.find(page => page.includes(pageUrl));
-  if (panel) {
-    const index = panel.indexOf(pageUrl) + 1;
-    const arrayLength = panel.length;
-    return { index: index, length: arrayLength };
-  } else {
-    return { index: 0, length: 0 };
-  }
 }
 
 const panels = [{
@@ -87,9 +75,31 @@ const panels = [{
 const allRoutes = panels.map(({ routes }) => routes.map(r => r.path));
 const allPath = allRoutes.flat();
 
+function getPageData(url: string, array: string[][]): PageData {
+  const pageUrl = url.split('/').pop();
+  const panel = array.find(page => page.includes(pageUrl));
+  if (panel) {
+    const index = panel.indexOf(pageUrl) + 1;
+    const arrayLength = panel.length;
+    return { index: index, length: arrayLength };
+  } else {
+    return { index: 0, length: 0 };
+  }
+}
+
+/**
+ * @description returns the next or previous page where the router should go to
+ * @param current current url
+ * @param arithmeticOperator plus or minus
+ */
+function getPage(url: string, arithmeticOperator: number): string {
+  const current = url.split('/').pop();
+  const i: number = allPath.indexOf(current) + arithmeticOperator;
+  return allPath[i];
+}
 
 @Component({
-  selector: 'catalog-layout',
+  selector: 'catalog-movie-tunnel',
   templateUrl: './movie-tunnel.component.html',
   styleUrls: ['./movie-tunnel.component.scss'],
   providers: [MovieForm],
@@ -97,64 +107,36 @@ const allPath = allRoutes.flat();
 })
 export class MovieTunnelComponent implements OnInit, OnDestroy {
   private sub: Subscription;
+  private url$ = this.routerQuery.select(({ state }) => state.url);
   public panels = panels;
-  public pageData$ = this.routerQuery
-    .select('state')
-    .pipe(map(({ url }) => getPageData(url, allRoutes)));
+  
+  public next$ = this.url$.pipe(map(url => getPage(url, 1)));
+  public previous$ = this.url$.pipe(map(url => getPage(url, -1)));
+  public pageData$ = this.url$.pipe(map(url => getPageData(url, allRoutes)));
 
-  public next$: Observable<string> = this.routerQuery.select('state').pipe(
-    map(state => {
-      const url = state.url.split('/').pop();
-      return this.getPage(url, 1);
-    })
-  );
-  public previous$: Observable<string> = this.routerQuery.select('state').pipe(
-    map(state => {
-      const url = state.url.split('/').pop();
-      return this.getPage(url, -1);
-    })
-  );
 
   constructor(
     @Host() private form: MovieForm,
     private service: MovieService,
-    private route: ActivatedRoute,
-    private routerQuery: RouterQuery
+    private query: MovieQuery,
+    private routerQuery: RouterQuery,
+    private snackBar: MatSnackBar
   ) {}
 
-  ngOnInit() {
-    this.sub = this.route.params.subscribe(async ({ movieId }: { movieId: string }) => {
-      const movie = await this.service.getValue(movieId) // Edit movie
-      this.form.patchValue(movie);
-    });
+  async ngOnInit() {
+    const movieId = this.query.getActiveId();
+    const movie = await this.service.getValue(movieId) // Edit movie
+    this.form.patchValue(movie);
   }
 
   ngOnDestroy() {
     this.sub.unsubscribe();
   }
 
-  /**
-   * @description returns the next or previous page where the router should go to
-   * @param current current url
-   * @param arithmeticOperator plus or minus
-   */
-  private getPage(current: string, arithmeticOperator: number): string {
-    const i: number = allPath.indexOf(current) + arithmeticOperator;
-    if (i < 0) {
-      return 'start';
-    }
-    return allPath[i];
-  }
-
-  // Should save movie, contract & deal
-  private save() {
-    // Movie
-    this.form.value.id
-      ? this.service.update(this.form.value)
-      : this.service.add(this.form.value);
-    // Contract
-
-    // Deal
-
+  // Should save movie
+  public async save() {
+    const movieId = this.query.getActiveId();
+    await this.service.update(movieId, this.form.value);
+    this.snackBar.open('Saved', 'close', { duration: 500 });
   }
 }
