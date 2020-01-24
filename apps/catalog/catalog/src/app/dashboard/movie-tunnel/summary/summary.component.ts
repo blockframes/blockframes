0f8334d6a1f1f5ed1221a25495c826b0ee1e695d
hugo-hontans <hugohontans@gmail.com>
2020-01-24 15:42:01 +0100

    summary links redirect to movieId

--- apps/catalog/catalog/src/app/dashboard/movie-tunnel/summary/summary.component.ts
+++ apps/catalog/catalog/src/app/dashboard/movie-tunnel/summary/summary.component.ts
@@ -1,14 +1,21 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { MovieForm } from '@blockframes/movie/movieform/movie.form';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'catalog-summary-tunnel',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-
 export class TunnelSummaryComponent {
+  constructor(
+    public form: MovieForm,
+    private route: ActivatedRoute
+  ) {}
 
-  constructor(public form: MovieForm) { }
+  public getPath(segment: string) {
+    const movieId = this.route.snapshot.params.movieId;
+    return `/c/o/dashboard/movie-tunnel/${movieId}/${segment}`;
+  }
 }
