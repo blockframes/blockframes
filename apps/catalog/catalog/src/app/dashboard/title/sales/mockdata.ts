ecae32d63b4e7c7974536ab29c1425dc397c430d
DK <jenna_kim1@hotmail.com>
2020-01-27 14:49:16 +0100

    string date

--- apps/catalog/catalog/src/app/dashboard/title/sales/mockdata.ts
+++ apps/catalog/catalog/src/app/dashboard/title/sales/mockdata.ts
@@ -1,82 +1,37 @@
 export const analyticsMockData = {
   addedToWishlist: [
     {
-      event_date: new Date('01 12 2019'),
+      event_date: '20200101',
       event_name: 'addded_to_wishlist', 
       hits: 5
     }, 
     {
-      event_date: new Date('02 12 2019'),
-      event_name: 'addded_to_wishlist', 
-      hits: 10 
-    }, 
-    {
-      event_date: new Date('03 12 2019'),
-      event_name: 'addded_to_wishlist', 
-      hits: 3 
-    }, 
-    {
-      event_date: new Date('04 12 2019'),
-      event_name: 'addded_to_wishlist', 
-      hits: 12 
-    }, 
-    {
-      event_date: new Date('05 12 2019'),
+      event_date: '20200102',
       event_name: 'addded_to_wishlist', 
       hits: 2 
     }, 
     {
-      event_date: new Date('06 12 2019'),
-      event_name: 'addded_to_wishlist', 
-      hits: 5 
-    }, 
-    {
-      event_date: new Date('07 12 2019'),
-      event_name: 'addded_to_wishlist', 
-      hits: 7 
-    }, 
-    {
-      event_date: new Date('08 12 2019'),
+      event_date: '20200103',
       event_name: 'addded_to_wishlist', 
       hits: 14
     },
     {
-      event_date: new Date('01 01 2020'),
+      event_date: '20200104',
       event_name: 'addded_to_wishlist', 
       hits: 5
     }, 
     {
-      event_date: new Date('01 02 2020'),
-      event_name: 'addded_to_wishlist', 
-      hits: 10 
-    }, 
-    {
-      event_date: new Date('01 03 2020'),
-      event_name: 'addded_to_wishlist', 
-      hits: 3 
-    }, 
-    {
-      event_date: new Date('01 04 2020'),
+      event_date: '20200105',
       event_name: 'addded_to_wishlist', 
       hits: 12 
-    }, 
+    },
     {
-      event_date: new Date('01 05 2020'),
-      event_name: 'addded_to_wishlist', 
-      hits: 2 
-    }, 
-    {
-      event_date: new Date('01 06 2020'),
-      event_name: 'addded_to_wishlist', 
-      hits: 5 
-    }, 
-    {
-      event_date: new Date('01 07 2020'),
+      event_date: '20200106',
       event_name: 'addded_to_wishlist', 
       hits: 7 
     }, 
     {
-      event_date: new Date('01 08 2020'),
+      event_date: '20200107',
       event_name: 'addded_to_wishlist', 
       hits: 14
     }
@@ -84,86 +39,36 @@ export const analyticsMockData = {
   movieViews: 
   [
     {
-      event_date: new Date('01 01 2020'),
+      event_date: '20200117',
       event_name: 'page_view', 
       hits: 40 
     }, 
     {
-      event_date: new Date('02 01 2020'),
+      event_date: '20200118',
       event_name: 'page_view', 
       hits: 10 
     }, 
     {
-      event_date: new Date('03 01 2020'),
+      event_date: '20200119',
       event_name: 'page_view', 
       hits: 30 
-    }, 
-    {
-      event_date: new Date('04 01 2020'),
-      event_name: 'page_view', 
-      hits: 10 
-    }, 
-    {
-      event_date: new Date('05 01 2020'),
-      event_name: 'page_view', 
-      hits: 20 
-    }, 
-    {
-      event_date: new Date('06 01 2020'),
-      event_name: 'page_view', 
-      hits: 5 
-    }, 
-    {
-      event_date: new Date('07 01 2020'),
-      event_name: 'page_view', 
-      hits: 10 
-    }, 
-    {
-      event_date: new Date('08 01 2020'),
-      event_name: 'page_view', 
-      hits: 15 
     }
   ],
   promoReelOpened: [ 
     {
-      event_date: new Date('01 01 2020'),
+      event_date: '20200107',
       event_name: 'page_view', 
       hits: 1 
     }, 
     {
-      event_date: new Date('02 01 2020'),
+      event_date: '20200108',
       event_name: 'page_view', 
       hits: 10 
     }, 
     {
-      event_date: new Date('03 01 2020'),
+      event_date: '20200109',
       event_name: 'page_view', 
       hits: 100
-    }, 
-    {
-      event_date: new Date('04 01 2020'),
-      event_name: 'page_view', 
-      hits: 50
-    }, 
-    {
-      event_date: new Date('05 01 2020'),
-      event_name: 'page_view', 
-      hits: 100
-    }, 
-    {
-      event_date: new Date('06 01 2020'),
-      event_name: 'page_view', 
-      hits: 50
-    }, 
-    {
-      event_date: new Date('07 01 2020'),
-      event_name: 'page_view', 
-      hits: 500
-    }, 
-    {
-      event_date: new Date('08 01 2020'),
-      event_name: 'page_view', 
-      hits: 250
     }
   ],
 }
