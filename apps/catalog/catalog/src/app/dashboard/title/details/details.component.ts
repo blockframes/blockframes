5c4ff67192dffacbb0ed3efa5b21b1bff5a81526
hugo-hontans <hugohontans@gmail.com>
2020-01-28 17:41:50 +0100

    update title details component

--- apps/catalog/catalog/src/app/dashboard/title/details/details.component.ts
+++ apps/catalog/catalog/src/app/dashboard/title/details/details.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieQuery } from '@blockframes/movie/movie+state/movie.query';
+import { MovieForm } from '@blockframes/movie/movieform/movie.form';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'catalog-title-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TitleDetailsComponent implements OnInit {
+export class TitleDetailsComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+  public form = new MovieForm();
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private movieQuery: MovieQuery) {}
 
   ngOnInit() {
+    this.subscription = this.movieQuery
+      .selectActive()
+      .subscribe(movie => this.form.patchValue(movie));
   }
 
+  public getPath(segment: string) {
+    const movieId = this.route.snapshot.params.movieId;
+    return `/c/o/dashboard/movie-tunnel/${movieId}/${segment}`;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
