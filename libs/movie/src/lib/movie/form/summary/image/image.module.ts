bb2bfd2a5df8af34d0c52a2eb3a0a23d0c335cb8
hugo-hontans <hugohontans@gmail.com>
2020-01-28 13:05:12 +0100

    remove missing pipes

--- libs/movie/src/lib/movie/form/summary/image/image.module.ts
+++ libs/movie/src/lib/movie/form/summary/image/image.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MissingPipeModule } from '@blockframes/utils/pipes/missing.module';
 import { MovieSummaryImageComponent } from './image.component';
 
 @NgModule({
   declarations: [MovieSummaryImageComponent],
   imports: [
     CommonModule,
-    MissingPipeModule
   ],
   exports: [MovieSummaryImageComponent]
 })
