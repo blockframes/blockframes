7b5d94d351a36304c4bd6d8e0e2d98100271303d
hugo-hontans <hugohontans@gmail.com>
2020-01-28 11:35:02 +0100

    use missing-control component for story and evaluation

--- libs/movie/src/lib/movie/form/summary/evaluation/evaluation.module.ts
+++ libs/movie/src/lib/movie/form/summary/evaluation/evaluation.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MissingSlugPipeModule } from '@blockframes/utils';
 import { MovieSummaryEvaluationComponent } from './evaluation.component';
+import { MissingControlModule } from '@blockframes/ui/missing-control/missing-control.module';
 
 @NgModule({
   declarations: [MovieSummaryEvaluationComponent],
   imports: [
     CommonModule,
     MissingSlugPipeModule,
+    MissingControlModule
   ],
   exports: [MovieSummaryEvaluationComponent]
 })
