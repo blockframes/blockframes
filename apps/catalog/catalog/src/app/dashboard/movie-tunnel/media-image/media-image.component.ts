999e52c99b5f70b57a9256446afaf3b3697208d7
DK <jenna_kim1@hotmail.com>
2020-01-28 13:11:40 +0100

    resolve #1666 update image&file path

--- apps/catalog/catalog/src/app/dashboard/movie-tunnel/media-image/media-image.component.ts
+++ apps/catalog/catalog/src/app/dashboard/movie-tunnel/media-image/media-image.component.ts
@@ -1,5 +1,6 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { MovieForm } from '@blockframes/movie/movie/form/movie.form';
+import { MovieQuery } from '@blockframes/movie/movie+state/movie.query';
 @Component({
   selector: 'catalog-tunnel-media-image',
   templateUrl: './media-image.component.html',
@@ -8,7 +9,15 @@ import { MovieForm } from '@blockframes/movie/movie/form/movie.form';
 })
 export class MediaImageComponent {
   
-  constructor(private form: MovieForm) { }
+  constructor(
+    private form: MovieForm,
+    private movieQuery: MovieQuery
+    ) {}
+
+  public movie = this.movieQuery.getActive();
+  public bannerPath = `movie/${this.movie.id}/Banner`;
+  public posterPath = `movie/${this.movie.id}/Poster`;
+  public stillPath = `movie/${this.movie.id}/Still`;
 
   get promotionalElements() {
     return this.form.get('promotionalElements')
