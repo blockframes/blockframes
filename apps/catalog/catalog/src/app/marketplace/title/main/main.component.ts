30db9287e8e66ef9ad69761204642202afb3fc61
DK <jenna_kim1@hotmail.com>
2020-01-31 15:17:44 +0100

    update for review comment & fix maintenance

--- apps/catalog/catalog/src/app/marketplace/title/main/main.component.ts
+++ apps/catalog/catalog/src/app/marketplace/title/main/main.component.ts
@@ -1,7 +1,5 @@
-import { Component, ChangeDetectionStrategy, HostBinding } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { MovieQuery, Movie } from '@blockframes/movie';
-import { Observable } from 'rxjs';
-import { getLabelBySlug } from '@blockframes/utils/static-model/staticModels';
 
 @Component({
   selector: 'catalog-movie-main',
@@ -10,14 +8,12 @@ import { getLabelBySlug } from '@blockframes/utils/static-model/staticModels';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MarketplaceMovieMainComponent {
-  @HostBinding('attr.page-id') pageId = 'catalog-movie-main';
   public movie$ = this.movieQuery.selectActive();
   public loading$ = this.movieQuery.selectLoading();
-  constructor(private movieQuery: MovieQuery,) { }
+  constructor(private movieQuery: MovieQuery) { }
 
   public hasStory({ story, promotionalDescription }: Movie): boolean {
-    if(story.synopsis || promotionalDescription.keywords.length > 0 || promotionalDescription.keyAssets)
-    return true;
+    return !!(story.synopsis || promotionalDescription.keywords.length > 0 || promotionalDescription.keyAssets)
   }
 
   public getPrize(prize) {
@@ -26,31 +22,27 @@ export class MarketplaceMovieMainComponent {
   
   // TODO#1658 Update LANGUAGES static model to be RFC-5646 compliant
   public getStakeholder(movie: Movie, role: string) {
-    const array = movie.main.stakeholders[role];
-    return array.map(e => {
-      if(e.countries && e.countries.length > 0) {
-        return `${e.displayName} (${e.countries})`;
-      }
-      return e.displayName;
+    return movie.main.stakeholders[role].map(stakeholder => {
+      return (stakeholder.countries && !!stakeholder.countries.length)
+        ? `${stakeholder.displayName} (${stakeholder.countries})`
+        :  stakeholder.displayName;
     }).join(', ');
   }
 
   public getSalesCast(movie: Movie, role: string) {
-    return movie.salesCast[role].map(e => {
-      if(e.role && e.role.length > 0) {
-        return `${e.firstName} ${e.lastName} (${e.role})`;
-      }
-      return `${e.firstName} ${e.lastName}`;
-    }).join(', ');
+    return movie.salesCast[role].map(cast => {
+      return (cast.role && !! cast.role.length)
+      ? `${cast.firstName} ${cast.lastName} (${cast.role})`
+      : `${cast.firstName} ${cast.lastName}`
+    })
   }
 
   public hasBudget({ budget, salesInfo, movieReview}: Movie): boolean {
-    if(budget.estimatedBudget ||
+    return !!(budget.estimatedBudget ||
        budget.boxOffice.length > 0 ||
        salesInfo.certifications.length > 0||
        salesInfo.rating.length > 0||
        movieReview.length > 0)
-       return true;
   }
 
   public budgetRange({ from, to }) {
