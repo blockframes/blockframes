f20a4034aa72f7faad3ba9cb3f0e81fcbd696ed1
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-01-31 19:42:23 +0100

    update signin/signup

--- libs/ui/src/lib/form/index.ts
+++ libs/ui/src/lib/form/index.ts
@@ -1,3 +1,4 @@
 export * from './country/country.module';
 export * from './language/language.module';
 export * from './display-name/display-name.module';
+export * from './password-confirm/password-confirm.module';
