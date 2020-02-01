f8942d9463c0b902d60eab019f9b30aa421a8b45
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-02-01 18:29:42 +0100

    Create search widget

--- libs/ui/src/lib/overlay-widget/overlay-widget.directive.ts
+++ libs/ui/src/lib/overlay-widget/overlay-widget.directive.ts
@@ -3,50 +3,88 @@ import { Overlay, OverlayRef } from '@angular/cdk/overlay';
 import { TemplatePortal } from '@angular/cdk/portal';
 import { OverlayWidgetComponent } from './overlay-widget.component';
 
-@Directive({
-  selector: "[widgetTarget]"
-})
-
-export class OverlayWidgetDirective implements OnDestroy{
+export class OverlayWidgetBase implements OnDestroy{
   private overlayRef: OverlayRef;
   private widget: TemplatePortal;
+  private opened = false;
 
   @Input() widgetTarget: OverlayWidgetComponent;
-  @HostListener('click')
-    open() {
-      if (!this.overlayRef) {
-        const positionStrategy = this.overlay
-        .position()
-        .flexibleConnectedTo(this._elementRef)
-        .withPositions([{
-          originX: 'start',
-          originY: 'bottom',
-          overlayX: 'start',
-          overlayY: 'top'
-        }]);
-        this.overlayRef = this.overlay.create({
-          hasBackdrop: true,
-          backdropClass: 'cdk-overlay-transparent-backdrop',
-          positionStrategy,
-        });
-        this.widget = new TemplatePortal(this.widgetTarget.ref, this.viewContainerRef);
-        this.overlayRef.backdropClick().subscribe(() => this.overlayRef.detach());
-      }
+  @HostListener('click') onclick() {
+    this.open();
+  }
 
-      this.overlayRef.attach(this.widget);
+  constructor(
+    private overlay : Overlay,
+    private viewContainerRef: ViewContainerRef,
+    private _elementRef: ElementRef,
+  ){}
+
+  // Destroy the reference from the DOM if it exists and clean up overlayRef
+  ngOnDestroy() {
+    if (this.overlayRef) {
+      this.overlayRef.dispose();
+      delete this.overlayRef;
     }
-constructor(
-  private overlay : Overlay,
-  private viewContainerRef: ViewContainerRef,
-  private _elementRef: ElementRef,
-){}
+  }
+  open() {
+    if (this.opened) {
+      return;
+    }
+    this.opened = true;
+    if (!this.overlayRef) {
+      const positionStrategy = this.overlay
+      .position()
+      .flexibleConnectedTo(this._elementRef)
+      .withPositions([{
+        originX: 'start',
+        originY: 'bottom',
+        overlayX: 'start',
+        overlayY: 'top'
+      }]);
+      this.overlayRef = this.overlay.create({
+        hasBackdrop: true,
+        backdropClass: 'cdk-overlay-transparent-backdrop',
+        positionStrategy,
+      });
+      this.widget = new TemplatePortal(this.widgetTarget.ref, this.viewContainerRef);
+      this.overlayRef.backdropClick().subscribe(() => this.close());
+    }
+    this.overlayRef.attach(this.widget);
+  }
 
-// Destroy the reference from the DOM if it exists and clean up overlayRef
-ngOnDestroy() {
-  if (this.overlayRef) {
-    this.overlayRef.dispose();
-    delete this.overlayRef;
+  close() {
+    if (this.overlayRef && this.opened) {
+      this.opened = false;
+      this.overlayRef.detach();
+    }
   }
 }
 
+// OVERLAY TRIGGERED ON CLICK
+@Directive({ selector: "a[widgetTarget], button[widgetTarget]" })
+export class OverlayWidgetButtonDirective extends OverlayWidgetBase {
+
+  @HostListener('click') onclick() {
+    this.open();
+  }
+
+  constructor(overlay : Overlay, viewContainerRef: ViewContainerRef, el: ElementRef) {
+    super(overlay, viewContainerRef, el);
+  }
+}
+
+// OVERLAY TRIGGERED ON FOCUS / CLOSED ON BLUT
+@Directive({ selector: "input[widgetTarget]" })
+export class OverlayWidgetInputDirective extends OverlayWidgetBase {
+
+  @HostListener('focus') onfocus() {
+    this.open();
+  }
+  @HostListener('blur') onblur() {
+    this.open();
+  }
+
+  constructor(overlay : Overlay, viewContainerRef: ViewContainerRef, el: ElementRef) {
+    super(overlay, viewContainerRef, el);
+  }
 }
