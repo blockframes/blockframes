cd139a27b921ab85bc67f955be3b6fcdfddfc45c
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-02-02 19:01:56 +0100

    create reusable map

--- libs/movie/src/lib/distribution-deals/selection-map/selection-map.component.ts
+++ libs/movie/src/lib/distribution-deals/selection-map/selection-map.component.ts
@@ -3,6 +3,10 @@ import { HttpClient } from '@angular/common/http';
 import { Movie } from '@blockframes/movie/movie+state';
 import { map, geoJSON, GeoJSON, Layer } from 'leaflet';
 import { getTerritorySlugFromGeoJson, getTerritoryLabelFromGeoJson } from '@blockframes/utils/static-model/territories-ISO-3166';
+import { ISO3166TERRITORIES } from '@blockframes/utils/static-model/territories-ISO-3166';
+import { BehaviorSubject } from 'rxjs';
+
+const territories = ISO3166TERRITORIES.map(t => t['iso_a3']);
 
 @Component({
   selector: 'distribution-deals-selection-map',
@@ -10,63 +14,18 @@ import { getTerritorySlugFromGeoJson, getTerritoryLabelFromGeoJson } from '@bloc
   styleUrls: ['./selection-map.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SelectionMapComponent implements AfterViewInit {
+export class SelectionMapComponent {
 
   @Input() movie: Movie;
-  private geojson: GeoJSON;
-  private layer: Layer;
   public country: any;
+  public countries$ = new BehaviorSubject([]);
 
-  @ViewChild('leaflet', { static: false }) leafletEl: ElementRef;
 
-  constructor(
-    private http: HttpClient,
-    private cdr: ChangeDetectorRef,
-    private ngZone: NgZone
-  ) { }
+  constructor() { }
 
-  async ngAfterViewInit() {
-    const world = map(this.leafletEl.nativeElement, { zoomSnap: 0.5, attributionControl: false }).setView([40, 40], 1.5);
-    const countries = await this.http.get<GeoJSON.GeoJsonObject>('assets/maps/world.geo.json').toPromise();
-    this.geojson = geoJSON(countries, {
-      style: this.setStyle.bind(this),
-      onEachFeature: this.addFeature.bind(this)
-    });
-    this.geojson.addTo(world);
-  }
-
-  /** Set the default style of this  */
-  private setStyle(feature: GeoJSON.Feature) {
-    return {
-      fillColor: '#ECEFF9',
-      weight: 1,
-      color: '#000000A0',
-      fillOpacity: 1
-    };
-  }
-
-  private addFeature(feature: GeoJSON.Feature, layer: Layer): void {
-    this.ngZone.runOutsideAngular(() => {
-      layer.on({
-        mouseover: ({ target }) => {
-          if (layer !== this.layer) {
-            target.setStyle({ fillColor: '#8788A3' });
-          }
-        },
-        mouseout: () => {
-          if (layer !== this.layer) {
-            this.geojson.resetStyle(layer);
-          }
-        },
-        click: ({ target }) => {
-          this.geojson.resetStyle(this.layer);
-          this.layer = layer;
-          this.country = feature.properties;
-          this.cdr.markForCheck();
-          target.setStyle({ fillColor: '#334AEC' });
-        }
-      });
-    })
+  public select(e) {
+    this.countries$.next([...this.countries$.getValue(), e['iso_a3'] ]);
+    console.log(this.countries$.getValue());
   }
 
   public getTerritorySlugFromGeoJson(code: string) {
