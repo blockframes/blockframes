33743f57a512115a94ba857391339b3abe3b5cdd
Yohann <ioa.nikas@gmail.com>
2020-02-03 09:37:44 +0100

    Add movie contract list in avails

--- apps/catalog/catalog/src/app/dashboard/title/avails/avails.module.ts
+++ apps/catalog/catalog/src/app/dashboard/title/avails/avails.module.ts
@@ -5,12 +5,14 @@ import { RouterModule } from '@angular/router';
 import { TitleAvailsComponent } from './avails.component';
 
 import { DistributionDealSelectionMapModule } from '@blockframes/movie/distribution-deals/selection-map/selection-map.module';
+import { ContractListModule } from '@blockframes/contract/contract/list/contract-list.module';
 
 @NgModule({
   declarations: [TitleAvailsComponent],
   imports: [
     CommonModule,
     DistributionDealSelectionMapModule,
+    ContractListModule,
     RouterModule.forChild([{ path: '', component: TitleAvailsComponent }])
   ]
 })
