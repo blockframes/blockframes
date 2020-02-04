7d43743081e782577802c9cd72f6a9323dd14852
Laurent Senta <laurent.senta@gmail.com>
2020-02-04 15:11:37 +0100

    utils: show snackbar in error logger

--- libs/utils/src/lib/error-logger.module.ts
+++ libs/utils/src/lib/error-logger.module.ts
@@ -5,15 +5,20 @@
  * This ErrorLoggerHandler is a hack to make sure we are actually logging the error.
  */
 import { ErrorHandler, Injectable, NgModule } from '@angular/core';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Injectable()
 export class ErrorLoggerHandler implements ErrorHandler {
+  constructor(private snackBar: MatSnackBar) {}
+
   handleError(error) {
     console.error(error);
+    this.snackBar.open(`${error}`.substring(0, 100));
   }
 }
 
 @NgModule({
+  imports: [MatSnackBarModule],
   providers: [
     {
       provide: ErrorHandler,
