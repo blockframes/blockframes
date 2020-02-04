5831df1f277099faa48c30cf07192a623ae4247f
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-02-04 14:08:38 +0100

    quick patch to change color and tags

--- libs/movie/src/lib/distribution-deals/selection-map/selection-map.component.ts
+++ libs/movie/src/lib/distribution-deals/selection-map/selection-map.component.ts
@@ -1,10 +1,11 @@
 import { Component, AfterViewInit, ViewChild, ElementRef, ChangeDetectionStrategy, ChangeDetectorRef, NgZone, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Movie } from '@blockframes/movie/movie+state';
-import { map, geoJSON, GeoJSON, Layer } from 'leaflet';
+import { geoJSON, GeoJSON, Layer } from 'leaflet';
 import { getTerritorySlugFromGeoJson, getTerritoryLabelFromGeoJson } from '@blockframes/utils/static-model/territories-ISO-3166';
 import { ISO3166TERRITORIES } from '@blockframes/utils/static-model/territories-ISO-3166';
 import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 const territories = ISO3166TERRITORIES.map(t => t['iso_a3']);
 
@@ -19,19 +20,23 @@ export class SelectionMapComponent {
   @Input() movie: Movie;
   // Selected country on the map
   private country = new BehaviorSubject('');
-  public country$ = this.country.asObservable().pipe(
+  public country$ = this.country.asObservable();
+  // Countries you want to display in blue on the map
+  public countries$ = new BehaviorSubject(['RUS', 'ZAF']);
+  public contract$ = this.country$.pipe(
+    map(tag => getTerritorySlugFromGeoJson(tag))
     // map(country => IMPLEMENT MAPPING LOGIC HERE TO GET THE RIGHT CONTENT)
   );
-  // Countries you want to display in blue on the map
-  public countries$ = new BehaviorSubject([]);
 
-
-  constructor() { }
+  constructor(private cdr: ChangeDetectorRef) { }
 
   /** Whenever you click on a country */
   public select(e) {
-    const territory = getTerritorySlugFromGeoJson(e['iso_a3']);
-    this.country.next(territory);
+    this.country.next(e['iso_a3']);
+  }
+
+  public tackByTag(tag) {
+    return tag;
   }
 
 }
