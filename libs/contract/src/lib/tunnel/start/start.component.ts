5ae8360cac05f8df11330124d44e5e52af23c2e3
maxstgt <mfritz@cascade8.com>
2020-02-04 11:57:48 +0100

    added change detection

--- libs/contract/src/lib/tunnel/start/start.component.ts
+++ libs/contract/src/lib/tunnel/start/start.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'contract-tunnel-start',
   templateUrl: './start.component.html',
-  styleUrls: ['./start.component.scss']
+  styleUrls: ['./start.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TunnelStartComponent {}
