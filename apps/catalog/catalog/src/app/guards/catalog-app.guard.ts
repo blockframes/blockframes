40fc3c74cb3ae6c0190523cb3a40e6d50bcde191
hugo-hontans <hugohontans@gmail.com>
2020-02-04 15:53:06 +0100

    create catalog app guard

---
+++ apps/catalog/catalog/src/app/guards/catalog-app.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { OrganizationService } from '@blockframes/organization/+state/organization.service';
+import { OrganizationQuery } from '@blockframes/organization/+state/organization.query';
+
+@Injectable({ providedIn: 'root' })
+export class CatalogAppGuard implements CanActivate {
+  constructor(
+    protected service: OrganizationService,
+    protected router: Router,
+    private query: OrganizationQuery
+  ) { }
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const isMarketplace = state.url.split('/').includes('marketplace');
+    const org = this.query.getActive();
+    if (isMarketplace) {
+      return org.appAccess.marketplace ? true : this.router.parseUrl('c/o/dashboard');
+    } else {
+      return org.appAccess.dashboard ? true : this.router.parseUrl('c/o/marketplace');
+    }
+  }
+}
