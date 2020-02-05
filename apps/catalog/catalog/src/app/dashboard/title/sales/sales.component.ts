d9489bece094dc97f271de5a82747928cd7ac65a
DK <jenna_kim1@hotmail.com>
2020-02-05 10:59:29 +0100

    created movie-analytics-chart

--- apps/catalog/catalog/src/app/dashboard/title/sales/sales.component.ts
+++ apps/catalog/catalog/src/app/dashboard/title/sales/sales.component.ts
@@ -1,28 +1,10 @@
-import { Component, OnInit, ChangeDetectionStrategy, ViewChild } from '@angular/core';
-import { MovieAnalytics } from '@blockframes/movie/movie+state/movie.model';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { MovieAnalytics } from '@blockframes/movie/movie+state/movie.firestore';
 import { MovieService } from '@blockframes/movie/movie+state/movie.service';
 import { MovieQuery } from '@blockframes/movie/movie+state/movie.query';
 import { Observable } from 'rxjs';
-import { ChartComponent } from 'ng-apexcharts';
-import { ChartOptions, lineChartOptions } from './default-chart-options';
 
-const lineCharts = [
-  {
-    label: 'movieViews',
-    title: 'Page views',
-    icon: 'eye'
-  },
-  {
-    label: 'addedToWishlist',
-    title: 'Adds to Wishlist',
-    icon: 'mouse_pointer'
-  },
-  {
-    label: 'promoReelOpened',
-    title: 'Clicks on promotional elements',
-    icon: 'specific_delivery_list'
-  }
-]
+const eventList = ['movieViews', 'addedToWishlist', 'promoReelOpened']
 
 @Component({
   selector: 'catalog-title-sales',
@@ -31,52 +13,12 @@ const lineCharts = [
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TitleSalesComponent implements OnInit {
-  @ViewChild('chart', { static: false }) chart: ChartComponent;
-  public lineChartOptions: Partial<ChartOptions>;
-  public barChartOptions: Partial<ChartOptions>;
   public movieAnalytics$: Observable<MovieAnalytics[]>;
-  lineCharts = lineCharts;
+  public eventList = eventList;
   
-  constructor(private movieService: MovieService, private movieQuery: MovieQuery) {   
-    this.lineChartOptions = lineChartOptions;
-  }
+  constructor(private movieService: MovieService, private movieQuery: MovieQuery) {}
 
   ngOnInit() {
     this.movieAnalytics$ = this.movieService.getMovieAnalytics([this.movieQuery.getActiveId()]);
   }
-
-  populateData(data: object, name: string, key: string, period: string):number[] {
-    const numArray = data[0][name][period].map(d => d[key]);
-    return numArray;
-  }
-
-  getLineChartSeries(data: object, name: string) {
-    return [{name, data: this.populateData(data, name, 'hits', 'current')}];
-  }
-
-  getLineChartXaxis(data: object, name: string) {
-    return {
-      categories:  this.populateData(data, name, 'event_date', 'current'), 
-      labels: {show: false},  
-      axisBorder: {show: false},  
-      axisTicks: {show: false}
-    };
-  }
-
-  sum(array: number[]): number {
-    return array.reduce((p, c) => p + c, 0);
-  }
-
-  calculatePercentage(data: object, name: string): number {
-    const current = this.sum(this.populateData(data, name, 'hits', 'current'));
-    const past = this.sum(this.populateData(data, name, 'hits', 'past'));
-    if (current && past && (current > past)) {
-      return (current - past) / past * 100
-    } else if (past > current) {
-      return - (past - current) / past * 100
-    } else {
-      return 0
-    }
-  }
-
 }
---
+++ libs/movie/src/lib/movie/components/movie-analytics-chart/mockData.ts
@@ -0,0 +1,65 @@
+export const analyticsMockData = [
+  { 
+  "movieId": "P5j0GhuqNPMwQehMtlV3", 
+  "addedToWishlist": 
+    { "current":  [ 
+        { 
+          "event_name": "added_to_wishlist", 
+          "hits": 18, 
+          "event_date": "20200203", 
+          "movieId": "P5j0GhuqNPMwQehMtlV3" 
+        },
+        { 
+          "event_name": "added_to_wishlist", 
+          "hits": 12, 
+          "event_date": "20200204", 
+          "movieId": "P5j0GhuqNPMwQehMtlV3" 
+        } ,
+        { 
+          "event_name": "added_to_wishlist", 
+          "hits": 17, 
+          "event_date": "20200205", 
+          "movieId": "P5j0GhuqNPMwQehMtlV3" 
+        } ,
+        { 
+          "event_name": "added_to_wishlist", 
+          "hits": 3, 
+          "event_date": "20200206", 
+          "movieId": "P5j0GhuqNPMwQehMtlV3" 
+        } 
+      ], 
+    "past": [
+      { 
+        "event_name": "added_to_wishlist", 
+        "hits": 15, 
+        "event_date": "20200103", 
+        "movieId": "P5j0GhuqNPMwQehMtlV3" 
+      } ,
+      { 
+        "event_name": "added_to_wishlist", 
+        "hits": 10, 
+        "event_date": "20200104", 
+        "movieId": "P5j0GhuqNPMwQehMtlV3" 
+      } 
+    ] }, 
+    "promoReelOpened": 
+      { "current": [], "past": [] }, 
+      "movieViews": { 
+        "current": [ 
+          { "event_name": "page_view", 
+          "hits": 2, 
+          "event_date": "20200203", 
+          "movieId": "P5j0GhuqNPMwQehMtlV3" 
+        },
+        { "event_name": "page_view", 
+        "hits": 10, 
+        "event_date": "20200204", 
+        "movieId": "P5j0GhuqNPMwQehMtlV3" 
+      } 
+      ], 
+      "past": [
+
+      ] 
+    } 
+  } 
+] 
