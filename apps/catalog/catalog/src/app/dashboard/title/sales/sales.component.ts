10459b5e19c8487d2a8882876de42f9bbcec9bae
DK <jenna_kim1@hotmail.com>
2020-02-07 16:04:56 +0100

    sales revenu in sales

--- apps/catalog/catalog/src/app/dashboard/title/sales/sales.component.ts
+++ apps/catalog/catalog/src/app/dashboard/title/sales/sales.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { MovieAnalytics } from '@blockframes/movie/movie/+state/movie.firestore';
 import { MovieService } from '@blockframes/movie/movie/+state/movie.service';
 import { MovieQuery } from '@blockframes/movie/movie/+state/movie.query';
-import { Observable } from 'rxjs';
+import { getMovieReceipt } from '@blockframes/movie/movie/+state/movie.model';
+import { Observable, Subscription } from 'rxjs';
+import { Contract } from '@blockframes/contract/contract/+state/contract.model';
+import { getContractLastVersion } from '@blockframes/contract/version/+state/contract-version.model';
+import { ContractQuery } from '@blockframes/contract/contract/+state';
+import { map } from 'rxjs/operators';
 
 const eventList = ['movieViews', 'addedToWishlist', 'promoReelOpened'];
 
@@ -15,11 +20,26 @@ const eventList = ['movieViews', 'addedToWishlist', 'promoReelOpened'];
 export class TitleSalesComponent implements OnInit {
   public movieAnalytics$: Observable<MovieAnalytics[]>;
   public eventList = eventList;
+  public contracts: Contract[]
+  public sales: number;
+  public receipts: number;
   
-  constructor(private movieService: MovieService, private movieQuery: MovieQuery) {}
-
+  constructor(
+    private movieService: MovieService, 
+    private movieQuery: MovieQuery,
+    private contractQuery: ContractQuery
+  ) {}
+  
   ngOnInit() {
     const movieId = this.movieQuery.getActiveId();
     this.movieAnalytics$ = this.movieService.getMovieAnalytics([movieId]);
+    let ownContracts = []
+    this.contractQuery.selectAll().pipe(
+      map(movies => movies.map(movie => {
+        ownContracts = this.contracts.filter(c => getContractLastVersion(c).titles[movie.id]);
+      }))
+      )
+     this.receipts = getMovieReceipt(ownContracts, movieId);
+     this.sales = ownContracts.length;
   }
 }
