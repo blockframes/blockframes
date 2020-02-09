e3dfd524ec072774badea652ec7f1c173306d5e6
DK <jenna_kim1@hotmail.com>
2020-02-09 11:24:52 +0100

    address inside organization form

--- libs/organization/src/lib/forms/organization-form/organization-form.module.ts
+++ libs/organization/src/lib/forms/organization-form/organization-form.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { OrganizationFormAddressModule } from '@blockframes/organization/forms/organization-form-address/organization-form-address.module';
 
 // Modules
 import { CropperModule } from '@blockframes/ui/media/cropper/cropper.module';
@@ -21,6 +22,7 @@ import { OrganizationFormComponent } from './organization-form.component';
     ReactiveFormsModule,
     FlexLayoutModule,
     CropperModule,
+    OrganizationFormAddressModule,
 
     // Material
     MatFormFieldModule,
