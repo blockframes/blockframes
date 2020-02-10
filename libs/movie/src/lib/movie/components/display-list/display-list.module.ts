105b9027b250299402b2466b1b7214bb7811d1f6
Yohann <ioa.nikas@gmail.com>
2020-02-10 14:06:29 +0100

    Fix list view in line-up page

--- libs/movie/src/lib/movie/components/display-list/display-list.module.ts
+++ libs/movie/src/lib/movie/components/display-list/display-list.module.ts
@@ -1,12 +1,14 @@
 import { MovieDisplayListComponent } from './display-list.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ImageReferenceModule } from '@blockframes/ui/media/image-reference/image-reference.module';
+
 // Materials
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [CommonModule, MatTableModule, MatIconModule],
+  imports: [CommonModule, ImageReferenceModule, MatTableModule, MatIconModule],
   declarations: [MovieDisplayListComponent],
   exports: [MovieDisplayListComponent]
 })
