c5d64de365f48791362b7a8f95f44f80f883e1c4
DK <jenna_kim1@hotmail.com>
2020-02-10 12:24:11 +0100

    display user position

--- libs/organization/src/lib/pages/view/view.component.ts
+++ libs/organization/src/lib/pages/view/view.component.ts
@@ -5,6 +5,8 @@ import { OrganizationQuery } from '@blockframes/organization/+state/organization
 import { TunnelService } from '@blockframes/ui/tunnel/tunnel.service';
 import { OrganizationService } from '@blockframes/organization/+state/organization.service';
 import { OrganizationForm } from '@blockframes/organization/forms/organization.form';
+import { User } from '@blockframes/auth/+state/auth.store';
+import { AuthQuery } from '@blockframes/auth/+state/auth.query';
 
 const   navLinks = [{
   path: 'org',
@@ -25,15 +27,17 @@ export class OrganizationViewComponent implements OnInit {
   public previousPage: string;
   public navLinks = navLinks;
   public organizationForm = new OrganizationForm(this.service);
-
+  public user$: Observable<User>;
 
   constructor(
     private query: OrganizationQuery,
     private tunnelService: TunnelService,
     private service: OrganizationService,
+    private authQuery: AuthQuery
   ) { }
 
   ngOnInit() {
+    this.user$ = this.authQuery.user$;
     this.organization$ = this.query.selectActive();
     this.previousPage = this.tunnelService.previousUrl;
   }
