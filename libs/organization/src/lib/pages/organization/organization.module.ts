70a0d8872afbd775168a20352e32415954b9b39b
DK <jenna_kim1@hotmail.com>
2020-02-10 11:33:46 +0100

    added update button

--- libs/organization/src/lib/pages/organization/organization.module.ts
+++ libs/organization/src/lib/pages/organization/organization.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-// import { ReactiveFormsModule } from '@angular/forms';
 import { OrganizationFormModule } from '@blockframes/organization/forms/organization-form/organization-form.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { OrganizationComponent } from './organization.component';
 
+// Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [OrganizationComponent],
   imports: [
     CommonModule,
-    // ReactiveFormsModule,
     OrganizationFormModule,
+    FlexLayoutModule,
+    // Material
+    MatButtonModule,
+    MatIconModule,
     RouterModule.forChild([{ path: '', component: OrganizationComponent }])
   ]
 })
