5df3ed2548ea1875e2735b06cf1aad5ab882cbcb
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-02-11 16:15:06 +0100

    Create movie banner

---
+++ libs/movie/src/lib/movie/components/banner/banner.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Movie } from '../../+state';
+import { Title } from '../../+state/movie.firestore';
+import { ImgRef } from '@blockframes/utils';
+
+@Component({
+  selector: 'movie-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss']
+})
+export class BannerComponent implements OnInit {
+
+  public poster: ImgRef;
+  public title: Title;
+  public director: string;
+  public countries: string;
+  public date: number;
+
+  @Input() set movie(movie: Movie) {
+    this.poster = movie.promotionalElements.poster[0].media;
+    this.title = movie.main.title;
+    this.director = movie.main.directors[0].displayName;
+    this.countries = movie.main.originCountries.join(', ');
+    this.date = movie.main.productionYear;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
