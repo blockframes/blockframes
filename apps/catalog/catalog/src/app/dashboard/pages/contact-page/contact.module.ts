75a18340018bbb792780cfff51628bdec3920ecb
Hugo-Hontans <hugohontans@gmail.com>
2020-02-11 16:27:46 +0100

    start to refactor contact page

--- apps/catalog/catalog/src/app/dashboard/pages/contact-page/contact.module.ts
+++ apps/catalog/catalog/src/app/dashboard/pages/contact-page/contact.module.ts
@@ -2,6 +2,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 // Pages
 import { ContactPageComponent } from './contact-page.component';
@@ -9,6 +10,10 @@ import { ContactPageComponent } from './contact-page.component';
 // Material
 import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [ContactPageComponent],
@@ -16,6 +21,11 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     CommonModule,
     MatCardModule,
     FlexLayoutModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
     RouterModule.forChild([
       {
         path: '',
