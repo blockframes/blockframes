7b096c3029ca653c32db931bce16eba1c0b59b19
Hugo-Hontans <hugohontans@gmail.com>
2020-02-13 14:08:26 +0100

    fix multiple missing and mandatory tags

--- libs/movie/src/lib/movie/form/summary/credit/credit.module.ts
+++ libs/movie/src/lib/movie/form/summary/credit/credit.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieSummaryCreditComponent } from './credit.component';
 import { MissingControlModule } from '@blockframes/ui/missing-control/missing-control.module';
+import { TranslateSlugModule } from '@blockframes/utils';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [MovieSummaryCreditComponent],
   imports: [
     CommonModule,
-    MissingControlModule
+    RouterModule,
+    MissingControlModule,
+    TranslateSlugModule
   ],
   exports: [MovieSummaryCreditComponent]
 })
--- libs/movie/src/lib/movie/form/summary/main/main.module.ts
+++ libs/movie/src/lib/movie/form/summary/main/main.module.ts
@@ -5,11 +5,13 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 // Components
 import { MovieSummaryMainComponent } from './main.component';
 import { MissingControlModule } from '@blockframes/ui/missing-control/missing-control.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [MovieSummaryMainComponent],
   imports: [
     CommonModule,
+    RouterModule,
     FlexLayoutModule,
     MissingControlModule
   ],
