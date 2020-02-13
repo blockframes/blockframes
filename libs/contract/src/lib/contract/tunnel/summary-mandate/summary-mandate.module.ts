59cb6a084294ff730af7bfd4f40060a585a46ffe
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-02-13 11:39:41 +0100

    start summary mandate

--- libs/contract/src/lib/contract/tunnel/summary-mandate/summary-mandate.module.ts
+++ libs/contract/src/lib/contract/tunnel/summary-mandate/summary-mandate.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { SummaryMandateComponent } from './summary-mandate.component';
 
+import { TunnelPageModule } from '@blockframes/ui/tunnel';
+import { MovieBannerModule } from '@blockframes/movie/movie/components/banner';
+import { RightListModule } from '@blockframes/movie/distribution-deals/components/right-list/right-list.module';
+import { MissingControlModule } from '@blockframes/ui/missing-control/missing-control.module';
+
+// Material
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [SummaryMandateComponent],
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    TunnelPageModule,
+    MovieBannerModule,
+    RightListModule,
+    MissingControlModule,
+    // Material
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
     RouterModule.forChild([{ path: '', component: SummaryMandateComponent }])
   ]
 })
--- libs/contract/src/lib/contract/tunnel/summary-sale/summary-sale.component.ts
+++ libs/contract/src/lib/contract/tunnel/summary-sale/summary-sale.component.ts
@@ -9,6 +9,7 @@ import { DistributionDealTermsControl } from '@blockframes/movie/distribution-de
 import { ContractVersionPriceControl, ContractVersionPriceForm } from '@blockframes/contract/version/form';
 import { MovieCurrenciesSlug } from '@blockframes/utils/static-model';
 import { ContractVersion } from '@blockframes/contract/version/+state';
+import { displayPaymentSchedule, displayTerms } from '../utils';
 
 @Component({
   selector: 'contract-tunnel-summary-sale',
@@ -22,11 +23,11 @@ export class SummarySaleComponent implements OnInit {
   public dealForms: FormEntity<DealControls>;
   public form: ContractForm;
   public parties: { licensee: FormControl[], licensor: FormControl[] };
-  public terms: DistributionDealTermsControl;
+  public terms: string;
   public price: ContractVersionPriceControl;
   public moviePrices: Record<string, FormControl> = {}
   public currency: MovieCurrenciesSlug;
-  public payments: string[];
+  public payments: { type: string, list: string[] } = { type: '', list: [] };
 
   constructor(private tunnel: ContractTunnelComponent) { }
 
@@ -38,31 +39,17 @@ export class SummarySaleComponent implements OnInit {
     const lastVersion = this.form.get('versions').last();
 
     this.parties = {
-      licensee: this.getParties('licensor'),
-      licensor: this.getParties('licensee')
+      licensee: this.getParties('licensee'),
+      licensor: this.getParties('licensor')
     };
-    this.terms = lastVersion.get('scope').controls;
+    this.terms = displayTerms(lastVersion.get('scope').value);
     this.price = lastVersion.get('price').controls;
     this.currency = this.price.currency.value;
     for (const movieId in lastVersion.get('titles').controls) {
       this.moviePrices[movieId] = lastVersion.get('titles').get(movieId).get('price').get('amount');
     }
 
-    // Payment Schedule
-    if (lastVersion.get('customPaymentSchedule')) {
-      this.payments = [lastVersion.get('customPaymentSchedule').value]
-    } else if (this.isPeriodicPayment(lastVersion.value)) {
-      this.payments = lastVersion.get('paymentSchedule').value.map(({ percentage, date }) => {
-        const { temporality, duration, unit } = date.floatingDuration;
-        const { start, floatingStart } = date;
-        return `${percentage} ${temporality} ${duration} ${unit} starting from (${start} | ${floatingStart})`
-      });
-    } else {
-      const payments = lastVersion.get('paymentSchedule').value;
-      this.payments = payments.map(({ percentage, date }, i) => {
-        return `${percentage} upon ${date.floatingStart} | ${payments[i+1].percentage} upon ${payments[i+1].date.floatingStart}`
-      });
-    }
+    this.payments = displayPaymentSchedule(lastVersion.value);
   }
 
   private getParties(role: 'licensee' | 'licensor') {
@@ -70,9 +57,4 @@ export class SummarySaleComponent implements OnInit {
     return parties.map(p => p.get('party').get('displayName'));
   }
 
-  private isPeriodicPayment(version: ContractVersion) {
-    return version.paymentSchedule[0]
-      ? version.paymentSchedule[0].date.floatingDuration.temporality === TemporalityUnit.every
-      : false;
-  }
 }
