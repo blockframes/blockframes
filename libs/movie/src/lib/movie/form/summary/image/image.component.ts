3c892a68fb81af152cb9474bbb3c61bb5c3128a4
Hugo-Hontans <hugohontans@gmail.com>
2020-02-13 15:48:27 +0100

    update image summary

--- libs/movie/src/lib/movie/form/summary/image/image.component.ts
+++ libs/movie/src/lib/movie/form/summary/image/image.component.ts
@@ -1,10 +1,19 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, ChangeDetectorRef, OnInit } from '@angular/core';
+import { MoviePromotionalElementsForm } from '../../promotional-elements/promotional-elements.form';
 
 @Component({
-  selector: 'movie-summary-image',
+  selector: '[promotionalElements] movie-summary-image',
   templateUrl: './image.component.html',
   styleUrls: ['./image.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MovieSummaryImageComponent {
+export class MovieSummaryImageComponent implements OnInit {
+  @Input() promotionalElements: MoviePromotionalElementsForm;
+  @Input() link: string;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.promotionalElements.valueChanges.subscribe(_ => this.cdr.markForCheck());
+  }
 }
--- libs/movie/src/lib/movie/form/summary/image/image.module.ts
+++ libs/movie/src/lib/movie/form/summary/image/image.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieSummaryImageComponent } from './image.component';
+import { MissingControlModule } from '@blockframes/ui/missing-control/missing-control.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [MovieSummaryImageComponent],
   imports: [
     CommonModule,
+    RouterModule,
+    MissingControlModule
   ],
   exports: [MovieSummaryImageComponent]
 })
