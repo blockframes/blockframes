591db266cf74bec1e72c84750d2270839c9a54f8
Hugo-Hontans <hugohontans@gmail.com>
2020-02-13 14:08:26 +0100

    fix multiple missing and mandatory tags

--- libs/movie/src/lib/movie/form/summary/budget/budget.module.ts
+++ libs/movie/src/lib/movie/form/summary/budget/budget.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MovieSummaryBudgetComponent } from './budget.component';
 import { MissingControlModule } from '@blockframes/ui/missing-control/missing-control.module';
 import { TranslateSlugModule } from '@blockframes/utils';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [MovieSummaryBudgetComponent],
   imports: [
     CommonModule,
+    RouterModule,
     MissingControlModule,
     TranslateSlugModule
   ],
