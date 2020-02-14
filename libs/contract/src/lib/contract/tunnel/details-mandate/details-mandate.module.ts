2c5c6c52a521f5971f415079c363c7bb8b8b417b
maxstgt <mfritz@cascade8.com>
2020-02-14 10:08:45 +0100

    init

--- libs/contract/src/lib/contract/tunnel/details-mandate/details-mandate.module.ts
+++ libs/contract/src/lib/contract/tunnel/details-mandate/details-mandate.module.ts
@@ -1,13 +1,27 @@
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DetailsMandateComponent } from './details-mandate.component';
+import { TunnelPageModule } from '@blockframes/ui/tunnel';
+import { ContractFormPartyModule } from '../../form/party/party.module';
+import { DistributionDealTermsModule } from '@blockframes/movie/distribution-deals/form/terms/terms.module';
 
 
 
 @NgModule({
   declarations: [DetailsMandateComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([{ path: '', component: DetailsMandateComponent }]),
+
+    // Components
+    TunnelPageModule,
+    ContractFormPartyModule,
+    DistributionDealTermsModule,
+
+    // Material
+    MatCardModule
   ]
 })
 export class DetailsMandateModule { }
