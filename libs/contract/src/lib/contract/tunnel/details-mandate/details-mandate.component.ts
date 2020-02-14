cf1846dd67efb9a739d5db36918349152947c057
maxstgt <mfritz@cascade8.com>
2020-02-14 10:08:45 +0100

    init

--- libs/contract/src/lib/contract/tunnel/details-mandate/details-mandate.component.ts
+++ libs/contract/src/lib/contract/tunnel/details-mandate/details-mandate.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ContractTunnelComponent } from '../contract-tunnel.component';
 
 @Component({
   selector: 'contract-details-mandate',
   templateUrl: './details-mandate.component.html',
-  styleUrls: ['./details-mandate.component.scss']
+  styleUrls: ['./details-mandate.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DetailsMandateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private tunnel: ContractTunnelComponent) { }
 
   ngOnInit() {
   }
 
+  get parties() {
+    return this.tunnel.contractForm.get('parties')
+  }
+
+  get versions() {
+    return this.tunnel.contractForm.get('versions');
+  }
+
+  public terms(index: number) {
+    return this.versions.at(index).get('scope');
+  }
 }
