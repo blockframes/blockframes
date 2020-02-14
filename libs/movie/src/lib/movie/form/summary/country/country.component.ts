39a23b15b7e4462e1e849fbc9aa28dc8a576d11b
Hugo-Hontans <hugohontans@gmail.com>
2020-02-14 15:33:11 +0100

    add stakeholders to summary page

--- libs/movie/src/lib/movie/form/summary/country/country.component.ts
+++ libs/movie/src/lib/movie/form/summary/country/country.component.ts
@@ -1,23 +1,22 @@
 import { Component, ChangeDetectionStrategy, Input, ChangeDetectorRef, OnInit } from '@angular/core';
 import { MovieSalesInfoForm } from '../../sales-info/sales-info.form';
-import { FormList } from '@blockframes/utils/form/forms/list.form';
-import { TerritoriesSlug } from '@blockframes/utils/static-model/types';
+import { MovieMainForm } from '../../main/main.form';
 
 @Component({
-  selector: '[salesInfo] [originCountries] movie-summary-country',
+  selector: '[salesInfo] [main] movie-summary-country',
   templateUrl: './country.component.html',
   styleUrls: ['./country.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MovieSummaryCountryComponent implements OnInit {
   @Input() salesInfo: MovieSalesInfoForm;
-  @Input() originCountries: FormList<TerritoriesSlug>;
+  @Input() main: MovieMainForm;
   @Input() link: string;
 
   constructor(private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
     this.salesInfo.valueChanges.subscribe(_ => this.cdr.markForCheck());
-    this.originCountries.valueChanges.subscribe(_ => this.cdr.markForCheck());
+    this.main.valueChanges.subscribe(_ => this.cdr.markForCheck());
   }
 }
