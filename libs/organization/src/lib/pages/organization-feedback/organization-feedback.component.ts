5afd631ccc453082b9b84ab053b35c7d22f6786a
DK <jenna_kim1@hotmail.com>
2020-02-14 15:11:17 +0100

    typed invitations & removed orgname

--- libs/organization/src/lib/pages/organization-feedback/organization-feedback.component.ts
+++ libs/organization/src/lib/pages/organization-feedback/organization-feedback.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component, OnInit, NgZone } from '@angular/cor
 import { AuthQuery } from '@blockframes/auth';
 import { InvitationService } from '@blockframes/notification/invitation/+state/invitation.service';
 import { MatSnackBar } from '@angular/material';
+import { InvitationFromUserToOrganization } from '@blockframes/invitation/types';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'organization-feedback',
@@ -10,8 +12,9 @@ import { MatSnackBar } from '@angular/material';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OrganizationFeedbackComponent implements OnInit {
-  public invitations;
-  public orgName: string;
+  public invitations: InvitationFromUserToOrganization[];
+  public orgName: Observable<string>;
+  public isCanceled: boolean;
 
   constructor(
     private service: InvitationService,
@@ -20,21 +23,16 @@ export class OrganizationFeedbackComponent implements OnInit {
     private _ngZone: NgZone
   ) {}
 
-  ngOnInit() {
-    this.getInvitation();
-  }
-
-  async getInvitation() {
+  async ngOnInit() {
     const uid = this.authQuery.userId;
-    this.invitations = await this.service.getValue(ref => ref.where('user.uid', '==', uid));
-    this.orgName = this.invitations[0].organization.name
-    this._ngZone.run(() => {});
+    this.invitations = await this.service.getValue(ref => ref.where('user.uid', '==', uid)) as any;
   }
   
   public removeInvitation() {
     try {
       this.service.remove(this.invitations[0].id)
       this.snackBar.open('Your request to join an organization has been canceled.', 'close', { duration: 2000 });
+      this.isCanceled = true;
     } catch (error) {
       this.snackBar.open(error.message, 'close', { duration: 2000 });
     }
