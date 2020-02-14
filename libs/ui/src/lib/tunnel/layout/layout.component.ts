52a3c4c2f452332c595be8fe1aa570464ca7ff57
Hugo-Hontans <hugohontans@gmail.com>
2020-02-14 16:59:29 +0100

    update responsive sidenav with burger

--- libs/ui/src/lib/tunnel/layout/layout.component.ts
+++ libs/ui/src/lib/tunnel/layout/layout.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, ViewEncapsulation, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { RouterQuery } from '@datorama/akita-ng-router-store';
 import { fade } from '@blockframes/utils/animations/fade';
 import { TunnelStep } from '../tunnel.model';
 import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
-
-
+import { MediaMatcher } from '@angular/cdk/layout';
 
 /**
  * @description returns the next or previous page where the router should go to
@@ -22,7 +21,6 @@ function getPage(steps: TunnelStep[], url: string, arithmeticOperator: number): 
   }
 }
 
-
 @Component({
   selector: 'tunnel-layout',
   templateUrl: './layout.component.html',
@@ -35,26 +33,40 @@ function getPage(steps: TunnelStep[], url: string, arithmeticOperator: number): 
     '[@fade]': 'fade'
   }
 })
-export class TunnelLayoutComponent implements OnInit {
+export class TunnelLayoutComponent implements OnInit, OnDestroy {
 
   private navigation = new BehaviorSubject<TunnelStep[]>([]);
   private url$ = this.routerQuery.select(({ state }) => state.url);
+  private _mobileQueryListener: () => void;
   public steps$ = this.navigation.asObservable();
   public urlBynav$: Observable<[string, TunnelStep[]]>;
   public next$: Observable<string>;
   public previous$: Observable<string>;
+  public mobileQuery: MediaQueryList;
 
   @Input() set steps(steps: TunnelStep[]) {
     this.navigation.next(steps || []);
   }
-  
-  constructor(private routerQuery: RouterQuery) { }
+
+  constructor(
+    private routerQuery: RouterQuery,
+    private changeDetectorRef: ChangeDetectorRef,
+    private media: MediaMatcher
+  ) {}
 
   ngOnInit() {
     // Share the url by navigation plan
     this.urlBynav$ = combineLatest([this.url$, this.steps$]).pipe(shareReplay());
     this.next$ = this.urlBynav$.pipe(map(([ url, steps ]) => getPage(steps, url, 1)));
     this.previous$ = this.urlBynav$.pipe(map(([ url, steps ]) => getPage(steps, url, -1)));
+
+    this.mobileQuery = this.media.matchMedia('(max-width: 959px)');
+    this._mobileQueryListener = () => this.changeDetectorRef.detectChanges();
+    this.mobileQuery.addEventListener('change', this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeEventListener('change', this._mobileQueryListener);
   }
 
 }
