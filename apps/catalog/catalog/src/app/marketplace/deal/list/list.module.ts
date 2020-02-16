e56a566b98d3e05a4bbc2a5fd612d3a305693a73
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-02-16 13:48:47 +0100

    create deal pages structure

---
+++ apps/catalog/catalog/src/app/marketplace/deal/list/list.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { ListComponent } from './list.component';
+
+
+@NgModule({
+  declarations: [ListComponent],
+  exports: [ListComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([{ path: '', component: ListComponent }])
+  ]
+})
+export class DealListModule { }
---
+++ apps/catalog/catalog/src/app/marketplace/deal/view/view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'marketplace-deal-view',
+  templateUrl: './view.component.html',
+  styleUrls: ['./view.component.scss']
+})
+export class ViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
