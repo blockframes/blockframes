3e953d3fbeb4db6b4c43e2ba02482d89c322fcc9
DK <jenna_kim1@hotmail.com>
2020-02-17 12:27:41 +0100

    commented sales revenue on dashboard home

--- apps/catalog/catalog/src/app/dashboard/pages/home/home.component.ts
+++ apps/catalog/catalog/src/app/dashboard/pages/home/home.component.ts
@@ -3,10 +3,7 @@ import { MovieQuery } from '@blockframes/movie/movie/+state/movie.query';
 import { Observable } from 'rxjs';
 import { MovieAnalytics } from '@blockframes/movie/movie/+state/movie.firestore';
 import { MovieService } from '@blockframes/movie/movie/+state/movie.service';
-import { map, switchMap } from 'rxjs/operators';
-import { ContractQuery } from '@blockframes/contract/contract/+state';
-import { Contract } from '@blockframes/contract/contract/+state/contract.model';
-import { getContractLastVersion } from '@blockframes/contract/version/+state/contract-version.model';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'catalog-home',
@@ -16,22 +13,15 @@ import { getContractLastVersion } from '@blockframes/contract/version/+state/con
 })
 export class HomeComponent implements OnInit {
   public movieAnalytics$: Observable<MovieAnalytics[]>;
-  public loading$: Observable<boolean>;
-  public contracts$: Observable<Contract[]>;
-  public ids: Observable<string[]>;
 
   constructor(
     private movieQuery: MovieQuery, 
     private movieService: MovieService,
-    private contractQuery: ContractQuery
   ) {}
 
   ngOnInit() {
-    this.loading$ = this.movieQuery.selectLoading();
     this.movieAnalytics$ = this.movieQuery.select('ids').pipe(
-      switchMap(ids => this.movieService.getMovieAnalytics(ids))
-    );
-  this.ids = this.movieQuery.select('ids');
-  }
+      switchMap(ids => this.movieService.getMovieAnalytics(ids)));
+    }
 
 }
