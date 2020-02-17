285be419436a5411bea2b7f58c6d258e360e9367
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-02-17 19:20:46 +0100

    update negociation page

--- apps/catalog/catalog/src/app/marketplace/deal/negociation/negociation.module.ts
+++ apps/catalog/catalog/src/app/marketplace/deal/negociation/negociation.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NegociationComponent } from './negociation.component';
 import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { NegociationComponent } from './negociation.component';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [NegociationComponent],
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    // Material
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    // Router
     RouterModule.forChild([{ path: '', component: NegociationComponent }])
   ]
 })
