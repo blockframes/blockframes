8504111d974da0851a042df8ca94f811850adbc3
Laurent Senta <laurent.senta@gmail.com>
2020-02-17 14:43:22 +0100

    berlin maintenance

--- libs/ui/src/lib/maintenance/maintenance.service.ts
+++ libs/ui/src/lib/maintenance/maintenance.service.ts
@@ -1,23 +1,20 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { filter, tap, map, first } from 'rxjs/operators';
-import { isInMaintenance, IMaintenanceDoc, maintenancePath } from '@blockframes/utils/maintenance';
+import { filter, first, tap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class MaintenanceService {
-  
   // if document doesn't exist, we are not in the maintenance
-  isInMaintenance$ = this.db.doc<IMaintenanceDoc>(maintenancePath).valueChanges().pipe(
-    map(maintenanceDoc => maintenanceDoc ? isInMaintenance(maintenanceDoc) : false)
-  );
+  isInMaintenance$ = of(true);
 
   constructor(private db: AngularFirestore) {}
 
   redirectOnMaintenance() {
     return this.isInMaintenance$.pipe(
       filter(isMaintenance => !!isMaintenance),
-      first(),  // Change on maintenance can only happen once during the session
+      first(), // Change on maintenance can only happen once during the session
       tap(_ => window.location.reload())
-    )
+    );
   }
 }
