227be1f6cf07faa0594ad19dd83bb2565bd03431
Laurent Senta <laurent.senta@gmail.com>
2020-02-18 17:57:02 +0100

    migration: org empty data

--- apps/backend-ops/src/firestoreMigrations/index.ts
+++ apps/backend-ops/src/firestoreMigrations/index.ts
@@ -6,6 +6,7 @@ import * as v0005 from './0005';
 import * as v0006 from './0006';
 import * as v0007 from './0007';
 import * as v0008 from './0008';
+import * as v0009 from './0009';
 
 import { Firestore } from '../admin';
 
@@ -25,7 +26,8 @@ export const MIGRATIONS = {
   5: v0005,
   6: v0006,
   7: v0007,
-  8: v0008
+  8: v0008,
+  9: v0009
 };
 
 export const VERSIONS_NUMBERS = Object.keys(MIGRATIONS).map(s => parseInt(s, 10));
