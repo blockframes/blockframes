e5b1ef32cf28b97fa7a27d7102f0268456f62481
Laurent Senta <laurent.senta@gmail.com>
2020-02-18 16:43:13 +0100

    migrations: orgs timestamps

--- apps/backend-ops/src/firestoreMigrations/index.ts
+++ apps/backend-ops/src/firestoreMigrations/index.ts
@@ -4,6 +4,7 @@ import * as v0003 from './0003';
 import * as v0004 from './0004';
 import * as v0005 from './0005';
 import * as v0006 from './0006';
+import * as v0007 from './0007';
 
 import { Firestore } from '../admin';
 
@@ -21,7 +22,8 @@ export const MIGRATIONS = {
   3: v0003,
   4: v0004,
   5: v0005,
-  6: v0006
+  6: v0006,
+  7: v0007
 };
 
 export const VERSIONS_NUMBERS = Object.keys(MIGRATIONS).map(s => parseInt(s, 10));
