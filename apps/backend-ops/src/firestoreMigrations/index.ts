dd50570b1bf1c8a015d2790eca2287f8064704da
Laurent Senta <laurent.senta@gmail.com>
2020-02-18 17:08:20 +0100

    ops: add migration for perm docs roles

--- apps/backend-ops/src/firestoreMigrations/index.ts
+++ apps/backend-ops/src/firestoreMigrations/index.ts
@@ -5,6 +5,7 @@ import * as v0004 from './0004';
 import * as v0005 from './0005';
 import * as v0006 from './0006';
 import * as v0007 from './0007';
+import * as v0008 from './0008';
 
 import { Firestore } from '../admin';
 
@@ -23,7 +24,8 @@ export const MIGRATIONS = {
   4: v0004,
   5: v0005,
   6: v0006,
-  7: v0007
+  7: v0007,
+  8: v0008
 };
 
 export const VERSIONS_NUMBERS = Object.keys(MIGRATIONS).map(s => parseInt(s, 10));
