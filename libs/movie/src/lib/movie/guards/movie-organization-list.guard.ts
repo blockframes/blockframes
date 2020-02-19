7ac6682b8d1891f1b942932adc32bb636420db1f
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-02-19 11:04:00 +0100

    update deal list

--- libs/movie/src/lib/movie/guards/movie-organization-list.guard.ts
+++ libs/movie/src/lib/movie/guards/movie-organization-list.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CollectionGuard } from 'akita-ng-fire';
 import { MovieState, MovieService, MovieQuery } from '../+state';
 
+/** Sync all movie from the active organization */
 @Injectable({ providedIn: 'root' })
 export class MovieOrganizationListGuard extends CollectionGuard<MovieState> {
   constructor(protected service: MovieService, private query: MovieQuery) {
