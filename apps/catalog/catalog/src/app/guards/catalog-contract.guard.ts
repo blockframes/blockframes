761de764167244abb29abc02a7a3a04fb3b96510
maxstgt <mfritz@cascade8.com>
2020-02-20 17:01:18 +0100

    worked on guard

--- apps/catalog/catalog/src/app/guards/catalog-contract.guard.ts
+++ apps/catalog/catalog/src/app/guards/catalog-contract.guard.ts
@@ -1,17 +1,16 @@
-import { Router } from '@angular/router';
+import { getContractLastVersion } from '@blockframes/contract/version/+state/contract-version.model';
+import { Router, ActivatedRouteSnapshot, CanActivate } from '@angular/router';
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot } from '@angular/router';
-import { ContractVersionService } from '@blockframes/contract/version/+state';
+import { ContractStore } from '@blockframes/contract';
 
 @Injectable({ providedIn: 'root' })
 export class CatalogContractGuard implements CanActivate {
 
-    constructor(private contractVersionService: ContractVersionService, private router: Router) { }
+    constructor(private contractStore: ContractStore, private router: Router) { }
 
     async canActivate(next: ActivatedRouteSnapshot) {
-        const contractId = next.paramMap.get('contractId');
-        const versions = await this.contractVersionService.getContractVersions(contractId);
-        const isDraft = versions.map(version => version.status === 'draft');
-        return isDraft.includes(true) ? this.router.parseUrl('c/o/dashboard/home') : true;
+        const contract = this.contractStore.getValue().entities[next.paramMap.get('contractId')];
+        const lastVersion = getContractLastVersion(contract)
+        return lastVersion.status === 'draft' ? this.router.parseUrl('c/o/dashboard/deals/list') : true;
     }
 }
