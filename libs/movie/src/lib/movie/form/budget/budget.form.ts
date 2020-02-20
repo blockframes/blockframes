94723f7944a361e10cb4cc7c1ff9fe5638d0f58f
bruce <delorme.bruce.michel@gmail.com>
2020-02-20 16:34:28 +0100

    temp

--- libs/movie/src/lib/movie/form/budget/budget.form.ts
+++ libs/movie/src/lib/movie/form/budget/budget.form.ts
@@ -6,13 +6,13 @@ import { createBoxOffice } from '../../+state/movie.model';
 import { NumberRange } from '@blockframes/utils/common-interfaces/range';
 
 export const BUDGET_LIST: NumberRange[] = [
-  { from: 0, to: 1000000, label: 'Less than 1 million' },
-  { from: 1000000, to: 5000000, label: '1 - 5 millions' },
-  { from: 5000000, to: 10000000, label: '5 - 10 millions' },
-  { from: 10000000, to: 50000000, label: '10 - 50 millions' },
-  { from: 50000000, to: 100000000, label: '50 - 100 millions' },
-  { from: 100000000, to: 300000000, label: '100 - 300 millions' },
-  { from: 300000000, to: 999999999, label: 'More than 300 millions' },
+  { from: 0, to: 1000000, label: 'Less than $1 million' },
+  { from: 1000000, to: 2000000, label: '$1 - 2 millions' },
+  { from: 2000000, to: 3500000, label: '$2 - 3.5 millions' },
+  { from: 3500000, to: 5000000, label: '$3.5 - 5 millions' },
+  { from: 5000000, to: 10000000, label: '$5 - 10 millions' },
+  { from: 10000000, to: 20000000, label: '$10 - 20 millions' },
+  { from: 20000000, to: 999999999, label: 'More than $20 millions' },
 ];
 
 function createBudgetFormControl(entity?: Partial<MovieBudget>) {
