dd029bf09ce48ca3c892e4e30abda04b4cb0dc79
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-02-21 17:50:54 +0100

    remove url validators on movie form

--- apps/catalog/catalog/src/app/dashboard/movie-tunnel/summary/summary.component.ts
+++ apps/catalog/catalog/src/app/dashboard/movie-tunnel/summary/summary.component.ts
@@ -4,6 +4,7 @@ import { MovieService } from '@blockframes/movie';
 import { StoreStatus } from '@blockframes/movie/movie/+state/movie.firestore';
 import { MatSnackBar } from '@angular/material';
 import { MovieTunnelComponent } from '../movie-tunnel.component';
+import { FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'catalog-summary-tunnel',
@@ -36,5 +37,23 @@ export class TunnelSummaryComponent {
     } else {
       this.snackBar.open('Fill all mandatory fields before submitting', '', { duration: 2000 });
     }
+  }
+
+  /* Utils function to get the list of invalid form. Not used yet, but could be useful later */
+  public findInvalidControlsRecursive(formToInvestigate:FormGroup|FormArray):string[] {
+    const invalidControls:string[] = [];
+    const recursiveFunc = (form:FormGroup|FormArray) => {
+      Object.keys(form.controls).forEach(field => { 
+        const control = form.get(field);
+        if (control.invalid) invalidControls.push(field);
+        if (control instanceof FormGroup) {
+          recursiveFunc(control);
+        } else if (control instanceof FormArray) {
+          recursiveFunc(control);
+        }        
+      });
+    }
+    recursiveFunc(formToInvestigate);
+    return invalidControls;
   }
 }
