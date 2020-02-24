aa5af827fc2e011b1343057468823d0879a64533
hugo-hontans <hugohontans@gmail.com>
2020-02-24 15:00:01 +0100

    add responsive to the video

--- apps/catalog/catalog/src/app/landing/components/view-marketplace/view-marketplace.component.ts
+++ apps/catalog/catalog/src/app/landing/components/view-marketplace/view-marketplace.component.ts
@@ -1,5 +1,6 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef, OnInit } from '@angular/core';
 import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'catalog-view-marketplace',
@@ -7,14 +8,34 @@ import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./view-marketplace.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CatalogViewMarketplaceComponent {
+export class CatalogViewMarketplaceComponent implements OnInit {
   public cover = true;
   public url: SafeResourceUrl;
+  public mobileQuery: MediaQueryList;
+  public tabletQuery: MediaQueryList;
 
-  constructor(public sanitizer: DomSanitizer) {}
+  private _responsiveQueryListener: () => void;
+
+  constructor(public sanitizer: DomSanitizer, private changeDetectorRef: ChangeDetectorRef, private media: MediaMatcher) {}
+
+  ngOnInit() {
+    this.mobileQuery = this.media.matchMedia('(max-width: 599px)');
+    this.tabletQuery = this.media.matchMedia('(max-width: 959px)');
+    this._responsiveQueryListener = () => this.changeDetectorRef.detectChanges();
+    this.mobileQuery.addEventListener('change', this._responsiveQueryListener);
+    this.tabletQuery.addEventListener('change', this._responsiveQueryListener);
+  }
 
   play() {
     this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://player.vimeo.com/video/391939808?autoplay=1');
     this.cover = !this.cover;
+  }
+
+  get videoWidth() {
+    return this.mobileQuery.matches ? '340' : this.tabletQuery.matches ? '600' : '1024';
+  }
+
+  get videoHeight() {
+    return this.mobileQuery.matches ? '300' : this.tabletQuery.matches ? '400' : '560';
   }
 }
