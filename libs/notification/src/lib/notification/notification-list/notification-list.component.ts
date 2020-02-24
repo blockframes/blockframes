9e4f5e96338bfabe5ce7427634c79fbc68bc6634
Yohann <ioa.nikas@gmail.com>
2020-02-24 10:54:20 +0100

    Add route for movie notifications

--- libs/notification/src/lib/notification/notification-list/notification-list.component.ts
+++ libs/notification/src/lib/notification/notification-list/notification-list.component.ts
@@ -37,8 +37,11 @@ export class NotificationListComponent implements OnInit {
     try {
       if (notification.type === NotificationType.newContract || NotificationType.contractInNegotiation) {
         this.router.navigateByUrl(`c/o/dashboard/deals/${notification.docId}`);
-        this.service.readNotification(notification);
       }
+      if (notification.type === NotificationType.movieSubmitted || NotificationType.movieAccepted) {
+        this.router.navigateByUrl(`c/o/dashboard/titles/${notification.docId}`);
+      }
+      this.service.readNotification(notification);
     } catch (error) {
       throw new Error(error.message);
     }
