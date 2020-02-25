4a8e5bbb429fa9008ea9f315704017ab12dbca01
maxstgt <max.j.f@gmx.de>
2020-02-25 11:53:41 +0100

    improved type

--- libs/movie/src/lib/movie/form/sales-cast/sales-cast.component.ts
+++ libs/movie/src/lib/movie/form/sales-cast/sales-cast.component.ts
@@ -13,7 +13,7 @@ export class MovieFormSalesCastComponent {
 
   @Input() form: FormList<CreditForm>;
 
-  @Input() role: string
+  @Input() role: 'CREW_ROLES' | 'CAST_ROLES' | 'PRODUCER_ROLES' | 'STAKEHOLDER_ROLES' | 'SUB_LICENSOR_ROLES' | 'LEGAL_ROLES'
 
   add() {
     this.form.add(new CreditForm())
