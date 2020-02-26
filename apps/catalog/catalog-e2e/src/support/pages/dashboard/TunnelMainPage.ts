b2376be13b3bcd5d15c6b31788fd0c99b40b3eb2
hugo-hontans <hugohontans@gmail.com>
2020-02-26 16:14:38 +0100

    start implements movie-tunnel-2

--- apps/catalog/catalog-e2e/src/support/pages/dashboard/TunnelMainPage.ts
+++ apps/catalog/catalog-e2e/src/support/pages/dashboard/TunnelMainPage.ts
@@ -1,2 +1,34 @@
 export default class TunnelMainPage {
+  constructor() {
+    cy.get('catalog-movie-tunnel-main', { timeout: 5000 });
+  }
+
+  public static navigateToPage(movieId: string) {
+    cy.visit(`c/o/dashboard/tunnel/movie/${movieId}/main`);
+    return new TunnelMainPage();
+  }
+
+  public clickContentType() {
+    cy.get('catalog-movie-tunnel-main movie-form-content-type mat-select[test-id=content-type]').click();
+  }
+
+  public selectContentType(contentType: string) {
+    cy.get('mat-option').contains(contentType).click();
+  }
+
+  public assertContentTypeExists(contentType: string) {
+    cy.get('catalog-movie-tunnel-main movie-form-content-type mat-select[test-id=content-type]').contains(contentType);
+  }
+
+  public clickFreshness() {
+    cy.get('catalog-movie-tunnel-main movie-form-content-type mat-select[test-id=freshness]').click();
+  }
+
+  public selectFreshness(freshness: string) {
+    cy.get('mat-option').contains(freshness).click();
+  }
+
+  public assertFreshnessExists(freshness: string) {
+    cy.get('catalog-movie-tunnel-main movie-form-content-type mat-select[test-id=freshness]').contains(freshness);
+  }
 }
