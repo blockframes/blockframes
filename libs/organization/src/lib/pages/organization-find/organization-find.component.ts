7e25e0e3aef69facb24ad1864c904e655c58324f
maxstgt <max.j.f@gmx.de>
2020-02-26 14:07:30 +0100

    added cleaner way for algolia results

--- libs/organization/src/lib/pages/organization-find/organization-find.component.ts
+++ libs/organization/src/lib/pages/organization-find/organization-find.component.ts
@@ -1,7 +1,7 @@
 import { Component, ChangeDetectionStrategy, OnInit, Inject } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, switchMap, pluck } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { InvitationService } from '@blockframes/notification';
@@ -33,11 +33,8 @@ export class OrganizationFindComponent implements OnInit {
     this.searchResults$ = this.orgControl.valueChanges.pipe(
       debounceTime(200),
       distinctUntilChanged(),
-      switchMap(name => {
-        return new Promise<OrganizationAlgoliaResult[]>((res, rej) => {
-          this.organizationIndex.search(name, (err, result) => (err ? rej(err) : res(result.hits)));
-        });
-      })
+      switchMap(text => this.organizationIndex.search(text)),
+      pluck('hits')
     )    
   }
 
