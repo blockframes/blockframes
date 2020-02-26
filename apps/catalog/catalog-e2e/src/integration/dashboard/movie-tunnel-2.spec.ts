58eda21ac6b67c7863e3ebb6f8a5ea84954d0ae3
hugo-hontans <hugohontans@gmail.com>
2020-02-26 16:14:38 +0100

    start implements movie-tunnel-2

--- apps/catalog/catalog-e2e/src/integration/dashboard/movie-tunnel-2.spec.ts
+++ apps/catalog/catalog-e2e/src/integration/dashboard/movie-tunnel-2.spec.ts
@@ -1,5 +1,16 @@
 /// <reference types="cypress" />
 
+import { WelcomeViewPage, LoginViewPage } from "../../support/pages/auth";
+import { HomePage } from "../../support/pages/marketplace";
+import { User } from "../../support/utils/type";
+import { USERS } from "../../support/utils/users";
+import { TunnelMainPage } from "../../support/pages/dashboard";
+
+// Select user: cytest@blockframes.com
+const LOGIN_CREDENTIALS: Partial<User> = USERS[0];
+
+const MOVIE_ID = 'P5ErzmOtap9ju9X8rWvd'; // Empty movie
+
 beforeEach(() => {
   cy.clearCookies();
   cy.clearLocalStorage();
@@ -9,5 +20,22 @@ beforeEach(() => {
 
 describe('User can navigate to the movie tunnel page 2 and fill all the fieldsn and navigate to page 3', () => {
   it('Login into an existing account, navigate on main page, complete main fields, go on movie tunnel page 3', () => {
+    // Connexion
+    const p1: WelcomeViewPage = new WelcomeViewPage();
+    const p2: LoginViewPage = p1.clickCallToAction();
+    p2.switchMode();
+    p2.fillSignin(LOGIN_CREDENTIALS);
+    const p3: HomePage = p2.clickSignIn();
+
+    // Navigate to movie-tunnel-2 and fill the fields
+    const p4: TunnelMainPage = TunnelMainPage.navigateToPage(MOVIE_ID);
+
+    // Content Type
+    p4.clickContentType();
+    p4.selectContentType('TV Film');
+    p4.assertContentTypeExists('TV Film');
+    p4.clickFreshness();
+    p4.selectFreshness('Library');
+    p4.assertFreshnessExists('Library');
   });
 });
