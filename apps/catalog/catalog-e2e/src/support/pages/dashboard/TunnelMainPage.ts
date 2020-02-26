aa0a36c8d585b19f73a49618b8fdd54fbb28ba10
hugo-hontans <hugohontans@gmail.com>
2020-02-26 16:44:19 +0100

    finish content type

--- apps/catalog/catalog-e2e/src/support/pages/dashboard/TunnelMainPage.ts
+++ apps/catalog/catalog-e2e/src/support/pages/dashboard/TunnelMainPage.ts
@@ -31,4 +31,40 @@ export default class TunnelMainPage {
   public assertFreshnessExists(freshness: string) {
     cy.get('catalog-movie-tunnel-main movie-form-content-type mat-select[test-id=freshness]').contains(freshness);
   }
+
+  public clickProductionStatus() {
+    cy.get('catalog-movie-tunnel-main movie-form-content-type mat-select[test-id=production-status]').click();
+  }
+
+  public selectProductionStatus(status: string) {
+    cy.get('mat-option').contains(status).click();
+  }
+
+  public assertProductionStatusExists(status: string) {
+    cy.get('catalog-movie-tunnel-main movie-form-content-type mat-select[test-id=production-status]').contains(status);
+  }
+
+  public fillInternationalTitle(title: string) {
+    cy.get('catalog-movie-tunnel-main movie-form-content-type input[test-id=international-title]').type(title);
+  }
+
+  public assertInternationalTitleExists(title: string) {
+    cy.get('catalog-movie-tunnel-main movie-form-content-type input[test-id=international-title]').should('have.value', title);
+  }
+
+  public fillOriginalTitle(title: string) {
+    cy.get('catalog-movie-tunnel-main movie-form-content-type input[test-id=original-title]').type(title);
+  }
+
+  public assertOriginalTitleExists(title: string) {
+    cy.get('catalog-movie-tunnel-main movie-form-content-type input[test-id=original-title]').should('have.value', title);
+  }
+
+  public fillReference(reference: string) {
+    cy.get('catalog-movie-tunnel-main movie-form-content-type input[test-id=reference]').type(reference);
+  }
+
+  public assertReferenceExists(reference: string) {
+    cy.get('catalog-movie-tunnel-main movie-form-content-type input[test-id=reference]').should('have.value', reference);
+  }
 }
