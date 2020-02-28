011269a2b642f82c2885d7054eae4d53fae89b44
hugo-hontans <hugohontans@gmail.com>
2020-02-28 16:09:39 +0100

    login and navigate to Tunnel Main before each e2e test

--- apps/catalog/catalog-e2e/src/integration/dashboard/movie-tunnel-6.spec.ts
+++ apps/catalog/catalog-e2e/src/integration/dashboard/movie-tunnel-6.spec.ts
@@ -1,56 +1,45 @@
 /// <reference types="cypress" />
 
-import { WelcomeViewPage, LoginViewPage } from "../../support/pages/auth";
-import { HomePage } from "../../support/pages/marketplace";
-import { TunnelTechnicalInfoPage } from "../../support/pages/dashboard";
-import { User } from "../../support/utils/type";
-import { USERS } from "../../support/utils/users";
+import { TunnelTechnicalInfoPage, TunnelMainPage } from "../../support/pages/dashboard";
+import { signInAndNavigateToMain } from "../../support/utils/utils";
 
-// Select user: cytest@blockframes.com
-const LOGIN_CREDENTIALS: Partial<User> = USERS[0];
+const NAVIGATION = ['Media', 'Technical Info.'];
 
 const FORMATS = ['1.66', 'SD', 'Color', 'Dolby SR'];
 const PARTIAL_LANGUAGE = 'en';
 const LANGUAGE = 'English';
-
-const MOVIE_ID = 'P5ErzmOtap9ju9X8rWvd'; // Empty movie
 
 beforeEach(() => {
   cy.clearCookies();
   cy.clearLocalStorage();
   cy.visit('/auth');
   cy.viewport('ipad-2', 'landscape');
+  signInAndNavigateToMain();
 });
 
 describe('User can navigate to the movie tunnel page 6, complete the fields, and navigate to page 7', () => {
-  it.skip('Login into an existing account, navigate on technical info page, complete fields, go on movie tunnel page 7', () => {
-    // Connexion
-    const p1: WelcomeViewPage = new WelcomeViewPage();
-    const p2: LoginViewPage = p1.clickCallToAction();
-    p2.switchMode();
-    p2.fillSignin(LOGIN_CREDENTIALS);
-    const p3: HomePage = p2.clickSignIn();
-
-    // Navigate to movie-tunnel-6 and fill the fields
-    const p4: TunnelTechnicalInfoPage = TunnelTechnicalInfoPage.navigateToPage(MOVIE_ID);
+  it('Login into an existing account, navigate on technical info page, complete fields, go on movie tunnel page 7', () => {
+    const p1 = new TunnelMainPage();
+    p1.navigateToTunnelPage(NAVIGATION[0], NAVIGATION[1]);
+    const p2 = new TunnelTechnicalInfoPage();
 
     // Format
-    p4.selectShootingFormat(FORMATS[0]);
-    p4.assertShootingFormatIsSelected(FORMATS[0]);
-    p4.selectFormatQuality(FORMATS[1]);
-    p4.assertFormatQualityIsSelected(FORMATS[1]);
-    p4.selectColor(FORMATS[2]);
-    p4.assertColorIsSelected(FORMATS[2]);
-    p4.selectSoundQuality(FORMATS[3]);
-    p4.assertSoundQualityIsSelected(FORMATS[3]);
+    p2.selectShootingFormat(FORMATS[0]);
+    p2.assertShootingFormatIsSelected(FORMATS[0]);
+    p2.selectFormatQuality(FORMATS[1]);
+    p2.assertFormatQualityIsSelected(FORMATS[1]);
+    p2.selectColor(FORMATS[2]);
+    p2.assertColorIsSelected(FORMATS[2]);
+    p2.selectSoundQuality(FORMATS[3]);
+    p2.assertSoundQualityIsSelected(FORMATS[3]);
 
     // Available Versions
-    p4.selectLanguage(PARTIAL_LANGUAGE, LANGUAGE);
-    p4.assertLanguageExists(LANGUAGE);
-    p4.checkSubtitled();
-    p4.assertSubtitledIsChecked();
+    p2.selectLanguage(PARTIAL_LANGUAGE, LANGUAGE);
+    p2.assertLanguageExists(LANGUAGE);
+    p2.checkSubtitled();
+    p2.assertSubtitledIsChecked();
 
     // TODO: return PromotionalImagesPage
-    p4.clickNext();
+    p2.clickNext();
   });
 });
---
+++ apps/catalog/catalog-e2e/src/support/utils/utils.ts
@@ -0,0 +1,22 @@
+import { WelcomeViewPage, LoginViewPage } from "../pages/auth";
+import { HomePage } from "../pages/marketplace";
+import { TitlesListPage, StartTunnelPage, TunnelMainPage } from "../pages/dashboard";
+import { User } from "./type";
+import { USERS } from "./users";
+
+// Select user: cytest@blockframes.com
+const LOGIN_CREDENTIALS: Partial<User> = USERS[0];
+
+export function signInAndNavigateToMain () {
+  // Connexion
+  const p1: WelcomeViewPage = new WelcomeViewPage();
+  const p2: LoginViewPage = p1.clickCallToAction();
+  p2.switchMode();
+  p2.fillSignin(LOGIN_CREDENTIALS);
+  const p3: HomePage = p2.clickSignIn();
+
+  // Navigate to movie-tunnel-main
+  const p4: TitlesListPage = TitlesListPage.navigateToPage();
+  const p5: StartTunnelPage = p4.clickAdd();
+  const p6: TunnelMainPage = p5.clickBegin();
+}
