94a2dd5bfef5f2b1bd0e534a239799b296eae601
hugo-hontans <hugohontans@gmail.com>
2020-03-03 15:20:13 +0100

    finish tunnel spec 12

--- apps/catalog/catalog-e2e/src/support/pages/dashboard/TunnelChainOfTitlesPage.ts
+++ apps/catalog/catalog-e2e/src/support/pages/dashboard/TunnelChainOfTitlesPage.ts
@@ -1,2 +1,21 @@
+import { uploadFile, assertUploadStatus } from "../../utils/utils";
+import TunnelValuationPage from "./TunnelValuationPage";
+
 export default class TunnelChainOfTitlesPage {
+  constructor() {
+    cy.get('catalog-chain-of-titles');
+  }
+
+  public uploadChainOfTitlesFile(p: string) {
+    uploadFile(p, 'application/pdf', 'chain-titles');
+  }
+
+  public assertChainOfTitlesFileHasUploadStatus(content: string) {
+    assertUploadStatus(content, 'chain-titles');
+  }
+
+  public clickNext() {
+    cy.get('[test-id=next]').click();
+    return new TunnelValuationPage();
+  }
 }
