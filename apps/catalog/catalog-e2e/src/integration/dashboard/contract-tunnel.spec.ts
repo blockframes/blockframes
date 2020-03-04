dd082e66843d332e6aad55996ddf1ea31e9bc182
hugo-hontans <hugohontans@gmail.com>
2020-03-04 17:34:38 +0100

    finish first part of Contract Details test

--- apps/catalog/catalog-e2e/src/integration/dashboard/contract-tunnel.spec.ts
+++ apps/catalog/catalog-e2e/src/integration/dashboard/contract-tunnel.spec.ts
@@ -15,6 +15,10 @@ const ROLES = ['Licensor', 'Licensee'];
 const EVENT = 'Contract Signature Date';
 const DURATION = '90';
 const PERIOD = 'Months';
+const CURRENCY = 'Euro';
+const PACKAGE_PRICE = '1000000';
+const PERCENTAGE = '60';
+const TRIGGERING_EVENT = 'First theatrical release';
 
 beforeEach(() => {
   clearDataAndPrepareTest();
@@ -51,5 +55,26 @@ describe('User can navigate to the movie tunnel page 1 and 2', () => {
     p5.assertDurationExists(DURATION);
     p5.selectPeriod(PERIOD);
     p5.assertPeriodIsSelected(PERIOD);
+
+    // Title Selection & Price
+    p5.selectCurrency(CURRENCY);
+    p5.assertCurrencyIsSelected(CURRENCY);
+    // TODO: search selection
+    p5.fillPackagePrice(PACKAGE_PRICE);
+    p5.assertPackagePriceExists(PACKAGE_PRICE);
+
+    // Payment Schedules
+    p5.selectPaymentsUponEvent();
+    p5.fillPercentagePaymentUponEvent(PERCENTAGE);
+    p5.assertPercentagePaymentUponEventExists(PERCENTAGE);
+    p5.selectTriggeringEvent(TRIGGERING_EVENT);
+    p5.assertTriggeringEventIsSelected(TRIGGERING_EVENT);
+
+    p5.fillPaymentTermDuration(DURATION);
+    p5.assertPaymentTermDurationExists(DURATION);
+    p5.selectPaymentDurationPeriod(PERIOD);
+    p5.assertPaymentDurationPeriodIsSelected(PERIOD);
+    p5.selectPaymentTermEvent(TRIGGERING_EVENT);
+    p5.assertPaymentTermEventIsSelected(TRIGGERING_EVENT);
   });
 });
