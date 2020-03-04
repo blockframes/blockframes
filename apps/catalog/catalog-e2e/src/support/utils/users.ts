fae44889b98e0a50357940741e118c8851252840
Laurent Senta <laurent.senta@gmail.com>
2020-03-04 10:02:41 +0100

    use prod anon users

--- apps/catalog/catalog-e2e/src/support/utils/users.ts
+++ apps/catalog/catalog-e2e/src/support/utils/users.ts
@@ -1,165 +1,270 @@
-import { User } from "./type";
+import { User } from './type';
 
 /** This list of users is linked to backend-ops/users.fixture. */
 export const USERS: Partial<User>[] = [
   {
-    uid: '8sIkEsgILVNIk6gET7SyuphCrGp1',
-    email: 'cytest@blockframes.com',
-    password: 'azerty'
-  },
-  {
-    uid: 'uJY5HNFXj8PUq8CcQvqni1Ywm0s1',
-    email: 'cytest.thejokers@blockframes.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'mykt2DfYNFNvLeslFqY8R2GRnhm1',
-    email: 'cytest.cjentertainment@blockframes.com',
-    password: 'blockframes'
-  },
-  {
-    uid: '1H0F5NJqfkMHRwLIqAnYbUECiO22',
-    email: 'forfun@vincent.fr',
-    password: 'blockframes'
-  },
-  {
-    uid: '1cDhS7rdLeZARDuRDixCxtPBUe42',
-    email: 'hello@clockworks.com',
-    password: 'blockframes'
-  },
-  {
-    uid: '3MjmItCZi2Nghuvks91frDP8hQ03',
-    email: 'hello@syncopatedfilms.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'CART4GbCHPTrpWgAbZtsmUyfwly1',
-    email: 'dazdad@gmail.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'CVE2GVi44SgwtKA0YUaOe5nfamy2',
-    email: 'hello@netflix.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'EDIWkg04UeTBgAEyVA88RtTJmUr1',
-    email: 'ddada@test.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'LKKobibLhCXxlPuDJwrxqDalSjg2',
-    email: 'hello@barunsonea.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'UCcHMvd398TTc3wsJ3KKewdy3t63',
-    email: 'hello@empire.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'UsYWmseMoOQVN2hWrJ8oxlaQm0C2',
-    email: 'hello@wearefabula.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'bPpo9XhbhpZ1Ic1RGkWKYzP7WBJ2',
-    email: 'hello@logicalpictures.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'fqK319OtikZsW4TbV3tnVXVC6hs1',
-    email: 'hello@charades.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'jJKxS7Ix2VP1fYeTCaLVSR3sk4F3',
-    email: 'hello@volgafilms.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'qMywJAW4VtY4uG5iv44AAxCLK9D3',
-    email: 'hello@thejokers.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'XfE8uBoeFRToxrPipXOHPgZJG9U2',
-    email: 'hello@cascade8.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'rjcV3BfLGlPalqUrbRX91zWqwqB2',
-    email: 'cjentertainment@blockframes.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'rjcrjcfLGlPalqUrbRX91zWqwqB2',
-    email: 'cypressCRUDdelivery@blockframes.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'LIXEkDP2jTXPxHcfZ9h05NVdu3p2',
-    email: 'amarkina@cascade8.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'vYluCjIoB5P6wjgg54azmoBbeaG2',
-    email: 'bdelorme@cascade8.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'bDNnNUpbqtN7CP0SH1OZOcNQpqm2',
-    email: 'cmandonnet@cascade8.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'UkhbKhGmG5dJh2ex0353e3ns9Uz1',
-    email: 'dkim@cascade8.com',
-    password: 'blockframes'
-  },
-  {
-    uid: '5Qpy8k9IWiXDOuIBvxz6ju3RfA72',
-    email: 'equipe@cascade8.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'my1cvypMC3cozovVoNdqnbeypPk2',
-    email: 'fguezengar@cascade8.com',
-    password: 'blockframes'
-  },
-  {
-    uid: '636h4x3FcQQUX5epbjc6EPRgx9q1',
-    email: 'hhontans@cascade8.com',
-    password: 'blockframes'
-  },
-  {
-    uid: '1M9DUDBATqayXXaXMYThZGtE9up1',
-    email: 'maltmayer@cascade8.com',
-    password: 'blockframes'
-  },
-  {
-    uid: 'Q6UitPGaItXfbwPmOKAfaaPlow42',
-    email: 'mdugue@cascade8.com',
-    password: 'blockframes'
-  },
-  {
+    password: 'blockframes',
     uid: '1DI4VrSsitRD29BGPowRPmRkVud2',
-    email: 'mfritz@cascade8.com',
-    password: 'blockframes'
+    email: 'pamela.cooper@gillespie-lawrence.fake.cascade8.com'
   },
   {
-    uid: 'MIvjAxTVULPLqmj4EY2iU7ttsME2',
-    email: 'vchoukroun@cascade8.com',
-    password: 'blockframes'
+    password: 'blockframes',
+    uid: '1M9DUDBATqayXXaXMYThZGtE9up1',
+    email: 'david.ewing@gillespie-lawrence.fake.cascade8.com'
   },
   {
-    uid: 'zjdwnjf3Eserzy1KJTMU3rNhlU42',
-    email: 'team@cascade8.com',
-    password: 'blockframes'
+    password: 'blockframes',
+    uid: '2OJUZoWtTVcew27YDZa8FQQdg5q2',
+    email: 'wayne.massey@hart-caldwell.fake.cascade8.com'
   },
   {
+    password: 'blockframes',
+    uid: '38rjqqlz0hMlYKOugRv5hh4UyA73',
+    email: 'jeffrey.spencer@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: '3JoqXILLCFeRJ6verk9BerbVaim1',
+    email: 'john.bryant@love-and-sons.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: '3juKOxpXjdeHZIWWSEzNofXc6Zs1',
+    email: 'kelly.howell@phillips-wolfe.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: '4NYUrYT2GMgMpcTnR3LJnUczX5u2',
+    email: 'thomas.hill@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: '4dZyHNwQMuYZSc0pnAxNFtB73rE2',
+    email: 'gabrielle.nguyen@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: '5Qpy8k9IWiXDOuIBvxz6ju3RfA72',
+    email: 'jared.robinson@soto-plc.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: '636h4x3FcQQUX5epbjc6EPRgx9q1',
+    email: 'johnathan.williams@gillespie-lawrence.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: '8SeuG3LfoqZYhRreaDOBH1Bob832',
+    email: 'thomas.solomon@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'ADaFh7cFkdMHtPEQGwljkrohqr93',
+    email: 'alexander.warner@turner-gray.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'Coow7hjqiRXwv6e7HNSzZjmMBGo1',
+    email: 'dustin.smith@santiago-group.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'GllJv56HasOVmDRggIny6TTGHYr2',
+    email: 'barbara.brooks@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'LIXEkDP2jTXPxHcfZ9h05NVdu3p2',
+    email: 'amber.ortiz@gillespie-lawrence.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'MDnN2GlVUeadIVJbzTToQQNAMWZ2',
+    email: 'brian.young@gillespie-lawrence.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'NGKwG6VtYXVbAv1xP1O979Y4vtB2',
+    email: 'emily.johnson@turner-gray.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'NijoMTWtoZa4OZIAZPp74ye3y4I3',
+    email: 'robert.stephenson@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'NroAS6nhWuU1aTnCa330TuJGr872',
+    email: 'jessica.fleming@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'Q6UitPGaItXfbwPmOKAfaaPlow42',
+    email: 'scott.gardner@gillespie-lawrence.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'RdjMx2N1deVbgXUIFf0X2BuvWxC3',
+    email: 'drew.bradley@santiago-group.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'SddisA4K6eeTQSUIWFaGOVnQmti1',
+    email: 'james.jackson@smith-aguilar-and-collins.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'T9xhEA45KDXnz0UhYHwIaq5qEKr2',
+    email: 'brian.jackson@hale-hicks-and-hanson.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'UkhbKhGmG5dJh2ex0353e3ns9Uz1',
+    email: 'james.gomez@gillespie-lawrence.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'UwKVrXMLIoMEcgzMiiS4xXsaVR82',
+    email: 'yolanda.moore@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'VQXiMgkYl1hwFbKxSV1vs414xpI3',
+    email: 'thomas.juarez@ellis-and-sons.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'VeOBnf0wW1e83MGChMNXe2l3QLM2',
+    email: 'john.hebert@gillespie-lawrence.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'X05QCuYZnGPyY78YkXeifLOETCj2',
+    email: 'steven.campos@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'Xb1YlB3ruMcYEyQQxrl2fC2KSGs1',
+    email: 'zachary.dickerson@campos-group.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'ZMnFf1CqZuaToaEEl30l5JimUBo1',
+    email: 'kelly.mack@campos-ltd.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'bDNnNUpbqtN7CP0SH1OZOcNQpqm2',
+    email: 'barbara.keith@gillespie-lawrence.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'dTlqocomyqZ5apFGzhuaPSZ4u682',
+    email: 'grant.bauer@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'eLPtspLbkXbr5aewjgrIe4wrFh52',
+    email: 'sarah.gregory@turner-gray.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'fuzXvdKcPKMyeSSSEDB4seZkkgF2',
+    email: 'charles.casey@ellis-and-sons.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'gj2xpZd1mYSVEMPYGU2xeRJp3MH3',
+    email: 'brandy.robles@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'iSWcgk11XgWeNxVl9ta66n5nouh2',
+    email: 'joseph.brown@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'mKRK0xMcAaWTSscA6eRHSgl4wBR2',
+    email: 'stanley.jordan@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'my1cvypMC3cozovVoNdqnbeypPk2',
+    email: 'kayla.webb@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'nHKd2U0S1wNktKpIPEBkPv0uhIs1',
+    email: 'michelle.brown@santiago-group.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'oXFmFYKrJ7bCeoEqhnIlRCkZrYP2',
+    email: 'elizabeth.gray@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'rUOaBNIaPRYdzPUmIgM5cgIqqC42',
+    email: 'billy.clark@hale-hicks-and-hanson.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 's7D2GINz32gyLjZyydzg2Hwn5OF2',
+    email: 'dana.wilson@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 't2ByvJaTVlXZBJRVAZK7VFWKoHI2',
+    email: 'jessica.roberts@davis-bryan-and-stokes.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'tGSqTWwOIPSvj1LLXzeMFy7O7Vz1',
+    email: 'elaine.odonnell@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'tem0h25h75TGPdNFXAfLLfrfrni1',
+    email: 'jeffrey.mosley@curtis-klein-and-romero.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'tmsa2YlfLEhONUmuKAIInFFKVYN2',
+    email: 'wendy.baker@curtis-klein-and-romero.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'vYluCjIoB5P6wjgg54azmoBbeaG2',
+    email: 'louis.rivera@gillespie-lawrence.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'wMwd3BkiL3XlPnL9MmCsxjL4yDl1',
+    email: 'veronica.griffin@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'wimv0GLYgDMwVwbALAZbd6KJGBy2',
+    email: 'austin.sanchez@davis-bryan-and-stokes.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'wq7tUZlVrgOt6Mn5obhPyc1jnrJ3',
+    email: 'alexis.smith@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
     uid: 'z7GwTTzxukhxUiOruEGcWrapRNs2',
-    email: 'ynikas@cascade8.com',
-    password: 'blockframes'
+    email: 'sarah.rodriguez@gillespie-lawrence.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'z7bkBiCRX2P2QyY3VlBBgz74TN53',
+    email: 'arthur.porter@blevins-williams.fake.cascade8.com'
+  },
+  {
+    password: 'blockframes',
+    uid: 'zjdwnjf3Eserzy1KJTMU3rNhlU42',
+    email: 'janet.burnett@hale-hicks-and-hanson.fake.cascade8.com'
   }
 ];
