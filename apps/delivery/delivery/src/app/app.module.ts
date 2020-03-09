1b30676101990e7b85f4a92a06598f00cbb6e347
Laurent Senta <laurent.senta@gmail.com>
2020-03-09 15:41:04 +0100

    remove environments files

--- apps/delivery/delivery/src/app/app.module.ts
+++ apps/delivery/delivery/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { RouterModule } from '@angular/router';
 
 // Akita
 import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
-import { environment } from '../environments/environment';
+import { firebase, persistenceSettings } from '@env';
 
 // Components
 import { AppComponent } from './app.component';
@@ -102,8 +102,8 @@ import { sentryDsn } from '@env';
     sentryDsn ? SentryModule : [],
 
     // Firebase
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule.enablePersistence(environment.persistenceSettings),
+    AngularFireModule.initializeApp(firebase),
+    AngularFirestoreModule.enablePersistence(persistenceSettings),
     AngularFireFunctionsModule,
     AngularFirePerformanceModule,
     AngularFireAuthModule,
