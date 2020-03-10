84b9998c154b9fe360eab6d86f7c68814e5b83f2
DK <jenna_kim1@hotmail.com>
2020-03-10 15:07:36 +0100

    except changing table info

--- apps/catalog/catalog/src/app/marketplace/movie/selection/selection.module.ts
+++ apps/catalog/catalog/src/app/marketplace/movie/selection/selection.module.ts
@@ -16,6 +16,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [MarketplaceSelectionComponent],
@@ -35,6 +36,7 @@ import { MatSelectModule } from '@angular/material/select';
     MatFormFieldModule,
     MatIconModule,
     MatSelectModule,
+    MatCardModule,
 
     RouterModule.forChild([
       {
