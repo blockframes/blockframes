fd2626937dc2f301c68cb36bde5ad1b68516fdcc
bruce <delorme.bruce.michel@gmail.com>
2020-03-10 16:48:34 +0100

    added pending org guard

---
+++ libs/organization/src/lib/guard/pending-organization.guard.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {
+  OrganizationService,
+  OrganizationState,
+  OrganizationQuery
+} from '../+state';
+import { Router } from '@angular/router';
+import { CollectionGuard, CollectionGuardConfig } from 'akita-ng-fire';
+import { map, switchMap } from 'rxjs/operators';
+import { AuthQuery } from '@blockframes/auth';
+import { of } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+@CollectionGuardConfig({ awaitSync: true })
+export class PendingOrganizationGuard extends CollectionGuard<OrganizationState> {
+  constructor(
+    protected service: OrganizationService,
+    protected router: Router,
+    private query: OrganizationQuery,
+    private authQuery: AuthQuery
+  ) {
+    super(service);
+  }
+
+  sync() {
+    return this.authQuery.user$.pipe(
+      switchMap(user => {
+        if (!user.orgId) {
+          return of('/c/organization');
+        } else {
+          return this.service.syncActive({ id: user.orgId }).pipe(
+            map(_ => this.query.getActive()),
+            map(org => {
+              if (!org) {
+                return '/c/organization';
+              }
+
+              if (org.status === 'accepted') {
+                return '/c/o/dashboard/home';
+              }
+            })
+          );
+        }
+      })
+    );
+  }
+}
