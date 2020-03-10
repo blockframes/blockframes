b621fb761417909f1b17068ded5eedfaab6e2e1e
Yohann <ioa.nikas@gmail.com>
2020-03-10 16:09:55 +0100

    E2E: add functions to call 'fillKey' methods from the page

--- apps/catalog/catalog-e2e/src/support/pages/organization/OrganizationCreatePage.ts
+++ apps/catalog/catalog-e2e/src/support/pages/organization/OrganizationCreatePage.ts
@@ -1,10 +1,10 @@
-import OrganizationAppAccessPage from "./OrganizationAppAccessPage";
-import { Location, Organization, BankAccount } from "../../utils/type";
+import OrganizationAppAccessPage from './OrganizationAppAccessPage';
+import { Location, Organization, BankAccount } from '../../utils/type';
 
-type Entity = 'org' | 'bank' | 'lol';
+type Entity = 'org' | 'bank';
 
 function form(entity: Entity): string {
-  switch(entity) {
+  switch (entity) {
     case 'org':
       return 'organization-form-address';
     case 'bank':
@@ -14,57 +14,52 @@ function form(entity: Entity): string {
   }
 }
 
+/**
+ * This function takes fields (key: value) and calls `target.fillKey(value)`
+ * @param target the page containing the methods we want to call.
+ * The methods needs to begin with prefix 'fill'.
+ * @param fields properties we want to use to call a method of the target
+ * @param entity optional parameter to select specific forms in methods
+ */
+function fillAllFields(target, fields, entity?: Entity) {
+  Object.entries(fields).forEach(([key, value]) => {
+    const newKey = key.charAt(0).toUpperCase() + key.substring(1);
+    const methodName = `fill${newKey}`;
+    target[methodName](value, entity);
+  });
+}
+
 export default class OrganizationCreatePage {
   constructor() {
     cy.get('organization-create');
   }
 
   public assertMoveToOrgCreatePage() {
-    cy.location().should((loc) => {
-      expect(loc.pathname).to.eq('/c/organization/create')
-    })
+    cy.location().should(loc => {
+      expect(loc.pathname).to.eq('/c/organization/create');
+    });
   }
 
-  /**
-   * Fills every field of organization create form
-   * @param excludeName can be set on true to not fill the organization name
-   */
-  public fillEveryFields(organization: Organization, excludeName: boolean = false) {
-    if (!excludeName) {
-      this.fillCompanyName(organization.name);
-    }
-    this.fillEmailAddress(organization.email);
-    this.fillAddress(organization.address, 'org');
-    this.fillActivity(organization.activity);
-    this.fillFiscalNumber(organization.fiscalNumber);
-    this.fillBankInformation(organization.bankAccount);
+  /** Fills every field of organization create form */
+  public fillEveryFields(organization: Partial<Organization>) {
+    fillAllFields(this, organization, 'org');
   }
 
   /** Fills every fields of address */
   public fillAddress(address: Location, entity: Entity) {
-    this.fillStreet(address.street, entity);
-    this.fillCity(address.city, entity);
-    this.fillZipCode(address.zipCode, entity);
-    this.fillCountry(address.country, entity);
-    if (!!address.phoneNumber) {
-      this.fillPhoneNumber(address.phoneNumber);
-    }
+    fillAllFields(this, address, entity);
   }
 
   /** Fills every fields of bank information */
-  public fillBankInformation(bankAccount: BankAccount) {
-    this.fillIBAN(bankAccount.IBAN);
-    this.fillBIC(bankAccount.BIC);
-    this.fillHolderName(bankAccount.holderName);
-    this.fillBankName(bankAccount.bankName);
-    this.fillAddress(bankAccount.address, 'bank');
+  public fillBankAccount(bankAccount: BankAccount) {
+    fillAllFields(this, bankAccount, 'bank');
   }
 
-  public fillCompanyName(name: string) {
+  public fillName(name: string) {
     cy.get('organization-form input[test-id=name]').type(name);
   }
 
-  public fillEmailAddress(email: string) {
+  public fillEmail(email: string) {
     cy.get('organization-form input[type=email]').type(email);
   }
 
@@ -82,7 +77,9 @@ export default class OrganizationCreatePage {
 
   public fillCountry(country: string, entity: Entity) {
     cy.get(`${form(entity)} form-country input[test-id=country]`).click();
-    cy.get('mat-option').contains(country).click();
+    cy.get('mat-option')
+      .contains(country)
+      .click();
   }
 
   public fillPhoneNumber(phoneNumber: string) {
@@ -115,7 +112,7 @@ export default class OrganizationCreatePage {
 
   /** If navigate is set to false, this doesn't return a new page. */
   public clickCreate(navigate: boolean = true) {
-    cy.get('organization-create button[test-id=create]').click({force: true});
+    cy.get('organization-create button[test-id=create]').click({ force: true });
     if (navigate) {
       return new OrganizationAppAccessPage();
     }
