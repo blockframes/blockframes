90bae2fba301b5957f048c94f0c5a5a0408330a2
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-03-11 12:42:44 +0100

    Update storybook toolkit

--- libs/ui/src/lib/theme/index.ts
+++ libs/ui/src/lib/theme/index.ts
@@ -1,4 +1,3 @@
 export { ImgAssetModule } from './img-asset.module';
 export { PathAssetModule } from './path-asset.module';
-export { ThemeWidgetModule } from './theme-widget/theme-widget.module';
 export { ThemeService } from './theme.service';
