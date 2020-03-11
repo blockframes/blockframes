521799c619b1ae41a022d8a15a0ed718d48d4872
maxstgt <max.j.f@gmx.de>
2020-03-11 10:03:55 +0100

    worked on FG change request

--- libs/organization/src/lib/forms/organization-form/organization-form.component.ts
+++ libs/organization/src/lib/forms/organization-form/organization-form.component.ts
@@ -1,9 +1,7 @@
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 import { network, baseEnsDomain } from '@env';
 import { getProvider, orgNameToEnsDomain } from '@blockframes/ethers/helpers';
 import { OrganizationService } from './../../+state/organization.service';
-import { debounceTime, distinctUntilChanged, tap } from 'rxjs/operators';
-import { Subscription } from 'rxjs';
-import { Component, ChangeDetectionStrategy, Input, OnInit, OnDestroy } from '@angular/core';
 import { OrganizationForm } from '@blockframes/organization/forms/organization.form';
 
 @Component({
@@ -12,42 +10,28 @@ import { OrganizationForm } from '@blockframes/organization/forms/organization.f
   styleUrls: ['./organization-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OrganizationFormComponent implements OnInit, OnDestroy {
+export class OrganizationFormComponent {
   @Input() form: OrganizationForm;
 
-  private sub: Subscription;
-
   constructor(private service: OrganizationService) { }
-
-  ngOnInit() {
-    this.sub = this.form.get('name').valueChanges.pipe(
-      debounceTime(200),
-      distinctUntilChanged(),
-      tap(name => this.uniqueOrgName(name))
-    ).subscribe();
-  }
 
   get bankAccounts() {
     return this.form.get('bankAccounts');
   }
 
   /** Check if the `name` field of an Organization create form already exists as an ENS domain */
-  private async uniqueOrgName(name: string) {
+  public async uniqueOrgName() {
+    const orgName = this.form.get('name').value
 
     let uniqueOnEthereum = false;
     let uniqueOnFirestore = false;
 
-    const orgENS = orgNameToEnsDomain(name, baseEnsDomain);
+    const orgENS = orgNameToEnsDomain(orgName, baseEnsDomain);
     const provider = getProvider(network);
     const orgEthAddress = await provider.resolveName(orgENS);
     uniqueOnEthereum = !orgEthAddress ? true : false;
 
-    uniqueOnFirestore = await this.service.orgNameExist(name).then(exist => !exist);
+    uniqueOnFirestore = await this.service.orgNameExist(orgName).then(exist => !exist);
     (uniqueOnEthereum && uniqueOnFirestore) ? null : this.form.get('name').setErrors({ notUnique: true });
-  }
-
-
-  ngOnDestroy() {
-    this.sub.unsubscribe();
   }
 }
