751686519cfab2b3ae13f65c67ce4629e3f96a6d
Yohann <ioa.nikas@gmail.com>
2020-03-12 13:56:25 +0100

    E2E: Store string values into const and use selectors before test-id

--- apps/catalog/catalog-e2e/src/support/pages/organization/OrganizationCongratulationPage.ts
+++ apps/catalog/catalog-e2e/src/support/pages/organization/OrganizationCongratulationPage.ts
@@ -1,11 +1,14 @@
+const PATH = '/c/organization/create-congratulations';
+
 export default class OrganizationCongratulationPage {
+
   constructor() {
     cy.get('organization-create-feedback');
   }
 
   public assertMoveToOrgCongratulationPage() {
-    cy.location().should((loc) => {
-      expect(loc.pathname).to.eq('/c/organization/create-congratulations')
+    cy.location().should(loc => {
+      expect(loc.pathname).to.eq(PATH)
     })
   }
 }
