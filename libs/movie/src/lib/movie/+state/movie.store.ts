8641c7358fde3008e3489ac5d5f71196f1c15101
hugo-hontans <hugohontans@gmail.com>
2020-03-12 15:04:43 +0100

    use movie analytics store for dashboard home and titles

--- libs/movie/src/lib/movie/+state/movie.store.ts
+++ libs/movie/src/lib/movie/+state/movie.store.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { EntityStore, StoreConfig, EntityState, ActiveState } from '@datorama/akita';
 import { Movie, createMovie } from './movie.model';
-import { MovieAnalytics } from './movie.model';
 
 export interface MovieState extends EntityState<Movie, string>, ActiveState<string> {}
 
