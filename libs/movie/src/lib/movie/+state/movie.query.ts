693c65a34461aa0a407544507e4ed3114a32defd
hugo-hontans <hugohontans@gmail.com>
2020-03-12 15:04:06 +0100

    create a analytics store in movie store and a analytics query in movie query

--- libs/movie/src/lib/movie/+state/movie.query.ts
+++ libs/movie/src/lib/movie/+state/movie.query.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { QueryEntity } from '@datorama/akita';
 import { MovieStore, MovieState } from './movie.store';
 import { Movie } from './movie.model';
+import { MovieAnalytics } from './movie.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieQuery extends QueryEntity<MovieState, Movie> {
   movieFormChanges$ = this.select(state => state.akitaForm as Movie);
+  analytics = new QueryEntity<MovieState, MovieAnalytics>(this.store.analytics);
 
   constructor(protected store: MovieStore) {
     super(store);
--- libs/movie/src/lib/movie/+state/movie.store.ts
+++ libs/movie/src/lib/movie/+state/movie.store.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { EntityStore, StoreConfig, EntityState, ActiveState } from '@datorama/akita';
 import { Movie, createMovie } from './movie.model';
+import { MovieAnalytics } from './movie.model';
 
 export interface MovieState extends EntityState<Movie, string>, ActiveState<string> {}
 
 @Injectable({ providedIn: 'root' })
 @StoreConfig({ name: 'movies', idKey: 'id' })
 export class MovieStore extends EntityStore<MovieState> {
+  analytics = new EntityStore<MovieState>(null, { name: 'movieAnalytics', idKey: 'movieId' });
 
   constructor() {
     super();
