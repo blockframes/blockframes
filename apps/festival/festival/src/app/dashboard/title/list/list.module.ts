601dde457b8bf16165c47afb8ed5621d39c62223
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-03-18 16:48:27 +0100

    Add festival dashboard title list

--- apps/festival/festival/src/app/dashboard/title/list/list.module.ts
+++ apps/festival/festival/src/app/dashboard/title/list/list.module.ts
@@ -1,13 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { TableFilterModule } from '@blockframes/ui/list/table-filter/table-filter.module';
+
 import { ListComponent } from './list.component';
+import { ImgAssetModule } from '@blockframes/ui/theme/img-asset.module';
 
-
+// Material
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [ListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    TableFilterModule,
+    ImgAssetModule,
+    // Material
+    MatButtonModule,
+    MatTabsModule,
+    MatIconModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    // Router
+    RouterModule.forChild([{ path: '', component: ListComponent }])
   ]
 })
 export class TitleListModule { }
