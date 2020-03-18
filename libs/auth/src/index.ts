e451a562cbc198201b4bede44dff391408a0f90e
Yohann <ioa.nikas@gmail.com>
2020-03-18 14:57:54 +0100

    Refactor user redirection guard and move it into auth lib

--- libs/auth/src/index.ts
+++ libs/auth/src/index.ts
@@ -2,7 +2,10 @@ export * from './lib/auth.module';
 export * from './lib/+state/auth.query';
 export * from './lib/+state/auth.service';
 export * from './lib/+state/auth.store';
+
+// Guards
 export * from './lib/guard/auth.guard';
+export * from './lib/guard/user-redirection.guard';
 
 // Component Module
 export * from './lib/components/email-verify/email-verify.module';
