8e19ad7876cd43ff07e8b13afcfee26edc6e8cb8
hugo-hontans <hugohontans@gmail.com>
2020-03-26 09:48:48 +0100

    revert users 0 and 1

--- libs/e2e/src/lib/utils/users.ts
+++ libs/e2e/src/lib/utils/users.ts
@@ -4,13 +4,13 @@ import { User } from './type';
 export const USERS: Partial<User>[] = [
   {
     password: 'blockframes',
-    uid: '1M9DUDBATqayXXaXMYThZGtE9up1',
-    email: 'david.ewing@gillespie-lawrence.fake.cascade8.com'
+    uid: '1DI4VrSsitRD29BGPowRPmRkVud2',
+    email: 'pamela.cooper@gillespie-lawrence.fake.cascade8.com'
   },
   {
     password: 'blockframes',
-    uid: '1DI4VrSsitRD29BGPowRPmRkVud2',
-    email: 'pamela.cooper@gillespie-lawrence.fake.cascade8.com'
+    uid: '1M9DUDBATqayXXaXMYThZGtE9up1',
+    email: 'david.ewing@gillespie-lawrence.fake.cascade8.com'
   },
   {
     password: 'blockframes',
