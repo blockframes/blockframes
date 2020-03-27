863b7400c969b43a231389b3a7c1abc22d7e5461
Ioannis <ioa.nikas@gmail.com>
2020-03-27 10:02:52 +0100

    Remove dist-deals index.ts and references

--- libs/admin/src/lib/admin-panel/pages/movie/movie.component.ts
+++ libs/admin/src/lib/admin-panel/pages/movie/movie.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MovieAdminForm } from '../../forms/movie-admin.form';
 import { staticModels } from '@blockframes/utils/static-model';
-import { DistributionDealService } from '@blockframes/distribution-deals';
+import { DistributionDealService } from '@blockframes/distribution-deals/+state/distribution-deal.service';
 import { getValue } from '@blockframes/utils/helpers';
 import { storeType, storeStatus } from '@blockframes/movie/+state/movie.firestore';
 import { Movie } from '@blockframes/movie/+state/movie.model';
