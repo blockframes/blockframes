86492e89de3453fae6b1bb0037a7ab9b72dbd1a3
bruce <delorme.bruce.michel@gmail.com>
2020-04-02 17:25:35 +0200

    WIP Example

--- libs/notification/src/lib/invitation/+state/invitation.firestore.ts
+++ libs/notification/src/lib/invitation/+state/invitation.firestore.ts
@@ -9,6 +9,7 @@ export interface Invitation {
   id: string;
   app: App;
   type: InvitationType;
+  mode?: InvitationMode,
   status: InvitationStatus;
   date: Timestamp;
   /** 
@@ -26,33 +27,45 @@ export type InvitationOrUndefined = InvitationDocument | undefined;
 /** Specific Invitation send by an Organization to another Organization to work on a document. */
 export interface InvitationToWorkOnDocument extends Invitation {
   type: 'toWorkOnDocument';
-  /** @dev TitleId for example*/ 
+  /** @dev TitleId for example*/
   docId: string;
+  /**
+   *  @TODO (#244) should rename to better understand
+   * who is inviting and who is invited.
+   */
   organization: PublicOrganization;
   user?: PublicUser;
 }
 
-/** Specific Invitation send by an Organization to an User to attend an Event. */
-export interface InvitationToAttendAnEvent extends Invitation {
-  type: 'invitationToAttendAnEvent';
-  /** @dev EventId */ 
-  docId: string;
-  organization: PublicOrganization;
-  user: PublicUser;
-}
 
-/** Specific Invitation send by an User to attend an Event. */
-export interface RequestToAttendAnEvent extends Invitation {
-  type: 'requestToAttendAnEvent';
-  /** @dev EventId */ 
+
+/** 
+ * Specific Invitation/Request to attend an Event.
+ * @see #2244 When a invitation is created
+ * A backend function will check if:
+ * If we have an user or an org we can create a notification.
+ * If we have an email, the function will send an email.
+ * If user created an account, we will then be able to replace email by user
+ * */
+export interface InvitationToAnEvent extends Invitation {
+  mode: InvitationMode,
+  type: 'event';
+  fromOrg?: PublicOrganization,
+  fromUser?: PublicUser,
+  toOrg?: PublicOrganization,
+  toUser?: PublicUser,
+  toEmail?: string,
+  /** @dev EventId */
   docId: string;
-  organization: PublicOrganization;
-  user: PublicUser;
 }
 
 /**  Specific Invitation send by an Organization to a User to join it. */
 export interface InvitationFromOrganizationToUser extends Invitation {
   type: 'fromOrganizationToUser';
+  /**
+   *  @TODO (#244) should rename to better understand
+   * who is inviting and who is invited.
+   */
   user: PublicUser;
   organization: PublicOrganization;
 }
@@ -60,6 +73,10 @@ export interface InvitationFromOrganizationToUser extends Invitation {
 /** Specific Invitation send by a User to join an Organization. */
 export interface InvitationFromUserToOrganization extends Invitation {
   type: 'fromUserToOrganization';
+  /**
+   *  @TODO (#244) should rename to better understand
+   * who is inviting and who is invited.
+   */
   user: PublicUser;
   organization: PublicOrganization;
 }
@@ -68,4 +85,6 @@ export interface InvitationFromUserToOrganization extends Invitation {
 export type InvitationStatus = 'accepted' | 'declined' | 'pending';
 
 /** Type of Invitation depending of its purpose. */
-export type InvitationType = 'fromUserToOrganization' | 'fromOrganizationToUser' | 'toWorkOnDocument' | 'invitationToAttendAnEvent' | 'requestToAttendAnEvent';
+export type InvitationType = 'fromUserToOrganization' | 'fromOrganizationToUser' | 'toWorkOnDocument' | 'event' ;
+
+export type InvitationMode = 'request' | 'invitation';
