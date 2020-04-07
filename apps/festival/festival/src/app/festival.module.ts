6b6e8a3638b08a6433130ab2a25cc8bb8b477c45
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-04-07 16:20:50 +0200

    explicitly define routes to be prerendered

--- apps/festival/festival/src/app/festival.module.ts
+++ apps/festival/festival/src/app/festival.module.ts
@@ -1,43 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
-// import { MovieCollectionGuard } from '@blockframes/movie/guards/movie-collection.guard';
-// import { createRoutes } from '@blockframes/utils/routes/create-routes';
+import { MovieCollectionGuard } from '@blockframes/movie/guards/movie-collection.guard';
+import { createRoutes } from '@blockframes/utils/routes/create-routes';
 
 // TODO: Add AppGuard
 
-const routes: Routes = [ {
-  path: 'landing',
-  loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)
-} ];
-// const routes: Routes = createRoutes({
-//   appName: 'festival',
-//   landing: {
-//     path: '',
-//     loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)
-//   },
-//   appsRoutes: [
-//     {
-//     path: '',
-//     redirectTo: 'marketplace',
-//     pathMatch: 'full'
-//   },
-//   {
-//     path: 'marketplace',
-//     canActivate: [MovieCollectionGuard /*, CatalogAppGuard*/],
-//     canDeactivate: [MovieCollectionGuard],
-//     loadChildren: () => import('./marketplace/marketplace.module').then(m => m.MarketplaceModule)
-//   },
-//   {
-//     path: 'dashboard',
-//     canActivate: [/*CatalogAppGuard*/],
-//     loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
-//   },
-//   {
-//     path: 'admin',
-//     loadChildren: () => import('@blockframes/admin/admin/admin.module').then(m => m.AdminModule)
-//   }
-//   ]
-// });
+const routes: Routes = createRoutes({
+  appName: 'festival',
+  landing: {
+    path: '',
+    loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)
+  },
+  appsRoutes: [
+    {
+      path: '',
+      redirectTo: 'marketplace',
+      pathMatch: 'full'
+    },
+    {
+      path: 'marketplace',
+      canActivate: [MovieCollectionGuard /*, CatalogAppGuard*/],
+      canDeactivate: [MovieCollectionGuard],
+      loadChildren: () => import('./marketplace/marketplace.module').then(m => m.MarketplaceModule)
+    },
+    {
+      path: 'dashboard',
+      canActivate: [/*CatalogAppGuard*/],
+      loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+    },
+    {
+      path: 'admin',
+      loadChildren: () => import('@blockframes/admin/admin/admin.module').then(m => m.AdminModule)
+    }
+  ]
+});
 
 @NgModule({
   declarations: [],
