e6c4e60506328f1cf0e9874c5aea3adb02a45721
hugo-hontans <hugohontans@gmail.com>
2020-04-09 11:53:27 +0200

    implement empty image when no deals in marketplace/deals

--- apps/catalog/catalog/src/app/marketplace/deal/list/list.module.ts
+++ apps/catalog/catalog/src/app/marketplace/deal/list/list.module.ts
@@ -3,8 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { DealListComponent } from './list.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ImgAssetModule } from '@blockframes/ui/theme/img-asset.module';
 
+import { DealListComponent } from './list.component';
 import { ContractTableModule } from '@blockframes/contract/contract/components';
 
 // Material
@@ -15,8 +17,10 @@ import { MatTabsModule } from '@angular/material/tabs';
   declarations: [DealListComponent],
   imports: [
     CommonModule,
+    FlexLayoutModule,
     ReactiveFormsModule,
     ContractTableModule,
+    ImgAssetModule,
 
     // Material
     MatTabsModule,
