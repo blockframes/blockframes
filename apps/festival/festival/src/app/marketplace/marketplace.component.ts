feead14cd679237a79cb111158a9cc2284fe698d
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-04-10 18:24:29 +0200

    add invitation to marketplace

--- apps/festival/festival/src/app/marketplace/marketplace.component.ts
+++ apps/festival/festival/src/app/marketplace/marketplace.component.ts
@@ -2,11 +2,12 @@ import { Component, ChangeDetectionStrategy, OnInit, ViewChild, OnDestroy, After
 import { MatSidenav } from '@angular/material/sidenav';
 import { CatalogCartQuery } from '@blockframes/organization/cart/+state/cart.query';
 import { RouterQuery } from '@datorama/akita-ng-router-store';
-import { Observable, Subscription } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, Subscription, combineLatest } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { AuthQuery } from '@blockframes/auth/+state/auth.query';
 import { Wishlist } from '@blockframes/organization/organization/+state/organization.model';
 import { routeAnimation } from '@blockframes/utils/animations/router-animations';
+import { InvitationService, InvitationQuery } from '@blockframes/invitation/+state';
 
 @Component({
   selector: 'festival-marketplace',
@@ -20,16 +21,26 @@ export class MarketplaceComponent implements OnInit, AfterViewInit, OnDestroy {
   public user$ = this.authQuery.select('profile');
   public currentWishlist$: Observable<Wishlist>;
   public wishlistCount$: Observable<number>;
+  public invitationCount$ = this.invitationQuery.selectCount();
 
   @ViewChild('sidenav') sidenav: MatSidenav;
 
   constructor(
     private catalogCartQuery: CatalogCartQuery,
+    private invitationService: InvitationService,
+    private invitationQuery: InvitationQuery,
     private authQuery: AuthQuery,
     private routerQuery: RouterQuery,
   ) {}
 
   ngOnInit() {
+    // DON'T LET ME MERGE : unsubscribe
+    this.authQuery.user$.pipe(
+      switchMap(user => combineLatest([
+        this.invitationService.syncCollection(ref => ref.where('toUser', '==', user.uid)),
+        this.invitationService.syncCollection(ref => ref.where('toEmail', '==', user.email))
+      ]))
+    ).subscribe();
     this.currentWishlist$ = this.catalogCartQuery.wishlistWithMovies$.pipe(
       map(wishlists => wishlists.find(wishlist => wishlist.status === 'pending'))
     );
