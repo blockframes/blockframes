a7f7bdf7bc4e6450c15e853759974e9798303cee
pldespaigne <pl.despaigne@gmail.com>
2020-04-16 11:23:54 +0200

    fix search

--- apps/festival/festival/src/app/marketplace/title/list-new/list.component.ts
+++ apps/festival/festival/src/app/marketplace/title/list-new/list.component.ts
@@ -3,14 +3,17 @@ import {
   ChangeDetectionStrategy,
   OnInit,
   OnDestroy,
+  Inject,
 } from '@angular/core';
 import { Subscription, Observable } from 'rxjs';
-
 import { MovieService, MovieQuery } from '@blockframes/movie/+state';
-import { algolia } from '@env';
 import { FormControl } from '@angular/forms';
 import { staticModels } from '@blockframes/utils/static-model';
 import { MovieSearchForm } from '@blockframes/movie/form/search.form';
+import { map, distinctUntilChanged, debounceTime, filter, switchMap, pluck, startWith } from 'rxjs/operators';
+import { MoviesIndex } from '@blockframes/utils/algolia';
+import { Index } from 'algoliasearch';
+import { sortMovieBy } from '@blockframes/utils/akita-helper/sort-movie-by';
 
 
 @Component({
@@ -29,20 +32,37 @@ export class ListComponent implements OnInit, OnDestroy {
   public genres = staticModels['GENRES'];
   public countries = staticModels['TERRITORIES'];
   public movieProductionStatuses = staticModels['MOVIE_STATUS'];
-  public movieIndex = algolia.indexNameMovies;
-
 
   public filterForm = new MovieSearchForm();
 
   constructor(
     private movieService: MovieService,
     private movieQuery: MovieQuery,
+    @Inject(MoviesIndex) public movieIndex: Index,
   ) { }
 
   ngOnInit() {
     this.sub = this.movieService.syncCollection(ref => ref.limit(30)).subscribe();
 
-    this.movieSearchResults$ = this.movieQuery.selectAll({limitTo: 10});
+    this.movieSearchResults$ = this.filterForm.valueChanges.pipe(
+      debounceTime(300),
+      filter(search => !MovieSearchForm.isEmpty(search)),
+      distinctUntilChanged((a, b) => MovieSearchForm.equals(a, b)),
+      map(() => this.filterForm.toAlgoliaQuery()),
+      switchMap(query => this.movieIndex.search(query)),
+      pluck('hits'),
+      map(result => result.map(movie => movie.objectID)),
+
+      // join retrieved movieIds from algolia with the movies from the state
+      switchMap(movieIds => this.movieQuery.selectAll({
+        filterBy: movie => movieIds.includes(movie.id),
+        sortBy: (a, b) => sortMovieBy(a, b, this.sortByControl.value),
+      })),
+
+      // display the first 10 movies from the state (no useless queries)
+      // prevent the user to see an empty page at the beginning
+      startWith(this.movieQuery.getAll({limitTo: 10})),
+    );
   }
 
   ngOnDestroy() {
