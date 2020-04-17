895698ba7f56c12f3c696c47c1ed61140d60ac4a
Laurent Senta <laurent.senta@gmail.com>
2020-04-17 15:19:12 +0200

    demo1: catch up envs

--- env/demo/env.demo1.ts
+++ env/demo/env.demo1.ts
@@ -10,6 +10,11 @@ export const persistenceSettings = {
   experimentalTabSynchronization: true
 };
 
+// Analytics
+// =========
+
+export const sentryDsn = 'https://a5f8a3139c754fa088453dbd710d9418@sentry.io/1540126';
+
 // Firebase
 // ========
 
@@ -30,7 +35,8 @@ export const firebase = {
 export const algolia = {
   appId: '8E9YO1I9HB',
   searchKey: '4a2990a293c0ee0bfde9ebd66270a47f',
-  indexNameOrganizations: 'demo1_orgs'
+  indexNameOrganizations: 'demo1_orgs',
+  indexNameMovies: 'demo1_movies'
 };
 
 // Ethereum
@@ -65,3 +71,58 @@ export const relayer = {
 
 export const backupBucket = 'demo1-backups';
 export const sendgridAPIKey = null; // defined in functions.config, see backend-functions/environments
+
+
+// Sendgrid Emails
+
+export const templateIds = {
+  welcomeMessage: 'd-eb8e1eb7c5a24eb8af1d2d32539ad714',
+  userVerifyEmail: 'd-81438bdf511b43cfa866ca63a45a02ae',
+  orgInviteUser: 'd-7a0edb51795c493d9514fe4a595b40ac',
+  userCredentials: 'd-a34ce9ea59c5477f9feae8f556157b6b',
+  orgAccepted: 'd-8c5f7009cd2f4f1b877fa168b4efde48',
+  joinAnOrgPending: 'd-88665c2583dc46ea85588a39fa8ca1ee',
+  joinYourOrg: 'd-b1ab5d21def145ccb759520e2d984436',
+  resetPassword: 'd-6a0710945bc841ffb6955e3dc202704c',
+  userHasJoined: 'd-f84d8c5a70884316870ca4ef657e368f',
+  userRequestAccepted: 'd-d32b25a504874a708de6bfc50a1acba7',
+  wishlistPending: 'd-e0cd8970d19346eea499a81f67f1a557',
+}
+
+// Yandex Metrika Id
+export const yandexId = 0;
+
+// Intercom
+// ========
+export const intercomId = 'srwfltp4';
+
+// Quorum
+// ========
+export const quorum = {
+  archipelNode: {
+    url: 'https://e0rf4hbql8-e0cy67u40h-rpc.de0-aws.kaleido.io',
+    user: 'e0xwcvgknw',
+    privateFor: 'PJg4NoFMk73mGCbkJ7/griaiKfkbS+edhfjO5PzztQs=',
+    ethAddress: '0x7E5D163D7390A6068d44C8e2F3c2861B5133daa4',
+  },
+  pulsarlNode: {
+    url: 'https://e0rf4hbql8-e0zhtusyfh-rpc.de0-aws.kaleido.io',
+    user: 'e0xwcvgknw',
+    privateFor: 'fnF4IPKvDcmM9bgmEKHoYjNyXG6cXqJjv806RK1F5y8=',
+    ethAddress: '0x43c92D51ba8c0B83062F8116B036D6616ebe4746',
+  },
+  bankNode: {
+    url: 'https://e0rf4hbql8-e0jbt507aa-rpc.de0-aws.kaleido.io',
+    user: 'e0xwcvgknw',
+    privateFor: 'Tj879+7P6IgX2UJTOLtWx5IjrPlABb7HO//kNNbnt28=',
+    ethAddress: '0xe795245444d459CD0d8e12A26232646B5521e72F',
+  },
+}
+
+// BigQuery
+// ========
+export const bigQueryAnalyticsTable = 'blockframes-staging.analytics_194475853.events_';
+
+// Archipel Content OrgId
+// ======================
+export const centralOrgID = "jnbHKBP5YLvRQGcyQ8In";
