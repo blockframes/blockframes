4f63fab6f02607ffb66f219bcfa9de54f04d9b83
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-04-17 17:11:51 +0200

    update invitation view

--- libs/event/src/lib/layout/view/view.component.ts
+++ libs/event/src/lib/layout/view/view.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Event } from '../../+state/event.model';
+import { InvitationService, Invitation } from '@blockframes/invitation/+state';
+import { AuthQuery } from '@blockframes/auth/+state';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'event-view',
@@ -7,12 +11,35 @@ import { Event } from '../../+state/event.model';
   styleUrls: ['./view.component.scss']
 })
 export class EventViewComponent implements OnInit {
+  private _event = new BehaviorSubject<Event>(null);
+  event$ = this._event.asObservable();
+  invitation$: Observable<Invitation>;
 
-  @Input() event: Event;
-
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() set event(event: Event) {
+    this._event.next(event);
   }
 
+  constructor(
+    private invitationService: InvitationService,
+    private authQuery: AuthQuery
+  ) { }
+
+  ngOnInit(): void {
+    const uid = this.authQuery.userId;
+    // this.invitation$ = this.event$.pipe(
+    //   switchMap(event => {
+    //     const querFn = ref => ref.where('docId', '==', this.event.id).where('toUser.uid', '==', uid);
+    //     this.invitationService.valueChanges()
+    //   })
+
+    // )
+  }
+
+  addToCalendar() {}
+
+  accept() {
+    // this.invitationService.acceptInvitation(this.event);
+  }
+
+  refuse() {}
 }
--- libs/event/src/lib/layout/view/view.module.ts
+++ libs/event/src/lib/layout/view/view.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventViewComponent } from './view.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { BackgroundAssetModule } from '@blockframes/ui/theme/background-asset.module';
+import { EventRangeModule } from '../../pipes/event-range.pipe';
+
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -11,6 +17,10 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   imports: [
     CommonModule,
     FlexLayoutModule,
+    BackgroundAssetModule,
+    EventRangeModule,
+    MatButtonModule,
+    MatIconModule,
   ]
 })
 export class EventViewModule { }
