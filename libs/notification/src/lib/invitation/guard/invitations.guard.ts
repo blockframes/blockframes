3b3e0a64b387fc93a4d29d773618effd98ed9781
Laurent Senta <laurent.senta@gmail.com>
2020-04-24 15:25:35 +0200

    check security

--- libs/notification/src/lib/invitation/guard/invitations.guard.ts
+++ libs/notification/src/lib/invitation/guard/invitations.guard.ts
@@ -6,12 +6,33 @@ import { InvitationService } from '../+state/invitation.service';
 import { switchMap } from 'rxjs/operators';
 import { combineLatest } from 'rxjs';
 
+const testData = (query, name) => {
+  query.get().then(data => {
+    console.info(name, 'success!', data.docs.map(x => x.data()))
+  }).catch(err => {
+    console.error(name, 'failed!', err)
+  })
+}
+
 @Injectable({ providedIn: 'root' })
 @CollectionGuardConfig({ awaitSync: false })
 export class InvitationGuard extends CollectionGuard<InvitationState> {
   constructor(service: InvitationService, private authQuery: AuthQuery) {
     super(service);
-  }
+    // @ts-ignore
+    window.service = service;
+    // @ts-ignore
+    window.db = service.db.firestore;
+    //@ts-ignore
+    testData(service.db.collection('invitations').ref.where('fromOrg.id', '==', 'jnbHKBP5YLvRQGcyQ8In'), 'query my org')
+    //@ts-ignore
+    testData(service.db.collection('invitations').ref.where('fromUser.uid', '==', '1M9DUDBATqayXXaXMYThZGtE9up1'), 'query my user')
+    //@ts-ignore
+    testData(service.db.collection('invitations').ref, 'query all')
+    //@ts-ignore
+    testData(service.db.collection('invitations').ref.where('fromOrg.id', '==', 'XXX'), 'query another org')
+    //@ts-ignore
+    testData(service.db.collection('invitations').ref.where('fromUser.uid', '==', 'jnbHKBP5YLvRQGcyQ8In'), 'query another user')  }
 
   /** This sync on invitations where userId is the same as the connected user id */
   sync() {
