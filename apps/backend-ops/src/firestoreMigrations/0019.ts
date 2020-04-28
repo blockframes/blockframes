6e23143fb9af1c60b0ef5e727cef525bb6b0026a
Yohann <ioa.nikas@gmail.com>
2020-04-28 17:41:59 +0200

    Refactor migration script

--- apps/backend-ops/src/firestoreMigrations/0019.ts
+++ apps/backend-ops/src/firestoreMigrations/0019.ts
@@ -65,18 +65,9 @@ async function updateUsers(
   batch: FirebaseFirestore.WriteBatch
 ) {
   users.forEach(async doc => {
-    const user = doc.data();
-    if (user.avatar || user.avatar === '') {
-      const { ref, url } = user.avatar;
-      delete user.avatar;
-
-      const newData = {
-        ...user,
-        avatar: createNewImgRef(ref, url)
-      };
-      return batch.set(doc.ref, newData);
-    }
-  });
+    const user = updateAvatar(doc.data())
+    return batch.set(doc.ref, user);
+  })
 }
 
 async function updateOrganizations(
@@ -84,18 +75,9 @@ async function updateOrganizations(
   batch: FirebaseFirestore.WriteBatch
 ) {
   organizations.forEach(async doc => {
-    const organization = doc.data();
-    if (organization.logo) {
-      const { ref, url } = organization.logo;
-      delete organization.logo;
-
-      const newData = {
-        ...organization,
-        logo: createNewImgRef(ref, url)
-      };
-      return batch.set(doc.ref, newData);
-    }
-  });
+    const organization = updateLogo(doc.data())
+    return batch.set(doc.ref, organization);
+  })
 }
 
 async function updateMovies(
@@ -105,134 +87,51 @@ async function updateMovies(
   movies.forEach(async doc => {
     const movie = doc.data();
 
-    updatePromotionalElementsMedia(movie.promotionalElements);
-    updateSalesCastAvatar(movie.salesCast);
-    updateDirectorsAvatar(movie.main.directors);
-    updateStakeholdersLogo(movie.main.stakeholders);
+    movie.main.directors = movie.main.directors.map(updateAvatar);
+    for (const key in movie.promotionalElements) {
+      movie.promotionalElements[key] = updateMedia(movie.promotionalElements[key])
+    }
+    for (const key in movie.main.salesCast) {
+      movie.promotionalElements[key] = updateAvatar(movie.salesCast[key])
+    }
+    for (const key in movie.main.stakeholders) {
+      movie.main.stakeholders[key] = updateLogo(movie.main.stakeholders[key])
+    }
 
     return batch.set(doc.ref, movie);
   });
 }
 
-function updatePromotionalElementsMedia(elements) {
-  for (const key in elements) {
-    if (Array.isArray(elements[key])) {
-      elements[key] = elements[key].map(value => {
-        if (value.hasOwnProperty('media')) {
-          const { ref, url } = value.media;
-          delete value.media;
-          return {
-            ...value,
-            media: createNewImgRef(ref, url)
-          };
-        } else {
-          return {
-            ...value,
-            logo: createNewImgRef()
-          };
-        }
-      });
-    } else {
-      const { ref, url } = elements[key].media;
-      delete elements[key].media;
-
-      elements[key] = {
-        ...elements[key],
-        media: createNewImgRef(ref, url)
-      };
+const update = (obj, fieldName) => {
+  if (Array.isArray(obj)) {
+    return obj.map(x => {
+      const field = x[fieldName];
+      return {
+        ...x,
+        [fieldName] : createNewImgRef(field?.ref, field?.url)
+      }
+    })
+  } else {
+    const field = obj[fieldName];
+    return {
+      ...obj,
+      [fieldName]: createNewImgRef(field?.ref, field?.url)
     }
   }
 }
 
-function updateSalesCastAvatar(salesCast) {
-  for (const key in salesCast) {
-    if (Array.isArray(salesCast[key])) {
-      salesCast[key] = salesCast[key].map(cast => {
-        if (cast.hasOwnProperty('avatar')) {
-          const { ref, url } = cast.avatar;
-          delete cast.avatar;
-          return {
-            ...cast,
-            avatar: createNewImgRef(ref, url)
-          };
-        } else {
-          return {
-            ...cast,
-            logo: createNewImgRef()
-          };
-        }
-      });
-    } else {
-      const { ref, url } = salesCast[key].avatar;
-      delete salesCast[key].avatar;
-
-      salesCast[key] = {
-        ...salesCast[key],
-        avatar: createNewImgRef(ref, url)
-      };
-    }
-  }
-}
-
-function updateDirectorsAvatar(directors) {
-  for (const key in directors) {
-    if (directors[key].hasOwnProperty('avatar')) {
-      const { ref, url } = directors[key].avatar;
-      delete directors[key].avatar;
-      directors[key] = {
-        ...directors[key],
-        avatar: createNewImgRef(ref, url)
-      };
-    } else {
-      directors[key] = {
-        ...directors[key],
-        avatar: createNewImgRef()
-      };
-    }
-  }
-}
-
-function updateStakeholdersLogo(stakeholders) {
-  for (const key in stakeholders) {
-    if (Array.isArray(stakeholders[key])) {
-      stakeholders[key] = stakeholders[key].map(sh => {
-        if (sh.hasOwnProperty('logo')) {
-          const { ref, url } = sh.logo;
-          delete sh.logo;
-          return {
-            ...sh,
-            logo: createNewImgRef(ref, url)
-          };
-        } else {
-          return {
-            ...sh,
-            logo: createNewImgRef()
-          };
-        }
-      });
-    } else {
-      const { ref, url } = stakeholders[key].logo;
-      delete stakeholders[key].logo;
-      stakeholders[key] = {
-        ...stakeholders[key],
-        logo: createNewImgRef(ref, url)
-      };
-    }
-  }
-}
+const updateAvatar = x => update(x, 'avatar')
+const updateLogo = x => update(x, 'logo')
+const updateMedia = x => update(x, 'media')
 
 /**
- * Create an empty ImgRef object
+ * Create an ImgRef object
  * @param ref
  * @param url
  */
-function createNewImgRef(ref?, url?) {
+function createNewImgRef(ref, url) {
   return {
-    refs: {
-      original: ref ? ref : ''
-    },
-    urls: {
-      original: url ? url : ''
-    }
+    refs: { original: ref || '' },
+    urls: { original: url || '' }
   };
 }
