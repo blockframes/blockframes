b194b7fcbb49469925e9eb63b7b2a760c40f7fb9
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-05-01 18:36:55 +0200

    update event edit

--- apps/festival/festival/src/app/dashboard/dashboard.module.ts
+++ apps/festival/festival/src/app/dashboard/dashboard.module.ts
@@ -5,7 +5,6 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { DashboardLayoutModule } from '@blockframes/ui/layout/dashboard/dashboard.module';
 import { ImageReferenceModule } from '@blockframes/ui/media/image-reference/image-reference.module';
 // Guards
-import { EventActiveGuard } from '@blockframes/event/guard/event-active.guard';
 import { MovieActiveGuard } from '@blockframes/movie/guards/movie-active.guard';
 import { MovieTunnelGuard } from '@blockframes/movie/guards/movie-tunnel.guard';
 import { MovieOrganizationListGuard } from '@blockframes/movie/guards/movie-organization-list.guard';
@@ -70,8 +69,6 @@ const routes: Routes = [{
           loadChildren: () => import('./event/list/list.module').then(m => m.EventListModule)
         }, {
           path: ':eventId',
-          canActivate: [EventActiveGuard],
-          canDeactivate: [EventActiveGuard],
           children: [{
             path: '',
             loadChildren: () => import('./event/review/review.module').then(m => m.EventReviewModule)
--- apps/festival/festival/src/app/dashboard/event/edit/edit.component.ts
+++ apps/festival/festival/src/app/dashboard/event/edit/edit.component.ts
@@ -1,44 +1,65 @@
 import { Component, OnInit, ViewChild, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
 import { EventForm } from '@blockframes/event/form/event.form';
+import { EventService } from '@blockframes/event/+state';
+import { EventTypes } from '@blockframes/event/+state/event.firestore';
 import { EventEditComponent } from '@blockframes/event/layout/edit/edit.component';
-import { EventQuery } from '@blockframes/event/+state/event.query';
 import { Movie, MovieService } from '@blockframes/movie/+state';
 import { InvitationService, Invitation } from '@blockframes/invitation/+state';
 import { OrganizationQuery } from '@blockframes/organization/+state';
-import { Observable, Subscription } from 'rxjs';
-import { switchMap, startWith } from 'rxjs/operators';
+import { Observable, Subscription, combineLatest, BehaviorSubject } from 'rxjs';
+import { switchMap, pluck } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
+import { slideUpList } from '@blockframes/utils/animations/fade';
 
 @Component({
   selector: 'festival-event-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.scss'],
+  animations: [slideUpList('h2, mat-card')],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EditComponent implements OnInit, OnDestroy {
 
   private sub: Subscription;
-  form = new EventForm({ type: 'screening' });  // force the screening type to have default meta fields
+  loading$ = new BehaviorSubject(true);
+  form: EventForm;
   titles$: Observable<Movie[]>;
   invitations$: Observable<Invitation[]>;
+  type: EventTypes;
 
   @ViewChild(EventEditComponent) editCmpt: EventEditComponent;
 
   constructor(
-    private query: EventQuery,
+    private service: EventService,
     private movieService: MovieService,
     private invitationService: InvitationService,
     private orgQuery: OrganizationQuery,
+    private route: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
-    this.invitations$ = this.query.selectActiveId().pipe(
-      switchMap(id => this.invitationService.valueChanges(ref => ref.where('docId', '==', id))),
-      startWith([])
+    const orgId$ = this.orgQuery.selectActiveId();
+    const eventId$ = this.route.params.pipe(pluck('eventId'));
+
+    this.invitations$ = combineLatest([ orgId$, eventId$ ]).pipe(
+      switchMap(([orgId, eventId]) => {
+        const queryFn = ref => ref.where('fromOrg.id', '==', orgId).where('docId', '==', eventId)
+        return this.invitationService.valueChanges(queryFn);
+      }),
     );
+      
+    // will be executed only if "screening" as Observable are lazy
     this.titles$ = this.orgQuery.selectActive().pipe(
       switchMap(org => this.movieService.getValue(org.movieIds))
     );
-    this.sub = this.query.selectActive().subscribe(event => this.form.reset(event));
+
+    this.sub = eventId$.pipe(
+      switchMap((eventId: string) => this.service.valueChanges(eventId))
+    ).subscribe(event => {
+      this.type = event.type;
+      this.form = new EventForm(event);
+      this.loading$.next(false);
+    });
   }
 
   ngOnDestroy() {
