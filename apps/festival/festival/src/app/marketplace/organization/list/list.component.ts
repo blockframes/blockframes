a4d2ccdebf7b2bf5224be7d9a6bb626be9ecc3c0
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-05-06 15:39:20 +0200

    fix query

--- apps/festival/festival/src/app/marketplace/organization/list/list.component.ts
+++ apps/festival/festival/src/app/marketplace/organization/list/list.component.ts
@@ -1,23 +1,23 @@
 import { Component, ChangeDetectionStrategy, HostBinding } from '@angular/core';
 import { OrganizationService } from '@blockframes/organization/+state/organization.service';
-import { scaleOut, scaleOutList } from '@blockframes/utils/animations/fade';
+import { scaleOut } from '@blockframes/utils/animations/fade';
 import { Observable } from 'rxjs';
+import { Organization } from '@blockframes/organization/+state';
+import { tap } from 'rxjs/operators';
 
 @Component({
-  selector: 'festival-marketplace-organization-list',
+  selector: 'festival-organization-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
-  animations: [scaleOut, scaleOutList('a')],
+  animations: [scaleOut],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ListComponent {
   @HostBinding('@scaleOut') animation = true;
-  // @HostBinding('@scaleOutList') animList = true;
-  organizations;
+  orgs$: Observable<Organization[]>;
 
   constructor(private service: OrganizationService) {
-    // TODO #2570
-    this.organizations = this.service.getValue(ref => ref.where('appAccess.festival.dashboard', '==', 'true'));
+    this.orgs$ = this.service.valueChanges(ref => ref.where('appAccess.festival.dashboard', '==', true));
   }
 
 }
