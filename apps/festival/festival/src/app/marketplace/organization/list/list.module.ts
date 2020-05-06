7179373d5d3a59f48802a1a653f7d3b0eb30956d
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-05-06 13:52:47 +0200

    create org-card

--- apps/festival/festival/src/app/marketplace/organization/list/list.module.ts
+++ apps/festival/festival/src/app/marketplace/organization/list/list.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list.component';
-import { ImageReferenceModule } from '@blockframes/ui/media/image-reference/image-reference.module';
-import { ToLabelModule } from '@blockframes/utils/pipes';
+import { OrganizationCardModule } from '@blockframes/organization/components/card/card.module';
 // Material
-import { MatCardModule } from '@angular/material/card';
 import { MatRippleModule } from '@angular/material/core';
 
 
@@ -13,9 +11,7 @@ import { MatRippleModule } from '@angular/material/core';
   declarations: [ListComponent],
   imports: [
     CommonModule,
-    ImageReferenceModule,
-    ToLabelModule,
-    MatCardModule,
+    OrganizationCardModule,
     MatRippleModule,
     RouterModule.forChild([{ path: '', component: ListComponent }]),
   ]
