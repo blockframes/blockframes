eea0b216420e3755647097e3b7311cd1a0e6b637
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-05-06 16:13:58 +0200

    add todo for meeting ownership

--- apps/festival/festival/src/app/marketplace/event/calendar/calendar.component.ts
+++ apps/festival/festival/src/app/marketplace/event/calendar/calendar.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
 import { EventService } from '@blockframes/event/+state/event.service';
 import { Event } from '@blockframes/event/+state';
 import { InvitationQuery } from '@blockframes/invitation/+state';
 import { map, switchMap } from 'rxjs/operators';
+import { AuthQuery } from '@blockframes/auth/+state';
 
 @Component({
   selector: 'festival-event-calendar',
@@ -17,16 +18,26 @@ export class EventCalendarComponent implements OnInit {
 
   constructor(
     private service: EventService,
+    private authQuery: AuthQuery,
     private invitationQuery: InvitationQuery,
     private cdr: ChangeDetectorRef
   ) { }
 
   ngOnInit(): void {
-    this.events$ = this.invitationQuery.selectAll({
+    // @todo(#2711) Should take into account user & org ownerId
+    // Event to which user is invited
+    const invited$ = this.invitationQuery.selectAll({
       filterBy: ({ type, status }) => type === 'attendEvent' && status === 'accepted'
     }).pipe(
       map(invitations => invitations.map(i => i.docId)),
       switchMap(eventIds => this.service.valueChanges(eventIds))
+    );
+    // Event that user owned
+    const owned$ = this.authQuery.select('uid').pipe(
+      switchMap(uid => this.service.queryByType(['meeting'], ref => ref.where('ownerId', '==', uid)))
+    );
+    this.events$ = combineLatest([invited$, owned$]).pipe(
+      map(events => events.flat())
     );
   }
 
