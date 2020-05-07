45c197107fc361c28ebbce9f1d118d3f4bd88a14
Bruce <eelios@gmail.com>
2020-05-07 15:50:19 +0200

    removed duplicate org update (#2730)

--- apps/festival/festival/src/app/dashboard/tunnel/start/start-tunnel.component.ts
+++ apps/festival/festival/src/app/dashboard/tunnel/start/start-tunnel.component.ts
@@ -49,14 +49,10 @@ export class StartTunnelComponent implements OnInit {
 
   async begin() {
     this.loading = true;
-    let movieId;
     const createdBy = this.auth.getValue().uid;
     const movie = createMovie({ _meta: { createdBy } });
     try {
-      await this.movieService.runTransaction(async (write) => {
-        movieId = await this.movieService.add(movie, { write });
-        await this.orgService.update(this.orgQuery.getActiveId(), (org) => ({ movieIds: [...org.movieIds, movieId] }), { write })
-      })
+      const movieId = await this.movieService.add(movie);
       this.loading = false;
       this.router.navigate([movieId], { relativeTo: this.route });
     } catch (err) {
