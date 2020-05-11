9ef5cc54c4c01a1cc9eb8af0fb92a5020127f52e
Laurent Senta <laurent.senta@gmail.com>
2020-05-11 09:44:46 +0200

    backend: get rid of duplicate logerrors

--- apps/backend-functions/src/main.ts
+++ apps/backend-functions/src/main.ts
@@ -144,7 +144,7 @@ export const onMovieUpdateEvent = onDocumentUpdate(
  */
 export const onMovieDeleteEvent = onDocumentDelete(
   'movies/{movieId}',
-  logErrors(onMovieDelete)
+  onMovieDelete
 )
 
 //------------------------------------------------
