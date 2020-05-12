9216a5ef03f3b78e23f6a17db409a12095a0a633
Max Fritz <max.j.f@gmx.de>
2020-05-12 11:10:02 +0200

    added review card component

---
+++ libs/ui/src/lib/review-card/review-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { MovieReview } from '@blockframes/movie/+state';
+
+function createMovieReviewView(review: Partial<MovieReview>) {
+  return {
+    journalName: review.journalName || 'Journale name couldn\'t be loaded',
+    criticQuote: review.criticQuote || 'Quote couldn\'t be loaded',
+    link: review.revueLink
+  }
+}
+
+@Component({
+  selector: '[review] review-card',
+  templateUrl: 'review-card.component.html',
+  styleUrls: ['./review-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class ReviewCardComponent {
+  public movieReview
+  @Input()
+  set review(review: MovieReview) {
+    this.movieReview = createMovieReviewView(review);
+  }
+}
