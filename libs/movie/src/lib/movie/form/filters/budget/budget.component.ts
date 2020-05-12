b9ceba9c031a4dc1f1fb62ef1d7df2c4f0217267
Yohann <ioa.nikas@gmail.com>
2020-05-12 10:35:04 +0200

    Applying change request

--- libs/movie/src/lib/movie/form/filters/budget/budget.component.ts
+++ libs/movie/src/lib/movie/form/filters/budget/budget.component.ts
@@ -1,6 +1,5 @@
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { MatSliderChange } from '@angular/material/slider';
 
 @Component({
   selector: '[form] filter-budget',
@@ -12,17 +11,14 @@ export class BudgetFilterComponent {
 
   @Input() form: FormControl; // FormControl of number
 
-  public currentValue = '0';
-
-  updateCurrentValue(input: MatSliderChange) {
-    const value = input.value
+  formatLabel(value: number) {
     if (value >= 1000 && value < 1000000) {
-      this.currentValue = Math.round(value / 1000) + 'k';
+      return Math.round(value / 1000) + 'k';
     } else if (value >= 1000000) {
-      this.currentValue = (value / 1000000) + 'M';
-    } else {
-      this.currentValue = value.toString();
+      return (value / 1000000) + 'M';
     }
+
+    return value;
   }
 
 }
