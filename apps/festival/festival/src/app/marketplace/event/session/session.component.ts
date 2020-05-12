b17c6a224349813d71b0115f59fc40744f3b62c8
Mussuranae <mandonnetdev@outlook.com>
2020-05-12 17:22:37 +0200

    have beautiful time for waiting room

--- apps/festival/festival/src/app/marketplace/event/session/session.component.ts
+++ apps/festival/festival/src/app/marketplace/event/session/session.component.ts
@@ -33,7 +33,10 @@ export class SessionComponent implements OnInit {
     this.background = `url(${this.movie.promotionalElements.banner.media.url})`;
     this.org = this.orgQuery.getEntity(this.event.ownerId);
     // this.url = this.sanitizer.bypassSecurityTrustResourceUrl(`${this.movie.promotionalElements.screener_link.media.url}`);
-    this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://player.vimeo.com/video/391939808');
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(' ');
+
+    //! 2 questions here, if timeBeforeNextScreening > 24h, do we display days and hours ?
+    //! second question : when do we switch between not yet page and almost page ?
   }
 
   playVideo() {
@@ -42,7 +45,16 @@ export class SessionComponent implements OnInit {
     return;
   }
 
-  timeBeforeNextScreening() {
-    return 'ah ah ah !!!';
+  timeBeforeNextScreening(date) {
+    const milliseconds = Date.parse(date) - Date.now();
+    const msInHours = 1000 * 60 * 60;
+    const msInMins = 1000 * 60;
+
+    const hours = Math.trunc(milliseconds / msInHours);
+    const mins = Math.trunc((milliseconds - (hours*msInHours)) / msInMins);
+
+    console.log('heure : ' + hours);
+    console.log('mins : ' + mins);
+    return `${hours}h ${mins}mins`;
   }
 }
