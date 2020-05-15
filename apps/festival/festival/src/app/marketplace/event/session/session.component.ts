b86657b6ef5a0b3f69cef46be6feab6b8f01554e
Mussuranae <mandonnetdev@outlook.com>
2020-05-15 18:19:34 +0200

    add responsive for session page

--- apps/festival/festival/src/app/marketplace/event/session/session.component.ts
+++ apps/festival/festival/src/app/marketplace/event/session/session.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { EventService, Event } from '@blockframes/event/+state';
 import { pluck, switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
@@ -15,13 +14,11 @@ import { Observable } from 'rxjs';
 export class SessionComponent implements OnInit {
 
   event$: Observable<Event>;
-  url: SafeResourceUrl;
   public showSession = true;
 
   constructor(
     private service: EventService,
     private route: ActivatedRoute,
-    private sanitizer: DomSanitizer,
   ) { }
 
   ngOnInit(): void {
@@ -29,7 +26,6 @@ export class SessionComponent implements OnInit {
       pluck('eventId'),
       switchMap(eventId => this.service.queryDocs(eventId))
     );
-    this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://player.vimeo.com/video/391939808');
   }
 
   playVideo() {
