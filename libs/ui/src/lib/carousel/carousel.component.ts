b6020337443d0f7ca11598d36a90de3734bb2544
Max Fritz <max.j.f@gmx.de>
2020-05-18 10:22:53 +0200

    changing branches

--- libs/ui/src/lib/carousel/carousel.component.ts
+++ libs/ui/src/lib/carousel/carousel.component.ts
@@ -20,7 +20,7 @@ export class CarouselComponent implements AfterViewInit {
 
   public currentPosition: number;
 
-  private totalWidth: number;
+  public showBackwardsBtn = false;;
 
   @ViewChild(CdkScrollable) scrollable: CdkScrollable;
 
@@ -28,16 +28,30 @@ export class CarouselComponent implements AfterViewInit {
 
   ngAfterViewInit() {
     this.currentPosition = this.scrollable.measureScrollOffset('left')
-    this.totalWidth = this.scrollable.measureScrollOffset('end');
+    console.log(this.layout.width.getValue(), this.scrollable.measureScrollOffset('end'))
   }
 
   scrollTo(direction: 'left' | 'right') {
+    /*  If right === 0 it means that we are at the end and want to start from the beginning again. */
+    if (!this.scrollable.measureScrollOffset('right')) {
+      this.scrollable.scrollTo({ left: 0 })
+      this.showBackwardsBtn = false
+    }
     this.currentPosition = this.scrollable.measureScrollOffset('left');
-    // TODO MF this.layotu.width is not 100% correct cause it is not respecting the margin
+    /* 
+    * If the forward button is clicked the current position is still 0, cause the cdk
+    * only updates the state after the scroll animation happened. But we know, if this function
+    * was called, we can be sure the user scrolled to the left. So we want to show the backwards button.
+    * If the user reached the very right end, we scroll back to the beginning and the currentPosition is !== 0
+    * so we hide the button
+    */
+    this.currentPosition === 0
+      ? this.showBackwardsBtn = true
+      : this.showBackwardsBtn = false
+
     direction === 'right'
       ? this.scrollable.scrollTo({ left: this.currentPosition + this.layout.width.getValue() })
       : this.scrollable.scrollTo({ left: this.currentPosition - this.layout.width.getValue() })
-
   }
 }
 
