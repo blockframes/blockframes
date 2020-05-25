aa2bbe782baee4f89b972338cc3c667f0eb5baba
hugo-hontans <hugohontans@gmail.com>
2020-05-25 17:41:57 +0200

    manage invitation action with status

--- libs/notification/src/lib/invitation/components/action/action.component.ts
+++ libs/notification/src/lib/invitation/components/action/action.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { Event } from '@blockframes/event/+state';
 import { Invitation, InvitationService } from '../../+state';
+import { AuthQuery } from '@blockframes/auth/+state/auth.query';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'invitation-action',
@@ -8,19 +10,32 @@ import { Invitation, InvitationService } from '../../+state';
   styleUrls: ['./action.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ActionComponent {
+export class ActionComponent implements OnInit {
 
   @Input() event: Event;
   @Input() invitation: Invitation;
 
-  constructor(private service: InvitationService) {}
+  public isFromUser: boolean;
+
+  constructor(
+    private service: InvitationService,
+    private authQuery: AuthQuery,
+    private snackBar: MatSnackBar) {}
+
+  ngOnInit() {
+    this.isFromUser = !!this.invitation && this.invitation.fromUser.uid === this.authQuery.userId;
+  }
 
   accept(invitation: Invitation) {
     this.service.acceptInvitation(invitation);
+    // Todo try to redirect user on the event page when user clics on close button (rename it into More details)
+    this.snackBar.open('Invitation accepted', 'close', { duration: 2000 });
   }
 
   decline(invitation: Invitation) {
     this.service.declineInvitation(invitation);
+    // Todo try to redirect user on the event page when user clics on close button (rename it into More details)
+    this.snackBar.open('Invitation declined', 'close', { duration: 2000 });
   }
 
   /** Request the owner to accept invitation (automatically accepted if event is public) */
