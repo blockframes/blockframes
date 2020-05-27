325847f714e0b2d3134e5ac90342e8014664c76b
Sebastian Montoya <ing.sebastian.montoya@gmail.com>
2020-05-27 18:13:56 +0200

    Last error, sorry about this.

--- libs/ui/src/lib/feedback/feedback-message.module.ts
+++ libs/ui/src/lib/feedback/feedback-message.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { FeedbackMessageComponent } from './feedback-message.component';
 import { MatButtonModule } from '@angular/material/button';
 import { FlexModule } from '@angular/flex-layout';
+import { ImageReferenceModule } from '@blockframes/ui/media/image-reference/image-reference.module';
 
 @NgModule({
   declarations: [FeedbackMessageComponent],
   imports: [
+    ImageReferenceModule,
     MatButtonModule,
     FlexModule,
   ],
