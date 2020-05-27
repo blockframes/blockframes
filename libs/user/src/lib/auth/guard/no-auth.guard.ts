ed524a12ed70a74485396914261356a5b661e999
bruce <delorme.bruce.michel@gmail.com>
2020-05-27 14:56:24 +0200

    WIP

---
+++ libs/user/src/lib/auth/guard/no-auth.guard.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { AuthQuery, AuthService, AuthState } from '../+state';
+import { switchMap, map } from 'rxjs/operators';
+import { CollectionGuard, CollectionGuardConfig } from 'akita-ng-fire';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { OrganizationService } from '@blockframes/organization/+state/organization.service';
+import { getOrgModuleAccess, getCurrentApp } from '@blockframes/utils/apps';
+import { RouterQuery } from '@datorama/akita-ng-router-store';
+
+@Injectable({
+  providedIn: 'root'
+})
+@CollectionGuardConfig({ awaitSync: true })
+export class NoAuthGuard extends CollectionGuard<AuthState> {
+  constructor(
+    service: AuthService,
+    private orgService: OrganizationService,
+    private query: AuthQuery,
+    private afAuth: AngularFireAuth,
+    private routerQuery: RouterQuery,
+  ) {
+    super(service);
+  }
+
+  sync() {
+    return this.afAuth.authState.pipe(
+      switchMap(userAuth => {
+        if (!userAuth) { return new Promise(r => r(true)); }
+        return this.service.sync().pipe(
+          map(_ => this.query.orgId),
+          switchMap(orgId => orgId ? this.orgService.getValue(orgId) : new Promise<false>(r => r(false))),
+          map(org => {
+            if (!org) { return '/c/organization'; }
+            const app = getCurrentApp(this.routerQuery);
+            const moduleAccess = getOrgModuleAccess(org, app);
+            return `/c/o/${moduleAccess[0] || 'dashboard'}/home`;
+          })
+        );
+      })
+    );
+  }
+}
