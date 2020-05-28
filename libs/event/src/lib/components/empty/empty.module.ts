995df2cbf213ca5fbc6022d7ea89fa3f15415ae9
Sebastian Montoya <ing.sebastian.montoya@gmail.com>
2020-05-28 15:57:47 +0200

    APP-2528 Removing the last missing leftover of imgAssetModule.

--- libs/event/src/lib/components/empty/empty.module.ts
+++ libs/event/src/lib/components/empty/empty.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmptyComponent } from './empty.component';
-import { ImgAssetModule } from '@blockframes/ui/theme';
-
-
 
 @NgModule({
   declarations: [EmptyComponent],
   exports: [EmptyComponent],
   imports: [
     CommonModule,
-    ImgAssetModule,
   ]
 })
 export class EventEmptyModule { }
