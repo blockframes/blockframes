c7468e2c5ebb14a69a3973fb94246750811b69e9
Sebastian Montoya <ing.sebastian.montoya@gmail.com>
2020-05-28 15:59:24 +0200

    APP-2528 Instead of removing it, I should've just replaced it with imgReferenceModule.

--- libs/event/src/lib/components/empty/empty.module.ts
+++ libs/event/src/lib/components/empty/empty.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmptyComponent } from './empty.component';
+import { ImageReferenceModule } from '@blockframes/ui/media/image-reference/image-reference.module';
 
 @NgModule({
   declarations: [EmptyComponent],
   exports: [EmptyComponent],
   imports: [
+    ImageReferenceModule,
     CommonModule,
   ]
 })
