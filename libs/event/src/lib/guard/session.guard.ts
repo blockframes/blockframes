de4875aacfe6d7277f5db1e56bbc55cd476d484a
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-05-29 15:32:36 +0200

    improve guard

--- libs/event/src/lib/guard/session.guard.ts
+++ libs/event/src/lib/guard/session.guard.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTree } from '@angular/router';
 import { InvitationQuery, Invitation } from '@blockframes/invitation/+state';
+import { EventService } from '../+state';
+import { eventTime } from '../pipes/event-time.pipe';
 
 @Injectable({ providedIn: 'root' })
 export class SessionGuard implements CanActivate {
   constructor(
     private invitationQuery: InvitationQuery,
+    private service: EventService,
     private router: Router
   ) {}
 
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
-    const { eventId } = next.params;
+  async canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean | UrlTree> {
+    const eventId: string = next.params.eventId;
     const canNavigate = this.invitationQuery.hasEntity((invitation: Invitation) => {
       return invitation.docId === eventId && invitation.status === 'accepted';
     });
-    return canNavigate || this.router.parseUrl(`/c/o/marketplace/event/${eventId}`);
+    if (!canNavigate) {
+      return this.router.parseUrl(`/c/o/marketplace/event/${eventId}`);
+    }
+    const event = await this.service.getValue(eventId);
+    if (eventTime(event) !== 'onTime') {
+      return this.router.parseUrl(`/c/o/marketplace/event/${eventId}`);
+    }
+    return true;
   }
 }
