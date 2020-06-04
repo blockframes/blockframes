e53cfd9eda1884adf2b89633c418b99f4d719bcb
bruce <delorme.bruce.michel@gmail.com>
2020-06-04 12:26:35 +0200

    linter

--- libs/admin/src/lib/admin-panel/pages/dev-area/dev-area.component.ts
+++ libs/admin/src/lib/admin-panel/pages/dev-area/dev-area.component.ts
@@ -12,7 +12,7 @@ import { firebase } from '@env';
 export class DevAreaComponent implements OnInit {
   public token: string;
   public projectId = firebase.projectId;
-  public firebaseConsoleLink: string = `https://console.firebase.google.com/project/${firebase.projectId}/database/`;
+  public firebaseConsoleLink = `https://console.firebase.google.com/project/${firebase.projectId}/database/`;
 
   constructor(
     private afAuth: AngularFireAuth,
