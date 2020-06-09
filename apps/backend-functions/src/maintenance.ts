1c968cd5a9c35bc31a9e3f3c011f5367b9f40b8d
Laurent Senta <laurent.senta@gmail.com>
2020-06-09 09:59:53 +0200

    Revert "Merge pull request #2340 from blockframes/maintenance-mode-definitive"
    This reverts commit ddc7fe27648c4a02406d41f64802fc272963ab5f, reversing
    changes made to 30ac4cd6f8413de8882a8db7e50a215c1354e29d.

--- apps/backend-functions/src/maintenance.ts
+++ apps/backend-functions/src/maintenance.ts
@@ -1,8 +1,9 @@
 import { db } from './internals/firebase';
 import * as admin from 'firebase-admin';
-import { EventContext } from 'firebase-functions';
 
 type Timestamp = admin.firestore.Timestamp;
+
+const EIGHT_MINUTES_IN_MS = 8 * 60 * 1000;
 
 interface IMaintenanceDoc {
   startedAt: Timestamp | null;
@@ -16,10 +17,7 @@ const maintenanceRef = () => {
 };
 
 export async function startMaintenance() {
-  return maintenanceRef().set({
-    startedAt: admin.firestore.FieldValue.serverTimestamp(),
-    endedAt: null
-  });
+  return maintenanceRef().set({ startedAt: admin.firestore.FieldValue.serverTimestamp(), endedAt: null });
 }
 
 export async function endMaintenance() {
@@ -29,59 +27,45 @@ export async function endMaintenance() {
   });
 }
 
-async function getMaintenanceDoc(): Promise<IMaintenanceDoc | null> {
+export async function isInMaintenance() {
   const ref = maintenanceRef();
   const doc = await ref.get();
 
   // we've never seen any maintenance
   if (!doc.exists) {
-    return null;
-  }
-
-  return doc.data() as IMaintenanceDoc;
-}
-
-/**
- * Return wether the given event context occurred during the last maintenance time.
- * @param context
- */
-export async function wasInMaintenance(context: EventContext) {
-  const doc = await getMaintenanceDoc();
-  if (!doc) {
     return false;
   }
 
-  const eventTime: Timestamp = admin.firestore.Timestamp.fromDate(new Date(context.timestamp));
-  const { startedAt, endedAt } = doc;
+  const { startedAt, endedAt } = doc.data() as IMaintenanceDoc;
+  const now = admin.firestore.Timestamp.now();
 
-  if (!startedAt) {
-    return false;
+  if (endedAt) {
+    // Wait 30s before allowing any operation on the db.
+    // this prevents triggering firebase events.
+    // NOTE: this is hack-ish but good enough for our needs! we'll revisit this later.
+    return endedAt.toMillis() + EIGHT_MINUTES_IN_MS > now.toMillis();
   }
 
-  if (!endedAt) {
-    // still in maintenance.
-    // TODO: we could let this code run, it comes from a previous event, but that sounds dangerous.
-    // the best would be to disable the whole application for a few minutes BEFORE
-    // starting the migration code. So this case never happens.
+  if (startedAt) {
     return true;
   }
 
-  // The event occured within the last migration timeframe.
-  return startedAt < eventTime && eventTime < endedAt;
+  throw new Error(
+    'Unexpected cases for maintenance check! please check the _META/_MAINTENANCE document.'
+  );
 }
 
-export function skipInMaintenance<T, R>(
-  f: (data: T, context: EventContext) => R | undefined
-): (data: T, context: EventContext) => Promise<R | undefined> {
+// TODO: take the time to fix the types,
+// probably turn this into a generic (f: T) to and preserve types.
+export const skipInMaintenance = (f: any) => {
   // return a new function that is:
   // the old function + a check that early exits when we are restoring.
-  return async (x: T, context: EventContext) => {
+  return async (...args: any[]) => {
     // early exit
-    if (await wasInMaintenance(context)) {
-      console.debug('skip during maintenance');
-      return;
+    if (await isInMaintenance()) {
+      return true;
     }
 
-    return f(x, context);
+    return f(...args);
   };
-}
+};
