276ce43088b9e14b825d9f0f5e373be72c92a7e3
hugo-hontans <hugohontans@gmail.com>
2020-06-10 16:07:28 +0200

    fix img link and heart icon for movie-card in responsive view

--- libs/movie/src/lib/movie/components/card/card.component.ts
+++ libs/movie/src/lib/movie/components/card/card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import { Movie } from '../../+state/movie.model';
 import { scaleIn } from '@blockframes/utils/animations/fade';
+import { BreakpointsService } from '@blockframes/utils/breakpoint/breakpoints.service';
 
 @Component({
   selector: '[movie] movie-card',
@@ -12,10 +13,13 @@ import { scaleIn } from '@blockframes/utils/animations/fade';
 export class CardComponent {
   public hasOverlay = false;
   public placeholder: string;
+  public ltLg$ = this.breakpointsService.ltLg;
 
   @Input() size: 'banner' | 'poster' | 'avatar';
   @Input() movie: Movie;
   @Input() link = "..";
+
+  constructor(private breakpointsService: BreakpointsService) {}
 
   get placeholderAsset() {
     if (this.size === 'banner') {
