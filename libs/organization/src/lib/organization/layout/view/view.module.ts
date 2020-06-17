4ccde7a818eb6245cb350ea00c446b5c2d252d27
Bruce <eelios@gmail.com>
2020-06-17 10:48:33 +0200

    [admin-panel] Org name bigger on org page & reset password email (#3078)

--- libs/organization/src/lib/organization/layout/view/view.module.ts
+++ libs/organization/src/lib/organization/layout/view/view.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
+
+// Modules
 import { ImageReferenceModule } from '@blockframes/ui/media/image-reference/image-reference.module';
 import { AppBarModule } from '@blockframes/ui/app-bar';
 import { ToLabelModule, TranslateSlugModule } from '@blockframes/utils/pipes';
+import { OrgNameModule } from '@blockframes/organization/pipes/org-name.pipe';
+
+// Components
 import { ViewComponent } from './view.component';
 
 // Material
@@ -12,8 +17,6 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
-
 
 @NgModule({
   declarations: [ViewComponent],
@@ -26,6 +29,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     AppBarModule,
     ToLabelModule,
     TranslateSlugModule,
+    OrgNameModule,
     // Material
     MatTabsModule,
     MatButtonModule,
