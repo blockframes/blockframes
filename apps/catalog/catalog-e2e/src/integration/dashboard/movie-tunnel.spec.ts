42e4375aed681f8cb02d31c945603a236a94ef97
hugo-hontans <hugohontans@gmail.com>
2020-06-22 14:33:32 +0200

    create a new movie-tunnel-spec with all movie tunnel tests

---
+++ apps/catalog/catalog-e2e/src/integration/dashboard/movie-tunnel.spec.ts
@@ -0,0 +1,68 @@
+/// <reference types="cypress" />
+
+import { clearDataAndPrepareTest } from '@blockframes/e2e/utils/functions';
+import { signInAndNavigateToMain } from '../../support/utils/utils';
+import {
+  mainTest,
+  storylineTest,
+  creditsTest, budgetTest,
+  technicalInfoTest,
+  promotionalImagesTest,
+  filesTest,
+  chainOfTitlesTest,
+  valuationTest
+} from '../../support/movie-tunnel-tests';
+
+describe('User can navigate to the movie tunnel pages start and main.', () => {
+  it('Login into an existing account, navigate on titles list page, go to movie tunnel start page, go on movie tunnel main page', () => {
+    clearDataAndPrepareTest();
+    signInAndNavigateToMain();
+  });
+
+  // Main page
+  it('Complete main fields, go on movie tunnel storyline page', () => {
+    mainTest();
+  });
+
+  // Storyline page
+  it('Complete storyline fields, go on movie tunnel credits page', () => {
+    storylineTest();
+  });
+
+  // Credits page
+  it('Complete credits fields, go on movie tunnel budget page', () => {
+    creditsTest();
+  });
+
+  // Budget page
+  it('Complete budget fields, go on movie tunnel technical info page', () => {
+    budgetTest();
+  });
+
+  // Technical info page
+  it('Complete technical info fields, go on movie tunnel promotional images page', () => {
+    technicalInfoTest();
+  });
+
+  // Promotional images page
+  it('Go on movie tunnel files page', () => {
+    promotionalImagesTest();
+  });
+
+  // Files page
+  it('Complete files and links fields, go on movie tunnel chain of titles page', () => {
+    filesTest();
+  });
+
+  // Chain of titles pages
+  it('Complete chain of titles fields, go on movie tunnel valuation page', () => {
+    chainOfTitlesTest();
+  });
+
+  // Valuation page
+  it('Complete valuation field, go on movie tunnel summary page', () => {
+    valuationTest();
+  });
+
+  // TODO: issue #2124 summary test
+});
