5836510b39262ce2e327d26520015f09843f103d
hugo-hontans <hugohontans@gmail.com>
2020-06-22 14:54:29 +0200

    add comment

--- apps/catalog/catalog-e2e/src/integration/dashboard/movie-tunnel.spec.ts
+++ apps/catalog/catalog-e2e/src/integration/dashboard/movie-tunnel.spec.ts
@@ -14,6 +14,7 @@ import {
 } from '../../support/movie-tunnel-tests';
 
 describe('User can navigate to the movie tunnel pages start and main.', () => {
+  // Log in and create a new movie
   it('Login into an existing account, navigate on titles list page, go to movie tunnel start page, go on movie tunnel main page', () => {
     clearDataAndPrepareTest();
     signInAndNavigateToMain();
