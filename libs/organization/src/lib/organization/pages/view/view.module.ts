d3e772ea946be9764f61ffee09fd9ddda68d9561
bruce <delorme.bruce.michel@gmail.com>
2020-06-23 18:20:18 +0200

    revert

--- libs/organization/src/lib/organization/pages/view/view.module.ts
+++ libs/organization/src/lib/organization/pages/view/view.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { ImageReferenceModule } from '@blockframes/media/directives/image-reference/image-reference.module';
 import { UserGuard } from '@blockframes/user/guard/user.guard';
 
 // Modules
+import { ImageReferenceModule } from '@blockframes/media/directives/image-reference/image-reference.module';
 import { OrgAddressModule } from '@blockframes/organization/pipes/org-address.pipe';
 import { ToLabelModule } from '@blockframes/utils/pipes';
 import { OrgNameModule } from '@blockframes/organization/pipes/org-name.pipe';
