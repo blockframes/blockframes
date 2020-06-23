778a816a1d41978100a631c5366eb80543a0ee98
bruce <delorme.bruce.michel@gmail.com>
2020-06-23 15:41:34 +0200

    merge back to develop

--- libs/media/src/lib/+state/media.model.ts
+++ libs/media/src/lib/+state/media.model.ts
@@ -1,4 +1,5 @@
 import { ImgRef, OldImgRef } from './media.firestore';
+import { isSafari } from '@blockframes/utils/safari-banner/safari.utils';
 
 export * from './media.firestore';
 
@@ -43,21 +44,39 @@ const formats = {
     height: 160,
     width: 120
   }
-};
+} as const;
 
 export type Formats = keyof typeof formats;
 
 export function getRatio(format: Formats) {
-  const { height, width } = formats[format];
-  return width / height;
+  const { height, width } = format[format];
+  return width/height;
 }
 
 // @todo(#3063) Remove this verifier
 export function isOldImgRef(ref: ImgRef | OldImgRef): ref is OldImgRef {
   return 'url' in ref;
-}
+} 
 
 // @todo(#3063) Update this function to unsupport oldImgRef
 export function getMediaUrl(ref: ImgRef | OldImgRef) {
-  return isOldImgRef(ref) ? ref.url : ref.urls.original;
+  if (ref) {
+    if (isOldImgRef(ref)) {
+      return ref.url;
+    } else {
+      return isSafari() ? ref.urls.fallback : ref.urls.original;
+    }
+  }
+}
+
+/** Used this only for background to let the browser deal with that with picture */
+export function getAssetPath(asset: string, theme: 'dark' | 'light', type: 'images' | 'logo' = 'images') {
+  const format = asset.split('.').pop();
+  if (format === 'webp') {
+    return isSafari()
+      ? `assets/${type}/${theme}-fallback/${asset.replace('.webp', '.png')}`
+      : `assets/${type}/${theme}/${asset}`
+  } else {
+    return`assets/${type}/${theme}/${asset}`;
+  }
 }
