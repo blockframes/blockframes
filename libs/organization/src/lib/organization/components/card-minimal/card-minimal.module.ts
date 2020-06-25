711183cfbea90025638f070df1593f49500e066b
RemcoSimonides <remcosimonides@gmail.com>
2020-06-25 11:02:11 +0200

    Removed matRipple and height width in css

--- libs/organization/src/lib/organization/components/card-minimal/card-minimal.module.ts
+++ libs/organization/src/lib/organization/components/card-minimal/card-minimal.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { OrganizationCardMinimalComponent } from './card-minimal.component';
-import { ImageReferenceModule } from '@blockframes/media/directives/image-reference/image-reference.module';
-import { OrgNameModule } from '../../pipes/org-name.pipe';
 
-// Material
-import { MatRippleModule } from '@angular/material/core';
+// Blockframes
+import { ImageReferenceModule } from '@blockframes/media/directives/image-reference/image-reference.module';
+import { OrganizationCardMinimalComponent } from './card-minimal.component';
+import { OrgNameModule } from '../../pipes/org-name.pipe';
 
 @NgModule({
   declarations: [OrganizationCardMinimalComponent],
@@ -18,7 +17,6 @@ import { MatRippleModule } from '@angular/material/core';
     FlexLayoutModule,
     ImageReferenceModule,
     OrgNameModule,
-    MatRippleModule,
   ]
 })
 export class OrganizationCardMinimalModule { }
