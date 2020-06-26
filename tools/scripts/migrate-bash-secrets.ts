9c79a6cb1f9d9a6ddc17bd6fb70244f57c9973dd
George Grigorian <george.g93@gmail.com>
2020-06-26 19:15:36 +1000

    build(migrate-bash-secrets): initial commit

---
+++ tools/scripts/migrate-bash-secrets.ts
@@ -0,0 +1,2 @@
+// TODO: Write script that will migrate secrets.sh to .env, gitignore updated in next release.
+// ? when should this be run? auto hook on install? update? by the deploy script if secrets found? when to remove gitignore entry?
