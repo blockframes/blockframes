2eee6cf4a90c4817db15cdc2656b52d884b5dea9
George Grigorian <george.g93@gmail.com>
2020-06-26 22:00:09 +1000

    fix(secrets.sh): improve parsing functionality

--- tools/scripts/deploy-function-config.ts
+++ tools/scripts/deploy-function-config.ts
@@ -1,44 +1,21 @@
 import * as firebaseTools from 'firebase-tools';
-import { readFileSync, existsSync } from 'fs';
-import { resolve } from 'path';
 import * as dotenv from 'dotenv';
+import { loadSecretsFile } from './lib';
 
 const dotenvResult = dotenv.config();
 
 // const fileExists = (fileName: string) => existsSync(resolve(process.cwd(), fileName))
 
-const SECRETS_FILEPATH = 'secrets.sh';
-const SECRETS_TEMPLATE_FILEPATH = 'secrets.template.sh';
-
-const FIREBASE_CONFIG: any = {};
-
 if (!process.env.SENDGRID_API_KEY || !process.env.ETHEREUM_MNEMONIC) {
-  // Env probably doesnt exist in env
+  // Env config values probably doesn't exist in env
   loadSecretsFile();
 }
 
 setFirebaseConfig();
 
-function loadSecretsFile() {
-  let deploySecrets: { [key: string]: string };
-  let secretsBuffer: Buffer;
-  try {
-    secretsBuffer = readFileSync(resolve(process.cwd(), SECRETS_FILEPATH));
-  } catch (e) {
-    // * secrets file read failed, use template instead
-    secretsBuffer = readFileSync(resolve(process.cwd(), SECRETS_TEMPLATE_FILEPATH));
-  } finally {
-    // * Parse secrets.sh file.
-    deploySecrets = parseBashExports(secretsBuffer.toString()); // forEach(char => console.log(char, '\n'));
-  }
-  // * Write parsed keys to environment for use in other scripts
-  Object.keys(deploySecrets).forEach(key => {
-    process.env[key] = deploySecrets[key];
-  });
-}
-
 async function setFirebaseConfig() {
   // * Check if we are in CI
+  const FIREBASE_CONFIG: any = {};
   if (process.env.FIREBASE_CI_TOKEN) FIREBASE_CONFIG.token = process.env.FIREBASE_CI_TOKEN;
   process.stdout.write('Existing Firebase Functions Config Values:\n');
   await firebaseTools.functions.config.get(undefined, FIREBASE_CONFIG).then(console.log);
@@ -64,24 +41,5 @@ function getKeyValFormat(): string[] {
   output.push(`algolia.api_key=${process.env?.ALGOLIA_API_KEY}`);
   output.push(`admin.password=${process.env?.ADMIN_PASSWORD}`);
   output.push(`admin.email=${process.env?.CASCADE8_ADMIN}`);
-  return output;
-}
-
-/**
- * Checks for `export`s in a bash script and parses them as strings
- * @param input a string with the contents of the file
- * @returns a map with key-value pairs
- */
-function parseBashExports(input: string): { [key: string]: string } {
-  const lines = input.split('\n');
-  const output = {};
-  lines.forEach(line => {
-    const words = line.split(/\s/g);
-    if (words?.[0] !== 'export') return;
-    const assignment = words[1].split('=');
-    const key = assignment[0];
-    const value = assignment[1].replace(/"|'/g, '');
-    output[key] = value;
-  });
   return output;
 }
