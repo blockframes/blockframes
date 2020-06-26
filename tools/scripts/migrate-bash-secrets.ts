e831811a9266456da12e0880cd4d131447bc8ec6
George Grigorian <george.g93@gmail.com>
2020-06-26 22:00:23 +1000

    build(migrate-bash-secrets): write script and hook in postinstall

--- tools/scripts/migrate-bash-secrets.ts
+++ tools/scripts/migrate-bash-secrets.ts
@@ -1,2 +1,22 @@
-// TODO: Write script that will migrate secrets.sh to .env, gitignore updated in next release.
-// ? when should this be run? auto hook on install? update? by the deploy script if secrets found? when to remove gitignore entry?
+// TODO: remove secrets.sh from gitignore in next release, maybe remove from postinstall
+import { loadSecretsFile } from './lib';
+import { existsSync, writeFileSync } from 'fs';
+import { resolve } from 'path';
+
+const envPath = resolve(process.cwd(), '.env');
+
+console.log('This script will generate a .env file from your secrets.sh if no .env file exists.\n');
+
+if (existsSync(envPath)) {
+  console.log('Looks like you have already set up your .env file! Aborting migration...');
+} else {
+  console.log('No .env file detected, running migration...');
+  const parsedSecrets = loadSecretsFile();
+  const envArray = Object.keys(parsedSecrets).map(key => {
+    return `${key}="${parsedSecrets[key]}"`;
+  });
+  writeFileSync(envPath, envArray.join('\n'));
+  console.log(
+    'Migration Complete!\n\nPlease delete your secrets.sh and secrets.template.sh after checking .env file is correct.'
+  );
+}
