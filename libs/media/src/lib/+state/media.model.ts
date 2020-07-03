0b3c75fee97bd4c7628645a962a89c951ad6c18b
pldespaigne <pl.despaigne@gmail.com>
2020-07-03 11:55:09 +0200

    merged for user

--- libs/media/src/lib/+state/media.model.ts
+++ libs/media/src/lib/+state/media.model.ts
@@ -1,6 +1,61 @@
-import { ImgRef } from './media.firestore';
+import { ImgRef, HostedMediaFormValue } from './media.firestore';
 import { isSafari } from '@blockframes/utils/safari-banner/safari.utils';
+import { cloneDeep } from 'lodash';
 export * from './media.firestore';
+
+/**
+ * This function **clean** a document from it's medias before updating it in the firestore.
+ * We need to clean it because the backend functions are supposed to manage medias in the db,
+ * and **not the front**.
+ * The function also return an array of media to upload, we can then pass this array to the media service.
+ */
+export function extractMediaFromDocumentBeforeUpdate(document: any) {
+
+  // const cleanedDocument = JSON.parse(JSON.stringify(document));
+  const cleanedDocument = cloneDeep(document);
+
+  const medias = extractMediaFromDocument(cleanedDocument);
+  return {
+    documentToUpdate: cleanedDocument,
+    mediasToUpload: medias,
+  };
+}
+
+function extractMediaFromDocument(document: any) {
+  let medias: HostedMediaFormValue[] = [];
+
+  for (const key in document) {
+
+    if (isMedia(document[key])) {
+
+      if (mediaNeedsUpdate(document[key])) {
+        medias.push(document[key]);
+      }
+
+      delete document[key];
+
+    } else if (typeof document[key] === 'object' && !!document[key]) {
+
+      const childMedias = extractMediaFromDocument(document[key]);
+      medias = medias.concat(childMedias);
+
+    }
+  }
+  return medias;
+}
+
+function isMedia(obj: any) {
+  return (
+    typeof obj === 'object' &&
+    !!obj &&
+    'ref' in obj &&
+    'url' in obj
+  );
+}
+
+function mediaNeedsUpdate(media: HostedMediaFormValue) {
+  return media.delete || (!!media.ref && !!media.blobOrFile);
+}
 
 const formats = {
   avatar: {
--- libs/media/src/lib/+state/media.service.ts
+++ libs/media/src/lib/+state/media.service.ts
@@ -7,7 +7,7 @@ import { ComponentPortal } from '@angular/cdk/portal';
 // State
 import { MediaStore, isDone } from "./media.store";
 import { MediaQuery } from "./media.query";
-import { UploadFile, ImgRef, imgSizeDirectory } from "./media.firestore";
+import { UploadFile, ImgRef, imgSizeDirectory, HostedMediaFormValue } from "./media.firestore";
 import { HostedMediaForm } from "../directives/media/media.form";
 
 // Blockframes
@@ -183,36 +183,36 @@ export class MediaService {
     }
   }
 
-  async uploadOrDeleteMedia(mediaForms: HostedMediaForm[]) {
+  async uploadOrDeleteMedia(mediaForms: HostedMediaFormValue[]) {
     const promises = mediaForms.map(async mediaForm => {
 
-      if (!!mediaForm.fileName.value) {
+      if (!!mediaForm.fileName) {
         // remove every characters after the 100th to avoid file too long error
         // this way we don't need to sanitize name anymore
         // (firebase also supports file names with unicode chars)
-        mediaForm.fileName.setValue(mediaForm.fileName.value.substr(0, 100));
+        mediaForm.fileName = mediaForm.fileName.substr(0, 100);
       }
 
       // if the file needs to be deleted and we know its path
-      if (mediaForm.delete.value && !!mediaForm.oldRef.value) {
+      if (mediaForm.delete && !!mediaForm.oldRef) {
 
-        await this.removeFile(mediaForm.oldRef.value);
+        await this.removeFile(mediaForm.oldRef);
 
       // if we have a blob = the user created or updated the file
-      } else if (!!mediaForm.blobOrFile.value) {
+      } else if (!!mediaForm.blobOrFile) {
 
         // if the file already have a path it means that we are in an update
         // we first need to delete the old file
-        if (mediaForm.oldRef.value !== '') {
+        if (mediaForm.oldRef !== '') {
           try {
-            await this.removeFile(mediaForm.oldRef.value);
+            await this.removeFile(mediaForm.oldRef);
           } catch (error) {
             console.warn('Deletion of previous file failed, but new file will still be uploaded');
           }
         }
 
         // upload the new file
-        await this.upload(mediaForm.ref.value, mediaForm.fileName.value, mediaForm.blobOrFile.value);
+        await this.upload(mediaForm.ref, mediaForm.fileName, mediaForm.blobOrFile);
       }
     });
     await Promise.all(promises);
