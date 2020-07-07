4cbe60cddd4b805ec676647f2099af413ff1ee9d
George Grigorian <george.g93@gmail.com>
2020-07-07 20:59:54 +1000

    refactor(testing): reorganise libs into one testing lib

--- libs/testing/firebase/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'testing-firebase',
-  preset: '../../../jest.config.js',
-  transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest'
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../../coverage/libs/testing/firebase'
-};
--- libs/testing/util/README.md
+++
@@ -1,7 +0,0 @@
-# testing-util
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `ng test testing-util` to execute the unit tests via [Jest](https://jestjs.io).
