8a877922538c5b6b277e8b5c4ae535e4a27f8948
RemcoSimonides <remcosimonides@gmail.com>
2020-07-07 15:32:35 +0200

    Fix Circular Dependency v2

--- libs/media/src/lib/+state/media.tasks.ts
+++
@@ -1,4 +0,0 @@
-import { InjectionToken } from "@angular/core";
-import { AngularFireUploadTask } from "@angular/fire/storage";
-
-export const REFERENCES = new InjectionToken('References', { factory: () => ({ tasks: <Record<string, AngularFireUploadTask>> {} })});
