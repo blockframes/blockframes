13465556b80ca9b71cdc67b48339566fcae9e9c9
bruce <delorme.bruce.michel@gmail.com>
2020-07-07 17:15:17 +0200

    debuged docIndex & cleaning unused private config

--- libs/utils/src/lib/common-interfaces/utility.ts
+++ libs/utils/src/lib/common-interfaces/utility.ts
@@ -20,14 +20,7 @@ export interface BankAccount {
 export interface DocsIndex {
   /** @dev doc author'id. Setted by a backend function */
   authorOrgId: string;
-  /** @dev private custom config for a doc. Setted by an https callable function */
-  config?: PrivateConfig
 }
-
-export interface PrivateConfig {
-  url?: string
-};
-
 /** A factory function that creates an Address/Location */
 export function createLocation(params: Partial<Location> = {}): Location {
   return {
