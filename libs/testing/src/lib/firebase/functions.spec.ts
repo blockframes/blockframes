14d945ac98e4c8b41ae18fd0e6ef4ff68b5d838b
xmano <mano@xkanda.com>
2020-07-08 13:32:27 +0530

    update(test-case): add a test case to directly connect local-emulator.

--- libs/testing/src/lib/firebase/functions.spec.ts
+++ libs/testing/src/lib/firebase/functions.spec.ts
@@ -1,8 +1,20 @@
 import { initFunctionsTestMock } from './functions';
+import * as firebaseTesting from '@firebase/testing'
 import { firestore, initializeApp } from 'firebase-admin';
+
+const project_ID = "blockframes-mano";
 
 describe('firebase testing library', () => {
   test.todo('Hello World!');
+
+  it('should access emulator directly', async () => {
+    const db = firebaseTesting.initializeTestApp({
+      projectId: project_ID
+    }).firestore();
+    const testDoc = await db.collection("testCollection").doc("testDoc");
+    firebaseTesting.assertSucceeds(testDoc.get());
+  });
+
   it.skip('should be able to access the emulator', async () => {
     const firebaseTest = initFunctionsTestMock();
     const db = firestore();
