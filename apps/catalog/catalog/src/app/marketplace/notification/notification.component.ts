a67a332656bc04a1d3ef58895e24ba07d87d79d1
fritzschoff <max.j.f@gmx.de>
2020-07-08 15:16:39 +0200

    updated catalog

--- apps/catalog/catalog/src/app/marketplace/notification/notification.component.ts
+++ apps/catalog/catalog/src/app/marketplace/notification/notification.component.ts
@@ -1,6 +1,7 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { NotificationQuery } from '@blockframes/notification/+state/notification.query';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
+import { NotificationService } from '@blockframes/notification/+state';
 
 @Component({
   selector: 'catalog-marketplace-notification',
@@ -13,5 +14,11 @@ export class NotificationComponent {
   public hasNotifications$ = this.query.selectCount().pipe(map(count => !!count))
   public notifications$ = this.query.selectAll();
 
-  constructor(private query: NotificationQuery) {}
+  constructor(private query: NotificationQuery, private service: NotificationService) {}
+
+  markAll() {
+    this.notifications$.pipe(take(1)).subscribe(notifications => notifications.forEach(notification => {
+      this.service.readNotification(notification)
+    }))
+  }
 }
