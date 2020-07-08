f090ccd7e753b7ad35aec2a73374cb338c2809a2
xmano <mano@xkanda.com>
2020-07-08 16:37:37 +0530

    update(test): to use testing library infra. Read test fails. TBD.

--- libs/testing/src/lib/firebase/functions.spec.ts
+++ libs/testing/src/lib/firebase/functions.spec.ts
@@ -2,38 +2,53 @@ import { initFunctionsTestMock } from './functions';
 import * as firebaseTesting from '@firebase/testing'
 import { firestore, initializeApp } from 'firebase-admin';
 
-const project_ID = "blockframes-mano";
-
 describe('firebase testing library', () => {
-  test.todo('Hello World!');
 
-  it('should access emulator directly', async () => {
-    const db = firebaseTesting.initializeTestApp({
-      projectId: project_ID
-    }).firestore();
-    const testDoc = await db.collection("testCollection").doc("testDoc");
-    firebaseTesting.assertSucceeds(testDoc.get());
+  describe('Direct access to emulator', () => {
+    it.skip('should access emulator directly', async () => {
+      //TODO : This is for reference
+      const db = firebaseTesting.initializeTestApp({
+        projectId: String(Math.random())
+      }).firestore();
+      const testDoc = await db.collection("testCollection").doc("testDoc");
+      firebaseTesting.assertSucceeds(testDoc.get());
+    });
+  
+    it.skip('should set a field in document', async () => {
+      const db = firebaseTesting.initializeTestApp({
+        projectId: String(Math.random())
+      }).firestore();
+      const testStr = "blockframes";
+      const testDoc = db.collection("testCollection")
+                      .doc("testDoc").set({testStr});
+      //TODO: Debug the test failure.                
+      await expect(db.doc('testCollection/testDoc').get())
+            .resolves.toBe({testStr});
+    });
+  })
+
+  describe('Access emulator through library', () => {
+    let db;
+
+    beforeAll(() => {
+      const firebaseTest = initFunctionsTestMock();
+      db = firestore();      
+    }) 
+
+    it('should be able to access the emulator', async () => {
+      const testDoc = await db.collection("testCollection").doc("testDoc");
+      firebaseTesting.assertSucceeds(testDoc.get());    
+    });
+
+    it('should be able to read the document', async () => {
+      const testStr = "blockframes";
+      const testDoc = db.collection("testCollection")
+                      .doc("testDoc").set({testStr});
+      //TODO: Debug the test failure.      
+      //Expectation failure : Received promise rejected instead of resolved          
+      await expect(db.doc('testCollection/testDoc').get())
+            .resolves.toBe({testStr});   
+    });
   });
 
-  it('should set a field in document', async () => {
-    const db = firebaseTesting.initializeTestApp({
-      projectId: project_ID
-    }).firestore();
-    const testStr = "blockframes";
-    const testDoc = db.collection("testCollection")
-                    .doc("testDoc").set({testStr});
-    await expect(db.doc('testCollection/testDoc').get())
-          .resolves.toBe({testStr});
-  });
-
-  it.skip('should be able to access the emulator', async () => {
-    const firebaseTest = initFunctionsTestMock();
-    const db = firestore();
-    const test = 5;
-    await db
-      .collection('test')
-      .doc('test')
-      .set({ test });
-    await expect(db.doc('test/test').get()).resolves.toBe(5);
-  });
 });
