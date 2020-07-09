0a22f92a227b7a253b63e635d686abf98be136e9
George Grigorian <george.g93@gmail.com>
2020-07-09 20:35:39 +1000

    feat(scedule-daily): add scedule for daily DB backup - completed & tested

--- apps/backend-functions/src/pubsub/schedule-daily.ts
+++ apps/backend-functions/src/pubsub/schedule-daily.ts
@@ -1,9 +1,18 @@
 import { pubsub } from 'firebase-functions';
 import { freeze } from '../backup';
 
-export default pubsub.schedule('daily').onRun(context => {
+export default pubsub.schedule('0 0 * * *').onRun(context => {
   // const { eventId, eventType, params, resource, timestamp, auth, authType } = context;
   console.log('This will be run daily! Running DB Backup!');
-  return freeze({}, {});
+  console.log('Context: ', context);
+  // Mock - return resp.status(200).send('success');
+  return freeze(
+    {},
+    {
+      // TODO: This is messy - decouple backup code in testing library
+      // @ts-ignore
+      status: number => ({ send: string => null })
+    }
+  );
   // return null;
 });
