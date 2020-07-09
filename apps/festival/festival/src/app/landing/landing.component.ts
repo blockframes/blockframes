bb6292d3cc687fec7f85116e0487eae9209a4fbc
JoanOfDart <ing.sebastian.montoya@gmail.com>
2020-07-09 14:24:11 +0200

    APP-3235 Should have working images. Related to #3235 - Note: IFCIC_logo has no png fallback.

--- apps/festival/festival/src/app/landing/landing.component.ts
+++ apps/festival/festival/src/app/landing/landing.component.ts
@@ -1,6 +1,7 @@
 import { Component, ChangeDetectionStrategy, HostListener } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { distinctUntilChanged, map } from 'rxjs/operators';
+import { isSafari } from '@blockframes/utils/safari-banner/safari.utils';
 
 @Component({
   selector: 'festival-landing',
@@ -26,12 +27,12 @@ export class LandingComponent {
   public tabTitle = [
     {
       title: 'For Sales Agents.',
-      imgSrc: '/assets/images/lp_dashboard_market_sales_agents.png',
+      imgSrc: `/assets/images/lp_dashboard_market_sales_agents.${ isSafari() ? 'png':'webp' }`,
       description: 'Showcase your line-up, get in touch with Buyers and manage meetings and screenings.'
     },
     {
       title: 'For Buyers.',
-      imgSrc: '/assets/images/lp_archipel_market_buyers.png',
+      imgSrc: `/assets/images/lp_archipel_market_buyers.${ isSafari() ? 'png':'webp' }`,
       description: 'Explore a large library of films, get in touch with sales agents, plan meetings and watch screenings.'
     }
   ]
