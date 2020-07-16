6290cefed6cf3a69b6ecffb7ab1545424b552154
hugo-hontans <hugohontans@gmail.com>
2020-07-16 11:09:05 +0200

    add todo for e2e test chain of titles

--- apps/catalog/catalog-e2e/src/support/movie-tunnel-tests/8-chain-of-titles.ts
+++ apps/catalog/catalog-e2e/src/support/movie-tunnel-tests/8-chain-of-titles.ts
@@ -4,9 +4,7 @@ import { TunnelChainOfTitlesPage, TunnelValuationPage } from "../pages/dashboard
 export const chainOfTitlesTest = () => {
   const p1 = new TunnelChainOfTitlesPage();
 
-  createFakeScript(`${randomID()}`)
-  .then(path => p1.uploadChainOfTitlesFile(path))
-  .then(() => p1.assertChainOfTitlesFileHasUploadSucceeded());
+  // TODO: Test upload of chain of titles
 
   // GO to movie tunnel valuation page
   const p2: TunnelValuationPage = p1.clickNext();
