45734345a0a4bcbdcfe644ced971cc0d03d38993
xmano <mano@xkanda.com>
2020-07-17 20:53:06 +0530

    Test(notif): Add more unit tests for notification service.

--- libs/notification/src/lib/notification/+state/notification.service.spec.ts
+++ libs/notification/src/lib/notification/+state/notification.service.spec.ts
@@ -1,4 +1,4 @@
-import {TestBed} from '@angular/core/testing';
+﻿import {TestBed} from '@angular/core/testing';
 
 import { NotificationService } from './notification.service';
 import { NotificationStore } from './notification.store';
@@ -6,16 +6,25 @@ import { AngularFireModule } from '@angular/fire';
 import { SETTINGS, AngularFirestoreModule, AngularFirestore } from '@angular/fire/firestore';
 import { loadFirestoreRules, clearFirestoreData } from '@firebase/testing';
 import { readFileSync } from 'fs';
+import { Notification } from './notification.model';
 
 class NotificationStoreStub  {
   formatNotification(notification) {
-    return { format: JSON.stringify(notification) };
+    return { format: notification };
   }
 };
 
 describe('Notifications Test Suite', () => {
   let service: NotificationService;
-  let db: AngularFirestore
+  let db: AngularFirestore;
+  const notif: Notification = {
+    type: 'newContract',
+    toUserId: "",
+    id: "",
+    isRead: false,
+    message: "Jest Notify",
+    date: new Date()
+  };
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
@@ -40,10 +49,19 @@ describe('Notifications Test Suite', () => {
 
   afterEach(() => clearFirestoreData({projectId: 'test'}))
 
+  it('Should check notif service is created', () => {
+    expect(service).toBeTruthy();
+  })
+
   it('Should mark notifications as read', async () => {
     await db.doc('notifications/1').set({ isRead: false });
     await service.readNotification({ id: '1' });
     const doc = await db.doc('notifications/1').ref.get();
     expect(doc.data().isRead).toBeTruthy();
+  });
+
+  it('formats notification', () => {
+    const newNotif = service.formatFromFirestore(notif);
+    expect(newNotif).toEqual({ ...notif, format: notif});
   });
 });
