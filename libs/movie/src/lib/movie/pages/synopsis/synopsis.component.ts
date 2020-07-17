f9a5e1b7818cf64f3aaa18e37dd1dc9bc800de9d
JoanOfDart <ing.sebastian.montoya@gmail.com>
2020-07-17 10:33:57 +0200

    Tids and bits regarding synopsis component merge.

--- libs/movie/src/lib/movie/pages/synopsis/synopsis.component.ts
+++ libs/movie/src/lib/movie/pages/synopsis/synopsis.component.ts
@@ -2,6 +2,9 @@ import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { MovieFormShellComponent } from '../shell/shell.component';
 import { Observable } from 'rxjs';
 import { startWith } from 'rxjs/operators';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'movie-form-synopsis',
@@ -12,6 +15,8 @@ import { startWith } from 'rxjs/operators';
 export class MovieFormSynopsisComponent implements OnInit {
   values$: Observable<string[]>;
   form = this.shell.form;
+  keyword = new FormControl();
+  public separatorKeysCodes: number[] = [ENTER, COMMA];
 
   constructor(private shell: MovieFormShellComponent) { }
 
@@ -29,5 +34,16 @@ export class MovieFormSynopsisComponent implements OnInit {
 
   get keywords() {
     return this.form.get('promotionalDescription').get('keywords');
+  }
+
+  public add(event: MatChipInputEvent): void {
+    const { value = '' } = event;
+
+    this.keywords.add(value)
+    this.keyword.reset();
+  }
+
+  public remove(i: number): void {
+    this.keywords.removeAt(i);
   }
 }
