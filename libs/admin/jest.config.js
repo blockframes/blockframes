30e62de81d9accf84b7fed6061b9848f7b91ab1c
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2020-07-21 11:41:41 +0200

    upgrade to nx-10

--- libs/admin/jest.config.js
+++ libs/admin/jest.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   name: 'admin',
   preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/admin'
+  coverageDirectory: '../../coverage/libs/admin',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
 };
--- libs/contract/jest.config.js
+++ libs/contract/jest.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   name: 'contract',
   preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/contract'
+  coverageDirectory: '../../coverage/libs/contract',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
 };
