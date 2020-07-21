98dfee54e15fd1bce16f31b78a18d15c8694bb45
fritzschoff <max.j.f@gmx.de>
2020-07-21 11:12:58 +0200

    removed rxjs from table component

--- libs/ui/src/lib/form/list/form-list.module.ts
+++ libs/ui/src/lib/form/list/form-list.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms'
 
 // Components
-import { FormListComponent } from './form-list.component';
+import { FormListComponent, FormViewDirective } from './form-list.component';
 import { FormListTableModule } from './table/form-list-table.module';
 
 // Material
@@ -23,7 +23,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatButtonModule,
     MatIconModule
   ],
-  declarations: [FormListComponent],
-  exports: [FormListComponent, FormListTableModule]
+  declarations: [FormListComponent, FormViewDirective],
+  exports: [FormListComponent, FormListTableModule, FormViewDirective]
 })
 export class FormListModule { }
