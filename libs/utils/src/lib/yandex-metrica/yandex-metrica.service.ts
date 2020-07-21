a590dc12b9a68181891499a99209e7cae9685e87
Mussuranae <mandonnetdev@outlook.com>
2020-07-21 14:29:13 +0200

    yandex metrika ok

--- libs/utils/src/lib/yandex-metrica/yandex-metrica.service.ts
+++ libs/utils/src/lib/yandex-metrica/yandex-metrica.service.ts
@@ -1,5 +1,5 @@
-import { DOCUMENT, isPlatformBrowser } from '@angular/common';
-import { Injectable, Inject, InjectionToken, PLATFORM_ID } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Injectable, Inject, InjectionToken } from '@angular/core';
 
 export const YM_CONFIG = new InjectionToken('ngx-metrika Config');
 
@@ -10,34 +10,35 @@ export class YandexMetricaService {
   }
 
   private insertMetrika(config: number) {
-    if (!isPlatformBrowser(PLATFORM_ID)) {
-        return;
-    }
-
-    // Get the first head element
-    const head = this.document.getElementsByTagName('head')[0];
-    // Create a script tag
-    const script = document.createElement('script');
-    // Specify the type
-    script.type = 'text/javascript';
-    // Implement the function
-    script.textContent = `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    if (window !== undefined) {
+      // Get the first head element
+      const head = this.document.getElementsByTagName('head')[0];
+      // Create a script tag
+      const script = document.createElement('script');
+      // Specify the type
+      script.type = 'text/javascript';
+      // Implement the function
+      script.text = `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
         m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
         (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
         ym(${config}, "init", {
-             clickmap:true,
-             trackLinks:true,
-             accurateTrackBounce:true,
-             webvisor:true
-        });`;
-    // Append script tag into the head
-    const insetScriptTag = () => head.appendChild(script);
-    // Fix for Opera browser
-    if ((window as any).opera === '[object Opera]') {
-      this.document.addEventListener('DOMContentLoaded', insetScriptTag, false);
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true,
+          webvisor:true
+      });`;
+
+      // Append script tag into the head
+      const insetScriptTag = () => head.appendChild(script);
+      // Fix for Opera browser
+      if ((window as any).opera === '[object Opera]') {
+        this.document.addEventListener('DOMContentLoaded', insetScriptTag, false);
+      } else {
+        insetScriptTag();
+      }
     } else {
-      insetScriptTag();
+      return;
     }
   }
 }
