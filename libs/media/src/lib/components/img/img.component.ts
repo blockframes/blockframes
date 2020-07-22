dc6ebea6bfba415c251ce101399900bb28157a0a
pldespaigne <pl.despaigne@gmail.com>
2020-07-22 17:46:16 +0200

    remove picture & refactored img directive

--- libs/media/src/lib/components/img/img.component.ts
+++ libs/media/src/lib/components/img/img.component.ts
@@ -3,11 +3,10 @@ import { Subscription, BehaviorSubject, combineLatest } from 'rxjs';
 import { ThemeService } from '@blockframes/ui/theme';
 import { map } from 'rxjs/operators';
 import { getAssetPath, HostedMedia } from '@blockframes/media/+state/media.model';
-import { ImageParameters, formatParameters } from '@blockframes/media/directives/image-reference/imgix-helpers';
-import { BreakpointsService } from '@blockframes/utils/breakpoint/breakpoints.service';
+import { ImageParameters, generateImageSrcset } from '@blockframes/media/directives/image-reference/imgix-helpers';
 
 @Component({
-  selector: '[ref] bf-img, [asset] bf-img',
+  selector: '[ref] [asset] bf-img, [asset] bf-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -23,13 +22,10 @@ export class ImgComponent implements OnInit, OnDestroy {
     fit: 'crop',
   };
 
-  private ref$ = new BehaviorSubject('');
-  public refSrc: string;
-
   private asset$ = new BehaviorSubject('');
-  public assetSrc: string;
-
-  public format: string;
+  private ref$ = new BehaviorSubject('');
+  public srcset: string;
+  public src: string;
 
   /** The alt attribute for the image */
   @Input() alt: string;
@@ -37,16 +33,6 @@ export class ImgComponent implements OnInit, OnDestroy {
   // -----------------------------------
   //           MEDIA IMAGE INPUT
   // -----------------------------------
-
-  /** width of the image in px */
-  @Input() set width(w: number) {
-    this.parameters.width = w;
-  }
-
-  /** height of the image in px */
-  @Input() set height(h: number) {
-    this.parameters.height = h;
-  }
 
   /** the image to display */
   @Input() set ref(image: HostedMedia) {
@@ -85,7 +71,6 @@ export class ImgComponent implements OnInit, OnDestroy {
   constructor(
     private themeService: ThemeService,
     private cdr: ChangeDetectorRef,
-    private breakpointsService: BreakpointsService,
   ) {}
 
   ngOnInit() {
@@ -102,26 +87,20 @@ export class ImgComponent implements OnInit, OnDestroy {
       this.asset$,
       this.ref$,
       theme$,
-      this.breakpointsService.currentWidth,
-    ]).subscribe(([asset, ref, theme, width]) => {
+    ]).subscribe(([asset, ref, theme]) => {
 
-      // asset
-      this.assetSrc = getAssetPath(asset, theme, this.type);
-      this.format = this.assetSrc.split('.').pop();
-
-      // ref
       if (!!ref) {
-        // copy the image parameters because we might change the width
-        const sizeParameters = { ...this.parameters };
 
-        // get the smallest image width
-        sizeParameters.width = Math.min(
-          width,
-          this.parameters.width || Infinity // in case width is undefined we pick Infinity instead
-        );
+        // ref
+        this.srcset = generateImageSrcset(ref, this.parameters);
+        this.src = this.srcset.split(' ')[0];
 
-        const query = formatParameters(sizeParameters);
-        this.refSrc = `https://blockframes-pl-2.imgix.net/${ref}?${query}`;
+      } else {
+
+        // asset
+        this.srcset = getAssetPath(asset, theme, this.type);
+        this.src = this.srcset;
+
       }
 
       this.cdr.markForCheck()
@@ -130,5 +109,16 @@ export class ImgComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.sub.unsubscribe();
+  }
+
+  error() {
+
+    const asset = this.asset$.getValue();
+    const local = this.localTheme$.getValue();
+    const global = this.themeService.theme;
+    const theme = local || global;
+
+    this.srcset = getAssetPath(asset, theme, this.type);
+    this.src = this.srcset
   }
 }
