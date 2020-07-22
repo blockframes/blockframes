7fc905b21eb968bc5390e09703ccea5007a2630b
hugo-hontans <hugohontans@gmail.com>
2020-07-22 15:52:43 +0200

    create invitation component in invitation lib

---
+++ libs/invitation/src/lib/invitation.module.ts
@@ -0,0 +1,30 @@
+// Angular
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+// Blockframes
+import { InvitationComponent } from './invitation.component';
+import { InvitationListModule } from './components/list/list.module';
+import { ImageReferenceModule } from '@blockframes/media/directives/image-reference/image-reference.module';
+
+// Material
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [InvitationComponent],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    InvitationListModule,
+    ImageReferenceModule,
+    RouterModule.forChild([{ path: '', component: InvitationComponent }]),
+
+    // Material
+    MatIconModule,
+    MatButtonModule,
+  ]
+})
+export class InvitationModule { }
