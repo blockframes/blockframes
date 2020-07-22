fb0253228686daf76419fd3a13256e4f8dfd15b5
JoanOfDart <ing.sebastian.montoya@gmail.com>
2020-07-22 12:23:47 +0200

    APP-3183 Synopsis page (Storyline)

--- libs/movie/src/lib/movie/pages/synopsis/synopsis.module.ts
+++ libs/movie/src/lib/movie/pages/synopsis/synopsis.module.ts
@@ -1,35 +1,50 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { MovieFormSynopsisComponent } from './synopsis.component';
-import { TunnelPageModule } from '@blockframes/ui/tunnel';
-import { MovieFormPromotionalDescriptionModule } from '@blockframes/movie/form/promotional-description/promotional-description.module';
-import { MovieFormStoryModule } from '@blockframes/movie/form/story/story.module';
+import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MovieFormKeywordsModule } from '@blockframes/movie/form/promotional-description/keywords/keywords.module';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 // Material
-import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
-import { CommonModule } from '@angular/common';
+
+// Blockframes Movie
+import { MovieFormPromotionalDescriptionModule } from '@blockframes/movie/form/promotional-description/promotional-description.module';
+import { MovieFormStoryModule } from '@blockframes/movie/form/story/story.module';
+import { MovieFormKeywordsModule } from '@blockframes/movie/form/promotional-description/keywords/keywords.module';
+
+// Blockframes UI
+import { TunnelPageModule } from '@blockframes/ui/tunnel';
+
+import { MovieFormSynopsisComponent } from './synopsis.component';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [MovieFormSynopsisComponent],
   imports: [
+    CommonModule,
     ReactiveFormsModule,
-    TunnelPageModule,
+    FlexLayoutModule,
+
+    // Movie Form Modules
     MovieFormKeywordsModule,
     MovieFormPromotionalDescriptionModule,
     MovieFormStoryModule,
+
+    // Other Modules
+    TunnelPageModule,
+
+    // Material
     MatFormFieldModule,
     MatChipsModule,
     MatIconModule,
     MatInputModule,
-    CommonModule,
+    MatDividerModule,
+
+    // Routes
     RouterModule.forChild([{ path: '', component: MovieFormSynopsisComponent }]),
-    MatCardModule
   ],
 })
 export class MovieFormSynopsisModule { }
