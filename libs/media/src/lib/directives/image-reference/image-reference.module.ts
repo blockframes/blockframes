20e7fa5b2a50ce6c630d813366cb352fba314c29
pldespaigne <pl.despaigne@gmail.com>
2020-07-22 17:46:16 +0200

    remove picture & refactored img directive

--- libs/media/src/lib/directives/image-reference/image-reference.module.ts
+++ libs/media/src/lib/directives/image-reference/image-reference.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BackgroundReferenceDirective } from './background-reference.directive';
 import { ImgModule } from '@blockframes/media/components/img/img.module';
+import { ImageReferenceDirective } from './image-reference.directive';
 
 @NgModule({
-  declarations: [BackgroundReferenceDirective],
+  declarations: [BackgroundReferenceDirective, ImageReferenceDirective],
   imports: [ImgModule],
-  exports: [ImgModule, BackgroundReferenceDirective]
+  exports: [ImgModule, BackgroundReferenceDirective, ImageReferenceDirective]
 })
 export class ImageReferenceModule {}
