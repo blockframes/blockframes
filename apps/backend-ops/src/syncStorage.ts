7554b52f1e5e924b953c8ef94f4b2e9d8ca842ea
Remco <RemcoSimonides@gmail.com>
2020-07-23 17:24:49 +0200

    Empty media refs in db

--- apps/backend-ops/src/syncStorage.ts
+++ apps/backend-ops/src/syncStorage.ts
@@ -1,6 +1,8 @@
 import { loadAdminServices } from './admin';
-import { getStorageBucketName } from 'apps/backend-functions/src/internals/firebase';
+import { getStorageBucketName, db } from 'apps/backend-functions/src/internals/firebase';
 import { getDocAndPath } from 'apps/backend-functions/src/media';
+import { getCollection } from 'apps/backend-functions/src/data/internals';
+import { createHostedMedia, createImgRef } from '@blockframes/media/+state/media.model';
 import objectPath from 'object-path';
 
 /**
@@ -8,17 +10,99 @@ import objectPath from 'object-path';
  */
 export async function syncStorage() {
 
+  console.log('//////////////');
+  console.log('// Deleting all media references in firestore');
+  console.log('//////////////');
+
+  // reference to the location of all medias in the db
+  const mediaReferences = [
+    { 
+      collection: 'users',
+      fields: [
+        { field: 'watermark', type: mediaFieldType.hostedMedia },
+        { field: 'avatar', type: mediaFieldType.imgRef },
+      ]
+    },
+    { 
+      collection: 'orgs', 
+      fields: [
+        { field: 'logo', type: mediaFieldType.imgRef }
+      ]
+    },
+    {
+      collection: 'movies',
+      fields: [
+        { field: 'main.banner.media', type: mediaFieldType.imgRef }, // TODO issue #3291
+        { field: 'main.poster.media', type: mediaFieldType.imgRef }, // TODO issue #3291
+        { field: 'promotionalElements.still_photo', type: mediaFieldType.recordOfImgRefs }
+      ]
+    }
+  ];
+
+  for (const ref of mediaReferences) {
+    const docs = await getCollection<any>(ref.collection);
+    const promises = [];
+
+    for (const doc of docs) {
+      const docId = doc.id ? doc.id : doc.uid;
+      const docRef = db.collection(ref.collection).doc(docId);
+      
+      for (const field of ref.fields) {
+        let data; 
+
+        switch (field.type) {
+          case mediaFieldType.hostedMedia:
+            // media field without different sizes
+            data = createHostedMedia();
+            break;
+          case mediaFieldType.imgRef:
+            // media field with different sizes
+            data = createImgRef();
+            break;
+          case mediaFieldType.recordOfImgRefs:
+            // media field with multiple medias
+            data = {};
+            break;
+        }
+
+        // if (docId === 'oHdU7uVJvXjpkd2THvuV') console.log('data: ', data);
+        const promise = docRef.update({[field.field]: data});
+        promises.push(promise);
+      }
+    }
+
+    await Promise.all(promises);
+  
+  }
+
+  console.log('//////////////');
+  console.log('// Recreating media references in firestore');
+  console.log('//////////////');
+
   const { storage } = loadAdminServices();
   const bucket = storage.bucket(getStorageBucketName());
   const [files] = await bucket.getFiles();
 
   const promises = files.map(async file => {
-
     try {
       const { filePath, doc, docData, fieldToUpdate } = await getDocAndPath(file.name);
 
       if (!objectPath.has(docData, fieldToUpdate)) {
-        throw new Error(`Lost File: no media field available in db`);
+
+        // exclude records of imgRefs from not finding a record because it will never exist.
+        const records = []
+        mediaReferences.forEach(ref => {
+          ref.fields.forEach(field => {
+            if (field.type === mediaFieldType.recordOfImgRefs) {
+              records.push(field.field);
+            }
+          })
+        })
+
+        if (!records.some(record => file.name.includes(record))) { 
+          throw new Error(`Lost File: no media field available in db`);
+        }
+
       }
 
       const [ signedUrl ] = await file.getSignedUrl({
@@ -33,9 +117,14 @@ export async function syncStorage() {
     } catch (error) {
       console.log(`An error happened when syncing ${file.name}!`, error.message);
     }
-    
-  })
+  });
 
   await Promise.all(promises);
 
+}
+
+enum mediaFieldType {
+  hostedMedia,
+  imgRef,
+  recordOfImgRefs
 }
