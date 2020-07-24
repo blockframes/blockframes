f15f4d4306ec49ea17352109f369720151c595c4
fritzschoff <max.j.f@gmx.de>
2020-07-24 15:59:40 +0200

    readded form list component

---
+++ libs/ui/src/lib/form/list/form-list.module.ts
@@ -0,0 +1,26 @@
+// Angular
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Components
+import { FormListComponent, ItemRefDirective, FormViewDirective } from './form-list.component';
+
+// Material
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  imports: [
+    CommonModule,
+
+    // Material
+    MatListModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  exports: [FormListComponent, FormViewDirective, ItemRefDirective],
+  declarations: [FormListComponent, FormViewDirective, ItemRefDirective],
+
+})
+export class FormListModule { } 
