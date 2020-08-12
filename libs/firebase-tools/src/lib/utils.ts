147decc2db6626cea352dad8e31ee77360928dd6
bruce <delorme.bruce.michel@gmail.com>
2020-08-12 11:03:56 +0200

    cleaning

--- libs/firebase-tools/src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import * as admin from 'firebase-admin';
-const db = admin.firestore();
-
-export function getDocument<T>(path: string): Promise<T> {
-  return db
-    .doc(path)
-    .get()
-    .then(doc => doc.data() as T);
-}
