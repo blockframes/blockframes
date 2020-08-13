aa38d88a9b4c54535d5f4976dd4d4469eb5a8e02
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2020-08-13 09:50:12 +0200

    Use folder for gitignore

--- tools/scripts/get-prod-backup.ts
+++ tools/scripts/get-prod-backup.ts
@@ -1,4 +1,6 @@
 import { backupBucket, firebase } from '../../env/env.prod';
+import { existsSync, mkdirSync } from 'fs';
+import { join } from 'path';
 import * as admin from 'firebase-admin';
 import { config } from 'dotenv';
 config();
@@ -6,28 +8,35 @@ config();
 admin.initializeApp({
   storageBucket: backupBucket,
   projectId: firebase.projectId,
-  credential: admin.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS),
+  credential: admin.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS)
 });
-
 const storage = admin.storage();
+const folder = join(process.cwd(), 'tmp');
 
 async function getProdBackup() {
-  const [files] = await storage.bucket(backupBucket).getFiles();
-  const sortableFiles = files.map((file) => ({ file, date: new Date(file.metadata?.timeCreated) }));
-  // for (const file of files) {
-  //   const date = new Date(file.metadata?.timeCreated);
-  //   console.log(date);
-  // }
-  let latestFile = sortableFiles.pop();
-  for (const { date, file } of sortableFiles) {
-    if (date > latestFile.date) {
-      latestFile = { date, file };
+  try {
+    // Get latest backup DB
+    const [ files ] = await storage.bucket(backupBucket).getFiles();
+    const last = files.sort((a, b) => a.metadata?.timeCreated - b.metadata?.timeCreated).pop();
+    console.log('Latest backup:', last.metadata.timeCreated);
+
+    // Ensure parent folder exist
+    if(!existsSync(folder)) {
+      mkdirSync(folder);
     }
+
+    // Dowload lastest backup
+    const destination = join(folder, last.name);
+    await last.download({ destination });
+    console.log('Backup has been saved to:', destination);
+
+  } catch (err) {
+    if ('errors' in err) {
+      err.errors.forEach(error => console.error('ERROR:', error.message));
+    } else{
+      console.log(err)
+    } 
   }
-  console.log(`Latest backup: ${latestFile.date}`);
-  const destination = `${process.cwd()}/${latestFile.file.name}`;
-  await latestFile.file.download({ destination });
-  console.log(`Backup has been saved to: ${destination}`);
 }
 
 getProdBackup().then(() => process.exit(0));
