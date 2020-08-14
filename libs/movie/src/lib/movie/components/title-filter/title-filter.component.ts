34380f121588e8a9e5df77ddcaab1edf713f5583
Remco <RemcoSimonides@gmail.com>
2020-08-14 10:31:54 +0200

    Active movie filter has active color

--- libs/movie/src/lib/movie/components/title-filter/title-filter.component.ts
+++ libs/movie/src/lib/movie/components/title-filter/title-filter.component.ts
@@ -17,22 +17,48 @@ import { map, startWith } from 'rxjs/operators';
 export class FilterDirective implements OnInit {
   @Input() label = 'filter name';
   @Input() form: AbstractControl;
-  private active$ = new BehaviorSubject(false);
+  private filterOpened$ = new BehaviorSubject(false);
+  public active = false;
   public color$: Observable<'primary' | ''>;
   constructor(public template: TemplateRef<any>) {}
 
-  set active(isActive: boolean) {
-    this.active$.next(isActive);
+  set filterOpened(isOpened: boolean) {
+    this.filterOpened$.next(isOpened);
   }
 
   ngOnInit() {
     this.color$ = combineLatest([
-      this.active$,
+      this.filterOpened$,
       this.form.valueChanges.pipe(startWith(this.form.value)),
     ]).pipe(
-      map(([active]) => active || this.form.dirty),
+      map(([isOpened]) => isOpened || this.form.dirty),
       map(hasColor => hasColor ? 'primary' : '')
     );
+
+    this.form.valueChanges.subscribe(value => {
+      switch (this.label) {
+        case "Sales Agents":
+        case "Genres":
+        case "Country of Origin":
+        case "Production Status":
+           this.active = value.length !== 0;
+          break;
+  
+        case "Language & Version":
+          this.active = !(value.original.length === 0 &&
+            value.dubbed.length === 0 &&
+            value.subtitle.length === 0 &&
+            value.caption.length === 0);
+          break;
+  
+        case "Budget":
+          this.active = value !== null && value !== 0;
+          break;
+  
+        default:
+          throw Error(`Unknown label ${this.label}`);
+      }
+    });
   }
 }
 
