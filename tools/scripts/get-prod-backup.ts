11cc2dab849e3934603d793cdde4992c2f241ba1
George Grigorian <george.g93@gmail.com>
2020-08-14 20:02:07 +1000

    feat(get-prod-backup.ts): added more verbose logging

--- tools/scripts/get-prod-backup.ts
+++ tools/scripts/get-prod-backup.ts
@@ -12,7 +12,7 @@ if (!('FIREBASE_PRODUCTION_SERVICE_ACCOUNT' in process.env)) {
 let cert: string | admin.ServiceAccount;
 try {
   // If service account is a stringified json object
-  cert = JSON.parse(process.env.FIREBASE_PRODUCTION_SERVICE_ACCOUNT)
+  cert = JSON.parse(process.env.FIREBASE_PRODUCTION_SERVICE_ACCOUNT);
 } catch (err) {
   // If service account is a path
   cert = process.env.FIREBASE_PRODUCTION_SERVICE_ACCOUNT;
@@ -21,20 +21,25 @@ try {
 admin.initializeApp({
   storageBucket: backupBucket,
   projectId: firebase.projectId,
-  credential: admin.credential.cert(cert)
+  credential: admin.credential.cert(cert),
 });
 const storage = admin.storage();
 const folder = join(process.cwd(), 'tmp');
 
 async function getProdBackup() {
+  console.log('Current projectId: ', firebase.projectId);
+  console.log('Current backup bucket name: ', backupBucket);
   try {
     // Get latest backup DB
-    const [ files ] = await storage.bucket(backupBucket).getFiles();
+    const [files] = await storage.bucket(backupBucket).getFiles();
     const last = files.sort((a, b) => a.metadata?.timeCreated - b.metadata?.timeCreated).pop();
     console.log('Latest backup:', last.metadata.timeCreated);
+    console.log('File name: ', last.name);
+    console.dir('File metadata is: ', last.metadata);
+    console.log('Bucket name: ', last.bucket.name);
 
     // Ensure parent folder exist
-    if(!existsSync(folder)) {
+    if (!existsSync(folder)) {
       mkdirSync(folder);
     }
 
@@ -42,13 +47,12 @@ async function getProdBackup() {
     const destination = join(folder, 'backup-prod.jsonl');
     await last.download({ destination });
     console.log('Backup has been saved to:', destination);
-
   } catch (err) {
     if ('errors' in err) {
-      err.errors.forEach(error => console.error('ERROR:', error.message));
-    } else{
-      console.log(err)
-    } 
+      err.errors.forEach((error) => console.error('ERROR:', error.message));
+    } else {
+      console.log(err);
+    }
   }
 }
 
