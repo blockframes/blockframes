2646ab9906648c39d998bc06f77dcde72f057b7b
bruce <delorme.bruce.michel@gmail.com>
2020-08-20 17:30:33 +0200

    added some conditionnal check to prevent from crashing

--- apps/backend-ops/src/db-cleaning.ts
+++ apps/backend-ops/src/db-cleaning.ts
@@ -105,13 +105,13 @@ export function cleanNotifications(
 async function cleanOneNotification(doc: QueryDocumentSnapshot, notification: NotificationDocument) {
   if (notification.organization) {
     const d = await getDocument<PublicOrganization>(`orgs/${notification.organization.id}`);
-    notification.organization.logo = d.logo || EMPTY_MEDIA;
+    notification.organization.logo = d?.logo || EMPTY_MEDIA;
   }
 
   if (notification.user) {
     const d = await getDocument<PublicUser>(`users/${notification.user.uid}`);
-    notification.user.avatar = d.avatar || EMPTY_MEDIA;
-    notification.user.watermark = d.watermark || EMPTY_MEDIA;
+    notification.user.avatar = d?.avatar || EMPTY_MEDIA;
+    notification.user.watermark = d?.watermark || EMPTY_MEDIA;
   }
 
   await doc.ref.update(notification);
@@ -136,24 +136,24 @@ export function cleanInvitations(
 async function cleanOneInvitation(doc: QueryDocumentSnapshot, invitation: InvitationDocument) {
   if (invitation.fromOrg?.id) {
     const d = await getDocument<PublicOrganization>(`orgs/${invitation.fromOrg.id}`);
-    invitation.fromOrg.logo = d.logo || EMPTY_MEDIA;
+    invitation.fromOrg.logo = d?.logo || EMPTY_MEDIA;
   }
 
   if (invitation.toOrg?.id) {
     const d = await getDocument<PublicOrganization>(`orgs/${invitation.toOrg.id}`);
-    invitation.toOrg.logo = d.logo || EMPTY_MEDIA;
+    invitation.toOrg.logo = d?.logo || EMPTY_MEDIA;
   }
 
   if (invitation.fromUser?.uid) {
     const d = await getDocument<PublicUser>(`users/${invitation.fromUser.uid}`);
-    invitation.fromUser.avatar = d.avatar || EMPTY_MEDIA;
-    invitation.fromUser.watermark = d.watermark || EMPTY_MEDIA;
+    invitation.fromUser.avatar = d?.avatar || EMPTY_MEDIA;
+    invitation.fromUser.watermark = d?.watermark || EMPTY_MEDIA;
   }
 
   if (invitation.toUser?.uid) {
     const d = await getDocument<PublicUser>(`users/${invitation.toUser.uid}`);
-    invitation.toUser.avatar = d.avatar || EMPTY_MEDIA;
-    invitation.toUser.watermark = d.watermark || EMPTY_MEDIA;
+    invitation.toUser.avatar = d?.avatar || EMPTY_MEDIA;
+    invitation.toUser.watermark = d?.watermark || EMPTY_MEDIA;
   }
 
   await doc.ref.update(invitation);
