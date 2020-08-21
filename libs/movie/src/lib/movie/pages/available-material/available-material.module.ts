35f9a46118f9e84c935da1eae7f2443c65dad708
fritzschoff <max.j.f@gmx.de>
2020-08-21 12:15:30 +0200

    updated form

--- libs/movie/src/lib/movie/pages/available-material/available-material.module.ts
+++ libs/movie/src/lib/movie/pages/available-material/available-material.module.ts
@@ -15,7 +15,6 @@ import { TunnelPageModule } from '@blockframes/ui/tunnel';
 // Material
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -34,7 +33,6 @@ import { MatDividerModule } from '@angular/material/divider';
     // Material
     MatCheckboxModule,
     MatSlideToggleModule,
-    MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     MatIconModule,
