ac62a67b55b690708597c786c1070a3f7f7c8ef5
fritzschoff <max.j.f@gmx.de>
2020-08-26 10:57:23 +0200

    fixing add information

--- libs/movie/src/lib/movie/form/budget/budget.component.ts
+++ libs/movie/src/lib/movie/form/budget/budget.component.ts
@@ -14,6 +14,7 @@ import { FormControl } from '@angular/forms';
 export class MovieFormBudgetComponent {
   @Input() form: FormControl;
   @Input() placeholder?: String;
+  @Input() appearance = 'outline'
 
   optionsBudget = BUDGET_LIST;
 }
