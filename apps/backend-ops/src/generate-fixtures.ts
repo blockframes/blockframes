c606b44761e28d4aeaf02b989223fa7bf40ed155
George Grigorian <george.g93@gmail.com>
2020-08-27 23:13:59 +1000

    feat(generateFixtures): added generation of movies.json and orgs.json

--- apps/backend-ops/src/generate-fixtures.ts
+++ apps/backend-ops/src/generate-fixtures.ts
@@ -3,13 +3,17 @@ import { User } from '@blockframes/user/types';
 import { promises as fsPromises } from 'fs';
 import { join } from 'path';
 import { USER_FIXTURES_PASSWORD } from './users';
+import { Movie } from '@blockframes/movie/+state/movie.model';
+import { Organization } from '@blockframes/organization/+state/organization.model';
 
 export async function generateFixtures() {
   await generateUsers();
+  await generateMovies();
+  await generateOrgs();
 }
 
 /**
- * This function will generate `tools/users.fixture.json` from users currently in Firestore
+ * This function will generate `tools/fixtures/users.json` from users currently in Firestore
  */
 async function generateUsers() {
   console.log('Generating user fixtures file from Firestore...');
@@ -24,6 +28,45 @@ async function generateUsers() {
     password: USER_FIXTURES_PASSWORD,
   }));
   const dest = join(process.cwd(), 'tools', 'fixtures', 'users.json');
+  await fsPromises.writeFile(dest, JSON.stringify(output));
+  console.log('Fixture saved:', dest);
+}
+
+/**
+ * This function will generate `tools/fixtures/movies.json` from users currently in Firestore
+ */
+async function generateMovies() {
+  console.log('Generating movie fixtures file from Firestore...');
+
+  console.time('Fetching movies from Firestore');
+  const movies = await getCollection<Movie>('movies');
+  console.timeEnd('Fetching movies from Firestore');
+
+  const output: Partial<Movie>[] = movies.map((movie) => ({
+    id: movie.id,
+    main: movie.main,
+  }));
+  const dest = join(process.cwd(), 'tools', 'fixtures', 'movies.json');
+  await fsPromises.writeFile(dest, JSON.stringify(output));
+  console.log('Fixture saved:', dest);
+}
+
+/**
+ * This function will generate `tools/fixtures/orgs.json` from users currently in Firestore
+ */
+async function generateOrgs() {
+  console.log('Generating orgs fixtures file from Firestore...');
+
+  console.time('Fetching orgs from Firestore');
+  const orgs = await getCollection<Organization>('orgs');
+  console.timeEnd('Fetching orgs from Firestore');
+
+  const output: Partial<Organization>[] = orgs.map((org) => ({
+    movieIds: org.movieIds,
+    appAccess: org.appAccess,
+    userIds: org.userIds,
+  }));
+  const dest = join(process.cwd(), 'tools', 'fixtures', 'orgs.json');
   await fsPromises.writeFile(dest, JSON.stringify(output));
   console.log('Fixture saved:', dest);
 }
