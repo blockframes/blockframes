27b74076b2d9a0f87a08deacf999c9ce33d461d7
xmano <mano@xkanda.com>
2020-09-02 07:16:41 +0530

    Update(e2e): Add the missing screenings fixture

---
+++ libs/e2e/src/lib/utils/screenings.ts
@@ -0,0 +1,46 @@
+import { Screening } from './type';
+
+export const EVENTS: Partial<Screening>[] = [
+  {
+    event: 'Felicità Private Screening',
+    by : {
+      email: 'jeanfelix@fake.com'
+    },
+    movie: { 
+      title: {
+        international: 'Felicità'
+      }
+    },
+    org : {
+      name: 'Charades'
+    },
+    invitees: [{
+        email: 'vchoukroun@fake.com'
+      }, {
+        
+      }
+    ],
+    private: true
+  },
+  {
+    event: 'Hunted Screening',
+    by : {
+      email: 'jeanfelix@fake.com'
+    },
+    movie: { 
+      title: {
+        international: 'Hunted'
+      }
+    },
+    org : {
+      name: 'Charades'
+    },
+    invitees: [{
+        email: 'vchoukroun@fake.com'
+      }, {
+        
+      }
+    ],
+    private: true
+  },
+];
