b3753797c88b14a05e57ab6bbcc80c18006f670a
Remco <RemcoSimonides@gmail.com>
2020-09-03 15:00:39 +0200

    Send email from organization

--- apps/backend-functions/src/data/internals.ts
+++ apps/backend-functions/src/data/internals.ts
@@ -10,7 +10,7 @@ import { ContractDocument } from '@blockframes/contract/contract/+state/contract
 import { createHostedMedia } from '@blockframes/media/+state/media.firestore';
 import { createDenomination } from '@blockframes/organization/+state/organization.firestore';
 import { App, getOrgAppAccess, getSendgridFrom, applicationUrl } from '@blockframes/utils/apps';
-import { EmailData } from '@sendgrid/helpers/classes/email-address';
+import { EmailJSON } from '@sendgrid/helpers/classes/email-address';
 import { getDocument } from '@blockframes/firebase-utils';
 
 export { getDocument };
@@ -108,7 +108,7 @@ export async function getAppUrl(_org: OrganizationDocument | string): Promise<st
  * This guess the app from the org app access and returns the "from" email address to use
  * @param _org
  */
-export async function getFromEmail(_org: OrganizationDocument | string): Promise<EmailData> {
+export async function getFromEmail(_org: OrganizationDocument | string): Promise<EmailJSON> {
   const key = await getOrgAppKey(_org);
   return getSendgridFrom(key);
 }
