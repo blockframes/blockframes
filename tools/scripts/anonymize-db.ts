4f79ee02c7994285d455f4f2079991f0432c0245
George Grigorian <george.g93@gmail.com>
2020-09-08 00:28:06 +1000

    feat(anon-db): working orgs anonymization script

--- tools/scripts/anonymize-db.ts
+++ tools/scripts/anonymize-db.ts
@@ -13,13 +13,9 @@ import { NotificationDocument } from '@blockframes/notification/types';
 import { Invitation } from '@blockframes/invitation/+state';
 import { JsonlDbRecord } from '@blockframes/firebase-utils';
 
-function throwError(e: any): never {
-  throw e;
-}
-
 type Id = string;
-const newUsers: Record<Id, User> = {};
-const newOrgs: Record<Id, Organization> = {};
+const newUsers: Record<Id, User | PublicUser> = {};
+const newOrgs: Record<Id, Organization | PublicOrganization> = {};
 
 const fakeEmail = (name: string) =>
   `dev+${name.replace(/\W/g, '').toLowerCase()}-${Math.random()
@@ -35,96 +31,93 @@ function assertType<T extends object>(doc: any, ...keys: (keyof T)[]): asserts d
   if (hasKeys<T>(doc, ...keys)) throw Error('WRONG OBJECT TYPE!');
 }
 
-function processUser(u: User) {
+function processUser<T extends User | PublicUser>(u: T): T {
   const firstName = faker.name.firstName();
   const lastName = faker.name.lastName();
   const email = fakeEmail(firstName);
   return { ...u, firstName, lastName, email };
 }
 
-function processOrg(o: Organization) {
+function processOrg<T extends Organization | PublicOrganization>(o: T): T {
   const companyName = faker.company.companyName();
   const denomination = { full: companyName, public: companyName };
   const email = fakeEmail(companyName);
   return { ...o, denomination, email };
 }
 
-function updateUser(user: User | PublicUser /*private?: boolean*/) {
+function processInvitation(i: Invitation): Invitation {
+  return {
+    ...i,
+    fromOrg: updateOrg(i.fromOrg),
+    toOrg: updateOrg(i.toOrg),
+    fromUser: updateUser(i.fromUser),
+    toUser: updateUser(i.toUser),
+  };
+}
+
+function processNotification(n: NotificationDocument): NotificationDocument {
+  return {
+    ...n,
+    organization: updateOrg(n.organization),
+    user: updateUser(n.user as PublicUser), // ! TODO: If possible don't use Partial in this type!
+  };
+}
+
+function updateUser(user: User | PublicUser) {
   if (!user) return;
-  if (hasKeys<PublicUser>(user, 'uid') && !hasKeys<User>(user, 'watermark')) {
-    // is public
-    return createPublicUser(
-      newUsers?.[user.uid] ?? processUser(user as User)
-      // throwError(Error(`CORRESPONDING USER NOT FOUND: ${JSON.stringify(user)}`))
-    );
-  } else if (hasKeys<PublicUser>(user, 'uid')) {
+  if (hasKeys<PublicUser>(user, 'uid') && !hasKeys<User>(user, 'watermark')) { // Is public
+    return createPublicUser( newUsers?.[user.uid] || (newUsers[user.uid] = processUser(user)));
+    // If not in cache, process the user, write to cache, make it a publicUser and return it
+  } else if (hasKeys<User>(user, 'uid')) {
     return newUsers?.[user.uid] || (newUsers[user.uid] = processUser(user));
   }
-  throw Error(`Unable to process user: ${JSON.stringify(user)}`);
-  // if (newUsers?.[user.uid]) {
-  //   // Already set this user previously
-  //   Object.assign(user, newUsers?.[user.uid]);
-  // } else {
-  //   // Process user
-  //   const firstName = faker.name.firstName();
-  //   const lastName = faker.name.lastName();
-  //   const email = `dev+${firstName.replace(/\W/g, '').toLowerCase()}-${Math.random()
-  //     .toString(36)
-  //     .replace(/[^a-z]+/g, '')
-  //     .substr(0, 3)}@cascade8.com`;
-  //   Object.assign(user, { firstName, lastName, email });
-  //   userTable.push([user.uid, user]);
-  // }
+  throw Error(`Unable to process user: ${JSON.stringify(user, null, 4)}`);
 }
 
 function updateOrg(org: Organization | PublicOrganization) {
   if (!org) return;
-  if (hasKeys<PublicOrganization>(org, 'denomination') && !hasKeys<Organization>(org, 'email')) {
-    // This is already a public one. Find corresponding,
-    return createPublicOrganization(
-      newOrgs?.[org.id] ?? throwError(Error(`CORRESPONDING ORG NOT FOUND: ${JSON.stringify(org)}`))
+  if (hasKeys<PublicOrganization>(org, 'denomination') && !hasKeys<Organization>(org, 'email')) { // Is public
+    return createPublicOrganization(newOrgs?.[org.id] || (newOrgs[org.id] = processOrg(org))
     );
   } else if (hasKeys<Organization>(org, 'email')) {
-    // Full org, process it
     return newOrgs?.[org.id] || (newOrgs[org.id] = processOrg(org));
   }
-  throw Error(`Unable to process ORG: ${JSON.stringify(org)}`);
-  // createPublicOrganization()
-  // const foundOrgTuple = orgTable.find(([id]) => org.id === id);
-  // if (foundOrgTuple) {
-  //   const [, newOrg] = foundOrgTuple;
-  //   Object.assign(org, newOrg);
-  // } else {
-  //   const companyName = faker.company.companyName();
-  //   const denomination = {
-  //     full: companyName,
-  //     public: companyName,
-  //   };
-  //   org.denomination = denomination;
-  //   org.email = `dev+${denomination.full.replace(/\W/g, '').toLowerCase()}-${Math.random()
-  //     .toString(36)
-  //     .replace(/[^a-z]+/g, '')
-  //     .substr(0, 3)}@cascade8.com`;
-  //   orgTable.push([org.id, org]);
-  // }
+  throw Error(`Unable to process org: ${JSON.stringify(org, null, 4)}`);
 }
 
-function updateInvitation(invitation: Invitation): Invitation {
-  return {
-    ...invitation,
-    fromOrg: updateOrg(invitation.fromOrg),
-    toOrg: updateOrg(invitation.toOrg),
-    fromUser: updateUser(invitation.fromUser),
-    toUser: updateUser(invitation.toUser),
-  };
-}
+function anonymizeDocument({ docPath, content: doc }: JsonlDbRecord) {
+  // function pathHasAny(path: string) { return docPath.includes(path) }
+  function pathHasAny(...paths: string[]) {
+    return !paths.every((path) => !docPath.includes(path));
+  }
+  const ignorePaths = [
+    '_META/',
+    'blockframesAdmin/',
+    'contracts/',
+    'docsIndex/',
+    'events/',
+    'movies/',
+    'permissions/',
+    'publicContracts/',
+  ];
+  if (!doc || pathHasAny(...ignorePaths)) return { docPath, content: doc };
 
-function updateNotification(notification: NotificationDocument): NotificationDocument {
-  return {
-    ...notification,
-    organization: updateOrg(notification.organization),
-    user: updateUser(notification.user as PublicUser),
-  };
+  try {
+    if (pathHasAny('users/') && hasKeys<User>(doc, 'uid') && doc?.email) { // USERS
+      return { docPath, content: updateUser(doc) };
+    } else if (pathHasAny('orgs/') && hasKeys<Organization>(doc, 'id', 'denomination')) { // ORGS
+      return { docPath, content: updateOrg(doc) };
+    } else if (pathHasAny('invitations/') && hasKeys<Invitation>(doc, 'type', 'status', 'mode')) { // INVITATIONS
+      return { docPath, content: processInvitation(doc) };
+    } else if (pathHasAny('notifications/') && hasKeys<NotificationDocument>(doc, 'isRead')) { // NOTIFICATIONS
+      return { docPath, content: processNotification(doc) };
+    }
+  } catch (e) {
+    throw [Error(`Error docPath: ${docPath}`), e];
+  }
+  throw Error(
+    `CRITICAL: could not clean this document! Path: ${docPath} \ncontent:\n${JSON.stringify(doc, null, 4)}`
+  );
 }
 
 function getPathOrder(path: string): number {
@@ -134,55 +127,6 @@ function getPathOrder(path: string): number {
   if (path.includes('notifications/')) return 4;
   return 5;
 }
-
-// type anonymizeDocument = (doc: JsonlDbRecord) => JsonlDbRecord;
-function anonymizeDocument({ docPath, content: doc }: JsonlDbRecord) {
-  if (
-    !doc ||
-    docPath.includes('_META/') ||
-    docPath.includes('blockframesAdmin/') ||
-    docPath.includes('contracts/') ||
-    docPath.includes('docsIndex/') ||
-    docPath.includes('events/') ||
-    docPath.includes('movies/') ||
-    docPath.includes('permissions/') ||
-    docPath.includes('publicContracts/')
-  )
-    return { docPath, content: doc };
-
-  try {
-    if (docPath.includes('users/') && hasKeys<User>(doc, 'uid') && doc?.email) {
-      // USERS
-      return { docPath, content: updateUser(doc) };
-    } else if (docPath.includes('orgs/') && hasKeys<Organization>(doc, 'id', 'denomination')) {
-      // ORGS
-      return { docPath, content: updateOrg(doc) };
-    } else if (
-      docPath.includes('invitations/') &&
-      hasKeys<Invitation>(doc, 'type', 'status', 'mode')
-    ) {
-      // INVITATIONS
-      return { docPath, content: updateInvitation(doc) };
-    } else if (docPath.includes('notifications/') && hasKeys<NotificationDocument>(doc, 'isRead')) {
-      // NOTIFICATIONS
-      // assertType<NotificationDocument>(content, 'organization')
-      return { docPath, content: updateNotification(doc) };
-    }
-  } catch (e) {
-    throw [Error(`Error docPath: ${docPath}`), e];
-  }
-  throw Error(
-    `CRITICAL: could not clean this document! Path: ${docPath} \ncontent:\n${JSON.stringify(doc)}`
-  );
-}
-
-// function updateDocument(doc: User | Organization) {
-//   if (!doc) return;
-//   if (hasKeys<User>(doc, 'uid') && doc?.email) updateUser(doc);
-//   else if (hasKeys<Organization>(doc, 'id', 'denomination')) updateOrg(doc);
-//   else if (doc?.email)
-//     throw Error(`CRITICAL: could not clean production email data!!\n${JSON.stringify(doc)}`);
-// }
 
 // First argument
 const src = resolve(process.cwd(), process.argv[2] || 'tmp/backup-prod.jsonl');
