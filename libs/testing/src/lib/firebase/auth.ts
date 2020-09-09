936e842f981449318f01f34bde71c7bfc3761eea
George Grigorian <george.g93@gmail.com>
2020-09-10 02:58:49 +1000

    chore

--- libs/testing/src/lib/firebase/auth.ts
+++ libs/testing/src/lib/firebase/auth.ts
@@ -2,39 +2,16 @@
 import type * as admin from 'firebase-admin';
 
 export async function deleteAllUsers(auth: admin.auth.Auth) {
-  const allUsers: admin.auth.UserRecord[] = [];
-  let result: any = { pageToken: undefined };
+  let result: Partial<admin.auth.ListUsersResult> = { pageToken: undefined };
 
-  console.log('Loading users now...');
-  let timeMsg = 'Loading users took';
+  console.log('Deleting users now...');
+  const timeMsg = 'Deleting users took';
   console.time(timeMsg);
   do {
     result = await auth.listUsers(1000, result.pageToken);
-    allUsers.push(...result.users);
+    await auth.deleteUsers(result.users.map(record => record.uid))
   } while (result.pageToken);
   console.timeEnd(timeMsg);
-
-  console.log('Starting user deletion...');
-  timeMsg = `Deleting ${allUsers.length} users took`;
-  console.time(timeMsg);
-
-  const allUsersUids = allUsers.map((record) => record.uid);
-  async function* iterateDeleteUsers(uids: string[]) {
-    while (uids.length > 0) {
-      const batch = uids.splice(0, 1000);
-      yield auth.deleteUsers(batch);
-    }
-  }
-  const delUsersIterator = iterateDeleteUsers(allUsersUids);
-  const output = { errors: [], successCount: 0, failureCount: 0 };
-  for await (const deletion of delUsersIterator) {
-    const { errors, failureCount, successCount } = deletion;
-    output.errors.push(errors);
-    output.successCount += successCount;
-    output.failureCount += failureCount;
-  }
-  console.timeEnd(timeMsg);
-  return output;
 }
 
 export async function importAllUsers(auth: admin.auth.Auth, users: admin.auth.UserImportRecord[]) {
