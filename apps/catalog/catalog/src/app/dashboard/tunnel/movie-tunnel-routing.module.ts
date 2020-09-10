b4e5a30b16b2f74a48f0c8872d8c452822f5ed4f
fritzschoff <max.j.f@gmx.de>
2020-09-10 14:49:50 +0200

    text

--- apps/catalog/catalog/src/app/dashboard/tunnel/movie-tunnel-routing.module.ts
+++ apps/catalog/catalog/src/app/dashboard/tunnel/movie-tunnel-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MovieTunnelComponent } from './movie-tunnel.component';
 import { TunnelGuard } from '@blockframes/ui/tunnel';
-import  staticConsts  from '@blockframes/utils/static-model/staticConsts'
+import staticConsts from '@blockframes/utils/static-model/staticConsts'
 
 const tunnelRoutes: Routes = [
   {
@@ -17,7 +17,8 @@ const tunnelRoutes: Routes = [
       },
       {
         path: 'title-status',
-        loadChildren: () => import('@blockframes/movie/form/title-status/title-status.module').then(m => m.TitleStatusModule)
+        loadChildren: () => import('@blockframes/movie/form/title-status/title-status.module').then(m => m.TitleStatusModule),
+        data: { disabled: Object.keys(staticConsts.productionStatus).filter(status => status !== 'released') }
       },
       {
         path: 'main',
