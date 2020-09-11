218c2c6dc36070db6b7473c40c590c7a604e624d
Mussuranae <mandonnetdev@outlook.com>
2020-09-11 15:01:32 +0200

    WIP improve shell html

--- libs/movie/src/lib/movie/dashboard/components/view/shell/shell.module.ts
+++ libs/movie/src/lib/movie/dashboard/components/view/shell/shell.module.ts
@@ -3,8 +3,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { DashboardTitleShellComponent } from './shell.component';
+
 
 @NgModule({
   declarations: [DashboardTitleShellComponent],
@@ -13,6 +15,7 @@ import { DashboardTitleShellComponent } from './shell.component';
     CommonModule,
     RouterModule,
     FlexLayoutModule,
+    MatTabsModule,
   ],
 })
 export class DashboardTitleShellModule { }
