d680646eb1eac1d51c174bf4b05819c74c7d576e
fritzschoff <max.j.f@gmx.de>
2020-09-16 08:40:59 +0200

    added comma

--- apps/catalog/catalog/src/app/dashboard/dashboard.module.ts
+++ apps/catalog/catalog/src/app/dashboard/dashboard.module.ts
@@ -158,7 +158,7 @@ const routes: Routes = [
     ImageReferenceModule,
     OrgNameModule,
     ToLabelModule,
-    MovieFormShellModule
+    MovieFormShellModule,
 
     // Material
     MatDividerModule,
