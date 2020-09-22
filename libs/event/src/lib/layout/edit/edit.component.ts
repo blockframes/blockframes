45ecd926b787ee1cf1c85927a2300bfeab365405
Remco <RemcoSimonides@gmail.com>
2020-09-22 15:44:48 +0200

    Move end date with changing start date

--- libs/event/src/lib/layout/edit/edit.component.ts
+++ libs/event/src/lib/layout/edit/edit.component.ts
@@ -1,11 +1,11 @@
-import { Component, ChangeDetectionStrategy, Input, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { EventForm } from '../../form/event.form';
 import { EventService } from '../../+state/event.service';
 import { MEETING_MAX_INVITATIONS_NUMBER } from '../../+state/event.firestore';
 import { Invitation }  from '@blockframes/invitation/+state/invitation.model';
 import { createAlgoliaUserForm } from '@blockframes/utils/algolia';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'event-edit',
@@ -13,7 +13,7 @@ import { Observable, BehaviorSubject } from 'rxjs';
   styleUrls: ['./edit.component.scss'],
   changeDetection: ChangeDetectionStrategy.Default  // required for changes on "pristine" for the save button
 })
-export class EventEditComponent implements OnInit {
+export class EventEditComponent implements OnInit, OnDestroy {
 
   @Input() form = new EventForm();
   @Input() invitations: Invitation[] = [];
@@ -22,6 +22,9 @@ export class EventEditComponent implements OnInit {
   sending = new BehaviorSubject(false);
   eventLink: string;
   limit = Infinity;
+  sub: Subscription;
+
+  private _previousStartValue: Date;
 
   constructor(
     private service: EventService,
@@ -34,6 +37,21 @@ export class EventEditComponent implements OnInit {
       this.limit = MEETING_MAX_INVITATIONS_NUMBER;
     }
     this.eventLink = `/c/o/marketplace/event/${this.form.value.id}/session`;
+
+    this._previousStartValue = new Date(this.form.get('start').value);
+    this.sub = this.form.get('start').valueChanges.subscribe(start => {
+      const end = this.form.get('end').value as Date;
+      if (start >= end) {
+        var diff = Math.abs(this._previousStartValue.getTime() - start.getTime());
+        const newEndDate = new Date(end.setTime(end.getTime() + diff));
+        this.form.get('end').setValue(newEndDate);
+        this._previousStartValue = new Date(this.form.get('start').value);
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
   get meta() {
