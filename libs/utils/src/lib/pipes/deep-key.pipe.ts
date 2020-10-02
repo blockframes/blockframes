7c3ca1ee3f487a6198a7043844f6d429f7225ef1
Remco <RemcoSimonides@gmail.com>
2020-10-02 14:39:33 +0200

    Removed external package to find deepKey

--- libs/utils/src/lib/pipes/deep-key.pipe.ts
+++ libs/utils/src/lib/pipes/deep-key.pipe.ts
@@ -1,23 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { NgModule } from '@angular/core';
-import * as objPath from 'object-path';
 
 @Pipe({ name: 'deepKey' })
 export class DeepKeyPipe implements PipeTransform {
   transform(value: Object, deepKey: string) {
     if (!value) return;
 
-    const getDeepValue = (val: Object) => {
-      const deepKeys = deepKey.split('||').map(p => p.trim());
-      const usedKey = deepKeys.find(key => objPath.has(val, key));
-      return objPath.get(val, usedKey);
-    };
+    const conditions = deepKey.split('||').map(p => p.trim());
+    const getDeepValue = (val: Object, condition: string) => condition.split('.').reduce((result, key) => result?.[key], val);
 
     if (Array.isArray(value)) {
-      return value.map(obj => getDeepValue(obj));
+      return value.map(obj => conditions.find(c => !!getDeepValue(obj, c)));
     } else {
-      return getDeepValue(value);
-    } 
+      return conditions.find(c => !!getDeepValue(value, c));
+    }
   }
 }
 
