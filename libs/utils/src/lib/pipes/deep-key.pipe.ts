67b42a77fb6c14dda42783538d6648a3d25e4b8e
Remco <RemcoSimonides@gmail.com>
2020-10-02 12:16:11 +0200

    Pass in multiple deepKeys

--- libs/utils/src/lib/pipes/deep-key.pipe.ts
+++ libs/utils/src/lib/pipes/deep-key.pipe.ts
@@ -1,11 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { NgModule } from '@angular/core';
+import * as objPath from 'object-path';
 
 @Pipe({ name: 'deepKey' })
 export class DeepKeyPipe implements PipeTransform {
   transform(value: Object, deepKey: string) {
     if (!value) return;
-    const getDeepValue = (val: Object) => deepKey.split('.').reduce((result, key) => result[key], val);
+
+    const getDeepValue = (val: Object) => {
+      const deepKeys = deepKey.split('||').map(p => p.trim());
+      const usedKey = deepKeys.find(key => objPath.has(val, key));
+      return objPath.get(val, usedKey);
+    };
 
     if (Array.isArray(value)) {
       return value.map(obj => getDeepValue(obj));
