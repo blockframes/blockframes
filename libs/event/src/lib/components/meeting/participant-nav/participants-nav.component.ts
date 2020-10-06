4f0123e40c58fecd528ec0205aab02a03103539b
OmarSht <sebouai_o@hotmail.fr>
2020-10-06 17:20:02 +0200

    removed useless fontsize - observable

--- libs/event/src/lib/components/meeting/participant-nav/participants-nav.component.ts
+++ libs/event/src/lib/components/meeting/participant-nav/participants-nav.component.ts
@@ -1,15 +1,13 @@
 import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {IParticipantMeeting} from "@blockframes/event/components/meeting/+state/meeting.interface";
-import {Observable} from "rxjs";
 
 
 @Component({
   selector: 'event-meeting-participants-nav',
   templateUrl: './participants-nav.component.html',
-  styleUrls: ['./participants-nav.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./participants-nav.component.scss']
 })
 export class ParticipantsNavComponent {
 
-  @Input() participants$: Observable<IParticipantMeeting[]>;
+  @Input() participants: IParticipantMeeting[];
 }
