0fd57f8f698ea9e2d67ceb212032a3f44768e90f
Mussuranae <mandonnetdev@outlook.com>
2020-10-06 19:30:51 +0200

    tab additional done

---
+++ libs/movie/src/lib/movie/dashboard/additional/additional.module.ts
@@ -0,0 +1,26 @@
+// Angular
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
+
+// Components
+import { MovieViewAdditionalComponent } from './additional.component';
+import { MovieTunnelSummaryModule } from '@blockframes/movie/form/summary/summary.module';
+import { GetPathModule } from '@blockframes/utils/pipes/get-path.pipe';
+import { MatDividerModule } from '@angular/material/divider';
+
+
+@NgModule({
+  declarations: [MovieViewAdditionalComponent],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    MovieTunnelSummaryModule,
+    GetPathModule,
+    MatDividerModule,
+    RouterModule.forChild([{ path: '', component: MovieViewAdditionalComponent }])
+  ],
+  exports: [MovieViewAdditionalComponent]
+})
+export class MovieViewAdditionalModule { }
