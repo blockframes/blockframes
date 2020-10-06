bbb49c0c12c1202dc1d42088d83221a4ea31e0f3
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2020-10-06 09:49:48 +0200

    fix dashboard list title on financiers

--- libs/utils/src/lib/pipes/deep-key.pipe.ts
+++ libs/utils/src/lib/pipes/deep-key.pipe.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 @Pipe({ name: 'deepKey' })
 export class DeepKeyPipe implements PipeTransform {
   transform(value: Object, deepKey: string) {
-    if (!value) return;
+    if (!value || !deepKey) return;
 
     const conditions = deepKey.split('||').map(p => p.trim());
     const getDeepValue = (val: Object, condition: string) => condition.split('.').reduce((result, key) => result?.[key], val);
@@ -12,11 +12,11 @@ export class DeepKeyPipe implements PipeTransform {
     if (Array.isArray(value)) {
       return value.map(obj => {
         const condition = conditions.find(c => !!getDeepValue(obj, c));
-        return condition ? !!getDeepValue(obj, condition) : undefined;
+        return condition ? getDeepValue(obj, condition) : undefined;
       });
     } else {
       const condition = conditions.find(c => !!getDeepValue(value, c));
-      return condition ? !!getDeepValue(value, condition) : undefined;
+      return condition ? getDeepValue(value, condition) : undefined;
     }
   }
 }
