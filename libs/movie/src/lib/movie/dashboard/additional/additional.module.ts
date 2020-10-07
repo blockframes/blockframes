40ff4e1cd12189be39b4061a86f7441256d77f32
Mussuranae <mandonnetdev@outlook.com>
2020-10-07 09:41:26 +0200

    css

--- libs/movie/src/lib/movie/dashboard/additional/additional.module.ts
+++ libs/movie/src/lib/movie/dashboard/additional/additional.module.ts
@@ -9,6 +9,7 @@ import { MovieViewAdditionalComponent } from './additional.component';
 import { MovieTunnelSummaryModule } from '@blockframes/movie/form/summary/summary.module';
 import { GetPathModule } from '@blockframes/utils/pipes/get-path.pipe';
 import { MatDividerModule } from '@angular/material/divider';
+import { HasStatusModule } from '@blockframes/movie/pipes/has-status.pipe';
 
 
 @NgModule({
@@ -19,6 +20,7 @@ import { MatDividerModule } from '@angular/material/divider';
     MovieTunnelSummaryModule,
     GetPathModule,
     MatDividerModule,
+    HasStatusModule,
     RouterModule.forChild([{ path: '', component: MovieViewAdditionalComponent }])
   ],
   exports: [MovieViewAdditionalComponent]
