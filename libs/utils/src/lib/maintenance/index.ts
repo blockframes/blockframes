d9f173776dca623ce044d6afbcc7a72cb5b9a067
bruce <delorme.bruce.michel@gmail.com>
2020-10-08 16:23:24 +0200

    maintenance mode fix

--- libs/utils/src/lib/maintenance/index.ts
+++ libs/utils/src/lib/maintenance/index.ts
@@ -1,25 +1,36 @@
 import { firestore } from 'firebase/app';
 
 export interface IMaintenanceDoc {
-  endedAt:firestore.Timestamp
+  endedAt: firestore.Timestamp
   startedAt: firestore.Timestamp
 }
 
 /** Delay before considering the maintenance over */
-export const delay = 8 * 60 * 1000;  // 8 minutes in ms
-export const maintenancePath = '_META/_MAINTENANCE';
+export const EIGHT_MINUTES_IN_MS = 8 * 60 * 1000; // 8 minutes in ms
+export const META_COLLECTION_NAME = '_META';
+export const MAINTENANCE_DOCUMENT_NAME = '_MAINTENANCE';
 
-export function isInMaintenance({ endedAt, startedAt }: IMaintenanceDoc): boolean {
-  const now = firestore.Timestamp.now();
-  if (endedAt) {
-    // Wait 30s before allowing any operation on the db.
-    // this prevents triggering firebase events.
-    // NOTE: this is hack-ish but good enough for our needs! we'll revisit this later.
-    return endedAt.toMillis() + delay > now.toMillis();
-  }
-  if (startedAt) {
+export function _isInMaintenance({ endedAt, startedAt }: IMaintenanceDoc, delay = EIGHT_MINUTES_IN_MS): boolean {
+  try {
+    const now = firestore.Timestamp.now();
+
+    if (startedAt) {
+      return true;
+    }
+
+    if (endedAt) {
+      // Wait `delay` minutes before allowing any operation on the db.
+      // this prevents triggering firebase events.
+      // NOTE: this is hack-ish but good enough for our needs! we'll revisit this later.
+      return endedAt.toMillis() + delay > now.toMillis();
+    }
+
+    // We shouldn't throw here else if this happen it create cache issues
+    console.error('Unexpected cases for maintenance check! please check the _META/_MAINTENANCE document.');
+
+    return true;
+  } catch (e) {
+    console.error(`Error while checking if app is in maintenance : ${e.message}`);
     return true;
   }
-  // We shouldn't throw here else if this happen it create cache issues
-  console.error('Unexpected cases for maintenance check! please check the _META/_MAINTENANCE document.');
 }
