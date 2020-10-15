6f5a2d99278189bd77da1629b0b46075fa0b889e
Remco <RemcoSimonides@gmail.com>
2020-10-15 16:11:35 +0200

    Fix ui of button

--- apps/festival/festival/src/app/dashboard/files/view/view.component.ts
+++ apps/festival/festival/src/app/dashboard/files/view/view.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 const navLinks = [
   {
@@ -13,11 +13,8 @@ const navLinks = [
   styleUrls: ['./view.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FilesViewComponent implements OnInit {
+export class FilesViewComponent {
 
   public navLinks = navLinks;
 
-  constructor() { }
-
-  ngOnInit() { }
 }
