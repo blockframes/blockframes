1d1a743f4c9034a54ff6088268dc5c3a184cfbdb
bruce <delorme.bruce.michel@gmail.com>
2020-10-28 16:32:44 +0100

    update after review

--- libs/utils/src/lib/emails/sendgrid.service.ts
+++ libs/utils/src/lib/emails/sendgrid.service.ts
@@ -3,25 +3,26 @@ import { AngularFireFunctions } from '@angular/fire/functions';
 import { ErrorResultResponse } from '@blockframes/utils/utils';
 import { EmailParameters } from '@blockframes/utils/emails/utils';
 import { callFunction } from 'akita-ng-fire';
-import { App } from '../apps';
 
 @Injectable({ providedIn: 'root' })
 export class SendgridService {
 
   /**
    * Allowed only to Blockframes Admins
-   * @param request 
-   * @param from 
+   * @param data EmailParameters
    */
-  sendAsAdmin: (data: EmailParameters) => Promise<ErrorResultResponse> = callFunction.bind(this, this.functions, 'sendMailAsAdmin');
+  sendAsAdmin: (data: EmailParameters) => Promise<ErrorResultResponse> = this.callable('sendMailAsAdmin');
 
   /**
    * Allowed to every user, but they must use a template Id and cannot choose "from".
    * Template ids can be allowed to use in apps/backend-functions/src/internals/email.ts isAllowedToUseTemplate
-   * @param request 
-   * @param app 
+   * @param data EmailParameters
    */
-  sendWithTemplate: (data: EmailParameters) => Promise<ErrorResultResponse> = callFunction.bind(this, this.functions, 'sendMailWithTemplate');
+  sendWithTemplate: (data: EmailParameters) => Promise<ErrorResultResponse> = this.callable('sendMailWithTemplate');
 
   constructor(private functions: AngularFireFunctions) { }
+
+  callable(method: string) {
+    return callFunction.bind(this, this.functions, method);
+  }
 }
