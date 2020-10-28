16a19d1c614888b3d483e134441a280e2cbbb176
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2020-10-28 17:36:38 +0100

    filter movie campaign

--- apps/financiers/financiers/src/app/dashboard/title/list/list.component.ts
+++ apps/financiers/financiers/src/app/dashboard/title/list/list.component.ts
@@ -1,12 +1,14 @@
 import { Component, ChangeDetectionStrategy, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormControl } from '@angular/forms';
-import { startWith, map, switchMap, tap } from 'rxjs/operators';
-import { Observable, Subscription } from 'rxjs';
-import { Movie } from '@blockframes/movie/+state/movie.model';
-import { MovieService } from '@blockframes/movie/+state/movie.service';
+import { MovieService, Movie } from '@blockframes/movie/+state';
+import { CampaignService, MovieCampaign } from '@blockframes/campaign/+state/campaign.service';
 import { OrganizationQuery } from '@blockframes/organization/+state';
 import { DynamicTitleService } from '@blockframes/utils/dynamic-title/dynamic-title.service';
+import { Observable, Subscription } from 'rxjs';
+import { map, startWith, switchMap, tap } from 'rxjs/operators';
+
+type Filters = 'all' | 'draft' | 'ongoing' | 'achieved';
 
 const columns = {
   title: 'Title',
@@ -16,6 +18,14 @@ const columns = {
   'storeConfig.status': 'Status'
 };
 
+function filterMovieCampaign(movies: MovieCampaign[], filter: Filters) {
+  switch (filter) {
+    case 'all': return movies;
+    case 'draft': return movies.filter(movie => movie.storeConfig.status === 'draft');
+    case 'ongoing': return movies.filter(movie => movie.storeConfig.status === 'accepted' && movie.campaign?.cap > movie.campaign?.received);
+    case 'achieved': return movies.filter(movie => movie.storeConfig.status === 'accepted' && movie.campaign?.cap === movie.campaign?.received);
+  }
+}
 
 @Component({
   selector: 'financiers-dashboard-title-list',
@@ -26,13 +36,15 @@ const columns = {
 export class ListComponent implements OnInit, OnDestroy {
   columns = columns;
   initialColumns = ['title', 'view', 'directors', 'productionStatus', 'storeConfig.status'];
-  titles$: Observable<Movie[]>;
+  titles$: Observable<MovieCampaign[]>;
   filter = new FormControl('all');
+  filter$ = this.filter.valueChanges.pipe(startWith(this.filter.value));
 
   private sub: Subscription;
 
   constructor(
-    private service: MovieService,
+    private movieService: MovieService,
+    private campaignService: CampaignService,
     private orgQuery: OrganizationQuery,
     private router: Router,
     private route: ActivatedRoute,
@@ -42,13 +54,18 @@ export class ListComponent implements OnInit, OnDestroy {
   ngOnInit() {
     // Sync with anaytics: It's ok to give ALL movieIds they'll just be set to 0
     this.sub = this.orgQuery.selectActive().pipe(
-      switchMap(org => this.service.syncWithAnalytics(org.movieIds)),
+      switchMap(org => this.movieService.syncWithAnalytics(org.movieIds)),
     ).subscribe();
 
+    this.filter$.subscribe(console.log);
+
     this.titles$ = this.orgQuery.selectActive().pipe(
-      switchMap(org => this.service.valueChanges(org.movieIds)),
+      switchMap(org => this.campaignService.queryMoviesCampaign(org.movieIds)),
       map(movies => movies.filter(movie => !!movie)),
       map(movies => movies.filter(movie => movie.storeConfig.appAccess.financiers)),
+      switchMap(movies => {
+        return this.filter$.pipe(map(filter => filterMovieCampaign(movies, filter)))
+      }),
       tap(movies => {
         !!movies.length ?
           this.dynTitle.setPageTitle('My titles') :
@@ -62,8 +79,8 @@ export class ListComponent implements OnInit, OnDestroy {
     this.router.navigate([title.id], { relativeTo: this.route });
   }
 
-  public applyFilter(status: 'all' | 'draft' | 'ongoing' | 'achieved') {
-    this.filter.setValue(status);
+  public applyFilter(filter: Filters) {
+    this.filter.setValue(filter);
   }
 
   ngOnDestroy() {
