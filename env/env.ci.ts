185af4994d1b0ec9805d7ed879b64b28a46d4442
fritzschoff <max.j.f@gmx.de>
2020-10-29 16:29:29 +0100

    updating envs

--- env/env.ci.ts
+++ env/env.ci.ts
@@ -53,7 +53,11 @@ export const algolia = {
   appId: '8E9YO1I9HB',
   searchKey: '4a2990a293c0ee0bfde9ebd66270a47f',
   indexNameOrganizations: 'ci_orgs',
-  indexNameMovies: 'ci_movies',
+  indexNameMovies: {
+    festival: 'ci_festival_movies',
+    financiers: 'ci_financiers_movies',
+    catalog: 'ci_catalog_movies',
+  },
   indexNameUsers: 'ci_users',
 };
 
