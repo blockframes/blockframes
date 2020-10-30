cd1a8214b73cde9338c06aa5edea6a648976a2d3
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2020-10-30 12:18:53 +0100

    remove amount in perks

--- libs/organization/campaign/src/lib/+state/campaign.model.ts
+++ libs/organization/campaign/src/lib/+state/campaign.model.ts
@@ -41,10 +41,10 @@ export interface Perk {
   title: string,
   description: string,
   minPledge: number,
-  amount: {
-    current: number,
-    total: number
-  }
+  // amount: {
+  //   current: number,
+  //   total: number
+  // }
 }
 
 export function createCampaign(params: Partial<Campaign> = {}): Campaign {
@@ -64,10 +64,10 @@ export function createPerk(params: Partial<Perk> = {}): Perk {
     title: '',
     description: '',
     minPledge: 0,
-    amount: {
-      total: 0,
-      current: 0,
-    },
+    // amount: {
+    //   total: 0,
+    //   current: 0,
+    // },
     ...params
   }
 }
