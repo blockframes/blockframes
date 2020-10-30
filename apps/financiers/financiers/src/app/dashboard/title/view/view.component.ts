f36129fc71f08048aa71e0f07baef4726a1faad1
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2020-10-30 12:02:06 +0100

    add form for current amount

--- apps/financiers/financiers/src/app/dashboard/title/view/view.component.ts
+++ apps/financiers/financiers/src/app/dashboard/title/view/view.component.ts
@@ -1,9 +1,37 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ViewChild, TemplateRef } from '@angular/core';
 import { Observable } from 'rxjs';
 import { getLabelBySlug } from '@blockframes/utils/static-model/staticModels';
 import { Movie } from '@blockframes/movie/+state/movie.model';
 import { MovieQuery } from '@blockframes/movie/+state/movie.query';
 import { RouteDescription } from '@blockframes/utils/common-interfaces/navigation';
+import { Campaign, CampaignService } from '@blockframes/campaign/+state';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { CampaignForm } from '@blockframes/campaign/form/form';
+import { CrossFieldErrorMatcher } from '@blockframes/utils/form/matchers';
+
+
+const links: RouteDescription[] = [
+  {
+    path: 'main',
+    label: 'Main Information'
+  },
+  {
+    path: 'artistic',
+    label: 'Artistic Information'
+  },
+  {
+    path: 'production',
+    label: 'Production Information'
+  },
+  {
+    path: 'financing',
+    label: 'Financial Elements'
+  },
+  {
+    path: 'campaign',
+    label: 'Investment',
+  }
+];
 
 @Component({
   selector: 'financiers-dashboard-title-view',
@@ -12,45 +40,36 @@ import { RouteDescription } from '@blockframes/utils/common-interfaces/navigatio
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TitleViewComponent implements OnInit {
+  @ViewChild('dialogTemplate') dialogTemplate: TemplateRef<any>;
+  private dialogRef: MatDialogRef<any, any>;
   public movie$: Observable<Movie>;
   public loading$: Observable<boolean>;
+  public navLinks = links;
   public getLabelBySlug = getLabelBySlug;
 
-  navLinks: RouteDescription[] = [
-    {
-      path: 'main',
-      label: 'Main Information'
-    },
-    {
-      path: 'artistic',
-      label: 'Artistic Information'
-    },
-    {
-      path: 'production',
-      label: 'Production Information'
-    },
-    {
-      path: 'financing',
-      label: 'Financial Elements'
-    },
-    {
-      path: 'campaign',
-      label: 'Investment',
-    }
-  ];
 
-  constructor(private movieQuery: MovieQuery) {}
+  constructor(
+    private movieQuery: MovieQuery,
+    private campaignService: CampaignService,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit() {
-    this.getMovie();
-  }
-
-  private getMovie() {
     this.loading$ = this.movieQuery.selectLoading();
     this.movie$ = this.movieQuery.selectActive();
   }
 
-  public getDirectors(movie: Movie) {
-    return movie.directors.map(d => `${d.firstName}  ${d.lastName}`).join(', ');
+  async openDialog(movieId: string) {
+    const campaign = await this.campaignService.getValue(movieId);
+    if (campaign) {
+      const form = new CampaignForm(campaign);
+      const errorMatcher = new CrossFieldErrorMatcher();
+      this.dialogRef = this.dialog.open(this.dialogTemplate, { minWidth: '50vw', data: {form, errorMatcher} });
+    }
+  }
+
+  async save(movieId: string, campaign: Campaign) {
+    this.dialogRef.close();
+    this.campaignService.update(movieId, campaign);
   }
 }
