080e2af5418ff3c37a8325d265211eea75c393dd
bruce <delorme.bruce.michel@gmail.com>
2020-11-03 17:43:59 +0100

    update after review

--- libs/notification/src/lib/notification.component.ts
+++ libs/notification/src/lib/notification.component.ts
@@ -1,10 +1,7 @@
-﻿import { Component, ChangeDetectionStrategy, OnInit, ChangeDetectorRef, OnDestroy } from '@angular/core';
-import { map } from 'rxjs/operators';
+﻿import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { NotificationQuery } from './+state/notification.query';
 import { NotificationService } from './+state';
 import { DynamicTitleService } from '@blockframes/utils/dynamic-title/dynamic-title.service';
-import { Subscription } from 'rxjs';
-
 
 @Component({
   selector: 'notification-view',
@@ -12,28 +9,18 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./notification.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NotificationComponent implements OnInit, OnDestroy {
+export class NotificationComponent implements OnInit {
 
-  public hasNotifications$ = this.query.selectCount().pipe(map(count => !!count))
   public notifications$ = this.query.selectAll();
-  public isDataLoaded = false;
-  private sub: Subscription;
 
   constructor(
     private query: NotificationQuery,
     private service: NotificationService,
     private dynTitle: DynamicTitleService,
-    private cdr: ChangeDetectorRef,
   ) { }
 
   ngOnInit() {
     this.dynTitle.setPageTitle('Activity Feed');
-
-    this.sub = this.notifications$.subscribe(_ => {
-      this.isDataLoaded = true;
-      this.cdr.markForCheck();
-    });
-
   }
 
   markAll() {
@@ -42,7 +29,4 @@ export class NotificationComponent implements OnInit, OnDestroy {
     }
   }
 
-  ngOnDestroy() {
-    this.sub.unsubscribe();
-  }
 }
