79aa18e230da808360f1f8ac880333c15bf49c20
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2020-11-04 16:20:28 +0100

    reorganize homepage

--- libs/ui/src/lib/marketplace/pages/wishlist/wishlist.component.ts
+++ libs/ui/src/lib/marketplace/pages/wishlist/wishlist.component.ts
@@ -65,7 +65,7 @@ export class WishlistComponent implements OnInit, OnDestroy {
   }
 
   public async redirectToMovie(movieId: string) {
-    this.router.navigate([`../title/${movieId}`], { relativeTo: this.route });
+    this.router.navigate(['../title', movieId], { relativeTo: this.route });
   }
 
   public remove(movie: Movie, event: Event) {
