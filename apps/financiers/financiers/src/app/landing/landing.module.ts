4cedbcffe9a8b7aebe9deb726fdce27c07b715bc
fritzschoff <max.j.f@gmx.de>
2020-11-10 16:31:35 +0100

    fixing CI

--- apps/financiers/financiers/src/app/landing/landing.module.ts
+++ apps/financiers/financiers/src/app/landing/landing.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LandingComponent } from './landing.component';
 import { LandingShellModule } from '@blockframes/landing/shell/shell.module';
 import { ImageReferenceModule } from '@blockframes/media/directives/image-reference/image-reference.module';
+import { AppLogoModule } from '@blockframes/ui/layout/app-logo/app-logo.module';
 
 // Material
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
 
 
 
@@ -20,11 +23,15 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     FlexLayoutModule,
     LandingShellModule,
+    ReactiveFormsModule,
     ImageReferenceModule,
+    AppLogoModule,
+
     // Material
-    MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    MatCardModule,
+    MatTabsModule,
     RouterModule.forChild([{ path: '', component: LandingComponent }])
   ]
 })
