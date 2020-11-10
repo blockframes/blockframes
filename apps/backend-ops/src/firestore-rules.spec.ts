cb159318b86b034865ef598713be75b8b12d41cf
xmano <mano@xkanda.com>
2020-11-10 14:35:56 +0530

    Update(UT): Add more tests and TODOs.

--- apps/backend-ops/src/firestore-rules.spec.ts
+++ apps/backend-ops/src/firestore-rules.spec.ts
@@ -5,6 +5,8 @@ import fs from 'fs';
 
 type Firestore = ReturnType<typeof initFirestoreApp>;
 
+//TODO : Refactor initFirestoreApp to use better method to update firestore data
+//Issue : 4192
 const initFirestoreApp = (projectId: string, auth?: any) => {
   //Define these env vars to avoid getting console warnings
   process.env.GCLOUD_PROJECT = projectId;
@@ -73,6 +75,186 @@ describe('General User', () => {
   });
 });
 
+describe('Users Collection Rules Tests', () => {
+  const projectId = `rules-spec-${Date.now()}`;
+  const uid = 'uid-user2';
+  const user = {uid, email: 'u2@cascade8.com', note: 'JestTestSet-User2'};
+  let db: Firestore;
+
+  beforeAll(async () => {
+    db  = initFirestoreApp(projectId, {uid: 'uid-user2'});
+    await setRules(projectId, 'firestore.test.rules');
+    await setData(db, testFixture);
+    await setRules(projectId, 'firestore.rules');
+  });
+
+  afterAll(() => Promise.all(apps().map(app => app.delete())));
+
+  test("should allow user to read own user doc (own uid)", async () => {
+    const usersRef = db.doc("users/uid-user2");
+    await assertSucceeds(usersRef.get({}));
+  });
+
+  test("should allow user to delete own user doc (own uid)", async () => {
+    const usersRef = db.doc("users/uid-user2");
+    await assertSucceeds(usersRef.delete());
+  });
+
+  test("shouldn't allow user to create own user doc (own uid)", async () => {
+    const usersRef = db.doc("users/uid-user2");
+    await assertFails(usersRef.set({email: 'u2@cascade8.com', note: 'JestTestSet-User2'}));
+  });
+
+  test("should allow user to create own user doc (own uid)", async () => {
+    const usersRef = db.doc(`users/${uid}`);
+    await assertSucceeds(usersRef.set(user));
+  });
+
+  test("should contain exact fields for own user docs", async () => {
+    const usersRef = db.doc(`users/${uid}`);
+    const expUserSnap = await usersRef.get();
+    const expUser = await expUserSnap.data();
+    expect(user).toEqual(expUser);
+  });
+
+});
+
+//TODO: 4190
+describe.skip('Notification Rules Tests', () => {
+  const projectId = `rules-spec-${Date.now()}`;
+  let db: Firestore;
+
+  beforeAll(async () => {
+    db  = initFirestoreApp(projectId, {uid: 'uid-user2'});
+    await setRules(projectId, 'firestore.test.rules');
+    await setData(db, testFixture);
+    await setRules(projectId, 'firestore.rules');
+  });
+
+  afterAll(() => Promise.all(apps().map(app => app.delete())));
+
+  test("should not allow user to create notification", async () => {
+    const notifRef = db.doc("notifications/001");
+    await assertFails(notifRef.set({note: 'A notification'}));
+  });
+});
+
+//TODO: 4195
+describe.skip('Invitation Rules Tests', () => {
+  const projectId = `rules-spec-${Date.now()}`;
+  let db: Firestore;
+
+  beforeAll(async () => {
+    db  = initFirestoreApp(projectId, {uid: 'uid-user2'});
+    await setRules(projectId, 'firestore.test.rules');
+    await setData(db, testFixture);
+    await setRules(projectId, 'firestore.rules');
+  });
+
+  afterAll(() => Promise.all(apps().map(app => app.delete())));
+
+  test("should not allow user to create ", async () => {
+    const notifRef = db.doc("invitations/001");
+    await assertFails(notifRef.set({note: 'A notification'}));
+  });
+});
+
+//TODO: 4197
+describe.skip('Organization Rules Tests', () => {
+  const projectId = `rules-spec-${Date.now()}`;
+  let db: Firestore;
+
+  beforeAll(async () => {
+    db  = initFirestoreApp(projectId, {uid: 'uid-user2'});
+    await setRules(projectId, 'firestore.test.rules');
+    await setData(db, testFixture);
+    await setRules(projectId, 'firestore.rules');
+  });
+
+  afterAll(() => Promise.all(apps().map(app => app.delete())));
+
+  test("test", async () => {
+
+  });
+});
+
+//TODO: 4198
+describe.skip('Permission Rules Tests', () => {
+  const projectId = `rules-spec-${Date.now()}`;
+  let db: Firestore;
+
+  beforeAll(async () => {
+    db  = initFirestoreApp(projectId, {uid: 'uid-user2'});
+    await setRules(projectId, 'firestore.test.rules');
+    await setData(db, testFixture);
+    await setRules(projectId, 'firestore.rules');
+  });
+
+  afterAll(() => Promise.all(apps().map(app => app.delete())));
+
+  test("test", async () => {
+
+  });
+});
+
+//TODO: 4199
+describe.skip('Movies Rules Tests', () => {
+  const projectId = `rules-spec-${Date.now()}`;
+  let db: Firestore;
+
+  beforeAll(async () => {
+    db  = initFirestoreApp(projectId, {uid: 'uid-user2'});
+    await setRules(projectId, 'firestore.test.rules');
+    await setData(db, testFixture);
+    await setRules(projectId, 'firestore.rules');
+  });
+
+  afterAll(() => Promise.all(apps().map(app => app.delete())));
+
+  test("test ", async () => {
+
+  });
+});
+
+//TODO: 4200
+describe.skip('Contracts Rules Tests', () => {
+  const projectId = `rules-spec-${Date.now()}`;
+  let db: Firestore;
+
+  beforeAll(async () => {
+    db  = initFirestoreApp(projectId, {uid: 'uid-user2'});
+    await setRules(projectId, 'firestore.test.rules');
+    await setData(db, testFixture);
+    await setRules(projectId, 'firestore.rules');
+  });
+
+  afterAll(() => Promise.all(apps().map(app => app.delete())));
+
+  test("test", async () => {
+
+  });
+});
+
+//TODO: 4201
+describe.skip('Events Rules Tests', () => {
+  const projectId = `rules-spec-${Date.now()}`;
+  let db: Firestore;
+
+  beforeAll(async () => {
+    db  = initFirestoreApp(projectId, {uid: 'uid-user2'});
+    await setRules(projectId, 'firestore.test.rules');
+    await setData(db, testFixture);
+    await setRules(projectId, 'firestore.rules');
+  });
+
+  afterAll(() => Promise.all(apps().map(app => app.delete())));
+
+  test("test ", async () => {
+
+  });
+});
+
+//TODO: 4187
 describe.skip('Campaign Security Rules', () => {
 
   beforeAll(async () => {
--- apps/backend-ops/src/fixtures/data.ts
+++ apps/backend-ops/src/fixtures/data.ts
@@ -1,4 +1,5 @@
 export const testFixture = {
+  //Users Collection
   "users/uid-c8" : {
     email: 'c8@cascade8.com',
     uid: 'uid-c8' 
@@ -7,7 +8,21 @@ export const testFixture = {
     email: 'u2@cascade8.com',
     uid: 'uid-user2' 
   },
+
+  //Blockframes Admin
   "blockframesAdmin/uid-c8" : {
+  },
+
+  //Notifications
+  "notifications/001" : {
+      id : "001",
+      date : Date.now(),
+      toUserId: "uid-c8",
+      type: "invitationToAttendEventAccepted",
+      docId: "100",
+      user: {uid: "uid-user2"},
+      organization: {id: "O001"},
+      movies: [{id: "M001"}]
   }
 
 }
