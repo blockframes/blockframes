4af52c3d2c2a086647f5b0427e57b70b5434635e
Remco <RemcoSimonides@gmail.com>
2020-11-11 15:12:01 +0100

    Fix initialising index and removed app filters

--- libs/ui/src/lib/algolia/chips-autocomplete/algolia-chips-autocomplete.component.ts
+++ libs/ui/src/lib/algolia/chips-autocomplete/algolia-chips-autocomplete.component.ts
@@ -29,7 +29,7 @@ function splitValue(value: string, keycodes: number[]) {
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AlgoliaChipsAutocompleteComponent implements OnInit, OnDestroy {
-  public indexName: string;
+  /** The initialized client for algolia */
   public searchCtrl = new FormControl();
   /** Holds the results of algolia */
   public algoliaSearchResults$: Observable<any[]>;
@@ -40,9 +40,7 @@ export class AlgoliaChipsAutocompleteComponent implements OnInit, OnDestroy {
   // INPUT ----------------------------
 
   /**
-   * Should be fed with the algolia index name out of the `env.ts`
-   * @example [index]="algolia.org" // 'pl_orgs' from the env.ts
-   * @example but if index is a movie, just pass down `movie`
+   * Set index
    */
   @Input() index: AlgoliaIndex;
 
@@ -101,11 +99,12 @@ export class AlgoliaChipsAutocompleteComponent implements OnInit, OnDestroy {
     }
 
     let indexSearch: Index;
-    if(this.index === 'movie') {
+    if (this.index === 'user') {
+      indexSearch = searchClient.initIndex(algoliaIndex[this.index] as string);
+    } else {
       const app = this.routerQuery.getValue().state?.root.data.app;
       indexSearch = searchClient.initIndex(algoliaIndex[this.index][app]);
     }
-    indexSearch = searchClient.initIndex(algoliaIndex[this.index] as any);
 
     // create search functions
     const regularSearch = (text: string) => indexSearch.search({ query: text, facetFilters: this.getFilter() }).then(result => result.hits);
