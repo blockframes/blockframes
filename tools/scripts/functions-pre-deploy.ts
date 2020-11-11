069ad801141b9339950c0631bbb3ddf907d49b0f
George Grigorian <george.g93@gmail.com>
2020-11-12 03:58:07 +1100

    hotfix

--- tools/scripts/functions-pre-deploy.ts
+++ tools/scripts/functions-pre-deploy.ts
@@ -1,18 +1,18 @@
-import { execSync } from 'child_process';
+import 'tsconfig-paths/register'
+import { runShellCommand } from '@blockframes/firebase-utils';
 import { config } from 'dotenv';
 
 config()
 
-function execLog(cmd: string) {
-  const output = execSync(cmd);
-  process.stdout.write(output);
+async function predeploy() {
+  if (!process.env['SKIP_PREDEPLOY_HOOKS']) {
+    // * run predeploy hooks
+    await runShellCommand('npm run lint backend-functions');
+    await runShellCommand('npm run build:backend-functions');
+    await runShellCommand('npm run deploy:functions:config');
+  } else {
+    console.log('Env variable "SKIP_PREDEPLOY_HOOKS" is set, skipping pre-deploy hooks...');
+  }
 }
 
-if (!process.env['SKIP_PREDEPLOY_HOOKS']) {
-  // * run predeploy hooks
-  execLog('npm run lint backend-functions');
-  execLog('npm run build:backend-functions');
-  execLog('npm run deploy:functions:config');
-} else {
-  console.log('Env variable "SKIP_PREDEPLOY_HOOKS" is set, skipping pre-deploy hooks...');
-}
+predeploy().then(() => process.exit(0));
