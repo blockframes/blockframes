dae2c22d9219299179951baa4b1052bae6048800
bruce <delorme.bruce.michel@gmail.com>
2020-11-12 16:26:24 +0100

    fix #4181

--- libs/event/src/lib/components/screening-item/screening-item.component.ts
+++ libs/event/src/lib/components/screening-item/screening-item.component.ts
@@ -4,6 +4,7 @@ import { ScreeningEvent } from '../../+state';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Movie } from '@blockframes/movie/+state';
+import { AuthQuery } from '@blockframes/auth/+state';
 
 @Component({
   selector: 'event-screening-item',
@@ -16,16 +17,25 @@ export class ScreeningItemComponent {
   public movie: Movie;
   public screening: ScreeningEvent;
   public invitation$: Observable<Invitation>;
+  public uid = this.authQuery.userId;
 
   @Input() set event(screening: ScreeningEvent) {
     this.screening = screening;
     this.movie = screening.movie;
     this.poster = screening.movie?.poster;
     this.invitation$ = this.invitationQuery.selectAll().pipe(
-      map(invits => invits.find(e => e.docId === screening.id))
+      map(invits => invits.find(e => {
+        return e.docId === screening.id && (
+          (e.mode === 'request' && e.fromUser.uid === this.uid) ||
+          (e.mode === 'invitation' && e.toUser.uid === this.uid)
+        );
+      }))
     );
   }
 
-  constructor(private invitationQuery: InvitationQuery) { }
+  constructor(
+    private invitationQuery: InvitationQuery,
+    private authQuery: AuthQuery
+  ) { }
 
 }
