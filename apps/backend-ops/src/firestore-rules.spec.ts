0c33cf1234970786dabf239ab27745ca86522e77
xmano <mano@xkanda.com>
2020-11-17 14:49:23 +0530

    Update(UT): Add event create, update and delete unit tests.

--- apps/backend-ops/src/firestore-rules.spec.ts
+++ apps/backend-ops/src/firestore-rules.spec.ts
@@ -255,6 +255,47 @@ describe.only('Events Rules Tests', () => {
       await assertSucceeds(eventRef.get());
     });
 
+    test("user with valid org, invalid event id shouldn't be able to create event", async () => {
+      const eventRef = db.doc("events/E007");
+      const eventDetails = {id: 'E077'};
+      await assertFails(eventRef.set(eventDetails));
+    });
+
+    test("user with valid org, invalid ownerId shouldn't be able to create event", async () => {
+      const eventRef = db.doc("events/E007");
+      const eventDetails = {id: 'E007', ownerId: 'uid-007'};
+      await assertFails(eventRef.set(eventDetails));
+    });
+
+    test("user with valid org, ownerId as uid should be able to create event", async () => {
+      const eventRef = db.doc("events/E007");
+      const eventDetails = {id: 'E007', ownerId: 'uid-user2'};
+      await assertSucceeds(eventRef.set(eventDetails));
+    });
+
+    test("user with valid org, ownerId as orgId should be able to create event", async () => {
+      const eventRef = db.doc("events/E007");
+      const eventDetails = {id: 'E007', ownerId: 'O001'};
+      await assertSucceeds(eventRef.set(eventDetails));
+    });
+
+    test("user with valid org, ownerId as orgId, modifying id shouldn't be able to update event", async () => {
+      const eventRef = db.doc("events/E007");
+      const eventDetails = {id: 'E008'};
+      await assertFails(eventRef.update(eventDetails));
+    });
+
+    test("user with valid org, ownerId as orgId should be able to update event", async () => {
+      const eventRef = db.doc("events/E007");
+      const eventDetails = {notes: 'Unit Test'};
+      await assertSucceeds(eventRef.update(eventDetails));
+    });
+
+    test("user with valid org, ownerId as orgId should be able to delete event", async () => {
+      const eventRef = db.doc("events/E007");
+      const eventDetails = {id: 'E007', ownerId: 'O001'};
+      await assertSucceeds(eventRef.delete());
+    });
   });
 
   describe('With User not in org', () => {
@@ -271,6 +312,23 @@ describe.only('Events Rules Tests', () => {
     test("user without valid org shouldn't be able to read event", async () => {
       const eventRef = db.doc("events/E001");
       await assertFails(eventRef.get());
+    });
+
+    test("user without valid org shouldn't be able to create event", async () => {
+      const eventRef = db.doc("events/E007");
+      const eventDetails = {id: 'E007'};
+      await assertFails(eventRef.set(eventDetails));
+    });
+
+    test("user without valid org shouldn't be able to update event", async () => {
+      const eventRef = db.doc("events/E001");
+      const eventDetails = {notes: 'Unit Test'};
+      await assertFails(eventRef.update(eventDetails));
+    });
+
+    test("user without valid org shouldn't be able to delete event", async () => {
+      const eventRef = db.doc("events/E001");
+      await assertFails(eventRef.delete());
     });
   });
 
