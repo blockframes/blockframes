b24e405d12c1e90a13308cb2cb8a4172072230dc
Mussuranae <mandonnetdev@outlook.com>
2020-11-19 18:59:56 +0100

    add module for delete dialog

---
+++ libs/admin/src/lib/admin-panel/components/delete-dialog/delete-dialog.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DeleteDialogComponent } from './delete-dialog.component';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@NgModule({
+  declarations: [DeleteDialogComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatInputModule,
+    MatFormFieldModule
+  ],
+  exports: [DeleteDialogComponent],
+  entryComponents: [DeleteDialogComponent]
+})
+export class DeleteDialogModule { }
