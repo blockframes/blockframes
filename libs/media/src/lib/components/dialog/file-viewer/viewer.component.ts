627c3214ec2d9e8ef4d62278e3cc1076c897155b
Remco <RemcoSimonides@gmail.com>
2020-11-23 11:08:49 +0100

    Change selector name

--- libs/media/src/lib/components/dialog/file-viewer/viewer.component.ts
+++ libs/media/src/lib/components/dialog/file-viewer/viewer.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
-  selector: '[ref] media-viewer',
+  selector: '[ref] file-viewer',
   templateUrl: 'viewer.component.html'
 })
 
