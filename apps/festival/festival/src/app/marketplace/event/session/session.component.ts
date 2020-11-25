c2f6ef88bcdba9d338cf31932f28d07aa3685911
Remco <RemcoSimonides@gmail.com>
2020-11-25 11:43:46 +0100

    Get event observable from eventQuery

--- apps/festival/festival/src/app/marketplace/event/session/session.component.ts
+++ apps/festival/festival/src/app/marketplace/event/session/session.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, OnDestroy, HostListener } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { EventService, Event } from '@blockframes/event/+state';
-import { pluck, switchMap, tap } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { EventService, Event, EventQuery } from '@blockframes/event/+state';
+import { Observable, Subscription } from 'rxjs';
 import { Meeting, Screening } from '@blockframes/event/+state/event.firestore';
 import { MovieService } from '@blockframes/movie/+state/movie.service';
 
@@ -22,44 +20,44 @@ export class SessionComponent implements OnInit, OnDestroy {
   public screeningFileRef: string;
 
   private event: Event;
+  private sub: Subscription;
 
   constructor(
+    private eventQuery: EventQuery,
     private service: EventService,
-    private route: ActivatedRoute,
     private movieService: MovieService
   ) { }
 
   ngOnInit(): void {
-    this.event$ = this.route.params.pipe(
-      pluck('eventId'),
-      switchMap((eventId: string) => this.service.queryDocs(eventId)),
-      tap(async event => {
-        this.event = event;
-        if (event.isOwner) {
-          this.mediaContainerSize = '40%';
-          this.visioContainerSize = '60%';
-        } else {
-          this.mediaContainerSize = '60%';
-          this.visioContainerSize = '40%';
-        }
+    this.event$ = this.eventQuery.selectActive();
+    this.sub = this.event$.subscribe(async event => {
+      this.event = event;
+      if (event.isOwner) {
+        this.mediaContainerSize = '40%';
+        this.visioContainerSize = '60%';
+      } else {
+        this.mediaContainerSize = '60%';
+        this.visioContainerSize = '40%';
+      }
 
-        if (event.type === 'screening') {
-          if (!!(event.meta as Screening).titleId) {
-            const movie = await this.movieService.getValue(event.meta.titleId as string)
-            this.screeningFileRef = movie.promotional.videos?.screener?.ref ?? '';
-          }
-        } else if (event.type === 'meeting') {
-          if (event.isOwner) {
-            (event.meta as Meeting).ownerIsPresent = true;
-            await this.service.update(event.id, { meta: event.meta });
-          }
+      if (event.type === 'screening') {
+        if (!!(event.meta as Screening).titleId) {
+          const movie = await this.movieService.getValue(event.meta.titleId as string)
+          this.screeningFileRef = movie.promotional.videos?.screener?.ref ?? '';
         }
-      }),
-    );
+      } else if (event.type === 'meeting') {
+        if (event.isOwner) {
+          const meta = {...event.meta} as Meeting
+          meta.ownerIsPresent = true;
+          await this.service.update(event.id, { meta });
+        }
+      }
+    })
   }
 
   ngOnDestroy() {
     this.ownerLeaves();
+    this.sub.unsubscribe();
   }
 
   @HostListener('window:beforeunload')
