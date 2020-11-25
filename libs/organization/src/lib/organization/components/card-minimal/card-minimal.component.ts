6aee09a79381df119ed20ff1f184567f23ec88d5
fritzschoff <max.j.f@gmx.de>
2020-11-25 10:05:14 +0100

    adding types

--- libs/organization/src/lib/organization/components/card-minimal/card-minimal.component.ts
+++ libs/organization/src/lib/organization/components/card-minimal/card-minimal.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Organization } from '@blockframes/organization/+state';
+import { AlgoliaOrganization } from '@blockframes/utils/algolia';
 
 function parseOrg(org) {
   /* Only the org from Algolia has the key `objectID` */
-  if (org?.objectID) {
+  if ((org as AlgoliaOrganization)?.objectID) {
     const { denomination } = org.denomination
     return {
       id: org.objectID,
@@ -23,7 +25,7 @@ export class OrganizationCardMinimalComponent {
 
   private _org;
   get org() { return this._org }
-  @Input() set org(org) {
+  @Input() set org(org: Organization | AlgoliaOrganization) {
     this._org = parseOrg(org);
   };
 
