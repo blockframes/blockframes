246488d7178084e530f049301c31d4dd067f76f1
fritzschoff <max.j.f@gmx.de>
2020-11-26 16:23:47 +0100

    light fixed

--- libs/notification/src/lib/+state/notification.store.ts
+++ libs/notification/src/lib/+state/notification.store.ts
@@ -70,14 +70,14 @@ export class NotificationStore extends EntityStore<NotificationState, Notificati
         return {
           date: toDate(notification.date),
           message: `${organizationName} submitted a contract.`,
-          placeholderUrl: 'Organization_250.webp', // TODO: ISSUE#2262
+          placeholderUrl: 'gears.webp', // TODO: ISSUE#2262
           url: `/c/o/dashboard/deals/${notification.docId}`, // TODO check url : see  #2716
         };
       case 'contractInNegotiation':
         return {
           date: toDate(notification.date),
           message: `A new offer has been created.`,
-          placeholderUrl: 'WelcomeArchipelContent_500.webp', // TODO: ISSUE#2262
+          placeholderUrl: 'welcome_archipel_content.webp', // TODO: ISSUE#2262
           url: `/c/o/dashboard/deals/${notification.docId}`, // TODO check url : see  #2716
         };
       case 'movieSubmitted':
