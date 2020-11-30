7d20242f5a0dce85d2956c51ccfb0f46c183e89f
xmano <mano@xkanda.com>
2020-11-30 16:59:33 +0530

    Feat(UT): Add test for campaign rules.

--- apps/backend-ops/src/fixtures/data.ts
+++ apps/backend-ops/src/fixtures/data.ts
@@ -1,116 +1,124 @@
-﻿
-
-export const testFixture = {
-
+﻿export const testFixture = {
   //Permissions
-  "permissions/O001" : {
-    "roles": {
-      "uid-admin": 'admin',
-      "uid-user2": 'member',
-      "uid-sAdmin": 'superAdmin'
-    }
+  'permissions/O001': {
+    roles: {
+      'uid-admin': 'admin',
+      'uid-user2': 'member',
+      'uid-sAdmin': 'superAdmin',
+    },
   },
 
-  "permissions/O001/documentPermissions/D001" : {
-    "ownerId" : 'O001'
+  'permissions/O001/documentPermissions/D001': {
+    ownerId: 'O001',
   },
-  "permissions/O001/documentPermissions/MI-000": {
-    "canCreate": false
+  'permissions/O001/documentPermissions/MI-000': {
+    canCreate: false,
   },
-  "permissions/O001/documentPermissions/MI-007": {
-     "canCreate": true,
-     "canDelete": true,
+  'permissions/O001/documentPermissions/MI-007': {
+    canCreate: true,
+    canDelete: true,
+    canUpdate: true,
+    ownerId: 'O001',
   },
-  "permissions/O001/documentPermissions/MI-0d7": {
-    "canDelete": true,
-    "ownerId": 'O001'
-  },  
-  "permissions/O001/documentPermissions/MI-077": {
-    "canUpdate": true,
-    "canCreate": true,
-    "ownerId": 'O001'
+  'permissions/O001/documentPermissions/MI-0d7': {
+    canDelete: true,
+    ownerId: 'O001',
+  },
+  'permissions/O001/documentPermissions/MI-077': {
+    canUpdate: true,
+    canCreate: true,
+    ownerId: 'O001',
   },
 
   //Orgs
-  "orgs/O001" : {
-    "status": 'accepted',           //belongs to O001
+  'orgs/O001': {
+    status: 'accepted', //belongs to O001
   },
 
   //Users Collection
-  "users/uid-bfAdmin" : {
+  'users/uid-bfAdmin': {
     email: 'bfadmin@cascade8.com',
-    uid: 'uid-bfAdmin'
+    uid: 'uid-bfAdmin',
   },
-  "users/uid-sAdmin" : {
+  'users/uid-sAdmin': {
     email: 'admin@cascade8.com',
     uid: 'uid-sAdmin',
-    orgId: 'O001'
+    orgId: 'O001',
   },
-  "users/uid-admin" : {
+  'users/uid-admin': {
     email: 'admin@cascade8.com',
     uid: 'uid-admin',
-    orgId: 'O001'
+    orgId: 'O001',
   },
-  "users/uid-c8" : {
+  'users/uid-c8': {
     email: 'c8@cascade8.com',
-    uid: 'uid-c8' 
+    uid: 'uid-c8',
   },
-  "users/uid-user2" : {
+  'users/uid-user2': {
     email: 'u2@cascade8.com',
     uid: 'uid-user2',
-    orgId: 'O001'
+    orgId: 'O001',
+    ownerId: 'O001',
   },
-  "users/uid-peeptom" : {
+  'users/uid-peeptom': {
     email: 'tom@no-org.com',
     uid: 'uid-peeptom',
   },
 
   //Blockframes Admin
-  "blockframesAdmin/uid-bfAdmin" : {
-  },
+  'blockframesAdmin/uid-bfAdmin': {},
 
   //Movies
-  "movies/M001": {
-    "id": 'M001',
-    "title": {
-      "original": 'UnitTest'
+  'movies/M001': {
+    id: 'M001',
+    title: {
+      original: 'UnitTest',
     },
-    "distributionRights/DR001": {
-      "id": 'DR001'
-    }
-  },
-  "movies/MI-0d7": {
-    "id": 'MI-0d7',
-    "title": {
-      "original": 'UnitTest Eraser'
+    'distributionRights/DR001': {
+      id: 'DR001',
     },
   },
-  "movies/MI-077": {
-    "id": 'MI-077',
-    "title": {
-      "original": 'UnitTest'
+  'movies/MI-0d7': {
+    id: 'MI-0d7',
+    title: {
+      original: 'UnitTest Eraser',
+    },
+  },
+  'movies/MI-077': {
+    id: 'MI-077',
+    title: {
+      original: 'UnitTest',
     },
     storeConfig: {
-      appAccess: {festival: true},
-      status: 'draft' 
-    },    
-    "note": ''
+      appAccess: { festival: true },
+      status: 'draft',
+    },
+    note: '',
   },
 
-  "events/E001": {
-    "id": 'E001'
+  'events/E001': {
+    id: 'E001',
   },
 
   //Notifications
-  "notifications/001" : {
-      id : "001",
-      date : Date.now(),
-      toUserId: "uid-c8",
-      type: "invitationToAttendEventAccepted",
-      docId: "100",
-      user: {uid: "uid-user2"},
-      organization: {id: "O001"},
-      movies: [{id: "M001"}]
-  }
+  'notifications/001': {
+    id: '001',
+    date: Date.now(),
+    toUserId: 'uid-c8',
+    type: 'invitationToAttendEventAccepted',
+    docId: '100',
+    user: { uid: 'uid-user2' },
+    organization: { id: 'O001' },
+    movies: [{ id: 'M001' }],
+  },
 
-}
+  //Campaigns
+  'campaigns/M001': {
+    id: 'M001',
+    orgId: 'O001',
+  },
+  'campaigns/MI-077': {
+    id: 'MI-077',
+    orgId: 'MI-UK',
+  },
+};
