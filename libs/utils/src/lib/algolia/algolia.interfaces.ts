26eec9e15fb489a55fbc118eb1552eae1c7a0302
Mussuranae <mandonnetdev@outlook.com>
2020-12-01 15:47:46 +0100

    add org denomination to user algolia

--- libs/utils/src/lib/algolia/algolia.interfaces.ts
+++ libs/utils/src/lib/algolia/algolia.interfaces.ts
@@ -9,7 +9,7 @@ import {
 } from '../static-model';
 import { MovieRunningTime, MovieRelease, MovieLanguageSpecification } from '@blockframes/movie/+state/movie.firestore';
 import { Module, ModuleAccess } from '../apps';
-import { PublicOrganization } from '@blockframes/organization/+state';
+import { PublicOrganization, Denomination } from '@blockframes/organization/+state';
 
 export interface AlgoliaConfig {
     searchableAttributes: string[];
@@ -149,4 +149,5 @@ export interface AlgoliaUser extends AlgoliaDefaultProperty {
     firstName: string,
     lastName: string,
     avatar: string,
-} 
+    orgName: Denomination
+}
