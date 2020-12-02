2bb2379e20b382cfcb071824d30a1ba3ad3c3427
pldespaigne <pl.despaigne@gmail.com>
2020-12-02 10:39:02 +0100

    meeting-over style

--- apps/festival/festival/src/app/marketplace/event/ended/meeting-ended.module.ts
+++ apps/festival/festival/src/app/marketplace/event/ended/meeting-ended.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { MeetingEndedComponent } from './meeting-ended.component';
 import { DurationModule } from '@blockframes/utils/pipes/duration.pipe';
+import { EventRangeModule } from '@blockframes/event/pipes/event-range.pipe';
+
+import { MeetingEndedComponent } from './meeting-ended.component';
+import { ImageReferenceModule } from '@blockframes/media/directives/image-reference/image-reference.module';
 
 @NgModule({
   declarations: [ MeetingEndedComponent ],
   imports: [
     CommonModule,
     DurationModule,
+    EventRangeModule,
+    ImageReferenceModule,
 
     RouterModule.forChild([{ path: '', component: MeetingEndedComponent }])
   ],
