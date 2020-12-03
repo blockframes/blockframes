59072817d7e0e275b3eb9037c1d5510631b183f9
Remco <RemcoSimonides@gmail.com>
2020-12-03 12:53:46 +0100

    File preview in file selector of event files

--- libs/media/src/lib/components/file-selector/file-selector.module.ts
+++ libs/media/src/lib/components/file-selector/file-selector.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+// Blockframes
+import { MediaViewerModule } from '@blockframes/media/components/dialog/file-viewer/viewer.module';
+import { FileNameModule } from '@blockframes/utils/pipes/fileName.pipe';
+import { FileSelectorComponent } from './file-selector.component';
+import { MaxLengthModule } from '@blockframes/utils/pipes';
+
+// Material
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { FileNameModule } from '@blockframes/utils/pipes/fileName.pipe';
-import { FileSelectorComponent } from './file-selector.component';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatListModule } from '@angular/material/list';
-import { MaxLengthModule } from '@blockframes/utils/pipes';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
@@ -22,6 +26,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     FileNameModule,
     MaxLengthModule,
     FlexLayoutModule,
+    MediaViewerModule,
 
     MatExpansionModule,
     MatIconModule,
