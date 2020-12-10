08701f1dd1987df827d8c33f746a77f2c55ca206
samir <samir.touira@epitech.eu>
2020-12-10 11:45:07 +0100

    invitation debug when the org is owner

--- libs/event/src/lib/components/screening-item/screening-item.component.ts
+++ libs/event/src/lib/components/screening-item/screening-item.component.ts
@@ -1,9 +1,10 @@
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 import { InvitationQuery, Invitation } from '@blockframes/invitation/+state';
 import { ScreeningEvent } from '../../+state';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Movie } from '@blockframes/movie/+state';
+import { Event } from '../../+state/event.model';
 
 @Component({
   selector: 'event-screening-item',
@@ -16,14 +17,20 @@ export class ScreeningItemComponent {
   public movie: Movie;
   public screening: ScreeningEvent;
   public invitation$: Observable<Invitation>;
+  private _event = new BehaviorSubject<Event>(null);
+  event$ = this._event.asObservable();
 
   @Input() set event(screening: ScreeningEvent) {
+    this._event.next(screening);
     this.screening = screening;
     this.movie = screening.movie;
     this.poster = screening.movie?.poster;
     this.invitation$ = this.invitationQuery.whereCurrentUserIsGuest().pipe(
       map(invits => invits.find(e => e.docId === screening.id))
     );
+  }
+  get event() {
+    return this._event.getValue() as ScreeningEvent;
   }
 
   constructor(private invitationQuery: InvitationQuery) { }
