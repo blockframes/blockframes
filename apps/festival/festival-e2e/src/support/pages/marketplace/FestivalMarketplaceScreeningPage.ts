35d2dd8aaa86dfaf91fa576992e2c74e3c48b5ef
xmano <mano@xkanda.com>
2020-12-10 18:56:39 +0530

    Update: Fix review comments.

--- apps/festival/festival-e2e/src/support/pages/marketplace/FestivalMarketplaceScreeningPage.ts
+++ apps/festival/festival-e2e/src/support/pages/marketplace/FestivalMarketplaceScreeningPage.ts
@@ -3,19 +3,18 @@ import { TO } from "@blockframes/e2e/utils";
 export default class FestivalMarketplaceScreeningPage {
   constructor() {
     cy.log('->FestivalMarketplaceScreeningPage: Access festival-session');
-    cy.get('festival-session', {timeout: TO.PAGE_LOAD});
+    cy.get('festival-session', { timeout: TO.PAGE_LOAD });
   }
 
   clickPlay() {
     cy.log('>FestivalMarketplaceScreeningPage: Start Play [test-id=play]');
-    cy.get('festival-session [test-id=play]', {timeout: TO.PAGE_ELEMENT})
+    cy.get('festival-session [test-id=play]', { timeout: TO.PAGE_ELEMENT })
       .click();
     cy.wait(TO.ONE_SEC);
   }
 
   runVideo() {
     cy.log('>FestivalMarketplaceScreeningPage: Play video');
-    cy.get('festival-session video', { timeout: TO.VSLOW_UPDATE })
-      .click({ force: true });
+    cy.get('festival-session video', { timeout: TO.VSLOW_UPDATE }).click({ force: true });
   }
 }
