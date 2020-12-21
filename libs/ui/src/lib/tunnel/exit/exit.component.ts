12019333cdc477ff0aabb8c6f23760064b4363fa
fritzschoff <max.j.f@gmx.de>
2020-12-21 16:54:40 +0100

    rebaising

--- libs/ui/src/lib/tunnel/exit/exit.component.ts
+++ libs/ui/src/lib/tunnel/exit/exit.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, ChangeDetectionStrategy, Input, Inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ShellConfig, FORMS_CONFIG } from '@blockframes/movie/form/shell/shell.component';
+import { of, Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { TunnelConfirmComponent } from '../layout/confirm/confirm.component';
 
 @Component({
   selector: '[exitRedirect] tunnel-exit',
@@ -13,18 +17,47 @@ export class ExitComponent implements OnInit {
 
   @Input() exitRedirect: string;
 
+  private sub: Subscription;
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    @Inject(FORMS_CONFIG) private configs: ShellConfig
+    @Inject(FORMS_CONFIG) private configs: ShellConfig,
+    private dialog: MatDialog
   ) { }
 
   ngOnInit() {
     this.routeBeforeTunnel = this.exitRedirect || '/c/o/';
   }
 
-  async redirect() {
-    console.log(this.configs)
-    this.router.navigate([this.routeBeforeTunnel], { relativeTo: this.route });
+  redirect() {
+    const isPristine = Object.values(this.configs).every(config => config.form.pristine);
+    console.log(isPristine)
+    if (isPristine) {
+      return of(true);
+    }
+    const dialogRef = this.dialog.open(TunnelConfirmComponent, {
+      width: '80%',
+      data: {
+        title: 'You are going to leave the Movie Form.',
+        subtitle: 'Pay attention, if you leave now your changes will not be saved.'
+      }
+    });
+    this.sub = dialogRef.afterClosed().pipe(
+      switchMap(shouldSave => {
+        /* Undefined means, user clicked on the backdrop, meaning just close the modal */
+        if (typeof shouldSave === 'undefined') {
+          return of(false)
+        }
+        return shouldSave ? this.save() : of(true)
+      })
+    ).subscribe(result => console.log(result));
+    /* this.router.navigate([this.routeBeforeTunnel], { relativeTo: this.route }); */
+  }
+
+  private async save() {
+    for (const name in this.configs) {
+      await this.configs[name].onSave(false);
+    }
   }
 }
--- libs/ui/src/lib/tunnel/index.ts
+++ libs/ui/src/lib/tunnel/index.ts
@@ -1,8 +1,8 @@
-export * from './tunnel.guard';
 export * from './tunnel.service';
 export * from './page/page.module';
 export * from './exit/exit.module';
 export * from './layout/layout.module';
 export * from './tunnel.model';
 
+// TODO MF REMOVE BEFORE MERGE!
 export * from './layout/confirm/confirm.component';
