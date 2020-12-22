ba2b9b041aa7b872865675fa2ac61db7d2088e03
fritzschoff <max.j.f@gmx.de>
2020-12-22 09:49:06 +0100

    removed export

--- libs/ui/src/lib/tunnel/index.ts
+++ libs/ui/src/lib/tunnel/index.ts
@@ -3,6 +3,3 @@ export * from './page/page.module';
 export * from './exit/exit.module';
 export * from './layout/layout.module';
 export * from './tunnel.model';
-
-// TODO MF REMOVE BEFORE MERGE!
-export * from './layout/confirm/confirm.component';
