56ab2daafcc721dca9b706ffb4a353a37779d4f3
fritzschoff <max.j.f@gmx.de>
2020-12-23 11:27:55 +0100

    added func from userService

--- libs/user/src/lib/auth/components/widget/widget.component.ts
+++ libs/user/src/lib/auth/components/widget/widget.component.ts
@@ -3,8 +3,7 @@ import { AuthService, AuthQuery } from '../../+state';
 import { ThemeService } from '@blockframes/ui/theme';
 import { OrganizationQuery } from '@blockframes/organization/+state/organization.query';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { map, switchMap } from 'rxjs/operators';
+import { UserService } from '@blockframes/user/+state';
 
 @Component({
   selector: 'auth-widget',
@@ -16,18 +15,15 @@ export class AuthWidgetComponent {
   user$ = this.query.user$;
   organization$ = this.organizationQuery.selectActive();
   theme$ = this.themeService.theme$;
-  isBfAdmin = this.query.user$.pipe(switchMap(user =>
-    this.af.doc(`blockframesAdmin/${user.uid}`).valueChanges()),
-    map(value => !!value))
+  isBfAdmin = this.userService.isBlockframesAdmin(this.query.getValue().uid);
 
   constructor(
     private service: AuthService,
     private query: AuthQuery,
     private organizationQuery: OrganizationQuery,
     private themeService: ThemeService,
-    private af: AngularFirestore
+    private userService: UserService
   ) { }
-
 
   public async logout() {
     await this.service.signOut();
