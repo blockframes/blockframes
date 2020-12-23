b71cef22de86e51f0c38cad6dd474bdc3c5f39eb
fritzschoff <max.j.f@gmx.de>
2020-12-23 11:19:00 +0100

    added bf admin

--- libs/user/src/lib/auth/components/widget/widget.component.ts
+++ libs/user/src/lib/auth/components/widget/widget.component.ts
@@ -3,6 +3,8 @@ import { AuthService, AuthQuery } from '../../+state';
 import { ThemeService } from '@blockframes/ui/theme';
 import { OrganizationQuery } from '@blockframes/organization/+state/organization.query';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'auth-widget',
@@ -14,13 +16,17 @@ export class AuthWidgetComponent {
   user$ = this.query.user$;
   organization$ = this.organizationQuery.selectActive();
   theme$ = this.themeService.theme$;
+  isBfAdmin = this.query.user$.pipe(switchMap(user =>
+    this.af.doc(`blockframesAdmin/${user.uid}`).valueChanges()),
+    map(value => !!value))
 
   constructor(
     private service: AuthService,
     private query: AuthQuery,
     private organizationQuery: OrganizationQuery,
     private themeService: ThemeService,
-  ){}
+    private af: AngularFirestore
+  ) { }
 
 
   public async logout() {
