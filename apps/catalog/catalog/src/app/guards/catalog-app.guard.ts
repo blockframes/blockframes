7ed51fe15f77307f59b3935b7a02f5a0ccef287a
Mussuranae <mandonnetdev@outlook.com>
2020-12-29 15:18:53 +0100

    add snackbar when stucked on blank page

--- apps/catalog/catalog/src/app/guards/catalog-app.guard.ts
+++ apps/catalog/catalog/src/app/guards/catalog-app.guard.ts
@@ -2,22 +2,30 @@ import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { OrganizationService } from '@blockframes/organization/+state/organization.service';
 import { OrganizationQuery } from '@blockframes/organization/+state/organization.query';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({ providedIn: 'root' })
 export class CatalogAppGuard implements CanActivate {
   constructor(
     protected service: OrganizationService,
     protected router: Router,
-    private query: OrganizationQuery
+    private query: OrganizationQuery,
+    private snackBar: MatSnackBar
   ) { }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const isMarketplace = state.url.split('/').includes('marketplace');
     const org = this.query.getActive();
     if (isMarketplace) {
-      return org.appAccess.catalog.marketplace ? true : this.router.parseUrl('c/o/dashboard');
+      if(!org.appAccess.catalog.marketplace) {
+        this.snackBar.open('You don\'t have access to this application.', '', { duration: 10000 });
+        return false
+      } else this.router.parseUrl('c/o/dashboard');
     } else {
-      return org.appAccess.catalog.dashboard ? true : this.router.parseUrl('c/o/marketplace');
+      if(!org.appAccess.catalog.dashboard) {
+        this.snackBar.open('You don\'t have access to this application.', '', { duration: 10000 });
+        return false
+      } else this.router.parseUrl('c/o/marketplace');
     }
   }
 }
