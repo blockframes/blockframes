9fb6a3ef5c9bc5933bc20a3197b20f79f3e3e273
fritzschoff <max.j.f@gmx.de>
2021-01-04 16:21:01 +0100

    cleanup bf-form-table

--- libs/utils/src/lib/animations/slide.ts
+++ libs/utils/src/lib/animations/slide.ts
@@ -1,3 +1,4 @@
+import { trigger, transition, style, query, animateChild, group, animate, keyframes } from "@angular/animations";
 import { style, state, trigger, transition, animate, animation, keyframes, useAnimation } from '@angular/animations';
 
 export const top = 'translateY(-100%)';
@@ -18,37 +19,37 @@ export const slideOut = animation([
 export const slide = trigger('slideIn', [
   state('in', style({ opacity: 1 })),
   state('out', style({ opacity: 0 })),
-  transition(':enter', [ 
+  transition(':enter', [
     useAnimation(animation([
       style({ transform: '{{translate}}', opacity: 0 }),
       animate('500ms ease-in', style({ transform: 'translateX(0)', opacity: 1 }))
-    ]), 
-    {
-      params: {
-        translate: 'translateY(-100%)'
-      }
-    })
+    ]),
+      {
+        params: {
+          translate: 'translateY(-100%)'
+        }
+      })
   ]),
   transition(':leave', [
     useAnimation(animation([
       animate('500ms ease-in', style({ transform: '{{translate}}', opacity: 1 }))
-    ]), 
-    {
-      params: {
-        translate: 'translateY(-100%)'
-      }
-    })
+    ]),
+      {
+        params: {
+          translate: 'translateY(-100%)'
+        }
+      })
   ])
 ]);
 
 export const slideCalendar = trigger('direction', [
   state('in', style({ opacity: 1 })),
   state('out', style({ opacity: 0 })),
-  transition('void => left', [ 
+  transition('void => left', [
     style({ transform: 'translateX(-350%)', opacity: 0 }),
     animate('300ms ease-in', style({ transform: 'translateX(0)', opacity: 1 }))
   ]),
-  transition('void => right', [ 
+  transition('void => right', [
     style({ transform: 'translateX(350%)', opacity: 0 }),
     animate('300ms ease-in', style({ transform: 'translateX(0)', opacity: 1 }))
   ]),
@@ -60,15 +61,160 @@ export const slideCalendar = trigger('direction', [
   ])
 ]);
 
-export const slideInTop = useAnimation(slideIn, {params: { transform: 'translateY(-100%)' }});
-export const slideOutTop = useAnimation(slideOut, {params: { transform: 'translateY(-100%)' }});
 
-export const slideInBot = useAnimation(slideIn, {params: { transform: 'translateY(100%)' }});
-export const slideOutBot = useAnimation(slideOut, {params: { transform: 'translateY(100%)' }});
+export const slideInAnimation =
+  trigger('slideInAnimation', [
+    transition('FirstPage => SecondPage => ThirdPage...', [
+      style({ position: 'relative' }),
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          width: '100%'
+        })
+      ]),
+      query(':enter', [
+        style({ left: '100%' })
+      ]),
+      query(':leave', animateChild()),
+      group([
+        query(':leave', [
+          animate('300ms ease-out', style({ left: '-100%' }))
+        ]),
+        query(':enter', [
+          animate('300ms ease-out', style({ left: '0%' }))
+        ])
+      ]),
+      query(':enter', animateChild()),
+    ]),
 
-export const slideInLeft = useAnimation(slideIn, {params: { transform: 'translateX(-100%)' }});
-export const slideOutLeft = useAnimation(slideOut, {params: { transform: 'translateX(-100%)' }});
+    transition('FirstPage => SecondPage => ThirdPage...', [
+      style({ position: 'relative' }),
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          width: '100%'
+        })
+      ]),
+      query(':enter', [
+        style({ left: '-100%' })
+      ]),
+      query(':leave', animateChild()),
+      group([
+        query(':leave', [
+          animate('300ms ease-out', style({ left: '100%' }))
+        ]),
+        query(':enter', [
+          animate('300ms ease-out', style({ left: '0%' }))
+        ])
+      ]),
+      query(':enter', animateChild()),
+    ]),
 
-export const slideInRight = useAnimation(slideIn, {params: { transform: 'translateX(100%)' }});
-export const slideOutRight = useAnimation(slideOut, {params: { transform: 'translateX(100%)' }});
+    transition('FirstPage => SecondPage => ThirdPage...', [
+      style({ position: 'relative' }),
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          width: '100%'
+        })
+      ]),
+      query(':enter', [
+        style({ left: '100%' })
+      ]),
+      query(':leave', animateChild()),
+      group([
+        query(':leave', [
+          animate('300ms ease-out', keyframes([
+            style({
+              opacity: 1,
+              offset: 0
+            }),
+            style({
+              opacity: 0,
+              transform: 'scale3d(1.3, 1.3, 1.3)',
+              offset: 0.5
+            }),
+            style({
+              opacity: 0,
+              offset: 1
+            })
+          ]))
+        ]),
+        query(':enter', [
+          animate('300ms ease-out', keyframes([
+            style({
+              opacity: 0,
+              transform: 'scale3d(.3, .3, .3)',
+              offset: 0
+            }),
+            style({
+              opacity: 1,
+              transform: 'scale3d(1, 1, 1)',
+              offset: 0.5
+            })
+          ]))
+        ])
+      ]),
+      query(':enter', animateChild()),
+    ]),
 
+    transition('FirstPage => SecondPage => ThirdPage...', [
+      style({ position: 'relative' }),
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          width: '100%'
+        })
+      ]),
+      query(':enter', [
+        style({ left: '100%' })
+      ]),
+      query(':leave', animateChild()),
+      group([
+        query(':leave', [
+          animate('300ms ease-out', keyframes([
+            style({
+              opacity: 1,
+              offset: 0
+            }),
+            style({
+              opacity: 0,
+              transform: 'scale3d(.3, .3, .3)',
+              offset: 0.5
+            }),
+            style({
+              opacity: 0,
+              offset: 1
+            })
+          ]))
+        ]),
+        query(':enter', [
+          animate('300ms ease-out', keyframes([
+            style({
+              opacity: 0,
+              transform: 'scale3d(.3, .3, .3)',
+              offset: 0
+            }),
+            style({
+              opacity: 1,
+              transform: 'scale3d(1, 1, 1)',
+              offset: 0.5
+            })
+          ]))
+        ])
+      ]),
+      query(':enter', animateChild()),
+    ]),
+  ]);
+
+export const slideInTop = useAnimation(slideIn, { params: { transform: 'translateY(-100%)' } });
+export const slideOutTop = useAnimation(slideOut, { params: { transform: 'translateY(-100%)' } });
+
+export const slideInBot = useAnimation(slideIn, { params: { transform: 'translateY(100%)' } });
+export const slideOutBot = useAnimation(slideOut, { params: { transform: 'translateY(100%)' } });
+
+export const slideInLeft = useAnimation(slideIn, { params: { transform: 'translateX(-100%)' } });
+export const slideOutLeft = useAnimation(slideOut, { params: { transform: 'translateX(-100%)' } });
+
+export const slideInRight = useAnimation(slideIn, { params: { transform: 'translateX(100%)' } });
+export const slideOutRight = useAnimation(slideOut, { params: { transform: 'translateX(100%)' } });
