55eb6a61b8b96514050d9e44f0b27630c565a982
Remco <RemcoSimonides@gmail.com>
2021-01-08 15:38:52 +0100

    4479 Add spinner and empty list view

--- apps/festival/festival/src/app/marketplace/event/list/list.component.ts
+++ apps/festival/festival/src/app/marketplace/event/list/list.component.ts
@@ -20,11 +20,11 @@ export class ListComponent implements OnInit, OnDestroy {
   private _events = new BehaviorSubject<Event[]>([]);
   private allEvents: Event[] = [];
 
-  timeFrames = ascTimeFrames;
+  timeFrames = [];
 
   public events$ = this._events.asObservable();
-
   public searchForm = FormList.factory<AlgoliaOrganization>([]);
+  public loading = true;
 
   private sub: Subscription;
 
@@ -42,23 +42,28 @@ export class ListComponent implements OnInit, OnDestroy {
       // firestore supports only one orderBy if it uses .where()
       map(events => events.filter(event => !!event.meta.titleId)),
       tap(events => {
-        !!events.length ?
-          this.dynTitle.setPageTitle('Screening List') :
+        if (!!events.length) {
+          this.timeFrames = ascTimeFrames;
+          this.loading = false;
+          this.dynTitle.setPageTitle('Screening List');
+        } else {
           this.dynTitle.setPageTitle('Screening List', 'Empty');
+        }
       }),
       // Putting the events in their timeframes
       tap(events => this.allEvents = events),
       switchMap(() => this.searchForm.valueChanges.pipe(
         startWith(this.searchForm.value)
       )),
-      map(results => results.map(result => result.objectID)),
-      tap(results => {
-        if (!results.length) {
-          this._events.next(this.allEvents)
-        } else {
-          const events = this.allEvents.filter(event => results.includes(event.org.id))
+      map(results => results.map(org => org.objectID)),
+      tap(orgIds => {
+        if (!!orgIds.length) {
+          const events = this.allEvents.filter(event => orgIds.includes(event.org.id))
           this._events.next(events)
+        } else {
+          this._events.next(this.allEvents)
         }
+        this.timeFrames = this._events.value.length ? ascTimeFrames : [];
       })
     ).subscribe();
   }
