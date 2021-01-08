65c412e024d60941cacdc1e844fffd2b8cfce959
Remco <RemcoSimonides@gmail.com>
2021-01-08 11:51:54 +0100

    4479 Implement list page

--- apps/festival/festival/src/app/marketplace/event/list/list.module.ts
+++ apps/festival/festival/src/app/marketplace/event/list/list.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 // Component
 import { ListComponent } from './list.component';
 
 // Blockframes
-import { EventListModule } from '@blockframes/event/components/list/list.module';
 import { ScreeningItemModule } from '@blockframes/event/components/screening-item/screening-item.module';
-import { EventEmptyModule } from '@blockframes/event/components/empty/empty.module';
 import { ListPageModule } from '@blockframes/ui/list/page/list-page.module'
 import { ListFilterModule } from '@blockframes/ui/list/filter/list-filter.module';
 import { AlgoliaChipsAutocompleteModule } from "@blockframes/ui/algolia/chips-autocomplete/algolia-chips-autocomplete.module";
@@ -18,22 +15,25 @@ import { FilterByDateModule } from '@blockframes/utils/pipes/filter-by-date.pipe
 
 // Material
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDividerModule } from '@angular/material/divider'
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [ListComponent],
   imports: [
     CommonModule,
-    EventListModule,
-    EventEmptyModule,
+    FlexLayoutModule,
     ScreeningItemModule,
-    MatIconModule,
     RouterModule.forChild([{ path: '', component: ListComponent }]),
     ListPageModule,
     ListFilterModule,
-    MatProgressSpinnerModule,
     AlgoliaChipsAutocompleteModule,
     FilterByDateModule,
-    FlexLayoutModule
+    
+    // Material
+    MatDividerModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
   ]
 })
 export class ListModule { }
