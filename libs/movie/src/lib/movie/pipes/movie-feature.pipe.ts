82263ea00161564ea538e33c01b8898717a47ed0
Remco <RemcoSimonides@gmail.com>
2021-01-15 14:22:06 +0100

    4729 add production countries to title features

--- libs/movie/src/lib/movie/pipes/movie-feature.pipe.ts
+++ libs/movie/src/lib/movie/pipes/movie-feature.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform, NgModule } from '@angular/core';
 import { Movie } from '@blockframes/movie/+state/movie.model';
-import { genres as staticGenres, languages } from '@blockframes/utils/static-model/static-model';
+import { genres as staticGenres, languages, territoriesISOA2 } from '@blockframes/utils/static-model/static-model';
 import { formatRunningTime } from "@blockframes/movie/pipes/running-time.pipe";
 
 @Pipe({
@@ -9,7 +9,7 @@ import { formatRunningTime } from "@blockframes/movie/pipes/running-time.pipe";
 })
 export class MovieFeaturePipe implements PipeTransform {
   transform(value: Movie): string {
-    const { contentType, runningTime, genres, originalLanguages, release } = value;
+    const { contentType, runningTime, genres, originalLanguages, release, stakeholders } = value;
 
     let displayedGenres = '';
     if (genres.length > 0) displayedGenres += staticGenres[genres[0]];
@@ -19,9 +19,25 @@ export class MovieFeaturePipe implements PipeTransform {
     if (originalLanguages.length > 0) displayedLanguages += languages[originalLanguages[0]];
     if (originalLanguages.length > 1) displayedLanguages += ', ...';
 
+    let displayedProductionCountries = '';
+    if (!!stakeholders) {
+      const productionCountries = [];
+      for (const company of stakeholders.productionCompany) {
+        for (const country of company.countries) {
+          if (!productionCountries.includes(country)) productionCountries.push(country);
+        }
+      }
+      productionCountries.forEach((value, index) => {
+        if (index === 0) displayedProductionCountries += territoriesISOA2[value];
+        if (index === 1) displayedProductionCountries += `, ${territoriesISOA2[value]}`;
+        if (index === 2) displayedProductionCountries += `, ...`;
+      })
+    }
+
     return [
       contentType ? contentType[contentType] : '',
       displayedGenres,
+      displayedProductionCountries,
       displayedLanguages,
       release.year,
       formatRunningTime(runningTime, false)
