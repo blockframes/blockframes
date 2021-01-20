87fd5188c697747f5c81884ccaaf1561c82403b4
Remco <RemcoSimonides@gmail.com>
2021-01-20 13:14:12 +0100

    4733 Add random sorting on titles

--- libs/utils/src/lib/pipes/sort-array.pipe.ts
+++ libs/utils/src/lib/pipes/sort-array.pipe.ts
@@ -1,25 +1,22 @@
 import { NgModule, Pipe, PipeTransform } from '@angular/core';
 
-export type SortingOptions = 'default' | 'random';
+const sortFn = {
+  random: (a, b) => Math.random() - .5
+}
+export type SortingOptions = 'default' | keyof typeof sortFn;
 export const sortingOptions: SortingOptions[] = ['default', 'random'];
 
-@Pipe({ name: 'sortArrayBy' })
-export class SortArrayByPipe implements PipeTransform {
-  transform(array: any[], method: SortingOptions) {
-    if (Array.isArray(array)) {
-      if (method === 'random') {
-        for (let i = array.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [array[i], array[j]] = [array[j], array[i]];
-        }
-      }
-    }
-    return array;
+
+@Pipe({ name: 'sortBy' })
+export class SortByPipe implements PipeTransform {
+  transform(array: any[], method: SortingOptions = 'default') {
+    if (method === 'default' || !Array.isArray(array)) return array;
+    return array.sort(sortFn[method]);
   }
 }
 
 @NgModule({
-  declarations: [SortArrayByPipe],
-  exports: [SortArrayByPipe]
+  declarations: [SortByPipe],
+  exports: [SortByPipe]
 })
-export class SortArrayPipeModule { }
+export class SortByPipeModule { }
