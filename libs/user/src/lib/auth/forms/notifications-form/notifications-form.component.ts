c65050dddf4cc2cab51ad4e21972bb80362c0319
Mussuranae <mandonnetdev@outlook.com>
2021-01-29 09:05:37 +0100

    test table

--- libs/user/src/lib/auth/forms/notifications-form/notifications-form.component.ts
+++ libs/user/src/lib/auth/forms/notifications-form/notifications-form.component.ts
@@ -15,6 +15,75 @@ import { MatSlideToggleChange } from '@angular/material/slide-toggle';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NotificationsFormComponent {
+  public notifications = [{
+    title: 'Company Management Notifications',
+    subNotification: [{
+      subtitle: 'A new user requests to join your organization.',
+      email: false,
+      app: true
+    },
+    {
+      subtitle: 'A new member joins your organization.',
+      email: false,
+      app: false
+    }]
+  },
+  {
+    title: 'Content Management Notifications',
+    subNotification: [{
+      subtitle: 'A title is successfully submitted for validation.',
+      email: false,
+      app: false
+    },
+    {
+      subtitle: 'A title gets published to the marketplace.',
+      email: false,
+      app: false
+    }]
+  },
+  {
+    title: 'Event Management Notifications',
+    subNotification: [{
+      subtitle: 'An organization invites you to a screening.',
+      email: false,
+      app: false
+    },
+    {
+      subtitle: 'An user invites you to a meeting.',
+      email: false,
+      app: false
+    },
+    {
+      subtitle: 'An user answers your invitation to an event.',
+      email: false,
+      app: false
+    },
+    {
+      subtitle: 'An user requests an access to an event you organize.',
+      email: false,
+      app: false
+    },
+    {
+      subtitle: 'Your request to access an event has been sent.',
+      email: false,
+      app: false
+    },
+    {
+      subtitle: 'An organization answers your request to access an event.',
+      email: false,
+      app: false
+    },
+    {
+      subtitle: 'Reminder 24h before an event starts.',
+      email: false,
+      app: false
+    },
+    {
+      subtitle: 'Reminder 1h before an event starts.',
+      email: false,
+      app: false
+    }]
+  }];
 
   @Input() form: NotificationsForm;
   selectAllEmail = false;
@@ -30,6 +99,13 @@ export class NotificationsFormComponent {
   selectAll(event: MatSlideToggleChange) {
     if (event.source.name === 'selectAllEmail') this.selectAllEmail = event.checked;
     if (event.source.name === 'selectAllApp') this.selectAllApp = event.checked;
+  }
+
+  someComplete(): boolean {
+    if (this.notifications.subNotification == null) {
+      return false;
+    }
+    return this.notifications.subNotification.filter(n => n.email.value).length > 0;
   }
 
 }
