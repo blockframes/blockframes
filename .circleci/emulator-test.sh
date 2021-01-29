d18039a6a800ee616a063283b3867a29cb26058a
bruce <delorme.bruce.michel@gmail.com>
2021-01-29 17:59:44 +0100

    revert

--- .circleci/emulator-test.sh
+++ .circleci/emulator-test.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 firebase setup:emulators:firestore
-firebase emulators:exec --only firestore 'cross-env nx run user:test'
+firebase emulators:exec --only firestore 'nx run user:test'
 
