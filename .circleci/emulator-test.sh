0ed00fb52af14ca509325b9c476fbd590d913b60
bruce <delorme.bruce.michel@gmail.com>
2021-01-29 18:16:24 +0100

    typo

--- .circleci/emulator-test.sh
+++ .circleci/emulator-test.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 firebase setup:emulators:firestore
-firebase emulators:exec --only firestore 'npx nx run user:test && npx nx run rules:test && nx test backend-functions --skip-nx-cache  && nx test backend-ops --skip-nx-cache'
+firebase emulators:exec --only firestore 'npx nx run user:test && npx nx run rules:test && npx nx test backend-functions --skip-nx-cache && npx nx test backend-ops --skip-nx-cache'
 
