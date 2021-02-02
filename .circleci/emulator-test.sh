cc226c28ce77e18cc1eb0de621318cbb2daf2f61
xmano <mano@xkanda.com>
2021-02-02 17:00:26 +0530

    Change way to test

--- .circleci/emulator-test.sh
+++ .circleci/emulator-test.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
-firebase emulators:exec --only firestore 'npx nx affected:test --base=origin/master --skip-nx-cache'
+firebase emulators:exec --only firestore 'nx test event --skip-nx-cache && nx test notification --skip-nx-cache && nx test import --skip-nx-cache && nx test invitation  --skip-nx-cache'
 
