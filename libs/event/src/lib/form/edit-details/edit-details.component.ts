f024d06833eb70817a0c4089f4e23bd672bce9f7
Remco <RemcoSimonides@gmail.com>
2021-02-03 16:07:55 +0100

    Fix prefix

--- libs/event/src/lib/form/edit-details/edit-details.component.ts
+++ libs/event/src/lib/form/edit-details/edit-details.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { EventForm } from '../event.form';
 
 @Component({
-  selector: '[form] edit-event-details',
+  selector: '[form] event-details-edit',
   templateUrl: 'edit-details.component.html',
   styleUrls: ['./edit-details.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
