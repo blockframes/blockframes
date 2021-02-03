cad6b8c40415c88a1470ad1b1a5888b04d4c1b30
bruce <delorme.bruce.michel@gmail.com>
2021-02-03 15:57:55 +0100

    cleaning

--- libs/notification/src/lib/+state/notification.store.ts
+++ libs/notification/src/lib/+state/notification.store.ts
@@ -49,7 +49,7 @@ export class NotificationStore extends EntityStore<NotificationState, Notificati
           placeholderUrl: 'empty_organization.webp',
           url: `/c/o/organization/${notification.organization.id}/view/org`,
         };
-      case 'invitationFromUserToJoinOrgDecline': // @TODO #4046 add to form and handle mail associated  
+      case 'invitationFromUserToJoinOrgDecline':
         return {
           _meta: { ...notification._meta, createdAt: toDate(notification._meta.createdAt) },
           message: `${displayUserName}'s request to join your organization was refused.`,
