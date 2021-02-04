2876a6e62dbf7a7096845bedc8fd44ff966b5a20
xmano <mano@xkanda.com>
2021-02-04 17:49:38 +0530

    Change origin

--- .circleci/emulator-test.sh
+++ .circleci/emulator-test.sh
@@ -3,5 +3,5 @@
 # Check Docker file to install other emulators
 # Modify the command as needed
 echo "Starting emulators & launching affected unit-tests..."
-firebase emulators:exec --only firestore, functions, auth, pubsub 'npx nx affected:test --base=origin/master'
+firebase emulators:exec --only firestore, functions, auth, pubsub 'npx nx affected:test --base=origin/develop'
 
