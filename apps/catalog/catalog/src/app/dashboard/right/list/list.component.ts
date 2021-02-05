df7670d4d43178d0c46c157f938f20d629b3a417
fritzschoff <max.j.f@gmx.de>
2021-02-05 16:53:04 +0100

    update

--- apps/catalog/catalog/src/app/dashboard/right/list/list.component.ts
+++ apps/catalog/catalog/src/app/dashboard/right/list/list.component.ts
@@ -21,7 +21,7 @@ export class RightListComponent {
   };
   initialColumns = ['id', 'status'];
   filter = new FormControl();
-  filter$: Observable<any> = this.filter.valueChanges.pipe(startWith(this.filter.value));
+  filter$: Observable<string> = this.filter.valueChanges.pipe(startWith(this.filter.value));
   contracts = []
 
   constructor(
