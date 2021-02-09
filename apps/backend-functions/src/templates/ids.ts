b80de2c24dec60d6732faca540e06425d3722bbd
Bruce <eelios@gmail.com>
2021-02-09 15:36:34 +0100

    small fixes (#4880)
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>

--- apps/backend-functions/src/templates/ids.ts
+++ apps/backend-functions/src/templates/ids.ts
@@ -42,8 +42,8 @@ export const templateIds = {
   invitation: {
     attendEvent: {
       created: 'd-1a7cc9ca846c4ae1b4e8cf8a76455cc5',
-      accepted: '5d014e60af4c431dbe7316369f41591e',
-      declined: 'b7ea357cfd41404e9ff82f8d75410079'
+      accepted: 'd-5d014e60af4c431dbe7316369f41591e',
+      declined: 'd-b7ea357cfd41404e9ff82f8d75410079'
     },
     organization: {
       declined: 'd-d3c17695e25a453a98ab5540ea171c5c'
