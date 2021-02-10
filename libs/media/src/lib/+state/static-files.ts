e8d0b7a4a9eaed753cabf6d5c89edc0cefe63f07
pldespaigne <pl.despaigne@gmail.com>
2021-02-10 14:30:32 +0100

    requested changes 2

--- libs/media/src/lib/+state/static-files.ts
+++ libs/media/src/lib/+state/static-files.ts
@@ -1,3 +1,4 @@
+
 import { FileMetaData } from "./media.model";
 
 
@@ -35,31 +36,31 @@ export const fileLabels = [
 ] as const;
 export type FileLabel = typeof fileLabels[number];
 
-export function getFileStoragePath(collection: CollectionHoldingFile, label: FileLabel) {
-  const storagePaths: Record<CollectionHoldingFile, Partial<Record<FileLabel, string>>> = {
-    'orgs': {
-      'logo': 'logo',
-    },
-    'users': {
-      'avatar': 'avatar',
-      'watermark': 'watermark',
-    },
-    'movies': {
-      'poster': 'poster',
-      'banner': 'banner',
-      'scenario': 'promotional.scenario',
-      'moodboard': 'promotional.moodboard',
-      'presentation_deck': 'promotional.',
-      'still_photo': 'promotional.still_photo',
-      'screener': 'promotional.videos.screener',
-      'otherVideo': 'promotional.videos.otherVideos',
-      'salesPitch': 'promotional.salesPitch',
-    },
-    'campaigns': {
-      // TODO issue#4002
-    },
-  }
+export const storagePaths: Record<CollectionHoldingFile, Partial<Record<FileLabel, string>>> = {
+  orgs: {
+    logo: 'logo',
+  },
+  users: {
+    avatar: 'avatar',
+    watermark: 'watermark',
+  },
+  movies: {
+    poster: 'poster',
+    banner: 'banner',
+    scenario: 'promotional.scenario',
+    moodboard: 'promotional.moodboard',
+    'presentation_deck': 'promotional.',
+    'still_photo': 'promotional.still_photo',
+    screener: 'promotional.videos.screener',
+    otherVideo: 'promotional.videos.otherVideos',
+    salesPitch: 'promotional.salesPitch',
+  },
+  campaigns: {
+    // TODO issue#4002
+  },
+};
 
+export function getFileStoragePath(collection: CollectionHoldingFile, label: FileLabel) {
   const pathPart = storagePaths[collection][label];
   if (!pathPart) return '';
 
@@ -68,27 +69,27 @@ export function getFileStoragePath(collection: CollectionHoldingFile, label: Fil
 
 export function getFileMetadata(collection: CollectionHoldingFile, label: FileLabel, docId: string, index?: number) {
   const metadatas: Record<CollectionHoldingFile, Partial<Record<FileLabel, FileMetaData>>> = {
-    'orgs': {
-      'logo': { uid: '', privacy: 'public', collection, docId, field: 'logo' },
+    orgs: {
+      logo: { uid: '', privacy: 'public', collection, docId, field: 'logo' },
     },
-    'users': {
-      'avatar': { uid: '', privacy: 'public', collection, docId, field: 'avatar' },
-      'watermark': { uid: '', privacy: 'public', collection, docId, field: 'watermark' },
+    users: {
+      avatar: { uid: '', privacy: 'public', collection, docId, field: 'avatar' },
+      watermark: { uid: '', privacy: 'public', collection, docId, field: 'watermark' },
     },
-    'movies': {
-      'poster': { uid: '', privacy: 'public', collection, docId, field: 'poster' },
-      'banner': { uid: '', privacy: 'public', collection, docId, field: 'poster' },
-      'scenario': { uid: '', privacy: 'public', collection, docId, field: 'promotional.scenario' },
-      'moodboard': { uid: '', privacy: 'public', collection, docId, field: 'promotional.moodboard' },
+    movies: {
+      poster: { uid: '', privacy: 'public', collection, docId, field: 'poster' },
+      banner: { uid: '', privacy: 'public', collection, docId, field: 'poster' },
+      scenario: { uid: '', privacy: 'public', collection, docId, field: 'promotional.scenario' },
+      moodboard: { uid: '', privacy: 'public', collection, docId, field: 'promotional.moodboard' },
       'presentation_deck': { uid: '', privacy: 'public', collection, docId, field: 'promotional.presentation_deck' },
       'still_photo': { uid: '', privacy: 'public', collection, docId, field: `promotional.still_photo[${index}]` },
-      'screener': { uid: '', privacy: 'protected', collection, docId, field: 'promotional.videos.screener' },
-      'otherVideo': { uid: '', privacy: 'public', collection, docId, field: `promotional.videos.otherVideos[${index}]` },
-      'salesPitch': { uid: '', privacy: 'public', collection, docId, field: 'promotional.salesPitch' },
+      screener: { uid: '', privacy: 'protected', collection, docId, field: 'promotional.videos.screener' },
+      otherVideo: { uid: '', privacy: 'public', collection, docId, field: `promotional.videos.otherVideos[${index}]` },
+      salesPitch: { uid: '', privacy: 'public', collection, docId, field: 'promotional.salesPitch' },
     },
-    'campaigns': {
+    campaigns: {
       // TODO issue#4002
     },
-  }
+  };
   return metadatas[collection][label];
 }
