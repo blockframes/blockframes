d28a23c6129e0fefc9268d02dd6f3ed95db5fa2a
Remco <RemcoSimonides@gmail.com>
2021-02-16 11:02:58 +0100

    4002 refactor media

--- libs/movie/src/lib/movie/components/promotional-links/promotional-links.pipe.ts
+++ libs/movie/src/lib/movie/components/promotional-links/promotional-links.pipe.ts
@@ -2,6 +2,7 @@ import { Pipe, PipeTransform, NgModule } from '@angular/core';
 import { Movie } from '@blockframes/movie/+state/movie.model';
 import { MediaService } from '@blockframes/media/+state/media.service';
 import { promotionalElementTypes } from '@blockframes/utils/static-model/static-model';
+import { StorageFile } from '@blockframes/media/+state/media.firestore';
 
 @Pipe({
   name: 'promotionalLinks',
@@ -15,10 +16,15 @@ export class PromotionalLinksPipe implements PipeTransform {
 
   async transform(links: string[], movie: Movie): Promise<any[]> {
     const _links = await Promise.all(links.map(async link => {
-      const ref = (movie.promotional[link] as string);
+      const ref = (movie.promotional[link] as StorageFile | string);
       const useImgIx = ['scenario', 'presentation_deck', 'moodboard'].includes(link);
+
+      if (!useImgIx && typeof ref !== 'string') {
+        throw new Error('Referene should be of type string')
+      }
+
       if (!!ref) {
-        const url = useImgIx ? await this.mediaService.generateImgIxUrl(ref) : ref;
+        const url = useImgIx ? await this.mediaService.generateImgIxUrl(ref as StorageFile) : ref as string;
         if (!!url) {
           const linkLabel = promotionalElementTypes[link];
           const icon = useImgIx ? 'cloud_download' : 'play_arrow';
