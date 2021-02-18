e1e7e551be6626b9003949a73fb436af65d827c1
Mussuranae <mandonnetdev@outlook.com>
2021-02-18 14:27:03 +0100

    add subscription to email input

--- libs/user/src/lib/auth/components/email-input/email-input.component.ts
+++ libs/user/src/lib/auth/components/email-input/email-input.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Output } from "@angular/core";
+import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output } from "@angular/core";
 import { FormControl } from "@angular/forms";
 import { AngularFireFunctions } from '@angular/fire/functions';
 import { PublicOrganization } from "@blockframes/organization/+state";
@@ -9,22 +9,27 @@ import { PublicOrganization } from "@blockframes/organization/+state";
   styleUrls: ['./email-input.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EmailInputComponent {
+export class EmailInputComponent implements OnInit {
 
-  @Output() organization: EventEmitter<PublicOrganization> = new EventEmitter<PublicOrganization>();
-  @Output() showInvitationInput: EventEmitter<boolean> = new EventEmitter<false>();
+  @Output() private readonly organization = new EventEmitter<PublicOrganization>();
+  @Output() showInvitationInput = new EventEmitter<boolean>(false);
+
   private getInvitationLinkedToEmail = this.functions.httpsCallable('getInvitationLinkedToEmail')
   public emailForm = new FormControl();
 
   constructor(private functions: AngularFireFunctions) { }
 
+  ngOnInit() {
+    this.emailForm.valueChanges.subscribe(_ => this.searchForInvitation());
+  }
+
   async searchForInvitation() {
     const doc = await this.getInvitationLinkedToEmail(this.emailForm.value).toPromise();
 
     if (typeof doc === 'object') {
+      this.showInvitationInput.emit(true);
       return this.organization.emit(doc);
-    } else {
-      return this.showInvitationInput.emit(doc);
     }
+    else return this.showInvitationInput.emit(doc);
   }
 }
