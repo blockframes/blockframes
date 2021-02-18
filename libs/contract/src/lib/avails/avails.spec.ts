7b0c4f608028a45fbd32d0aa14957b6624cff6dd
fritzschoff <max.j.f@gmx.de>
2021-02-18 15:50:59 +0100

    updated tests

--- libs/contract/src/lib/avails/avails.spec.ts
+++ libs/contract/src/lib/avails/avails.spec.ts
@@ -1,6 +1,6 @@
 import { Mandate, Sale } from '../contract/+state/contract.model';
 import { Term } from '../term/+state/term.model';
-import { getAvails, filterAvail } from './avails';
+import { filterAvail, filterSales } from './avails';
 import firebase from 'firebase';
 
 export type Timestamp = firebase.firestore.Timestamp;
@@ -10,12 +10,13 @@ describe('Avails', () => {
     let contracts;
     let mandateTerms: Term<Timestamp>[];
     let saleTerms: Term<Timestamp>[];
-    firebase.initializeApp({ projectId: 'test' }).firestore().useEmulator('localhost', 8080)
+    firebase.initializeApp({ projectId: 'test' }).firestore().useEmulator('localhost', 8080);
     const db = firebase.firestore()
 
     beforeEach(async () => {
         const contractsRef = await db.collection('contracts').where('titleId', '==', titleId).get()
-        contracts = contractsRef.docs.map(ref => ref.data() as (Mandate | Sale));
+        contracts = contractsRef.docs.map(ref => ref.data());
+
         const mandates = contracts.filter((contract: Mandate) => contract.type === 'mandate') as Mandate[]
         const sales = contracts.filter((contract: Sale) => contract.type === 'sale') as Sale[]
 
@@ -23,41 +24,24 @@ describe('Avails', () => {
         const saleTermsIds = sales.map(sale => sale.termsIds).flat();
 
         const mandateTermsRef = await Promise.all(mandateTermsIds.map(id => db.doc(`terms/${id}`).get()));
-        mandateTerms = mandateTermsRef.map(ref => ref.data()) as Term<Timestamp>[]
+        mandateTerms = mandateTermsRef.map(ref => ref.data()) as Term<Timestamp>[];
 
         const salesTermsRef = await Promise.all(saleTermsIds.map(id => db.doc(`terms/${id}`).get()))
-        saleTerms = salesTermsRef.map(ref => ref.data()) as Term<Timestamp>[]
+        saleTerms = salesTermsRef.map(ref => ref.data()) as Term<Timestamp>[];
+        const d = await db.collection('terms').doc('HJvSSzBny8BHdFFoor4y').get();
     });
 
-    it.only(`Mandate test (territory)
+    it(`Mandate test (territory)
     Terms: 01/01/2022 - 06/30/2023
     Territory: South Korea
     Rights: Free TV
     Exclusive: No
     Expected result: Not licensed`, () => {
-        /* 
-        @MF Many steps to take
-        check if AC has the mandate,
-        check if buyer wants to have exclusive rights,
-        if yes then check every sales that has been made on the movie,
-        if non exclusive right, then only filter upon exclusive sales
-
-        ATTENTION
-        ALSO THERE IS A BUG; DURATION ON MANDATE IS SWITCHED; FROM AND TO NEEDS TO BE SWITCHED!!!!!!!!!!
-        ATTENTION
-        */
-        const toDateMandateAvails = mandateTerms.map(parseTimestampsOnTerms);
-        console.log(toDateMandateAvails[0].duration)
-        const toDateAvails = saleTerms.map(parseTimestampsOnTerms);
-        const isCoveredByMandate = filterAvail({
+        const acMandateTerms = mandateTerms.map(parseTimestampsOnTerms);
+        const acHasRights = filterAvail({
             duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, territories: ['south-korea'], medias: ['freeTv'], exclusive: false
-        }, toDateMandateAvails)
-        console.log(isCoveredByMandate)
-        const results = filterAvail({
-            duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, territories: ['south-korea'], medias: ['freeTv'], exclusive: false
-        }, toDateAvails);
-        const isTaken = results.every(e => e)
-        expect(isTaken).toBe(false);
+        }, acMandateTerms);
+        expect(acHasRights.length).toBe(0);
     })
 
     it(`Mandate test (terms)
@@ -66,11 +50,11 @@ describe('Avails', () => {
     Rights: Free TV
     Exclusive: No
     Expected result: Not licensed`, () => {
-        const avails = getAvails({ mandateTerms, saleTerms });
-        const toDateAvails = avails.map(parseTimestampsOnTerms);
-        const isLicensed = filterAvail(
-            { duration: { to: new Date('01/01/2028'), from: new Date('06/30/2036') }, exclusive: false, territories: ['afghanistan'], medias: ['freeTv'] }, toDateAvails)
-        expect(isLicensed).toBe(false)
+        const toDateAvails = mandateTerms.map(parseTimestampsOnTerms);
+        const ACRights = filterAvail(
+            { duration: { to: new Date('06/30/2036'), from: new Date('01/01/2028') }, exclusive: false, territories: ['afghanistan'], medias: ['freeTv'] },
+            toDateAvails);
+        expect(ACRights.length).toBe(0)
     });
 
     it(`Terms: 01/01/2028 - 06/30/2036
@@ -78,11 +62,11 @@ describe('Avails', () => {
     Rights: Planes
     Exclusive: No
     Expected result: Not licensed`, () => {
-        const avails = getAvails({ mandateTerms, saleTerms });
-        const toDateAvails = avails.map(parseTimestampsOnTerms);
-        const isLicensed = filterAvail(
-            { duration: { to: new Date('01/01/2028'), from: new Date('06/30/2036') }, exclusive: false, territories: ['france'], medias: ['planes'] }, toDateAvails)
-        expect(isLicensed).toBe(false)
+        const toDateAvails = mandateTerms.map(parseTimestampsOnTerms);
+        const ACRights = filterAvail(
+            { duration: { to: new Date('06/30/2036'), from: new Date('01/01/2028') }, exclusive: false, territories: ['france'], medias: ['planes'] },
+            toDateAvails);
+        expect(ACRights.length).toBe(0)
     });
 
     it(`Terms check (existing ended sale)
@@ -91,11 +75,17 @@ describe('Avails', () => {
     Rights: Free TV
     Exclusive: Yes
     Expected result: Available`, () => {
-        const avails = getAvails({ mandateTerms, saleTerms });
-        const toDateAvails = avails.map(parseTimestampsOnTerms);
-        const isLicensed = filterAvail(
-            { duration: { to: new Date('01/01/2033'), from: new Date('06/30/2033') }, exclusive: true, territories: ['germany', 'russia', 'czech'], medias: ['freeTv'] }, toDateAvails)
-        expect(isLicensed).toBe(true)
+        const toDateAvails = mandateTerms.map(parseTimestampsOnTerms);
+        const ACRights = filterAvail(
+            { duration: { to: new Date('06/30/2033'), from: new Date('01/01/2033') }, exclusive: true, territories: ['germany', 'russia', 'czech'], medias: ['freeTv'] },
+            toDateAvails);
+        expect(ACRights.length).toBe(1);
+        const toDateSales = saleTerms.map(parseTimestampsOnTerms);
+        const avails = filterSales(
+            { duration: { to: new Date('06/30/2033'), from: new Date('01/01/2033') }, exclusive: true, territories: ['germany', 'russia', 'czech'], medias: ['freeTv'] },
+            toDateSales);
+            console.log(avails)
+        expect(avails).toBe(true);
     });
 
     it(`Terms check (existing ongoing sale)
@@ -104,89 +94,126 @@ describe('Avails', () => {
     Rights: Free TV
     Exclusive: Yes
     Expected result: Not available`, () => {
-        const avails = getAvails({ mandateTerms, saleTerms });
-        const toDateAvails = avails.map(parseTimestampsOnTerms);
-        const isLicensed = filterAvail(
-            { duration: { to: new Date('01/01/2019'), from: new Date('06/30/2031') }, exclusive: true, territories: ['germany', 'russia', 'czech'], medias: ['freeTv'] }, toDateAvails)
-        expect(isLicensed).toBe(false)
+        const toDateAvails = mandateTerms.map(parseTimestampsOnTerms);
+        const ACRights = filterAvail(
+            { duration: { to: new Date('06/30/2031'), from: new Date('01/01/2029') }, exclusive: true, territories: ['germany', 'russia', 'czech'], medias: ['freeTv'] },
+            toDateAvails)
+        expect(ACRights.length).toBe(1);
+        const toDateSales = saleTerms.map(parseTimestampsOnTerms);
+        const isRightPossible = filterSales(
+            { duration: { to: new Date('06/30/2031'), from: new Date('01/01/2019') }, exclusive: true, territories: ['germany', 'russia', 'czech'], medias: ['freeTv'] },
+            toDateSales);
+        expect(!isRightPossible).toBe(false);
     });
 
     it(`Cross territory check + exclusivity
-    Terms: 01/01/2022 - 06/30/2022
-    Territory: Germany, Russia, Czech Republic
-    Rights: Free TV
-    Exclusive: No
-    Expected result: Available`, () => {
-        const avails = getAvails({ mandateTerms, saleTerms });
-        const toDateAvails = avails.map(parseTimestampsOnTerms);
-        const isLicensed = filterAvail(
-            { duration: { to: new Date('01/01/2022'), from: new Date('06/30/2022') }, exclusive: false, territories: ['germany', 'russia', 'czech'], medias: ['freeTv'] }, toDateAvails)
-        expect(isLicensed).toBe(true)
+        Terms: 01/01/2022 - 06/30/2022
+        Territory: Germany, Russia, Czech Republic
+        Rights: Free TV
+        Exclusive: No
+        Expected result: Available`, () => {
+        const toDateAvails = mandateTerms.map(parseTimestampsOnTerms);
+        const ACRights = filterAvail(
+            { duration: { to: new Date('06/30/2022'), from: new Date('01/01/2022') }, exclusive: false, territories: ['germany', 'russia', 'czech'], medias: ['freeTv'] },
+            toDateAvails)
+        expect(ACRights.length).toBe(1);
+        const toDateSales = saleTerms.map(parseTimestampsOnTerms);
+        const avails = filterSales(
+            { duration: { to: new Date('06/30/2022'), from: new Date('01/01/2022') }, exclusive: false, territories: ['germany', 'russia', 'czech'], medias: ['freeTv'] },
+            toDateSales);
+        expect(avails).toBe(true);
     });
 
     it(`Cross territory check + exclusivity
-    Terms: 01/01/2022 - 06/30/2022
-    Territory: Germany, Russia, Czech Republic
-    Rights: Free TV
-    Exclusive: Yes
-    Expected result: Not available`, () => {
-        const avails = getAvails({ mandateTerms, saleTerms });
-        const toDateAvails = avails.map(parseTimestampsOnTerms);
-        const isLicensed = filterAvail(
-            { duration: { to: new Date('01/01/2022'), from: new Date('06/30/2022') }, exclusive: true, territories: ['germany', 'russia', 'czech'], medias: ['freeTv'] }, toDateAvails)
-        expect(isLicensed).toBe(false)
+        Terms: 01/01/2022 - 06/30/2022
+        Territory: Germany, Russia, Czech Republic
+        Rights: Free TV
+        Exclusive: Yes
+        Expected result: Not available`, () => {
+        const toDateAvails = mandateTerms.map(parseTimestampsOnTerms);
+        const ACRights = filterAvail(
+            { duration: { to: new Date('06/30/2022'), from: new Date('01/01/2022') }, exclusive: true, territories: ['germany', 'russia', 'czech'], medias: ['freeTv'] },
+            toDateAvails)
+        expect(ACRights.length).toBe(1);
+        const toDateSales = saleTerms.map(parseTimestampsOnTerms);
+        const avails = filterSales(
+            { duration: { to: new Date('06/30/2022'), from: new Date('01/01/2022') }, exclusive: true, territories: ['germany', 'russia', 'czech'], medias: ['freeTv'] },
+            toDateSales);
+        expect(!avails).toBe(false);
     });
 
     it(`Rights check
-    Terms: 01/01/2022 - 06/30/2022
-    Territory: Argentina
-    Rights: S-VOD
-    Exclusive: Yes
-    Expected result: Available`, () => {
-        const avails = getAvails({ mandateTerms, saleTerms });
-        const toDateAvails = avails.map(parseTimestampsOnTerms);
-        const isLicensed = filterAvail(
-            { duration: { to: new Date('01/01/2022'), from: new Date('06/30/2022') }, exclusive: true, territories: ['argentina'], medias: ['sVod'] }, toDateAvails)
-        expect(isLicensed).toBe(true)
+      Terms: 01/01/2022 - 06/30/2022
+      Territory: Argentina
+      Rights: S-VOD
+      Exclusive: Yes
+      Expected result: Available`, () => {
+        const toDateAvails = mandateTerms.map(parseTimestampsOnTerms);
+        const ACRights = filterAvail(
+            { duration: { to: new Date('06/30/2022'), from: new Date('01/01/2022') }, exclusive: true, territories: ['argentina'], medias: ['sVod'] },
+            toDateAvails)
+        expect(ACRights.length).toBe(1);
+        const toDateSales = saleTerms.map(parseTimestampsOnTerms);
+        const avails = filterSales(
+            { duration: { to: new Date('06/30/2022'), from: new Date('01/01/2022') }, exclusive: true, territories: ['argentina'], medias: ['sVod'] },
+            toDateSales);
+        expect(avails).toBe(true);
     });
 
-    it(`Rights check
+
+    it.only(`Rights check
     Terms: 01/01/2022 - 06/30/2022
     Territory: Argentina
     Rights: Pay TV
     Exclusive: Yes
     Expected result: Not available`, () => {
-        const avails = getAvails({ mandateTerms, saleTerms });
-        const toDateAvails = avails.map(parseTimestampsOnTerms);
-        const isLicensed = filterAvail(
-            { duration: { to: new Date('01/01/2022'), from: new Date('06/30/2022') }, exclusive: true, territories: ['argentina'], medias: ['payTv'] }, toDateAvails)
-        expect(isLicensed).toBe(false)
+        const toDateAvails = mandateTerms.map(parseTimestampsOnTerms);
+        const ACRights = filterAvail(
+            { duration: { to: new Date('06/30/2022'), from: new Date('01/01/2022') }, exclusive: true, territories: ['argentina'], medias: ['payTv'] },
+            toDateAvails)
+        expect(ACRights.length).toBe(1);
+        const toDateSales = saleTerms.map(parseTimestampsOnTerms);
+        const avails = filterSales(
+            { duration: { to: new Date('06/30/2022'), from: new Date('01/01/2022') }, exclusive: true, territories: ['argentina'], medias: ['payTv'] },
+            toDateSales);
+            console.log(avails)
+        expect(!avails).toBe(false);
     });
 
     it(` Exclusivity test (non exclusive)
-    Terms: 01/01/2022 - 06/30/2022
-    Territory: Germany
-    Rights: Free TV
-    Exclusive: No
-    Expected result: Available`, () => {
-        const avails = getAvails({ mandateTerms, saleTerms });
-        const toDateAvails = avails.map(parseTimestampsOnTerms);
-        const isLicensed = filterAvail(
-            { duration: { to: new Date('01/01/2022'), from: new Date('06/30/2022') }, exclusive: false, territories: ['germany'], medias: ['freeTv'] }, toDateAvails)
-        expect(isLicensed).toBe(true)
+      Terms: 01/01/2022 - 06/30/2022
+      Territory: Germany
+      Rights: Free TV
+      Exclusive: No
+      Expected result: Available`, () => {
+        const toDateAvails = mandateTerms.map(parseTimestampsOnTerms);
+        const ACRights = filterAvail(
+            { duration: { to: new Date('06/30/2022'), from: new Date('01/01/2022') }, exclusive: false, territories: ['germany'], medias: ['freeTv'] },
+            toDateAvails)
+        expect(ACRights.length).toBe(1);
+        const toDateSales = saleTerms.map(parseTimestampsOnTerms);
+        const avails = filterSales(
+            { duration: { to: new Date('06/30/2022'), from: new Date('01/01/2022') }, exclusive: false, territories: ['germany'], medias: ['freeTv'] },
+            toDateSales);
+        expect(avails).toBe(true);
     });
 
     it(`Exclusivity test (exclusive)
-    Terms: 01/01/2020 - 06/30/2020
-    Territory: Canada
-    Rights: Free TV
-    Exclusive: Yes
-    Expected result: Not available`, () => {
-        const avails = getAvails({ mandateTerms, saleTerms });
-        const toDateAvails = avails.map(parseTimestampsOnTerms);
-        const isLicensed = filterAvail(
-            { duration: { to: new Date('01/01/2020'), from: new Date('06/30/2020') }, exclusive: true, territories: ['canada'], medias: ['freeTv'] }, toDateAvails)
-        expect(isLicensed).toBe(false)
+      Terms: 01/01/2020 - 06/30/2020
+      Territory: Canada
+      Rights: Free TV
+      Exclusive: Yes
+      Expected result: Not available`, () => {
+        const toDateAvails = mandateTerms.map(parseTimestampsOnTerms);
+        const ACRights = filterAvail(
+            { duration: { to: new Date('06/30/2020'), from: new Date('01/01/2020') }, exclusive: true, territories: ['canada'], medias: ['freeTv'] },
+            toDateAvails)
+        expect(ACRights.length).toBe(1);
+        const toDateSales = saleTerms.map(parseTimestampsOnTerms);
+        const avails = filterSales(
+            { duration: { to: new Date('06/30/2020'), from: new Date('01/01/2020') }, exclusive: true, territories: ['canada'], medias: ['freeTv'] },
+            toDateSales);
+        expect(!avails).toBe(false);
     });
 })
 
--- libs/contract/src/lib/avails/avails.ts
+++ libs/contract/src/lib/avails/avails.ts
@@ -2,64 +2,58 @@ import { Media, Territory } from "@blockframes/utils/static-model";
 import { Term } from "../term/+state/term.model";
 import { Timestamp } from './avails.spec';
 
-export function getAvails({ mandateTerms, saleTerms }: { mandateTerms: Partial<Term<Timestamp>[]>, saleTerms: Partial<Term<Timestamp>[]> }) {
-  return mandateTerms.filter(positive => {
-    for (const negative of saleTerms) {
-
-      // Territory
-      const hasTerritory = positive.territories.every(t => negative.territories.includes(t));
-
-      // Media
-      const hasMedia = positive.medias.every(m => negative.medias.includes(m));
-
-      // Duration
-      const startDuringDuration = positive.duration.from > negative.duration.from || positive.duration.from < negative.duration.to
-      const endDuringDuration = positive.duration.to < negative.duration.from || positive.duration.to > negative.duration.to
-      const inDuration = startDuringDuration || endDuringDuration;
-
-      if (hasTerritory && hasMedia && inDuration) return false;
-    }
-    return true;
-  });
-}
-
 interface AvailsFilter { medias: Media[], duration: { from: Date, to: Date }, territories: Territory[], exclusive: boolean }
 
 export function filterAvail({ medias, duration, territories, exclusive }: AvailsFilter,
-  terms: Term<Date>[]): boolean[] {
-  const results = []
+  terms: Term<Date>[]): Term<Timestamp>[] {
+  const occupiedTerms = []
   for (const term of terms) {
     if (exclusive) {
-      if (duration.from.getTime() < term.duration.from.getTime() && duration.to.getTime() > term.duration.to.getTime()) {
+      if (duration.from.getTime() > term.duration.from.getTime() && duration.to.getTime() < term.duration.to.getTime()) {
+
         if (medias.every(media => term.medias.includes(media))) {
           if (territories.every(territory => term.territories.includes(territory))) {
-            results.push(true)
+
+            occupiedTerms.push(term);
           }
         }
       }
     } else {
       if (term.exclusive) {
-        if (duration.from.getTime() < term.duration.from.getTime() && duration.to.getTime() > term.duration.to.getTime()) {
+        if (duration.from.getTime() > term.duration.from.getTime() && duration.to.getTime() < term.duration.to.getTime()) {
+
           if (medias.every(media => term.medias.includes(media))) {
+
             if (territories.every(territory => term.territories.includes(territory))) {
-              results.push(true)
+
+              occupiedTerms.push(term);
             }
           }
         }
       }
-      else {
-        results.push(true)
-      }
     }
-    results.push(false);
   }
-  return results
+  return occupiedTerms
 }
 
-/* If a buyer wants to buy rights on a movie, check if AC has the right on this specificaiton.
-If a buyer wants to buy a non exclusive right on a movie, we just need to check if there are already exclusive
-rights for that very specification that the buyer specified.
-if a buyer wants to buy a exclusive right, we need to check for previously sales that are made on the movie,
-if already an exclusive right or a non exclusive right exists, you can't sell the right to him, since it is already taken
-guess we won't handle any buy back rights for V0
-We don’t look at exclusivity on mandates and assume it’s exclusive for AC */
+export function filterSales({ medias, duration, territories, exclusive }: AvailsFilter,
+  terms: Term<Date>[]): boolean {
+  const hasOnGoingSales = []
+  for (const term of terms) {
+
+    const isInTimeSpan = duration.from.getTime() > term.duration.from.getTime()
+      && duration.to.getTime() < term.duration.to.getTime();
+
+    const hasMedia = medias.every(media => term.medias.includes(media))
+
+    const hasTerritories = territories.every(territory => term.territories.includes(territory));
+
+    if (isInTimeSpan && hasMedia && hasTerritories && exclusive) {
+      hasOnGoingSales.push(true)
+    } else if (isInTimeSpan && hasMedia && hasTerritories && term.exclusive) {
+      hasOnGoingSales.push(true)
+    }
+  }
+  if(!hasOnGoingSales.length) hasOnGoingSales.push(false)
+  return !hasOnGoingSales.every(x => x);
+}
