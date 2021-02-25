8a6268044f728b1667d1d56866717ab372f46240
fritzschoff <max.j.f@gmx.de>
2021-02-25 14:06:26 +0100

    updated features

--- libs/movie/src/lib/movie/components/title-features/title-features.component.ts
+++ libs/movie/src/lib/movie/components/title-features/title-features.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import { Movie } from '@blockframes/movie/+state';
-import { productionStatus, contentType, getISO3166TerritoryFromSlug, languages } from '@blockframes/utils/static-model';
+import { productionStatus, contentType, getISO3166TerritoryFromSlug, languages, genres as staticGenres } from '@blockframes/utils/static-model';
 import { formatRunningTime } from "@blockframes/movie/pipes/running-time.pipe";
 
 interface TitleFeature {
@@ -9,24 +9,29 @@ interface TitleFeature {
 }
 
 function createTitleFeatureView(movie: Movie): TitleFeature[] {
-  const { genres, runningTime, originalLanguages, originCountries, stakeholders } = movie;
-  const convertedGenres = genres.map(genre => genres[genre]);
-  const convertedRunTime = formatRunningTime(runningTime);
+  const { genres, runningTime, originalLanguages, originCountries } = movie;
+  const isSeriesOrDocumentary = movie.contentType === ('series' || 'documentary')
+  const convertedGenres = genres.map(genre => staticGenres[genre]);
+  const convertedRunTime = isSeriesOrDocumentary ? [formatRunningTime(runningTime), 'Per Episode'] : formatRunningTime(runningTime);
   const convertedOriginalLanguages = originalLanguages.map(language => languages[language]);
   const convertedOriginCountries = originCountries.map(country => getISO3166TerritoryFromSlug(country)).map(country => country.iso_a2);
   const statusLabel = productionStatus[movie.productionStatus];
+  const season = `Season ${movie.title.series || 'TBA'}`;
+  const episodeCount = `${runningTime.episodeCount} episodes`
   const features = [
     contentType[movie.contentType],
     convertedRunTime,
     convertedGenres,
     convertedOriginCountries,
     convertedOriginalLanguages,
+    season,
+    episodeCount,
     statusLabel
   ]
   return features.map(feature => {
-    /* If array, show the first value and the tooltip gets the string version of it */
+    /* If feature is an array, take the first value for displaying it and the rest for the tooltip */
     if (Array.isArray(feature)) {
-      return { view: feature[0], tooltip: feature.length > 1 ? feature.join(', ') : '' }
+      return { view: feature[0], tooltip: feature.length > 1 ? feature.slice(1, features.length).join(', ') : '' }
     }
     return { view: feature, tooltip: '' }
   })
