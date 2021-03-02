a8a8b985561e75ba1093dd44ef7cd40e3daaa260
bruce <delorme.bruce.michel@gmail.com>
2021-03-02 17:31:24 +0100

    fixed bad indent

--- libs/utils/src/lib/algolia/algolia.service.ts
+++ libs/utils/src/lib/algolia/algolia.service.ts
@@ -9,32 +9,32 @@ import { parseFilters, parseFacets } from './helper.utils';
 @Injectable({ providedIn: 'root' })
 export class AlgoliaService {
 
-    private indices: Record<string, SearchIndex> = {};
+  private indices: Record<string, SearchIndex> = {};
 
-    private appName: App;
+  private appName: App;
 
-    constructor(private routerQuery: RouterQuery) {
-        this.appName = getCurrentApp(this.routerQuery);
+  constructor(private routerQuery: RouterQuery) {
+    this.appName = getCurrentApp(this.routerQuery);
+  }
+
+  getIndex(name: 'movie' | 'org' | 'user'): SearchIndex {
+    if (!this.indices[name]) {
+      if (name === 'user') {
+        this.indices[name] = algoliasearch(algolia.appId, algolia.searchKey).initIndex(algoliaIndex[name]);
+      } else {
+        this.indices[name] = algoliasearch(algolia.appId, algolia.searchKey).initIndex(algoliaIndex[name][this.appName]);
+      }
     }
+    return this.indices[name];
 
-    getIndex(name: 'movie' | 'org' | 'user'): SearchIndex {
-        if (!this.indices[name]) {
-            if (name === 'user') {
-                this.indices[name] = algoliasearch(algolia.appId, algolia.searchKey).initIndex(algoliaIndex[name]);
-            } else {
-                this.indices[name] = algoliasearch(algolia.appId, algolia.searchKey).initIndex(algoliaIndex[name][this.appName]);
-            }
-        }
-        return this.indices[name];
+  }
 
-    }
-
-    query<K extends keyof AlgoliaQueries>(name: K, config: AlgoliaQueries[K]): Promise<SearchResponse<AlgoliaObject[K]>> {
-        return this.getIndex(name).search<AlgoliaObject[K]>(config.text ?? '', {
-            hitsPerPage: config.limitResultsTo,
-            page: config.activePage,
-            facetFilters: parseFacets(config.facets),
-            filters: parseFilters(config.filters)
-        }).then(e => e);
-    }
+  query<K extends keyof AlgoliaQueries>(name: K, config: AlgoliaQueries[K]): Promise<SearchResponse<AlgoliaObject[K]>> {
+    return this.getIndex(name).search<AlgoliaObject[K]>(config.text ?? '', {
+      hitsPerPage: config.limitResultsTo,
+      page: config.activePage,
+      facetFilters: parseFacets(config.facets),
+      filters: parseFilters(config.filters)
+    }).then(e => e);
+  }
 }
