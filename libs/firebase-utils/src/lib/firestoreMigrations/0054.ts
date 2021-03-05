56f359573d11ec0d8524922e0cd599b9cfe89b88
bruce <delorme.bruce.michel@gmail.com>
2021-03-05 16:07:37 +0100

    typo

--- libs/firebase-utils/src/lib/firestoreMigrations/0054.ts
+++ libs/firebase-utils/src/lib/firestoreMigrations/0054.ts
@@ -5,7 +5,7 @@ import { privacies } from '@blockframes/utils/file-sanitizer';
 
 function createStorageFile(data: StorageFile) {
   if (!!data.ref) delete data.ref;
-  if (data.storagePath === 'object') {
+  if (typeof data.storagePath === 'object') {
     data.storagePath = ''
     console.log(`Invalid value in storagePath for : ${data.docId} ${data.collection}`);
     return data;
