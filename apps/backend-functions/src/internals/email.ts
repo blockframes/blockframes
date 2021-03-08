d88bae059b1b4547ae92888cd55780b22cd7211d
Mussuranae <mandonnetdev@outlook.com>
2021-03-08 18:43:57 +0100

    remove TODO

--- apps/backend-functions/src/internals/email.ts
+++ apps/backend-functions/src/internals/email.ts
@@ -28,7 +28,6 @@ const groupsToDisplay = [unsubscribeGroupIds.allExceptCriticals];
  *
  * Handles development mode: logs a warning when no sendgrid API key is provided.
  */
-// TODO #4710 Define which template is critical and pass the id to the function when it's called
 export async function sendMail({ to, subject, text }: EmailRequest, from: EmailJSON = getSendgridFrom(), groupId: number = criticalsEmailsGroupId): Promise<any> {
   const msg: MailDataRequired = {
     from,
@@ -42,7 +41,6 @@ export async function sendMail({ to, subject, text }: EmailRequest, from: EmailJ
   return send(msg);
 }
 
-// TODO #4710 Define which template is critical and pass the id to the function when it's called
 export function sendMailFromTemplate({ to, templateId, data }: EmailTemplateRequest, app: App, groupId: number = criticalsEmailsGroupId): Promise<any> {
   const from: EmailJSON = getSendgridFrom(app);
   const { label } = getAppName(app);
