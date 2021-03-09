6c920f2929371bddb1db82035ebf602c6156c475
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2021-03-09 16:51:31 +0100

    start subscribing on buckets

--- apps/catalog/catalog/src/app/marketplace/marketplace.component.ts
+++ apps/catalog/catalog/src/app/marketplace/marketplace.component.ts
@@ -1,9 +1,8 @@
 import { Component, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
 import { OrganizationQuery } from '@blockframes/organization/+state';
-import { MarketplaceQuery } from './+state/marketplace.query';
 import { BucketQuery, BucketService } from '@blockframes/contract/bucket/+state';
-import { Subscription } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Subscription, Observable } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'catalog-marketplace',
@@ -14,22 +13,24 @@ import { switchMap } from 'rxjs/operators';
 
 export class MarketplaceComponent implements OnDestroy {
   private sub: Subscription;
-  public cartCount$ = this.marketplaceQuery.selectCount();
+  public contractCount$: Observable<number>;
   public canAccessDeals = !this.orgQuery.getActive().appAccess.catalog.dashboard;
   public org$ = this.orgQuery.selectActive();
 
   constructor(
     private bucketQuery: BucketQuery,
     private bucketService: BucketService,
-    private marketplaceQuery: MarketplaceQuery,
     private orgQuery: OrganizationQuery,
   ) {
-    // this.sub = this.orgQuery.selectActiveId().pipe(
-    //   switchMap(id => this.bucketService.syncActive({ id }))
-    // ).subscribe();
+    this.sub = this.orgQuery.selectActiveId().pipe(
+      switchMap(id => this.bucketService.syncActive({ id }))
+    ).subscribe();
+    this.contractCount$ = this.bucketQuery.selectActive().pipe(
+      map(bucket => bucket?.contracts.length ?? 0)
+    );
   }
 
   ngOnDestroy() {
-    // this.sub.unsubscribe();
+    this.sub.unsubscribe();
   }
 }
