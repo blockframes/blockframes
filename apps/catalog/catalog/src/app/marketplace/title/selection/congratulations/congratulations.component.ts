ab95795fd6c763ac9a68072d7b23cf23501de04a
Remco <RemcoSimonides@gmail.com>
2021-03-12 16:07:20 +0100

    5005

---
+++ apps/catalog/catalog/src/app/marketplace/title/selection/congratulations/congratulations.component.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component, Optional } from '@angular/core';
+import { Intercom } from 'ng-intercom';
+
+@Component({
+  selector: 'catalog-congratulations',
+  templateUrl: 'congratulations.component.html',
+  styleUrls: ['./congratulations.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CongratulationsComponent {
+  
+  constructor(
+    @Optional() private intercom: Intercom
+  ) { }
+
+  public openIntercom() {
+    return this.intercom.show();
+  }
+}
