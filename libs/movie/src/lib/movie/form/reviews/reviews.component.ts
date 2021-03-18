c4b818bc1b63750b153c3d1dfbca631c172d0201
samir <samir.touira@epitech.eu>
2021-03-18 20:55:06 +0100

    small change

--- libs/movie/src/lib/movie/form/reviews/reviews.component.ts
+++ libs/movie/src/lib/movie/form/reviews/reviews.component.ts
@@ -25,7 +25,6 @@ export class MovieFormReviewsComponent {
   }
 
   onClick(group: MatButtonToggleGroup, form: FormControl) {
-    console.log(group.value);
     if (!this.toggleChanged) group.value = '';
     form.setValue(group.value);
     this.toggleChanged = false;
