a93d0b374a31c73c73464194a0922fba5c05f30b
Remco <RemcoSimonides@gmail.com>
2021-03-18 16:49:49 +0100

    5286

--- apps/catalog/catalog/src/app/marketplace/title/selection/selection.module.ts
+++ apps/catalog/catalog/src/app/marketplace/title/selection/selection.module.ts
@@ -19,6 +19,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [MarketplaceSelectionComponent],
@@ -42,6 +43,7 @@ import { MatSelectModule } from '@angular/material/select';
     MatFormFieldModule,
     MatIconModule,
     MatSelectModule,
+    MatSnackBarModule,
 
     RouterModule.forChild([
       {
