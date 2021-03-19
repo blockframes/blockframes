41d4c1e1d277abb8a588fd4cfa40f8aed6ea236b
fritzschoff <max.j.f@gmx.de>
2021-03-19 14:12:17 +0100

    new tests

--- libs/contract/src/lib/avails/avails.spec.ts
+++ libs/contract/src/lib/avails/avails.spec.ts
@@ -208,6 +208,253 @@ describe('isTermSold', () => {
         bucket.push({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] });
         expect(isInBucket({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] }, bucket)).toBe(true)
     })
+
+    it(`Mandate test (terms)
+    Terms: 01/01/2028 - 06/30/2036
+    Territory: Afghanistan
+    Rights: Free TV
+    Exclusive: No
+    Expected results:
+    Not licensed: Resurrected, Gaza mon amour (mandate ended)
+    Not available: Mother Schmuckers, 512 Hours With Marina Abramovic (sale)
+    Available: Bigfoot Family`, () => {
+        const ACRights = getMandateTerm(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            mandateTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(!!ACRights).toBe(true);
+        const isTermSold = isSold(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            saleTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(isTermSold).toBe(true);
+        bucket.push({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] });
+        expect(isInBucket({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] }, bucket)).toBe(true)
+    })
+
+    it(`Mandate test (terms)
+    Terms: 01/01/2028 - 06/30/2036
+    Territory: Afghanistan
+    Rights: Free TV
+    Exclusive: No
+    Expected results:
+    Not licensed: Resurrected, Gaza mon amour (mandate ended)
+    Not available: Mother Schmuckers, 512 Hours With Marina Abramovic (sale)
+    Available: Bigfoot Family`, () => {
+        const ACRights = getMandateTerm(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            mandateTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(!!ACRights).toBe(true);
+        const isTermSold = isSold(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            saleTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(isTermSold).toBe(true);
+        bucket.push({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] });
+        expect(isInBucket({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] }, bucket)).toBe(true)
+    })
+
+    it(`Mandate test (rights)
+    Terms: 01/01/2028 - 06/30/2030
+    Territory: France
+    Rights: Planes
+    Exclusive: No
+    Expected results:
+    Not licensed: Resurrected (right not licensed)
+    Not available: Mother Schmuckers (sale)
+    Available: Bigfoot Family, 512 Hours With Marina Abramovic, Gaza mon amour`, () => {
+        const ACRights = getMandateTerm(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            mandateTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(!!ACRights).toBe(true);
+        const isTermSold = isSold(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            saleTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(isTermSold).toBe(true);
+        bucket.push({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] });
+        expect(isInBucket({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] }, bucket)).toBe(true)
+    })
+
+    it(`Terms check (existing ended sale)
+    Terms: 01/01/2033 - 06/30/2033
+    Territory: Germany, Russia, Czech Republic
+    Rights: Free TV
+    Exclusive: Yes
+    Expected results:
+    Not available: Mother Schmuckers (non exclusive German sale), Gaza mon amour (non exclusive Russian sale)
+    Available: Resurrected, Bigfoot Family, 512 Hours With Marina Abramovic`, () => {
+        const ACRights = getMandateTerm(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            mandateTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(!!ACRights).toBe(true);
+        const isTermSold = isSold(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            saleTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(isTermSold).toBe(true);
+        bucket.push({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] });
+        expect(isInBucket({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] }, bucket)).toBe(true)
+    })
+
+
+    it(`Terms check (existing ongoing sale)
+     Terms: 01/01/2029 - 06/30/2031
+    Territory: Germany, Russia, Czech Republic
+    Rights: Free TV
+    Exclusive: Yes
+    Expected results:
+    Not available: Resurrected (existing ongoing sale)
+    Available: Bigfoot Family, 512 Hours With Marina Abramovic, Gaza mon amour, Mother Schmuckers`, () => {
+        const ACRights = getMandateTerm(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            mandateTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(!!ACRights).toBe(true);
+        const isTermSold = isSold(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            saleTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(isTermSold).toBe(true);
+        bucket.push({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] });
+        expect(isInBucket({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] }, bucket)).toBe(true)
+    })
+
+
+    it(`Cross territory check + exclusivity
+    Terms: 01/01/2022 - 06/30/2022
+    Territory: Germany, Russia, Czech Republic
+    Rights: Free TV
+    Exclusive: No
+    Expected results:
+    Not available: Bigfoot Family (exclu sale), 512 Hours With Marina Abramovic (exclu sale)
+    Available: Gaza mon amour (non exclusive sale), Mother Schmuckers, Resurrected`, () => {
+        const ACRights = getMandateTerm(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            mandateTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(!!ACRights).toBe(true);
+        const isTermSold = isSold(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            saleTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(isTermSold).toBe(true);
+        bucket.push({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] });
+        expect(isInBucket({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] }, bucket)).toBe(true)
+    })
+
+
+    it(`Cross territory check + exclusivity
+    Terms: 01/01/2022 - 06/30/2022
+    Territory: Germany, Russia, Czech Republic
+    Rights: Free TV
+    Exclusive: Yes
+    Expected results:
+    Not available: Resurrected (non exclu sale), Bigfoot Family (exclu sale), 512 Hours With Marina Abramovic (exclu sale), Gaza mon amour (non exclusive sale)
+    Available: Mother Schmuckers`, () => {
+        const ACRights = getMandateTerm(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            mandateTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(!!ACRights).toBe(true);
+        const isTermSold = isSold(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            saleTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(isTermSold).toBe(true);
+        bucket.push({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] });
+        expect(isInBucket({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] }, bucket)).toBe(true)
+    })
+
+    it(`Rights check
+    Terms: 01/01/2022 - 06/30/2022
+    Territory: Argentina
+    Rights: S-VOD
+    Exclusive: Yes
+    Expected results:
+    Not available: Bigfoot Family (svod sale),
+    Available: Mother Schmuckers, Resurrected, 512 Hours With Marina Abramovic, Gaza mon amour`, () => {
+        const ACRights = getMandateTerm(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            mandateTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(!!ACRights).toBe(true);
+        const isTermSold = isSold(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            saleTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(isTermSold).toBe(true);
+        bucket.push({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] });
+        expect(isInBucket({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] }, bucket)).toBe(true)
+    })
+
+    it(`Rights check
+    Terms: 01/01/2022 - 06/30/2022
+    Territory: Argentina
+    Rights: Pay TV
+    Exclusive: Yes
+    Expected results:
+    Not available: Resurrected (sale),
+    Available: Mother Schmuckers, 512 Hours With Marina Abramovic, Gaza mon amour, Bigfoot Family`, () => {
+        const ACRights = getMandateTerm(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            mandateTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(!!ACRights).toBe(true);
+        const isTermSold = isSold(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            saleTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(isTermSold).toBe(true);
+        bucket.push({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] });
+        expect(isInBucket({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] }, bucket)).toBe(true)
+    })
+
+    it(`Rights check
+    Terms: 01/01/2022 - 06/30/2022
+    Territory: Argentina
+    Rights: Pay TV
+    Exclusive: Yes
+    Expected results:
+    Not available: Resurrected (sale),
+    Available: Mother Schmuckers, 512 Hours With Marina Abramovic, Gaza mon amour, Bigfoot Family`, () => {
+        const ACRights = getMandateTerm(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            mandateTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(!!ACRights).toBe(true);
+        const isTermSold = isSold(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            saleTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(isTermSold).toBe(true);
+        bucket.push({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] });
+        expect(isInBucket({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] }, bucket)).toBe(true)
+    })
+
+    it(`Exclusivity test (non exclusive)
+    Terms: 01/01/2022 - 06/30/2022
+    Territory: Germany
+    Rights: Free TV
+    Exclusive: No
+    Expected results:
+    Not available: 512 Hours With Marina Abramovic, Bigfoot Family (exclusive sales)
+    Available: Mother Schmuckers, , Gaza mon amour, Resurrected`, () => {
+        const ACRights = getMandateTerm(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            mandateTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(!!ACRights).toBe(true);
+        const isTermSold = isSold(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            saleTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(isTermSold).toBe(true);
+        bucket.push({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] });
+        expect(isInBucket({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] }, bucket)).toBe(true)
+    })
+
+
+    it(`Exclusivity test (exclusive)
+    Terms: 01/01/2020 - 06/30/2020
+    Territory: Canada
+    Rights: Free TV
+    Exclusive: Yes
+    Expected results:
+    Not available: Resurrected
+    Available: Mother Schmuckers, Gaza mon amour, 512 Hours With Marina Abramovic, Bigfoot Family`, () => {
+        const ACRights = getMandateTerm(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            mandateTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(!!ACRights).toBe(true);
+        const isTermSold = isSold(
+            { duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] },
+            saleTerms.filter(sale => sale.titleId === GazaMonAmour));
+        expect(isTermSold).toBe(true);
+        bucket.push({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] });
+        expect(isInBucket({ duration: { to: new Date('06/30/2023'), from: new Date('01/01/2022') }, exclusive: false, territories: ['south-korea'], medias: ['freeTv'] }, bucket)).toBe(true)
+    })
 })
 
 function toDate(term: Term<Date>): Term<Date> {
