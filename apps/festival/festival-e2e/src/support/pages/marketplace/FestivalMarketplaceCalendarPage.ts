f07a45d7fbfa877c9eb8666bb314b2d715c53742
xmano <mano@xkanda.com>
2021-03-23 14:40:34 +0530

    Fix make event-card to be available in a better way.

--- apps/festival/festival-e2e/src/support/pages/marketplace/FestivalMarketplaceCalendarPage.ts
+++ apps/festival/festival-e2e/src/support/pages/marketplace/FestivalMarketplaceCalendarPage.ts
@@ -3,14 +3,15 @@ import { SEC } from '@blockframes/e2e/utils';
 
 export default class FestivalMarketplaceCalendarPage {
   constructor() {
-    cy.get('festival-event-calendar')
+    cy.get('festival-event-calendar', {timeout: 30 * SEC});
+    cy.wait(5 * SEC);
   }
 
   clickOnEvent(movieTitle: string) {
     cy.get('festival-marketplace main', {timeout: 3 * SEC})
       .scrollTo('top');
-    cy.wait(1 * SEC);
-    cy.get('festival-event-calendar event-card h5[test-id=movie-title]')
+    cy.wait(2 * SEC);
+    cy.get('festival-event-calendar event-card h5[test-id=movie-title]', {timeout: 30 * SEC})
       .contains(movieTitle).click();
     return new FestivalMarketplaceEventPage();
   }
