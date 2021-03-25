0aa82769e11d9ea6b7230b32d45696e025f49636
xmano <mano@xkanda.com>
2021-03-25 22:10:27 +0530

    Add permission doc read test

--- apps/rules/src/tests/firestore/permission.spec.ts
+++ apps/rules/src/tests/firestore/permission.spec.ts
@@ -1,9 +1,8 @@
-﻿import { apps } from '@firebase/rules-unit-testing';
+﻿import { apps, assertSucceeds } from '@firebase/rules-unit-testing';
 import { testFixture } from './fixtures/data';
 import { Firestore, initFirestoreApp } from '@blockframes/testing/firebase/functions';
 
 
-//TODO: 4198
 describe('Permission Rules Tests', () => {
   const projectId = `permrules-spec-${Date.now()}`;
   let db: Firestore;
@@ -15,16 +14,35 @@ describe('Permission Rules Tests', () => {
 
     afterAll(() => Promise.all(apps().map((app) => app.delete())));
 
-    test('test', async () => {});
+    test('user with member role should be able to read document', async () => {
+      const permissionDocRef = db.doc('permissions/O001');
+      await assertSucceeds(permissionDocRef.get());
+    });
   });
 
   describe('With User as Org Admin', () => {
     beforeAll(async () => {
-      db = await initFirestoreApp(projectId, 'firestore.rules', testFixture, { uid: 'uid-user2' });
+      db = await initFirestoreApp(projectId, 'firestore.rules', testFixture, { uid: 'uid-admin' });
     });
 
     afterAll(() => Promise.all(apps().map((app) => app.delete())));
 
-    test('test', async () => {});
+    test('user with admin role should be able to read document', async () => {
+      const permissionDocRef = db.doc('permissions/O001');
+      await assertSucceeds(permissionDocRef.get());
+    });
   });
+
+  describe('With User as Super Admin', () => {
+    beforeAll(async () => {
+      db = await initFirestoreApp(projectId, 'firestore.rules', testFixture, { uid: 'uid-sAdmin' });
+    });
+
+    afterAll(() => Promise.all(apps().map((app) => app.delete())));
+
+    test('user with superadmin role should be able to read document', async () => {
+      const permissionDocRef = db.doc('permissions/O001');
+      await assertSucceeds(permissionDocRef.get());
+    });
+  });  
 });
