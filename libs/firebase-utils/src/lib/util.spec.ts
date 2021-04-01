d31d27880fbd39c771c94ba0783a6d49a0f1deb4
xmano <mano@xkanda.com>
2021-04-01 15:53:28 +0530

    Remove skip to enable test to run.

--- libs/firebase-utils/src/lib/util.spec.ts
+++ libs/firebase-utils/src/lib/util.spec.ts
@@ -1,7 +1,6 @@
 import * as util from './util'
 
-//TODO: 4838. Fix failing test
-describe.skip('Test util.ts generator function getCollectionInBatches', () => {
+describe('Test util.ts generator function getCollectionInBatches', () => {
   const { db } = util.loadAdminServices()
   it('should return batches of the correct size', async () => {
     const usersRef = db.collection('users')
