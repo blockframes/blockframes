40a1feee0109dca945d552a8cf532a74963cc187
xmano <mano@xkanda.com>
2021-04-05 15:27:36 +0530

    Organization rules unit-tests - read, delete working tests

--- apps/rules/src/tests/firestore/organization.spec.ts
+++ apps/rules/src/tests/firestore/organization.spec.ts
@@ -1,17 +1,77 @@
-﻿import { apps } from '@firebase/rules-unit-testing';
+﻿import { apps, assertFails, assertSucceeds } from '@firebase/rules-unit-testing';
 import { testFixture } from './fixtures/data';
 import { Firestore, initFirestoreApp } from '@blockframes/testing/firebase/functions';
 
-//TODO: 4197
 describe('Organization Rules Tests', () => {
   const projectId = `orgrules-spec-${Date.now()}`;
   let db: Firestore;
 
-  beforeAll(async () => {
-    db = await initFirestoreApp(projectId, 'firestore.rules', testFixture, { uid: 'uid-user2' });
+  describe('With User as Org Member', () => {
+    beforeAll(async () => {
+      db = await initFirestoreApp(projectId, 'firestore.rules', testFixture, { uid: 'uid-user2' });
+    });
+
+    afterAll(() => Promise.all(apps().map((app) => app.delete())));
+
+    test('should be able to read document', async () => {
+      const orgDocRef = db.doc('orgs/O001');
+      await assertSucceeds(orgDocRef.get());
+    });
+
+    test('should not be able to delete document', async () => {
+      const orgDocRef = db.doc('orgs/O001');
+      await assertFails(orgDocRef.delete());
+    });
   });
 
-  afterAll(() => Promise.all(apps().map((app) => app.delete())));
+  describe('With User as Org Admin', () => {
+    beforeAll(async () => {
+      db = await initFirestoreApp(projectId, 'firestore.rules', testFixture, { uid: 'uid-user2' });
+    });
 
-  test('test', async () => {});
+    afterAll(() => Promise.all(apps().map((app) => app.delete())));
+
+    test('should be able to read document', async () => {
+      const orgDocRef = db.doc('orgs/O001');
+      await assertSucceeds(orgDocRef.get());
+    });
+
+    test('should not be able to delete document', async () => {
+      const orgDocRef = db.doc('orgs/O001');
+      await assertFails(orgDocRef.delete());
+    });
+  });
+});
+
+
+//Carts sub collection
+describe('Carts Sub Collection Rules Tests', () => {
+  const projectId = `doc-permrules-spec-${Date.now()}`;
+  let db: Firestore;
+
+  describe('With User as Org Member', () => {
+    beforeAll(async () => {
+      db = await initFirestoreApp(projectId, 'firestore.rules', testFixture, { uid: 'uid-user2' });
+    });
+
+    afterAll(() => Promise.all(apps().map((app) => app.delete())));
+
+    test('should be able to read document', async () => {
+      const cartDocRef = db.doc('orgs/O001/carts/C001');
+      await assertSucceeds(cartDocRef.get());
+    });
+  });
+
+  describe('With User as Org Admin', () => {
+    beforeAll(async () => {
+      db = await initFirestoreApp(projectId, 'firestore.rules', testFixture, { uid: 'uid-user2' });
+    });
+
+    afterAll(() => Promise.all(apps().map((app) => app.delete())));
+
+    test('should be able to read document', async () => {
+      const cartDocRef = db.doc('orgs/O001/carts/C001');
+      await assertSucceeds(cartDocRef.get());
+    });
+  });
 });
