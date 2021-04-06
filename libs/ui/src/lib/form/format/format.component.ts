ba12f88784291f90ca5d010ab1e32923a874a01f
fritzschoff <max.j.f@gmx.de>
2021-04-06 13:17:22 +0200

    added new genre

--- libs/ui/src/lib/form/format/format.component.ts
+++ libs/ui/src/lib/form/format/format.component.ts
@@ -19,7 +19,6 @@ export class FormFormatComponent implements OnInit {
 
   ngOnInit() {
     this.contentTypes = (Object.keys(contentType) as ContentType[])
-      .filter(c => !['volume', 'episode', 'collection'].includes(c));
 
     this.filteredTypes$ = this.form.valueChanges
       .pipe(
