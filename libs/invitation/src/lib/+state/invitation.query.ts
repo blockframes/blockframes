f1f638b2e29b4492ad79789e44008a1e8f2ef27d
Remco <RemcoSimonides@gmail.com>
2021-04-08 16:13:02 +0200

    5486 Rework to service in order to prevent flashes

--- libs/invitation/src/lib/+state/invitation.query.ts
+++ libs/invitation/src/lib/+state/invitation.query.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { QueryEntity, QueryConfig, Order } from '@datorama/akita';
 import { InvitationStore, InvitationState } from './invitation.store';
-import { Invitation } from './invitation.model';
 import { AuthQuery } from '@blockframes/auth/+state';
 
 @Injectable({ providedIn: 'root' })
@@ -10,36 +9,5 @@ export class InvitationQuery extends QueryEntity<InvitationState> {
 
   constructor(protected store: InvitationStore, private authQuery: AuthQuery) {
     super(store);
-  }
-
-  isFromMe(invitation: Invitation) {
-    const user = this.authQuery.user;
-    return invitation.fromOrg?.id === user.orgId || invitation.fromUser?.uid === user.uid;
-  }
-
-  isToMe(invitation: Invitation) {
-    const user = this.authQuery.user;
-    return invitation.toOrg?.id === user.orgId || invitation.toUser?.uid === user.uid;
-  }
-
-  currentUserIsGuest(invitation: Invitation) {
-    const user = this.authQuery.user;
-    return (invitation.mode === 'request' && invitation.fromUser.uid === user.uid) ||
-    (invitation.mode === 'invitation' && invitation.toUser.uid === user.uid)
-  }
-
-  /** Query all invitation where current user is a guest */
-  whereCurrentUserIsGuest(filter: (invitation: Invitation) => boolean = () => true) {
-    return this.selectAll({ filterBy: i => this.currentUserIsGuest(i) && filter(i) });
-  }
-
-  /** Query all invitation from current user / org */
-  fromMe(filter: (invitation: Invitation) => boolean = () => true) {
-    return this.selectAll({ filterBy: i => this.isFromMe(i) && filter(i) });
-  }
-
-  /** Query all invitation to current user / org */
-  toMe(filter: (invitation: Invitation) => boolean = () => true) {
-    return this.selectAll({ filterBy: i => this.isToMe(i) && filter(i) });
   }
 }
