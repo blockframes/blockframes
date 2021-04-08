0d4308b23c7bbf961196a8472b92e2d1ef1f5f65
fritzschoff <max.j.f@gmx.de>
2021-04-08 14:05:09 +0200

    updated e2e test

--- apps/festival/festival-e2e/src/support/pages/dashboard/movietunnel/TunnelMainPage.ts
+++ apps/festival/festival-e2e/src/support/pages/dashboard/movietunnel/TunnelMainPage.ts
@@ -88,12 +88,12 @@ export default class TunnelMainPage {
 
   // Country of origin
   public selectCountry(partialCountry: string, country: string) {
-    cy.get('catalog-movie-tunnel-main movie-form-original-releases form-country input').type(partialCountry);
+    cy.get('catalog-movie-tunnel-main movie-form-original-releases static-select input').type(partialCountry);
     cy.get('mat-option').contains(country).click();
   }
 
   public assertCountryIsSelected(country: string) {
-    cy.get('catalog-movie-tunnel-main movie-form-original-releases form-country input').should('have.value', country);
+    cy.get('catalog-movie-tunnel-main movie-form-original-releases static-select input').should('have.value', country);
   }
 
   public selectMedia(media: string) {
