6020d0a98cde1978073c308cca4385ee1a67756e
xmano <mano@xkanda.com>
2021-04-21 21:37:17 +0530

    Added test to upload a file

--- apps/catalog/catalog-e2e/src/integration/dashboard/import.spec.ts
+++ apps/catalog/catalog-e2e/src/integration/dashboard/import.spec.ts
@@ -11,16 +11,52 @@ import { SEC } from "@blockframes/e2e/utils/env";
 const userFixture = new User();
 const users  =  [ userFixture.getByUID(USER.Hettie) ];
 
+const movieFixture = 'movie1.xlsx';
 
 describe('User can fill and save contract tunnel form', () => {
   beforeEach(() => {
-    clearDataAndPrepareTest('c/o/dashboard/home');
+    clearDataAndPrepareTest('c/o/dashboard/import');
     //cy.visit('/');
     //const p1 = new LandingPage();
     //p1.clickLogin();
   });
   
-  it('Login as admin, Select contracts and import ', () => {
+  it('Login as admin, Select Movies and import ', () => {
+    // Connexion
+    //const p2: AuthLoginPage = new AuthLoginPage();
+    //p2.fillSignin(users[0]);
+    //p2.clickSignIn();
+    //const p3 = new HomePage();
+
+    // Navigate to tunnel-contract
+    //TODO: Check if we are in marketplace and switch to dashboard
+
+    //cy.get('a[test-id="import-titles"]', {timeout: 30 * SEC})
+    //  .click();
+
+    cy.wait(1 * SEC);
+
+    cy.log("Select movie type to upload");
+    cy.get('mat-form-field', {timeout: 30 * SEC})
+      .click();
+    
+    cy.get('mat-option')
+      .contains("Movies")
+      .click();
+      
+    //Import the Movie file here: filePicker
+    cy.log("Start upload by attaching the fixture");
+    cy.get('#filePicker', {timeout: 10 * SEC})
+      .attachFile(movieFixture);
+
+    cy.get('[test-id="status-import"]', { timeout: 30 * SEC })
+      .should('be.visible')
+      .should('contain', 'File uploaded');
+
+    cy.log("Movies uploaded successfully");
+  });
+
+  it.skip('Login as admin, Select contracts and import ', () => {
     // Connexion
     //const p2: AuthLoginPage = new AuthLoginPage();
     //p2.fillSignin(users[0]);
