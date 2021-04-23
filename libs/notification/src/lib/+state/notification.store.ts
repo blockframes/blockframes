3ccf3a3c3c9f7888c5c6dcabf61418eb0b95b608
Remco <RemcoSimonides@gmail.com>
2021-04-23 15:51:10 +0200

    5356 notification improvements

--- libs/notification/src/lib/+state/notification.store.ts
+++ libs/notification/src/lib/+state/notification.store.ts
@@ -128,7 +128,7 @@ export class NotificationStore extends EntityStore<NotificationState, Notificati
         };
       case 'orgAppAccessChanged':
         const message = !!notification.appAccess
-          ? `Your organization has now access to ${notification.appAccess}`
+          ? `Your organization has now access to ${appName[notification.appAccess]}`
           : 'Your organization\'s app access have changed.'
         return {
           _meta: { ...notification._meta, createdAt: toDate(notification._meta.createdAt) },
