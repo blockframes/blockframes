3b6c2f1fa0da88a6133ddd8a2793795eccfecc7b
Bruce <eelios@gmail.com>
2021-04-28 17:08:57 +0200

    cleaning and avail tab back (#5660)
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>

--- apps/catalog/catalog/src/app/marketplace/title/avails/avails.component.ts
+++ apps/catalog/catalog/src/app/marketplace/title/avails/avails.component.ts
@@ -1,9 +1,12 @@
 import { MovieQuery, Movie } from '@blockframes/movie/+state';
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { MarketplaceStore, MarketplaceQuery } from '../../+state';
-import { TerritoryValue, Territory } from '@blockframes/utils/static-model';
+import { TerritoryValue, TerritoryISOA3Value } from '@blockframes/utils/static-model';
 import { territories, territoriesISOA3 } from '@blockframes/utils/static-model';
+
+interface TerritoryMarker {
+  isoA3: TerritoryISOA3Value,
+  label: TerritoryValue
+}
 
 @Component({
   selector: 'catalog-movie-avails',
@@ -12,14 +15,12 @@ import { territories, territoriesISOA3 } from '@blockframes/utils/static-model';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MarketplaceMovieAvailsComponent {
-  public availsForm
   public movie: Movie = this.movieQuery.getActive();
-  public territories = territories;
 
   /** List of world map territories */
-  public notLicensedTerritories: Territory[] = [];
-  public rightsSoldTerritories: Territory[] = [];
-  public availableTerritories: Territory[] = [];
+  public notLicensedTerritories: TerritoryMarker[] = [];
+  public rightsSoldTerritories: TerritoryMarker[] = [];
+  public availableTerritories: TerritoryMarker[] = [];
 
   public hoveredTerritory: {
     name: string;
@@ -28,58 +29,30 @@ export class MarketplaceMovieAvailsComponent {
 
   constructor(
     private movieQuery: MovieQuery,
-    private marketplaceStore: MarketplaceStore,
-    private marketplaceQuery: MarketplaceQuery,
-    private snackBar: MatSnackBar
-  ) { }
+  ) {
+    /**
+     * @dev this is an example. France will be selected in map, if your 
+     * mouse is over the tooltip is displayed (right top)
+     * if you click on it, selected territory is in browser console.
+     */
+    this.rightsSoldTerritories.push({ isoA3: territoriesISOA3.netherlands, 'label': territories.netherlands });
+    this.availableTerritories.push({ isoA3: territoriesISOA3.france, 'label': territories.france });
+  }
 
   /** Whenever you click on a territory, add it to availsForm.territories. */
-  public select(territory: Territory) {
-    const territorySlug = territoriesISOA3[territory];
-    this.availsForm.addTerritory(territorySlug);
+  public select(territory: any) { // @TODO #5647 find correct typing
+    console.log(territory);
   }
 
   /** Get a list of iso_a3 strings from the territories of the form. */
   public get territoriesIsoA3(): string[] {
-    return this.availsForm.territory.value.map(territorySlug => territoriesISOA3[territorySlug]);
+    /** @dev this is an example. */
+    return [territoriesISOA3['united-states-of-america']];
+    //return this.availsForm.territory.value.map(territorySlug => territoriesISOA3[territorySlug]);
   }
 
   public trackByTag(tag) {
     return tag;
-  }
-
-  /** Apply filters and display results on the world map. */
-  public async applyAvailsFilter() {
-    try {
-      this.availsForm.disable();
-    } catch (error) {
-      this.snackBar.open(error.message, 'close', { duration: 3000 });
-    }
-  }
-
-  /** Reenable the form for a new search. */
-  // TODO: bind every controls to the form to avoid tricky disable => ISSUE#1942
-  public deactivateAvailsFilter() {
-    this.availsForm.get('isActive').setValue(false);
-    this.availsForm.enable();
-  }
-
-  /** Add a distribution right to the user selection for the active movie. */
-  public addRight() {
-    try {
-
-      // If title don't exist in the marketplace store, create one.
-      if (!this.marketplaceQuery.getEntity(this.movie.id)) {
-        this.marketplaceStore.addTitle(this.movie.id);
-      }
-
-      this.snackBar.open('Exploitation Rights added to your Selection', 'close', {
-        duration: 3000
-      });
-
-    } catch (error) {
-      this.snackBar.open(error.message, 'close', { duration: 3000 });
-    }
   }
 
   /** Display the territories information in the tooltip */
