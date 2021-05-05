9365b9a0bb00db9657eee464b06be284fe75d70b
Remco <RemcoSimonides@gmail.com>
2021-05-05 14:03:22 +0200

    5698 Prevent leaving without saving

--- apps/catalog/catalog/src/app/marketplace/title/avails/avails.component.ts
+++ apps/catalog/catalog/src/app/marketplace/title/avails/avails.component.ts
@@ -4,7 +4,7 @@ import { MovieQuery, Movie, createMovieLanguageSpecification } from '@blockframe
 import { TerritoryValue, TerritoryISOA3Value, territoriesISOA3, territories, Language } from '@blockframes/utils/static-model';
 import { Organization } from '@blockframes/organization/+state/organization.model';
 import { OrganizationService, OrganizationQuery } from '@blockframes/organization/+state';
-import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { BehaviorSubject, Observable, of, Subscription } from 'rxjs';
 import { Contract, ContractService } from '@blockframes/contract/contract/+state';
 import { getMandateTerms, getSoldTerms } from '@blockframes/contract/avails/avails';
 import { Term, TermService } from '@blockframes/contract/term/+state';
@@ -13,6 +13,9 @@ import { Bucket, BucketQuery, BucketService } from '@blockframes/contract/bucket
 import { BucketTermForm, BucketForm } from '@blockframes/contract/bucket/form';
 import { VersionSpecificationForm } from '@blockframes/movie/form/movie.form';
 import { AvailsForm } from '@blockframes/contract/avails/form/avails.form';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmComponent } from '@blockframes/ui/confirm/confirm.component';
+import { switchMap } from 'rxjs/operators';
 
 interface TerritoryMarker {
   isoA3: TerritoryISOA3Value,
@@ -65,6 +68,7 @@ export class MarketplaceMovieAvailsComponent implements OnInit, OnDestroy {
     private termService: TermService,
     private snackBar: MatSnackBar,
     private bucketQuery: BucketQuery,
+    private dialog: MatDialog,
     private bucketService: BucketService
   ) { }
 
@@ -87,6 +91,29 @@ export class MarketplaceMovieAvailsComponent implements OnInit, OnDestroy {
 
   public ngOnDestroy() {
     this.sub.unsubscribe();
+  }
+
+  confirmExit() {
+    const isPristine = this.bucketForm.pristine;
+    if (isPristine) {
+      return of(true);
+    }
+    const dialogRef = this.dialog.open(ConfirmComponent, {
+      data: {
+        title: 'You are about to leave the page',
+        question: 'Some changes have not been added to Selection. If you leave now, you will lose these changes.',
+        buttonName: 'Leave anyway'
+      }
+    })
+    return dialogRef.afterClosed().pipe(
+      switchMap(exit => {
+        /* Undefined means user clicked on the backdrop, meaning just close the modal */
+        if (typeof exit === 'undefined') {
+          return of(false);
+        }
+        return of(exit)
+      })
+    )
   }
 
   applyFilters() {
