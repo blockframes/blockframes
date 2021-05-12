5980631b41521c60be0e855d0a45a5add4d676c0
bruce <delorme.bruce.michel@gmail.com>
2021-05-12 11:00:08 +0200

    added new tests

--- libs/contract/src/lib/avails/tests/avails.bucketForm.spec.ts
+++ libs/contract/src/lib/avails/tests/avails.bucketForm.spec.ts
@@ -1,8 +1,9 @@
 import { createMandate } from "../../contract/+state/contract.model";
-import { toTerritoryMarker } from "./../avails";
+import { AvailsFilter, findSameTermIndex, getTerritories, toTerritoryMarker } from "./../avails";
 import { BucketForm } from '@blockframes/contract/bucket/form'
 import { createTerm } from "../../term/+state/term.model";
 import { AvailsForm } from "../form/avails.form";
+import { createBucketTerm } from "@blockframes/contract/bucket/+state";
 
 describe('BucketForm', () => {
 
@@ -60,7 +61,7 @@ describe('BucketForm', () => {
     termA.duration.from = new Date('01/01/2020');
     termA.duration.to = new Date('01/01/2030');
     termA.medias = ['theatrical'];
-    termA.territories = ['france','australia'];
+    termA.territories = ['france', 'australia'];
     termA.exclusive = true;
 
     const mandateB = createMandate({
@@ -74,7 +75,7 @@ describe('BucketForm', () => {
     termA.duration.from = new Date('01/01/2020');
     termA.duration.to = new Date('01/01/2030');
     termA.medias = ['theatrical'];
-    termA.territories = ['germany','india'];
+    termA.territories = ['germany', 'india'];
     termA.exclusive = true;
 
     const selected1 = toTerritoryMarker('france', 'MandateA', [mandateA], termA);
@@ -130,8 +131,158 @@ describe('BucketForm', () => {
 
     bucketForm.removeTerritory(availsForm.value, selected1);
     expect(bucketForm.isAlreadyInBucket(availsForm.value, selected1)).toBe(false);
-  
+
     expect(bucketForm.value.contracts[0].terms.length).toBe(0);
+  });
+
+  it('Should return find and return termIndex', () => {
+    const availsForm = new AvailsForm({ territories: [] }, ['duration']);
+    availsForm.get('duration').get('from').setValue(new Date('06/01/2021'));
+    availsForm.get('duration').get('to').setValue(new Date('12/01/2021'));
+    availsForm.get('medias').setValue(['theatrical']);
+    availsForm.get('exclusive').setValue(true);
+
+    const bucketTermA = createBucketTerm({ // Not same medias
+      duration: {
+        from: new Date('06/01/2021'),
+        to: new Date('12/01/2021')
+      },
+      medias: ['theatrical', 'hotels'],
+      exclusive: true,
+      territories: ['france', 'spain', 'united-states-of-america']
+    });
+
+    const bucketTermB = createBucketTerm({// Not same dates
+      duration: {
+        from: new Date('06/01/2022'), 
+        to: new Date('12/01/2022')
+      },
+      medias: ['theatrical', 'hotels'],
+      exclusive: true,
+      territories: ['france', 'spain', 'united-states-of-america']
+    });
+
+    const bucketTermC = createBucketTerm({ // Matching
+      duration: {
+        from: new Date('06/01/2021'),
+        to: new Date('12/01/2021')
+      },
+      medias: ['theatrical'], 
+      exclusive: true,
+      territories: ['france', 'spain', 'united-states-of-america']
+    });
+
+    const terms = [bucketTermA, bucketTermB, bucketTermC];
+    const termIndex = findSameTermIndex(terms,availsForm.value);
+    expect(termIndex).toBe(2);
+  });
+
+  it('Should return -1 if index is not found', () => {
+    const availsForm = new AvailsForm({ territories: [] }, ['duration']);
+    availsForm.get('duration').get('from').setValue(new Date('06/01/2021'));
+    availsForm.get('duration').get('to').setValue(new Date('12/01/2021'));
+    availsForm.get('medias').setValue(['theatrical']);
+    availsForm.get('exclusive').setValue(true);
+
+    const bucketTermA = createBucketTerm({ // Not same medias
+      duration: {
+        from: new Date('06/01/2021'),
+        to: new Date('12/01/2021')
+      },
+      medias: ['theatrical', 'hotels'],
+      exclusive: true,
+      territories: ['france', 'spain', 'united-states-of-america']
+    });
+
+    const bucketTermB = createBucketTerm({// Not same dates
+      duration: {
+        from: new Date('06/01/2022'), 
+        to: new Date('12/01/2022')
+      },
+      medias: ['theatrical', 'hotels'],
+      exclusive: true,
+      territories: ['france', 'spain', 'united-states-of-america']
+    });
+
+    const terms = [bucketTermA, bucketTermB];
+    const termIndex = findSameTermIndex(terms,availsForm.value);
+    expect(termIndex).toBe(-1);
+  });
+
+  it('Should return territories selected', () => {
+    const bucketForm = new BucketForm();
+    const mandate = createMandate({
+      id: 'MandateA',
+      termIds: ['termA'],
+      titleId: 'titleA',
+      sellerId: 'orgA'
+    });
+
+    const availDetails: AvailsFilter = {
+      duration: { from: new Date('01/01/2020'), to: new Date('01/01/2030') }, 
+      exclusive: true,
+      medias: ['theatrical']
+    };
+
+    const term = createTerm({ id: 'termA', contractId: mandate.id });
+    term.duration.from = new Date('01/01/2020');
+    term.duration.to = new Date('01/01/2030');
+    term.medias = ['theatrical'];
+    term.territories = ['france', 'germany', 'spain'];
+    term.exclusive = true;
+
+    const selected1 = toTerritoryMarker('germany', 'MandateA', [mandate], term);
+    bucketForm.addTerritory(availDetails, selected1);
+
+    const selected2 = toTerritoryMarker('france', 'MandateA', [mandate], term);
+    bucketForm.addTerritory(availDetails, selected2);
+
+    const availFilter: AvailsFilter = {
+      duration: { from: new Date('01/01/2020'), to: new Date('01/01/2030') }, 
+      exclusive: true,
+      medias: ['theatrical']
+    };
+    const territories = getTerritories(availFilter, bucketForm.value, 'exact');
+    
+    expect(territories.length).toBe(2);
+    expect(territories.some(t => t === 'france')).toBe(true);
+    expect(territories.some(t => t === 'germany')).toBe(true);
+  });
+
+  it('Should return territories in selection', () => {
+    const bucketForm = new BucketForm();
+    const mandate = createMandate({
+      id: 'MandateA',
+      termIds: ['termA'],
+      titleId: 'titleA',
+      sellerId: 'orgA'
+    });
+
+    const availDetails: AvailsFilter = {
+      duration: { from: new Date('01/01/2019'), to: new Date('01/01/2031') }, 
+      exclusive: true,
+      medias: ['theatrical']
+    };
+
+    const term = createTerm({ id: 'termA', contractId: mandate.id });
+    term.duration.from = new Date('01/01/2020');
+    term.duration.to = new Date('01/01/2030');
+    term.medias = ['theatrical'];
+    term.territories = ['france', 'germany', 'spain'];
+    term.exclusive = true;
+
+    const selected1 = toTerritoryMarker('germany', 'MandateA', [mandate], term);
+    bucketForm.addTerritory(availDetails, selected1);
+
+    const availFilter: AvailsFilter = {
+      duration: { from: new Date('01/01/2020'), to: new Date('01/01/2030') }, 
+      exclusive: true,
+      medias: ['theatrical']
+    };
+    const territories = getTerritories(availFilter, bucketForm.value, 'in');
+    
+    expect(territories.length).toBe(1);
+    expect(territories.some(t => t === 'germany')).toBe(true);
   });
 })
 
