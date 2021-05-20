a7c175da328f3b9ba73ca436ecbdeaa06a929241
xmano <mano@xkanda.com>
2021-05-20 16:36:49 +0530

    Fix lint errors in import lib - step 1

--- libs/import/src/lib/components/view-extracted-elements/contract/view-extracted-contracts.component.ts
+++ libs/import/src/lib/components/view-extracted-elements/contract/view-extracted-contracts.component.ts
@@ -31,14 +31,14 @@ const errorsMap: {[key in errorCodes]: SpreadsheetImportError} = {
   'no-seller-id': {
     type: 'warning',
     field: 'contract.sellerId',
-    reason: `Couldn\'t find Licensor with the provided name.`,
+    reason: `Couldn't find Licensor with the provided name.`,
     name: 'Seller ID',
     hint: 'Edit corresponding sheet field.'
   },
   'no-buyer-id': {
     type: 'warning',
     field: 'contract.buyerId',
-    reason: `Couldn\'t find Licensee with the provided name.`,
+    reason: `Couldn't find Licensee with the provided name.`,
     name: 'Buyer ID',
     hint: 'Edit corresponding sheet field.'
   },
@@ -201,7 +201,7 @@ export class ViewExtractedContractsComponent implements OnInit {
       if (!buyerId) errors.push(errorsMap['no-buyer-id']);
 
       // STAKEHOLDER
-      const getStakeholders = !!_stakeholdersList ? split(_stakeholdersList).map(orgName => this.getOrgId(orgName)) : [];
+      const getStakeholders = _stakeholdersList ? split(_stakeholdersList).map(orgName => this.getOrgId(orgName)) : [];
       const stakeholders = (await Promise.all(getStakeholders)).filter(s => !!s);
       if (!stakeholders.length) errors.push(errorsMap['no-stakeholders']);
 
