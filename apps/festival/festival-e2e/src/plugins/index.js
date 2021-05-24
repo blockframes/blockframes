b2f6293a46fda4739118c41369e933c716f790e5
xmano <mano@xkanda.com>
2021-05-24 21:44:01 +0530

    Remove typescript preprocessor plugin dependency

--- apps/festival/festival-e2e/src/plugins/index.js
+++ apps/festival/festival-e2e/src/plugins/index.js
@@ -11,8 +11,6 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-const { preprocessTypescript } = require('@nrwl/cypress/plugins/preprocessor');
-
 const fs = require('fs');
 const PDFDocument = require('pdfkit');
 
@@ -39,8 +37,6 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 
-  // Preprocess Typescript file using Nx helper
-  on('file:preprocessor', preprocessTypescript(config));
   on('task', {
     'random:pdf': createFakeScript
   });
