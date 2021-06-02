d89c62bcad00e261539b4ca2a45b53d4ada31ac8
Bruce <eelios@gmail.com>
2021-06-02 16:23:08 +0200

    part of #5898 (#5937)
    * fix "In app" isn't checked despite all it's childs being ticked
    
    * revert
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>

--- libs/user/src/lib/auth/forms/notifications-form/notifications-form.component.ts
+++ libs/user/src/lib/auth/forms/notifications-form/notifications-form.component.ts
@@ -11,24 +11,24 @@ import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-interface NotificationSetting { text: string, tooltip: boolean};
+interface NotificationSetting { text: string, tooltip: boolean };
 const titleType: Record<NotificationTypesBase, NotificationSetting> = {
   movieAccepted: { text: 'A title is succesfully published on the marketplace.', tooltip: false },
-  requestFromUserToJoinOrgCreate: { text:'A user requests to join your organization.', tooltip: true },
-  requestFromUserToJoinOrgDeclined: { text:'A user\'s request to join your organization was declined. ', tooltip: false },
-  orgMemberUpdated: { text:'A user joins or leaves your organization.', tooltip: false },
-  requestToAttendEventSent: { text:'Your request to join an event is successfully sent.', tooltip: false },
-  eventIsAboutToStart: { text:'REMINDER - An event you\'re attending will start in 1 hour. (RECOMMENDED)', tooltip: false },
-  oneDayReminder: { text:'REMINDER - An event you\'re attending will start in 24 hours. (RECOMMENDED)', tooltip: false },
-  invitationToAttendEventUpdated: { text:'A user answers your invitation to an event you\'re organizing.', tooltip: false },
-  requestToAttendEventUpdated: { text:'Your request to join an event gets accepted or declined.', tooltip: false },
-  requestToAttendEventCreated: { text:'A user wants to join an event you\'re organizing. (RECOMMENDED)', tooltip: true },
-  invitationToAttendMeetingCreated: { text:'You are invited to a meeting. (RECOMMENDED)', tooltip: true },
-  invitationToAttendScreeningCreated: { text:'You are invited to a screening. (RECOMMENDED)', tooltip: true },
+  requestFromUserToJoinOrgCreate: { text: 'A user requests to join your organization.', tooltip: true },
+  requestFromUserToJoinOrgDeclined: { text: 'A user\'s request to join your organization was declined. ', tooltip: false },
+  orgMemberUpdated: { text: 'A user joins or leaves your organization.', tooltip: false },
+  requestToAttendEventSent: { text: 'Your request to join an event is successfully sent.', tooltip: false },
+  eventIsAboutToStart: { text: 'REMINDER - An event you\'re attending will start in 1 hour. (RECOMMENDED)', tooltip: false },
+  oneDayReminder: { text: 'REMINDER - An event you\'re attending will start in 24 hours. (RECOMMENDED)', tooltip: false },
+  invitationToAttendEventUpdated: { text: 'A user answers your invitation to an event you\'re organizing.', tooltip: false },
+  requestToAttendEventUpdated: { text: 'Your request to join an event gets accepted or declined.', tooltip: false },
+  requestToAttendEventCreated: { text: 'A user wants to join an event you\'re organizing. (RECOMMENDED)', tooltip: true },
+  invitationToAttendMeetingCreated: { text: 'You are invited to a meeting. (RECOMMENDED)', tooltip: true },
+  invitationToAttendScreeningCreated: { text: 'You are invited to a screening. (RECOMMENDED)', tooltip: true },
   offerCreatedConfirmation: { text: 'You send an offer', tooltip: false }
 };
 
-const tables: {title: string, types: string[], appAuthorized: App[]}[] = [
+const tables: { title: string, types: string[], appAuthorized: App[] }[] = [
   {
     title: 'Company Management',
     types: ['requestFromUserToJoinOrgCreate', 'orgMemberUpdated', 'requestFromUserToJoinOrgDeclined'],
@@ -78,13 +78,13 @@ export class NotificationsFormComponent {
     private authQuery: AuthQuery,
     private authService: AuthService,
     private snackBar: MatSnackBar
-    ) { }
+  ) { }
 
   toogleAll(event: MatSlideToggleChange, mode: 'email' | 'app') {
     const checked = event.checked;
     for (const control of Object.values(this.form.controls)) {
       const c = control.get(mode);
-      if(!c.disabled) c.setValue(checked);
+      if (!c.disabled) c.setValue(checked);
     }
   }
 
@@ -92,7 +92,7 @@ export class NotificationsFormComponent {
     const checked = event.checked;
     for (const type of types) {
       const c = this.form.get(type).get(mode);
-      if(!c.disabled) c.setValue(checked);
+      if (!c.disabled) c.setValue(checked);
     }
   }
 
@@ -105,29 +105,29 @@ export class NotificationsFormComponent {
   }
 }
 
-@Pipe({name: 'someChecked'})
+@Pipe({ name: 'someChecked' })
 export class SomeCheckedPipe implements PipeTransform {
   transform(value: NotificationsForm['value'], mode: 'email' | 'app', types: NotificationTypesBase[]) {
     let checked = 0;
     for (const type of types) {
-      if (value[type]?.[mode]) checked ++;
+      if (value[type]?.[mode]) checked++;
     }
     return checked > 0 && checked < types.length;
   }
 }
 
-@Pipe({name: 'everyChecked'})
+@Pipe({ name: 'everyChecked' })
 export class EveryCheckedPipe implements PipeTransform {
   transform(value: NotificationsForm['value'], mode: 'email' | 'app', types: NotificationTypesBase[]) {
-    return types.every(type => !!value[type]?.[mode]);
+    return types.every(type => !!value[type]?.[mode] || value[type]?.[mode] === undefined);
   }
 }
 
-@Pipe({name: 'showNotification'})
+@Pipe({ name: 'showNotification' })
 export class ShowNotificationPipe implements PipeTransform {
   currentApp = getCurrentApp(this.routerQuery);
   public tables = tables;
-  constructor(private routerQuery: RouterQuery) {}
+  constructor(private routerQuery: RouterQuery) { }
 
   transform(index: number) {
     return this.tables[index].appAuthorized.includes(this.currentApp);
