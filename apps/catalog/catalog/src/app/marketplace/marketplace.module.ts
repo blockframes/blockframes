6d662933948fc3e81db2b373d4f63fafb8fa0112
samir <samir.touira@epitech.eu>
2021-06-08 16:27:15 +0200

    removed Offers & Deals and all related to

--- apps/catalog/catalog/src/app/marketplace/marketplace.module.ts
+++ apps/catalog/catalog/src/app/marketplace/marketplace.module.ts
@@ -63,17 +63,6 @@ const routes: Routes = [{
       data: { animation: 'wishlist' }
     },
     {
-      path: 'deals',
-      children: [{
-        path: '',
-        canActivate: [CatalogDealsGuard],
-        loadChildren: () => import('./right/list/list.module').then(m => m.RightListModule),
-      }, {
-        path: ':contractId',
-        loadChildren: () => import('./right/view/view.module').then(m => m.RightViewModule)
-      }]
-    },
-    {
       path: 'title',
       children: [{
         path: '',
