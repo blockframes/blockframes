cc77e2dd24149fcab8444c896f165156d99d6a44
Bruce <eelios@gmail.com>
2021-06-10 10:36:09 +0200

    avail bug fix for prod (#6016)
    * bug fix for prod
    
    * update after review
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>

--- apps/catalog/catalog/src/app/marketplace/title/avails/map/avails-map.component.ts
+++ apps/catalog/catalog/src/app/marketplace/title/avails/map/avails-map.component.ts
@@ -2,13 +2,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { combineLatest } from 'rxjs';
-import { filter, map, shareReplay, startWith, take } from 'rxjs/operators';
+import { filter, map, pluck, shareReplay, startWith, take } from 'rxjs/operators';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import {
   getSoldTerms,
-  getTerritories,
+  getSelectedTerritories,
   TerritoryMarker,
   toTerritoryMarker,
   getTerritoryMarkers,
@@ -17,6 +17,7 @@ import {
 import { territoriesISOA3, TerritoryValue } from '@blockframes/utils/static-model';
 
 import { MarketplaceMovieAvailsComponent } from '../avails.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'catalog-movie-avails-map',
@@ -48,8 +49,9 @@ export class MarketplaceMovieAvailsMapComponent {
     this.availsForm.value$,
     this.shell.bucketForm.value$,
     this.territoryMarkers$,
+    this.route.params.pipe(pluck('movieId'))
   ]).pipe(
-    map(([avail, bucket, markers]) => getTerritories(avail, bucket, 'exact').map(t => markers[t])),
+    map(([avail, bucket, markers, movieId]) => getSelectedTerritories(movieId, avail, bucket, 'exact').map(t => markers[t])),
     startWith([]),
   );
 
@@ -57,8 +59,9 @@ export class MarketplaceMovieAvailsMapComponent {
     this.availsForm.value$,
     this.shell.bucketForm.value$,
     this.territoryMarkers$,
+    this.route.params.pipe(pluck('movieId'))
   ]).pipe(
-    map(([avail, bucket, markers]) => getTerritories(avail, bucket, 'in').map(t => markers[t])),
+    map(([avail, bucket, markers, movieId]) => getSelectedTerritories(movieId, avail, bucket, 'in').map(t => markers[t])),
     startWith([]),
   );
 
@@ -94,6 +97,7 @@ export class MarketplaceMovieAvailsMapComponent {
   constructor(
     private snackbar: MatSnackBar,
     private shell: MarketplaceMovieAvailsComponent,
+    private route: ActivatedRoute
   ) { }
 
   /** Display the territories information in the tooltip */
