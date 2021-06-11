e5dd769a260930193b26cd5840f3579b50767127
bruce <delorme.bruce.michel@gmail.com>
2021-06-11 14:39:53 +0200

    typo

--- apps/backend-ops/src/main.ts
+++ apps/backend-ops/src/main.ts
@@ -104,7 +104,7 @@ async function runCommand() {
         return;
       }
       await startMaintenance(db);
-      await migrate({ withBackup: !["0", 'false'].includes(arg1) || false });
+      await migrate({ withBackup: !['0', 'false'].includes(arg1) || false });
       await endMaintenance(db);
       break;
     case 'syncUsers':
