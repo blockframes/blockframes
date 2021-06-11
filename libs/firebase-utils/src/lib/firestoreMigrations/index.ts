3b8d8707ce7893fc3664e948aa7ef297bb20c7a0
Bruce <eelios@gmail.com>
2021-06-11 15:54:01 +0200

    Video upload on org documents#5191 (#6033)
    * WIP org videos
    
    * to test
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>

--- libs/firebase-utils/src/lib/firestoreMigrations/index.ts
+++ libs/firebase-utils/src/lib/firestoreMigrations/index.ts
@@ -60,6 +60,7 @@ import * as v0059 from './0059';
 import * as v0060 from './0060';
 import * as v0061 from './0061';
 import * as v0062 from './0062';
+import * as v0063 from './0063';
 
 import { Firestore, Storage } from '../types';
 
@@ -133,7 +134,8 @@ export const MIGRATIONS = {
   59: v0059,
   60: v0060,
   61: v0061,
-  62: v0062
+  62: v0062,
+  63: v0063
 };
 
 export const VERSIONS_NUMBERS = Object.keys(MIGRATIONS).map(s => parseInt(s, 10));
