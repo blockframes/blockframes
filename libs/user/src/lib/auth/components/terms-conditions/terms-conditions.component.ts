0f797dbed6f59b51c68ea6c9795d3dc7709f526c
Yonga Springfield <springfieldyonga@outlook.com>
2021-06-14 11:00:23 +0100

    refactor(privacy and terms)

--- libs/user/src/lib/auth/components/terms-conditions/terms-conditions.component.ts
+++ libs/user/src/lib/auth/components/terms-conditions/terms-conditions.component.ts
@@ -1,11 +1,9 @@
-import { ChangeDetectionStrategy, Component, OnInit, Optional } from "@angular/core";
-import { Location } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { getCurrentApp, applicationUrl } from "@blockframes/utils/apps";
 import { getAppLocation } from "@blockframes/utils/helpers";
 import { RouterQuery } from "@datorama/akita-ng-router-store";
-import { MatDialogRef } from "@angular/material/dialog";
 
 @Component({
   selector: 'auth-terms-conditions',
@@ -21,8 +19,6 @@ export class TermsConditionsComponent implements OnInit {
 
   constructor(
     private routerQuery: RouterQuery,
-    private location: Location,
-    @Optional() private ref?: MatDialogRef<unknown>
   ) { }
 
   ngOnInit() {
@@ -31,11 +27,4 @@ export class TermsConditionsComponent implements OnInit {
     this.appUrl = applicationUrl[this.appName];
   }
 
-  goBack() {
-    if (this.ref) {
-      this.ref.close();
-    } else {
-      this.location.back();
-    }
-  }
 }
