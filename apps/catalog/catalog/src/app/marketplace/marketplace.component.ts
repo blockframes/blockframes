bae7e80785dcafc2941e0fccb3228e11c6517e0a
JEREMIE <jeremie@macbook-air-de-jeremie.home>
2021-06-21 16:33:59 +0200

    issue login view selection replace akita

--- apps/catalog/catalog/src/app/marketplace/marketplace.component.ts
+++ apps/catalog/catalog/src/app/marketplace/marketplace.component.ts
@@ -1,8 +1,8 @@
 import { Component, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
 import { OrganizationQuery } from '@blockframes/organization/+state';
-import { BucketQuery, BucketService } from '@blockframes/contract/bucket/+state';
-import { Subscription, Observable } from 'rxjs';
-import { switchMap, map } from 'rxjs/operators';
+import { BucketService } from '@blockframes/contract/bucket/+state';
+import { Observable } from 'rxjs';
+import { switchMap, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'catalog-marketplace',
@@ -11,26 +11,17 @@ import { switchMap, map } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
-export class MarketplaceComponent implements OnDestroy {
-  private sub: Subscription;
+export class MarketplaceComponent {
   public contractCount$: Observable<number>;
   public canAccessDeals = !this.orgQuery.getActive().appAccess.catalog.dashboard;
   public org$ = this.orgQuery.selectActive();
 
   constructor(
-    private bucketQuery: BucketQuery,
     private bucketService: BucketService,
     private orgQuery: OrganizationQuery,
   ) {
-    this.sub = this.orgQuery.selectActiveId().pipe(
-      switchMap(id => this.bucketService.syncActive({ id }))
-    ).subscribe();
-    this.contractCount$ = this.bucketQuery.selectActive().pipe(
+    this.contractCount$ = this.bucketService.active$.pipe(
       map(bucket => bucket?.contracts.length ?? 0)
     );
-  }
-
-  ngOnDestroy() {
-    this.sub.unsubscribe();
   }
 }
