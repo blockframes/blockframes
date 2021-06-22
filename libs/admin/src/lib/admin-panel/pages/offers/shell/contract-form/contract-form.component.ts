4ff040ff2fdc7dd153da714c05436b15ceb3f549
pldespaigne <pl.despaigne@gmail.com>
2021-06-22 14:53:28 +0200

    cleaning

--- libs/admin/src/lib/admin-panel/pages/offers/shell/contract-form/contract-form.component.ts
+++ libs/admin/src/lib/admin-panel/pages/offers/shell/contract-form/contract-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'contract-form',
@@ -6,11 +6,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./contract-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ContractFormComponent implements OnInit {
+export class ContractFormComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
--- libs/admin/src/lib/admin-panel/pages/offers/shell/contract-view/contract-view.component.ts
+++ libs/admin/src/lib/admin-panel/pages/offers/shell/contract-view/contract-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'contract-view',
@@ -6,11 +6,6 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./contract-view.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ContractViewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class ContractViewComponent {
 
 }
