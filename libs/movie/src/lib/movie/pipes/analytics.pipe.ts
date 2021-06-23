4008f02804d3dca2626c4c912db9be4d9dd8bf9e
Remco <RemcoSimonides@gmail.com>
2021-06-23 14:45:44 +0200

    6101 Dashboard analytics infinite reload

--- libs/movie/src/lib/movie/pipes/analytics.pipe.ts
+++ libs/movie/src/lib/movie/pipes/analytics.pipe.ts
@@ -1,7 +1,7 @@
 import { NgModule, Pipe, PipeTransform } from "@angular/core";
 import { Observable } from "rxjs";
 import { AnalyticsService } from "@blockframes/utils/analytics/analytics.service";
-import { distinctUntilChanged, map } from "rxjs/operators";
+import { map } from "rxjs/operators";
 import { MovieAnalytics } from "../+state/movie.firestore";
 import { Movie } from '@blockframes/movie/+state/movie.model';
 
@@ -21,11 +21,9 @@ export class GetViewsPipe implements PipeTransform {
 @Pipe({ name: 'getTitlesAnalytics' })
 export class GetTitlesAnalyticsPipe implements PipeTransform {
   constructor(private analytics: AnalyticsService) {}
-  transform(movies: Movie[]): Observable<MovieAnalytics[]> {
+  async transform(movies: Movie[]): Promise<MovieAnalytics[]> {
     const movieIds = movies.map(m => m.id);
-    return this.analytics.valueChanges(movieIds).pipe(
-      distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b)),
-      map(value => value.filter(v => !!v)));
+    return this.analytics.getValue(movieIds).then(value => value.filter(v => !!v));
   }
 }
 
