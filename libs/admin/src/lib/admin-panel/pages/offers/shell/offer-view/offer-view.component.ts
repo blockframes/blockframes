65bb54ecb9c8a64d6083637947b88ec3456d20e8
Yonga Springfield <springfieldyonga@outlook.com>
2021-06-28 12:29:07 +0100

    feat(offer-view):finished design and connected data

--- libs/admin/src/lib/admin-panel/pages/offers/shell/offer-view/offer-view.component.ts
+++ libs/admin/src/lib/admin-panel/pages/offers/shell/offer-view/offer-view.component.ts
@@ -1,4 +1,19 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit, Pipe, PipeTransform } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Organization, OrganizationService } from '@blockframes/organization/+state';
+import { UserService } from '@blockframes/user/+state';
+import { Query, queryChanges } from 'akita-ng-fire';
+import { combineLatest } from 'rxjs';
+import { map, shareReplay, tap } from 'rxjs/operators';
+import { OfferShellComponent } from '../shell.component';
+
+
+const columns = {
+  'seller_approved': 'Seller Approved',
+  'sellers_name': 'Seller\'s name (Email)',
+  'organization_name': 'Organization Name',
+  'id': 'Actions',
+};
 
 @Component({
   selector: 'offer-view',
@@ -6,6 +21,76 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./offer-view.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OfferViewComponent {
+export class OfferViewComponent implements OnInit {
 
+  public offer$ = this.shell.offer$.pipe(shareReplay(1));
+  public buyerOrg$ = this.shell.buyerOrg$;
+  public contracts$ = this.shell.contracts$;
+  public incomes$ = this.shell.incomes$;
+  public form = this.fb.group({
+    type: this.fb.control('general_contract_status'),
+    buyers_specific_term: this.fb.control(null),
+    buyers_delivery_wishlist: this.fb.control(null),
+  })
+  public titles: {
+    seller_approved
+    sellers_name
+    organization_name
+    id: string
+  }[] = [];
+  columns = columns;
+  initialColumns = ['seller_approved', 'sellers_name', 'organization_name', 'id'];
+
+  constructor(
+    private shell: OfferShellComponent,
+    public fb: FormBuilder,
+    private userService: UserService,
+  ) { }
+
+  ngOnInit() {
+    combineLatest(
+      this.offer$,
+      this.buyerOrg$,
+      this.contracts$,
+      this.incomes$,
+    ).subscribe(
+      ([offer, buyerOrg, contracts, incomes]) => {
+        console.log({ offer, buyerOrg, contracts, incomes })
+        this.form.get('buyers_specific_term').setValue(offer.specificity)
+        this.form.get('buyers_delivery_wishlist').setValue(offer.delivery)
+        this.titles = contracts.map(_ => ({
+          seller_approved: _.status,
+          sellers_name: _.sellerId,
+          organization_name: buyerOrg.denomination.public,
+          id: _.id,
+        }))
+      }
+    )
+  }
+
+  update() {
+    console.log('hello')
+  }
+
+}
+
+
+@Pipe({ name: 'getUser' })
+export class GetUserPipe implements PipeTransform {
+  constructor(private orgService: OrganizationService) { }
+
+  transform(orgid: string) {
+    const queryOffer: Query<Organization> = {
+      path: `orgs/${orgid}`,
+      //@ts-ignore
+      userIds: (org: Organization) => ({
+        path: 'users',
+        queryFn: ref => ref.where('uid', '==', org.userIds[0]),
+      }),
+    }
+    return queryChanges.call(this.orgService, queryOffer).pipe(
+      tap(s => console.log({ s })),
+      map((_) => _.userIds[0])
+    )
+  }
 }
--- libs/admin/src/lib/admin-panel/pages/offers/shell/offer-view/offer-view.module.ts
+++ libs/admin/src/lib/admin-panel/pages/offers/shell/offer-view/offer-view.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OfferViewComponent } from './offer-view.component';
+import { GetUserPipe, OfferViewComponent } from './offer-view.component';
 import { RouterModule } from '@angular/router';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { TableFilterModule } from '@blockframes/ui/list/table-filter/table-filter.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [OfferViewComponent],
+  declarations: [OfferViewComponent, GetUserPipe],
   imports: [
     CommonModule,
+    TableFilterModule,
+    ReactiveFormsModule,
+
+    //Material
+    MatSelectModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+
     RouterModule.forChild([{ path: '', component: OfferViewComponent }]),
   ]
 })
