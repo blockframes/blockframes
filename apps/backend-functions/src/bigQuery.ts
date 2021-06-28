46cafe3fa30bbb1c44950500e5d6a19516cff1b2
Remco <RemcoSimonides@gmail.com>
2021-06-28 17:03:25 +0200

    6122 Remove duplicates

--- apps/backend-functions/src/bigQuery.ts
+++ apps/backend-functions/src/bigQuery.ts
@@ -27,9 +27,9 @@ WHERE
     )
 
 GROUP BY
-  event_name, event_date, eventId, eventIdPage, userId
+  event_name, eventId, eventIdPage, userId
 ORDER BY
-  event_name, event_date
+  event_name
 `
 
 const queryAnalyticsActiveUsers = `
@@ -96,7 +96,7 @@ const createEventAnalytics = (result, user: PublicUser | undefined, org: Organiz
   }
 };
 
-/** Call bigQuery with an array of eventId to tet their analytics. */
+/** Call bigQuery with an array of eventId to get their analytics. */
 export const requestEventAnalytics = async (
   data: { eventIds: string[] },
   context: CallableContext
