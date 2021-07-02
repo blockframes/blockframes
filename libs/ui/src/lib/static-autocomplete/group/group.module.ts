82adfce1f4b005af8bb548966c0b691df1de2d16
Yonga Springfield <springfieldyonga@outlook.com>
2021-07-02 13:25:20 +0100

    refactor

--- libs/ui/src/lib/static-autocomplete/group/group.module.ts
+++ libs/ui/src/lib/static-autocomplete/group/group.module.ts
@@ -11,7 +11,7 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatDividerModule } from "@angular/material/divider";
 import { MatRippleModule } from "@angular/material/core";
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { ToGroupLabelPipeModule, ToLabelModule } from '@blockframes/utils/pipes';
+import { JoinPipeModule, ToGroupLabelPipeModule, ToLabelModule } from '@blockframes/utils/pipes';
 
 @NgModule({
   imports: [
@@ -28,6 +28,7 @@ import { ToGroupLabelPipeModule, ToLabelModule } from '@blockframes/utils/pipes'
     MatTooltipModule,
     ToLabelModule,
     ToGroupLabelPipeModule,
+    JoinPipeModule,
   ],
   declarations: [StaticGroupComponent, GetModePipe],
   exports: [StaticGroupComponent]
