0537a22219f309f9ed6b80332acf10f07effe166
Yonga Springfield <springfieldyonga@outlook.com>
2021-07-06 11:52:46 +0100

    refactor

--- libs/contract/src/lib/income/pipe.ts
+++ libs/contract/src/lib/income/pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform, NgModule } from '@angular/core';
 import { ContractService } from '@blockframes/contract/contract/+state';
 import { IncomeService } from '@blockframes/contract/income/+state';
-import { map, switchMap, tap } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 
 @Pipe({ name: 'getIncomesFromTitle' })
 export class GetIncomesFromTitlePipe implements PipeTransform {
@@ -13,7 +13,6 @@ export class GetIncomesFromTitlePipe implements PipeTransform {
     return this.contractService.valueChanges(ref => ref.where('titleId', '==', titleId).where('status', '==', 'approved')).pipe(
       map(contracts => contracts.map(contract => contract.id)),
       switchMap(contractIds => this.incomeService.valueChanges(contractIds)),
-      tap(s => console.log({s}))
     );
   }
 }
