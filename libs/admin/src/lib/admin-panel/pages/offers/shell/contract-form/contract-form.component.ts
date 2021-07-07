57e519977ccbfab50df6873e035a804ade84bf90
JEREMIE <jeremie@MacBook-Air-de-JEREMIE.local>
2021-07-07 17:54:12 +0200

    saving terms incomes in contract

--- libs/admin/src/lib/admin-panel/pages/offers/shell/contract-form/contract-form.component.ts
+++ libs/admin/src/lib/admin-panel/pages/offers/shell/contract-form/contract-form.component.ts
@@ -5,7 +5,7 @@ import { ActivatedRoute } from '@angular/router';
 // Services
 import { MovieService } from "@blockframes/movie/+state";
 import { IncomeService } from '@blockframes/contract/income/+state';
-import { ContractService } from '@blockframes/contract/contract/+state';
+import { Contract, ContractService } from '@blockframes/contract/contract/+state';
 import { Term, TermService } from "@blockframes/contract/term/+state";
 
 // Forms
@@ -14,6 +14,10 @@ import { AvailsForm } from '@blockframes/contract/avails/form/avails.form';
 import { MovieVersionInfoForm } from "@blockframes/movie/form/movie.form";
 import { RunsForm } from "@blockframes/contract/avails/form/runs.form";
 
+function toTerm({ id, avails, runs, versions }, contractId: string): Partial<Term> {
+  return { id, contractId, runs, languages: versions, ...avails };
+}
+
 @Component({
   selector: 'contract-form',
   templateUrl: './contract-form.component.html',
@@ -21,10 +25,12 @@ import { RunsForm } from "@blockframes/contract/avails/form/runs.form";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ContractFormComponent implements OnInit {
+  private contract?: Contract
   form = new FormGroup({
     titleId: new FormControl(null, Validators.required),
     price: new FormControl(Validators.min(0)),
     terms: FormList.factory([], (term: Term) => new FormGroup({
+      id: new FormControl(term.id),
       avails: new AvailsForm(term, []),
       versions: new MovieVersionInfoForm(term.languages),
       runs: new RunsForm(term.runs)
@@ -59,8 +65,25 @@ export class ContractFormComponent implements OnInit {
         titleId: contract?.titleId,
         price: income?.price
       })
+      this.contract = contract;
       this.currency = income?.currency;
       const terms = await this.termService.getValue(contract.termIds);
       (this.form.get('terms') as FormList<any>).patchAllValue(terms);
+    }
+
+    async save() {
+      if (this.form.valid) {
+        const { terms, titleId, price } = this.form.value;
+        const contractId = this.route.snapshot.params.contractId;
+        const write = this.contractService.batch(); // create a batch
+        const termList = (terms as any[]).map(term => toTerm(term, contractId));
+        const termIds = await this.termService.upsert(termList, { write });
+        const existingTermIds = this.contract?.termIds || [];
+        const termIdsToDelete = existingTermIds.filter(id => !termIds.includes(id));
+        await this.termService.remove(termIdsToDelete);
+        await this.contractService.update(contractId, { titleId, termIds  }, { write });
+        await this.incomeService.update(contractId, { price }, { write }); // Update the price in the batch
+        await write.commit();
+      }
     }
 }
