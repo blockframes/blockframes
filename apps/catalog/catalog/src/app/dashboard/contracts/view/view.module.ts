74d2388906d492ea7eb387222f08a1bce53216cd
Yonga Springfield <springfieldyonga@outlook.com>
2021-07-09 14:36:23 +0100

    refactor

--- apps/catalog/catalog/src/app/dashboard/contracts/view/view.module.ts
+++ apps/catalog/catalog/src/app/dashboard/contracts/view/view.module.ts
@@ -4,6 +4,7 @@ import { CatalogContractViewComponent } from './view.component';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+
 import { MatIconModule } from '@angular/material/icon';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaxLengthModule, ToLabelModule } from '@blockframes/utils/pipes';
@@ -11,6 +12,7 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { ContractItemModule } from '@blockframes/contract/contract/item/contract-item.module';
 import { IncomePipeModule } from '@blockframes/contract/income/pipe';
 import { TagModule } from '@blockframes/ui/tag/tag.module';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [CatalogContractViewComponent],
@@ -27,6 +29,7 @@ import { TagModule } from '@blockframes/ui/tag/tag.module';
     MatButtonModule,
     MatCardModule,
     MatSnackBarModule,
+    MatProgressSpinnerModule,
     MatIconModule,
     MatSnackBarModule,
 
