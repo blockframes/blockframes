245ecef9b7c9569a9e497fa0a023335012b76045
Remco <RemcoSimonides@gmail.com>
2021-07-13 16:16:30 +0200

    6121 add views to movie data

--- libs/movie/src/lib/movie/pipes/analytics.pipe.ts
+++ libs/movie/src/lib/movie/pipes/analytics.pipe.ts
@@ -1,22 +1,10 @@
 import { NgModule, Pipe, PipeTransform } from "@angular/core";
-import { Observable } from "rxjs";
 import { AnalyticsService } from "@blockframes/utils/analytics/analytics.service";
-import { map } from "rxjs/operators";
 import { MovieAnalytics } from "../+state/movie.firestore";
 import { Movie } from '@blockframes/movie/+state/movie.model';
 
 export const getViews = (analytics?: MovieAnalytics) => {
   return analytics?.movieViews.current.reduce((sum, event) => sum + event.hits, 0) || 0;
-}
-
-@Pipe({ name: 'getViews' })
-export class GetViewsPipe implements PipeTransform {
-  constructor(private analytics: AnalyticsService) {}
-  transform(id: string): Observable<number> {
-    return this.analytics.valueChanges(id).pipe(
-      map(getViews)
-    );
-  }
 }
 
 @Pipe({ name: 'getTitlesAnalytics' })
@@ -29,7 +17,7 @@ export class GetTitlesAnalyticsPipe implements PipeTransform {
 }
 
 @NgModule({
-  exports: [GetViewsPipe, GetTitlesAnalyticsPipe],
-  declarations: [GetViewsPipe, GetTitlesAnalyticsPipe],
+  exports: [GetTitlesAnalyticsPipe],
+  declarations: [GetTitlesAnalyticsPipe],
 })
 export class AnalyticsPipeModule {}
