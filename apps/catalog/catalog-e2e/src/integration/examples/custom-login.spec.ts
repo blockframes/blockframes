5da5cac542aa57a5c8c39f82a7aebeb6e0ef7bf8
xmano <mano@xkanda.com>
2021-07-14 15:18:39 +0530

    Working login command

--- apps/catalog/catalog-e2e/src/integration/examples/custom-login.spec.ts
+++ apps/catalog/catalog-e2e/src/integration/examples/custom-login.spec.ts
@@ -1,25 +1,22 @@
 /// <reference types="cypress" />
 
-
+import { login } from '../../support/commands';
 
 describe('Checking login with custom command', () => {
-  beforeEach(() => {
-    //Clear all messages on server before the test
-    // cy.mailosaurDeleteAllMessages(serverId).then(() => {
-    //   cy.log('Inbox empty. Ready to roll..');
-    // })
-    cy.visit('/');
+  before(() => {
+
+
   });
 
-  it('Test for custom login', () => {
+  it('Test for custom login', async () => {
     //Check for emails sent
     cy.log("Checking log-in");
-    cy.login("dev+troy-exl@blockframes.io", "blockframes")
-      .then((user) => {
-        cy.log(JSON.stringify(user));
-        console.log(user);
-      });
-    cy.window().should('have.property', 'LoggedIn');
+    const user = await login("dev+troy-exl@blockframes.io", "blockframes");
+    console.log(user);
   });
+
+  it('Go Home', () => {
+    cy.visit('/');
+  })
 
 });
--- apps/catalog/catalog-e2e/src/support/commands.ts
+++ apps/catalog/catalog-e2e/src/support/commands.ts
@@ -8,21 +8,11 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 //
-//
 import 'cypress-mailosaur';
-//import { AuthService } from '@blockframes/auth/+state';
-//import { AngularFireAuth } from 'angularfire2/auth';
 
 import firebase  from 'firebase/app';
+import "firebase/auth";
 import { firebase as firebaseConfig } from '@env';
-
-declare namespace Cypress {
-  interface Chainable<Subject> {
-    login(email: string, password: string): void;
-  }
-}
-
-firebase.initializeApp(firebaseConfig('catalog'));
 
 // -- This is a parent command --
 // Cypress.Commands.add("login", (email, password) => { ... })
@@ -39,23 +29,9 @@ firebase.initializeApp(firebaseConfig('catalog'));
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-Cypress.Commands.add("login", async (email, password) => { 
-  try {
-    //await AuthService.signin(email, password);
-    return firebase.auth().signInWithEmailAndPassword(email, password)
-    /*
-    cy.window().should('have.property', 'AuthService')
-    cy.window()
-      .then(async (w) => {
-        const authService = w['AuthService'];
-        await authService.signin({ email, password });
-        cy.log(`Logged in user: ${email}`);
-      });
-    */
-    //cy.window().should('have.property', 'LoggedIn')
-    //cy.log('Logged In');
-  } catch (e) {
-    //cy.log(`Login Err: ${e}`);
-    console.log(e);
-  }
-})
+//Init the app
+const app = firebase.initializeApp(firebaseConfig('catalog'));
+
+export const login = (email: string, password: string) => { 
+  return app.auth().signInWithEmailAndPassword(email, password);
+}
