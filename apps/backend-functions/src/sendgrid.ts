3b1fabd9276e4b385233a5cac77ca662302de1aa
Bruce <eelios@gmail.com>
2021-07-20 17:42:19 +0200

    fix for release 3.3 (#6332)
    * 6066 remove 2030 label
    
    * Remove 'Contracts' tab from sidebar Content Dashboard (#6329)
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>
    
    * 6088 verify request is from sendgrid (#6335)
    
    * 6088 verify request is from sendgrid
    
    * 6088 verify request is from sendgrid
    
    * 6088 verify request is from sendgrid
    
    * fix createOfferId (#6336)
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>
    
    Co-authored-by: Remco <RemcoSimonides@gmail.com>
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>

--- apps/backend-functions/src/sendgrid.ts
+++ apps/backend-functions/src/sendgrid.ts
@@ -1,5 +1,6 @@
 import { BigQuery } from '@google-cloud/bigquery';
 import { Request, Response } from "firebase-functions";
+import { EventWebhook } from '@sendgrid/eventwebhook';
 
 /** Return object with only the properties defined in fields parameter */
 function copyFields<T>(from: T, fields: string[]): T {
@@ -10,18 +11,36 @@ function copyFields<T>(from: T, fields: string[]): T {
   return result as T;
 }
 
+const displayError = (e) => typeof e === 'object' ? JSON.stringify(e) : e;
+
 /**
  * Listens to Events Webhook of Sengrid
  * 
  * Event Webhook Schema
- * email:string, timestamp:timestamp, event:string, asm_group_id:numeric, sg_event_id:string, sg_message_id:string, sg_template_id:string, sg_template_name:string, reason:string, status:string, projectId: string 
+ * email:string, timestamp:timestamp, event:string, asm_group_id:numeric, sg_event_id:string, sg_message_id:string, sg_template_id:string, sg_template_name:string, reason:string, status:string, projectId: string
  */
 export const eventWebhook = async (req: Request, res: Response) => {
 
   const datasetId = 'sendgrid';
   const tableId = 'events';
 
+  const signature = req.get('x-twilio-email-event-webhook-signature');
+  const timestamp = req.get('x-twilio-email-event-webhook-timestamp');
+  const publicKey = 'MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEXSIvSlqV1okLs57Q0SgCSe8BNTztzDQIrvSwd91YL6gKAenGm8ih2NlSOcI4Yozj+DQhODWEz62x609zS1ZyRw==';
+
   try {
+    const webhook = new EventWebhook();
+    const ecdsa = webhook.convertPublicKeyToECDSA(publicKey);
+    const rawBody = req['rawBody'].toString()
+
+    const verified = webhook.verifySignature(ecdsa, rawBody, signature, timestamp);
+
+    if (!verified) {
+      console.error('request not from SendGrid');
+      res.status(200).send('No permission! 🤨');
+      return
+    }
+
     const bigquery = new BigQuery();
     const dataset = bigquery.dataset(datasetId);
     const table = dataset.table(tableId);
@@ -39,14 +58,13 @@ export const eventWebhook = async (req: Request, res: Response) => {
       'reason',
       'status',
       'projectId'
-      // category (array of strings)
     ];
     const rows = body.map(event => copyFields(event, fields));
 
-    table.insert(rows).catch(err => console.error('error while inserting in bigquery: ', JSON.stringify(err)));
+    table.insert(rows).catch(err => console.error('error while inserting: ', displayError(err)));
 
   } catch (e) {
-    console.error(JSON.stringify(e));
+    console.error(displayError(e));
   }
 
   // in any case we return 200 OK to the sendgrid servers
