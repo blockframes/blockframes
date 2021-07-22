7c4ebec340f32e3c3ec4e07bdaa4ba8ede7f2dae
samir <samir.touira@epitech.eu>
2021-07-22 18:30:23 +0200

    request changes applied, some last stuff to do

--- libs/utils/src/lib/pipes/time-since.pipe.ts
+++ libs/utils/src/lib/pipes/time-since.pipe.ts
@@ -17,27 +17,18 @@ export class TimeSincePipe implements PipeTransform {
 
     const seconds = Math.floor((new Date().getTime() - date.getTime()) / 1000);
 
-    let interval = seconds / 31536000;
+    let interval = seconds / (60 * 60 * 24 * 365);
 
     if (interval > 1) {
       return Math.floor(interval) + " years";
     }
-    interval = seconds / 2592000;
-    if (interval > 1) {
-      return Math.floor(interval) + " months";
-    }
+    if (interval >= 1) return Math.floor(interval) + (interval === 1 ? ' month' : ' months');
     interval = seconds / 86400;
-    if (interval > 1) {
-      return Math.floor(interval) + " days";
-    }
+    if (interval >= 1) return Math.floor(interval) + (interval === 1 ? ' day' : ' days');
     interval = seconds / 3600;
-    if (interval > 1) {
-      return Math.floor(interval) + " hours";
-    }
+    if (interval >= 1) return Math.floor(interval) + (interval === 1 ? ' hour' : ' hours');
     interval = seconds / 60;
-    if (interval > 1) {
-      return Math.floor(interval) + " minutes";
-    }
+    if (interval >= 1) return Math.floor(interval) + (interval === 1 ? ' minute' : ' minutes');
     return Math.floor(seconds) + " seconds";
   }
 }
