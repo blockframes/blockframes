68bbf673c3dbf8c9a1bc02918c316eff759d40c7
samir <samir.touira@epitech.eu>
2021-07-23 12:10:09 +0200

    request changes

--- libs/utils/src/lib/pipes/time-since.pipe.ts
+++ libs/utils/src/lib/pipes/time-since.pipe.ts
@@ -6,7 +6,7 @@ import { NgModule } from '@angular/core';
 })
 export class TimeSincePipe implements PipeTransform {
   /**
-   * Convert the date to numbers from the time display
+   * Converting the date to seconds, minutes, hours, months and years from the date created
    * Example: "1 hour ago"
    */
   transform(value: Date) {
@@ -18,17 +18,20 @@ export class TimeSincePipe implements PipeTransform {
     const seconds = Math.floor((new Date().getTime() - date.getTime()) / 1000);
 
     let interval = seconds / (60 * 60 * 24 * 365);
+    const d = new Date();
+    d.setMonth(d.getMonth() - 3);
 
     if (interval > 1) {
       return Math.floor(interval) + " years";
     }
-    if (interval >= 1) return Math.floor(interval) + (interval === 1 ? ' month' : ' months');
+    interval = seconds / 2592000;
+    if (interval >= 1)  return (Math.floor(interval) + (interval < 2 || interval === 0 ? ' month' : ' months'));
     interval = seconds / 86400;
-    if (interval >= 1) return Math.floor(interval) + (interval === 1 ? ' day' : ' days');
+    if (interval >= 1) return (Math.floor(interval) + (interval < 2 || interval === 0 ? ' day' : ' days'));
     interval = seconds / 3600;
-    if (interval >= 1) return Math.floor(interval) + (interval === 1 ? ' hour' : ' hours');
+    if (interval >= 1) return Math.floor(interval) + (interval < 2 || interval === 0 ? ' hour' : ' hours');
     interval = seconds / 60;
-    if (interval >= 1) return Math.floor(interval) + (interval === 1 ? ' minute' : ' minutes');
+    if (interval >= 1) return Math.floor(interval) + (interval < 2 || interval === 0 ? ' minute' : ' minutes');
     return Math.floor(seconds) + " seconds";
   }
 }
