20f266f177c6338f3b3047fba834f0193dc3b3cd
Yonga Springfield <springfieldyonga@outlook.com>
2021-08-31 14:00:11 +0100

    wip

--- libs/ui/src/lib/static-autocomplete/group/group.component.ts
+++ libs/ui/src/lib/static-autocomplete/group/group.component.ts
@@ -3,18 +3,25 @@ import {
   Component,
   Input,
   forwardRef,
-  Pipe, PipeTransform, ElementRef, ViewChild, OnInit, OnDestroy
+  Pipe, PipeTransform, ElementRef, ViewChild, OnInit, OnDestroy, Optional, Self, HostBinding
 } from "@angular/core";
 import {
   FormControl,
   ControlValueAccessor,
   NG_VALUE_ACCESSOR,
-  Validators
+  Validators,
+  NgControl,
+  Validator,
+  AbstractControl,
+  ValidationErrors,
+  NG_VALIDATORS
 } from "@angular/forms";
-import { BehaviorSubject, combineLatest, Observable, Subscription, defer } from "rxjs";
+import { BehaviorSubject, combineLatest, Observable, Subscription, defer, Subject } from "rxjs";
 import { map, startWith, shareReplay, pairwise } from "rxjs/operators";
 import { GroupScope, Scope, StaticGroup, staticGroups, staticModel } from '@blockframes/utils/static-model';
 import { boolean } from '@blockframes/utils/decorators/decorators';
+import { MatFormFieldControl } from "@angular/material/form-field";
+import { coerceBooleanProperty } from "@angular/cdk/coercion";
 
 
 type GroupMode = 'indeterminate' | 'checked' | 'unchecked';
@@ -60,32 +67,61 @@ function getItems(groups: StaticGroup[]): string[] {
   styleUrls: ['./group.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => StaticGroupComponent),
-      multi: true
-    }
+    { provide: MatFormFieldControl, useExisting: forwardRef(() => StaticGroupComponent) }
+    // {
+    //   provide: NG_VALIDATORS,
+    //   multi: true,
+    //   useExisting: forwardRef(() => StaticGroupComponent),
+    // },
+    // {
+    //   provide: NG_VALUE_ACCESSOR,
+    //   useExisting: forwardRef(() => StaticGroupComponent),
+    //   multi: true
+    // }
   ]
 })
-export class StaticGroupComponent implements ControlValueAccessor, OnInit, OnDestroy {
+export class StaticGroupComponent implements ControlValueAccessor, OnInit, OnDestroy, Validator, MatFormFieldControl<string[]> {
   private subs: Subscription[] = [];
   private onTouch: () => void;
-  modes: Record<string, Observable<GroupMode>> = {};
+  modes: Record<string, Observable<GroupMode>> = { };
   filteredGroups$: Observable<StaticGroup[]>;
   groups$ = new BehaviorSubject<StaticGroup[]>([]);
-
-  hidden: Record<string, boolean> = {}
+  private _placeholder = 'Tap to filter';
+  focused = false;
+  touched = false;
+  _required = false;
+  _disabled = false;
+  hidden: Record<string, boolean> = { }
   // all items includes the values of checked items which are not in the filter
   allItems: string[] = [];
 
+  @HostBinding() id = `example-tel-input-${Math.random()}`;
+
+
   @ViewChild('inputEl') input: ElementRef<HTMLInputElement>;
   @Input() displayAll = '';
-  @Input() @boolean required = false;
-  @Input() @boolean disabled = false;
-  @Input() placeholder = 'Tap to filter'
+  @Input()
+  get required() {
+    return this._required;
+  }
+  set required(req) {
+    this._required = coerceBooleanProperty(req);
+    this.stateChanges.next();
+  }
+  @Input()
+  get disabled(): boolean { return this._disabled; }
+  set disabled(value: boolean) {
+    this._disabled = coerceBooleanProperty(value);
+    this._disabled ? this.form.disable() : this.form.enable();
+    this.stateChanges.next();
+  }
   @Input() withoutValues: string[] = [];
   @Input() scope: GroupScope;
   @Input() icon: string;
+  @Input() set placeholder(plh) {
+    this._placeholder = plh;
+    this.stateChanges.next();
+  };
 
   form = new FormControl([], this.required ? [Validators.required] : []);
   // defer the startWith value with subscription happens to get first value
@@ -101,7 +137,14 @@ export class StaticGroupComponent implements ControlValueAccessor, OnInit, OnDes
   search = new FormControl();
   trackByLabel = (i: number, group: StaticGroup) => group.label;
 
-  constructor() {
+  constructor(
+    @Optional() @Self() public ngControl: NgControl,
+    private _elementRef: ElementRef<HTMLElement>,
+  ) {
+    if (this.ngControl != null) {
+      this.ngControl.valueAccessor = this;
+    }
+
     this.filteredGroups$ = combineLatest([
       this.groups$.asObservable(),
       this.search.valueChanges.pipe(startWith(this.search.value))
@@ -129,7 +172,7 @@ export class StaticGroupComponent implements ControlValueAccessor, OnInit, OnDes
     if (this.withoutValues.length) {
       for (const group of groups) {
         /* eslint-disable */
-        group.items = (group.items as  any[]).filter((item:string) => !this.withoutValues.includes(item));
+        group.items = (group.items as any[]).filter((item: string) => !this.withoutValues.includes(item));
       }
     }
     this.groups$.next(groups);
@@ -149,6 +192,14 @@ export class StaticGroupComponent implements ControlValueAccessor, OnInit, OnDes
   writeValue(value: string[]): void {
     this.form.reset(value);
   }
+  stateChanges = new Subject<void>();
+
+  set value(tel: string[] | null) {
+    this.stateChanges.next();
+  }
+
+
+
   registerOnChange(fn: () => void): void {
     const sub = this.form.valueChanges.subscribe(fn);
     this.subs.push(sub);
@@ -160,8 +211,72 @@ export class StaticGroupComponent implements ControlValueAccessor, OnInit, OnDes
     this.disabled = isDisabled;
   }
 
+  checkIsControlValid(values: string[]): ValidationErrors | null {
+    if (values?.length < 1) {
+      return {
+        mustBePositive: {
+          length: values.length
+        }
+      };
+    }
+    return null;
+  }
+
+  validate(control: AbstractControl): ValidationErrors | null {
+    const values = control.value;
+    return this.checkIsControlValid(values)
+  }
+
+  get errorState(): boolean {
+    return Boolean(this.checkIsControlValid(this.form.value));
+  }
+
+
+  onFocusIn(event: FocusEvent) {
+    if (!this.focused) {
+      this.focused = true;
+      this.stateChanges.next();
+    }
+  }
+
+  onFocusOut(event: FocusEvent) {
+    if (!this._elementRef.nativeElement.contains(event.relatedTarget as Element)) {
+      this.touched = true;
+      this.focused = false;
+      this.touched = true;
+      this.stateChanges.next();
+    }
+  }
+
+  get empty() {
+    return true;
+    // return this.form.value.length < 1;
+  }
+
+  setDescribedByIds(ids: string[]) {
+    const controlElement = this._elementRef.nativeElement
+      .querySelector('.example-tel-input-container')!;
+    controlElement.setAttribute('aria-describedby', ids.join(' '));
+  }
+
+
+  @HostBinding('class.floating')
+  get shouldLabelFloat() {
+    return this.focused || !this.empty;
+  }
+
+
+  onContainerClick(event: MouseEvent) {
+    if ((event.target as Element).tagName.toLowerCase() != 'input') {
+      this._elementRef.nativeElement.querySelector('input').focus();
+    }
+  }
+
+
+
   ngOnDestroy() {
     this.subs.forEach(sub => sub.unsubscribe());
+    this.stateChanges.complete();
   }
 
   // all check
