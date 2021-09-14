3482f5991ab3a4fe5d47db97f337ca64a4caa780
bruce <delorme.bruce.michel@gmail.com>
2021-09-14 13:54:33 +0200

    cleaning

--- apps/backend-functions/src/utils.ts
+++ apps/backend-functions/src/utils.ts
@@ -1,4 +1,3 @@
-import { request, RequestOptions } from 'https';
 import { functions, skipInMaintenance } from './internals/firebase';
 import { logErrors } from './internals/sentry';
 export { ErrorResultResponse } from '@blockframes/utils/utils';
@@ -40,30 +39,3 @@ export function onDocumentCreate(docPath: string, fn: FunctionType) {
     .onCreate(skipInMaintenance(logErrors(fn)));
 }
 
-////////////////////
-// MISC FUNCTIONS //
-////////////////////
-
-export function sendRequest<T = unknown>(options: RequestOptions, data?: unknown): Promise<T> {
-  const postData = JSON.stringify(data) ?? '';
-  const postOptions: RequestOptions = {
-    ...options,
-    headers: {
-      ...options.headers,
-      'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength(postData),
-    }
-  };
-	return new Promise((resolve, reject) => {
-		const req = request(options.method === 'POST' ? postOptions : options, res => {
-      let body = '';
-      res.on('data', chunk => body += chunk);
-      res.on('end', () => resolve(JSON.parse(body) as T));
-      res.on('error', e => reject(e))
-		});
-    req.on('error', e => reject(e));
-    if (options.method === 'POST') req.write(postData);
-
-    req.end();
-	});
-}
