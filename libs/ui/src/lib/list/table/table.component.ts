e259a3a30577b988c2612302651b3ee70aa7345e
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2021-09-16 13:43:41 +0200

    apply sorting

--- libs/ui/src/lib/list/table/table.component.ts
+++ libs/ui/src/lib/list/table/table.component.ts
@@ -4,6 +4,7 @@ import { debounceTime, map, startWith, switchMap, tap } from 'rxjs/operators';
 import { getDeepValue } from '@blockframes/utils/pipes/deep-key.pipe';
 import { FormControl } from '@angular/forms';
 import { coerceNumberProperty } from '@angular/cdk/coercion';
+import { boolean } from '@blockframes/utils/decorators/decorators';
 import { Paginator } from './paginator';
 
 /** Ascending sorting */
@@ -11,42 +12,6 @@ function sortValue<T>(a: T, b: T) {
   if (typeof a === 'string' && typeof b === 'string') return a.toUpperCase() > b.toUpperCase() ? 1 : -1;
   if (typeof a === 'number' && typeof b === 'number') return a - b;
   return 0;
-}
-
-@Directive({ selector: '[colRef]' })
-export class ColumnDirective<T> {
-  control = new FormControl(true);
-
-  @Input('colRef') name!: string;
-  @Input() sortable: '' | boolean;
-  @Input() label?: string;
-  @Input() sort?: '' | ((a: any, b: any) => number);
-  @Input() filter = (input: string, value: any, row: T) => {
-    if (typeof value !== 'string') return false;
-    return value.toLowerCase().includes(input);
-  }
-
-  constructor(public template: TemplateRef<any>) {}
-
-  get isSortable() {
-    return this.sortable === '' || !!this.sortable;
-  }
-
-  get asc() {
-    return this.control.value;
-  }
-
-  toggleSort() {
-    this.control.setValue(!this.control.value);
-  }
-
-  $sort(data: T[]): Observable<T[]> {
-    const sort = this.sort ? this.sort : sortValue;
-    return this.control.valueChanges.pipe(
-      map(asc => asc ? 1 : -1),
-      map(order => data.sort((a, b) => sort(a, b) * order)),
-    );
-  }
 }
 
 
@@ -61,56 +26,69 @@ function filterTable<T>(data: T[], value: string, columns: QueryList<ColumnDirec
   });
 }
 
-@Component({
-  selector: 'bf-table-filter',
-  template: `
-    <mat-form-field appearance="outline">
-      <mat-label>Filter</mat-label>
-      <input matInput [formControl]="control" />
-    </mat-form-field>
-  `,
-  styles: [`
-    :host { display: block; }
-    mat-form-field { display: block; }
-  `],
-  exportAs: 'bfTableFilter',
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class TableFilterComponent<T> {
-  control = new FormControl();
-  @Input() columns: string[];
-  
-  $filter(data: T[], columns: QueryList<ColumnDirective<T>>) {
-    return this.control.valueChanges.pipe(
-      debounceTime(200),
-      map(value => filterTable(data, value, columns)),
-      startWith(data),
-    );
+@Directive({ selector: '[colRef]' })
+export class ColumnDirective<T> {
+  control = new FormControl(true);
+
+  @Input('colRef') name!: string;
+  @Input() label?: string;
+  @Input() sort?: '' | ((a: any, b: any) => number);
+  @Input() filter = (input: string, value: any, row: T) => {
+    if (typeof value !== 'string') return false;
+    return value.toLowerCase().includes(input);
   }
 
+  constructor(public template: TemplateRef<any>) {}
+
+  get isSortable() {
+    return this.sort === '' || !!this.sort;
+  }
+
+  get asc() {
+    return this.control.value;
+  }
+
+  toggleSort() {
+    this.control.setValue(!this.control.value);
+  }
+
+  $sort(data: T[]): Observable<T[]> {
+    const sort = this.sort || sortValue;
+    return this.control.valueChanges.pipe(
+      map(asc => asc ? 1 : -1),
+      map(order => data.sort((a, b) => {
+        const sorting = sort(getDeepValue(a, this.name), getDeepValue(b, this.name));
+        return sorting * order;
+      })),
+    );
+  }
 }
 
+
+
+
+
+
+
+
+
 @Component({
-  selector: 'table[bfTable]',
+  selector: 'bf-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
-  host: {
-    class: 'mat-table',
-    role: 'table'
-  },
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TableComponent<T> {
   private dataSource = new BehaviorSubject<T[]>([]);
-  private page = new BehaviorSubject(0);
+  search = new FormControl();
   paginator = new Paginator();
   data$: Observable<T[]>;
 
   @ContentChildren(ColumnDirective) columns!: QueryList<ColumnDirective<T>>;
-  @Input() filter?: TableFilterComponent<T>;
-  @Input('bfTable') set source(source: T[]) {
+  @Input() set source(source: T[]) {
     this.dataSource.next(source);
   }
+  @Input() @boolean filter: boolean;
   @Input() set pagination(amount: number | string) {
     const pageSize = coerceNumberProperty(amount);
     if (typeof pageSize === 'number') this.paginator.pageSize = pageSize;
@@ -118,22 +96,14 @@ export class TableComponent<T> {
 
   constructor() {
     this.data$ = this.dataSource.asObservable().pipe(
-      switchMap(data => this.filter ? this.filter.$filter(data, this.columns) : of(data)),
-      switchMap(data => {
-        const sorting = this.columns.filter(c => c.isSortable).map(column => column.$sort(data));
-        if (!sorting.length) return of(data);
-        return merge(...sorting).pipe(startWith(data)); // Prevent first merging to overfire
-      }),
-      switchMap(data => this.paginate(data)),
+      switchMap(data => this.$filter(data)),
+      switchMap(data => this.$sort(data)),
+      switchMap(data => this.$paginate(data)),
+      tap(console.log),
     );
   }
 
-
-  ////////////////
-  // Pagination //
-  ////////////////
-
-  private paginate(data: T[]) {
+  private $paginate(data: T[]) {
     this.paginator.size = data.length;
     if (this.paginator.pageIndex > this.paginator.maxIndex) this.paginator.last();
     return this.paginator.state.pipe(
@@ -141,6 +111,21 @@ export class TableComponent<T> {
         if (!pageSize) return data;
         return data.slice(pageIndex * pageSize, (pageIndex + 1) * pageSize);
       })
+    );
+  }
+
+  private $sort(data: T[]) {
+    const sorting = this.columns.filter(c => c.isSortable).map(column => column.$sort(data));
+    if (!sorting.length) return of(data);
+    return merge(...sorting).pipe(startWith(data)); // Prevent first merging to overfire
+  }
+
+  private $filter(data: T[]) {
+    if (!this.filter) return of(data);
+    return this.search.valueChanges.pipe(
+      debounceTime(200),
+      map(value => filterTable(data, value, this.columns)),
+      startWith(data),
     );
   }
 }
--- libs/ui/src/lib/list/table/table.module.ts
+++ libs/ui/src/lib/list/table/table.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { TableComponent, TableFilterComponent, ColumnDirective } from './table.component';
+import { TableComponent, ColumnDirective } from './table.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -11,8 +11,8 @@ import {MatPaginatorModule} from '@angular/material/paginator';
 import { DeepKeyPipeModule } from '@blockframes/utils/pipes';
 
 @NgModule({
-  declarations: [TableComponent, TableFilterComponent, ColumnDirective],
-  exports: [TableComponent, TableFilterComponent, ColumnDirective],
+  declarations: [TableComponent, ColumnDirective],
+  exports: [TableComponent, ColumnDirective],
   imports: [
     CommonModule,
     ReactiveFormsModule,
