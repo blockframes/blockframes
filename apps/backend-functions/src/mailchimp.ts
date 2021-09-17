c62cd6fbbb2e29cfa6c49746385a096584090fd3
Antoine <antoinepoppe@hotmail.fr>
2021-09-17 16:11:10 +0200

    load lib dynamically

--- apps/backend-functions/src/mailchimp.ts
+++ apps/backend-functions/src/mailchimp.ts
@@ -1,14 +1,23 @@
-import mailchimp from "@mailchimp/mailchimp_marketing";
 import { mailchimpAPIKey, mailchimpServer, mailchimpListId } from './environments/environment';
 import { MailchimpTag } from '@blockframes/utils/mailchimp/mailchimp-model';
 
-mailchimp.setConfig({
-  apiKey: mailchimpAPIKey,
-  server: mailchimpServer,
-});
+let _mailchimp;
+
+async function getMailChimp() {
+  if (!_mailchimp) {
+    _mailchimp = await import("@mailchimp/mailchimp_marketing").then(mailchimp => mailchimp.default);
+  }
+  return _mailchimp
+}
 
 export const registerToNewsletters = async (data: {email: string, tags: MailchimpTag[]}) => {
   const {email: email_address, tags} = data;
+  const mailchimp = await getMailChimp();
+
+  mailchimp.setConfig({
+    apiKey: mailchimpAPIKey,
+    server: mailchimpServer,
+  });
 
   return await mailchimp.lists.addListMember(mailchimpListId, 
     {email_address, 
