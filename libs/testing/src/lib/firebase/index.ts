eeef68d35218adc8a364a87a322f2fc5ac4d69f8
Bruce <eelios@gmail.com>
2021-09-22 13:59:13 +0200

    do not use 4Go/cloud function by default #6622 (#6689)
    * updated runTimeConfig for functions
    
    * cleaning
    
    * new way of getting distinct functionsBuilder instances
    
    * cleaning
    
    * cleaning
    
    * cleaning
    
    * more cleaning
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>

--- libs/testing/src/lib/firebase/index.ts
+++ libs/testing/src/lib/firebase/index.ts
@@ -1,7 +1,3 @@
-import * as functions from './functions';
-
 export { deleteAllUsers, importAllUsers } from './auth';
 export { AdminAuthMocked } from './adminAuthMocked';
 export { StorageMocked } from './storageMocked';
-
-export { functions }
