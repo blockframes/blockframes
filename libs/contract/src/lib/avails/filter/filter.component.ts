e593343fbd5b80d0691516541dcb6297f7ae61b2
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2021-09-28 16:03:10 +0200

    Refactor Avails Filter

---
+++ libs/contract/src/lib/avails/filter/filter.component.ts
@@ -0,0 +1,31 @@
+import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
+import { boolean } from '@blockframes/utils/decorators/decorators';
+import { AvailsForm, AvailControl } from '../form/avails.form';
+
+type AvailsKeys = keyof AvailControl;
+type AvailsRecord = Partial<Record<AvailsKeys, boolean>>;
+function toRecord(value: AvailsKeys | AvailsKeys[]): AvailsRecord {
+  const keys = (Array.isArray(value)) ? value : [value];
+  const result = {};
+  for (const key of ['territories', 'medias', 'duration', 'exclusive'] as const) {
+    result[key] = keys.includes(key);
+  }
+  return result;
+}
+
+@Component({
+  selector: 'form[availsFilter]',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AvailsFilterComponent {
+  hideKeys: AvailsRecord = {};
+  @Input('availsFilter') form: AvailsForm;
+  @HostBinding('class.vertical') @Input() @boolean vertical: boolean;
+  @Input() @boolean required: boolean;
+  @Input() set hide(keys: AvailsKeys | AvailsKeys[]) {
+    if (!keys) return;
+    this.hideKeys = toRecord(keys);
+  }
+}
