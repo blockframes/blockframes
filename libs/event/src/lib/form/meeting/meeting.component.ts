f94bdb230893ff084afccbf4e1fbd670d44937ad
Bruce <eelios@gmail.com>
2021-09-30 12:32:56 +0200

    Rework event form structure  #5895 (#6674)
    * WIP
    
    * cleaning WIP
    
    * comments
    
    * cleaning
    
    * cleaning
    
    * typo
    
    * cleaning
    
    * added comment to split work
    
    * moved shell component
    
    * cleaning
    
    * no way found to use relative route here
    
    * add underline on tab titles
    
    * add popup on event form quit
    
    * event form popup review
    
    * correct bad sliding after movie form update
    
    * removed comment
    
    * add form animations
    
    * form style
    
    * renaming (#6697)
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>
    
    * added tabs to ended event (#6704)
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>
    
    * event form css fix
    
    * correct review
    
    * standardize event form and review
    
    * fix invitation title
    
    * cleaning
    
    * useless css
    
    * removed edit component
    
    * removed review component
    
    * typo
    
    * update after review
    
    * loadChildren
    
    * debug
    
    * improved css
    
    * css cleaning
    
    * css cleaning
    
    * useless section
    
    * disabled input outline style in general style (#6779)
    
    * css update
    
    * css update
    
    * update after review
    
    * added comment
    
    * last update
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>
    Co-authored-by: Antoine <antoinepoppe@hotmail.fr>
    Co-authored-by: Poppe-A <35397614+Poppe-A@users.noreply.github.com>

---
+++ libs/event/src/lib/form/meeting/meeting.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, ChangeDetectionStrategy, ViewEncapsulation  } from '@angular/core';
+import { OrganizationQuery } from '@blockframes/organization/+state';
+import { UserService } from '@blockframes/user/+state';
+import { User } from '@blockframes/auth/+state';
+import { Observable } from 'rxjs';
+import { switchMap, } from 'rxjs/operators';
+import { DynamicTitleService } from '@blockframes/utils/dynamic-title/dynamic-title.service';
+import { EventFormShellComponent } from '../shell/shell.component';
+
+@Component({
+  selector: 'event-meeting',
+  templateUrl: './meeting.component.html',
+  styleUrls: ['./meeting.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MeetingComponent implements OnInit {
+
+  members$: Observable<User[]>;
+
+  constructor(
+    private userService: UserService,
+    private orgQuery: OrganizationQuery,
+    private dynTitle: DynamicTitleService,
+    private shell: EventFormShellComponent,
+  ) { }
+
+  get formMeta() {
+    return this.shell.form.get('meta');
+  }
+
+  get link() {
+    return this.shell.link;
+  }
+
+  ngOnInit(): void {
+    this.dynTitle.setPageTitle('Add an event', 'Meeting info');
+
+    this.members$ = this.orgQuery.selectActive().pipe(
+      switchMap(org => this.userService.valueChanges(org.userIds))
+    )
+  }
+
+}
--- libs/event/src/lib/form/meeting/meeting.module.ts
+++ libs/event/src/lib/form/meeting/meeting.module.ts
@@ -1,56 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { EditComponent } from './edit.component';
+import { MeetingComponent } from './meeting.component';
+import { EventFromShellModule } from '../shell/shell.module';
 import { DisplayNameModule } from '@blockframes/utils/pipes';
-import { FilePickerModule } from '@blockframes/media/file/picker/picker.module';
-import { EventEditModule as LayoutEventEditModule } from '@blockframes/event/layout/edit/edit.module';
-import { FilePreviewModule } from '@blockframes/media/file/preview/preview.module';
+import { RouterModule } from '@angular/router';
 
 // Material
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { FileNameModule } from '@blockframes/utils/pipes/fileName.pipe';
-import { EventAnalyticsModule } from '@blockframes/event/components/analytics/analytics.module';
-import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
 import { ClipboardModule } from '@angular/cdk/clipboard';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-  declarations: [EditComponent],
+  declarations: [MeetingComponent],
   imports: [
     CommonModule,
     FlexLayoutModule,
     ReactiveFormsModule,
-    LayoutEventEditModule,
-    EventAnalyticsModule,
+    EventFromShellModule,
     DisplayNameModule,
-    FilePickerModule,
-    FileNameModule,
     ClipboardModule,
-    FilePreviewModule,
+    RouterModule.forChild([{ path: '', component: MeetingComponent }]),
 
     // Material
-    MatProgressSpinnerModule,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatListModule,
     MatButtonModule,
     MatIconModule,
-    MatDividerModule,
     MatTooltipModule,
-
-    RouterModule.forChild([{ path: '', component: EditComponent }])
   ]
 })
-export class EventEditModule { }
+export class MeetingModule { }
