3d40e59bb6cf9499c33287a5b416519bccdea5cc
bruce <delorme.bruce.michel@gmail.com>
2021-09-30 11:11:19 +0200

    update after review

--- libs/event/src/lib/pages/analytics/analytics.module.ts
+++ libs/event/src/lib/pages/analytics/analytics.module.ts
@@ -2,15 +2,33 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnalyticsComponent } from './analytics.component';
 import { EventFromShellModule } from '@blockframes/event/form/shell/shell.module';
-import { EventAnalyticsModule } from '@blockframes/event/components/analytics/analytics.module';
 import { RouterModule } from '@angular/router';
+import { ImageModule } from '@blockframes/media/image/directives/image.module';
+import { TableFilterModule } from '@blockframes/ui/list/table-filter/table-filter.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ToLabelModule } from '@blockframes/utils/pipes';
+import { DurationModule } from '@blockframes/utils/pipes/duration.pipe';
+
+// Angular
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AnalyticsComponent],
   imports: [
     CommonModule,
+    ImageModule,
+    TableFilterModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    ToLabelModule,
+    DurationModule,
+    MatProgressSpinnerModule,
     EventFromShellModule,
-    EventAnalyticsModule,
     RouterModule.forChild([{ path: '', component: AnalyticsComponent }]),
   ]
 })
