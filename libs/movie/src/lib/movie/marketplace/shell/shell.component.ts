54a274c614be501bc44a6643a36d21146ee52842
Remco <RemcoSimonides@gmail.com>
2021-10-07 09:39:22 +0200

    6479 show only public videos and toggle privacy in CRM

--- libs/movie/src/lib/movie/marketplace/shell/shell.component.ts
+++ libs/movie/src/lib/movie/marketplace/shell/shell.component.ts
@@ -1,7 +1,7 @@
 import { Component, ChangeDetectionStrategy, Input, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { Observable, } from 'rxjs';
-import { Movie } from '@blockframes/movie/+state/movie.model';
+import { createMovie, Movie } from '@blockframes/movie/+state/movie.model';
 import { MovieQuery } from '@blockframes/movie/+state/movie.query';
 import { RouterQuery } from '@datorama/akita-ng-router-store';
 import { routeAnimation } from '@blockframes/utils/animations/router-animations';
@@ -10,6 +10,7 @@ import { FileListPreviewComponent } from '@blockframes/media/file/preview-list/p
 import { MatDialog } from '@angular/material/dialog';
 import { StorageFile } from '@blockframes/media/+state/media.firestore';
 import { scrollIntoView } from '@blockframes/utils/browser/utils';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'title-marketplace-shell',
@@ -33,7 +34,15 @@ export class TitleMarketplaceShellComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.movie$ = this.movieQuery.selectActive();
+    this.movie$ = this.movieQuery.selectActive().pipe(
+      map(movie => {
+        let title = createMovie(movie);
+        if (movie.promotional.videos?.otherVideos) {
+          title.promotional.videos.otherVideos = title.promotional.videos.otherVideos.filter(video => video.privacy === 'public');
+        }
+        return title;
+      })
+    );
   }
 
   scrollIntoView() {
