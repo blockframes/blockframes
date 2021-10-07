377d246c3558c607c794ab70963c00c1b5223410
Antoine <antoinepoppe@hotmail.fr>
2021-10-07 12:42:40 +0200

    correct type/static-model

--- libs/event/src/lib/+state/event.firestore.ts
+++ libs/event/src/lib/+state/event.firestore.ts
@@ -1,7 +1,7 @@
 import type firebase from 'firebase';
 import { AnalyticsEvents } from '@blockframes/utils/analytics/analytics-model';
 import { StorageFile } from '@blockframes/media/+state/media.firestore';
-
+import { AccessibilityTypes } from '@blockframes/utils/static-model/types'
 type Timestamp = firebase.firestore.Timestamp;
 
 // Event types
@@ -43,8 +43,6 @@ export interface Screening {
   description: string;
 }
 
-
-export type AccessibilityTypes = 'public' | 'invitation-only' | 'private';
 export interface EventBase<D extends Timestamp | Date, Meta extends EventMeta = Record<string, unknown>> {
   id: string;
   ownerOrgId: string;
