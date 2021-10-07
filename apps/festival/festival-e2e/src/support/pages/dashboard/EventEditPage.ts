08a5a30d4223b41b3cbec6573be800be5039bf04
Antoine <antoinepoppe@hotmail.fr>
2021-10-07 10:57:26 +0200

    update event form e2e test

--- apps/festival/festival-e2e/src/support/pages/dashboard/EventEditPage.ts
+++ apps/festival/festival-e2e/src/support/pages/dashboard/EventEditPage.ts
@@ -3,22 +3,22 @@ import { SEC } from '@blockframes/e2e/utils';
 
 export default class EventEditPage {
   constructor() {
-    cy.get('event-edit', { timeout: 90 * SEC });
+    cy.get('event-details-edit', { timeout: 90 * SEC });
     cy.wait(10 * SEC);
   }
 
   addEventTitle(title: string) {
-    cy.get('event-edit input[formControlName="title"]').clear().type(title);
+    cy.get('event-details-edit input[formControlName="title"]').clear().type(title);
   }
 
   //Sets if event is full day.
   checkAllDay(fullDay: boolean = true) {
     if (fullDay) {
-      cy.get('event-edit mat-slide-toggle[test-id=all-day]', { timeout: 3 * SEC })
+      cy.get('event-details-edit mat-slide-toggle[test-id=all-day]', { timeout: 3 * SEC })
         .find('input')
         .check({ force: true });
     } else {
-      cy.get('event-edit mat-slide-toggle[test-id=all-day]', { timeout: 3 * SEC })
+      cy.get('event-details-edit mat-slide-toggle[test-id=all-day]', { timeout: 3 * SEC })
         .find('input')
         .uncheck({ force: true });
     }
@@ -27,13 +27,13 @@ export default class EventEditPage {
   selectDate(date: Date) {
     // Start Date
     cy.get('mat-form-field[test-id=event-start]').click();
-    cy.get('event-edit time-picker[formControlName=start]')
+    cy.get('event-details-edit time-picker[formControlName=start]')
       .get('tbody')
       .contains(date.getDate())
       .click();
     // End Date
     cy.get('mat-form-field[test-id=event-end]').click();
-    cy.get('event-edit time-picker[formControlName=end]')
+    cy.get('event-details-edit time-picker[formControlName=end]')
       .get('tbody')
       .contains(date.getDate())
       .click();
@@ -42,18 +42,18 @@ export default class EventEditPage {
   //set event access
   uncheckPrivate(isPublic: boolean = false) {
     if (!isPublic) {
-      cy.get('event-edit mat-checkbox[formControlName="isPrivate"]', { timeout: 3 * SEC })
-        .find('input')
-        .check({ force: true });
+      cy.get('event-details-edit [type="radio"]', { timeout: 3 * SEC })
+        .first()
+        .check({force: true});
     } else {
-      cy.get('event-edit mat-checkbox[formControlName="isPrivate"]', { timeout: 3 * SEC })
-        .find('input')
-        .uncheck({ force: true });
+      cy.get('event-details-edit [type="radio"]', { timeout: 3 * SEC })
+        .eq(2)
+        .check({force: true});
     }
   }
 
   selectMovie(movieName: string) {
-    cy.get('event-edit mat-select[formControlName=titleId]', { timeout: 100 * SEC }).click();
+    cy.get('event-details-edit mat-select[formControlName=titleId]', { timeout: 100 * SEC }).click();
     cy.get('mat-option').contains(movieName).click();
   }
 
@@ -69,18 +69,18 @@ export default class EventEditPage {
     if (Array.isArray(email)) {
       let index = 0;
       while (index < email.length) {
-        cy.get('event-edit algolia-chips-autocomplete input').type(email[index]).type('{enter}');
+        cy.get('event-details-edit algolia-chips-autocomplete input').type(email[index]).type('{enter}');
         index++;
       }
     } else {
-      cy.get('event-edit algolia-chips-autocomplete input').type(email).type('{enter}');
+      cy.get('event-details-edit algolia-chips-autocomplete input').type(email).type('{enter}');
     }
-    cy.get('event-edit button[test-id=event-invite]').click({ timeout: 0.5 * SEC });
+    cy.get('event-details-edit button[test-id=event-invite]').click({ timeout: 0.5 * SEC });
     cy.wait(2 * SEC);
   }
 
   copyGuests() {
-    cy.get('event-edit invitation-guest-list button[test-id=invitation-copy]').click();
+    cy.get('event-details-edit invitation-guest-list button[test-id=invitation-copy]').click();
   }
 
   saveEvent() {
