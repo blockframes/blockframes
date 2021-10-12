09c129f45953ed8604745ab69faeb26bce30849f
bruce <delorme.bruce.michel@gmail.com>
2021-10-12 12:04:11 +0200

    lower waits

--- apps/crm/crm-e2e/src/integration/app.spec.ts
+++ apps/crm/crm-e2e/src/integration/app.spec.ts
@@ -2,12 +2,12 @@
 
 describe('crm-crm', () => {
   beforeEach(() => {
-    cy.wait(10 * SEC);
+    cy.wait(1 * SEC);
     cy.visit('/');
-    cy.wait(20 * SEC);
+    cy.wait(1 * SEC);
   });
 
   it('TBD Test', () => {
-    cy.wait(300 * SEC);
+    cy.wait(1 * SEC);
   });
 });
