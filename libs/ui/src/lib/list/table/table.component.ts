c17de879b78c4462fb50ea112219101bd5f78c7b
Yonga Springfield <springfieldyonga@outlook.com>
2021-10-26 12:24:58 +0100

    refactor

--- libs/ui/src/lib/list/table/table.component.ts
+++ libs/ui/src/lib/list/table/table.component.ts
@@ -10,10 +10,11 @@ import { Paginator } from './paginator';
 /** Ascending sorting */
 function sortValue<T>(a: T, b: T) {
   if (typeof a === 'string' && typeof b === 'string') return a.toUpperCase() > b.toUpperCase() ? 1 : -1;
-  if (
-    (typeof a === 'number' && typeof b === 'number') ||
-    (typeof a === 'boolean' && typeof b === 'boolean')
-  ) return (a as number) - (b as number);
+  if (typeof a === 'number' && typeof b === 'number') return a - b;
+  if (typeof a === 'boolean' && typeof b === 'boolean') {
+    if (a && b) return 0;
+    return a ? 1 : -1;
+  }
   return 0;
 }
 
