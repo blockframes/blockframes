f7f0d03debb2b215d605e62ff799333a99043b74
Bruce <eelios@gmail.com>
2021-10-27 16:10:44 +0200

    fixed guard (#6977)
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>

--- libs/event/src/lib/layout/main/main.module.ts
+++ libs/event/src/lib/layout/main/main.module.ts
@@ -21,7 +21,6 @@ import { EventGuard } from '../../guard/event.guard';
 import { EventActiveGuard } from '../../guard/event-active.guard';
 import { SessionGuard } from '../../guard/session.guard';
 import { IdentityCheckGuard } from '../../guard/identity-check-guard';
-import { AnonymousAuthGuard } from '@blockframes/auth/guard/anonymous-auth-guard';
 import { EventRoleGuard } from '../../guard/event-role.guard';
 import { InvitationGuard } from '@blockframes/invitation/guard/invitations.guard';
 import { NotificationsGuard } from '@blockframes/notification/notifications.guard';
@@ -36,74 +35,80 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
-const routes: Routes = [{
-  path: ':eventId',
-  component: MainComponent,
-  canActivate: [AnonymousAuthGuard, EventActiveGuard, InvitationGuard, NotificationsGuard],
-  canDeactivate: [AnonymousAuthGuard, EventActiveGuard, InvitationGuard, NotificationsGuard],
-  children: [
-    {
-      path: '',
-      canActivate: [IdentityCheckGuard],
-      loadChildren: () => import('./../../pages/role/role.module').then(m => m.RoleModule),
-    },
-    {
-      path: 'r',
-      canActivate: [EventRoleGuard],
-      children: [
-        {
-          path: '',
-          redirectTo: 'i',
-          pathMatch: 'full'
-        },
-        {
-          path: 'identity',
-          loadChildren: () => import('./../../pages/identity/identity.module').then(m => m.IdentityModule),
-        },
-        {
-          path: 'email',
-          loadChildren: () => import('./../../pages/email/email.module').then(m => m.EmailModule),
-        },
-        {
-          path: 'email-verify',
-          loadChildren: () => import('./../../pages/email-verify/email-verify.module').then(m => m.EmailVerifyModule),
-        },
-        {
-          path: 'login',
-          loadChildren: () => import('./../../pages/login/login.module').then(m => m.LoginModule),
-        },
-        {
-          path: 'i',
-          canActivate: [EventAccessGuard],
-          children: [
-            {
-              path: '',
-              canDeactivate: [EventActiveGuard],
-              loadChildren: () => import('./../../pages/view/view.module').then(m => m.EventViewModule),
-            },
-            {
-              path: 'session',
-              canActivate: [EventGuard, SessionGuard],
-              canDeactivate: [EventActiveGuard, EventGuard],
-              loadChildren: () => import('./../../pages/session/session.module').then(m => m.SessionModule),
-            },
-            {
-              path: 'lobby',
-              canActivate: [EventGuard],
-              canDeactivate: [EventGuard],
-              loadChildren: () => import('./../../pages/lobby/lobby.module').then(m => m.LobbyModule),
-            },
-            {
-              path: 'ended',
-              canActivate: [EventGuard],
-              loadChildren: () => import('./../../pages/ended/meeting-ended.module').then(m => m.MeetingEndedModule),
-            }
-          ]
-        }
-      ]
-    }
-  ]
-}];
+const routes: Routes = [
+  {
+    path: ':eventId',
+    component: MainComponent,
+    canActivate: [EventActiveGuard, InvitationGuard, NotificationsGuard],
+    canDeactivate: [EventActiveGuard, InvitationGuard, NotificationsGuard],
+    children: [
+      {
+        path: '',
+        canActivate: [IdentityCheckGuard],
+        loadChildren: () => import('./../../pages/role/role.module').then(m => m.RoleModule),
+      },
+      {
+        path: 'r',
+        canActivate: [EventRoleGuard],
+        children: [
+          {
+            path: '',
+            redirectTo: 'i',
+            pathMatch: 'full'
+          },
+          {
+            path: 'identity',
+            loadChildren: () => import('./../../pages/identity/identity.module').then(m => m.IdentityModule),
+          },
+          {
+            path: 'email',
+            loadChildren: () => import('./../../pages/email/email.module').then(m => m.EmailModule),
+          },
+          {
+            path: 'email-verify',
+            loadChildren: () => import('./../../pages/email-verify/email-verify.module').then(m => m.EmailVerifyModule),
+          },
+          {
+            path: 'login',
+            loadChildren: () => import('./../../pages/login/login.module').then(m => m.LoginModule),
+          },
+          {
+            path: 'i',
+            canActivate: [EventAccessGuard],
+            children: [
+              {
+                path: '',
+                canDeactivate: [EventActiveGuard],
+                loadChildren: () => import('./../../pages/view/view.module').then(m => m.EventViewModule),
+              },
+              {
+                path: 'session',
+                canActivate: [EventGuard, SessionGuard],
+                canDeactivate: [EventActiveGuard, EventGuard],
+                loadChildren: () => import('./../../pages/session/session.module').then(m => m.SessionModule),
+              },
+              {
+                path: 'lobby',
+                canActivate: [EventGuard],
+                canDeactivate: [EventGuard],
+                loadChildren: () => import('./../../pages/lobby/lobby.module').then(m => m.LobbyModule),
+              },
+              {
+                path: 'ended',
+                canActivate: [EventGuard],
+                loadChildren: () => import('./../../pages/ended/meeting-ended.module').then(m => m.MeetingEndedModule),
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '**',
+    loadChildren: () => import('@blockframes/ui/error/error-not-found.module').then(m => m.ErrorNotFoundModule)
+  }
+];
 
 @NgModule({
   declarations: [MainComponent],
