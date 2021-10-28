d01a5bc2e0642025dbccb6dc84a7f8a75ad18d32
Antoine <antoinepoppe@hotmail.fr>
2021-10-28 14:48:24 +0200

    catch log error

--- libs/event/src/lib/pages/login/login.component.ts
+++ libs/event/src/lib/pages/login/login.component.ts
@@ -1,9 +1,8 @@
-import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit, ViewChild, TemplateRef } from '@angular/core';
 import { AuthService, AuthStore } from '@blockframes/auth/+state';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { EventQuery } from '@blockframes/event/+state';
 
 @Component({
   selector: 'event-login',
@@ -12,25 +11,24 @@ import { EventQuery } from '@blockframes/event/+state';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EventLoginComponent implements OnInit {
-
+  @ViewChild('customSnackBarTemplate') customSnackBarTemplate: TemplateRef<unknown>;
+  public loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(24)])
+  });
+  public buttonText = 'Log in';
+  private snackbarDuration = 8000;
+  
   constructor(
     private service: AuthService,
     private authStore: AuthStore,
     private router: Router,
     private route: ActivatedRoute,
     private snackBar: MatSnackBar,
-    private eventQuery: EventQuery,
     ) { }
   
-  public eventId = this.eventQuery.getActiveId();
-  public loginForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(24)])
-  });
-
   ngOnInit() {
     const { email } = this.route.snapshot.queryParams;
-    this.eventId = this.eventQuery.getActiveId();
     this.loginForm.get('email').setValue(email);
   }
 
@@ -39,11 +37,22 @@ export class EventLoginComponent implements OnInit {
       this.snackBar.open('Form invalid, please check error messages', 'close', { duration: 2000 });
       return;
     }
-    await this.service.deleteAnonymousUser();
-    await this.service.signin(this.loginForm.value.email, this.loginForm.value.password)
-    .catch(_ => {
-      this.snackBar.open('Informations not valid', 'close', { duration: 2000 });
-    });
+    try {
+      this.buttonText = 'Logging in...'
+      const { email, password } = this.loginForm.value;
+      await this.service.deleteAnonymousUser();
+      await this.service.signin(email.trim(), password);
+    } catch (err) {
+      this.buttonText = 'Log in';
+      console.error(err); // let the devs see what happened
+      if (err.message.includes('INTERNAL ASSERTION FAILED')) {
+        this.snackBar.open('Network error. Please refresh this page.', 'close', { duration: this.snackbarDuration });
+      } else if (err.code === 'auth/user-not-found') {
+        this.snackBar.openFromTemplate(this.customSnackBarTemplate, { duration: this.snackbarDuration });
+      } else {
+        this.snackBar.open(err.message, 'close', { duration: this.snackbarDuration });
+      }
+    }
   }
 
   clickBack() {
