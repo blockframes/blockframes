88d57e07a860dca3617bd05dfe1d1c0fb3dbc9c3
Bruce <eelios@gmail.com>
2021-11-02 11:03:15 +0100

    debug (#7013)
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>

--- libs/ui/src/lib/layout/event/event.module.ts
+++ libs/ui/src/lib/layout/event/event.module.ts
@@ -1,14 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
-import { ScrollingModule } from '@angular/cdk/scrolling';
-import { RouterModule } from '@angular/router';
 import { EventComponent } from './event.component';
 import { ImageModule } from '@blockframes/media/image/directives/image.module';
 import { MatLayoutModule } from '../layout.module';
 
 // Widgets
-import { SearchWidgetModule } from '@blockframes/ui/search-widget';
 import { AuthWidgetModule } from '@blockframes/auth/components/widget/widget.module';
 import { AppBarModule } from '@blockframes/ui/app-bar';
 import { AppLogoModule } from '../app-logo/app-logo.module';
@@ -17,13 +14,11 @@ import { AsideModule } from '../marketplace/aside/aside.module';
 
 // Material
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatBadgeModule } from '@angular/material/badge';
-import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [EventComponent],
