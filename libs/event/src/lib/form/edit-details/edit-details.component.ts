0d633f93dd4dfacb0bdaabf71e68c92bdf83b43f
bruce <delorme.bruce.michel@gmail.com>
2021-11-09 16:23:16 +0100

    suggested updates

--- libs/event/src/lib/form/edit-details/edit-details.component.ts
+++ libs/event/src/lib/form/edit-details/edit-details.component.ts
@@ -7,6 +7,8 @@ import { InvitationService } from '@blockframes/invitation/+state';
 import { Intercom } from 'ng-intercom';
 import { MatDialog } from '@angular/material/dialog';
 import { Subscription } from 'rxjs';
+import { EventService } from '@blockframes/event/+state';
+import { AccessibilityTypes } from '@blockframes/utils/static-model';
 
 @Component({
   selector: '[form] event-details-edit',
@@ -19,32 +21,41 @@ export class EditDetailsComponent implements OnInit, OnDestroy {
   @Input() @boolean displayPrivacySettings = false;
   @ViewChild('noAccessibilityChange', { static: true }) noAccessibilityChange: TemplateRef<any>;
   private dialogSub: Subscription;
+  private accessibilitySub: Subscription;
   appName: string = appName[getCurrentApp(this.routerQuery)];
-  currentAccessibilityValue: string;
+  private previouslySavedAccessibility: AccessibilityTypes;
 
   constructor(
     private routerQuery: RouterQuery,
     private invitationService: InvitationService,
+    private eventService: EventService,
     private dialog: MatDialog,
     @Optional() private intercom: Intercom
   ) { }
 
   ngOnInit() {
-    this.currentAccessibilityValue = this.form.value.accessibility;
+    if (this.displayPrivacySettings) {
+      this.accessibilitySub = this.eventService.valueChanges(this.form.value.id).subscribe(e => {
+        this.previouslySavedAccessibility = e.accessibility;
+      });
+    }
   }
 
   ngOnDestroy() {
     if (this.dialogSub) {
       this.dialogSub.unsubscribe();
     }
+    if (this.accessibilitySub) {
+      this.accessibilitySub.unsubscribe();
+    }
   }
 
-  async radioButtonChanged(e) {
-    if (this.currentAccessibilityValue !== 'private' && e.value === 'private') {
+  async accessibilityChanged(e) {
+    if (this.previouslySavedAccessibility !== 'private' && e.value === 'private') {
       const invitations = await this.invitationService.getValue(ref => ref.where('type', '==', 'attendEvent').where('eventId', '==', this.form.value.id));
 
       if (invitations.length) {
-        this.form.patchValue({ accessibility: this.currentAccessibilityValue });
+        this.form.patchValue({ accessibility: this.previouslySavedAccessibility });
 
         const dialogRef = this.dialog.open(this.noAccessibilityChange, {
           width: '80%'
@@ -57,6 +68,5 @@ export class EditDetailsComponent implements OnInit, OnDestroy {
         return;
       }
     }
-    this.currentAccessibilityValue = e.value;
   }
 }
