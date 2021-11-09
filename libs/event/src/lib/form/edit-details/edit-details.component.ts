5bf75c5ca97ac748f1cbb49863b3cd2e0d9a653b
Poppe-A <35397614+Poppe-A@users.noreply.github.com>
2021-11-09 17:03:36 +0100

    Display popin when user change event access status (#7066)
    * Display popin when user change event access status
    
    * change accessibility condition
    
    * suggested updates (#7081)
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>
    Co-authored-by: Bruce <eelios@gmail.com>

--- libs/event/src/lib/form/edit-details/edit-details.component.ts
+++ libs/event/src/lib/form/edit-details/edit-details.component.ts
@@ -1,8 +1,14 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit, Optional, TemplateRef, ViewChild } from '@angular/core';
 import { EventForm } from '../event.form';
 import { appName, getCurrentApp } from '@blockframes/utils/apps';
 import { RouterQuery } from '@datorama/akita-ng-router-store';
 import { boolean } from '@blockframes/utils/decorators/decorators';
+import { InvitationService } from '@blockframes/invitation/+state';
+import { Intercom } from 'ng-intercom';
+import { MatDialog } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
+import { EventService } from '@blockframes/event/+state';
+import { AccessibilityTypes } from '@blockframes/utils/static-model';
 
 @Component({
   selector: '[form] event-details-edit',
@@ -10,10 +16,57 @@ import { boolean } from '@blockframes/utils/decorators/decorators';
   styleUrls: ['./edit-details.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EditDetailsComponent {
+export class EditDetailsComponent implements OnInit, OnDestroy {
   @Input() form: EventForm;
-  @Input() @boolean displayPrivacySettings = false; 
-  appName: string =  appName[getCurrentApp(this.routerQuery)];
-  
-  constructor(private routerQuery: RouterQuery) {}
+  @Input() @boolean displayPrivacySettings = false;
+  @ViewChild('noAccessibilityChange', { static: true }) noAccessibilityChange: TemplateRef<any>;
+  private dialogSub: Subscription;
+  private accessibilitySub: Subscription;
+  appName: string = appName[getCurrentApp(this.routerQuery)];
+  private previouslySavedAccessibility: AccessibilityTypes;
+
+  constructor(
+    private routerQuery: RouterQuery,
+    private invitationService: InvitationService,
+    private eventService: EventService,
+    private dialog: MatDialog,
+    @Optional() private intercom: Intercom
+  ) { }
+
+  ngOnInit() {
+    if (this.displayPrivacySettings) {
+      this.accessibilitySub = this.eventService.valueChanges(this.form.value.id).subscribe(e => {
+        this.previouslySavedAccessibility = e.accessibility;
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.dialogSub) {
+      this.dialogSub.unsubscribe();
+    }
+    if (this.accessibilitySub) {
+      this.accessibilitySub.unsubscribe();
+    }
+  }
+
+  async accessibilityChanged(e) {
+    if (this.previouslySavedAccessibility !== 'private' && e.value === 'private') {
+      const invitations = await this.invitationService.getValue(ref => ref.where('type', '==', 'attendEvent').where('eventId', '==', this.form.value.id));
+
+      if (invitations.length) {
+        this.form.patchValue({ accessibility: this.previouslySavedAccessibility });
+
+        const dialogRef = this.dialog.open(this.noAccessibilityChange, {
+          width: '80%'
+        });
+        this.dialogSub = dialogRef.afterClosed().subscribe(contactTeam => {
+          if (contactTeam) {
+            this.intercom.show();
+          }
+        });
+        return;
+      }
+    }
+  }
 }
