8ad0bf594dd8e6a883f2c2919dfda8af9d7f26c8
Bruce <eelios@gmail.com>
2021-11-16 18:31:25 +0100

    safari banner & watermark (#7146)
    * safari banner & watermark
    
    * update after review
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>

--- apps/festival/festival/src/app/event/event.component.ts
+++ apps/festival/festival/src/app/event/event.component.ts
@@ -1,4 +1,8 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, pluck, switchMap } from 'rxjs/operators';
+import { EventService } from '@blockframes/event/+state';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'festival-event',
@@ -6,4 +10,20 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./event.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EventComponent { }
+export class EventComponent implements OnInit {
+
+  isMeeting$: Observable<boolean>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private service: EventService,
+  ) { }
+
+  ngOnInit() {
+    this.isMeeting$ = this.route.params.pipe(
+      pluck('eventId'),
+      switchMap((eventId: string) => this.service.queryDocs(eventId)),
+      map(event => event?.type === 'meeting'),
+    );
+  }
+}
