027dfd70ec40473649f4732504b78621159b7e36
Remco <RemcoSimonides@gmail.com>
2021-11-18 10:49:47 +0100

    7166 small improvements

--- libs/invitation/src/lib/components/item/item.module.ts
+++ libs/invitation/src/lib/components/item/item.module.ts
@@ -13,14 +13,15 @@ import { DisplayNameModule } from '@blockframes/utils/pipes/display-name.pipe';
 import { EventFromInvitationPipeModule } from '@blockframes/invitation/pipes/event-from-invitation.pipe';
 import { EventRangeModule } from '@blockframes/event/pipes/event-range.pipe';
 import { DisplayUserModule } from '@blockframes/utils/pipes/display-user.pipe';
+import { TimeSinceModule } from '@blockframes/utils/pipes/time-since.pipe';
+import { ToDateModule } from '@blockframes/utils/pipes';
 
 // Material
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { ToDateModule } from '@blockframes/utils/pipes';
-import { TimeSinceModule } from '@blockframes/utils/pipes/time-since.pipe';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [ItemComponent],
@@ -44,6 +45,7 @@ import { TimeSinceModule } from '@blockframes/utils/pipes/time-since.pipe';
     MatButtonModule,
     MatIconModule,
     MatMenuModule,
+    MatTooltipModule
   ]
 })
 export class InvitationItemModule { }
