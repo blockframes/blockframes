f2f8cc986c5999ade2637a14cf701936fbd70e0a
Antoine <antoinepoppe@hotmail.fr>
2021-11-18 12:15:12 +0100

    scrollToTop landing page

--- libs/utils/src/lib/browser/utils.ts
+++ libs/utils/src/lib/browser/utils.ts
@@ -19,11 +19,7 @@ export function isSafari() {
 export function isChrome() {return navigator.userAgent.indexOf("Chrome") != -1}
 
 export function scrollIntoView(element:HTMLElement){
-  if(isChrome()){
-    element.scrollIntoView()
-  } else {
-   element.scrollIntoView({behavior:'smooth'})
-  }
+  element.scrollIntoView({behavior:'smooth'});
 }
 
 /**
