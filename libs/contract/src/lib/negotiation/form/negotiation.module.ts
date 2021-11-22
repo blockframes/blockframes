1845b73eb3e992d579798dee4daa92c086ed631a
Yonga Springfield <springfieldyonga@outlook.com>
2021-11-22 12:54:06 +0100

    addendum

---
+++ libs/contract/src/lib/negotiation/form/negotiation.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NegotiationFormComponent } from './negotiation-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AvailsFilterModule } from '@blockframes/contract/avails/filter/filter.module';
+import { LanguagesFormModule } from '@blockframes/movie/form/languages/languages.module';
+import { ToLabelModule, VersionPipeModule, JoinPipeModule, ToGroupLabelPipeModule } from '@blockframes/utils/pipes';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    AvailsFilterModule,
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    ToLabelModule,
+    VersionPipeModule,
+    JoinPipeModule,
+    LanguagesFormModule,
+    ToGroupLabelPipeModule,
+  ],
+  exports: [NegotiationFormComponent],
+  declarations: [NegotiationFormComponent]
+})
+export class NegotiationModule { }
