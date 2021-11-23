df72e1a8bd41ab6440bf8b3b3c378b3c868f599a
pldespaigne <pl.despaigne@gmail.com>
2021-11-23 11:50:23 +0100

    accept movie helper

--- apps/catalog/catalog-e2e/src/support/commands.ts
+++ apps/catalog/catalog-e2e/src/support/commands.ts
@@ -11,8 +11,10 @@
 import 'cypress-mailosaur';
 
 import firebase  from 'firebase/app';
-import "firebase/auth";
+import 'firebase/auth';
+import 'firebase/firestore';
 import { firebase as firebaseConfig } from '@env';
+import { App } from 'libs/utils/src/lib/apps';
 
 // -- This is a parent command --
 // Cypress.Commands.add("login", (email, password) => { ... })
@@ -31,19 +33,35 @@ import { firebase as firebaseConfig } from '@env';
 
 //Init the app
 const app = firebase.initializeApp(firebaseConfig('catalog'));
+const auth = app.auth();
+const db = app.firestore();
 
 const login = (email: string, password: string) => {
-  return app.auth().signInWithEmailAndPassword(email, password);
+  return auth.signInWithEmailAndPassword(email, password);
 }
 
 const logout = () => {
-  return app.auth().signOut();
+  return auth.signOut();
 }
+
+const acceptMovieById = (appName: App, movieId: string) => {
+  const movieRef = db.collection('movies').doc(movieId);
+  return movieRef.update(`app.${appName}`, {
+    access: true,
+    status: 'accepted',
+    refusedAt: null,
+    acceptedAt: firebase.firestore.FieldValue.serverTimestamp(),
+  });
+};
 
 Cypress.Commands.add('login', (email: string, password: string) => {
   return login(email, password);
-})
+});
 
 Cypress.Commands.add('logout', () => {
   return logout();
-})
+});
+
+Cypress.Commands.add('acceptMovieById', (app: App, movieId: string) => {
+  return acceptMovieById(app, movieId);
+});
