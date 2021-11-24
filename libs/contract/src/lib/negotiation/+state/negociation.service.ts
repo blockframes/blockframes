3995c5952944838b4b90d5e9812c87f61974b1d6
Yonga Springfield <springfieldyonga@outlook.com>
2021-11-24 13:51:14 +0100

    feat:added negotiation data model

---
+++ libs/contract/src/lib/negotiation/+state/negociation.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@angular/core";
+import { EntityState, ActiveState, StoreConfig, EntityStore } from "@datorama/akita";
+import { CollectionConfig, CollectionService } from "akita-ng-fire";
+import { Negotiation } from "./negotiation.firestore";
+import type firebase from 'firebase';
+import { formatDocumentMetaFromFirestore } from "@blockframes/utils/models-meta";
+
+export interface NegotiationState extends EntityState<Negotiation, string>, ActiveState<string> { }
+
+@Injectable({ providedIn: 'root' })
+@CollectionConfig({ path: 'contracts/:contractId/negotiations' })
+export class NegotiationService extends CollectionService<NegotiationState> {
+  useMemorization = true;
+  constructor(store: NegotiationStore) {
+    super(store)
+  }
+
+  formatFromFirestore(_negotiation: Negotiation<firebase.firestore.Timestamp>) {
+    const _meta = formatDocumentMetaFromFirestore(_negotiation?._meta);
+    const negotiation: Negotiation<Date> = {
+      ..._negotiation,
+      _meta
+    }
+    return negotiation;
+  }
+}
+
+// BOILETPLATE
+@Injectable({ providedIn: 'root' })
+@StoreConfig({ name: 'negotiation' })
+export class NegotiationStore extends EntityStore<NegotiationState> {
+  constructor() {
+    super();
+  }
+}
