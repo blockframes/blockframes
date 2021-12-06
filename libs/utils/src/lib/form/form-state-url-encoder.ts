5816e380607c65ab51f0be20cb1076a70f303f66
Remco <RemcoSimonides@gmail.com>
2021-12-06 11:58:56 +0100

    7242 search with a space

--- libs/utils/src/lib/form/form-state-url-encoder.ts
+++ libs/utils/src/lib/form/form-state-url-encoder.ts
@@ -1,23 +1,5 @@
 import { ActivatedRoute, Router } from "@angular/router"
 
-function dateReviver(key: string, value: string | number | boolean) {
-
-  /**
-   * The internet doesn't have a waterproof way to check if a string is a date
-   * This method doesn't work for string + number like 'Movie 1'
-   * https://stackoverflow.com/questions/7445328/check-if-a-string-is-a-date-value/30870755#30870755
-   * 
-   * To minimize side effects; only revive date for fields which support date value
-   */
-  const dateKeys = ['from', 'to'];
-  if (dateKeys.includes(key) && typeof value === 'string') {
-    // This could be an Invalid date
-    const date = new Date(value);
-    // check if the getTime is NaN. If not, it's a valid date.
-    if (!isNaN(date.getTime())) return date;
-  }
-  return value;
-}
 
 /**
  * Decode the app url and save it as form state
@@ -26,7 +8,7 @@ export function decodeUrl<T=any>(route: ActivatedRoute): T {
   const { formValue } = route.snapshot.queryParams;
   try {
     const fromUrl = decodeURIComponent(formValue ?? '{}');
-    return JSON.parse(fromUrl, dateReviver); // see comment about reviver
+    return JSON.parse(fromUrl);
   } catch (err) {
     console.warn(err);
   }
