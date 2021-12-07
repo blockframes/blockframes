681ea5a862f2fde6a9a13e44ca13b588c7e34483
Yonga Springfield <springfieldyonga@outlook.com>
2021-12-07 13:12:58 +0100

    refactor

--- libs/contract/src/lib/negotiation/pipe.ts
+++ libs/contract/src/lib/negotiation/pipe.ts
@@ -13,13 +13,6 @@ export class NegotiationStagePipe implements PipeTransform {
     private orgService: OrganizationService,
   ) { }
 
-  //here orgName is the seller that we need to get from stakeholders that are neither buyerId nor sellerId)
-  getOrgName(negotiation: Negotiation) {
-    const excluded = [negotiation.buyerId, negotiation.sellerId]
-    const seller = negotiation.stakeholders.find(s => !excluded.includes(s))
-    return seller;
-  }
-
   transform(negotiation: Negotiation): Observable<string> {
     if (!(negotiation?.status === 'pending')) return of('')
     if (negotiation?.createdByOrg !== this.activeOrgId) return of('To be Reviewed');
