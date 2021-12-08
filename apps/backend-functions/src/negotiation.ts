63a6b0bef9b0e6258e626e219f55338a3109c792
Yonga Springfield <springfieldyonga@outlook.com>
2021-12-08 13:27:25 +0100

    refactor

--- apps/backend-functions/src/negotiation.ts
+++ apps/backend-functions/src/negotiation.ts
@@ -1,5 +1,4 @@
 import { db } from './internals/firebase';
-import { Sale } from '@blockframes/contract/contract/+state/contract.model';
 import { Change, EventContext } from 'firebase-functions';
 import { Negotiation } from '@blockframes/contract/negotiation/+state/negotiation.firestore';
 
@@ -8,31 +7,21 @@ export async function onNegotiationUpdate(
   change: Change<FirebaseFirestore.DocumentSnapshot>, context: EventContext
 ) {
 
-  const before = change.before;
-  const after = change.after;
   const { contractId } = context.params
+  const before = change.before?.data() as Negotiation;
+  const after = change.after?.data() as Negotiation;
 
   if (!before || !after) {
     throw new Error('Parameter "change" not found');
   }
 
-  const negotiationBefore = before.data() as Negotiation;
-  const negotiationAfter = after.data() as Negotiation;
+  const hasStatusChanged = before.status !== after.status
 
-  const isStatusUnchanged = negotiationBefore.status === negotiationAfter.status
-  const isDeclined = negotiationAfter.status === 'declined'
+  if (!hasStatusChanged) return;
 
-  if (isStatusUnchanged) return;
+  const { status, declineReason = "" } = after;
 
-  const saleRef = db.doc(`contracts/${contractId}`);
-  const saleSnap = await saleRef.get();
-  const sale = saleSnap.data() as Sale<Date>;
+  const updates = status === 'declined' ? { declineReason, status } : { status };
 
-  if (sale.status !== 'pending' && sale.status !== 'negotiating') return
-
-  const updates: Partial<Negotiation> = { status: negotiationAfter.status };
-  if (isDeclined)
-    updates.declineReason = negotiationAfter.declineReason;
-
-  saleRef.update(updates);
+  db.doc(`contracts/${contractId}`).update(updates);
 }
