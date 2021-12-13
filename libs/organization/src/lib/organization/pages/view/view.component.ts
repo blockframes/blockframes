4ff2f6073234c8771afcfd12e2d7ea993da9196d
Remco <RemcoSimonides@gmail.com>
2021-12-13 15:40:40 +0100

    7362 7346 fix previous button navigation

--- libs/organization/src/lib/organization/pages/view/view.component.ts
+++ libs/organization/src/lib/organization/pages/view/view.component.ts
@@ -1,9 +1,12 @@
-import { Subscription } from 'rxjs';
 import { Component, OnInit, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Location } from '@angular/common';
+import { NavigationEnd, Router, Event } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { distinctUntilChanged, filter } from 'rxjs/operators';
+
+// blockframes
 import { Organization } from '@blockframes/organization/+state/organization.model';
 import { OrganizationQuery } from '@blockframes/organization/+state/organization.query';
-import { TunnelService } from '@blockframes/ui/tunnel/tunnel.service';
 import { OrganizationForm } from '@blockframes/organization/forms/organization.form';
 import { User } from '@blockframes/auth/+state/auth.store';
 import { AuthQuery } from '@blockframes/auth/+state/auth.query';
@@ -32,16 +35,21 @@ export class OrganizationViewComponent implements OnInit, OnDestroy {
   public navLinks = navLinks;
   public organizationForm = new OrganizationForm();
   public user$: Observable<User>;
-  private sub: Subscription;
+
+  private countRouteEvents = 1;
+  private subs: Subscription[] = [];
+  // private sub: Subscription;
+  // private routeSub: Subscription
 
   constructor(
     private query: OrganizationQuery,
-    public tunnelService: TunnelService,
     private authQuery: AuthQuery,
     private dynTitle: DynamicTitleService,
-    private routerQuery: RouterQuery
+    private routerQuery: RouterQuery,
+    private location: Location,
+    private router: Router,
   ) {
-    this.sub = this.routerQuery.select('state').subscribe(data => {
+    const sub = this.routerQuery.select('state').subscribe(data => {
       if (data.url.includes('members')) {
         this.dynTitle.setPageTitle('Members', `${this.query.getActive().denomination.full}`);
       } else if (data.url.includes('documents')) {
@@ -50,14 +58,27 @@ export class OrganizationViewComponent implements OnInit, OnDestroy {
         this.dynTitle.setPageTitle('Company details', `${this.query.getActive().denomination.full}`);
       }
     })
+    this.subs.push(sub);
   }
 
   ngOnInit() {
     this.user$ = this.authQuery.user$;
     this.organization$ = this.query.selectActive();
+
+    const sub = this.router.events.pipe(
+      filter((evt: Event) => evt instanceof NavigationEnd),
+      distinctUntilChanged((a: NavigationEnd, b: NavigationEnd) => a.url === b.url),
+    ).subscribe(() => this.countRouteEvents++);
+    this.subs.push(sub)
   }
 
   ngOnDestroy() {
-    if (this.sub) this.sub.unsubscribe()
+    for (const sub of this.subs) {
+      sub.unsubscribe();
+    }
+  }
+
+  goBack() {
+    this.location.historyGo(-this.countRouteEvents);
   }
 }
--- libs/user/src/lib/auth/pages/profile-view/profile-view.component.ts
+++ libs/user/src/lib/auth/pages/profile-view/profile-view.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
+import { Location } from '@angular/common'
+import { NavigationEnd, Router, Event } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { distinctUntilChanged, filter } from 'rxjs/operators';
+
+// blockframes
 import { AuthQuery } from '@blockframes/auth/+state/auth.query';
 import { OrganizationQuery } from '@blockframes/organization/+state/organization.query';
-import { Observable } from 'rxjs';
 import { Organization } from '@blockframes/organization/+state/organization.model';
-import { TunnelService } from '@blockframes/ui/tunnel';
 import { User } from '@blockframes/auth/+state/auth.store';
 import { DynamicTitleService } from '@blockframes/utils/dynamic-title/dynamic-title.service';
 import { getCurrentApp } from '@blockframes/utils/apps';
@@ -31,16 +35,20 @@ const navLinks = [
   styleUrls: ['./profile-view.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProfileViewComponent implements OnInit {
+export class ProfileViewComponent implements OnInit, OnDestroy {
   public organization$: Observable<Organization>;
   public navLinks = navLinks;
   public user$: Observable<User>;
 
+  private countRouteEvents = 1;
+  private sub: Subscription;
+
   constructor(
     private authQuery: AuthQuery,
     private organizationQuery: OrganizationQuery,
-    public tunnelService: TunnelService,
     private dynTitle: DynamicTitleService,
+    private location: Location,
+    private router: Router,
     private routerQuery: RouterQuery
   ) {
 
@@ -62,5 +70,18 @@ export class ProfileViewComponent implements OnInit {
     if (canHavePreferences(org, app)) {
       this.navLinks.push({ path: 'preferences', label: 'Buying Preferences' })
     }
+
+    this.sub = this.router.events.pipe(
+      filter((evt: Event) => evt instanceof NavigationEnd),
+      distinctUntilChanged((a: NavigationEnd, b: NavigationEnd) => a.url === b.url),
+    ).subscribe(() => this.countRouteEvents++);
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  goBack() {
+    this.location.historyGo(-this.countRouteEvents);
   }
 }
