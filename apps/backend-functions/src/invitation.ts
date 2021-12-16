2f6c5b102479e5cb73d866dd3c45365b99b99477
xmano <mano@xkanda.com>
2021-12-16 11:59:51 +0530

    Preparing to finalise invitation backend functions UT

--- apps/backend-functions/src/invitation.ts
+++ apps/backend-functions/src/invitation.ts
@@ -188,6 +188,7 @@ export const inviteUsers = async (data: UserInvitation, context: CallableContext
 
     try {
       const invitationSet = await db.collection('invitations').doc(invitation.id).set(invitation);
+      console.log(invitationSet);
       promises.push({ result: invitationSet, error: '' });
     } catch (error) {
       promises.push({ result: undefined, error });
