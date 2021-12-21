89778545d2022b829e36f387062cd26bda08739a
xmano <mano@xkanda.com>
2021-12-21 12:26:23 +0530

    Fix merge conflict and test

--- libs/testing/src/lib/firebase/functions.ts
+++ libs/testing/src/lib/firebase/functions.ts
@@ -1,4 +1,4 @@
-import * as admin from 'firebase-admin';
+﻿import * as admin from 'firebase-admin';
 import firebaseFunctionsTest from 'firebase-functions-test';
 import { runChunks } from '@blockframes/firebase-utils';
 import { join, resolve } from 'path';
@@ -8,10 +8,9 @@ import { initializeTestApp, loadFirestoreRules, initializeAdminApp } from '@fire
 import type { FeaturesList } from 'firebase-functions-test/lib/features';
 import type { AppOptions } from 'firebase-admin'; // * Correct Import
 import fs from 'fs';
-import { TokenOptions } from '@firebase/rules-unit-testing/dist/src/api';
 
 export interface FirebaseTestConfig extends FeaturesList {
-  firebaseConfig?: { projectId: string , app: admin.app.App}
+  firebaseConfig?: { projectId: string, app: admin.app.App };
 }
 
 let testIndex = 0;
@@ -45,7 +44,7 @@ export function initFunctionsTestMock(emulator = true, overrideConfig?: AppOptio
     return firebaseTest;
   }
 
-  const pathToServiceAccountKey = resolve(process.cwd(), process.env.GOOGLE_APPLICATION_CREDENTIALS)
+  const pathToServiceAccountKey = resolve(process.cwd(), process.env.GOOGLE_APPLICATION_CREDENTIALS);
   const testObj: FeaturesList = firebaseFunctionsTest({ ...firebaseEnv(), ...overrideConfig }, pathToServiceAccountKey);
   testObj.mockConfig(runtimeConfig);
   return testObj;
@@ -64,7 +63,7 @@ export async function initFirestoreApp(
   projectId: string,
   rulePath: string,
   data: Record<string, unknown> = {},
-  auth?: TokenOptions
+  auth?: Record<string, unknown>
 ) {
   //Define these env vars to avoid getting console warnings
   process.env.GCLOUD_PROJECT = projectId;
@@ -86,7 +85,6 @@ function setData(projectId: string, dataDB: Record<string, unknown>) {
   const promises = Object.entries(dataDB).map(([key, doc]) => db.doc(key).set(doc));
   return Promise.all(promises);
 }
-
 
 //////////////
 // DB TOOLS //
