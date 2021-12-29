1c78c2d87c2d923e7536b3db4d75149058d9dba5
bruce <delorme.bruce.michel@gmail.com>
2021-12-29 17:22:29 +0100

    css update

--- apps/crm/crm/src/app/crm/components/event-info/event-info.module.ts
+++ apps/crm/crm/src/app/crm/components/event-info/event-info.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 // Material
 import { MatCardModule } from '@angular/material/card';
@@ -23,6 +24,7 @@ import { EventInfoComponent } from './event-info.component';
   imports: [
     CommonModule,
     RouterModule,
+    FlexLayoutModule,
     EventRangeModule,
     GuestTableModule,
     GoToModule,
