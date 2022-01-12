d07b76606286baee9753ef0f7fa5ae64478b9423
bruce <delorme.bruce.michel@gmail.com>
2022-01-12 15:48:15 +0100

    cleaning #7524

--- apps/festival/festival-e2e/src/fixtures/data.ts
+++
@@ -1,8 +0,0 @@
-﻿export const NOW = new Date();
-export const ORG_NAME = 'Charades';
-
-export const PRIVATE_EVENTNAME_1 = 'Hunted Private Screening';
-export const PRIVATE_EVENTNAME_2 = 'Private Event 2';
-export const PRIVATE_EVENTNAME_3 = 'Private Event 3';
-
-export const PUBLIC_EVENTNAME = 'Hunted Public Screening';
