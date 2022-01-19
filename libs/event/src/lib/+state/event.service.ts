b8d2e0a69af209077a0557b340e4ecd7b42a085d
Bruce <eelios@gmail.com>
2022-01-19 17:46:40 +0100

    update event stats #7570 (#7572)
    * update event stats
    
    * typo
    
    * typo
    
    * update after review
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>

--- libs/event/src/lib/+state/event.service.ts
+++ libs/event/src/lib/+state/event.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { CollectionConfig, CollectionService, Query, WriteOptions, queryChanges } from 'akita-ng-fire';
 import { EventDocument, EventBase, EventTypes } from './event.firestore';
-import { Event, ScreeningEvent, createCalendarEvent, EventsAnalytics, MeetingEvent, isMeeting, isScreening } from './event.model';
+import { Event, ScreeningEvent, createCalendarEvent, MeetingEvent, isMeeting, isScreening } from './event.model';
 import { QueryFn } from '@angular/fire/firestore/interfaces';
-import { AngularFireFunctions } from '@angular/fire/functions';
 import { OrganizationQuery } from '@blockframes/organization/+state';
 import { PermissionsService } from '@blockframes/permissions/+state';
 import { Observable, combineLatest } from 'rxjs';
@@ -53,26 +52,12 @@ const eventQueries = {
 @CollectionConfig({ path: 'events' })
 export class EventService extends CollectionService<EventState> {
   readonly useMemorization = true;
-  private analytics: Record<string, EventsAnalytics> = {};
 
   constructor(
-    private functions: AngularFireFunctions,
     private permissionsService: PermissionsService,
     private orgQuery: OrganizationQuery,
   ) {
     super();
-  }
-
-  public async queryAnalytics(eventId: string): Promise<EventsAnalytics> {
-    if (this.analytics[eventId]) {
-      return this.analytics[eventId];
-    } else {
-      const f = this.functions.httpsCallable('getEventAnalytics');
-      const analytics = await f({ eventIds: [eventId] }).toPromise();
-      const eventAnalytics = analytics.find(a => a.eventId === eventId);
-      this.analytics[eventId] = eventAnalytics;
-      return eventAnalytics;
-    }
   }
 
   /** Verify if the current user / organisation is ownr of an event */
@@ -124,12 +109,6 @@ export class EventService extends CollectionService<EventState> {
       const queries = ids.map(id => queryChanges.call(this, eventQuery(id)))
       return combineLatest(queries);
     }
-  }
-
-  /** Call a firebase function to get analytics specify to an array of eventIds.*/
-  public getEventAnalytics(eventIds: string[]): Observable<EventsAnalytics[]> {
-    const f = this.functions.httpsCallable('getEventAnalytics');
-    return f({ eventIds });
   }
 
   /** Just save local time so we will be able to compute session duration at the end */
