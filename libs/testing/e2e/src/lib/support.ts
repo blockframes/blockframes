82b46764a81338bdfbe61162e24edf94088a26f0
bruce <delorme.bruce.michel@gmail.com>
2022-01-28 17:34:14 +0100

    proposal bruce

--- libs/testing/e2e/src/lib/support.ts
+++ libs/testing/e2e/src/lib/support.ts
@@ -1,4 +1,5 @@
 import type { AuthService } from '@blockframes/auth/+state';
+import { USER_FIXTURES_PASSWORD } from '@blockframes/firebase-utils/anonymize/util';
 
 export function loginWithUID(uid: string) {
   return cy.task('createUserToken', uid).then(token => {
@@ -10,16 +11,29 @@ export function loginWithUID(uid: string) {
   })
 }
 
-export function loginWithRandomUser() {
-  return cy.task('getRandomUID').then((uid: string) => {
-    return loginWithUID(uid);
+export function loginWithEmailAndPassword(email: string) {
+  cy.window().should('have.property', 'LoginService');
+  return cy.window().then((w) => {
+    const authService = w['LoginService'] as AuthService;
+    return authService.signin(email, USER_FIXTURES_PASSWORD);
   })
 }
 
+export function loginWithRandomUser(method: 'token' | 'emailAndPassword' = 'token') {
+  if (method === 'token') {
+    return cy.task('getRandomUID').then((uid: string) => loginWithUID(uid))
+  } else {
+    return cy.task('getRandomEmail').then((email: string) => loginWithEmailAndPassword(email))
+  }
+}
+
+/**
+ * @deprecated 
+ * @param email 
+ * @returns 
+ */
 export function loginWithEmail(email: string) {
-  return cy.task('getUIDFromEmail', email).then((uid: string) => {
-    return loginWithUID(uid);
-  })
+  return cy.task('getUIDFromEmail', email).then((uid: string) => loginWithUID(uid))
 }
 
 export function clearBrowserAuth() {
