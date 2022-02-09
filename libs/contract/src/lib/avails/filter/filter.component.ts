49905bb6638467d5bb5685867728c91af9f4c09b
Yonga Springfield <springfieldyonga@outlook.com>
2022-02-09 11:25:54 +0100

    automatically set hour,minutes and milliseconds of filter to 2 or 3am in the morning.

--- libs/contract/src/lib/avails/filter/filter.component.ts
+++ libs/contract/src/lib/avails/filter/filter.component.ts
@@ -1,10 +1,20 @@
-
 import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 
 import { boolean } from '@blockframes/utils/decorators/decorators';
 
 import { AvailsFilter } from '../avails';
 import { AvailsForm, MapAvailsForm, CalendarAvailsForm } from '../form/avails.form';
+
+/**
+ * Dates are saved with an hour of 2 and minutes of 0.
+ * setting `from` at 3 (am) will permit to search the
+ * mandate just after the start of the mandate for the selected date,
+ * For the `to`, just before the end of the mandate.
+ * https://github.com/blockframes/blockframes/issues/7310#issuecomment-1033572680
+ */
+const hours = { from: 3, to: 1 };
 
 @Component({
   selector: 'form[availsFilter]',
@@ -20,5 +30,12 @@ export class AvailsFilterComponent {
 
   public getControl(key: keyof AvailsFilter) {
     return this.form.controls[key];
+  }
+
+  public setTimeOfDate(controlName: 'from' | 'to', event: MatDatepickerInputEvent<Date>) {
+    const control = this.getControl('duration').controls[controlName] as FormControl;
+    const date = new Date(event.value);
+    date.setHours(hours[controlName],0,0,0);
+    control.setValue(date);
   }
 }
