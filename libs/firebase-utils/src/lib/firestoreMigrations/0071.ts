bbabe960e690959ebc6a13a3b08af2d1623c0803
Yonga Springfield <springfieldyonga@outlook.com>
2022-02-10 11:26:19 +0100

    refactor: import firebase-admin properly

--- libs/firebase-utils/src/lib/firestoreMigrations/0071.ts
+++ libs/firebase-utils/src/lib/firestoreMigrations/0071.ts
@@ -1,13 +1,13 @@
 import { Term } from '@blockframes/contract/term/+state';
 import { Timestamp } from '@blockframes/utils/common-interfaces/timestamp';
-import firebase from 'firebase';
+import * as admin from 'firebase-admin';
 import { runChunks } from '../firebase-utils';
 import { Firestore } from '../types';
 
 /**
  * Update time in terms duration
- * @param db 
- * @returns 
+ * @param db
+ * @returns
  */
 export async function upgrade(db: Firestore) {
   const terms = await db.collection('terms').get();
@@ -18,12 +18,12 @@ export async function upgrade(db: Firestore) {
 
     const from = term.duration.from.toDate();
     from.setHours(0, 0, 0, 0);
-    term.duration.from = firebase.firestore.Timestamp.fromDate(from);
+    term.duration.from = admin.firestore.Timestamp.fromDate(from)
 
 
     const to = term.duration.to.toDate();
     to.setHours(0, 0, 0, 0);
-    term.duration.to = firebase.firestore.Timestamp.fromDate(to);
+    term.duration.to = admin.firestore.Timestamp.fromDate(to);
 
 
     await doc.ref.set(term);
