72c2017b6df1c57f738cebc30e18fe3bd93afbbf
bruce <delorme.bruce.michel@gmail.com>
2022-02-10 10:30:04 +0100

    migration example

---
+++ libs/firebase-utils/src/lib/firestoreMigrations/0071.ts
@@ -0,0 +1,32 @@
+import { Term } from '@blockframes/contract/term/+state';
+import { Timestamp } from '@blockframes/utils/common-interfaces/timestamp';
+import firebase from 'firebase';
+import { runChunks } from '../firebase-utils';
+import { Firestore } from '../types';
+
+/**
+ * Update time in terms duration
+ * @param db 
+ * @returns 
+ */
+export async function upgrade(db: Firestore) {
+  const terms = await db.collection('terms').get();
+
+  return runChunks(terms.docs, async (doc) => {
+    const term = doc.data() as Term<Timestamp>;
+
+
+    const from = term.duration.from.toDate();
+    from.setHours(0, 0, 0, 0);
+    term.duration.from = firebase.firestore.Timestamp.fromDate(from);
+
+
+    const to = term.duration.to.toDate();
+    to.setHours(0, 0, 0, 0);
+    term.duration.to = firebase.firestore.Timestamp.fromDate(to);
+
+
+    await doc.ref.set(term);
+
+  });
+}
