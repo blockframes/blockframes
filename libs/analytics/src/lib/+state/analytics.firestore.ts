172867a80adc8dd2630c30c46ba6810f9d126d9f
Remco <RemcoSimonides@gmail.com>
2022-02-15 16:15:34 +0100

    7736 Data model

--- libs/analytics/src/lib/+state/analytics.firestore.ts
+++ libs/analytics/src/lib/+state/analytics.firestore.ts
@@ -1,41 +1,32 @@
-
-import type firebase from 'firebase';
 import { DocumentMeta } from "@blockframes/utils/models-meta";
-type Timestamp = firebase.firestore.Timestamp;
 
 export type EventName = 'page_view' | 'screening_requested' | 'promo_video_started' | 'added_to_wishlist' | 'promo_element_opened' | 'asking_price_requested';
-export type EventType = 'title' | 'event' | 'unknown';
 
-export interface DataRecord {
-  title: Title;
-  event: Event;
-  unknown: unknown;
+interface AnalyticsTypeRecord {
+  title: MetaTitle;
+  event: MetaEvent;
 }
 
-export interface DataEventBase<key extends keyof DataRecord, Date> {
+export type AnalyticsTypes = keyof AnalyticsTypeRecord;
+
+export interface Analytics<type extends AnalyticsTypes> {
   id: string;
   name: EventName;
-  type: key;
-  meta: DataRecord[key];
+  type: type;
+  meta: AnalyticsTypeRecord[type];
   _meta?: DocumentMeta<Date>;
 }
 
-export interface Title {
+export interface MetaTitle {
   titleId: string;
   orgId: string;
   userId: string;
   ownerOrgIds: string[];
 }
 
-export interface Event {
+export interface MetaEvent {
   eventId: string;
   userId: string;
   orgId?: string;
   ownerOrgId: string;
 }
-
-
-// firestore documents
-export type DataEventDocument = DataEventBase<'unknown', Timestamp>;
-export type TitleAnalyticsEventDocument = DataEventBase<'title', Timestamp>;
-export type EventAnalyticsEventDocument = DataEventBase< 'event', Timestamp>;
--- libs/analytics/src/lib/+state/analytics.model.ts
+++ libs/analytics/src/lib/+state/analytics.model.ts
@@ -1,53 +1,13 @@
-import { DocumentMeta } from '@blockframes/utils/models-meta';
-import { DataEventBase, DataRecord, EventType, Title, Event } from "./analytics.firestore";
+import { Analytics, AnalyticsTypes, MetaTitle } from "./analytics.firestore";
 
 
-export interface DataEvent<key extends keyof DataRecord> extends DataEventBase<key, Date>{
-  _meta: DocumentMeta<Date>
-}
-
-export function createDataEvent<key extends keyof DataRecord>(params: Partial<DataEvent<key>>): DataEvent<EventType>  {
-  if (isTitleDataEvent(params)) {
-    const meta = createTitleMeta(params.meta);
-    const res: DataEvent<'title'> = {
-      id: '',
-      name: 'page_view',
-      type: 'title',
-      ...params,
-      meta
-    }
-    return res
-  } else if (isEventDataEvent(params )) {
-    const meta = createEventMeta(params.meta);
-    const res: DataEvent<'event'> = {
-      id: '',
-      name: 'page_view',
-      type: 'event',
-      ...params,
-      meta
-    }
-    return res
-  }
-}
-
-
-export const isTitleDataEvent = (event: Partial<DataEvent<EventType>>): event is DataEvent<'title'> => event.type === 'title';
-export function createTitleMeta(meta: Partial<Title>): Title {
+export const isTitleDataEvent = (event: Partial<Analytics<AnalyticsTypes>>): event is Analytics<'title'> => event.type === 'title';
+export function createTitleMeta(meta: Partial<MetaTitle>): MetaTitle {
   return {
     titleId: '',
     orgId: '',
     userId: '',
     ownerOrgIds: [],
-    ...meta
-  };
-}
-
-export const isEventDataEvent = (event: Partial<DataEvent<EventType>>): event is DataEvent<'event'> => event.type === 'event';
-export function createEventMeta(meta: Partial<Event>): Event {
-  return {
-    eventId: '',
-    userId: '',
-    ownerOrgId: '',
     ...meta
   };
 }
