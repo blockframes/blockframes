a9f0165209d6da896bd5545b9f805c60a44a2839
Remco <RemcoSimonides@gmail.com>
2022-02-28 08:40:06 +0100

    7855 current analytics support new data model

--- apps/festival/festival/src/app/dashboard/title/activity/activity.component.ts
+++ apps/festival/festival/src/app/dashboard/title/activity/activity.component.ts
@@ -1,6 +1,6 @@
 import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { AnalyticsService } from '@blockframes/analytics/+state/analytics.service';
-import { map, pluck, switchMap, tap } from 'rxjs/operators';
+import { map, pluck, switchMap } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
 import { DynamicTitleService } from '@blockframes/utils/dynamic-title/dynamic-title.service';
 import { DashboardTitleShellComponent } from '@blockframes/movie/dashboard/shell/shell.component';
