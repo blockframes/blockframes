5714ecfbc5aa69f4be4d686c416742582f7744c6
Yonga Springfield <springfieldyonga@outlook.com>
2022-03-10 10:47:59 +0100

    refactor

--- libs/contract/src/lib/avails/tests/avails-calendar.spec.ts
+++ libs/contract/src/lib/avails/tests/avails-calendar.spec.ts
@@ -36,7 +36,7 @@ const { from: mandateFrom, to: mandateTo } = mandateMovie1.terms[0].duration;
 
 const { from: saleBelgiumFranceLuxembourgFrom, to: saleBelgiumFranceLuxembourgTo } = saleBelgiumFranceLuxembourgMovie1.terms[0].duration;
 
-describe.skip('Calendar', () => {
+describe('Calendar', () => {
   describe('Test Matrix', () => {
 
     it('Test isBefore', () => {
@@ -176,7 +176,7 @@ describe.skip('Calendar', () => {
   });
 });
 
-describe.skip('Test terms out of movie mandates', () => {
+describe('Test terms out of movie mandates', () => {
   it('Checks not licensed due to territory', () => {
     const markers = durationAvailabilities(availSouthKorea, [mandateMovie1], sales, []);
     expect(markers.available.length).toBe(0);
@@ -373,11 +373,11 @@ describe.skip('Test terms out of movie mandates', () => {
 
 
 describe('test getMatchingCalendarMandates method', () => {
-  it.skip('test avails on mandate with multiple terms that satisfy avail as a whole', () => {
+  it('test avails on mandate with multiple terms that satisfy avail as a whole', () => {
     const mandates = getMatchingCalendarMandates([mandateMovie6, mandateMovie7], availsFranceBrazil)
     expect(mandates.length).toBe(1);
   })
-  it.skip('test available on france and planes.', () => {
+  it('test available on france and planes.', () => {
     const mandates = getMatchingCalendarMandates([mandateMovie6, mandateMovie7], availsListFrance)
     expect(mandates.length).toBe(1);
   })
