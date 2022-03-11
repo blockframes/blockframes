430b9bbe684a4fc0ae92cf4b649e9b90cc43bc5d
kevin-m <kmarais@cascadae8.com>
2022-03-11 09:47:27 +0100

    remove imports

--- libs/invitation/src/lib/+state/invitation.model.ts
+++ libs/invitation/src/lib/+state/invitation.model.ts
@@ -1,8 +1,5 @@
-import { InvitationBase } from './invitation.firestore';
-import { Organization, PublicUser, Movie } from '@blockframes/model';
+import { Organization, PublicUser, Movie, InvitationBase } from '@blockframes/model';
 import { Event } from '@blockframes/event/+state/event.model';
-
-export { InvitationStatus, createInvitation } from './invitation.firestore';
 
 export type Invitation = InvitationBase<Date>;
 
