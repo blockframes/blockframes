544800e98b1e1b6f1b63350892b6e7320878f5f8
kevin-m <kmarais@cascadae8.com>
2022-03-15 10:53:12 +0100

    removed analytics.firestore.ts && analytics.model.ts => model

--- libs/model/src/lib/analytics.ts
+++ libs/model/src/lib/analytics.ts
@@ -30,3 +30,14 @@ export interface MetaEvent {
   orgId?: string;
   ownerOrgId: string;
 }
+
+export const isTitleDataEvent = (event: Partial<Analytics>): event is Analytics<'title'> => event.type === 'title';
+export function createTitleMeta(meta: Partial<MetaTitle>): MetaTitle {
+  return {
+    titleId: '',
+    orgId: '',
+    userId: '',
+    ownerOrgIds: [],
+    ...meta
+  };
+};
