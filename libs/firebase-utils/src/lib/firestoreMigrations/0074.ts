e493806b5bdedfa2d7f2a6de3d0611843a54d918
KevinDurand974 <k.durand.974@gmail.com>
2022-03-22 10:37:39 +0100

    create migration

--- libs/firebase-utils/src/lib/firestoreMigrations/0074.ts
+++ libs/firebase-utils/src/lib/firestoreMigrations/0074.ts
@@ -8,31 +8,22 @@ import { Movie } from '@blockframes/model';
  * @returns
  */
 export async function upgrade(db: Firestore) {
-  const genresToKeyword = {
-    // periodDrama: 'Period Drama',
-    // youngAdult: 'Young Adult',
-    // war: 'War',
-    // police: 'Police',
-    // webSeries: 'Web Series',
-    // wildlife: 'Wildlife',
-    // huntingFishing: 'Hunting & Fishing',
-    // archeologyHistory: 'Archeology & History',
-    // travelDiscovery: 'Travel & Discovery',
-    // natureEnvironment: 'Nature & Environment',
-    // fashion: 'Fashion',
-    // western: 'Western',
-    // tvShow: 'TV Show',
-    // virtualReality: 'Virtual Reality'
-  };
-  const removedGenres = Object.keys(genresToKeyword);
   const movies = await db.collection('movies').get();
 
   return runChunks(movies.docs, async (doc) => {
-    // const movie = doc.data() as Movie;
-    // const removed = movie.genres.filter(genre => removedGenres.includes(genre));
-    // const keywords: string[] = removed.map(genre => genresToKeyword[genre]);
-    // movie.keywords = Array.from(new Set([ ...movie.keywords, ...keywords ]));
-    // movie.genres =  movie.genres.filter(genre => !removedGenres.includes(genre));
-    // await doc.ref.set(movie);
+    const movie = doc.data() as Movie;
+
+    // Check if movie has a crew with mini 1 member
+    if (movie.crew.length == 0) return
+
+    // Replace all bad 'confiremd' with the fix one
+    movie.crew = movie.crew.map((crew) => ({
+      ...crew,
+      status: crew.status as any == 'confiremd' ? 'confirmed' : crew.status
+    }))
+    
+    // Update movie in DB
+    await doc.ref.set(movie);
+
   }).catch(err => console.error(err));
 }
