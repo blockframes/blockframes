7ada74a97a7ff6aa536457db0b849612227b60c9
KevinDurand974 <k.durand.974@gmail.com>
2022-03-25 13:50:56 +0100

    Refactoring html, style and script

--- apps/catalog/catalog/src/app/marketplace/title/avails/avails.component.ts
+++ apps/catalog/catalog/src/app/marketplace/title/avails/avails.component.ts
@@ -22,6 +22,7 @@ import { BucketTerm, Term } from '@blockframes/model';
 import { ExplanationComponent } from './explanation/explanation.component';
 import { HoldbackModalComponent } from '@blockframes/contract/contract/holdback/modal/holdback-modal.component';
 import { scrollIntoView } from '../../../../../../../../libs/utils/src/lib/browser/utils';
+import { territories } from '@blockframes/utils/static-model';
 
 @Component({
   selector: 'catalog-movie-avails',
@@ -40,6 +41,7 @@ export class MarketplaceMovieAvailsComponent implements AfterViewInit, OnDestroy
   public orgId = this.orgService.org.id;
   public periods = ['days', 'weeks', 'months', 'years'];
   public maxTerritories = 30;
+  public maxExcludingTerritories = 20;
 
   public bucketForm = new BucketForm();
 
@@ -232,5 +234,16 @@ export class MarketplaceMovieAvailsComponent implements AfterViewInit, OnDestroy
     scrollIntoView(document.querySelector('#avails'));
     this.avails.mapForm.reset();
     this.avails.calendarForm.reset();
+  }
+
+  excludingTerritories(currentTerritoryList: string[]) {
+    // List all keys of territories
+    const listKeys = Object.keys(territories);
+    // Remove the word 'world' from the array
+    listKeys.splice(listKeys.indexOf("world"), 1);
+    // Filter to include only current territories and get the value of each keys
+    return listKeys
+      .filter(territory => !currentTerritoryList.includes(territory))
+      .map(territory => territories[territory]);
   }
 }
