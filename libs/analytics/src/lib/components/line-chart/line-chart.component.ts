a65b7700bd645458e5dd8b08961889cd732b43de
Remco <RemcoSimonides@gmail.com>
2022-03-31 14:26:37 +0200

    7834 line chart

---
+++ libs/analytics/src/lib/components/line-chart/line-chart.component.ts
@@ -0,0 +1,114 @@
+import { ChangeDetectionStrategy, Component, Input, ViewChild } from "@angular/core";
+import { Analytics, EventName } from "@blockframes/model";
+import { eachDayOfInterval, isSameDay } from "date-fns";
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexDataLabels,
+  ApexGrid,
+  ApexLegend,
+  ApexStroke,
+  ApexTheme,
+  ApexXAxis,
+  ChartComponent,
+} from "ng-apexcharts";
+
+interface LineChartOptions {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  theme: ApexTheme;
+  legend: ApexLegend;
+  xaxis: ApexXAxis;
+  dataLabels: ApexDataLabels;
+  grid: ApexGrid;
+  stroke: ApexStroke;
+};
+
+const eventNameLabel: Record<EventName, string> = {
+  addedToWishlist: 'Added to Wishlist',
+  askingPriceRequested: 'Asking Price Requested',
+  pageView: 'Page Views',
+  promoReelOpened: 'Promoreel Opened',
+  removedFromWishlist: 'Removed from Wishlist',
+  screeningRequested: 'Screening Requested'
+}
+
+
+const getUniqueEventNames = (array: Analytics[]) => {
+  return array
+    .filter((item, index) => array.findIndex(i => i.name === item.name) === index)
+    .map(item => item.name);
+}
+
+@Component({
+  selector: '[data] analytics-line-chart',
+  templateUrl: './line-chart.component.html',
+  styleUrls: ['./line-chart.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LineChartComponent {
+  @ViewChild("chart") chart: ChartComponent;
+
+  lineChartOptions: LineChartOptions = {
+    series: [],
+    chart: {
+      width: '100%',
+      height: '300px',
+      type: "area",
+    },
+    theme: {
+      monochrome: {
+        enabled: true,
+        color: '#001ec7'
+      }
+    },
+    legend: {
+      show: true,
+      position: 'top',
+      showForSingleSeries: true
+    },
+    grid: {
+      show: true,
+      padding: {
+        left: 0,
+        right: 0
+      }
+    },
+    stroke: {
+      curve: "smooth",
+      width: 2.5
+    },
+    dataLabels: {
+      enabled: false
+    },
+    xaxis: {
+      type: 'datetime',
+    }
+  };
+
+  @Input() set data(data: Analytics[]) {
+    if (!data?.length) return;
+
+    const analytics = data.sort((a, b) => a._meta.createdAt.getTime() - b._meta.createdAt.getTime());
+    const start = analytics[0]._meta.createdAt;
+    const end = new Date();
+    const eachDay = eachDayOfInterval({ start, end });
+
+    const series: ApexAxisChartSeries = [];
+    const eventNames = getUniqueEventNames(analytics);
+    for (const name of eventNames) {
+      const data = eachDay.map(day => {
+        const analyticsOfDay = analytics
+          .filter(analytic => analytic.name === name)
+          .filter(analytic => isSameDay(day, analytic._meta.createdAt)
+        );
+
+        return [day.getTime(), analyticsOfDay.length] as [number, number];
+      });
+
+      series.push({ name: eventNameLabel[name], data });
+    }
+
+    this.chart?.updateSeries(series);
+  }
+}
