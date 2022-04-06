7405042ac4eba7810136198cdd4fe81b02a70ab7
KevinDurand974 <k.durand.974@gmail.com>
2022-04-06 15:21:52 +0200

    Add again the column deleted last commit, add a @input system to select which one to show, add event title, type and mode columns

--- apps/crm/crm/src/app/crm/components/guest-table/guest-table.component.ts
+++ apps/crm/crm/src/app/crm/components/guest-table/guest-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 import { getGuest } from '@blockframes/invitation/pipes/guest.pipe';
 import { Invitation, InvitationDetailed } from '@blockframes/model';
+import { boolean } from '@blockframes/utils/decorators/decorators';
 
 @Component({
   selector: 'invitation-guest-table',
@@ -19,6 +20,11 @@ export class GuestTableComponent {
       });
     }
   }
+  @Input() @boolean showOrganization = false;
+  @Input() @boolean showUser = false;
+  @Input() @boolean showInvitationMode = false;
+  @Input() @boolean showMovie = false;
+  @Input() @boolean showEvent = false;
 
   console(any: any) {
     console.log(any)
