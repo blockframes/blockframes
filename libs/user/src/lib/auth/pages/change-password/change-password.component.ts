eb796a6e76f5e2d4820d2438cc8c4df67187c650
bruce <delorme.bruce.michel@gmail.com>
2022-04-08 14:50:06 +0200

    cleaning

--- libs/user/src/lib/auth/pages/change-password/change-password.component.ts
+++ libs/user/src/lib/auth/pages/change-password/change-password.component.ts
@@ -63,7 +63,7 @@ export class ChangePasswordComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.destroyed.next(null); // TODO #7273 ok ?
+    this.destroyed.next(null);
     this.destroyed.unsubscribe();
   }
 }
