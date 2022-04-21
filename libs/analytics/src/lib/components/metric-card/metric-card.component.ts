1ba165b0540003123eb5710d985125a7bf623b9d
Remco <RemcoSimonides@gmail.com>
2022-04-21 11:57:14 +0200

    7841 vanity metrics and table

--- libs/analytics/src/lib/components/metric-card/metric-card.component.ts
+++ libs/analytics/src/lib/components/metric-card/metric-card.component.ts
@@ -1,11 +1,13 @@
-import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from "@angular/core";
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
 
 @Component({
   selector: '[title][value][icon] analytics-metric-card',
   templateUrl: './metric-card.component.html',
   styleUrls: ['./metric-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.None
+  host: {
+    class: 'surface'
+  }
 })
 export class MetricCardComponent {
   @Input() title: string;
