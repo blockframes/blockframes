cbd5e720b2b60acd22849a7e4ea7f9a113e716bc
KevinDurand974 <k.durand.974@gmail.com>
2022-04-26 15:33:04 +0200

    Update Anchor Link from #8131

--- libs/user/src/lib/auth/components/terms-conditions/terms-conditions.component.ts
+++ libs/user/src/lib/auth/components/terms-conditions/terms-conditions.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { App, applicationUrl } from '@blockframes/utils/apps';
 import { Location } from '@angular/common';
 import { APP } from '@blockframes/utils/routes/utils';
 import { supportEmails } from '@env'
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'auth-terms-conditions',
@@ -10,15 +11,22 @@ import { supportEmails } from '@env'
   styleUrls: ['./terms-conditions.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TermsConditionsComponent {
+export class TermsConditionsComponent implements AfterViewInit {
   appUrl = applicationUrl[this.app];
   canGoBack = window.history.length > 1;
   supportEmail = supportEmails.default;
 
   constructor(
+    private route: ActivatedRoute,
     private location: Location,
     @Inject(APP) public app: App,
   ) { }
+
+  ngAfterViewInit(): void {
+    this.route.fragment.subscribe(fragment => {
+      if (fragment) document.getElementById(fragment).scrollIntoView();
+    })
+  }
 
   goBack() {
     this.location.back();
