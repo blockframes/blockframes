f64bf5cc2810b44f6b11011d3d5db95b1e99b5c4
KevinDurand974 <k.durand.974@gmail.com>
2022-04-26 12:28:12 +0200

    Update 'update funding status' modal

---
+++ apps/financiers/financiers/src/app/dashboard/title/modal/modal.module.ts
@@ -0,0 +1,38 @@
+// Angular
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+// Components
+import { TitleModalComponent } from './modal.component';
+import { GlobalModalModule } from '@blockframes/ui/global-modal/global-modal.module';
+
+// Material
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+// Blockframes
+import { OrgAccessModule } from '@blockframes/organization/pipes';
+
+@NgModule({
+  declarations: [TitleModalComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    // Blockframes
+    OrgAccessModule,
+    GlobalModalModule,
+    // Material
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule
+  ]
+})
+export class TitleModalModule { }
--- apps/financiers/financiers/src/app/dashboard/title/view/view.component.ts
+++ apps/financiers/financiers/src/app/dashboard/title/view/view.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, ViewChild, TemplateRef } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ViewChild } from '@angular/core';
 import { RouteDescription } from '@blockframes/utils/common-interfaces/navigation';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { CrossFieldErrorMatcher } from '@blockframes/utils/form/matchers';
@@ -8,6 +8,7 @@ import { pluck, switchMap } from 'rxjs/operators';
 import { OrganizationService } from '@blockframes/organization/+state';
 import { MovieService } from '@blockframes/movie/+state/movie.service';
 import { ActivatedRoute } from '@angular/router';
+import { TitleModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'financiers-dashboard-title-view',
@@ -16,7 +17,6 @@ import { ActivatedRoute } from '@angular/router';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TitleViewComponent {
-  @ViewChild('dialogTemplate') dialogTemplate: TemplateRef<unknown>;
   @ViewChild(DashboardTitleShellComponent) shell: DashboardTitleShellComponent;
 
   private dialogRef: MatDialogRef<unknown, unknown>;
@@ -69,13 +69,17 @@ export class TitleViewComponent {
   async openDialog() {
     const form = this.shell.getForm('campaign');
     const errorMatcher = new CrossFieldErrorMatcher();
-    this.dialogRef = this.dialog.open(this.dialogTemplate, {
+    this.dialogRef = this.dialog.open(TitleModalComponent, {
       minWidth: '50vw',
-      data: { form, errorMatcher }
+      data: {
+        form,
+        errorMatcher,
+        onSave: this.save
+      }
     });
   }
 
-  async save() {
+  save = async () => {
     this.dialogRef.close();
     await this.shell.getConfig('campaign').onSave();
     this.snackbar.open('The funding status has been updated.', null, { duration: 1000 });
