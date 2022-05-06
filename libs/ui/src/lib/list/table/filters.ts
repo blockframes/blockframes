4bd3ff2cb9f48bdd687f47b534f96163c3a3f42e
Remco <RemcoSimonides@gmail.com>
2022-05-06 08:24:19 +0200

    8299 fix filter on bf table

--- libs/ui/src/lib/list/table/filters.ts
+++ libs/ui/src/lib/list/table/filters.ts
@@ -2,7 +2,7 @@ import { Movie, Scope, staticModel } from "@blockframes/model";
 import { displayName } from "@blockframes/utils/utils";
 
 export function getStaticModelFilter(scope: Scope) {
-  return (input: string, value: any) => {
+  return (input: string, value: string) => {
     if (typeof value !== 'string') return false;
     const label = staticModel[scope][value];
     return label.toLowerCase().includes(input);
@@ -10,6 +10,6 @@ export function getStaticModelFilter(scope: Scope) {
 }
 
 export const filters = {
-  movieDirectors: (input: string, _: undefined, movie: Movie) => movie.directors.map(director => displayName(director)).some(director => director.toLocaleLowerCase().includes(input)),
-  movieTitle: (input: string, _: undefined, movie: Movie) => movie?.title?.international.toLocaleLowerCase().includes(input)
+  movieDirectors: (input: string, _, movie: Movie) => movie?.directors?.map(director => displayName(director)).some(name => name.toLocaleLowerCase().includes(input)),
+  movieTitle: (input: string, _, movie: Movie) => movie?.title?.international.toLocaleLowerCase().includes(input)
 }
