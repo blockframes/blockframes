00618aac5fb8304cdd232135c9ed1872d8aef2ee
bruce <delorme.bruce.michel@gmail.com>
2022-05-06 15:17:48 +0200

    fixed review

--- libs/import/src/lib/utils.ts
+++ libs/import/src/lib/utils.ts
@@ -235,13 +235,10 @@ export function wrongTemplateError(templateImported: 'seller' | 'admin'): Import
     admin: 'Please contact team@archipelcontent.com or use a template that\'s appropriate to sellers',
     seller: 'Please contact team@archipelcontent.com or use a template that\'s appropriate to admins',
   }
-  const article = {
-    admin:'an',
-    seller: 'a',
-  }
+
   const option: LogOption = {
-    name: `Wrong Template`,
-    reason: `You are not permitted to import ${article[templateImported]} ${templateImported} template.`,
+    name: 'Wrong Template',
+    reason: `You are not permitted to import ${templateImported === 'admin' ? 'an' : 'a'} ${templateImported} template.`,
     message: messages[templateImported],
   };
   return new WrongTemplateError(templateImported, option);
