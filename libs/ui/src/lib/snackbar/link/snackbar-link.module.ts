cd20de993236d79625cf720b5d17cc5437308f4b
kevin-m <kmarais@cascadae8.com>
2022-05-09 11:59:22 +0200

    Fix responsive snackBarLink

--- libs/ui/src/lib/snackbar/link/snackbar-link.module.ts
+++ libs/ui/src/lib/snackbar/link/snackbar-link.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SnackbarLinkComponent } from './snackbar-link.component';
 import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 // Material
 import { MatButtonModule } from '@angular/material/button';
@@ -12,7 +13,8 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     CommonModule,
     MatButtonModule,
-    RouterModule
+    RouterModule,
+    FlexLayoutModule
   ]
 })
 export class SnackbarLinkModule { }
