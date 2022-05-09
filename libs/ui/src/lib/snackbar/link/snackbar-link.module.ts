f6a76482f267eec8f5c23dffc49f7b0f9155bec2
kevin-m <kmarais@cascadae8.com>
2022-05-09 14:10:16 +0200

    removed unused import

--- libs/ui/src/lib/snackbar/link/snackbar-link.module.ts
+++ libs/ui/src/lib/snackbar/link/snackbar-link.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SnackbarLinkComponent } from './snackbar-link.component';
 import { RouterModule } from '@angular/router';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 // Material
 import { MatButtonModule } from '@angular/material/button';
@@ -14,7 +13,6 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     MatButtonModule,
     RouterModule,
-    FlexLayoutModule
   ]
 })
 export class SnackbarLinkModule { }
