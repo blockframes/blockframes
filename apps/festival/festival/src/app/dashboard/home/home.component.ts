fa4e8e5d6948499f9d8bc30a6010822c7aec210d
Yonga Springfield <springfieldyonga@outlook.com>
2022-05-12 14:48:54 +0100

    added buyer map insights

--- apps/festival/festival/src/app/dashboard/home/home.component.ts
+++ apps/festival/festival/src/app/dashboard/home/home.component.ts
@@ -1,14 +1,19 @@
 // Angular
-import { Component, ChangeDetectionStrategy, Optional, Inject } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Optional, Inject, ChangeDetectorRef, Pipe, PipeTransform } from '@angular/core';
 
 // Blockframes
 import { MovieService, fromOrg } from '@blockframes/movie/+state/movie.service';
 import { OrganizationService } from '@blockframes/organization/+state';
 import { DynamicTitleService } from '@blockframes/utils/dynamic-title/dynamic-title.service';
-import { EventName, hasAppStatus, App } from '@blockframes/model';
+import {
+  EventName, hasAppStatus, App, territories,
+  territoriesISOA3, TerritoryISOA3Value, Territory, Organization, Analytics
+} from '@blockframes/model';
 import { APP } from '@blockframes/utils/routes/utils';
 import { AnalyticsService } from '@blockframes/analytics/+state/analytics.service';
 import { counter } from '@blockframes/analytics/+state/utils';
+import { joinWith } from '@blockframes/utils/operators';
+import { unique } from '@blockframes/utils/helpers';
 
 // RxJs
 import { map, switchMap, shareReplay, tap, filter } from 'rxjs/operators';
@@ -16,7 +21,53 @@ import { combineLatest } from 'rxjs';
 
 // Intercom
 import { Intercom } from 'ng-intercom';
-import { joinWith } from '@blockframes/utils/operators';
+
+interface TerritoryStat {
+  territoryISOA3: TerritoryISOA3Value;
+  count: number;
+  territory: typeof territories[Territory],
+}
+
+interface TerritoryCount {
+  zero: TerritoryStat[],
+  lessThan5: TerritoryStat[],
+  lessThan50: TerritoryStat[],
+  lessThan100: TerritoryStat[],
+}
+
+function getTerritoryAndCount({ orgs, analytics }: { orgs: Organization[], analytics: Analytics<'title'>[], orgIds: string[] }): TerritoryStat[] {
+  const territories = Object.values(territoriesISOA3)
+    .filter((country) => country !== '') as TerritoryISOA3Value[];
+  return territories.map(territoryISOA3 => {
+    const orgsInTerritory = orgs
+      .filter(o => territoriesISOA3[o.addresses.main.country] === territoryISOA3);
+
+    const orgIds = orgsInTerritory.map(({ id }) => id);
+    const analyticsOfCountry = analytics.filter(
+      analytic => orgIds.includes(analytic.meta.orgId)
+    );
+    const territory = territories[orgsInTerritory[0]?.addresses?.main?.country]
+    return { territoryISOA3, count: analyticsOfCountry.length, territory };
+  });
+}
+
+function getCount(stats: TerritoryStat[]) {
+  return stats.reduce(
+    (acc, stat) => {
+      if (stat.count === 0) acc.zero.push(stat);
+      else if (stat.count <= 5) acc.lessThan5.push(stat);
+      else if (stat.count <= 50) acc.lessThan50.push(stat);
+      else if (stat.count <= 100) acc.lessThan100.push(stat);
+      return acc;
+    },
+    {
+      zero: [],
+      lessThan5: [],
+      lessThan50: [],
+      lessThan100: [],
+    } as TerritoryCount
+  )
+}
 
 @Component({
   selector: 'dashboard-home',
@@ -70,6 +121,20 @@ export class HomeComponent {
     map(analytics => analytics.filter(analytic => analytic.name === 'pageView'))
   );
 
+  activeCountries$ = this.titleAnalytics$.pipe(
+    filter(analytics => analytics.length > 0),
+    map(analytics => {
+      const orgIds = unique(analytics.map(analytic => analytic.meta.orgId));
+      return { orgIds, analytics };
+    }),
+    joinWith({
+      orgs: ({ orgIds }) => this.orgService.valueChanges(orgIds)
+    }, { shouldAwait: true }),
+    map(getTerritoryAndCount),
+    map(stats => stats.sort(({ count: countA }, { count: countB }) => countA - countB)),
+    map(getCount)
+  );
+
   interactions: EventName[] = [
     'addedToWishlist',
     'askingPriceRequested',
@@ -83,10 +148,19 @@ export class HomeComponent {
     private orgService: OrganizationService,
     private dynTitle: DynamicTitleService,
     @Optional() private intercom: Intercom,
-    @Inject(APP) public app: App
+    @Inject(APP) public app: App,
+    private cdr: ChangeDetectorRef,
   ) { }
 
   public openIntercom(): void {
     return this.intercom.show();
+  }
+}
+
+
+@Pipe({ name: 'combineStats' })
+export class CombineStatsPipe implements PipeTransform {
+  transform({ zero, lessThan5, lessThan50, lessThan100 }: TerritoryCount) {
+    return [...lessThan100, ...lessThan50, ...lessThan5, ...zero];
   }
 }
--- apps/festival/festival/src/app/dashboard/home/home.module.ts
+++ apps/festival/festival/src/app/dashboard/home/home.module.ts
@@ -11,7 +11,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 // Pages
-import { HomeComponent } from './home.component';
+import { CombineStatsPipe, HomeComponent } from './home.component';
 
 // Modules
 import { CarouselModule } from '@blockframes/ui/carousel/carousel.module';
@@ -23,9 +23,13 @@ import { AppPipeModule } from '@blockframes/utils/pipes/app.pipe';
 import { NoTitleModule } from '@blockframes/ui/dashboard/components/no-title/no-title.module';
 import { ImageModule } from '@blockframes/media/image/directives/image.module';
 import { HasAppStatusModule } from '@blockframes/movie/pipes/has-app-status.pipe';
+import { TableModule } from '@blockframes/ui/list/table/table.module';
+import { MaxLengthModule } from '@blockframes/utils/pipes';
+import { MapModule } from "@blockframes/ui/map";
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, CombineStatsPipe],
   imports: [
     CommonModule,
     FlexLayoutModule,
@@ -38,12 +42,16 @@ import { HasAppStatusModule } from '@blockframes/movie/pipes/has-app-status.pipe
     NoTitleModule,
     ImageModule,
     HasAppStatusModule,
+    TableModule,
+    MaxLengthModule,
+    MapModule,
 
     // Material
     MatButtonModule,
     MatIconModule,
     MatTooltipModule,
     MatProgressSpinnerModule,
+    MatListModule,
 
     // Routing
     RouterModule.forChild([{ path: '', component: HomeComponent }])
