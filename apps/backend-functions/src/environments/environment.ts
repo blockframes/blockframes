e30e342ef86f49f7718c239908912670919e2951
bruce <delorme.bruce.michel@gmail.com>
2022-05-18 14:29:44 +0200

    test

--- apps/backend-functions/src/environments/environment.ts
+++ apps/backend-functions/src/environments/environment.ts
@@ -5,43 +5,38 @@
  * Use this setup (non-production) when the execution context is outside firebase
  * functions.
  */
-
-import { config } from 'firebase-functions';
-import { firebase } from '@env';
-import { mockConfigIfNeeded } from '@blockframes/firebase-utils/firebase-utils';
-
-console.log('Functions config() output: ')
-console.log(config());
-
-export const e2eMode = false;
-
-export {
-  production,
-  backupBucket,
-  appUrl,
-  sentryEnv,
-  sentryDsn,
-  bigQueryAnalyticsTable,
-  centralOrgId,
-  supportEmails,
-  playerId,
-} from '@env';
-
-export const { projectId, storageBucket } = firebase();
-
-export const sendgridAPIKey = mockConfigIfNeeded('sendgrid', 'api_key');
-
-export const jwplayerSecret = mockConfigIfNeeded('jwplayer', 'secret');
-export const jwplayerKey = mockConfigIfNeeded('jwplayer', 'key');
-export const jwplayerApiV2Secret = mockConfigIfNeeded('jwplayer', 'apiv2secret');
-
-export const imgixToken = mockConfigIfNeeded('imgix', 'token');
-
-export const twilioAccountSid = mockConfigIfNeeded('twilio', 'account', 'sid');
-export const twilioAccountSecret = mockConfigIfNeeded('twilio', 'account', 'secret');
-export const twilioApiKeySecret = mockConfigIfNeeded('twilio', 'api', 'key', 'secret');
-export const twilioApiKeySid = mockConfigIfNeeded('twilio', 'api', 'key', 'sid');
-
-export const mailchimpAPIKey = mockConfigIfNeeded('mailchimp', 'api_key');
-export const mailchimpServer = mockConfigIfNeeded('mailchimp', 'server');
-export const mailchimpListId = mockConfigIfNeeded('mailchimp', 'list_id');
+ import { firebase } from '@env';
+ import { mockConfigIfNeeded } from '@blockframes/firebase-utils/firebase-utils';
+ 
+ export const e2eMode = false;
+ 
+ export {
+   production,
+   backupBucket,
+   appUrl,
+   sentryEnv,
+   sentryDsn,
+   bigQueryAnalyticsTable,
+   centralOrgId,
+   supportEmails,
+   playerId,
+ } from '@env';
+ 
+ export const { projectId, storageBucket } = firebase();
+ 
+ export const sendgridAPIKey = mockConfigIfNeeded('sendgrid', 'api_key');
+ 
+ export const jwplayerSecret = mockConfigIfNeeded('jwplayer', 'secret');
+ export const jwplayerKey = mockConfigIfNeeded('jwplayer', 'key');
+ export const jwplayerApiV2Secret = mockConfigIfNeeded('jwplayer', 'apiv2secret');
+ 
+ export const imgixToken = mockConfigIfNeeded('imgix', 'token');
+ 
+ export const twilioAccountSid = mockConfigIfNeeded('twilio', 'account', 'sid');
+ export const twilioAccountSecret = mockConfigIfNeeded('twilio', 'account', 'secret');
+ export const twilioApiKeySecret = mockConfigIfNeeded('twilio', 'api', 'key', 'secret');
+ export const twilioApiKeySid = mockConfigIfNeeded('twilio', 'api', 'key', 'sid');
+ 
+ export const mailchimpAPIKey = mockConfigIfNeeded('mailchimp', 'api_key');
+ export const mailchimpServer = mockConfigIfNeeded('mailchimp', 'server');
+ export const mailchimpListId = mockConfigIfNeeded('mailchimp', 'list_id');
