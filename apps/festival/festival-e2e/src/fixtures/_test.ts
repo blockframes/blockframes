7fc23cf3bf27dd35dd5149d7a41cd4b6d0c203e4
n-germain-work <n.germain.work@gmail.com>
2022-05-20 23:24:01 +0200

    redundant tests erased + reworked examples' structure

--- apps/festival/festival-e2e/src/fixtures/_test.ts
+++ apps/festival/festival-e2e/src/fixtures/_test.ts
@@ -2,43 +2,47 @@ const time = Date.now();
 
 // The key works a follow : [`COLLECTION/DOC/SUBCOLLECTION/SUBDOC`]
 export const examples = {
-  singleDoc: {
-    // Collection + doc.id
-    [`example-single/single1-${time}`]: {
-      example: 'single 1',
-      id: `single1-${time}`,
+  //* CREATE examples
+
+  simpleDoc: {
+    // collection + doc.id
+    [`ex-create-simple/simple1-${time}`]: {
+      example: 'simple 1',
+      id: `simple1-${time}`,
     },
   },
 
-  singleDocBis: {
-    [`example-single/single2-${time}`]: {
-      example: 'single 2',
-      id: `single2-${time}`,
+  simpleDocBis: {
+    [`ex-create-simple/simple2-${time}`]: {
+      example: 'simple 2',
+      id: `simple2-${time}`,
     },
   },
-  singleDocTer: {
-    [`example-single/single3-${time}`]: {
-      example: 'single 3',
-      id: `single3-${time}`,
+
+  simpleDocTer: {
+    [`ex-create-simple/simple3-${time}`]: {
+      example: 'simple 3',
+      id: `simple3-${time}`,
     },
   },
+
   multipleDocs: {
-    [`example-multiple/multiple1-${time}`]: {
+    [`ex-create-multiple/multiple1-${time}`]: {
       example: 'multiple 1',
       id: `multiple1-${time}`,
     },
-    [`example-multiple/multiple2-${time}`]: {
+    [`ex-create-multiple/multiple2-${time}`]: {
       example: 'multiple 2',
       id: `multiple2-${time}`,
     },
-    [`example-multiple/multiple3-${time}`]: {
+    [`ex-create-multiple/multiple3-${time}`]: {
       example: 'multiple 3',
       id: `multiple3-${time}`,
     },
   },
 
   docWithArray: {
-    [`example-array/array1-${time}`]: {
+    [`ex-create-array/array1-${time}`]: {
       array: ['data1', 'data2', 'data3'],
       example: 'array 1',
       id: `array1-${time}`,
@@ -46,130 +50,252 @@ export const examples = {
   },
 
   docWithObject: {
-    [`example-object/object1-${time}`]: {
+    [`ex-create-object/object1-${time}`]: {
       example: 'object 1',
       id: `object1-${time}`,
       object: { object1: 'data1', object2: 'data2', object3: 'data3' },
     },
   },
 
-  docWithSubCollection1: {
-    // Collection + doc.id + subcollection + doc.id
-    [`example-subcollection/doc1-${time}`]: {
+  docWithSubcollection: {
+    // collection + doc.id + subcollection + doc.id
+    [`ex-subcollection/doc1-${time}`]: {
       example: 'doc 1',
       id: `doc1-${time}`,
     },
-    [`example-subcollection/doc1-${time}/subcollection1/subdoc1-${time}`]: {
+    // first subcollection
+    [`ex-subcollection/doc1-${time}/subcollection1/subdoc1-${time}`]: {
       example: 'subdoc 1',
       id: `subdoc1-${time}`,
     },
-    [`example-subcollection/doc1-${time}/subcollection2/subdoc2-${time}`]: {
+    [`ex-subcollection/doc1-${time}/subcollection1/subdoc2-${time}`]: {
       example: 'subdoc 2',
       id: `subdoc2-${time}`,
     },
-    [`example-subcollection/doc1-${time}/subcollection2/subdoc3-${time}`]: {
+    // second subcollection
+    [`ex-subcollection/doc1-${time}/subcollection2/subdoc3-${time}`]: {
       example: 'subdoc 3',
       id: `subdoc3-${time}`,
     },
   },
 
-  docWithSubCollection2: {
-    [`example-subcollection/doc2-${time}`]: {
-      example: 'doc 2',
-      id: `doc2-${time}`,
+  //* CRUD example
+
+  //*** docs without subcollections */
+
+  // doc test
+  simpleDoc1: {
+    [`ex-crud-simple1/simple1-${time}`]: {
+      example: 'simple 1',
+      id: `simple1-${time}`,
     },
-    [`example-subcollection/doc2-${time}/subcollection3/subdoc4-${time}`]: {
-      example: 'subdoc 4',
-      id: `subdoc4-${time}`,
+  },
+  //--------
+
+  // collection test
+  simpleDoc2: {
+    [`ex-crud-simple2/simple1-${time}`]: {
+      example: 'simple 1',
+      id: `simple1-${time}`,
+    },
+  },
+  simpleDoc3: {
+    [`ex-crud-simple2/simple2-${time}`]: {
+      example: 'simple 2',
+      id: `simple2-${time}`,
+    },
+  },
+  //--------
+
+  // array test
+  simpleDoc4: {
+    [`ex-crud-simple3/simple1-${time}`]: {
+      example: 'simple 1',
+      id: `simple1-${time}`,
+    },
+  },
+  simpleDoc5: {
+    [`ex-crud-simple3/simple2-${time}`]: {
+      example: 'simple 2',
+      id: `simple2-${time}`,
+    },
+  },
+  //--------
+
+  // array of collections test
+  simpleDoc6: {
+    [`ex-crud-simple4/simple1-${time}`]: {
+      example: 'simple 1',
+      id: `simple1-${time}`,
+    },
+  },
+  simpleDoc7: {
+    [`ex-crud-simple4/simple2-${time}`]: {
+      example: 'simple 2',
+      id: `simple2-${time}`,
+    },
+  },
+  simpleDoc8: {
+    [`ex-crud-simple5/simple3-${time}`]: {
+      example: 'simple 3',
+      id: `simple3-${time}`,
     },
   },
 
-  docWithSubCollection3: {
-    [`example-subcollectionBis/doc3-${time}`]: {
-      example: 'doc 3',
-      id: `doc3-${time}`,
-    },
-    [`example-subcollectionBis/doc3-${time}/subcollection4/subdoc5-${time}`]: {
-      example: 'subdoc 5',
-      id: `subdoc5-${time}`,
-    },
-  },
+  //*** docs with subcollections */
 
-  docToDelete1: {
-    [`example-deletion/delete1-${time}`]: {
-      example: 'delete 1',
-      id: `delete1-${time}`,
-    },
-  },
-
-  docToDelete2: {
-    [`example-deletion/delete2-${time}`]: {
-      example: 'delete 2',
-      id: `delete2-${time}`,
-    },
-  },
-
-  docToDelete3: {
-    [`example-deletion2/delete3-${time}`]: {
-      example: 'delete 3',
-      id: `delete3-${time}`,
-    },
-  },
-
-  docWithSubCollectionToDelete1: {
-    [`example-deletion/doc1-${time}`]: {
-      example: 'deletion 1',
+  // doc test
+  docWithSubcollection1: {
+    [`ex-crud-subcollection1/doc1-${time}`]: {
+      example: 'doc 1',
       id: `doc1-${time}`,
     },
-    [`example-deletion/doc1-${time}/subcollection1/subdoc1-${time}`]: {
-      example: 'deletion 1',
+    [`ex-crud-subcollection1/doc1-${time}/subcollection1/subdoc1-${time}`]: {
+      example: 'subdoc 1',
       id: `subdoc1-${time}`,
     },
-    [`example-deletion/doc1-${time}/subcollection1/subdoc2-${time}`]: {
-      example: 'deletion 2',
+    [`ex-crud-subcollection1/doc1-${time}/subcollection2/subdoc2-${time}`]: {
+      example: 'subdoc 2',
       id: `subdoc2-${time}`,
     },
-    [`example-deletion/doc1-${time}/subcollection2/subdoc3-${time}`]: {
-      example: 'deletion 3',
+    [`ex-crud-subcollection1/doc1-${time}/subcollection2/subdoc3-${time}`]: {
+      example: 'subdoc 3',
       id: `subdoc3-${time}`,
     },
   },
+  //--------
 
-  docWithSubCollectionToDelete2: {
-    [`example-deletion/doc2-${time}`]: {
-      example: 'deletion 2',
+  // collection test
+  docWithSubcollection2: {
+    [`ex-crud-subcollection2/doc1-${time}`]: {
+      example: 'doc 1',
+      id: `doc1-${time}`,
+    },
+    [`ex-crud-subcollection2/doc1-${time}/subcollection1/subdoc1-${time}`]: {
+      example: 'subdoc 1',
+      id: `subdoc1-${time}`,
+    },
+    [`ex-crud-subcollection2/doc1-${time}/subcollection2/subdoc2-${time}`]: {
+      example: 'subdoc 2',
+      id: `subdoc2-${time}`,
+    },
+    [`ex-crud-subcollection2/doc1-${time}/subcollection2/subdoc3-${time}`]: {
+      example: 'subdoc 3',
+      id: `subdoc3-${time}`,
+    },
+  },
+  docWithSubcollection3: {
+    [`ex-crud-subcollection2/doc2-${time}`]: {
+      example: 'doc 2',
       id: `doc2-${time}`,
     },
-    [`example-deletion/doc2-${time}/subcollection1/subdoc1-${time}`]: {
-      example: 'deletion 4',
+    [`ex-crud-subcollection2/doc2-${time}/subcollection1/subdoc4-${time}`]: {
+      example: 'subdoc 4',
       id: `subdoc4-${time}`,
     },
-    [`example-deletion/doc2-${time}/subcollection1/subdoc2-${time}`]: {
-      example: 'deletion 5',
+    [`ex-crud-subcollection2/doc2-${time}/subcollection2/subdoc5-${time}`]: {
+      example: 'subdoc 5',
       id: `subdoc5-${time}`,
     },
-    [`example-deletion/doc2-${time}/subcollection2/subdoc1-${time}`]: {
-      example: 'deletion 6',
+    [`ex-crud-subcollection2/doc2-${time}/subcollection2/subdoc6-${time}`]: {
+      example: 'subdoc 6',
       id: `subdoc6-${time}`,
     },
   },
+  //--------
 
-  docWithSubCollectionToDelete3: {
-    [`example-deletion2/doc3-${time}`]: {
-      example: 'deletion 3',
+  // array test
+  docWithSubcollection4: {
+    [`ex-crud-subcollection3/doc1-${time}`]: {
+      example: 'doc 1',
+      id: `doc1-${time}`,
+    },
+    [`ex-crud-subcollection3/doc1-${time}/subcollection1/subdoc1-${time}`]: {
+      example: 'subdoc 1',
+      id: `subdoc1-${time}`,
+    },
+    [`ex-crud-subcollection3/doc1-${time}/subcollection2/subdoc2-${time}`]: {
+      example: 'subdoc 2',
+      id: `subdoc2-${time}`,
+    },
+    [`ex-crud-subcollection3/doc1-${time}/subcollection2/subdoc3-${time}`]: {
+      example: 'subdoc 3',
+      id: `subdoc3-${time}`,
+    },
+  },
+  docWithSubcollection5: {
+    [`ex-crud-subcollection3/doc2-${time}`]: {
+      example: 'doc 2',
+      id: `doc2-${time}`,
+    },
+    [`ex-crud-subcollection3/doc2-${time}/subcollection1/subdoc4-${time}`]: {
+      example: 'subdoc 4',
+      id: `subdoc4-${time}`,
+    },
+    [`ex-crud-subcollection3/doc2-${time}/subcollection2/subdoc5-${time}`]: {
+      example: 'subdoc 5',
+      id: `subdoc5-${time}`,
+    },
+    [`ex-crud-subcollection3/doc2-${time}/subcollection2/subdoc6-${time}`]: {
+      example: 'subdoc 6',
+      id: `subdoc6-${time}`,
+    },
+  },
+  //--------
+
+  // array of collections test
+  docWithSubcollection6: {
+    [`ex-crud-subcollection4/doc1-${time}`]: {
+      example: 'doc 1',
+      id: `doc1-${time}`,
+    },
+    [`ex-crud-subcollection4/doc1-${time}/subcollection1/subdoc1-${time}`]: {
+      example: 'subdoc 1',
+      id: `subdoc1-${time}`,
+    },
+    [`ex-crud-subcollection4/doc1-${time}/subcollection2/subdoc2-${time}`]: {
+      example: 'subdoc 2',
+      id: `subdoc2-${time}`,
+    },
+    [`ex-crud-subcollection4/doc1-${time}/subcollection2/subdoc3-${time}`]: {
+      example: 'subdoc 3',
+      id: `subdoc3-${time}`,
+    },
+  },
+  docWithSubcollection7: {
+    [`ex-crud-subcollection4/doc2-${time}`]: {
+      example: 'doc 2',
+      id: `doc2-${time}`,
+    },
+    [`ex-crud-subcollection4/doc2-${time}/subcollection1/subdoc4-${time}`]: {
+      example: 'subdoc 4',
+      id: `subdoc4-${time}`,
+    },
+    [`ex-crud-subcollection4/doc2-${time}/subcollection2/subdoc5-${time}`]: {
+      example: 'subdoc 5',
+      id: `subdoc5-${time}`,
+    },
+    [`ex-crud-subcollection4/doc2-${time}/subcollection2/subdoc6-${time}`]: {
+      example: 'subdoc 6',
+      id: `subdoc6-${time}`,
+    },
+  },
+  docWithSubcollection8: {
+    [`ex-crud-subcollection5/doc3-${time}`]: {
+      example: 'doc 3',
       id: `doc3-${time}`,
     },
-    [`example-deletion2/doc3-${time}/subcollection3/subdoc1-${time}`]: {
-      example: 'deletion 7',
+    [`ex-crud-subcollection5/doc3-${time}/subcollection1/subdoc7-${time}`]: {
+      example: 'subdoc 7',
       id: `subdoc7-${time}`,
     },
-    [`example-deletion2/doc3-${time}/subcollection4/subdoc1-${time}`]: {
-      example: 'deletion 8',
+    [`ex-crud-subcollection5/doc3-${time}/subcollection2/subdoc8-${time}`]: {
+      example: 'subdoc 8',
       id: `subdoc8-${time}`,
     },
-    [`example-deletion2/doc3-${time}/subcollection4/subdoc2-${time}`]: {
-      example: 'deletion 9',
-      id: `subdoc10-${time}`,
+    [`ex-crud-subcollection5/doc3-${time}/subcollection2/subdoc9-${time}`]: {
+      example: 'subdoc 9',
+      id: `subdoc9-${time}`,
     },
   },
 };
--- apps/festival/festival-e2e/src/integration/_test/_test.spec.ts
+++ apps/festival/festival-e2e/src/integration/_test/_test.spec.ts
@@ -5,198 +5,128 @@ import { examples } from '../../fixtures/_test';
 //* TESTS --------------------------------------*//
 
 describe('Testing bridge between Cypress and node', () => {
-  beforeEach(() => {
-    firebase.clear();
+  describe('creation possibilities', () => {
+    beforeEach(() => {
+      firebase.clear();
+    });
+
+    //* CREATE *//
+
+    it('creates a single doc', () => {
+      firebase.create([examples.simpleDoc]);
+    });
+
+    it('creates multiple docs from an object', () => {
+      firebase.create([examples.multipleDocs]);
+    });
+
+    it('creates multiple docs from an array of objects', () => {
+      firebase.create([examples.simpleDocBis, examples.simpleDocTer]);
+    });
+
+    it('creates a doc with an array in a field', () => {
+      firebase.create([examples.docWithArray]);
+    });
+
+    it('creates a doc with an object', () => {
+      firebase.create([examples.docWithObject]);
+    });
+
+    it('creates a doc with a subcollection', () => {
+      firebase.create([examples.docWithSubcollection]);
+    });
   });
 
-  //* CREATE *//
+  describe('create/get/delete', () => {
+    beforeEach(() => {
+      firebase.clear();
+    });
 
-  it('creates a single doc', () => {
-    firebase.create([examples.singleDoc]);
+    //*** docs without subcollections */
+
+    it('document', () => {
+      firebase.create([examples.simpleDoc1]);
+      const path = Object.keys(examples.simpleDoc1)[0];
+      const exampleValues = exampleValuesFrom([examples.simpleDoc1]);
+      firebase.get([path]).then(data => expect(data).to.eql(exampleValues.flat()));
+      firebase.delete([path]);
+      firebase.get([path]).then(data => expect(data[0]).to.eql({}));
+    });
+
+    it('collection', () => {
+      const docs = [examples.simpleDoc2, examples.simpleDoc3];
+      firebase.create(docs);
+      const collectionPath = Object.keys(examples.simpleDoc2)[0].split('/')[0];
+      const exampleValues = exampleValuesFrom(docs);
+      firebase.get([collectionPath]).then((data: []) => expect(data.flat()).to.eql(exampleValues.flat()));
+      firebase.delete([collectionPath]);
+      firebase.get([collectionPath]).then((data: []) => expect(data.flat()).to.eql([]));
+    });
+
+    it('array of documents', () => {
+      const docs = [examples.simpleDoc4, examples.simpleDoc5];
+      firebase.create(docs);
+      const docPaths = docs.map(doc => Object.keys(doc)[0]);
+      const exampleValues = exampleValuesFrom(docs);
+      firebase.get(docPaths).then((data: []) => expect(data.flat()).to.eql(exampleValues.flat()));
+      firebase.delete(docPaths);
+      firebase.get(docPaths).then((data: []) => expect(data.flat()).to.eql([{}, {}]));
+    });
+
+    it('array of collection', () => {
+      const docs = [examples.simpleDoc6, examples.simpleDoc7, examples.simpleDoc8];
+      firebase.create(docs);
+      const collectionsPaths = [...new Set(docs.map(doc => Object.keys(doc)[0]))];
+      const exampleValues = exampleValuesFrom(docs);
+      firebase.get(collectionsPaths).then((data: []) => expect(data.flat()).to.eql(exampleValues.flat()));
+      firebase.delete(collectionsPaths);
+      firebase.get(collectionsPaths).then((data: []) => expect(data.flat()).to.eql([{}, {}, {}]));
+    });
+
+    //*** docs with subcollections */
+
+    it('document with subcollections', () => {
+      firebase.create([examples.docWithSubcollection1]);
+      const path = Object.keys(examples.docWithSubcollection1)[0];
+      const parentDocPath = path.split('/').slice(0, 2).join('/');
+      const exampleValues = exampleValuesFrom([examples.docWithSubcollection1]);
+      firebase.get([parentDocPath]).then(data => expect(data).to.eql(exampleValues.flat()));
+      firebase.delete([parentDocPath]);
+      firebase.get([parentDocPath]).then(data => expect(data[0]).to.eql({}));
+    });
+
+    it('collection of documents with subcollections', () => {
+      const docs = [examples.docWithSubcollection2, examples.docWithSubcollection3];
+      firebase.create(docs);
+      const collectionPath = Object.keys(docs[0])[0].split('/')[0];
+      const exampleValues = exampleValuesFrom(docs);
+      firebase.get([collectionPath]).then(data => expect(data[0]).to.eql(exampleValues.flat()));
+      firebase.delete([collectionPath]);
+      firebase.get([collectionPath]).then((data: []) => expect(data.flat()).to.eql([]));
+    });
+
+    it('array of documents with subcollections', () => {
+      const docs = [examples.docWithSubcollection4, examples.docWithSubcollection5];
+      firebase.create(docs);
+      const docPaths = docs.map(doc => Object.keys(doc)[1].split('/').slice(0, 2).join('/'));
+      const exampleValues = exampleValuesFrom(docs);
+      firebase.get(docPaths).then(data => expect(data).to.eql(exampleValues.flat()));
+      firebase.delete(docPaths);
+      firebase.get(docPaths).then((data: []) => expect(data.flat()).to.eql([{}, {}]));
+    });
+
+    it('array of collections of documents with subcollections', () => {
+      const docs = [examples.docWithSubcollection6, examples.docWithSubcollection7, examples.docWithSubcollection8];
+      firebase.create(docs);
+      const collectionsPaths = [...new Set(docs.map(doc => Object.keys(doc)[0].split('/')[0]))];
+      const exampleValues = exampleValuesFrom(docs);
+      firebase.get(collectionsPaths).then((data: []) => expect(data.flat()).to.eql(exampleValues.flat()));
+      firebase.delete(collectionsPaths);
+      firebase.get(collectionsPaths).then((data: []) => expect(data.flat()).to.eql([]));
+    });
+
+    //TODO : UPDATE
   });
-
-  it('creates multiple docs from an object', () => {
-    firebase.create([examples.multipleDocs]);
-  });
-
-  it('creates multiple docs from two objects', () => {
-    firebase.create([examples.singleDocBis, examples.singleDocTer]);
-  });
-
-  it('creates a doc with an array in a field', () => {
-    firebase.create([examples.docWithArray]);
-  });
-
-  it('creates a doc with an object', () => {
-    firebase.create([examples.docWithObject]);
-  });
-
-  it('creates a doc with a subcollection', () => {
-    firebase.create([examples.docWithSubCollection1]);
-  });
-
-  //* GET *//
-
-  //*** docs without subcollections */
-
-  it('retrieves a document', () => {
-    firebase.create([examples.singleDoc]);
-    const path = Object.keys(examples.singleDoc)[0];
-    const exampleValues = exampleValuesFrom([examples.singleDoc]);
-    firebase.get([path]).then(data => expect(data).to.eql(exampleValues.flat()));
-  });
-
-  it('retrieves a collection', () => {
-    firebase.create([examples.multipleDocs]);
-    const collectionPath = Object.keys(examples.multipleDocs)[0].split('/')[0];
-    const exampleValues = exampleValuesFrom([examples.multipleDocs]);
-    firebase.get([collectionPath]).then(data => expect(data).to.eql(exampleValues));
-  });
-
-  it('retrieves an array of documents', () => {
-    const docs = [examples.singleDoc, examples.singleDocBis, examples.singleDocTer];
-    firebase.create(docs);
-    const paths = docs.map(doc => Object.keys(doc)[0]);
-    const exampleValues = exampleValuesFrom(docs);
-    firebase.get(paths).then(data => expect(data).to.eql(exampleValues.flat()));
-  });
-
-  it('retrieves an array of collections', () => {
-    const docs = [examples.singleDoc, examples.multipleDocs];
-    firebase.create(docs);
-    const collectionsPaths = docs.map(doc => Object.keys(doc)[0].split('/')[0]);
-    const exampleValues = exampleValuesFrom(docs);
-    firebase.get(collectionsPaths).then(data => expect(data).to.eql(exampleValues));
-  });
-
-  //*** docs with subcollections */
-
-  it('retrieves a document with subcollections', () => {
-    firebase.create([examples.docWithSubCollection1]);
-    const path = Object.keys(examples.docWithSubCollection1)[0];
-    const parentDocPath = path.split('/').slice(0, 2).join('/');
-    const exampleValues = exampleValuesFrom([examples.docWithSubCollection1]);
-    firebase.get([parentDocPath]).then(data => expect(data).to.eql(exampleValues.flat()));
-  });
-
-  it('retrieves a collection of documents with subcollections', () => {
-    const docs = [examples.docWithSubCollection1, examples.docWithSubCollection2];
-    firebase.create(docs);
-    const collectionPath = Object.keys(docs[0])[0].split('/')[0];
-    const exampleValues = exampleValuesFrom(docs);
-    firebase.get([collectionPath]).then(data => expect(data[0]).to.eql(exampleValues.flat()));
-  });
-
-  it('retrieves a array of documents with subcollections', () => {
-    const docs = [examples.docWithSubCollection1, examples.docWithSubCollection2];
-    firebase.create(docs);
-    const docPaths = docs.map(doc => Object.keys(doc)[1].split('/').slice(0, 2).join('/'));
-    const exampleValues = exampleValuesFrom(docs);
-    firebase.get(docPaths).then(data => expect(data).to.eql(exampleValues.flat()));
-  });
-
-  it('retrieves an array of collection of documents with subcollections', () => {
-    const docs = [examples.docWithSubCollection1, examples.docWithSubCollection2, examples.docWithSubCollection3];
-    firebase.create(docs);
-    // keeps only unique collection paths
-    const collectionsPaths = [...new Set(docs.map(doc => Object.keys(doc)[0].split('/')[0]))];
-    const exampleValues = exampleValuesFrom(docs);
-    firebase.get(collectionsPaths).then((data: []) => expect(data.flat()).to.eql(exampleValues.flat()));
-  });
-
-  //* DELETE *//
-
-  //*** docs without subcollections */
-
-  it('deletes a doc', () => {
-    firebase.create([examples.docToDelete1]);
-    const path = Object.keys(examples.docToDelete1)[0];
-    const exampleValues = exampleValuesFrom([examples.docToDelete1]);
-    firebase.get([path]).then(data => expect(data).to.eql(exampleValues.flat()));
-    firebase.delete([path]);
-    firebase.get([path]).then(data => expect(data[0]).to.eql({}));
-  });
-
-  it('deletes a collection', () => {
-    const docs = [examples.docToDelete1, examples.docToDelete2];
-    firebase.create(docs);
-    const collectionPath = Object.keys(examples.docToDelete1)[0].split('/')[0];
-    const exampleValues = exampleValuesFrom(docs);
-    firebase.get([collectionPath]).then((data: []) => expect(data.flat()).to.eql(exampleValues.flat()));
-    firebase.delete([collectionPath]);
-    cy.wait(100); // temporary async fix
-    firebase.get([collectionPath]).then((data: []) => expect(data.flat()).to.eql([]));
-  });
-
-  it('deletes an array of documents', () => {
-    const docs = [examples.docToDelete1, examples.docToDelete2];
-    firebase.create(docs);
-    const docPaths = docs.map(doc => Object.keys(doc)[0]);
-    const exampleValues = exampleValuesFrom(docs);
-    firebase.get(docPaths).then((data: []) => expect(data.flat()).to.eql(exampleValues.flat()));
-    firebase.delete(docPaths);
-    firebase.get(docPaths).then((data: []) => expect(data.flat()).to.eql([{}, {}]));
-  });
-
-  it('deletes an array of collection of documents', () => {
-    const docs = [examples.docToDelete1, examples.docToDelete2, examples.docToDelete3];
-    firebase.create(docs);
-    const collectionsPaths = [...new Set(docs.map(doc => Object.keys(doc)[0]))];
-    const exampleValues = exampleValuesFrom(docs);
-    firebase.get(collectionsPaths).then((data: []) => expect(data.flat()).to.eql(exampleValues.flat()));
-    firebase.delete(collectionsPaths);
-    firebase.get(collectionsPaths).then((data: []) => expect(data.flat()).to.eql([{}, {}, {}]));
-  });
-
-  //*** docs with subcollections */
-
-  it('deletes a document with subcollections', () => {
-    firebase.create([examples.docWithSubCollectionToDelete1]);
-    const path = Object.keys(examples.docWithSubCollectionToDelete1)[0];
-    const parentDocPath = path.split('/').slice(0, 2).join('/');
-    const exampleValues = exampleValuesFrom([examples.docWithSubCollectionToDelete1]);
-    firebase.get([parentDocPath]).then(data => expect(data).to.eql(exampleValues.flat()));
-    firebase.delete([parentDocPath]);
-    firebase.get([parentDocPath]).then(data => expect(data[0]).to.eql({}));
-  });
-
-  it('deletes a collection of documents with subcollections', () => {
-    const docs = [examples.docWithSubCollectionToDelete1, examples.docWithSubCollectionToDelete2];
-    firebase.create(docs);
-    const collectionPath = Object.keys(docs[0])[0].split('/')[0];
-    const exampleValues = exampleValuesFrom(docs);
-    firebase.get([collectionPath]).then(data => expect(data[0]).to.eql(exampleValues.flat()));
-    firebase.delete([collectionPath]);
-    cy.wait(100); // temporary async fix
-    firebase.get([collectionPath]).then((data: []) => expect(data.flat()).to.eql([]));
-  });
-
-  it('deletes a array of documents with subcollections', () => {
-    const docs = [examples.docWithSubCollectionToDelete1, examples.docWithSubCollectionToDelete2];
-    firebase.create(docs);
-    const docPaths = docs.map(doc => Object.keys(doc)[1].split('/').slice(0, 2).join('/'));
-    const exampleValues = exampleValuesFrom(docs);
-    firebase.get(docPaths).then(data => expect(data).to.eql(exampleValues.flat()));
-    firebase.delete(docPaths);
-    firebase.get(docPaths).then((data: []) => expect(data.flat()).to.eql([{}, {}]));
-  });
-
-  it('deletes an array of collection of documents with subcollections', () => {
-    const docs = [
-      examples.docWithSubCollectionToDelete1,
-      examples.docWithSubCollectionToDelete2,
-      examples.docWithSubCollectionToDelete3,
-    ];
-    firebase.create(docs);
-    const collectionsPaths = [...new Set(docs.map(doc => Object.keys(doc)[0].split('/')[0]))];
-    const exampleValues = exampleValuesFrom(docs);
-    firebase.get(collectionsPaths).then((data: []) => expect(data.flat()).to.eql(exampleValues.flat()));
-    firebase.delete(collectionsPaths);
-    cy.wait(100); // temporary async fix
-    firebase.get(collectionsPaths).then((data: []) => expect(data.flat()).to.eql([]));
-  });
-
-  //TODO : UPDATE
 });
 
 //* FUNCTIONS -------------------------------*//
@@ -204,14 +134,21 @@ describe('Testing bridge between Cypress and node', () => {
 const firebase = {
   clear() {
     this.delete([
-      'example-single',
-      'example-multiple',
-      'example-array',
-      'example-object',
-      'example-subcollection',
-      'example-subcollectionBis',
-      'example-deletion',
-      'example-deletion2',
+      'ex-create-simple',
+      'ex-create-multiple',
+      'ex-create-array',
+      'ex-create-object',
+      'ex-subcollection',
+      'ex-crud-simple1',
+      'ex-crud-simple2',
+      'ex-crud-simple3',
+      'ex-crud-simple4',
+      'ex-crud-simple5',
+      'ex-crud-subcollection1',
+      'ex-crud-subcollection2',
+      'ex-crud-subcollection3',
+      'ex-crud-subcollection4',
+      'ex-crud-subcollection5',
     ]);
   },
 
