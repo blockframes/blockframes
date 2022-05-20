769e731bc8a6d21e4a3758450c720b1480185c31
Yonga Springfield <springfieldyonga@outlook.com>
2022-05-20 11:44:34 +0100

    refactor: remove extraneous comment

--- libs/import/src/lib/pipes/is-disabled.pipe.ts
+++ libs/import/src/lib/pipes/is-disabled.pipe.ts
@@ -2,7 +2,6 @@ import { NgModule, Pipe, PipeTransform } from '@angular/core';
 import { ImportState } from '../utils';
 import { errorCount } from './error-count.pipe';
 
-//@continue from here. The create button is disabled only if we set pure to false in the Pipe annotation.
 @Pipe({ name: 'isDisabled' })
 export class IsDisabledPipe implements PipeTransform {
   transform(element: ImportState, processing: number, imported=false) {
