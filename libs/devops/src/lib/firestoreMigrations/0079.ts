13cc1615f24f7e554aaf0f7fad4995609ab2c779
KevinDurand974 <k.durand.974@gmail.com>
2022-05-24 12:37:28 +0200

    Add migration for organizations, invitations and notifications

---
+++ libs/devops/src/lib/firestoreMigrations/0079.ts
@@ -0,0 +1,117 @@
+import { Firestore, runChunks } from '@blockframes/firebase-utils';
+import { Organization, Invitation, Notification, PublicOrganization } from '@blockframes/model';
+
+// Array for special Organizations
+const specialOrgs = [
+  // { id: 'org id', name: 'Org name' }
+];
+
+/**
+ * Remove isBlockchainEnabled from org documents
+ * @param db
+ * @returns
+ */
+export async function upgrade(db: Firestore) {
+  console.log('Migrate Organizations');
+  await migrateOrganizations(db);
+  console.log('Migrate Invitations');
+  await migrateInvitations(db);
+  console.log('Migrate Notifications');
+  return await migrateNotifications(db);
+}
+
+async function migrateOrganizations(db: Firestore) {
+  const orgs = await db.collection('orgs').get();
+
+  return runChunks(orgs.docs, async (doc) => {
+    const org = doc.data() as Organization;
+
+    // Skip all organization without denomination
+    if (!org?.denomination) return false;
+
+    const publicName = org.denomination.public;
+    const fullName = org.denomination.full;
+
+    const { updatedOrg, update } = updateOrganization(org, fullName, publicName);
+
+    if (update) await doc.ref.set(updatedOrg);
+  }).catch(err => console.error(err));
+}
+
+async function migrateInvitations(db: Firestore) {
+  const invitations = await db.collection('invitations').get();
+
+  return runChunks(invitations.docs, async (doc) => {
+    const invitationBase = doc.data() as Invitation;
+    const type = invitationBase?.fromOrg ? 'fromOrg' : 'toOrg';
+    const invitation = invitationBase[type];
+
+    // Skip all invitation without denomination
+    if (!invitation?.denomination) return false;
+
+    const publicName = invitation.denomination.public;
+    const fullName = invitation.denomination.full;
+
+    const { updatedOrg, update } = updateOrganization(invitation, fullName, publicName);
+    invitationBase[type] = updatedOrg;
+
+    if (update) await doc.ref.set(invitationBase);
+  }).catch(err => console.error(err));
+}
+
+async function migrateNotifications(db: Firestore) {
+  const notifications = await db.collection('notifications').get();
+
+  return runChunks(notifications.docs, async (doc) => {
+    const notificationBase = doc.data() as Notification;
+
+    // Skip all notifications without organization or organization.denomination
+    if (!notificationBase?.organization || !notificationBase.organization?.denomination) return false;
+
+    const notification = notificationBase.organization;
+    const publicName = notification.denomination.public;
+    const fullName = notification.denomination.full;
+
+    const { updatedOrg, update } = updateOrganization(notification, fullName, publicName);
+    notificationBase.organization = updatedOrg;
+
+    if (update) await doc.ref.set(notificationBase);
+  }).catch(err => console.error(err));
+}
+
+
+function updateOrganization(org: PublicOrganization, fullName: string, publicName: string) {
+  let update = false;
+
+  // If org is part of specialOrgs
+  const specialOrgsIds = specialOrgs.map(org => org.id);
+  if (specialOrgs.length && specialOrgsIds.includes(org.id)) {
+    const index = specialOrgsIds.indexOf(org.id);
+    delete org.denomination;
+    (org as any).name = specialOrgs[index].name;
+    update = true;
+  }
+
+  // If org denomination public === full
+  else if (fullName === publicName) {
+    delete org.denomination;
+    (org as any).name = fullName;
+    update = true;
+  }
+
+  // If org have a value on full but not in public
+  else if (fullName.length && !publicName.length) {
+    delete org.denomination;
+    (org as any).name = fullName;
+    update = true;
+  }
+
+  // If org have a value on public but not in full
+  else if (publicName.length && !fullName.length) {
+    delete org.denomination;
+    (org as any).name = publicName;
+    update = true;
+  }
+
+  return { updatedOrg: org, update };
+}
