92cd166922f0e87bcae89da4a5c082b07775838d
kevin-m <kmarais@cascadae8.com>
2022-05-30 12:02:11 +0200

    Removed Pipe and use 'this.mediaService.generateImgIxUrl' for donwload file

--- libs/movie/src/lib/movie/form/summary/available-materials/available-materials.component.ts
+++ libs/movie/src/lib/movie/form/summary/available-materials/available-materials.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { MediaService } from '@blockframes/media/service';
+import { StorageFile } from '@blockframes/model';
 import { MovieForm } from '@blockframes/movie/form/movie.form';
 import { boolean } from '@blockframes/utils/decorators/decorators';
 
@@ -16,6 +18,8 @@ export class SummaryAvailableMaterialsComponent implements OnInit {
 
   public versionLength: number;
 
+  constructor( private mediaService: MediaService ) { }
+
   ngOnInit() {
     this.versionLength = Object.keys(this.movie.languages.controls).length;
   }
@@ -24,4 +28,8 @@ export class SummaryAvailableMaterialsComponent implements OnInit {
     return this.movie.get('delivery').get('file').value;
   }
 
+  public async getDownloadUrl(file: StorageFile) {
+    const url = await this.mediaService.generateImgIxUrl(file, {});
+    window.open(url);
+  }
 }
