fe4c43f9e1527c8ed7c359b400925477714db898
George <george.g93@gmail.com>
2022-06-02 22:08:35 +1000

    Update apps/backend-functions/src/notification.ts
    Co-authored-by: Bruce <eelios@gmail.com>

--- apps/backend-functions/src/notification.ts
+++ apps/backend-functions/src/notification.ts
@@ -321,7 +321,7 @@ export async function onNotificationCreate(snap: FirebaseFirestore.DocumentSnaps
         break;
     }
 
-    // ! TODO! Don't do this - unify instantiation of Firestore
+    // ! TODO  #8376 Don't do this - unify instantiation of Firestore
     const db = admin.firestore();
     await db.collection('notifications').doc(notification.id).set({ email: notification.email }, { merge: true });
   }
