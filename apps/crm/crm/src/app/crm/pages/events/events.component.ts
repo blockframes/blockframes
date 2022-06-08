6ffc8bd806e66471704854e0839bed5d701fd62e
kevin-m <kmarais@cascadae8.com>
2022-06-08 15:08:57 +0200

    Remove unused import

--- apps/crm/crm/src/app/crm/pages/events/events.component.ts
+++ apps/crm/crm/src/app/crm/pages/events/events.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, ChangeDetectorRef, ChangeDetectionStrategy } from '@
 import { downloadCsvFromJson } from '@blockframes/utils/helpers';
 import { EventService } from '@blockframes/event/service';
 import { InvitationService } from '@blockframes/invitation/service';
-import { Router } from '@angular/router';
 import { OrganizationService } from '@blockframes/organization/service';
 import { orgName, toLabel } from '@blockframes/model';
 import { where } from 'firebase/firestore';
@@ -21,7 +20,6 @@ export class EventsComponent implements OnInit {
     private eventService: EventService,
     private invitationService: InvitationService,
     private cdRef: ChangeDetectorRef,
-    private router: Router,
     private orgService: OrganizationService
   ) { }
 
