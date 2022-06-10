1ebb06faa1dd6a493c04f348e83366934776860e
George Grigorian <george.g93@gmail.com>
2022-06-11 01:33:11 +1000

    cleanup

--- libs/devops/src/lib/firebase-utils/firestore/emulator.ts
+++ libs/devops/src/lib/firebase-utils/firestore/emulator.ts
@@ -409,18 +409,18 @@ export function writeRuntimeConfig(values: { [key: string]: string }, path: stri
  * This tuple array maps field names to the environment variable key to set them to for runtimeconfig.json and
  * firebase secrets
  */
-export const functionsConfigMap: Record<string, string> = {
-  'sendgrid.api_key': 'SENDGRID_API_KEY',// @see https://www.notion.so/cascade8/Setup-SendGrid-c8c6011ad88447169cebe1f65044abf0
-  'jwplayer.key': 'JWPLAYER_KEY',// @see https://www.notion.so/cascade8/Setup-JWPlayer-2276fce57b464b329f0b6d2e7c6d9f1d
+export const functionsConfigMap: { [key: string]: string } = {
+  'sendgrid.api_key': 'SENDGRID_API_KEY', // @see https://www.notion.so/cascade8/Setup-SendGrid-c8c6011ad88447169cebe1f65044abf0
+  'jwplayer.key': 'JWPLAYER_KEY', // @see https://www.notion.so/cascade8/Setup-JWPlayer-2276fce57b464b329f0b6d2e7c6d9f1d
   'jwplayer.secret': 'JWPLAYER_SECRET',
   'jwplayer.apiv2secret': 'JWPLAYER_APIV2SECRET',
   'algolia.api_key': 'ALGOLIA_API_KEY',
-  'imgix.token': 'IMGIX_TOKEN',// @see https://www.notion.so/cascade8/Setup-ImgIx-c73142c04f8349b4a6e17e74a9f2209a
+  'imgix.token': 'IMGIX_TOKEN', // @see https://www.notion.so/cascade8/Setup-ImgIx-c73142c04f8349b4a6e17e74a9f2209a
   'twilio.account.sid': 'TWILIO_ACCOUNT_SID',
   'twilio.account.secret': 'TWILIO_ACCOUNT_SECRET',
   'twilio.api.key.secret': 'TWILIO_API_KEY_SECRET',
   'twilio.api.key.sid': 'TWILIO_API_KEY_SID',
   'mailchimp.api_key': 'MAILCHIMP_API_KEY',
   'mailchimp.server': 'MAILCHIMP_SERVER',
-  'mailchimp.list_id': 'MAILCHIMP_LIST_ID'
-}
+  'mailchimp.list_id': 'MAILCHIMP_LIST_ID',
+};
