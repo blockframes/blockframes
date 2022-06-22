9241fab3880bc5c14f6c77bab5b1f48c315d15bb
kevin-m <kmarais@cascadae8.com>
2022-06-22 14:28:20 +0200

    fix review, remove unused modules

--- apps/catalog/catalog/src/app/marketplace/title/avails/explanation/explanation.module.ts
+++ apps/catalog/catalog/src/app/marketplace/title/avails/explanation/explanation.module.ts
@@ -5,15 +5,12 @@ import { ExplanationComponent } from './explanation.component';
 import { GlobalModalModule } from '@blockframes/ui/global-modal/global-modal.module';
 
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
-
 
 @NgModule({
   imports: [
     CommonModule,
     MatButtonModule,
-    MatIconModule,
     MatTabsModule,
     GlobalModalModule
   ],
--- libs/event/src/lib/components/explanation/explanation.module.ts
+++ libs/event/src/lib/components/explanation/explanation.module.ts
@@ -5,16 +5,11 @@ import { ExplanationComponent } from './explanation.component';
 import { GlobalModalModule } from '@blockframes/ui/global-modal/global-modal.module';
 
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTabsModule } from '@angular/material/tabs';
-
 
 @NgModule({
   imports: [
     CommonModule,
     MatButtonModule,
-    MatIconModule,
-    MatTabsModule,
     GlobalModalModule
   ],
   declarations: [ExplanationComponent],
