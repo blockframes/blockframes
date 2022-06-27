6d9032d5da01840f4570d3777418f756a51da5ef
KevinDurand974 <k.durand.974@gmail.com>
2022-06-27 13:45:44 +0200

    Re-remove orgNameModule with the merge of develop

--- apps/festival/festival/src/app/dashboard/analytics/title/title-analytics.module.ts
+++ apps/festival/festival/src/app/dashboard/analytics/title/title-analytics.module.ts
@@ -20,7 +20,6 @@ import { MetricCardListModule } from '@blockframes/analytics/components/metric-c
 import { GuestPipeModule } from '@blockframes/invitation/pipes/guest.pipe';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { LineChartModule } from '@blockframes/analytics/components/line-chart/line-chart.module';
-import { OrgNameModule } from '@blockframes/organization/pipes';
 import { DurationModule } from '@blockframes/utils/pipes/duration.pipe';
 
 @NgModule({
@@ -38,7 +37,6 @@ import { DurationModule } from '@blockframes/utils/pipes/duration.pipe';
     OngoingButtonModule,
     GuestPipeModule,
     LineChartModule,
-    OrgNameModule,
     DurationModule,
 
     // Material
