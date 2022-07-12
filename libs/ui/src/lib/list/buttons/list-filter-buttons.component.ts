afb54fb6ad17241d3f31c6f2dbb8c02ea2f233bf
kevin-m <kmarais@cascadae8.com>
2022-07-12 22:14:42 +0200

    Create component 'list-filter-buttons'

---
+++ libs/ui/src/lib/list/buttons/list-filter-buttons.component.ts
@@ -0,0 +1,71 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Inject,
+  OnDestroy,
+  OnInit,
+  Output,
+  EventEmitter,
+  Input,
+  ChangeDetectorRef
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { App } from '@blockframes/model';
+import { decodeUrl } from '@blockframes/utils/form/form-state-url-encoder';
+import { APP } from '@blockframes/utils/routes/utils';
+import { Subscription } from 'rxjs';
+
+type FilterButtonsState = Record<'save' | 'load', 'enabled' | 'active' | 'enabledAndActive' | 'disabled'>;
+
+const savedSearchIdentifier = 'saved-search';
+
+@Component({
+  selector: 'list-filter-buttons',
+  templateUrl: './list-filter-buttons.component.html',
+  styleUrls: ['./list-filter-buttons.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ListFilterButtonsComponent implements OnDestroy, OnInit {
+
+  public buttonsState: FilterButtonsState = {
+    save: 'enabled',
+    load: 'disabled',
+  }
+
+  private queryParamsSub: Subscription;
+  @Output() data: EventEmitter<string> = new EventEmitter();
+
+  constructor(
+    private route: ActivatedRoute,
+    private cdRef: ChangeDetectorRef,
+    @Inject(APP) public app: App,
+  ) { }
+
+
+  ngOnInit() {
+    this.queryParamsSub = this.route.queryParams.subscribe(_ => this.setButtonsState(this.app, this.buttonsState))
+  }
+
+  ngOnDestroy() {
+    this.queryParamsSub.unsubscribe();
+  }
+  save() {
+    const routeParams = decodeUrl(this.route);
+    localStorage.setItem(`${this.app}-${savedSearchIdentifier}`, JSON.stringify(routeParams));
+    this.setButtonsState(this.app, this.buttonsState);
+  }
+
+  load() {
+    const routeParams = localStorage.getItem(`${this.app}-${savedSearchIdentifier}`);
+    const parsedData = JSON.parse(routeParams);
+    this.data.emit(parsedData);
+  }
+
+  setButtonsState(app: App, buttons: FilterButtonsState) {
+    const dataStorage = localStorage.getItem(`${app}-${savedSearchIdentifier}`);
+    const currentRouteParams = this.route.snapshot.queryParams.formValue;
+    if (dataStorage) buttons.save = 'active', buttons.load = 'enabled';
+    if (dataStorage === currentRouteParams) buttons.save = 'enabledAndActive', buttons.load = 'active';
+    this.cdRef.markForCheck();
+  }
+}
