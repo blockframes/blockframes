d2a51d2cd1585a8e254b906ab3794b6bedff2967
Remco <RemcoSimonides@gmail.com>
2022-07-13 07:40:37 +0200

    8769 guard buyer activity for no org user

--- libs/analytics/src/lib/guard/has-organization.guard.ts
+++ libs/analytics/src/lib/guard/has-organization.guard.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
+import { OrganizationService } from '@blockframes/organization/service';
 import { UserService } from '@blockframes/user/service';
 
 @Injectable({ providedIn: 'root' })
 export class HasOrganizationGuard implements CanActivate {
-  constructor(private service: UserService) { }
+  constructor(
+    private service: UserService,
+    private router: Router,
+    private orgService: OrganizationService
+  ) { }
 
   async canActivate(next: ActivatedRouteSnapshot) {
     const userId: string = next.params['userId'];
     const user = await this.service.getValue(userId);
 
-    return !!user.orgId;
+    if (!user?.orgId) return this.router.createUrlTree(['c/o/dashboard/home/buyer']);
+
+    const org = await this.orgService.getValue(user.orgId);
+    return org.status === 'accepted' ? true : this.router.createUrlTree(['c/o/dashboard/home/buyer']);
   }
 }
