97f6dbaef5b599885c723fb6b279088e56aa9b45
Antoine <antoinepoppe@hotmail.fr>
2022-07-14 12:24:55 +0200

    fix org card number of movies and members

--- libs/organization/src/lib/organization/components/card/card.component.ts
+++ libs/organization/src/lib/organization/components/card/card.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input, ChangeDetectionStrategy, OnInit, Inject } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, Inject, ChangeDetectorRef } from '@angular/core';
 import { fromOrgAndAccepted, MovieService } from '@blockframes/movie/service';
 import { Organization, App } from '@blockframes/model';
-import { Observable } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { APP } from '@blockframes/utils/routes/utils';
 import { boolean } from '@blockframes/utils/decorators/decorators';
@@ -13,28 +13,39 @@ import { OrganizationService } from '@blockframes/organization/service';
   styleUrls: ['./card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OrganizationCardComponent implements OnInit {
+export class OrganizationCardComponent {
 
-  @Input() org: Organization;
+  private _org: Organization;
+  get org() {
+    return this._org;
+  }
+  @Input() set org(value: Organization) {
+    this._org = value;
+
+    const movies$ = this.movieService.valueChanges(fromOrgAndAccepted(this.org.id, this.app)).pipe(
+      map(movies =>
+        movies.filter(movie => movie.app[this.app].access)
+      ))
+    
+    Promise.all([
+      firstValueFrom(movies$),
+      this.orgService.getMembers(this.org, { removeConcierges: true })
+    ]).then(res => {
+      this.orgMovieCount = res[0].length;
+      this.memberCount = res[1].length;
+      this.cdr.markForCheck();
+    })
+  }
+
   @Input() @boolean hideTabs = false;
 
-  public orgMovieCount$: Observable<number>;
-  public memberCount = 0;
+  public orgMovieCount: number;
+  public memberCount: number;
 
   constructor(
+    private cdr: ChangeDetectorRef,
     private movieService: MovieService,
     private orgService: OrganizationService,
     @Inject(APP) public app: App
   ) { }
-
-  async ngOnInit() {
-    this.orgMovieCount$ = this.movieService.valueChanges(fromOrgAndAccepted(this.org.id, this.app)).pipe(
-      map(movies =>
-        movies.filter(movie => movie.app[this.app].access).length
-      )
-    );
-
-    const members = await this.orgService.getMembers(this.org, { removeConcierges: true });
-    this.memberCount = members.length;
-  }
 }
