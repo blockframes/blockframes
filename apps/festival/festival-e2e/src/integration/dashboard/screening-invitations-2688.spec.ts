4c508305fa004f3966102379a78b37f1f74b76bf
KevinDurand974 <k.durand.974@gmail.com>
2022-07-15 10:58:20 +0200

    Remove line who's not used anymore

--- apps/festival/festival-e2e/src/integration/dashboard/screening-invitations-2688.spec.ts
+++ apps/festival/festival-e2e/src/integration/dashboard/screening-invitations-2688.spec.ts
@@ -109,9 +109,6 @@ describe('Organiser invites other users to private screening', () => {
       cy.log('Reach Market Home & navigate to Screening Page from Screening Schedule');
       cy.visit('/c/o/marketplace/home');
 
-      // Click on left menu
-      cy.get('festival-marketplace button[test-id=menu]').first().click();
-
       festival.selectSalesAgents();
 
       festival.clickOnOrganization(org.name);
