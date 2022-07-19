ad8a68ad04846e8e59a4d7c63b5065e5005bcbcd
kevin-m <kmarais@cascadae8.com>
2022-07-19 16:24:36 +0200

    Catalog: add 'logo-spinner' on Avails page

--- apps/catalog/catalog/src/app/dashboard/avails/list/list.module.ts
+++ apps/catalog/catalog/src/app/dashboard/avails/list/list.module.ts
@@ -1,16 +1,23 @@
+// Angular
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 import { FlexLayoutModule } from "@angular/flex-layout";
+
+//Material
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
-import { RouterModule } from "@angular/router";
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+// Modules
 import { AvailsFilterModule } from "@blockframes/contract/avails/filter/filter.module";
 import { ImageModule } from "@blockframes/media/image/directives/image.module";
 import { TableModule } from "@blockframes/ui/list/table/table.module";
+import { LogoSpinnerModule } from '@blockframes/ui/rive/logo-spinner.module'
 import { MaxLengthModule } from "@blockframes/utils/pipes";
+
+// Pages
 import { CatalogAvailsListComponent } from "./list.component";
-import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -20,12 +27,12 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     CommonModule,
     MaxLengthModule,
     FlexLayoutModule,
+    //Blockframes
     TableModule,
     ImageModule,
     AvailsFilterModule,
-
+    LogoSpinnerModule,
     //Material
-    MatProgressSpinnerModule,
     MatButtonModule,
     MatIconModule,
     MatTooltipModule,
