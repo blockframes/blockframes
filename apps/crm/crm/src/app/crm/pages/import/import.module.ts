1090f6e48fb77a0e6b61336e97d36e1dfa600b64
bruce <delorme.bruce.michel@gmail.com>
2022-07-25 15:16:07 +0200

    cleaning

--- apps/crm/crm/src/app/crm/pages/import/import.module.ts
+++ apps/crm/crm/src/app/crm/pages/import/import.module.ts
@@ -30,10 +30,6 @@ const routes: Routes = [{
       path: 'contracts',
       loadChildren: () => import('@blockframes/contract/import/import.module').then(m => m.ContractImportModule),
     },
-    {
-      path: 'imdb',
-      loadChildren: () => import('./imdb/imdb-import.module').then(m => m.ImdbImportModule),
-    },
   ]
 }];
 
