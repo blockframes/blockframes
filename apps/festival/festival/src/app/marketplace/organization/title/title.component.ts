0945d14ad90d2b4c0f89f43c3d1e9701926ff24a
KevinDurand974 <k.durand.974@gmail.com>
2022-07-26 12:52:04 +0200

    Add filters

--- apps/festival/festival/src/app/marketplace/organization/title/title.component.ts
+++ apps/festival/festival/src/app/marketplace/organization/title/title.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, startWith, switchMap } from 'rxjs/operators';
 import { ViewComponent } from '../view/view.component';
-import { Movie } from '@blockframes/model';
+import { GetKeys, Movie, Territory } from '@blockframes/model';
 import { MovieService } from '@blockframes/movie/service';
 import { scaleIn } from '@blockframes/utils/animations/fade';
 import { DynamicTitleService } from '@blockframes/utils/dynamic-title/dynamic-title.service';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable, map } from 'rxjs';
 import { orderBy, where } from 'firebase/firestore';
+import { FormControl, FormGroup } from '@angular/forms';
+import { EntityControl, FormEntity, FormList, FormStaticValueArray } from '@blockframes/utils/form';
+import { MovieSearch, Versions } from '@blockframes/movie/form/search.form';
+
+interface LanguageVersion {
+  languages: GetKeys<'languages'>[],
+  versions: Versions
+}
 
 @Component({
   selector: 'festival-marketplace-organization-title',
@@ -17,6 +25,51 @@ import { orderBy, where } from 'firebase/firestore';
 })
 export class TitleComponent implements OnInit {
   public titles$: Observable<Movie[]>;
+  private searchForm = new FormGroup({
+    festivals: new FormStaticValueArray<'festival'>([], 'festival'),
+    genres: FormList.factory<GetKeys<'genres'>>([]),
+    languages: new FormEntity<EntityControl<LanguageVersion>, LanguageVersion>({
+      languages: FormList.factory<GetKeys<'languages'>>([]),
+      versions: new FormEntity<EntityControl<Versions>, Versions>({
+        original: new FormControl(false),
+        dubbed: new FormControl(false),
+        subtitle: new FormControl(false),
+        caption: new FormControl(false)
+      })
+    }),
+    minBudget: new FormControl(0),
+    minReleaseYear: new FormControl(0),
+    originCountries: FormList.factory<Territory>([]),
+    productionStatus: new FormStaticValueArray<'productionStatus'>([], 'productionStatus'),
+    certifications: new FormStaticValueArray<'certifications'>([], 'certifications')
+  });
+  private searchForm$ = this.searchForm.valueChanges;
+  private searchInitialValues = {
+    festivals: [],
+    genres: [],
+    languages: {
+      languages: [],
+      versions: {
+        original: false,
+        dubbed: false,
+        subtitle: false,
+        caption: false
+      }
+    },
+    minBudget: 0,
+    minReleaseYear: 0,
+    originCountries: [],
+    productionStatus: [],
+    certifications: []
+  }
+  public festivals = this.searchForm.controls.festivals;
+  public genres = this.searchForm.controls.genres;
+  public languages = this.searchForm.controls.languages;
+  public minBudget = this.searchForm.controls.minBudget;
+  public minReleaseYear = this.searchForm.controls.minReleaseYear;
+  public originCountries = this.searchForm.controls.originCountries;
+  public productionStatus = this.searchForm.controls.productionStatus;
+  public certifications = this.searchForm.controls.certifications;
 
   trackById = (i: number, doc: { id: string }) => doc.id;
 
@@ -28,7 +81,7 @@ export class TitleComponent implements OnInit {
 
   ngOnInit() {
     this.dynTitle.setPageTitle('Sales Agent', 'Line-up');
-    this.titles$ = this.parent.org$.pipe(
+    const titles$ = this.parent.org$.pipe(
       switchMap((org) => {
         return this.service.valueChanges([
           where('orgIds', 'array-contains', org.id),
@@ -39,5 +92,51 @@ export class TitleComponent implements OnInit {
       }),
       distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b))
     );
+
+    const searchForm$ = this.searchForm$.pipe(
+      startWith(this.searchInitialValues),
+      distinctUntilChanged(),
+      debounceTime(500)
+    );
+
+    this.titles$ = combineLatest([titles$, searchForm$]).pipe(
+      map(([movies, search]: [Movie[], MovieSearch]) => ({ movies, search })),
+      map(({ movies, search }) => movies.filter(movie => {
+        const haveFestivals = search.festivals.every(festival => movie.prizes.map(p => p.name).includes(festival));
+        const haveGenres = search.genres.every(genre => movie.genres.includes(genre));
+        const haveLanguages = search.languages.languages.every(lang => {
+          let bool = false;
+          for (const version in search.languages.versions) {
+            if (search.languages.versions[version]) {
+              if (
+                version !== "original" &&
+                search.languages.versions[version] === movie.languages[lang][version]
+              ) bool = true;
+              else if (
+                version === "original" &&
+                search.languages.languages.every(lang => movie.originalLanguages.includes(lang))
+              ) bool = true;
+            }
+          }
+          return bool;
+        });
+        const haveMinBudget = search.minBudget <= (movie.estimatedBudget || 0);
+        const haveMinReleaseYear = search.minReleaseYear <= (movie.release.year || 0);
+        const haveOriginCountries = search.originCountries.every(country => movie.originCountries.includes(country));
+        const haveProductionStatus = () => {
+          if (search.productionStatus.length) {
+            return search.productionStatus.some(status => movie.productionStatus === status);
+          }
+          return true;
+        }
+        const haveCertifications = search.certifications.every(certification => movie.certifications.includes(certification));
+
+        return haveFestivals && haveGenres && haveLanguages && haveMinBudget && haveMinReleaseYear && haveOriginCountries && haveProductionStatus() && haveCertifications;
+      }))
+    );
+  }
+
+  clear() {
+    this.searchForm.reset(this.searchInitialValues);
   }
 }
--- apps/festival/festival/src/app/marketplace/organization/title/title.module.ts
+++ apps/festival/festival/src/app/marketplace/organization/title/title.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { TitleComponent } from './title.component';
-import { MovieCardModule } from '@blockframes/movie/components/card/card.module';
 import { MatRippleModule } from '@angular/material/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
+import { TitleComponent } from './title.component';
+
+import { MovieCardModule } from '@blockframes/movie/components/card/card.module';
 import { RequestScreeningModule } from '@blockframes/event/components/request-screening/request-screening.module';
+import { ListFilterModule } from '@blockframes/ui/list/filter/list-filter.module';
+import { ChipsAutocompleteModule } from '@blockframes/ui/static-autocomplete/chips/chips-autocomplete.module';
+import { LanguageFilterModule } from '@blockframes/movie/form/filters/languages/language-filter.module';
+import { ReleaseYearFilterModule } from '@blockframes/movie/form/filters/release-year/release-year.module';
+import { StaticCheckBoxesModule } from '@blockframes/ui/static-autocomplete/check-boxes/check-boxes.module';
+import { BudgetFilterModule } from '@blockframes/movie/form/filters/budget/budget.module';
 
 @NgModule({
   declarations: [TitleComponent],
@@ -15,6 +25,17 @@ import { RequestScreeningModule } from '@blockframes/event/components/request-sc
     FlexLayoutModule,
     MovieCardModule,
     MatRippleModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    
+    ListFilterModule,
+    ChipsAutocompleteModule,
+    LanguageFilterModule,
+    ReleaseYearFilterModule,
+    StaticCheckBoxesModule,
+    BudgetFilterModule,
+
     RouterModule.forChild([{ path: '', component: TitleComponent }]),
     RequestScreeningModule
   ]
