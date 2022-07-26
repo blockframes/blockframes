214a6dd46eccf88c98f5420a171af48d6453cf9c
KevinDurand974 <k.durand.974@gmail.com>
2022-07-26 14:17:06 +0200

    Add Financiers part

--- apps/financiers/financiers/src/app/marketplace/organization/title/title.module.ts
+++ apps/financiers/financiers/src/app/marketplace/organization/title/title.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { TitleComponent } from './title.component';
-import { MovieCardModule } from '@blockframes/movie/components/card/card.module';
-import { CampaignPipeModule } from '@blockframes/campaign/pipes';
 
+import { TitleComponent } from './title.component';
+
+import { CampaignPipeModule } from '@blockframes/campaign/pipes';
+import { MovieCardModule } from '@blockframes/movie/components/card/card.module';
+import { RequestScreeningModule } from '@blockframes/event/components/request-screening/request-screening.module';
+import { ListFilterModule } from '@blockframes/ui/list/filter/list-filter.module';
+import { ChipsAutocompleteModule } from '@blockframes/ui/static-autocomplete/chips/chips-autocomplete.module';
+import { LanguageFilterModule } from '@blockframes/movie/form/filters/languages/language-filter.module';
+import { ReleaseYearFilterModule } from '@blockframes/movie/form/filters/release-year/release-year.module';
+import { StaticCheckBoxesModule } from '@blockframes/ui/static-autocomplete/check-boxes/check-boxes.module';
+import { BudgetFilterModule } from '@blockframes/movie/form/filters/budget/budget.module';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatRippleModule } from '@angular/material/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -18,8 +30,19 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     FlexLayoutModule,
     MovieCardModule,
     CampaignPipeModule,
+    RequestScreeningModule,
+    ChipsAutocompleteModule,
+    LanguageFilterModule,
+    ListFilterModule,
+    ReleaseYearFilterModule,
+    StaticCheckBoxesModule,
+    BudgetFilterModule,
+
     MatRippleModule,
     MatTooltipModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatButtonModule,
     RouterModule.forChild([{ path: '', component: TitleComponent }])
   ]
 })
