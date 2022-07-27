857ee294d2e1fa6b307444e0466ba0c9c93553a8
kevin-m <kmarais@cascadae8.com>
2022-07-27 10:01:23 +0200

    format

--- libs/devops/src/lib/firestoreMigrations/0084.ts
+++ libs/devops/src/lib/firestoreMigrations/0084.ts
@@ -10,22 +10,21 @@ import { DocumentSnapshot } from 'firebase-functions/v1/firestore';
  */
 export async function upgrade(db: Firestore) {
   return await deleteMoviesSubcollections(db);
-
 }
 
 async function deleteMoviesSubcollections(db: Firestore) {
   const movies = await db.collection('movies').get();
 
   return runChunks(movies.docs, async (doc) => {
-   const movie = await doc.data() as DocumentSnapshot
-   const subCollection = await doc.ref.listCollections() 
-   if (subCollection !== []) {
-    const batch = db.batch();
-    await removeAllSubcollections(doc, batch, db, { verbose: false });
-    await batch.commit();
-    await doc.ref.set(movie);
-   }   
+    const movie = await doc.data() as DocumentSnapshot;
+    const subCollection = await doc.ref.listCollections();
+
+    if (subCollection !== []) {
+      const batch = db.batch();
+      await removeAllSubcollections(doc, batch, db, { verbose: false });
+      await batch.commit();
+      await doc.ref.set(movie);
+    }
 
   }).catch(err => console.error(err));
 }
-    
