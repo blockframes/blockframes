c9cadaf11bb3d69ca9f32e0631951417b192a906
kevin-m <kmarais@cascadae8.com>
2022-07-28 09:32:07 +0200

    fix review

--- libs/devops/src/lib/firestoreMigrations/0084.ts
+++ libs/devops/src/lib/firestoreMigrations/0084.ts
@@ -7,22 +7,17 @@ import { DocumentSnapshot } from 'firebase-functions/v1/firestore';
  * @returns
  */
 export async function upgrade(db: Firestore) {
-  return await deleteMoviesSubcollection(db);
-}
-
-async function deleteMoviesSubcollection(db: Firestore) {
   const movies = await db.collection('movies').get();
 
   return runChunks(movies.docs, async (doc) => {
-    const movie = await doc.data() as DocumentSnapshot;
     const subCollection = await doc.ref.listCollections();
 
     if (subCollection !== []) {
       const batch = db.batch();
       await removeAllSubcollections(doc, batch, db, { verbose: false });
       await batch.commit();
-      await doc.ref.set(movie);
     }
 
   }).catch(err => console.error(err));
 }
+
