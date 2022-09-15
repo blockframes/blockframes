8392e17bf7f8cc996592dc856ae57ea20c1dede0
bruce <delorme.bruce.michel@gmail.com>
2022-09-15 18:45:55 +0200

    test

--- apps/festival/festival-e2e/src/integration/create-screening.spec.ts
+++ apps/festival/festival-e2e/src/integration/create-screening.spec.ts
@@ -60,7 +60,7 @@ describe('Screenings', () => {
     firestore.create([injectedData]).then(x => console.log('*** DATA INJECTED'));
     adminAuth.createUser({ uid: dashboardUser.uid, email: dashboardUser.email, emailVerified: true }).then(x => console.log('*** DASHBOARD AUTH'));
     adminAuth.createUser({ uid: marketplaceUser.uid, email: marketplaceUser.email, emailVerified: true }).then(x => console.log('*** MARKETPLACE AUTH'));
-    maintenance.end().then(x => console.log('*** MAINTENANCE FINIE'));;
+    //maintenance.end().then(x => console.log('*** MAINTENANCE FINIE'));; 
     browserAuth.clearBrowserAuth().then(x => console.log('*** AUTH CLEARED'));;
     cy.visit('');
     browserAuth.signinWithEmailAndPassword(dashboardUser.email).then(x => '*** SIGNIN');
