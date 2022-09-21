bdb4cecc97fc9f71e003e55b8ff664701967f7bd
n-germain-work <n.germain.work@gmail.com>
2022-09-21 09:10:15 +0200

    skipped integrations and related support files deleted

--- apps/catalog/catalog-e2e/src/support/pages/dashboard/TitlesActivityPage.ts
+++
@@ -1,9 +0,0 @@
-export default class TitlesActivityPage {
-  constructor() {
-    cy.get('catalog-title-view catalog-title-activity');
-  }
-
-  public assertTitleExists(title: string) {
-    cy.get('catalog-title-view header h1').contains(title);
-  }
-}
