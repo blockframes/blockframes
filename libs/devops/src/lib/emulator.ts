ab755e6b2be9fec12e453956188c8e740a3a3556
Bruce <eelios@gmail.com>
2022-10-17 14:43:20 +0200

    debug unit test (#8963)
    * debug unit test
    
    * intentional failing test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * typo
    
    * test with falling test
    
    * test
    
    * cleaning and failing test
    
    * should work
    
    * should fail
    
    * should work & debug
    
    * cleaning
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>

--- libs/devops/src/lib/emulator.ts
+++ libs/devops/src/lib/emulator.ts
@@ -68,7 +68,10 @@ export async function importEmulatorFromBucket({ importFrom }: ImportEmulatorOpt
 }
 
 interface StartEmulatorOptions {
-  importFrom: 'defaultImport' | string;
+  importFrom?: 'defaultImport' | string;
+  emulators?: ('auth' | 'functions' | 'firestore' | 'pubsub' | 'storage')[];
+  importData?: boolean;
+  execCommand?: string
 }
 
 /**
@@ -89,14 +92,20 @@ export async function loadEmulator({ importFrom = 'defaultImport' }: StartEmulat
   }
 }
 
-export async function startEmulators({ importFrom = 'defaultImport' }: StartEmulatorOptions = { importFrom: 'defaultImport' }) {
+export async function startEmulators({
+  importFrom = 'defaultImport',
+  emulators = ['auth', 'functions', 'firestore', 'pubsub'],
+  importData = true,
+  execCommand
+}: StartEmulatorOptions = { importFrom: 'defaultImport', importData: true, emulators: ['auth', 'functions', 'firestore', 'pubsub'] }) {
   const emulatorPath = importFrom === 'defaultImport' ? defaultEmulatorBackupPath : resolve(importFrom);
   let proc: ChildProcess;
   try {
     proc = await firebaseEmulatorExec({
-      emulators: ['auth', 'functions', 'firestore', 'pubsub'],
-      importPath: emulatorPath,
-      exportData: true,
+      execCommand,
+      emulators,
+      importPath: importData ? emulatorPath : undefined,
+      exportData: importData,
     });
     await awaitProcessExit(proc);
   } catch (e) {
@@ -105,20 +114,6 @@ export async function startEmulators({ importFrom = 'defaultImport' }: StartEmul
   }
 }
 
-export async function startEmulatorsForUnitTests({ execCommand }: { execCommand?: string } = {}) {
-  let proc: ChildProcess;
-  try {
-    proc = await firebaseEmulatorExec({
-      execCommand,
-      emulators: ['auth', 'firestore'],
-      exportData: false,
-    });
-    await awaitProcessExit(proc);
-  } catch (e) {
-    await shutdownEmulator(proc);
-    throw e;
-  }
-}
 /**
  * This creates users in Auth emulator from a running instance of the Firestore emulator
  * By keeping this clean and separate, we don't need to launch functions emulator when this is happening,
