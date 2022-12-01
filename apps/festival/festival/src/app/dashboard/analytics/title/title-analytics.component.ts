9ba33eab07a2d93de4b36beea966f0c448c058e3
bruce <delorme.bruce.michel@gmail.com>
2022-12-01 16:33:29 +0100

    converted some observables to promises

--- apps/festival/festival/src/app/dashboard/analytics/title/title-analytics.component.ts
+++ apps/festival/festival/src/app/dashboard/analytics/title/title-analytics.component.ts
@@ -57,14 +57,14 @@ export class TitleAnalyticsComponent {
     'screeningRequested',
   ];
 
-  titleId$ = this.route.params.pipe(
+  private titleId$ = this.route.params.pipe(
     pluck('titleId')
   ) as Observable<string>;
 
-  title$ = this.titleId$.pipe(
+  title$ = firstValueFrom(this.titleId$.pipe(
     switchMap((titleId: string) => this.movieService.valueChanges(titleId)),
     shareReplay({ bufferSize: 1, refCount: true })
-  );
+  ));
 
   titleAnalytics$ = this.titleId$.pipe(
     switchMap((titleId: string) => this.analyticsService.getTitleAnalytics({ titleId })),
@@ -78,19 +78,19 @@ export class TitleAnalyticsComponent {
     shareReplay({ bufferSize: 1, refCount: true })
   );
 
-  orgActivity$ = this.titleAnalytics$.pipe(
+  orgActivity$ = firstValueFrom(this.titleAnalytics$.pipe(
     map(analytics => counter(analytics, 'org.activity')),
     map(counted => countedToAnalyticData(counted, 'orgActivity'))
-  );
+  ));
 
-  territoryActivity$ = this.titleAnalytics$.pipe(
+  territoryActivity$ = firstValueFrom(this.titleAnalytics$.pipe(
     map(analytics => counter(analytics, 'org.addresses.main.country')),
     map(counted => countedToAnalyticData(counted, 'territories'))
-  );
+  ));
 
-  buyerAnalytics$ = this.titleAnalytics$.pipe(
+  buyerAnalytics$ = firstValueFrom(this.titleAnalytics$.pipe(
     map(aggregatePerUser)
-  );
+  ));
 
   filters = {
     name: nameFilter,
@@ -122,27 +122,27 @@ export class TitleAnalyticsComponent {
     shareReplay({ bufferSize: 1, refCount: true })
   );
 
-  ongoingScreenings$ = this.invitations$.pipe(
+  ongoingScreenings$ = firstValueFrom(this.invitations$.pipe(
     map(invitations => invitations.filter(({ event }) => eventTime(event) === 'onTime')),
     filter(invitations => !!invitations.length),
-  );
+  ));
 
-  aggregatedScreeningCards$ = this.invitations$.pipe(
+  aggregatedScreeningCards$ = firstValueFrom(this.invitations$.pipe(
     map(invitations => toScreenerCards(invitations))
-  );
+  ));
 
-  titleInteractions$ = this.titleAnalytics$.pipe(
+  titleInteractions$ = firstValueFrom(this.titleAnalytics$.pipe(
     map(analytics => analytics.filter(analytic => analytic.name !== 'pageView'))
-  );
+  ));
 
-  endedOrOngoingScreenings$ = this.titleId$.pipe(
+  endedOrOngoingScreenings$ = firstValueFrom(this.titleId$.pipe(
     switchMap((titleId: string) => this.eventService.valueChanges([
       where('meta.titleId', '==', titleId),
       where('ownerOrgId', '==', this.orgService.org.id),
       where('type', '==', 'screening')
     ])),
     map(events => events.filter(e => eventTime(e) !== 'early'))
-  );
+  ));
 
   constructor(
     private movieService: MovieService,
