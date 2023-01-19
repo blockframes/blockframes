ee14c4d111eef75d4fa7366583973f2f4fba5db9
bruce <delorme.bruce.michel@gmail.com>
2023-01-19 13:46:17 +0100

    factorized action column on CRM tables #9117

--- apps/crm/crm/src/app/crm/pages/movies/movies.component.ts
+++ apps/crm/crm/src/app/crm/pages/movies/movies.component.ts
@@ -79,11 +79,8 @@ export class MoviesComponent implements OnInit {
     );
   }
 
-  goToEditNewTab(id: string, $event: Event) {
-    $event.stopPropagation();
-    const urlTree = this.router.createUrlTree([`c/o/dashboard/crm/movie/${id}`]);
-    const url = this.router.serializeUrl(urlTree);
-    window.open(url, '_blank', 'noreferrer');
+  goToEdit(movie: Movie) {
+    this.router.navigate([`/c/o/dashboard/crm/movie/${movie.id}`]);
   }
 
   public exportTable(movies: CrmMovie[]) {
