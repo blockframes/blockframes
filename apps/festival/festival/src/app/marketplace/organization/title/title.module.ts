23995b0f86611122667ddafde7bd107b284cb486
Bruce <eelios@gmail.com>
2023-02-01 14:25:23 +0100

    Release 4 6 fixes (#9181)
    * updated mailto
    
    * no title placeholder
    
    * removed test-id
    
    ---------
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>

--- apps/festival/festival/src/app/marketplace/organization/title/title.module.ts
+++ apps/festival/festival/src/app/marketplace/organization/title/title.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { TitleComponent } from './title.component';
-import { MovieCardModule } from '@blockframes/movie/components/card/card.module';
-import { MatRippleModule } from '@angular/material/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+// Component
+import { TitleComponent } from './title.component';
+
+// Material
+import { MatRippleModule } from '@angular/material/core';
+
+// Modules
+import { MovieCardModule } from '@blockframes/movie/components/card/card.module';
 import { RequestScreeningModule } from '@blockframes/event/components/request-screening/request-screening.module';
+import { LogoSpinnerModule } from '@blockframes/ui/logo-spinner/logo-spinner.module';
+import { ImageModule } from '@blockframes/media/image/directives/image.module';
 
 @NgModule({
   declarations: [TitleComponent],
@@ -16,7 +23,9 @@ import { RequestScreeningModule } from '@blockframes/event/components/request-sc
     MovieCardModule,
     MatRippleModule,
     RouterModule.forChild([{ path: '', component: TitleComponent }]),
-    RequestScreeningModule
+    RequestScreeningModule,
+    LogoSpinnerModule,
+    ImageModule,
   ]
 })
 export class OrganizationTitleModule { }
