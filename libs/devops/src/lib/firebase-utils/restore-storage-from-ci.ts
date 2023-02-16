08937b3ad0201c3825f59415b6b99016f241c160
Bruce <eelios@gmail.com>
2023-02-16 13:43:22 +0100

    updated storage & anon process #8967 (#9202)
    * updated storage & anon process
    
    * added keepalive on shrinked db
    
    * debug
    
    * prevent upcoming merge conflicts
    
    * increased timeout for shrinked db
    
    ---------
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>

--- libs/devops/src/lib/firebase-utils/restore-storage-from-ci.ts
+++ libs/devops/src/lib/firebase-utils/restore-storage-from-ci.ts
@@ -1,51 +1,28 @@
 import { firebase } from 'env/env';
 import { firebase as firebaseProd } from 'env/env.blockframes';
-import * as admin from 'firebase-admin';
-import { getLatestDirName } from './anonymize';
+import { firebase as firebaseCI } from 'env/env.blockframes-ci';
 import { gsutilTransfer } from './commands';
+import { CI_ANONYMIZED_DATA, latestAnonStorageDir } from './utils';
 
-export const CI_STORAGE_BACKUP = 'blockframes-ci-storage-backup';
-export const latestAnonStorageDir = 'LATEST-ANON-STORAGE';
-
-/**
- * This method is
- * @deprecated
- */
-export async function restoreStorageFromCi(ciApp: admin.app.App) {
-  if (
-    firebase().storageBucket === 'blockframes.appspot.com' ||
-    firebase().storageBucket === firebaseProd().storageBucket
-  )
-    throw Error('ABORT: YOU ARE TRYING TO RUN SCRIPT AGAINST PROD - THIS WILL DELETE STORAGE!!');
-
-  const ciStorage = ciApp.storage();
-  const folderName = await getLatestDirName(ciStorage.bucket(CI_STORAGE_BACKUP))
-  console.log('Latest backup:', folderName);
-
-  console.log('Mirroring backup prod-storage bucket from blockframe-ci to your local project\'s main storage bucket...');
-  // Extension list defined in libs/model/src/lib/utils.ts allowedFiles.video.extension
-  const exclude = 'protected.*.mp4$|protected.*.mov$|protected.*.mkv$|protected.*.3gp$|protected.*.wmv$|protected.*.avi$|protected.*.m4v$|public.*.mp4$|public.*.mov$|public.*.mkv$|public.*.3gp$|public.*.wmv$|public.*.avi$|public.*.m4v$';
-  await gsutilTransfer({
-    exclude,
-    quiet: true,
-    mirror: true,
-    from: `gs://${CI_STORAGE_BACKUP}/${folderName}`,
-    to: `gs://${firebase().storageBucket}`,
-  });
+const isProdBucket = (bucketName: string) => {
+  return ['blockframes.appspot.com', firebaseProd().storageBucket].includes(bucketName);
 }
 
 export async function restoreAnonStorageFromCI() {
-  if (
-    firebase().storageBucket === 'blockframes.appspot.com' ||
-    firebase().storageBucket === firebaseProd().storageBucket
-  )
+  if (isProdBucket(firebase().storageBucket)) {
     throw Error('ABORT: YOU ARE TRYING TO RUN SCRIPT AGAINST PROD - THIS WILL DELETE STORAGE!!');
+  }
 
-  console.log("Copying prepared storage bucket from blockframe-ci to your local project's storage bucket...");
+  if (firebase().projectId === firebaseCI().projectId) {
+    console.log(`Skipping copy from ${CI_ANONYMIZED_DATA} to gs://${firebase().storageBucket} as ${firebaseCI().projectId} does not need storage files.`);
+    return;
+  }
+
+  console.log(`Copying prepared storage bucket from ${CI_ANONYMIZED_DATA} to your local project's storage bucket...`);
   await gsutilTransfer({
     quiet: true,
     mirror: true,
-    from: `gs://${CI_STORAGE_BACKUP}/${latestAnonStorageDir}`,
+    from: `gs://${CI_ANONYMIZED_DATA}/${latestAnonStorageDir}`,
     to: `gs://${firebase().storageBucket}`,
   });
 }
