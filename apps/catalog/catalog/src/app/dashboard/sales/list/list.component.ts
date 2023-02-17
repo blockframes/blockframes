b034ec87388be54977bb6b4927f61aa17621c2a9
Bruce <eelios@gmail.com>
2023-02-17 10:32:36 +0100

    Fixes of the day r 4 6 1 (#9210)
    * updated app version
    
    * sentry fix 069a05cc02244 (#9197)
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>
    
    * 45m for e2e catalog
    
    * fix sentry error (#9204)
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>
    
    * fixed tooltip (#9206)
    
    * fixed tooltip
    
    * small debugs meeting
    
    ---------
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>
    
    * fixed permission error (#9209)
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>
    
    ---------
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>

--- apps/catalog/catalog/src/app/dashboard/sales/list/list.component.ts
+++ apps/catalog/catalog/src/app/dashboard/sales/list/list.component.ts
@@ -2,14 +2,16 @@ import { Component, ChangeDetectionStrategy, Optional, OnInit } from '@angular/c
 import { ContractService } from '@blockframes/contract/contract/service';
 import { Intercom } from 'ng-intercom';
 import { joinWith } from 'ngfire';
-import { combineLatest, of, map } from 'rxjs';
+import { combineLatest, of, map, catchError } from 'rxjs';
 import { MovieService } from '@blockframes/movie/service';
 import { IncomeService } from '@blockframes/contract/income/service';
 import { DynamicTitleService } from '@blockframes/utils/dynamic-title/dynamic-title.service';
 import { getSeller } from '@blockframes/contract/contract/utils'
-import { Sale } from '@blockframes/model';
+import { deletedIdentifier, Sale } from '@blockframes/model';
 import { OrganizationService } from '@blockframes/organization/service';
 import { orderBy, where } from 'firebase/firestore';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { supportEmails } from '@env';
 
 function queryConstraints(orgId: string, options: { internal?: boolean }) {
   if (options.internal) {
@@ -30,6 +32,8 @@ function queryConstraints(orgId: string, options: { internal?: boolean }) {
   ]
 }
 
+const invalidTitleErrorMessage = `Some Offers are linked to invalid titles. Please contact ${supportEmails.catalog}.`;
+
 @Component({
   selector: 'catalog-sale-list',
   templateUrl: './list.component.html',
@@ -43,7 +47,13 @@ export class SaleListComponent implements OnInit {
     joinWith({
       licensor: (sale: Sale) => this.orgService.valueChanges(getSeller(sale)).pipe(map(org => org.name)),
       licensee: (sale: Sale) => this.orgService.valueChanges(sale.buyerId).pipe(map(org => org.name)),
-      title: (sale: Sale) => this.titleService.valueChanges(sale.titleId).pipe(map(title => title.title.international)),
+      title: (sale: Sale) => this.titleService.valueChanges(sale.titleId).pipe(
+        map(title => title.title.international),
+        catchError(_ => {
+          this.snackbar.open(invalidTitleErrorMessage, 'close', { duration: 5000 });
+          return of(deletedIdentifier.title);
+        })
+      ),
       negotiation: (sale: Sale) => this.contractService.lastNegotiation(sale.id)
     }),
   );
@@ -52,7 +62,13 @@ export class SaleListComponent implements OnInit {
     joinWith({
       licensor: (sale: Sale) => this.orgService.valueChanges(getSeller(sale)).pipe(map(org => org.name)),
       licensee: () => of('External'),
-      title: (sale: Sale) => this.titleService.valueChanges(sale.titleId).pipe(map(title => title.title.international)),
+      title: (sale: Sale) => this.titleService.valueChanges(sale.titleId).pipe(
+        map(title => title.title.international),
+        catchError(_ => {
+          this.snackbar.open(invalidTitleErrorMessage, 'close', { duration: 5000 });
+          return of(deletedIdentifier.title);
+        })
+      ),
       price: (sale: Sale) => this.incomeService.valueChanges(sale.id),
     }),
   );
@@ -65,6 +81,7 @@ export class SaleListComponent implements OnInit {
     private titleService: MovieService,
     private incomeService: IncomeService,
     private dynTitle: DynamicTitleService,
+    private snackbar: MatSnackBar,
     @Optional() private intercom: Intercom,
   ) { }
 
