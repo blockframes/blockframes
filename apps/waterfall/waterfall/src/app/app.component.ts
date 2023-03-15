580bb07e46b4f4118a9af8dd11a3d05ccd159628
bruce <delorme.bruce.michel@gmail.com>
2023-03-15 17:47:16 +0100

    debug

--- apps/waterfall/waterfall/src/app/app.component.ts
+++ apps/waterfall/waterfall/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'blockframes-root',
+  selector: 'waterfall-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
