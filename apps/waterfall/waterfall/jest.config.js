a99b68a4a94377fcad6d6b9f1150aa9f082c51cb
bruce <delorme.bruce.michel@gmail.com>
2023-03-15 17:34:58 +0100

    app setup

---
+++ apps/waterfall/waterfall/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'waterfall-waterfall',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: '../../coverage/apps/waterfall/waterfall',
+};
