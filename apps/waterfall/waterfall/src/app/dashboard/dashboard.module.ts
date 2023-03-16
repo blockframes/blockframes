ea08c84377d0b1f10706bdb5283d84ae266b392a
bruce <delorme.bruce.michel@gmail.com>
2023-03-16 15:47:22 +0100

    cleaned modules

--- apps/waterfall/waterfall/src/app/dashboard/dashboard.module.ts
+++ apps/waterfall/waterfall/src/app/dashboard/dashboard.module.ts
@@ -7,23 +7,12 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 // Blockframes
 import { DashboardLayoutModule } from '@blockframes/ui/layout/dashboard/dashboard.module';
-import { ImageModule } from '@blockframes/media/image/directives/image.module';
-import { ToLabelModule } from '@blockframes/utils/pipes';
-import { MovieFormShellModule } from '@blockframes/movie/form/shell/shell.module';
-import { MovieShellConfig } from '@blockframes/movie/form/movie.shell.config';
-import { FORMS_CONFIG } from '@blockframes/movie/form/movie.shell.interfaces';
-import { OrgAccessModule } from '@blockframes/organization/pipes/org-access.pipe';
-import { EventFromShellModule } from '@blockframes/event/form/shell/shell.module';
-import { LayoutEventReviewModule } from '@blockframes/event/layout/review/review.module';
 import { SidenavAuthModule } from '@blockframes/auth/components/sidenav-auth/sidenav-auth.module';
 import { SidenavWidgetModule } from '@blockframes/auth/components/sidenav-widget/sidenav-widget.module';
-
 
 // Material
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatDividerModule } from '@angular/material/divider';
 
 const routes: Routes = [{
   path: '',
@@ -73,26 +62,13 @@ const routes: Routes = [{
     CommonModule,
     FlexLayoutModule,
     DashboardLayoutModule,
-    ImageModule,
-    ToLabelModule,
-    OrgAccessModule,
-    MovieFormShellModule,
-    EventFromShellModule,
-    LayoutEventReviewModule,
     SidenavAuthModule,
     SidenavWidgetModule,
 
     // Material
-    MatDividerModule,
     MatListModule,
     MatIconModule,
-    MatToolbarModule,
     RouterModule.forChild(routes)
-  ],
-  providers: [{
-    provide: FORMS_CONFIG,
-    useFactory: (movie) => ({ movie }),
-    deps: [MovieShellConfig]
-  }]
+  ]
 })
 export class DashboardModule { }
--- apps/waterfall/waterfall/src/app/landing/landing.module.ts
+++ apps/waterfall/waterfall/src/app/landing/landing.module.ts
@@ -1,37 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { LandingComponent } from './landing.component';
 import { LandingShellModule } from '@blockframes/landing/shell/shell.module';
-import { ImageModule } from '@blockframes/media/image/directives/image.module';
-import { AppLogoModule } from '@blockframes/ui/layout/app-logo/app-logo.module';
-import { AppBridgeBannerModule } from '@blockframes/landing/appBridgeBanner/app-bridge-banner.module';
-
-// Material
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [LandingComponent],
   imports: [
     CommonModule,
-    FlexLayoutModule,
     LandingShellModule,
-    ReactiveFormsModule,
-    ImageModule,
-    AppLogoModule,
-    AppBridgeBannerModule,
-
-    // Material
-    MatButtonModule,
-    MatIconModule,
-    MatCardModule,
-    MatTabsModule,
     RouterModule.forChild([{ path: '', component: LandingComponent }])
   ]
 })
