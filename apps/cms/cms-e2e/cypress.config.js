35b1f876bb0872bc86695227ebf09af4bfc453dc
bruce <delorme.bruce.michel@gmail.com>
2023-03-23 14:02:32 +0100

    cypress cms migrate

---
+++ apps/cms/cms-e2e/cypress.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  fileServerFolder: '.',
+  fixturesFolder: './src/fixtures',
+  modifyObstructiveCode: false,
+  video: true,
+  videosFolder: '../../../dist/cypress/apps/cms/cms-e2e/videos',
+  screenshotsFolder: '../../../dist/cypress/apps/cms/cms-e2e/screenshots',
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 10000,
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: '../../../dist/test-reports/festival-[hash].xml',
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./src/plugins/index.ts').default(on, config)
+    },
+    specPattern: './src/integration/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: './src/support/index.ts',
+  },
+})
