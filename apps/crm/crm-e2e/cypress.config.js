ba535fb974edef964b6445490647770c412a7d4d
bruce <delorme.bruce.michel@gmail.com>
2023-03-23 18:33:13 +0100

    migrate crm cypress

---
+++ apps/crm/crm-e2e/cypress.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  fileServerFolder: '.',
+  fixturesFolder: './src/fixtures',
+  modifyObstructiveCode: false,
+  video: true,
+  videosFolder: '../../../dist/cypress/apps/crm/crm-e2e/videos',
+  screenshotsFolder: '../../../dist/cypress/apps/crm/crm-e2e/screenshots',
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 10000,
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: '../../../dist/test-reports/crm-[hash].xml',
+  },
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./src/plugins/index.ts').default(on, config)
+    },
+    specPattern: './src/integration/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: './src/support/index.ts',
+  },
+})
