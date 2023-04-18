2f4b4a09de313babeb4a03e6f03bdc29d46432b8
Nicolas <n.germain.work@gmail.com>
2023-04-18 13:56:14 +0200

    fix PR comment

--- libs/testing/cypress/browser/src/lib/helpers.ts
+++ libs/testing/cypress/browser/src/lib/helpers.ts
@@ -54,8 +54,8 @@ export function dateToMMDDYYYY(date: Date) {
  * Transform the first letter of each word to uppercase
  * @example: a magic letter => A Magic Letter
  **/
-export const titleCase = (txt: string) =>
-  txt
+export const titleCase = (str: string) =>
+  str
     .split(' ')
     .map(word => word[0].toUpperCase() + word.substring(1).toLowerCase())
     .join(' ');
