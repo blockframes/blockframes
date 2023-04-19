4e41ba3aa2783d46bb86afe5a81c9d9a0d802b53
Nicolas <n.germain.work@gmail.com>
2023-04-19 19:34:29 +0200

    mvp - continents in url for avails filter

--- libs/utils/src/lib/form/form-state-url-encoder.ts
+++ libs/utils/src/lib/form/form-state-url-encoder.ts
@@ -1,10 +1,17 @@
 import { ActivatedRoute, Router } from '@angular/router';
+import { Territory, TerritoryGroup, territories, territoriesGroup, territoriesISOA2 } from '@blockframes/model';
 
 /**
  * Decode the app url and save it as form state
  */
 export function decodeUrl<T = any>(route: ActivatedRoute): T {
-  const { formValue } = route.snapshot.queryParams;
+  let { formValue } = route.snapshot.queryParams;
+
+  if (formValue.includes('"avails":')) {
+    const data = JSON.parse(formValue);
+    formValue = replaceContinents(data);
+  }
+
   try {
     const fromUrl = decodeURIComponent(formValue ?? '{}');
     return JSON.parse(fromUrl);
@@ -17,9 +24,21 @@ export function decodeUrl<T = any>(route: ActivatedRoute): T {
  * Encode the form state and saves in the url
  * for possible sharing of form state as app url
  */
-export function encodeUrl<T>(
-  router: Router, route: ActivatedRoute, data: T
-) {
+export function encodeUrl<T>(router: Router, route: ActivatedRoute, data: T) {
+  let availTerritories: Territory[] = data['avails']['territories'];
+  let availContinents: string[] = [];
+
+  if (availTerritories.length) {
+    for (const continent of territoriesGroup) {
+      if (continent.items.every(country => availTerritories.includes(country))) {
+        availTerritories = availTerritories.filter(country => !continent.items.includes(country));
+        availContinents.push(continent.label.toLowerCase());
+        if (availContinents.length === territoriesGroup.length) availContinents = ['world'];
+      }
+    }
+    data['avails']['territories'] = [...availTerritories, ...availContinents];
+  }
+
   const formValue = JSON.stringify(data);
   //url musn't be longer than 2000 characters
   if (formValue.length < 1500) {
@@ -29,4 +48,27 @@ export function encodeUrl<T>(
       replaceUrl: true,
     });
   }
+}
+
+function replaceContinents(data: any) {
+  let availTerritories: string[] = data.avails.territories;
+  if (!availTerritories.length) return;
+
+  if (availTerritories[0] === 'world') {
+    availTerritories = Object.keys(territories).filter(e => e !== 'world');
+  } else {
+    for (const continent of territoriesGroup) {
+      if (availTerritories.includes(continent.label.toLowerCase())) {
+        const replacementTerritories = territoriesGroup
+          .map(group => group.label === continent.label && group.items)
+          .filter(Boolean)
+          .flat(2);
+        availTerritories = availTerritories.filter(e => e !== continent.label.toLowerCase());
+        availTerritories.push(...replacementTerritories);
+      }
+    }
+  }
+
+  data.avails.territories = availTerritories;
+  return JSON.stringify(data);
 }
