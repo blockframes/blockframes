34ada1c3df72815ae004ef1a769f79e5cdcc8435
Bruce <eelios@gmail.com>
2023-05-16 14:54:17 +0200

    terrawilly-demo (#9341)
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>

--- apps/waterfall/waterfall/src/app/dashboard/dashboard.module.ts
+++ apps/waterfall/waterfall/src/app/dashboard/dashboard.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [{
       children: [
         {
           path: '',
-          redirectTo: 'casablancas',
+          redirectTo: 'terrawilly-demo',
           pathMatch: 'full'
         },
         {
