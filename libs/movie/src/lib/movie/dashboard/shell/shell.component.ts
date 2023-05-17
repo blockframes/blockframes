06dbfa06ca964448ed2e94baf6deab2970a35b43
pldespaigne <pl.despaigne@gmail.com>
2023-05-17 16:21:19 +0200

    add edit link button

--- libs/movie/src/lib/movie/dashboard/shell/shell.component.ts
+++ libs/movie/src/lib/movie/dashboard/shell/shell.component.ts
@@ -39,6 +39,7 @@ export class DashboardTitleShellComponent implements OnInit, OnDestroy {
   public movie = firstValueFrom(this.movie$);
 
   @Input() routes: RouteDescription[];
+  @Input() editRoute?: string | any[]; // same type as expected by [routerLink]
 
   constructor(
     @Inject(FORMS_CONFIG) private configs: ShellConfig,
