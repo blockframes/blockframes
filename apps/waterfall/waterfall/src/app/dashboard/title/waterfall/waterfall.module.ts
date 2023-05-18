7cda9c58f014af7684c63ed82e40d77f1ec64671
pldespaigne <pl.despaigne@gmail.com>
2023-05-18 13:30:08 +0200

    requested changes

--- apps/waterfall/waterfall/src/app/dashboard/title/waterfall/waterfall.module.ts
+++ apps/waterfall/waterfall/src/app/dashboard/title/waterfall/waterfall.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
+// Modules
+import { GraphModule } from '../../../waterfall/components/g6/graph/graph.module';
 
 // Pages
 import { WaterfallComponent } from './waterfall.component';
-import { GraphModule } from '../../../waterfall/components/g6/graph/graph.module';
 
 @NgModule({
   declarations: [WaterfallComponent],
