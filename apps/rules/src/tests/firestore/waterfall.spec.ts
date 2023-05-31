f3b4a4dc1d90034276050e801ccd3cde49528a1a
Bruce <eelios@gmail.com>
2023-05-31 09:51:10 +0200

    Feat 9314 waterfall in db (#9370)
    * removed waterfall demo component
    
    * rules update
    
    * reduced movieOwner capabilities
    
    * initializes & reads waterfall from db
    
    ---------
    
    Co-authored-by: bruce <delorme.bruce.michel@gmail.com>

--- apps/rules/src/tests/firestore/waterfall.spec.ts
+++ apps/rules/src/tests/firestore/waterfall.spec.ts
@@ -33,9 +33,9 @@ describe('Movie Owner', () => {
       await assertSucceeds(ref.get());
     });
 
-    test('Should be able to update waterfall document', async () => {
+    test('Should not be able to update waterfall document', async () => {
       const ref = db.doc('waterfall/M001');
-      await assertSucceeds(ref.set({ id: 'M001', orgIds: ['O001', 'O003'] }));
+      await assertFails(ref.set({ id: 'M001', orgIds: ['O001', 'O003'] }));
     });
 
     test('Should not be able to change waterfall document id', async () => {
@@ -45,14 +45,14 @@ describe('Movie Owner', () => {
   });
 
   describe('Blocks', () => {
-    test('Should be able to create block document', async () => {
+    test('Should not be able to create block document', async () => {
       const ref = db.doc('waterfall/M001/blocks/B001');
-      await assertSucceeds(ref.set({ id: 'B001', actions: {} }));
+      await assertFails(ref.set({ id: 'B001', actions: {} }));
     });
 
-    test('Should be able to update block document', async () => {
+    test('Should not be able to update block document', async () => {
       const ref = db.doc('waterfall/M001/blocks/B001');
-      await assertSucceeds(ref.set({ id: 'B001', actions: { 1: { name: 'init' } } }));
+      await assertFails(ref.set({ id: 'B001', actions: { 1: { name: 'init' } } }));
     });
 
     test('Should not be able to change block document id', async () => {
@@ -60,26 +60,31 @@ describe('Movie Owner', () => {
       await assertFails(ref.set({ id: 'B002' }));
     });
 
-    test('Should be able to read block document', async () => {
+    test('Should not be able to read block document', async () => {
       const ref = db.doc('waterfall/M001/blocks/B001');
-      await assertSucceeds(ref.get());
+      await assertFails(ref.get());
     });
 
-    test('Should be able to list block documents', async () => {
+    test('Should not be able to list block documents', async () => {
       const ref = db.collection('waterfall/M001/blocks');
-      await assertSucceeds(ref.get());
+      await assertFails(ref.get());
     });
   });
 
   describe('Permissions', () => {
-    test('Should be able to create permissions document', async () => {
-      const ref = db.doc('waterfall/M001/permissions/O003');
-      await assertSucceeds(ref.set({ id: 'O003', role: ['financier'] }));
+    test('Should be able to create his own permissions document', async () => {
+      const ref = db.doc('waterfall/M001/permissions/O001');
+      await assertSucceeds(ref.set({ id: 'O001', roles: ['author'] }));
     });
 
-    test('Should be able to update permissions document', async () => {
+    test('Should not be able to create others permissions document', async () => {
       const ref = db.doc('waterfall/M001/permissions/O003');
-      await assertSucceeds(ref.set({ id: 'O003', role: ['financier'], scope: ['O001'] }));
+      await assertFails(ref.set({ id: 'O003', roles: ['financier'] }));
+    });
+
+    test('Should not be able to update permissions document', async () => {
+      const ref = db.doc('waterfall/M001/permissions/O003');
+      await assertFails(ref.set({ id: 'O003', roles: ['financier'], scope: ['O001'] }));
     });
 
     test('Should not be able to change permissions document id', async () => {
@@ -87,26 +92,26 @@ describe('Movie Owner', () => {
       await assertFails(ref.set({ id: 'O002' }));
     });
 
-    test('Should be able to read permissions document', async () => {
+    test('Should not be able to read permissions document', async () => {
       const ref = db.doc('waterfall/M001/permissions/O003');
-      await assertSucceeds(ref.get());
+      await assertFails(ref.get());
     });
 
-    test('Should be able to list permissions documents', async () => {
+    test('Should not be able to list permissions documents', async () => {
       const ref = db.collection('waterfall/M001/permissions');
-      await assertSucceeds(ref.get());
+      await assertFails(ref.get());
     });
   });
 
   describe('Budget', () => {
-    test('Should be able to create budget document', async () => {
+    test('Should not be able to create budget document', async () => {
       const ref = db.doc('waterfall/M001/budget/M001');
-      await assertSucceeds(ref.set({ id: 'M001' }));
+      await assertFails(ref.set({ id: 'M001' }));
     });
 
-    test('Should be able to update budget document', async () => {
+    test('Should not be able to update budget document', async () => {
       const ref = db.doc('waterfall/M001/budget/M001');
-      await assertSucceeds(ref.set({ id: 'M001', foo:'bar' }));
+      await assertFails(ref.set({ id: 'M001', foo:'bar' }));
     });
 
     test('Should not be able to change budget document id', async () => {
@@ -114,9 +119,9 @@ describe('Movie Owner', () => {
       await assertFails(ref.set({ id: 'M00X' }));
     });
 
-    test('Should be able to read budget document', async () => {
+    test('Should not be able to read budget document', async () => {
       const ref = db.doc('waterfall/M001/budget/M001');
-      await assertSucceeds(ref.get());
+      await assertFails(ref.get());
     });
 
   });
@@ -182,12 +187,12 @@ describe('User that is not owner of movie', () => {
   describe('Permissions', () => {
     test('Should not be able to create permissions document', async () => {
       const ref = db.doc('waterfall/M001/permissions/O003');
-      await assertFails(ref.set({ id: 'O003', role: ['financier'] }));
+      await assertFails(ref.set({ id: 'O003', roles: ['financier'] }));
     });
 
     test('Should not be able to update permissions document', async () => {
       const ref = db.doc('waterfall/MI-0d7/permissions/O003');
-      await assertFails(ref.set({ id: 'O003', role: ['financier'], scope: ['O001'] }));
+      await assertFails(ref.set({ id: 'O003', roles: ['financier'], scope: ['O001'] }));
     });
 
     test('Should not be able to read permissions document', async () => {
@@ -220,7 +225,7 @@ describe('User that is not owner of movie', () => {
 
 });
 
-describe('User that is linked to waterfall', () => {
+describe('User that is linked to waterfall but not producer', () => {
   const projectId = `waterfall-rules-spec-${Date.now()}`;
   let db: Firestore;
 
@@ -283,13 +288,13 @@ describe('User that is linked to waterfall', () => {
 
   describe('Permissions', () => {
     test('Should not be able to create permissions document', async () => {
-      const ref = db.doc('waterfall/M001/permissions/O002');
-      await assertFails(ref.set({ id: 'O002', role: ['financier'] }));
+      const ref = db.doc('waterfall/MI-077/permissions/O002');
+      await assertFails(ref.set({ id: 'O002', roles: ['financier'] }));
     });
 
     test('Should not be able to update permissions document', async () => {
       const ref = db.doc('waterfall/MI-0d7/permissions/O002');
-      await assertFails(ref.set({ id: 'O002', role: ['financier'], scope: ['O001'] }));
+      await assertFails(ref.set({ id: 'O002', roles: ['financier'], scope: ['O001'] }));
     });
 
     test('Should be able to read his own permissions document', async () => {
@@ -310,8 +315,8 @@ describe('User that is linked to waterfall', () => {
 
   describe('Budget', () => {
     test('Should not be able to create budget document', async () => {
-      const ref = db.doc('waterfall/M001/budget/M001');
-      await assertFails(ref.set({ id: 'M001' }));
+      const ref = db.doc('waterfall/MI-077/budget/MI-077');
+      await assertFails(ref.set({ id: 'MI-077' }));
     });
 
     test('Should not be able to update budget document', async () => {
@@ -324,5 +329,130 @@ describe('User that is linked to waterfall', () => {
       await assertSucceeds(ref.get());
     });
   });
+});
+
+describe('User that is linked to waterfall with producer role', () => {
+  const projectId = `waterfall-rules-spec-${Date.now()}`;
+  let db: Firestore;
+
+  beforeAll(async () => {
+    db = await initFirestoreApp(projectId, 'firestore.rules', testFixture, 'uid-user2', { firebase: { sign_in_provider: 'password' } });
+  });
+
+  afterAll(() => Promise.all(getApps().map((app) => deleteApp(app))));
+
+  describe('Waterfall', () => {
+
+    test('Should be able to read waterfall document', async () => {
+      const ref = db.doc('waterfall/WF-001');
+      await assertSucceeds(ref.get());
+    });
+
+    test('Should be able to query waterfall documents', async () => {
+      const ref = db.collection('waterfall').where('orgIds', 'array-contains', 'O001');
+      await assertSucceeds(ref.get());
+    });
+
+    test('Should be able to update waterfall document', async () => {
+      const ref = db.doc('waterfall/WF-001');
+      await assertSucceeds(ref.set({ id: 'WF-001', notes: 'updated by unit test' }));
+    });
+
+    test('Should not be able to change waterfall document id', async () => {
+      const ref = db.doc('waterfall/WF-001');
+      await assertFails(ref.set({ id: 'foo' }));
+    });
+  });
+
+  describe('Blocks', () => {
+    test('Should be able to create block document', async () => {
+      const ref = db.doc('waterfall/WF-001/blocks/B002');
+      await assertSucceeds(ref.set({ id: 'B002', actions: {} }));
+    });
+
+    test('Should be able to update block document', async () => {
+      const ref = db.doc('waterfall/WF-001/blocks/B001');
+      await assertSucceeds(ref.set({ id: 'B001', actions: { 1: { name: 'init' } } }));
+    });
+
+    test('Should not be able to change block document id', async () => {
+      const ref = db.doc('waterfall/WF-001/blocks/B001');
+      await assertFails(ref.set({ id: 'B002' }));
+    });
+
+    test('Should be able to read block document', async () => {
+      const ref = db.doc('waterfall/WF-001/blocks/B001');
+      await assertSucceeds(ref.get());
+    });
+
+    test('Should be able to list block documents', async () => {
+      const ref = db.collection('waterfall/WF-001/blocks');
+      await assertSucceeds(ref.get());
+    });
+  });
+
+  describe('Budget', () => {
+    test('Should be able to create budget document', async () => {
+      const ref = db.doc('waterfall/WF-001/budget/WF-001');
+      await assertSucceeds(ref.set({ id: 'WF-001' }));
+    });
+
+    test('Should be able to update budget document', async () => {
+      const ref = db.doc('waterfall/WF-001/budget/WF-001');
+      await assertSucceeds(ref.set({ id: 'WF-001', foo:'bar' }));
+    });
+
+    test('Should not be able to change budget document id', async () => {
+      const ref = db.doc('waterfall/WF-001/budget/WF-001');
+      await assertFails(ref.set({ id: 'M00X' }));
+    });
+
+    test('Should be able to read budget document', async () => {
+      const ref = db.doc('waterfall/WF-001/budget/WF-001');
+      await assertSucceeds(ref.get());
+    });
+
+  });
+
+  describe('Permissions', () => {
+    test('Should be able to create permissions document', async () => {
+      const ref = db.doc('waterfall/WF-001/permissions/O003');
+      await assertSucceeds(ref.set({ id: 'O003', roles: ['financier'] }));
+    });
+
+    test('Should be able to update permissions document', async () => {
+      const ref = db.doc('waterfall/WF-001/permissions/foo');
+      await assertSucceeds(ref.set({ id: 'foo', roles: ['producer', 'financier'] }));
+    });
+
+    test('Should not be able to change permissions document id', async () => {
+      const ref = db.doc('waterfall/WF-001/permissions/O001');
+      await assertFails(ref.set({ id: 'O002' }));
+    });
+
+    test('Should be able to read permissions document', async () => {
+      const ref = db.doc('waterfall/WF-001/permissions/foo');
+      await assertSucceeds(ref.get());
+    });
+
+    test('Should be able to list permissions documents', async () => {
+      const ref = db.collection('waterfall/WF-001/permissions');
+      await assertSucceeds(ref.get());
+    });
+
+    test('Should not be able to read permissions document if not producer anymore', async () => {
+      const ref1 = db.doc('waterfall/WF-001/permissions/foo');
+      await assertSucceeds(ref1.get());
+
+      // Self remove producer role
+      const ref2 = db.doc('waterfall/WF-001/permissions/O001');
+      await assertSucceeds(ref2.set({ id: 'O001', roles: ['financier'] }));
+
+      const ref3 = db.doc('waterfall/WF-001/permissions/foo');
+      await assertFails(ref3.get());
+
+    });
+  });
+
 });
 
--- apps/waterfall/waterfall/src/app/dashboard/title/waterfall/waterfall.component.ts
+++ apps/waterfall/waterfall/src/app/dashboard/title/waterfall/waterfall.component.ts
@@ -1,10 +1,15 @@
 // Angular
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 
 // Blockframes
-import { waterfall } from '@blockframes/waterfall/main';
 import { actions } from '@blockframes/waterfall/fixtures/terrawilly-demo';
-
+import { ActivatedRoute } from '@angular/router';
+import { WaterfallService } from '@blockframes/waterfall/waterfall.service';
+import { WaterfallPermissionsService } from '@blockframes/waterfall/permissions.service';
+import { TitleState, History, Waterfall } from '@blockframes/model';
+import { BehaviorSubject } from 'rxjs';
+import { BlockService } from '@blockframes/waterfall/block.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'waterfall-title-waterfall',
@@ -12,7 +17,55 @@ import { actions } from '@blockframes/waterfall/fixtures/terrawilly-demo';
   styleUrls: ['./waterfall.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WaterfallComponent {
+export class WaterfallComponent implements OnInit {
 
-  tree = waterfall('terrawilly-demo', actions);
+  public isLoading$ = new BehaviorSubject(true);
+  public tree: { state: TitleState; history: History[] };
+
+  private waterfall: Waterfall;
+
+  constructor(
+    private route: ActivatedRoute,
+    private waterfallService: WaterfallService,
+    private waterfallPermissionsService: WaterfallPermissionsService,
+    private blockService: BlockService,
+    private snackbar: MatSnackBar,
+  ) { }
+
+  async ngOnInit() {
+    const waterfallId: string = this.route.snapshot.params.movieId;
+    this.waterfall = await this.waterfallService.getValue(waterfallId);
+    await this.loadWaterfall();
+  }
+
+  async loadWaterfall() {
+    if (this.waterfall.versions.length) {
+      const firstVersion = this.waterfall.versions.shift();
+      const data = await this.waterfallService.buildWaterfall({ waterfallId: this.waterfall.id, versionId: firstVersion.id });
+      this.tree = data.waterfall;
+    }
+    this.isLoading$.next(false);
+  }
+
+  async initWaterfall() {
+    this.isLoading$.next(true);
+
+    const isProducer = await this.waterfallPermissionsService.hasRole(this.waterfall.id, 'producer');
+
+    if (!isProducer) {
+      this.snackbar.open('Only user with producer role can initialize waterfall', 'close', { duration: 5000 });
+      this.isLoading$.next(false);
+      return;
+    }
+
+    // TODO temp: initialize waterfall with json fixtures
+    const [init] = await Promise.all([
+      this.blockService.create(this.waterfall.id, 'init', actions),
+      this.waterfallService.addVersion(this.waterfall, { id: 'version_1', description: 'First Version' })
+    ]);
+
+    this.waterfall = await this.waterfallService.addBlocksToVersion(this.waterfall, 'version_1', [init]);
+
+    await this.loadWaterfall();
+  }
 }
