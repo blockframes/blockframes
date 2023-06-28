e6477cdc67c45d78662efc6d30bcd015bbc071c6
pldespaigne <pl.despaigne@gmail.com>
2023-06-28 15:07:04 +0200

    comment WIP code

--- apps/waterfall/waterfall/src/app/dashboard/title/documents/documents.component.ts
+++ apps/waterfall/waterfall/src/app/dashboard/title/documents/documents.component.ts
@@ -1,8 +1,19 @@
 
-// Angular
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+// External
+// import { FormControl } from '@angular/forms';
+// import { arrayUnion } from 'firebase/firestore';
+// import { ActivatedRoute } from '@angular/router';
+import { Component, ChangeDetectionStrategy, OnInit, OnDestroy } from '@angular/core';
+// import { BehaviorSubject, Observable, Subscription, combineLatest, map, switchMap, tap } from 'rxjs';
+
 
 // Blockframes
+// import { Waterfall, WaterfallAmendment, WaterfallDocument, WaterfallFile } from '@blockframes/model';
+// import { OrganizationService } from '@blockframes/organization/service';
+// import { WaterfallFileForm } from '@blockframes/waterfall/form/file.form';
+// import { WaterfallService } from '@blockframes/waterfall/waterfall.service';
+// import { FileUploaderService } from '@blockframes/media/file-uploader.service';
+// import { WaterfallDocumentsService } from '@blockframes/waterfall/documents.service';
 
 
 @Component({
@@ -11,6 +22,169 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./documents.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DocumentsComponent {
+export class DocumentsComponent implements OnInit, OnDestroy {
 
+  // TODO THIS IS EXPRIMENTAL CODE THAT COULD BE USEFUL FOR IMPLEMENTING THE DOCUMENT SYSTEM
+  // TODO DELETE THAT LATER issue #9389
+  ngOnInit(): void {
+    return; // nothing
+  }
+  ngOnDestroy(): void {
+    return; // nothing
+  }
+
+  // movieId: string;
+  // waterfall$: Observable<Waterfall>;
+  // documents$: Observable<unknown[]>;
+  // members$: Observable<{ name: string, id: string }[]>;
+
+  // fakeOrgName = new FormControl('');
+
+  // newDocumentId: string;
+  // fileForm:WaterfallFileForm;
+  // documentType = new FormControl('contract');
+  // licensor = new FormControl({ name: '', id: '' });
+  // licensee = new FormControl({ name: '', id: '' });
+  // folder = new FormControl('');
+  // dummyValue = new FormControl('');
+  // parentDocument = new FormControl(undefined);
+  // creating$ = new BehaviorSubject(false);
+  // parentDocSubscription?: Subscription;
+
+  // constructor(
+  //   private route: ActivatedRoute,
+  //   private waterfallService: WaterfallService,
+  //   private uploaderService: FileUploaderService,
+  //   private organizationService: OrganizationService,
+  //   private documentsService: WaterfallDocumentsService,
+  // ) {
+  //   this.movieId = this.route.snapshot.params.movieId;
+  //   this.newDocumentId = this.documentsService.createId();
+  //   this.fileForm = new WaterfallFileForm({ id: this.newDocumentId });
+  // }
+
+  // ngOnInit() {
+  //   this.waterfall$ = this.waterfallService.valueChanges(this.movieId);
+  //   const subDocs$ = this.documentsService.valueChanges({ waterfallId: this.movieId });
+  //   this.documents$ = combineLatest([
+  //     this.waterfall$,
+  //     subDocs$,
+  //   ]).pipe(
+  //     map(([waterfall, docs]) => { // merge WaterfallFile and (WaterfallDocument & WaterfallAmendment)
+  //       return waterfall.documents.map(file => ({
+  //         ...file,
+  //         ...docs.find(d => d.id === file.id),
+  //       }));
+  //     }),
+  //     map(docs => { // merge Amendment into their root parent document
+  //       const amendments = docs.filter(doc => doc.type === 'amendment') as (WaterfallAmendment & WaterfallFile)[];
+  //       const contracts = docs.filter(doc => doc.type !== 'amendment') as (WaterfallDocument & WaterfallFile)[];
+
+  //       const rootToChildren: Record<string, (WaterfallAmendment & WaterfallFile)[]> = {};
+  //       amendments.forEach(am => {
+  //         const children: (WaterfallAmendment & WaterfallFile)[] = [am];
+
+  //         for (let i = 0 ; i < 200 ; i++) {
+
+  //           const current = children[i];
+  //           if (!current) break;
+
+  //           const parent = docs.find(doc => doc.id === current.parentId);
+  //           if (!parent) break;
+
+  //           if (parent.type !== 'amendment') {
+  //             rootToChildren[parent.id] ||= [];
+  //             if (rootToChildren[parent.id].length < children.length) {
+  //               rootToChildren[parent.id] = children;
+  //             }
+  //           } else { 
+  //             children.push(parent as (WaterfallAmendment & WaterfallFile));
+  //           }
+  //         }
+  //       });
+
+  //       return contracts.map(doc => {
+  //         const children = rootToChildren[doc.id] ?? [];
+  //         const targetId = children[0]?.id ?? doc.id;
+  //         return { ...doc, children, targetId };
+  //       });
+  //     }),
+  //     tap(console.log),
+  //   );
+
+  //   const realOrg$ = this.waterfall$.pipe(
+  //     switchMap(waterfall => this.organizationService.valueChanges(waterfall.orgIds)),
+  //   );
+    
+  //   this.members$ = combineLatest([
+  //     realOrg$,
+  //     this.waterfall$,
+  //   ]).pipe(
+  //     map(([realOrgs, waterfall]) => {
+  //       const real = realOrgs.map(org => ({ name: org.name, id: org.id }));
+  //       const fake = waterfall.fakeOrgs.map(name => ({ name, id: 'fake' }));
+  //       return [...real, ...fake, ];
+  //     }),
+  //   );
+
+  //   this.parentDocSubscription = this.parentDocument.valueChanges.subscribe(doc => {
+  //     if (!doc) return;
+  //     this.dummyValue.setValue(doc.meta);
+  //   });
+  // }
+
+  // ngOnDestroy() {
+  //   this.parentDocSubscription?.unsubscribe();
+  // }
+
+  // async createFakeOrg() {
+  //   if (!this.fakeOrgName.value) return;
+
+  //   await this.waterfallService.update({ id: this.movieId, fakeOrgs: arrayUnion(this.fakeOrgName.value) });
+  //   this.fakeOrgName.setValue('');
+  // }
+
+  // compareOrgWith(option: { name: string, id: string }, value: { name: string, id: string }) {
+  //   return option.name === value.name;
+  // }
+
+  // async createDocument() {
+  //   this.creating$.next(true);
+
+  //   if (this.documentType.value === 'amendment') {
+  //     this.documentsService.add<WaterfallAmendment>({
+  //       id: this.newDocumentId,
+  //       type: 'amendment',
+  //       waterfallId: this.movieId,
+  //       parentId: this.parentDocument.value.targetId,
+  //       meta: this.dummyValue.value,
+  //     }, { params: { waterfallId: this.movieId } });
+  //   } else {
+  //     this.documentsService.add<WaterfallDocument>({
+  //       id: this.newDocumentId,
+  //       folder: this.folder.value,
+  //       waterfallId: this.movieId,
+  //       type: this.documentType.value as any,
+  //       ownerId: this.organizationService.org.id,
+  //       sharedWith: [],
+  //       meta: this.dummyValue.value,
+  //       licensor: this.licensor.value,
+  //       licensee: this.licensee.value,
+  //     }, { params: { waterfallId: this.movieId } });
+  //   }
+
+  //   this.uploaderService.upload();
+
+  //   // reset
+  //   this.newDocumentId = this.documentsService.createId();
+  //   this.fileForm = new WaterfallFileForm({ id: this.newDocumentId });
+  //   this.documentType.setValue('contract');
+  //   this.folder.setValue('');
+  //   this.dummyValue.setValue('');
+  //   this.licensor.setValue({ id: '', name: '' });
+  //   this.licensee.setValue({ id: '', name: '' });
+  //   this.parentDocument.setValue(undefined);
+
+  //   this.creating$.next(false);
+  // }
 }
--- apps/waterfall/waterfall/src/app/dashboard/title/documents/documents.module.ts
+++ apps/waterfall/waterfall/src/app/dashboard/title/documents/documents.module.ts
@@ -3,8 +3,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { IfModule } from '@rx-angular/template/if';
+import { ForModule } from '@rx-angular/template/for';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 // Blockframes
+import { FileNameModule } from '@blockframes/utils/pipes';
+import { ImageModule } from '@blockframes/media/image/directives/image.module';
+import { GetOrgPipeModule } from '@blockframes/organization/pipes/get-org.pipe';
+import { LogoSpinnerModule } from '@blockframes/ui/logo-spinner/logo-spinner.module';
+import { FileUploaderModule } from '@blockframes/media/file/file-uploader/file-uploader.module';
 
 // Pages
 import { DocumentsComponent } from './documents.component';
@@ -12,11 +26,25 @@ import { DocumentsComponent } from './documents.component';
 @NgModule({
   declarations: [DocumentsComponent],
   imports: [
+    IfModule,
+    ForModule,
     CommonModule,
+    ReactiveFormsModule,
 
     // Blockframes
+    LogoSpinnerModule,
+    GetOrgPipeModule,
+    FileUploaderModule,
+    FileNameModule,
+    ImageModule,
 
     // Material
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatProgressSpinnerModule,
 
     // Routing
     RouterModule.forChild([{ path: '', component: DocumentsComponent }]),
