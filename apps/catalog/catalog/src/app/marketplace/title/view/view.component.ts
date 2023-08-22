c66cfff5bcb59c5d24c8fb6b982e4cad5bf5cedf
Nicolas <n.germain.work@gmail.com>
2023-08-22 16:31:18 +0200

    spacing

--- apps/catalog/catalog/src/app/marketplace/title/view/view.component.ts
+++ apps/catalog/catalog/src/app/marketplace/title/view/view.component.ts
@@ -64,7 +64,7 @@ export class MarketplaceMovieViewComponent implements AfterViewInit {
     private dialog: MatDialog,
     private cdr: ChangeDetectorRef,
     @Optional() private intercom: Intercom
-    ) { }
+  ) { }
 
   ngAfterViewInit() {
     setTimeout(() => {
