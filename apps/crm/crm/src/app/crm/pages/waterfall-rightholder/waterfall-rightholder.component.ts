9f54b91fb8f4229302b113ee44d78e5b95b0c215
bruce <delorme.bruce.michel@gmail.com>
2023-08-31 15:37:01 +0200

    rightholder page details

---
+++ apps/crm/crm/src/app/crm/pages/waterfall-rightholder/waterfall-rightholder.component.ts
@@ -0,0 +1,61 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { Movie, Right, RightholderRole, Waterfall, WaterfallRightholder } from '@blockframes/model';
+import { MovieService } from '@blockframes/movie/service';
+import { RightService } from '@blockframes/waterfall/right.service';
+import { WaterfallService } from '@blockframes/waterfall/waterfall.service';
+import { where } from 'firebase/firestore';
+
+@Component({
+  selector: 'crm-waterfall-rightholder',
+  templateUrl: './waterfall-rightholder.component.html',
+  styleUrls: ['./waterfall-rightholder.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class WaterfallRightholderComponent implements OnInit {
+  public movie: Movie;
+  public waterfall: Waterfall;
+  public rightholder: WaterfallRightholder;
+  public waterfallRoleControl = new FormControl<RightholderRole[]>(undefined, [Validators.required]);
+  public rights: Right[] = [];
+
+  constructor(
+    private movieService: MovieService,
+    private waterfallService: WaterfallService,
+    private rightService: RightService,
+    private route: ActivatedRoute,
+    private cdRef: ChangeDetectorRef,
+    private snackBar: MatSnackBar
+  ) { }
+
+  async ngOnInit() {
+    const waterfallId = this.route.snapshot.paramMap.get('waterfallId');
+    const rightholderId = this.route.snapshot.paramMap.get('rightholderId');
+    const [movie, waterfall, rights] = await Promise.all([
+      this.movieService.getValue(waterfallId),
+      this.waterfallService.getValue(waterfallId),
+      this.rightService.getValue([where('rightholderId', '==', rightholderId)], { waterfallId }),
+    ])
+    this.movie = movie;
+    this.waterfall = waterfall;
+    this.rights = rights;
+
+    this.rightholder = this.waterfall.rightholders.find(r => r.id === rightholderId);
+
+    this.waterfallRoleControl.setValue(this.rightholder.roles);
+
+    this.cdRef.markForCheck();
+  }
+
+  public async save() {
+    const index = this.waterfall.rightholders.indexOf(this.rightholder);
+    this.rightholder.roles = this.waterfallRoleControl.value;
+    this.waterfall.rightholders[index] = this.rightholder;
+
+    await this.waterfallService.update({ id: this.waterfall.id, rightholders: this.waterfall.rightholders });
+
+    this.snackBar.open('Roles updated', 'close', { duration: 3000 });
+  }
+}
---
+++ apps/crm/crm/src/app/crm/pages/waterfall-rightholder/waterfall-rightholder.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+
+import { WaterfallRightholderComponent } from './waterfall-rightholder.component';
+
+// Blockframes
+import { MovieHeaderModule } from '@blockframes/movie/components/header/header.module';
+import { StaticSelectModule } from '@blockframes/ui/static-autocomplete/select/static-select.module';
+import { TableModule } from '@blockframes/ui/list/table/table.module';
+
+// Material
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+@NgModule({
+  declarations: [WaterfallRightholderComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ClipboardModule,
+    
+    MovieHeaderModule,
+    StaticSelectModule,
+    TableModule,
+
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
+
+    RouterModule.forChild([{ path: '', component: WaterfallRightholderComponent }])
+  ]
+})
+export class WaterfallRightholderModule { }
