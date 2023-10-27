5174c1d76df065b564c2dc0c22f134cff3234dd5
bruce <delorme.bruce.michel@gmail.com>
2023-10-27 10:46:38 +0200

    added dashboard list

--- apps/crm/crm/src/app/crm/pages/waterfall/view/view.module.ts
+++ apps/crm/crm/src/app/crm/pages/waterfall/view/view.module.ts
@@ -22,52 +22,57 @@ const routes: Routes = [{
       data: { animation: 0 }
     },
     {
+      path: 'dashboards',
+      loadChildren: () => import('../dashboards/dashboards.module').then(m => m.DashboardsModule),
+      data: { animation: 0 }
+    },
+    {
       path: 'rightholders',
       loadChildren: () => import('../rightholders/rightholders.module').then(m => m.RightholdersModule),
-      data: { animation: 1 }
+      data: { animation: 2 }
     },
     {
       path: 'documents',
       loadChildren: () => import('../documents/documents.module').then(m => m.DocumentsModule),
-      data: { animation: 2 }
+      data: { animation: 3 }
     },
     {
       path: 'contracts',
       loadChildren: () => import('../contracts/contracts.module').then(m => m.ContractsModule),
-      data: { animation: 3 }
+      data: { animation: 4 }
     },
     {
       path: 'sources',
       loadChildren: () => import('../sources/sources.module').then(m => m.SourcesModule),
-      data: { animation: 4 }
+      data: { animation: 5 }
     },
     {
       path: 'rights',
       loadChildren: () => import('../rights/rights.module').then(m => m.RightsModule),
-      data: { animation: 5 }
+      data: { animation: 6 }
     },
     {
       path: 'statements',
       loadChildren: () => import('../statements/statements.module').then(m => m.StatementsModule),
-      data: { animation: 6 }
+      data: { animation: 7 }
     },
     {
       path: 'incomes',
       loadChildren: () => import('../incomes/incomes.module').then(m => m.IncomesModule),
-      data: { animation: 7 }
+      data: { animation: 8 }
     },
     {
       path: 'expenses',
       loadChildren: () => import('../expenses/expenses.module').then(m => m.ExpensesModule),
-      data: { animation: 8 }
+      data: { animation: 9 }
     },
     {
       path: 'sales',
       loadChildren: () => import('../sales/sales.module').then(m => m.SalesModule),
-      data: { animation: 9 }
+      data: { animation: 10 }
     },
     {
-      path: 'dashboard/:versionId',
+      path: 'dashboards/:versionId',
       loadChildren: () => import('../dashboard/dashboard.module').then(m => m.DashboardModule)
     },
     {
