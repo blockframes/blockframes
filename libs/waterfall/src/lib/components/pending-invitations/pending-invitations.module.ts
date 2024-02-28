c884adb27d682c9ead36fd71625447d5be93818e
bruce <delorme.bruce.michel@gmail.com>
2024-02-28 12:54:53 +0100

    WIP

---
+++ libs/waterfall/src/lib/components/pending-invitations/pending-invitations.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// Components
+import { PendingInvitationsComponent } from './pending-invitations.component';
+
+// Blockframes
+import { TableModule } from '@blockframes/ui/list/table/table.module';
+import { DisplayNameModule, ToLabelModule } from '@blockframes/utils/pipes';
+
+// Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+@NgModule({
+  declarations: [
+    PendingInvitationsComponent,
+  ],
+  imports: [
+    CommonModule,
+
+    // Blockframes
+    TableModule,
+    DisplayNameModule,
+    ToLabelModule,
+
+    // Material
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+  ],
+  exports: [
+    PendingInvitationsComponent
+  ]
+})
+export class PendingInvitationsModule { }
