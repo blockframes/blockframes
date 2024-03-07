42c0690f342c3fb8cd2537e80df5e20c597d24a0
bruce <delorme.bruce.michel@gmail.com>
2024-03-07 17:29:07 +0100

    WIP document component

--- apps/waterfall/waterfall/src/app/dashboard/title/documents/documents.component.ts
+++ apps/waterfall/waterfall/src/app/dashboard/title/documents/documents.component.ts
@@ -2,6 +2,9 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 import { WaterfallContractForm } from '@blockframes/waterfall/form/contract.form';
+import { BehaviorSubject } from 'rxjs';
+
+type Path = 'Documents' | 'Contracts' | 'Financing Plan' | 'Budget';
 
 @Component({
   selector: 'waterfall-title-documents',
@@ -11,6 +14,12 @@ import { WaterfallContractForm } from '@blockframes/waterfall/form/contract.form
 })
 export class DocumentsComponent {
 
-  contractForm = new WaterfallContractForm({ id: '' });
+  public contractForm = new WaterfallContractForm({ id: '' });
+  public crumbs$ = new BehaviorSubject<Path[]>(['Documents']);
+  public currentPath$ = new BehaviorSubject<Path>('Documents');
 
+  public navigate(path: Path[]) {
+    this.crumbs$.next(path);
+    this.currentPath$.next(path[path.length - 1]);
+  }
 }
--- apps/waterfall/waterfall/src/app/dashboard/title/documents/documents.module.ts
+++ apps/waterfall/waterfall/src/app/dashboard/title/documents/documents.module.ts
@@ -6,11 +6,16 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 // Blockframes
-import { ContractListModule } from '@blockframes/waterfall/components/contract-list/contract-list.module';
-import { LogoSpinnerModule } from '@blockframes/ui/logo-spinner/logo-spinner.module';
+import { ContractListModule } from '@blockframes/waterfall/components/contract/contract-list/contract-list.module';
 
 // Pages
 import { DocumentsComponent } from './documents.component';
+
+// Material
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [DocumentsComponent],
@@ -20,10 +25,12 @@ import { DocumentsComponent } from './documents.component';
 
     // Blockframes
     ContractListModule,
-    LogoSpinnerModule,
 
     // Material
-
+    MatDividerModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
 
     // Routing
     RouterModule.forChild([{ path: '', component: DocumentsComponent }]),
