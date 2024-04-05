dc85ebbaa5c4ab6bff67ce905328896247d624e8
bruce <delorme.bruce.michel@gmail.com>
2024-04-05 15:59:53 +0200

    debug

--- libs/waterfall/src/lib/pipes/can-add-child.pipe.ts
+++ libs/waterfall/src/lib/pipes/can-add-child.pipe.ts
@@ -1,10 +1,10 @@
 import { NgModule, Pipe, PipeTransform } from '@angular/core';
-import { RightNode, VerticalNode } from '../components/graph/layout';
+import { HorizontalNode, RightNode, VerticalNode } from '../components/graph/layout';
 
 @Pipe({ name: 'canAddChild' })
 export class CanAddPipe implements PipeTransform {
-  transform(right: RightNode | VerticalNode) {
-    if (right.children.length > 1) return false;
+  transform(right: RightNode | VerticalNode, group?: VerticalNode | HorizontalNode) {
+    if (right.children.length > 1 || group?.children.length > 1) return false;
     return true;
   }
 }
