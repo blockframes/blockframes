7255426f595dfa62f8a132d0595d2e146e96caa7
bruce <delorme.bruce.michel@gmail.com>
2024-05-13 12:49:12 +0200

    i18n

--- libs/waterfall/src/lib/components/graph/conditions/step-item/step-item.component.ts
+++ libs/waterfall/src/lib/components/graph/conditions/step-item/step-item.component.ts
@@ -1,7 +1,6 @@
 
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 
-
 @Component({
   selector: 'waterfall-step-item',
   templateUrl: './step-item.component.html',
@@ -12,7 +11,13 @@ export class WaterfallStepItemComponent {
 
   @Input() index: number;
   @Input() conditionCount: number;
-  @Input() canUpdate: boolean;
+  @Input() set canUpdate(canUpdate: boolean) {
+    this._canUpdate = canUpdate;
+    this.tooltip = this._canUpdate ? $localize`Edit Step Conditions` : $localize`View Step Conditions`;
+  }
+  get canUpdate() { return this._canUpdate; }
+  public tooltip = $localize`Edit Step Conditions`;
+  private _canUpdate: boolean;
 
   @Output() editStep = new EventEmitter();
   @Output() deleteStep = new EventEmitter();
