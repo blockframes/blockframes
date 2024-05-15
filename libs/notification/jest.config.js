662cb64e7c9bc750ca6e479afc8b5765e8514bce
bruce <delorme.bruce.michel@gmail.com>
2024-05-15 14:28:43 +0200

    test jest 29

--- libs/notification/jest.config.js
+++ libs/notification/jest.config.js
@@ -7,7 +7,6 @@
   transform: {
     '^.+.(ts|mjs|js|html)$': 'jest-preset-angular'
   },
-  testEnvironment: '<rootDir>/jest-environment.js',
   transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
   moduleFileExtensions: ['ts', 'js', 'html'],
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
