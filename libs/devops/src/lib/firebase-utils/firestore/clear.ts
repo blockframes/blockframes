6013aaa95e10b44115e260371e9dd2dcc2d76259
bruce <delorme.bruce.michel@gmail.com>
2024-06-05 16:18:46 +0200

    fixed too much retention bug when running clearDb

--- libs/devops/src/lib/firebase-utils/firestore/clear.ts
+++ libs/devops/src/lib/firebase-utils/firestore/clear.ts
@@ -12,8 +12,27 @@ export async function clearDb(db: FirebaseFirestore.Firestore, allowProd = false
   if (!allowProd) throwOnProduction();
   const collections = (await db.listCollections()).filter((ref) => ref.id !== META_COLLECTION_NAME).map(ref => ref.id);
   const cmds = collections.map(collection => `firebase firestore:delete -P ${firebase().projectId} -r -f ${collection}`);
+  let retriesLeft = 10;
   for (const cmd of cmds) {
-    await runShellCommandExec(cmd);
+    try {
+      await runShellCommandExec(cmd);
+    } catch (e) {
+      /** @see https://firebase.google.com/docs/firestore/transaction-data-contention?hl=fr#transactions_and_data_contention*/
+      if (e.status === 409) {
+        console.log(`Command "${cmd}" failed with HTTP Error: 409, Too much contention on these documents. Please try again.`);
+      } else {
+        console.error(e);
+      }
+
+      if (retriesLeft === 0) {
+        throw new Error(`Command "${cmd}" failed and no more retries left..`);
+      } else {
+        retriesLeft--;
+        console.log(`Command "${cmd}" failed and will be retryied... ${retriesLeft} retries left`);
+        cmds.push(cmd);
+      }
+    }
+
   }
   await versionRef(db).delete();
   console.log(`Deleted ${dbVersionDoc}`);
