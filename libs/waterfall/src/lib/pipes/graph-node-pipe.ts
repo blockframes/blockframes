db3f4a464126cb7060ec4bb33f6b24c2a3ff63fc
bruce <delorme.bruce.michel@gmail.com>
2024-07-26 17:02:42 +0200

    removed isStep pipe

--- libs/waterfall/src/lib/pipes/graph-node-pipe.ts
+++ libs/waterfall/src/lib/pipes/graph-node-pipe.ts
@@ -1,19 +1,11 @@
 import { NgModule, Pipe, PipeTransform } from '@angular/core';
-import { Right, RightType } from '@blockframes/model';
+import { RightType } from '@blockframes/model';
 import { HorizontalNode, Node, VerticalNode } from '../components/graph/layout';
 
 @Pipe({ name: 'isHorizontal' })
 export class IsHorizontalPipe implements PipeTransform {
   transform(type: RightType) {
     return type === 'horizontal';
-  }
-}
-
-@Pipe({ name: 'isStep' }) // TODO #9896 remove this pipe as we cannot select steps anymore
-export class IsStepPipe implements PipeTransform {
-  transform(id: string, rights: Right[]) {
-    const groupId = rights.find(r => r.id === id)?.groupId;
-    return rights.find(r => r.id === groupId)?.type === 'vertical';
   }
 }
 
@@ -31,7 +23,7 @@ export class CanAddPipe implements PipeTransform {
 }
 
 @NgModule({
-  declarations: [IsHorizontalPipe, IsStepPipe, CanAddPipe],
-  exports: [IsHorizontalPipe, IsStepPipe, CanAddPipe],
+  declarations: [IsHorizontalPipe, CanAddPipe],
+  exports: [IsHorizontalPipe, CanAddPipe],
 })
 export class GraphNodePipeModule { }
